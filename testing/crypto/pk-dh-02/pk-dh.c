1fe0b93f4f8263f7c322483079c6657c026c327c
Michael Richardson <mcr@xelerance.com>
2006-08-24 16:51:44 -0400

    test case for doing modp operations on hifn7956.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/crypto/pk-dh-02/pk-dh.c
+++ testing/crypto/pk-dh-02/pk-dh.c
@@ -1,5 +1,6 @@
 /* 
  * unit tests for cryptographic helper function - calculate KE and nonce
+ *             using the memory mapped hifn 7956.
  *
  * Copyright (C) 2006 Michael C. Richardson <mcr@xelerance.com>
  *
@@ -19,4 +20,55 @@
 #include <fcntl.h>
 
 #define VULCAN_PK 1
-#include "../pk-dh-01/pk-dh.c"
+#define PK_DH_REGRESS 1
+
+int pkdh_verbose=0;
+
+#include "../../../programs/pluto/hmac.c"
+#include "../../../programs/pluto/crypto.c"
+#include "../../../programs/pluto/ike_alg.c"
+#include "../../../programs/pluto/crypt_utils.c"
+#include "../../../programs/pluto/crypt_dh.c"
+
+#include "crypto.h"
+
+char *progname;
+
+void exit_log(const char *message, ...)
+{
+	va_list args;
+	char m[LOG_WIDTH];	/* longer messages will be truncated */
+
+	va_start(args, message);
+	vsnprintf(m, sizeof(m), message, args);
+	va_end(args);
+
+	fprintf(stderr, "FATAL ERROR: %s\n", m);
+	exit(0);
+}
+
+void exit_tool(int code)
+{
+	exit(code);
+}
+
+#include "../pk-dh-01/tc2.c"
+
+int main(int argc, char *argv[])
+{
+	progname = argv[0];
+	
+	/* initialize list of moduli */
+	init_crypto();
+
+	if(argc>1) {
+		pk_verbose_execute=1;
+	}
+
+	calc_dh_shared = calc_dh_shared_vulcanpk;
+
+	perform_t2_test();
+
+	exit(0);
+}
+
