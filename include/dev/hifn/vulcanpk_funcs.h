881e00e3839f21703d53130aa83fa5c38edddfba
Michael Richardson <mcr@xelerance.com>
2006-09-01 14:42:55 -0400

    fix hexdump prototype
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- include/dev/hifn/vulcanpk_funcs.h
+++ include/dev/hifn/vulcanpk_funcs.h
@@ -3,7 +3,7 @@
 extern unsigned char * mapvulcanpk(void);
 extern void unmapvulcanpk(unsigned char *mapping);
 extern void print_status(u_int32_t stat);
-extern void hexdump(caddr_t base, unsigned int offset, int len);
+extern void hexdump(const unsigned char *base, unsigned int offset, int len);
 struct pkprogram;
 extern void execute_pkprogram(unsigned char *mapping, struct pkprogram *prog);
 extern void vulcanpk_init(unsigned char *mapping);
