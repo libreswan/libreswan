daa080ad9e4ecd67ea15d493244caae7d1449da7
Michael Richardson <mcr@xelerance.com>
2007-11-03 12:14:21 -0400

    make sure that backquote is dropped when we pass items to the shell.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- programs/pluto/lex.c
+++ programs/pluto/lex.c
@@ -11,7 +11,7 @@
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
- * RCSID $Id: lex.c,v 1.3 2004/04/27 12:43:27 ken Exp $
+ * RCSID $Id: lex.c,v 1.3.24.1 2007/10/22 15:55:13 paul Exp $
  */
 
 #include <stdio.h>
@@ -141,6 +141,7 @@ shift(void)
 
 	case '"':	/* quoted token */
 	case '\'':
+	case '`':   /* or execute quotes */
 	    if (p != sor)
 	    {
 		/* we have a quoted token: note and advance to its end */
