d2e396afa67a83e547144056b10a4ce18664f855
Paul Wouters <paul@libreswan.org>
2012-09-09 13:48:12 -0400

    * testing: change from transient to persistent networking in libvirt
    I had not realised virsh net-create only creates a "temporary" network.
    We have to use net-define to make it permanent. The xml files don't
    define whether to start it now or on boot, so we have to issue virsh
    commands for that as well.
    
    Also added the 192.9.2.0/24 and 192.9.4.0/24 networks (eth2 on the umls)

--- testing/fedora-setup/network-libvirt.sh
+++ testing/fedora-setup/network-libvirt.sh
@@ -4,10 +4,12 @@ for net in net/swan*
 do
   if [ ! -d /sys/class/$net ];
   then
-     sudo virsh net-create $net
-     echo $net created and activated
+	sudo virsh net-define $net
+	sudo virsh net-autostart $net
+	sudo virsh net-start 
+	echo $net created and activated
   else
-     echo $net already exists - not created
+	echo $net already exists - not created
   fi
 done
 
