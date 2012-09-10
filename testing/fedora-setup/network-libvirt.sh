906e7c4f6c674968cbf9a71acddf61b437c8248c
Paul Wouters <paul@libreswan.org>
2012-09-09 23:33:06 -0400

    * testing: use proper network names with virsh

--- testing/fedora-setup/network-libvirt.sh
+++ testing/fedora-setup/network-libvirt.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-for net in net/swan*
+for netname in net/swan*
 do
+  net=`echo $netname|sed "s/^net\///g"`
   if [ ! -d /sys/class/$net ];
   then
-	sudo virsh net-define $net
-	sudo virsh net-autostart $net
-	sudo virsh net-start 
-	echo $net created and activated
+	sudo virsh net-define net/$net
+	echo $net created 
   else
 	echo $net already exists - not created
   fi
 done
 
+for net in `virsh net-list --inactive| sed "s/^\(192.*\) *inactive.*$/\1/" |grep 192`
+do
+	sudo virsh net-autostart $net
+	sudo virsh net-start $net
+	echo $net activated
+done
