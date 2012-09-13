49d12deef02a5ae86d9ef0d6aa5245cbacd7be4f
Paul Wouters <paul@libreswan.org>
2012-09-13 16:25:39 -0400

    * testing: fix waiting-for-pluto in ikev2-05 test case

--- testing/pluto/ikev2-05-basic-psk/eastinit.sh
+++ testing/pluto/ikev2-05-basic-psk/eastinit.sh
@@ -3,6 +3,6 @@ TESTNAME=ikev2-05-basic-psk
 source /testing/pluto/bin/eastnlocal.sh
 
 ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
 ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add  westnet--eastnet-ikev2
-/testing/pluto/bin/wait-until-pluto-started
