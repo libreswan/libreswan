f2ad0c5a8c4fba213412edd7a8070aa9d929304c
Paul Wouters <paul@libreswan.org>
2012-09-14 11:13:30 -0400

    * testing: export testing/ not testing/fedora-setup/ to the guests

--- testing/fedora-setup/vm-libvirt.sh
+++ testing/fedora-setup/vm-libvirt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TESTING=`readlink -f $0  | sed "s/fedora-setup.*$/fedora-setup/"`
+TESTING=`readlink -f $0  | sed "s/testing.*$/testing/"`
 pushd $TESTING
 
 for hostname in east west;
@@ -9,7 +9,7 @@ do
 	cp vm/$hostname.xml vm/$hostname.xml.converted
 	sed -i "s:@@TESTING@@:$TESTING:" vm/$hostname.xml.converted
         sudo virsh define vm/$hostname.xml.converted
-	rm -f vm/$hostname.xml.converted 
+	#rm -f vm/$hostname.xml.converted 
         sudo virsh start $hostname
 done
 popd
