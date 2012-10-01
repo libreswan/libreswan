c61ae4523157596f97fe37068ff248ccba427aeb
Paul Wouters <paul@libreswan.org>
2012-10-01 19:12:04 -0400

    * testing: Updates to ipsec.conf.common - converted IKEv2 tests to NSS

--- testing/pluto/ikev2-allow-narrow-05/westinit.sh
+++ testing/pluto/ikev2-allow-narrow-05/westinit.sh
@@ -13,7 +13,7 @@ ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 
 ipsec whack --whackrecord /var/tmp/ikev2.record
-ipsec auto --add westnet--eastnet-ikev2
+ipsec auto --add westnet-eastnet-ikev2
 ipsec auto --status
 ipsec whack --debug-control --debug-controlmore --debug-parsing --debug-crypt
 
