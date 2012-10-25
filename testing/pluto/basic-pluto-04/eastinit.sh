e1ded44882a004fe491eacfea0bdbcf6bb0d95ad
Paul Wouters <paul@libreswan.org>
2012-10-24 20:15:48 -0400

    * testing: fixup basic-pluto-04 (untested)

--- testing/pluto/basic-pluto-04/eastinit.sh
+++ testing/pluto/basic-pluto-04/eastinit.sh
@@ -7,3 +7,4 @@ ipsec setup start
 
 ipsec auto --add westnet-eastnet-aes128
 /testing/pluto/basic-pluto-01/eroutewait.sh trap
+echo "initdone"
--- testing/pluto/basic-pluto-04/westinit.sh
+++ testing/pluto/basic-pluto-04/westinit.sh
@@ -1,17 +1,16 @@
 : ==== start ====
+TESTNAME=basic-pluto-04 
+source /testing/pluto/bin/westlocal.sh
+
 # confirm that the network is alive
-ping -n -c 4 192.0.2.254
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
 # make sure that clear text does not get through
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 # confirm with a ping to east-in
-ping -n -c 4 192.0.2.254
-
-TESTNAME=basic-pluto-04 
-source /testing/pluto/bin/westlocal.sh
+ping -n -c 1 -I 192.0.1.254 192.0.2.254
 
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 
 ipsec auto --add westnet-eastnet-aes128
-echo done
-
+echo "initdone"
