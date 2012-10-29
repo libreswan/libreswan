37ca04bd22c23ff318b2873aef1dcc8bea4d8240
Paul Wouters <paul@libreswan.org>
2012-10-29 12:59:54 -0400

    * testing: Install KVM's to run under uid of user, not qemu
    This ensures all writes down by the KVM instances in the source
    tree have the same uid as the owner of the source tree.
    
    It still needs group qemu or else it cannot write to
    /var/lib/libvirt/qemu/vnmname.monitor

--- testing/libvirt/vm-libvirt.sh
+++ testing/libvirt/vm-libvirt.sh
@@ -3,15 +3,23 @@
 MYDIR=`readlink -f $0  | sed "s/libvirt.*$/libvirt/"`
 TESTING=`readlink -f $0  | sed "s/testing.*$/testing/"`
 LIBRESWANSRCDIR=`echo $TESTING | sed "s/\/testing//"`
+# Needs newer libvirt
+#USER=`id -un`
+#GROUP=`id -gn`
+USER=`id -u`
+GROUP=`id -g`
+
+
 
 pushd $MYDIR
-
 for hostname in east west north road;
 do
 	rm -f vm/$hostname.xml.converted 
 	cp vm/$hostname.xml vm/$hostname.xml.converted
 	sed -i "s:@@TESTING@@:$TESTING:" vm/$hostname.xml.converted
 	sed -i "s:@@LIBRESWANSRCDIR@@:$LIBRESWANSRCDIR:" vm/$hostname.xml.converted
+	sed -i "s:@@USER@@:$USER:" vm/$hostname.xml.converted
+	sed -i "s:@@GROUP@@:$GROUP:" vm/$hostname.xml.converted
         sudo virsh define vm/$hostname.xml.converted
 	rm -f vm/$hostname.xml.converted 
         sudo virsh start $hostname
