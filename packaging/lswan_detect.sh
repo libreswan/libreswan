0d5b4e216cf5a15e426b60125cf1a97634a7e5a8
Paul Wouters <paul@libreswan.org>
2012-11-24 15:59:07 -0500

    * packaging update
    packaging/lswan_detect.sh detects the distro and init system used.
    
    Added links for centos/foobar to rhel (they use identical version numbers)
    Added packaging ubuntu/
    
    We detect Arch Linux and OpenSuse but don't much for those yet.

---
+++ packaging/lswan_detect.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+# hopefully this will be very cross platform. Standards people!!
+
+return_initsystem() {
+# try to detect the running init system, perhaps more then one can be installed?
+
+if test -z "`uname -o |grep inux`"
+then
+	echo "non-linux init system not supported yet"
+	return
+fi
+
+# works for systemd, not upstart
+if test "`cat /proc/1/comm`" = "systemd"
+then
+	echo "systemd"
+	return
+fi
+
+if test -f /lib/systemd/systemd -a -d /var/run/systemd
+then
+	echo "systemd"
+	return
+fi
+
+if test -f /sbin/upstart-socket-bridge -a -f /var/run/upstart-socket-bridge.pid
+then
+	echo "upstart"
+	return
+fi
+
+# really, most have this, it is probably a backwards compatiblity or realy sysv
+if test -d /etc/init.d
+then
+	echo "sysvinit"
+	return
+fi
+
+echo "unknown"
+}
+
+return_distro() {
+
+if test -z "`uname -o |grep inux`"
+then
+	# non-Linux, so this will be BSD, OSX or cygwin/Windows
+	echo "non-linux building not auto-detected yet"
+	return
+fi
+
+if test -f /etc/redhat-release
+then
+	VER="`grep 'Fedora release'  /etc/redhat-release | awk '{ print $3;}'`"
+	if test -n "$VER"
+	then
+		echo "fedora/$VER"
+		return
+	fi
+	VER="`grep 'Red Hat Enterprise'  /etc/redhat-release | awk '{ print $7;}'`"
+	if test -n "$VER"
+	then
+		echo "rhel/$VER"
+		return
+	fi
+	VER="`grep CentOS /etc/redhat-release | awk '{ print $3;}'`"
+	if test -n "$VER"
+	then
+		echo "centos/$VER"
+		return
+	fi
+fi
+
+# Check ubuntu before debian, as it also has /etc/debian_version
+if test -f /etc/lsb-release
+then
+	. /etc/lsb-release
+	if test "$DISTRIB_ID" = "Ubuntu"
+	then
+		echo "ubuntu/$DISTRIB_RELEASE"
+		return
+	fi
+fi
+
+if test -f /etc/debian_version
+then
+	VER="`cat /etc/debian_version | sed 's/^\([0-9]\.[0-9]\).*$/\1/'`"
+	echo "debian/$VER"
+	return
+fi
+
+if test -f /etc/arch-release
+then
+	# Arch Linux has no version, rolling release only
+	echo "archlinux"
+	return
+fi
+
+if test -f /etc/SuSE-release
+then
+	VER="`grep openSUSE /etc/SuSE-release | awk '{ print $2}'`"
+	echo "opensuse/$VER"
+	return
+fi
+
+echo "unknown please email dev@libreswan.org with:"
+uname -a
+exit 1
+
+}
+
+case "$1" in
+help|--help|-h|'')
+	echo "Usage: distro | init"
+	echo "distro will detect the distribution (eg fedora/18)"
+	echo "init will detect the init system used (systemd, upstart, sysv)"
+	exit 1
+	;;
+distro|--distro)
+	return_distro
+	;;
+
+init|--init|initsystem|--initsystem)
+	return_initsystem
+	;;
+esac
