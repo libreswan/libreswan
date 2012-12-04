284f9e4bc48a31ac8aa818f31fe70cb218030fef
Paul Wouters <paul@libreswan.org>
2012-12-03 20:15:13 -0500

    * interfaces.h was needed for one function

---
+++ include/ipsecconf/interfaces.h
@@ -0,0 +1,22 @@
+/* FreeS/WAN interfaces management (interfaces.h)
+ * Copyright (C) 2001-2002 Mathieu Lafon - Arkoon Network Security
+ * Copyright (C) 2012 Paul Wouters <paul@libreswan.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef _STARTER_INTERFACES_H_
+#define _STARTER_INTERFACES_H_
+
+int starter_iface_find(char *iface, int af, ip_address *dst, ip_address *nh);
+
+#endif /* _STARTER_INTERFACES_H_ */
+
