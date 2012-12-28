8ca77f767df5b8562baae7a0c4b582613921fd30
Paul Wouters <paul@libreswan.org>
2012-12-28 01:16:57 -0500

    * testing: run "swanprep" to copy testfiles to proper place in VM

--- testing/pluto/ikev2-05-basic-psk/testparams.sh
+++ testing/pluto/ikev2-05-basic-psk/testparams.sh
@@ -1,28 +1,16 @@
 #!/bin/sh
 
-TEST_PURPOSE=regress
-TEST_PROB_REPORT=0
-TEST_TYPE=umlplutotest
-
 TESTNAME=ikev2-05-basic-psk
 EASTHOST=east
 WESTHOST=west
 
-WEST_ARPREPLY=1
-
 EAST_INPUT=../../klips/inputs/01-sunrise-sunset-ping.pcap
 REF_WEST_OUTPUT=../../klips/west-icmp-01/spi1-cleartext.txt
 REF_WEST_FILTER=../../klips/fixups/no-arp-pcap2.pl
-WEST_ARPREPLY=true
 
-
-
-#THREEEIGHT=true
-
+# can be deduced?
 REF_EAST_CONSOLE_OUTPUT=east-console.txt
-REF26_EAST_CONSOLE_OUTPUT=east-console.txt
 REF_WEST_CONSOLE_OUTPUT=west-console.txt
-REF26_WEST_CONSOLE_OUTPUT=west-console.txt
 
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS east-prompt-splitline.pl"
