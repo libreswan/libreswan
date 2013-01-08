6e6d76a53a5dc06cd691fc8ad3179c44b1b58599
Antony Antony <antony@phenome.org>
2013-01-08 20:52:57 +0100

    don't ipsec setup start instead pluto ...

--- testing/pluto/ikev2-11-simple-psk/eastinit.sh
+++ testing/pluto/ikev2-11-simple-psk/eastinit.sh
@@ -2,11 +2,13 @@
 TESTNAME=ikev2-11-simple-psk
 /testing/guestbin/swanprep --testname $TESTNAME
 
-ipsec setup start
+ipsec setup stop
+/usr/local/libexec/ipsec/_stackmanager stop
+/usr/local/libexec/ipsec/_stackmanager start 
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
 
 ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add  westnet-eastnet-ipv4-psk-ikev2
 ipsec whack --debug-control --debug-controlmore --debug-crypt
 echo "initdone"
-
--- testing/pluto/ikev2-11-simple-psk/westinit.sh
+++ testing/pluto/ikev2-11-simple-psk/westinit.sh
@@ -10,7 +10,10 @@ iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 # confirm with a ping to east-in
 ping -n -c 1 -I 192.0.1.254 192.0.2.254
 
-ipsec setup start
+ipsec setup stop
+/usr/local/libexec/ipsec/_stackmanager stop
+/usr/local/libexec/ipsec/_stackmanager start
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 
 ipsec whack --whackrecord /var/tmp/ikev2.record
