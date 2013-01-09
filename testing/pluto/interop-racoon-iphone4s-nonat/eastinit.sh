e91c6a6f028ca4c80c08f4282d0693699c76bcb2
Paul Wouters <paul@libreswan.org>
2013-01-09 16:14:05 -0500

    * testing: added iphone 4s racoon config within testing framework
    This test is incomplete. But contains the racoon config extracted
    from an iphone 4s. It is likely modified for using the apple keychain
    to obtain certificates, so we will need to use the stock racoon
    method for specifying the certificates.

---
+++ testing/pluto/interop-racoon-iphone4s-nonat/eastinit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+: ==== start ====
+
+ipsec setup stop
+umount /var/tmp; mount /var/tmp
+umount /usr/local; mount /usr/local
+
+export TESTNAME=xauth-pluto-12
+source /testing/pluto/bin/eastlocal.sh
+
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+
+ipsec auto --add modecfg-road--eastnet-psk
+
+echo done.
+
+
+
+
