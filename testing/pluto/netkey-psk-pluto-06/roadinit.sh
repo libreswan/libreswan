ed8820992b3a8e3be3a46b789ab82b06a9b602a0
Paul Wouters <paul@libreswan.org>
2013-01-13 12:14:42 -0500

    * testing: added netkey-psk-pluto-06

---
+++ testing/pluto/netkey-psk-pluto-06/roadinit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+: ==== start ==== 
+TESTNAME=netkey-psk-pluto-06
+hostname road.uml.freeswan.org
+
+/testing/guestbin/swanprep --testname $TESTNAME 
+ipsec setup stop
+pidof pluto >/dev/null && killall pluto 2> /dev/null
+rm -fr /var/run/pluto/pluto.pid
+/usr/local/libexec/ipsec/_stackmanager stop
+/usr/local/libexec/ipsec/_stackmanager start
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
+/testing/pluto/bin/wait-until-pluto-started
+
+ipsec auto --add road--eastnet-psk
+
+ipsec auto --status
+sleep 2
+echo "initdone"
