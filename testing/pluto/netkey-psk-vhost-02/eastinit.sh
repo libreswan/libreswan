2430ea35fe155418d3442b304ca4e1bd86e15644
Paul Wouters <paul@libreswan.org>
2013-01-13 14:11:00 -0500

    * testing: Added testcases netkey-psk-vhost-0[1..4]
    These test cases investigate the behaviour of subnet=vhost:%no,%priv
    versus subnet=%vhost:%priv,%no with and without NAT on nic.
    
    They also use a virtual_private=%v4:!192.0.2.0/24,%v4:192.0.0.0/8
    which should NOT cause rejection.

---
+++ testing/pluto/netkey-psk-vhost-02/eastinit.sh
@@ -0,0 +1,14 @@
+: ==== start ====
+TESTNAME=netkey-psk-vhost-01
+/testing/guestbin/swanprep --testname $TESTNAME
+
+ipsec setup stop
+/usr/local/libexec/ipsec/_stackmanager stop
+rm -fr /var/run/pluto/pluto.pid
+/usr/local/libexec/ipsec/_stackmanager start
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
+/testing/pluto/bin/wait-until-pluto-started
+
+ipsec auto --add road-east-psk
+ipsec auto --status
+echo "initdone"
---
+++ testing/pluto/netkey-psk-vhost-02/roadinit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+: ==== start ==== 
+TESTNAME=netkey-psk-vhost-01
+
+/testing/guestbin/swanprep --testname $TESTNAME 
+ipsec setup stop
+pidof pluto >/dev/null && killall pluto 2> /dev/null
+rm -fr /var/run/pluto/pluto.pid
+/usr/local/libexec/ipsec/_stackmanager stop
+/usr/local/libexec/ipsec/_stackmanager start
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
+/testing/pluto/bin/wait-until-pluto-started
+
+ipsec auto --add road-east-psk
+
+ipsec auto --status
+sleep 2
+echo "initdone"
