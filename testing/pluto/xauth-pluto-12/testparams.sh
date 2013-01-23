1bd333af35fe20cef79d6093224c9c8f4a3d258d
Antony Antony <antony@phenome.org>
2013-01-23 21:54:39 +0200

    *testing forgot to commit with xauth-pluto-12

--- testing/pluto/xauth-pluto-12/testparams.sh
+++ testing/pluto/xauth-pluto-12/testparams.sh
@@ -4,7 +4,7 @@ TEST_PURPOSE=goal
 TEST_PROB_REPORT=0
 TEST_TYPE=umlXhost
 
-TESTNAME=xauth-pluto-12
+export TESTNAME=xauth-pluto-12
 
 XHOST_LIST="ROAD EAST"
 
@@ -13,18 +13,8 @@ REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS script-only.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS east-prompt-splitline.pl"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS cutout.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-setup-sanitize.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-debug-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS pluto-whack-sanitize.sed"
-
-EASTHOST=east
-EAST_INIT_SCRIPT=eastinit.sh
-EAST_FINAL_SCRIPT=final.sh
-REF_EAST_CONSOLE_OUTPUT=east-console.txt
-
-ROADHOST=road
-ROAD_INIT_SCRIPT=roadinit.sh
-ROAD_RUN_SCRIPT=roadrun.sh
-ROAD_FINAL_SCRIPT=final.sh
-REF_ROAD_CONSOLE_OUTPUT=road-console.txt
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS host-ping-sanitize.sed"
 
 ADDITIONAL_HOSTS="nic"
-
