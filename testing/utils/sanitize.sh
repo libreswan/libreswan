16c37ae41b54d8284ac723fa5663668a30d03316
Antony Antony <antony@phenome.org>
2013-01-23 21:05:06 +0200

    *testing: to sanitize manually sanitize.sh . run from the test dir

---
+++ testing/utils/sanitize.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+. ../../../kvmsetup.sh
+. ./testparams.sh 
+. ../setup.sh
+. $LIBRESAWNBASE/testing/utils/functions.sh 
+
+if [ -f eastinit.sh ] ; then
+        RESPONDER=east
+else
+        P=`pwd`
+        echo "can't idenity INITIATOR no $P/eastinit.sh"
+        exit 1
+fi
+
+NIC=""
+NIC_PID=""
+if [ -f nicinit.sh ] ; then
+        echo "will start nic nicinit.sh"
+        NIC=nic
+fi
+
+
+if [ -f westinit.sh ] ; then
+        INITIATOR=west
+elif [ -f roadinit.sh ] ; then
+        INITIATOR=road
+else 
+        echo "can't idenity INITIATOR"
+        exit 1
+fi
+
+consolediff ${INITIATOR} OUTPUT/${INITIATOR}.console.txt ${INITIATOR}.console.txt
+consolediff ${RESPONDER} OUTPUT/${RESPONDER}.console.txt ${RESPONDER}.console.txt
