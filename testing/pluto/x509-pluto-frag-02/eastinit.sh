c959ed4f730d22d6f05d15682c6e2315391eeba8
Paul Wouters <pwouters@redhat.com>
2013-01-30 22:30:33 -0500

    * testing: added more x509-pluto-frag-0* tests
    x509-pluto-frag-00 is used to confirm filtering UDP fragments
    will cause problems.
    
    x509-pluto-frag-01 is the default policy that should send/receive
    ike fragments
    
    x509-pluto-frag-02 uses ike_frag=force

--- testing/pluto/x509-pluto-frag-02/eastinit.sh
+++ testing/pluto/x509-pluto-frag-02/eastinit.sh
@@ -5,7 +5,6 @@
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 
-iptables -I OUTPUT -f -d 192.1.2.45 -j LOGDROP
 ipsec auto --add westnet-eastnet-x509-fragmentation
 ipsec auto --status
 echo "initdone"
