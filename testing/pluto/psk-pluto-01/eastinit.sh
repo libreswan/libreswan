bcaf1b74b984ab831c1c47e102b8269925afd522
Paul Wouters <pwouters@redhat.com>
2013-01-31 14:48:03 -0500

    * testing: updated testcase psk-pluto-01

--- testing/pluto/psk-pluto-01/eastinit.sh
+++ testing/pluto/psk-pluto-01/eastinit.sh
@@ -1,10 +1,7 @@
 : ==== start ====
-TESTNAME=psk-pluto-01
-/testing/guestbin/swan-prep --testname $TESTNAME
+#TESTNAME=psk-pluto-01
+#/testing/guestbin/swan-prep --testname $TESTNAME
 
-ipsec setup stop
-/usr/local/libexec/ipsec/_stackmanager stop
-rm -fr /var/run/pluto/pluto.pid
 /usr/local/libexec/ipsec/_stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
--- testing/pluto/psk-pluto-01/roadinit.sh
+++ testing/pluto/psk-pluto-01/roadinit.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 : ==== start ==== 
-TESTNAME=psk-pluto-01
-hostname road.uml.freeswan.org
-/testing/guestbin/swan-prep --testname $TESTNAME 
-ipsec setup stop
-pidof pluto >/dev/null && killall pluto 2> /dev/null
-rm -fr /var/run/pluto/pluto.pid
-/usr/local/libexec/ipsec/_stackmanager stop
+#TESTNAME=psk-pluto-01
+#/testing/guestbin/swan-prep --testname $TESTNAME 
+
 /usr/local/libexec/ipsec/_stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 
 ipsec auto --add road--eastnet-psk
-
 ipsec auto --status
-sleep 2
 echo "initdone"
