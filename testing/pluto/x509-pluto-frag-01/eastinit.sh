6af094e7c1c50288a23d99e9fdd8f5e05f155eed
Paul Wouters <pwouters@redhat.com>
2013-02-13 00:37:07 -0500

    * testing: updates testcases for fragmentation support

--- testing/pluto/x509-pluto-frag-01/eastinit.sh
+++ testing/pluto/x509-pluto-frag-01/eastinit.sh
@@ -1,11 +1,24 @@
-: ==== start ====
-# /testing/guestbin/swan-prep --testname `basename $PWD` --x509 
+#!/bin/sh
 
+: ==== start ====
+TESTNAME=`basename $PWD`
+export TESTNAME
+
+/testing/guestbin/swan-prep --testname $TESTNAME  --hostname east --x509 
+
+certutil -d /etc/ipsec.d -D road -n road
+certutil -L -d /etc/ipsec.d
+
+#iptables -I OUTPUT -f -j LOGDROP # drop UDP fragments
+iptables -I INPUT -p udp -m length --length 0x5dc:0xffff -j LOGDROP
+
+ipsec setup stop
+/usr/local/libexec/ipsec/_stackmanager stop
+rm -fr /var/run/pluto/pluto.pid
 /usr/local/libexec/ipsec/_stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 
-iptables -I OUTPUT -f -d 192.1.2.45 -j LOGDROP
-ipsec auto --add westnet-eastnet-x509-fragmentation
-ipsec auto --status
-echo "initdone"
+ipsec auto --add x509
+
+echo done.
--- testing/pluto/x509-pluto-frag-02/final.sh
+++ testing/pluto/x509-pluto-frag-02/final.sh
@@ -1,9 +1,9 @@
 : ==== cut ====
 ipsec auto --status
-cat /tmp/pluto.log
+ipsec look
+certutil -L -d /etc/ipsec.d
+if [ -f /tmp/core* ]; then echo CORE FOUND; mv /tmp/core* ./; fi
 : ==== tuc ====
 
 : ==== end ====
-
-
 
