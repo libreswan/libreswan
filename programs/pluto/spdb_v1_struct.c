libreswan/libreswan

commit 249fbd0eda68d71e466812ea8298dc28f6235d74

* pluto: more missing checks for failing out_raw() / out_struct() calls

programs/pluto/spdb_v1_struct.c
@@ -528,9 +528,12 @@ out_sa(pb_stream *outs
 		{
 		    passert(!oakley_mode);
 		    passert(st->st_pfs_group != &unset_group);
-		    out_attr(GROUP_DESCRIPTION, st->st_pfs_group->group
+		    if(!out_attr(GROUP_DESCRIPTION, st->st_pfs_group->group
 			, attr_desc, attr_val_descs
-			, &trans_pbs);
+			, &trans_pbs))
+		    {
+			return_on(ret, FALSE);
+		    }
 		}
 
 		/* automatically generate duration
@@ -538,13 +541,19 @@ out_sa(pb_stream *outs
 		 */
 		if (oakley_mode)
 		{
-		    out_attr(OAKLEY_LIFE_TYPE, OAKLEY_LIFE_SECONDS
+		    if(!out_attr(OAKLEY_LIFE_TYPE, OAKLEY_LIFE_SECONDS
 			, attr_desc, attr_val_descs
-			, &trans_pbs);
-		    out_attr(OAKLEY_LIFE_DURATION
+			, &trans_pbs))
+		    {
+			return_on(ret, FALSE);
+		    }
+		    if(!out_attr(OAKLEY_LIFE_DURATION
 			, st->st_connection->sa_ike_life_seconds
 			, attr_desc, attr_val_descs
-			, &trans_pbs);
+			, &trans_pbs))
+		    {
+			return_on(ret, FALSE);
+		    }
 		}
 		else
 		{
@@ -573,7 +582,7 @@ out_sa(pb_stream *outs
 #endif
 #endif
 
-			out_attr(ENCAPSULATION_MODE
+			if(!out_attr(ENCAPSULATION_MODE
 #ifdef NAT_TRAVERSAL
 #ifdef I_KNOW_TRANSPORT_MODE_HAS_SECURITY_CONCERN_BUT_I_WANT_IT
 			    , NAT_T_ENCAPSULATION_MODE(st,st->st_policy)
@@ -590,32 +599,46 @@ out_sa(pb_stream *outs
 			      ? ENCAPSULATION_MODE_TUNNEL : ENCAPSULATION_MODE_TRANSPORT
 #endif
 			    , attr_desc, attr_val_descs
-			    , &trans_pbs);
+			    , &trans_pbs))
+			{
+			   return_on(ret, FALSE);
+			}
 		    }
-		    out_attr(SA_LIFE_TYPE, SA_LIFE_TYPE_SECONDS
+		    if(!out_attr(SA_LIFE_TYPE, SA_LIFE_TYPE_SECONDS
 			, attr_desc, attr_val_descs
-			, &trans_pbs);
-		    out_attr(SA_LIFE_DURATION
+			, &trans_pbs))
+		    {
+			return_on(ret, FALSE);
+		    }
+		    if(!out_attr(SA_LIFE_DURATION
 			, st->st_connection->sa_ipsec_life_seconds
 			, attr_desc, attr_val_descs
-			, &trans_pbs);
+			, &trans_pbs))
+		    {
+			return_on(ret, FALSE);
+		    }
 #ifdef HAVE_LABELED_IPSEC
 		    if(st->sec_ctx != NULL && st->st_connection->labeled_ipsec) {
 			struct isakmp_attribute attr;
 			pb_stream val_pbs;
 			attr.isaat_af_type = secctx_attr_value | ISAKMP_ATTR_AF_TLV;
 			DBG(DBG_EMITTING, DBG_log("secctx_attr_value=%d, type=%d", secctx_attr_value, attr.isaat_af_type));
-			out_struct(&attr, attr_desc, &trans_pbs, &val_pbs); 
+			if(!out_struct(&attr, attr_desc, &trans_pbs, &val_pbs)
+			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("placing security context attribute in the out going structure"));
 			DBG(DBG_EMITTING, DBG_log("sending ctx_doi"));
-			out_raw(&st->sec_ctx->ctx_doi, sizeof(st->sec_ctx->ctx_doi),  &val_pbs, " variable length sec ctx: ctx_doi");
+			if(!out_raw(&st->sec_ctx->ctx_doi, sizeof(st->sec_ctx->ctx_doi),  &val_pbs, " variable length sec ctx: ctx_doi")
+			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("sending ctx_alg"));
-			out_raw(&st->sec_ctx->ctx_alg, sizeof(st->sec_ctx->ctx_alg),  &val_pbs, " variable length sec ctx: ctx_alg");
+			if(!out_raw(&st->sec_ctx->ctx_alg, sizeof(st->sec_ctx->ctx_alg),  &val_pbs, " variable length sec ctx: ctx_alg")
+			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("sending ctx_len after conversion to network byte order"));
 			u_int16_t net_ctx_len = htons(st->sec_ctx->ctx_len);
-			out_raw(&net_ctx_len, sizeof(st->sec_ctx->ctx_len),  &val_pbs, " variable length sec ctx: ctx_len");
+			if(!out_raw(&net_ctx_len, sizeof(st->sec_ctx->ctx_len),  &val_pbs, " variable length sec ctx: ctx_len")
+			   return_on(ret, FALSE);
 			/*Sending '\0'  with sec ctx as we get it from kernel*/
-			out_raw(st->sec_ctx->sec_ctx_value, st->sec_ctx->ctx_len, &val_pbs, " variable length sec ctx");
+			if(!out_raw(st->sec_ctx->sec_ctx_value, st->sec_ctx->ctx_len, &val_pbs, " variable length sec ctx")
+			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("placed security context attribute in the out going structure"));
         		close_output_pbs(&val_pbs);
 			DBG(DBG_EMITTING, DBG_log("end of security context attribute in the out going structure"));
@@ -630,11 +653,17 @@ out_sa(pb_stream *outs
 		    struct db_attr *a = &t->attrs[an];
 
 		    if(oakley_mode) {
-			out_attr(a->type.oakley, a->val
+			if(!out_attr(a->type.oakley, a->val
 				 , attr_desc, attr_val_descs
-				 , &trans_pbs);
+				 , &trans_pbs))
+			{
+			   return_on(ret, FALSE);
+			}
 		    } else {
-				out_attr(a->type.ipsec,  a->val , attr_desc, attr_val_descs , &trans_pbs);
+				if(!out_attr(a->type.ipsec,  a->val , attr_desc, attr_val_descs , &trans_pbs))
+				{
+				   return_on(ret, FALSE);
+				}
 			
 		    }
 
@@ -1947,10 +1976,13 @@ echo_proposal(
 	 * but we'll ignore that.
 	 */
 	pi->our_spi = get_my_cpi(sr, tunnel_mode);
-	out_raw((u_char *) &pi->our_spi
+	if(!out_raw((u_char *) &pi->our_spi
 	     + IPSEC_DOI_SPI_SIZE - IPCOMP_CPI_SIZE
 	    , IPCOMP_CPI_SIZE
-	    , &r_proposal_pbs, "CPI");
+	    , &r_proposal_pbs, "CPI"))
+	{
+	   impossible();
+	}
     }
     else
     {
@@ -1960,8 +1992,11 @@ echo_proposal(
 	    , sr
 	    , tunnel_mode);
 	/* XXX should check for errors */
-	out_raw((u_char *) &pi->our_spi, IPSEC_DOI_SPI_SIZE
-	    , &r_proposal_pbs, "SPI");
+	if(!out_raw((u_char *) &pi->our_spi, IPSEC_DOI_SPI_SIZE
+	    , &r_proposal_pbs, "SPI"))
+	{
+	   impossible();
+	}
     }
 
     /* Transform */

commit be27d31e1e9997d2d48cada82f2b1f9a45548e08

out_sa: fix syntax errors caused by 249fbd0eda68d71e466812ea8298dc28f6235d74

programs/pluto/spdb_v1_struct.c
@@ -623,21 +623,21 @@ out_sa(pb_stream *outs
 			pb_stream val_pbs;
 			attr.isaat_af_type = secctx_attr_value | ISAKMP_ATTR_AF_TLV;
 			DBG(DBG_EMITTING, DBG_log("secctx_attr_value=%d, type=%d", secctx_attr_value, attr.isaat_af_type));
-			if(!out_struct(&attr, attr_desc, &trans_pbs, &val_pbs)
+			if(!out_struct(&attr, attr_desc, &trans_pbs, &val_pbs))
 			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("placing security context attribute in the out going structure"));
 			DBG(DBG_EMITTING, DBG_log("sending ctx_doi"));
-			if(!out_raw(&st->sec_ctx->ctx_doi, sizeof(st->sec_ctx->ctx_doi),  &val_pbs, " variable length sec ctx: ctx_doi")
+			if(!out_raw(&st->sec_ctx->ctx_doi, sizeof(st->sec_ctx->ctx_doi),  &val_pbs, " variable length sec ctx: ctx_doi"))
 			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("sending ctx_alg"));
-			if(!out_raw(&st->sec_ctx->ctx_alg, sizeof(st->sec_ctx->ctx_alg),  &val_pbs, " variable length sec ctx: ctx_alg")
+			if(!out_raw(&st->sec_ctx->ctx_alg, sizeof(st->sec_ctx->ctx_alg),  &val_pbs, " variable length sec ctx: ctx_alg"))
 			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("sending ctx_len after conversion to network byte order"));
 			u_int16_t net_ctx_len = htons(st->sec_ctx->ctx_len);
-			if(!out_raw(&net_ctx_len, sizeof(st->sec_ctx->ctx_len),  &val_pbs, " variable length sec ctx: ctx_len")
+			if(!out_raw(&net_ctx_len, sizeof(st->sec_ctx->ctx_len),  &val_pbs, " variable length sec ctx: ctx_len"))
 			   return_on(ret, FALSE);
 			/*Sending '\0'  with sec ctx as we get it from kernel*/
-			if(!out_raw(st->sec_ctx->sec_ctx_value, st->sec_ctx->ctx_len, &val_pbs, " variable length sec ctx")
+			if(!out_raw(st->sec_ctx->sec_ctx_value, st->sec_ctx->ctx_len, &val_pbs, " variable length sec ctx"))
 			   return_on(ret, FALSE);
 			DBG(DBG_EMITTING, DBG_log("placed security context attribute in the out going structure"));
         		close_output_pbs(&val_pbs);