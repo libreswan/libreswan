e82619d2410083e2f8b638d12acf0763ace382fa
Paul Wouters <pwouters@redhat.com>
2013-02-26 01:43:03 -0500

    * testing: basic-pluto-01 now passes on bofh.nohats.ca.

--- testing/pluto/basic-pluto-01/eastinit.sh
+++ testing/pluto/basic-pluto-01/eastinit.sh
@@ -1,15 +1,12 @@
 : ==== start ====
-TESTNAME=basic-pluto-01
-/testing/guestbin/swan-prep --testname $TESTNAME
-
+/testing/guestbin/swan-prep
 ipsec setup stop
 pidof pluto >/dev/null && killall pluto 2> /dev/null
-rm -fr /var/run/pluto/pluto.pid
+rm -r /var/run/pluto/pluto.pid
 /usr/local/libexec/ipsec/_stackmanager stop
 /usr/local/libexec/ipsec/_stackmanager start 
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add westnet-eastnet
 ipsec auto --status
 echo "initdone"
