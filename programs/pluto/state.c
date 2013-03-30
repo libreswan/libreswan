18d929eb88e5984cd1635cabec0c918845d9ef82
Paul Wouters <pwouters@redhat.com>
2013-03-30 16:29:12 -0400

    * XAUTH: cleanup XAUTHuser in ipsec auto --status/--down
    Don't list it with connections and down events that don't have an XAUTHuser

--- programs/pluto/state.c
+++ programs/pluto/state.c
@@ -404,7 +404,7 @@ delete_state(struct state *st)
         sbcp = humanize_number(st->st_esp.our_bytes,
                                sbcp, sizeof(statebuf) - 1 - (sbcp - statebuf),
                                " out=%lu%s");
-	if (st->st_xauth_username) {
+	if (st->st_xauth_username && st->st_xauth_username[0]!='\0') {
 	    snprintf(statebufx, sizeof(statebufx) - 1 , "%s XAUTHuser=%s", statebuf, st->st_xauth_username);
 	    libreswan_log(statebufx);
 	} else {
@@ -419,7 +419,7 @@ delete_state(struct state *st)
         sbcp = humanize_number(st->st_ah.our_bytes,
                                sbcp, sizeof(statebuf) - 1 - (sbcp - statebuf),
                                " out=%lu%s");
-	if (st->st_xauth_username) {
+	if (st->st_xauth_username && st->st_xauth_username[0]!='\0') {
 	    snprintf(statebufx, sizeof(statebufx) - 1 , "%s XAUTHuser=%s", statebuf, st->st_xauth_username);
 	    libreswan_log(statebufx);
 	} else {
@@ -435,7 +435,7 @@ delete_state(struct state *st)
         sbcp = humanize_number(st->st_ipcomp.our_bytes,
                                sbcp, sizeof(statebuf) - 1 - (sbcp - statebuf),
                                " out=%lu%s");
-	if (st->st_xauth_username) {
+	if (st->st_xauth_username && st->st_xauth_username[0]!='\0') {
 	    snprintf(statebufx, sizeof(statebufx) - 1 , "%s XAUTHuser=%s", statebuf, st->st_xauth_username);
 	    libreswan_log(statebufx);
 	} else {
@@ -1628,15 +1628,18 @@ void fmt_state(struct state *st, const time_t n
 	    add_said(&c->spd.this.host_addr, st->st_tunnel_in_spi, SA_IPIP);
 	}
 #endif
+
 	snprintf(state_buf2, state_buf2_len
-	    , "#%lu: \"%s\"%s%s%s ref=%lu refhim=%lu XAUTHuser=%s %s"
+	    , "#%lu: \"%s\"%s%s%s ref=%lu refhim=%lu %s %s%s"
 	    , st->st_serialno
 	    , c->name, inst
 	    , lastused
 	    , buf
 	    , (unsigned long)st->st_ref, (unsigned long)st->st_refhim
-	    , (st->st_xauth_username) ? st->st_xauth_username : "[none]"
-	    , traffic_buf);
+	    , traffic_buf
+	    , (st->st_xauth_username && st->st_xauth_username[0]!='\0')  ? "XAUTHuser=" : ""
+	    , (st->st_xauth_username && st->st_xauth_username[0]!='\0')  ? st->st_xauth_username : ""
+		);
 
 #	undef add_said
     }
