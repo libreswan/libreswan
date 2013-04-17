7ef7e03f3026356ce2247d0a4cbd1717bbc6939f
Paul Wouters <pwouters@redhat.com>
2013-04-17 00:33:47 -0400

    * testing: Updated test case ipv6-v6-through-v6-klips-klips

--- testing/pluto/ipv6-v6-through-v6-klips-klips/eastinit.sh
+++ testing/pluto/ipv6-v6-through-v6-klips-klips/eastinit.sh
@@ -1,9 +1,7 @@
-: ==== start ====
-TESTNAME=ipv6-v6-through-v6-klips-klips
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec setup start
+/testing/guestbin/swan-prep
+ipsec _stackmanager start 
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add westnet-eastnet-6in6
-ipsec whack --debug-control --debug-controlmore --debug-crypt
+ipsec auto --status
+echo "initdone"
