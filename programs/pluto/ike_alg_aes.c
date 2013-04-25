34e669419f7b130ddeedf2c3559f75f98f73f316
Florian Weimer <fweimer@redhat.com>
2013-04-25 13:34:43 -0400

    * security: do_aes: Abort on failure
    The routine cannot signal encryption failures to the caller
    and would leave the buffer unencrypted on error.

--- programs/pluto/ike_alg_aes.c
+++ programs/pluto/ike_alg_aes.c
@@ -48,7 +48,7 @@ do_aes(u_int8_t *buf, size_t buf_len, u_int8_t *key, size_t key_size, u_int8_t *
 
     if (symkey == NULL) {
 	loglog(RC_LOG_SERIOUS, "do_aes: NSS derived enc key in NULL\n");
-	goto out;
+	abort();
     }
 
     ivitem.type = siBuffer;
@@ -58,7 +58,7 @@ do_aes(u_int8_t *buf, size_t buf_len, u_int8_t *key, size_t key_size, u_int8_t *
     secparam = PK11_ParamFromIV(ciphermech, &ivitem);
     if (secparam == NULL) {
 	loglog(RC_LOG_SERIOUS, "do_aes: Failure to set up PKCS11 param (err %d)\n",PR_GetError());
-	goto out;
+	abort();
    }
 
    outlen = 0;
@@ -69,8 +69,15 @@ do_aes(u_int8_t *buf, size_t buf_len, u_int8_t *key, size_t key_size, u_int8_t *
     }
 
     enccontext = PK11_CreateContextBySymKey(ciphermech, enc? CKA_ENCRYPT : CKA_DECRYPT, symkey, secparam); 
+    if (enccontext) {
+        loglog(RC_LOG_SERIOUS, "do_aes: PKCS11 context creation failure (err %d)\n", PR_GetError());
+        abort();
+    }
     rv = PK11_CipherOp(enccontext, tmp_buf, &outlen, buf_len, buf, buf_len);
-    passert(rv==SECSuccess);
+    if (rv != SECSuccess) {
+        loglog(RC_LOG_SERIOUS, "do_aes: PKCS11 operation failure (err %d)\n", PR_GetError());
+        abort();
+    }
     PK11_DestroyContext(enccontext, PR_TRUE);
     memcpy(buf,tmp_buf,buf_len);  
 
@@ -81,8 +88,6 @@ do_aes(u_int8_t *buf, size_t buf_len, u_int8_t *key, size_t key_size, u_int8_t *
     memcpy(iv, new_iv, AES_CBC_BLOCK_SIZE);
     PR_Free(tmp_buf);
 
-out:
- 
 if (secparam)
     SECITEM_FreeItem(secparam, PR_TRUE);
 DBG(DBG_CRYPT, DBG_log("NSS do_aes: exit"));
