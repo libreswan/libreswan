31059031c3db752ddf2bb824f05de8954323e1b0
Antony Antony <antony@phenome.org>
2013-06-06 14:24:16 +0000

    testing: run the swan-prep from init script

--- testing/pluto/psk-pluto-01/roadinit.sh
+++ testing/pluto/psk-pluto-01/roadinit.sh
@@ -1,12 +1,8 @@
-#!/bin/sh
-: ==== start ==== 
-#TESTNAME=psk-pluto-01
-#/testing/guestbin/swan-prep --testname $TESTNAME 
-
+/testing/guestbin/swan-prep 
 /usr/local/libexec/ipsec/_stackmanager start
+/usr/local/libexec/ipsec/_stackmanager stop
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add road--eastnet-psk
 ipsec auto --status
 echo "initdone"
