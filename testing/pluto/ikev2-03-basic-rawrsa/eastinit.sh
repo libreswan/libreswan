4c39e587f2626d00b341ce510776ac17ac9736c4
Paul Wouters <pwouters@redhat.com>
2013-06-20 19:59:35 -0400

    testing: Remove workaround for nhelpers for ikev2-03-basic-rawrsa

--- testing/pluto/ikev2-03-basic-rawrsa/eastinit.sh
+++ testing/pluto/ikev2-03-basic-rawrsa/eastinit.sh
@@ -1,6 +1,6 @@
 /testing/guestbin/swan-prep
 ipsec _stackmanager start 
-/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf --nhelpers 2
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
 ipsec auto --status
