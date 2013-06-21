f0b918828d5fc15c075e3c0793c1fe12c11f227b
Paul Wouters <pwouters@redhat.com>
2013-06-21 16:16:36 -0400

    testing: fix ikev2-11-simple-psk config and converted to kvm

--- testing/pluto/ikev2-11-simple-psk/eastinit.sh
+++ testing/pluto/ikev2-11-simple-psk/eastinit.sh
@@ -1,14 +1,7 @@
-: ==== start ====
-TESTNAME=ikev2-11-simple-psk
-/testing/guestbin/swan-prep --testname $TESTNAME
-
-ipsec setup stop
-/usr/local/libexec/ipsec/_stackmanager stop
-/usr/local/libexec/ipsec/_stackmanager start 
+/testing/guestbin/swan-prep
+ipsec _stackmanager start 
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-
-ipsec whack --whackrecord /var/tmp/ikev2.record
-ipsec auto --add  westnet-eastnet-ipv4-psk-ikev2
-ipsec whack --debug-control --debug-controlmore --debug-crypt
+ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
+ipsec auto --status
 echo "initdone"
