714b066cdf327e5875e55b2df6b81506207bead7
Paul Wouters <pwouters@redhat.com>
2013-06-25 22:40:02 -0400

    testing: Converted the loopback/labeled ipsec testcases to KVM
    They are still showing a problem of not being able to find the PSK
    for localhost in the secrets file.

--- testing/pluto/loopback-pluto-02/westinit.sh
+++ testing/pluto/loopback-pluto-02/westinit.sh
@@ -1,18 +1,13 @@
-: ==== start ====
-TESTNAME=loopback-pluto-02
-source /testing/pluto/bin/westlocal.sh
-
+/testing/guestbin/swan-prep
 # confirm that the network is alive
 ping -n -c 4 127.0.0.1
 # make sure that clear text does not get through
-#iptables -A INPUT -i lo -p icmp -j DROP
+iptables -A INPUT -i lo -p icmp  -j LOGDROP
 # confirm with a ping 
-#ping -n -c 4 127.0.0.1
-
-ipsec setup start
+ping -n -c 4 127.0.0.1
+ipsec _stackmanager start
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add loopback-02-westleft
-
-echo done
-
+ipsec auto --status
+echo "initdone"
