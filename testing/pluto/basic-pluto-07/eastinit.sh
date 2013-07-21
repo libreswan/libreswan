6c62519245e70eabd8f881cb6ff119189f77fa50
Paul Wouters <pwouters@redhat.com>
2013-07-21 19:52:48 -0400

    testing: WIP basic-pluto-05/07

--- testing/pluto/basic-pluto-07/eastinit.sh
+++ testing/pluto/basic-pluto-07/eastinit.sh
@@ -1,9 +1,7 @@
-: ==== start ====
-TESTNAME=basic-pluto-07
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec setup start
+/testing/guestbin/swan-prep
+ipsec _stackmanager start 
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add westnet-eastnet-twofish
-/testing/pluto/basic-pluto-01/eroutewait.sh trap
+ipsec auto --status
+echo "initdone"
