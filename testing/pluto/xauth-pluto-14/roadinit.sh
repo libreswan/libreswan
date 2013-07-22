18ccdce77f7eac4512a736f4ff6fa6ed770618e2
Paul Wouters <pwouters@redhat.com>
2013-07-22 12:45:56 -0400

    testing: updated xauth-pluto-14

--- testing/pluto/xauth-pluto-14/roadinit.sh
+++ testing/pluto/xauth-pluto-14/roadinit.sh
@@ -1,23 +1,6 @@
-#!/bin/sh
-
-: ==== start ====
-
-TESTNAME=`basename $PWD`
-export TESTNAME
-
-/testing/guestbin/swan-prep --testname $TESTNAME --hostname road
-hostname road.uml.freeswan.org
-netstat -rn
-
-ipsec setup stop
-pidof pluto >/dev/null && killall pluto 2> /dev/null
-rm -fr /var/run/pluto/pluto.pid
-/usr/local/libexec/ipsec/_stackmanager stop
+/testing/guestbin/swan-prep
 /usr/local/libexec/ipsec/_stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
-
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add modecfg-road-east
-
-echo done
+echo "initdone"
--- testing/pluto/xauth-pluto-14/roadrun.sh
+++ testing/pluto/xauth-pluto-14/roadrun.sh
@@ -1,4 +1,4 @@
 ipsec whack --xauthname 'use3' --xauthpass 'use1pass' --name modecfg-road-east --initiate
+ipsec eroute
 ping -n -c 4 -I 192.0.2.100 192.0.2.254
-echo done.
-
+echo done
