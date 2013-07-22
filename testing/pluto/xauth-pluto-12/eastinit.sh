9076664012ecbc6ba738902965bff893a16788fc
Paul Wouters <pwouters@redhat.com>
2013-07-22 13:32:42 -0400

    testing: updated xauth-pluto-12

--- testing/pluto/xauth-pluto-12/eastinit.sh
+++ testing/pluto/xauth-pluto-12/eastinit.sh
@@ -1,17 +1,6 @@
-#!/bin/sh
-
-: ==== start ====
-export TESTNAME=xauth-pluto-12
-
-/testing/guestbin/swan-prep --testname $TESTNAME 
-ipsec setup stop
-pidof pluto >/dev/null && killall pluto 2> /dev/null
-/usr/local/libexec/ipsec/_stackmanager stop
-rm -fr /var/run/pluto/pluto.pid
+/testing/guestbin/swan-prep 
 /usr/local/libexec/ipsec/_stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add modecfg-road--eastnet-psk
-
-echo done.
+echo initdone
