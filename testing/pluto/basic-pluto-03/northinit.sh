1fe2d726d8f6ce64b13e44a7999f40f49c3f3413
Paul Wouters <pwouters@redhat.com>
2013-07-26 22:07:26 -0400

    testing: updated basic-pluto-0[134] - partialy for new ipv6

--- testing/pluto/basic-pluto-03/northinit.sh
+++ testing/pluto/basic-pluto-03/northinit.sh
@@ -1,5 +1,9 @@
 /testing/guestbin/swan-prep
 ping -c 4 -n 192.0.3.254
+# make sure that clear text does not get through
+iptables -A INPUT -i eth1 -s 192.0.2.254/32 -j LOGDROP
+# confirm with a ping
+ping -c 4 -n 192.0.2.254
 ipsec _stackmanager start 
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
