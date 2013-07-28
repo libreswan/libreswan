eac81351b54e6efc1e8374e69336584e17c25166
Paul Wouters <pwouters@redhat.com>
2013-07-28 15:40:59 +0200

    testing: updated x509-pluto-01
    Deleted basic-pluto-06 as it was testing the exact same base x509
    connection. It mentions a refineconnection bug which I think was
    fixed eons ago.

--- testing/pluto/x509-pluto-01/eastinit.sh
+++ testing/pluto/x509-pluto-01/eastinit.sh
@@ -1,7 +1,7 @@
-: ==== start ====
-ipsec _stackmanager start
-/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
+/testing/guestbin/swan-prep --x509
+ipsec _stackmanager start 
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add westnet-eastnet-x509
+ipsec auto --status
 echo "initdone"
--- testing/pluto/x509-pluto-01/testparams.sh
+++ testing/pluto/x509-pluto-01/testparams.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-XHOSTS='east west' 
-x509=yes
-export TESTNAME
 
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS cut-postfinal.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS host-prompt-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS east-prompt-splitline.pl"
-REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS script-only.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS cutout.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS wilog.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-debug-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-setup-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS pluto-whack-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS host-ping-sanitize.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS kern-list-fixups.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-look-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-look-esp-sanitize.pl"
