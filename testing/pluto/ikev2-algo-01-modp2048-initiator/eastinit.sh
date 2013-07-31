7c1a009938a54544ff07f5f5ee045091eb27af85
Paul Wouters <pwouters@redhat.com>
2013-07-31 13:34:10 +0200

    testing: updated ikev2-algo-0[12]-modp2048-* tests

--- testing/pluto/ikev2-algo-01-modp2048-initiator/eastinit.sh
+++ testing/pluto/ikev2-algo-01-modp2048-initiator/eastinit.sh
@@ -1,10 +1,6 @@
-: ==== start ====
-TESTNAME=ikev2-algo-01-modp2048-initiator
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec setup start
+/testing/guestbin/swan-prep
+ipsec _stackmanager start 
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
-
-ipsec whack --whackrecord /var/tmp/ikev2.record
-ipsec auto --add  westnet-eastnet-ikev2
+ipsec auto --add westnet-eastnet-ikev2
 echo "initdone"
