3b204409f1033b31f2f794aa46747992192b70d5
Paul Wouters <pwouters@redhat.com>
2013-08-10 19:20:22 -0400

    testing: updated ikev2-05-basic-psk

--- testing/pluto/ikev2-05-basic-psk/eastinit.sh
+++ testing/pluto/ikev2-05-basic-psk/eastinit.sh
@@ -3,5 +3,4 @@ ipsec _stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
-ipsec auto --status
 echo "initdone"
