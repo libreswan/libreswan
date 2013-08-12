17305d768fdb9aa8c74268c9f506a15f97de7632
Paul Wouters <pwouters@redhat.com>
2013-08-12 16:50:54 -0400

    testing: fix interop-ikev1-strongswan* to use conn name with ikev1, not ikev2

--- testing/pluto/interop-ikev1-strongswan-02-psk-responder/westinit.sh
+++ testing/pluto/interop-ikev1-strongswan-02-psk-responder/westinit.sh
@@ -9,5 +9,5 @@ ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec _stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-ikev2
+ipsec auto --add westnet-eastnet-ikev1
 echo "initdone"
--- testing/pluto/interop-ikev1-strongswan-03-psk-initiator/eastinit.sh
+++ testing/pluto/interop-ikev1-strongswan-03-psk-initiator/eastinit.sh
@@ -2,5 +2,5 @@
 ipsec _stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-ikev2
+ipsec auto --add westnet-eastnet-ikev1
 echo "initdone"
