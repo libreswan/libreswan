614357de4193ac6b36e5f0f011c1a5455c6df04a
Paul Wouters <pwouters@redhat.com>
2013-09-17 16:05:56 -0400

    INTEROP: Fix compatibility with NAT-T and remote_peer_type=cisco
    Seems there are still plenty of old Cisco's around that don't have
    RFC3974 NAT-T vendorid support. For those we need to send the old
    nat-t draft VIDs. We did not send those when remote_peer_type=cisco

--- programs/pluto/nat_traversal.c
+++ programs/pluto/nat_traversal.c
@@ -209,30 +209,25 @@ bool nat_traversal_insert_vid(u_int8_t np, pb_stream *outs, struct state *st)
 		    nat_traversal_support_non_ike));
 
 	if (nat_traversal_support_port_floating) {
-		if (st->st_connection->remotepeertype == CISCO) {
-			if (r)
-				r = out_vid(np, outs, VID_NATT_RFC);
-		} else {
-			if (r)
-				r =
-					out_vid(ISAKMP_NEXT_VID, outs,
-						VID_NATT_RFC);
-			if (r)
-				r = out_vid(ISAKMP_NEXT_VID, outs,
-					    VID_NATT_IETF_03);
-			if (r)
-				r = out_vid(ISAKMP_NEXT_VID, outs,
-					    VID_NATT_IETF_02_N);
-			if (r)
-				r = out_vid(
-					nat_traversal_support_non_ike ? ISAKMP_NEXT_VID : np,
-					outs, VID_NATT_IETF_02);
-		}
+		if (r)
+			r =
+				out_vid(ISAKMP_NEXT_VID, outs,
+					VID_NATT_RFC);
+		if (r)
+			r = out_vid(ISAKMP_NEXT_VID, outs,
+				    VID_NATT_IETF_03);
+		if (r)
+			r = out_vid(ISAKMP_NEXT_VID, outs,
+				    VID_NATT_IETF_02_N);
+		if (r)
+			r = out_vid(
+				nat_traversal_support_non_ike ? ISAKMP_NEXT_VID : np,
+				outs, VID_NATT_IETF_02);
 	}
-	if (nat_traversal_support_non_ike &&
-	    st->st_connection->remotepeertype != CISCO)
+	if (nat_traversal_support_non_ike) {
 		if (r)
 			r = out_vid(np, outs, VID_NATT_IETF_00);
+	}
 	return r;
 }
 
