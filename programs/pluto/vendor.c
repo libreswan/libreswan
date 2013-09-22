939410ab2d81a2b26520e5b66a50a5cc17ec9f2e
Paul Wouters <pwouters@redhat.com>
2013-09-22 14:18:53 -0400

    vendor.c: no need to set vid_useful to TRUE - that's the default

--- programs/pluto/vendor.c
+++ programs/pluto/vendor.c
@@ -661,7 +661,7 @@ static void handle_known_vendorid(struct msg_digest *md,
 #endif
 
 	case VID_LIBRESWANSELF:
-		vid_useful = TRUE; /* not really useful, but it changes the msg from "ignored" to "received" */
+		/* not really useful, but it changes the msg from "ignored" to "received" */
 		break;
 
 	case VID_CISCO_IKE_FRAGMENTATION:
