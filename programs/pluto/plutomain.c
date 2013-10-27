433b364fcc808f3137bd9ec4c09d08574feebf4b
Paul Wouters <pwouters@redhat.com>
2013-10-27 18:14:11 -0400

    statsd: fix logging for statsbin= option
    Note that logging is a little strange, and depends on the logging
    settings. In theory, when processing the --config option, we don't
    have the logging system initialised yet. But various calls to
    libreswan_log() seem to work regardless.

--- programs/pluto/plutomain.c
+++ programs/pluto/plutomain.c
@@ -941,14 +941,16 @@ int main(int argc, char **argv)
 			set_cfg_string(&pluto_vendorid, cfg->setup.strings[KSF_MYVENDORID]); /* --vendorid */
 			/* no config option: pluto_adns_option */
 
-			set_cfg_string(&pluto_stats_binary, /* --statsbinary */
-				       cfg->setup.strings[KSF_STATSBINARY]);
-			if (access(pluto_stats_binary, X_OK) == 0) {
-				libreswan_log("statsbinary set to %s", pluto_stats_binary);
-			} else {
-				libreswan_log("statsbinary '%s' ignored - file does not exist or is not executable",
-					      pluto_stats_binary);
+			if (cfg->setup.strings[KSF_STATSBINARY] != NULL) {
+				set_cfg_string(&pluto_stats_binary, /* --statsbinary */
+					       cfg->setup.strings[KSF_STATSBINARY]);
+				if (access(pluto_stats_binary, X_OK) == 0) {
+					libreswan_log("statsbinary set to %s", pluto_stats_binary);
+				} else {
+					libreswan_log("statsbinary '%s' ignored - file does not exist or is not executable",
+						      pluto_stats_binary);
 					pluto_stats_binary = NULL;
+				}
 			}
 
 			pluto_natt_float_port =
