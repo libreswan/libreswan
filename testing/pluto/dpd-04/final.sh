7fbd9d854097cf32ba265a55634c12b496810a8c
Paul Wouters <pwouters@redhat.com>
2013-10-28 22:50:07 -0400

    testing: various test cases updated

--- testing/pluto/dpd-04/final.sh
+++ testing/pluto/dpd-04/final.sh
@@ -2,6 +2,10 @@ ipsec look
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
+ipsec whack --shutdown
+: ==== cut ====
+ipsec look
+: ==== tuc ====
 if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* ./; fi
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
-: ==== end ===
+: ==== end ====
