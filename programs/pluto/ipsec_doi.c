a9ddf554d2e01c78959e61fc0c880b5818ca32c7
Paul Wouters <pwouters@redhat.com>
2013-10-30 22:43:59 -0400

    ikev2: Fix ikev1 fallback when ikev2 fails
    Confirmed by testcase ikev2-01-fallback-ikev1

--- programs/pluto/ipsec_doi.c
+++ programs/pluto/ipsec_doi.c
@@ -230,8 +230,8 @@ static initiator_function *pick_initiator(struct connection *c UNUSED,
 					  lset_t policy)
 {
 	if ((policy & POLICY_IKEV1_DISABLE) == 0 &&
-	    (c->failed_ikev2 || (policy & POLICY_IKEV2_PROPOSE) == 0) &&
-	    (c->policy & (POLICY_IKEV1_DISABLE | POLICY_IKEV2_PROPOSE)) == 0) {
+	    (c->failed_ikev2 || ((policy & POLICY_IKEV2_PROPOSE) == 0))) {
+	    
 		if (policy & POLICY_AGGRESSIVE) {
 			return aggr_outI1;
 		} else {
