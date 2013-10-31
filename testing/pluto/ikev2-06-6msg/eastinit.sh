417ca2008167bd463a6ff8128b3488b25682d8fb
Paul Wouters <pwouters@redhat.com>
2013-10-30 22:42:15 -0400

    testing: updated ikev2 test cases

--- testing/pluto/ikev2-06-6msg/eastinit.sh
+++ testing/pluto/ikev2-06-6msg/eastinit.sh
@@ -1,11 +1,7 @@
-: ==== start ====
-TESTNAME=ikev2-06-6msg
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec setup start
+/testing/guestbin/swan-prep
+ipsec _stackmanager start 
+/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
-
-ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add westnet-eastnet-ikev2
-ipsec whack --debug-control --debug-controlmore --debug-crypt
+ipsec auto --status
 echo "initdone"
