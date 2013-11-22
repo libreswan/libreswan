9da583f1c1d045a7914b12447ce599bd584f65ad
Paul Wouters <pwouters@redhat.com>
2013-11-21 21:57:41 -0500

    testing: updated outpur for basic-pluto-0[1234]

--- testing/pluto/basic-pluto-03/northinit.sh
+++ testing/pluto/basic-pluto-03/northinit.sh
@@ -6,6 +6,6 @@ ping -c 4 -n -I 192.0.3.254 192.0.2.254
 ipsec _stackmanager start 
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf 
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add northnet--eastnet-nonat
+ipsec auto --add northnet-eastnet-nonat
 ipsec auto --status
 echo "initdone"
