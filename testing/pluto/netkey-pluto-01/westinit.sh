d13f0001ceb3ca05996a5178c1e84c44174319ba
Paul Wouters <pwouters@redhat.com>
2013-12-12 16:07:22 -0500

    testing: updated netkey-pluto-01

--- testing/pluto/netkey-pluto-01/westinit.sh
+++ testing/pluto/netkey-pluto-01/westinit.sh
@@ -1,18 +1,13 @@
-: ==== start ====
-TESTNAME=netkey-pluto-01
-source /testing/pluto/bin/westlocal.sh
-
+/testing/guestbin/swan-prep
 # confirm that the network is alive
-ping -n -c 4 192.0.2.254
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
 # make sure that clear text does not get through
-iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
-# confirm with a ping to east-in
-ping -n -c 4 192.0.2.254
-
+iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
+iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+# confirm with a ping
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add westnet-eastnet
-
-echo done
-
+ipsec auto --status
+echo "initdone"
