858517faaa21eb55e7a13664b540a29d869d6674
Paul Wouters <pwouters@redhat.com>
2013-12-18 21:09:09 -0500

    testing: update to various tests

--- testing/pluto/dpd-06/westrun.sh
+++ testing/pluto/dpd-06/westrun.sh
@@ -6,14 +6,12 @@ ipsec auto --up west-east
 ping -q -c 8 -n 192.1.2.23
 : show the tunnel
 ipsec eroute
-
 : Let R_U_THERE packets flow
 sleep 10
 : ==== cut ====
 ipsec eroute
 : ==== tuc ====
 sleep 10
-
 echo Create the block
 iptables -I INPUT -s 192.1.2.23/32  -d 0/0 -j DROP
 iptables -I OUTPUT -d 192.1.2.23/32 -s 0/0 -j DROP
@@ -24,4 +22,10 @@ echo Tunnel should be gone
 ipsec whack --listevents
 ipsec eroute
 : ==== tuc ====
-
+# remove the block
+iptables -D INPUT -s 192.1.2.23/32 -d 0/0 -j DROP
+iptables -D OUTPUT -d 192.1.2.23/32 -s 0/0 -j DROP
+sleep 10
+# Tunnel should be back up now
+ipsec eroute
+echo done
