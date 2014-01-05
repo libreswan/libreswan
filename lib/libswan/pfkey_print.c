1f7c917b787b817df9ee8056d49b2af5d3dca870
D. Hugh Redelmeier <hugh@mimosa.com>
2014-01-05 00:48:01 -0500

    - Fix signedness in declarations and formatting - use correct bit-sized formats for wider values - add more sanity checking of field lengths

--- lib/libswan/pfkey_print.c
+++ lib/libswan/pfkey_print.c
@@ -1,6 +1,7 @@
 /*
  * RFC2367 PF_KEYv2 Key management API message parser
  * Copyright (C) 2003 Michael Richardson <mcr@freeswan.org>
+ * Copyright (C) 2014 D. Hugh Redelmeier <hugh@mimosa.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -16,6 +17,7 @@
 
 #include <sys/types.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #include <libreswan.h>
 #include <libreswan/pfkeyv2.h>
@@ -23,53 +25,65 @@
 
 void pfkey_print(struct sadb_msg *msg, FILE *out)
 {
-	int len;
+	unsigned len;
 	struct sadb_ext *se;
 
 	fprintf(out,
-		"version=%d type=%d errno=%d satype=%d len=%d seq=%d pid=%d ",
+		"version=%u type=%u errno=%u satype=%u len=%u seq=%u pid=%u ",
 		msg->sadb_msg_version,
 		msg->sadb_msg_type,
 		msg->sadb_msg_errno,
 		msg->sadb_msg_satype,
 		msg->sadb_msg_len,
-		(int)msg->sadb_msg_seq,
-		(int)msg->sadb_msg_pid);
+		msg->sadb_msg_seq,
+		msg->sadb_msg_pid);
 
 	len = IPSEC_PFKEYv2_LEN(msg->sadb_msg_len);
 	len -= sizeof(struct sadb_msg);
 
 	se = (struct sadb_ext *)(&msg[1]);
-	while (len > (int)sizeof(struct sadb_ext)) {
-		fprintf(out, "{ext=%d len=%d ", se->sadb_ext_type,
-			se->sadb_ext_len);
+
+	while (len > sizeof(struct sadb_ext)) {
+		uint16_t ext_len = se->sadb_ext_len;	/* in units of IPSEC_PFKEYv2_ALIGN bytes */
+		unsigned elen = IPSEC_PFKEYv2_LEN(ext_len);	/* in units of bytes */
+		uint16_t ext_type = se->sadb_ext_type;
+		const char *too_small_for = NULL;
+
+		fprintf(out, "{ext=%u len=%u ", ext_type, ext_len);
 
 		/* make sure that there is enough left */
-		if ((int)IPSEC_PFKEYv2_LEN(se->sadb_ext_len) > len) {
-			fprintf(out, "short-packet(%d<%d) ", len,
-				(int)IPSEC_PFKEYv2_LEN(se->sadb_ext_len));
+		if (elen > len) {
+			fprintf(out, "short-packet(%u<%u) ", len, elen);
 
-			/* force it to match */
-			se->sadb_ext_len = IPSEC_PFKEYv2_WORDS(len);
-			goto dumpbytes;
+			/* truncate ext_len it to match len */
+			ext_len = IPSEC_PFKEYv2_WORDS(len);	/* partial words are ignored */
+			elen = IPSEC_PFKEYv2_LEN(ext_len);
+			ext_type = SADB_X_EXT_DEBUG;	/* force plain dump */
+		}
+
+		if (elen < sizeof(struct sadb_ext)) {
+			fprintf(out, "ext_len (%u) too small for sadb_ext header ", ext_len);
+			break;
 		}
 
 		/* okay, decode what we know */
-		switch (se->sadb_ext_type) {
+		switch (ext_type) {
 		case SADB_EXT_SA:
-		{
-			struct k_sadb_sa *sa = (struct k_sadb_sa *)se;
-			fprintf(out,
-				"spi=%08x replay=%d state=%d auth=%d encrypt=%d flags=%08x ref=%08x}",
-				(int)sa->sadb_sa_spi,
-				sa->sadb_sa_replay,
-				sa->sadb_sa_state,
-				sa->sadb_sa_auth,
-				sa->sadb_sa_encrypt,
-				(int)sa->sadb_sa_flags,
-				(int)sa->sadb_x_sa_ref);
-		}
-		break;
+			if (elen < sizeof(struct k_sadb_sa)) {
+				too_small_for = "struct k_sadb_sa";
+			} else {
+				struct k_sadb_sa *sa = (struct k_sadb_sa *)se;
+				fprintf(out,
+					"spi=%08x replay=%u state=%u auth=%u encrypt=%u flags=%08x ref=%08x}",
+					sa->sadb_sa_spi,
+					sa->sadb_sa_replay,
+					sa->sadb_sa_state,
+					sa->sadb_sa_auth,
+					sa->sadb_sa_encrypt,
+					sa->sadb_sa_flags,
+					sa->sadb_x_sa_ref);
+			}
+			break;
 
 		case SADB_X_EXT_ADDRESS_SRC_FLOW:
 		case SADB_X_EXT_ADDRESS_DST_FLOW:
@@ -77,52 +91,58 @@ void pfkey_print(struct sadb_msg *msg, FILE *out)
 		case SADB_X_EXT_ADDRESS_DST_MASK:
 		case SADB_EXT_ADDRESS_DST:
 		case SADB_EXT_ADDRESS_SRC:
-		{
-			struct sadb_address *addr = (struct sadb_address *)se;
-			int alen = IPSEC_PFKEYv2_LEN(addr->sadb_address_len) -
-				   sizeof(struct sadb_address);
-			unsigned char *bytes = (unsigned char *)&addr[1];
+			if (elen < sizeof(struct sadb_address)) {
+				too_small_for = "struct sadb_address";
+			} else {
+				struct sadb_address *addr = (struct sadb_address *)se;
+				int alen = IPSEC_PFKEYv2_LEN(addr->sadb_address_len) -
+					   sizeof(struct sadb_address);
+				unsigned char *bytes = (unsigned char *)&addr[1];
 
-			fprintf(out, "proto=%d prefixlen=%d addr=0x",
-				addr->sadb_address_proto,
-				addr->sadb_address_prefixlen);
+				fprintf(out, "proto=%u prefixlen=%u addr=0x",
+					addr->sadb_address_proto,
+					addr->sadb_address_prefixlen);
 
-			while (alen > 0) {
-				fprintf(out, "%02x", *bytes);
-				bytes++;
-				alen--;
+				while (alen > 0) {
+					fprintf(out, "%02x", *bytes);
+					bytes++;
+					alen--;
+				}
+				fprintf(out, " } ");
 			}
-			fprintf(out, " } ");
-		}
-		break;
+			break;
 
 		case SADB_X_EXT_PROTOCOL:
-		{
-			struct sadb_protocol *sp = (struct sadb_protocol *)se;
-			fprintf(out, "proto=%d direction=%d flags=%d } ",
-				sp->sadb_protocol_proto,
-				sp->sadb_protocol_direction,
-				sp->sadb_protocol_flags);
-		}
-		break;
+			if (elen < sizeof(struct sadb_protocol)) {
+				too_small_for = "struct sadb_protocol";
+			} else {
+				struct sadb_protocol *sp = (struct sadb_protocol *)se;
+				fprintf(out, "proto=%u direction=%u flags=%u } ",
+					sp->sadb_protocol_proto,
+					sp->sadb_protocol_direction,
+					sp->sadb_protocol_flags);
+			}
+			break;
 
 		case SADB_EXT_LIFETIME_CURRENT:
 		case SADB_EXT_LIFETIME_HARD:
 		case SADB_EXT_LIFETIME_SOFT:
-		{
-			struct sadb_lifetime *life =
-				(struct sadb_lifetime *)se;
+			if (elen < sizeof(struct sadb_lifetime)) {
+				too_small_for = "struct sadb_lifetime";
+			} else {
+				struct sadb_lifetime *life =
+					(struct sadb_lifetime *)se;
 
-			fprintf(out,
-				"allocations=%d bytes=%qd addtime=%qd usetime=%qd packets=%d",
-				(int)life->sadb_lifetime_allocations,
-				(long long)life->sadb_lifetime_bytes,
-				(long long)life->sadb_lifetime_addtime,
-				(long long)life->sadb_lifetime_usetime,
-				(int)life->sadb_x_lifetime_packets);
-			fprintf(out, " } ");
-		}
-		break;
+				fprintf(out,
+					"allocations=%u bytes=%" PRIu64 " addtime=%" PRIu64 " usetime=%" PRIu64 " packets=%u",
+					life->sadb_lifetime_allocations,
+					life->sadb_lifetime_bytes,
+					life->sadb_lifetime_addtime,
+					life->sadb_lifetime_usetime,
+					life->sadb_x_lifetime_packets);
+				fprintf(out, " } ");
+			}
+			break;
 
 		case SADB_EXT_RESERVED:
 		case SADB_EXT_ADDRESS_PROXY:
@@ -139,43 +159,33 @@ void pfkey_print(struct sadb_msg *msg, FILE *out)
 		case SADB_X_EXT_SATYPE2:
 		case SADB_X_EXT_SA2:
 		case SADB_X_EXT_ADDRESS_DST2:
-		case SADB_X_EXT_DEBUG:
+		case SADB_X_EXT_DEBUG:	/* also used in malformed case */
 		default:
 		{
-			unsigned int elen;
-			unsigned char *bytes;
-
-dumpbytes:
-
-			elen = IPSEC_PFKEYv2_LEN(se->sadb_ext_len) -
-			       sizeof(struct sadb_ext);
-			bytes = (unsigned char *)&se[1];
+			unsigned int count = elen - sizeof(struct sadb_ext);
+			unsigned char *bytes = (unsigned char *)&se[1];
 
 			fprintf(out, "bytes=0x");
-			while (elen > 0) {
+			while (count > 0) {
 				fprintf(out, "%02x", *bytes);
 				bytes++;
-				elen--;
+				count--;
 			}
 			fprintf(out, " } ");
 		}
 		break;
 		}
 
+		if (too_small_for != NULL)
+			fprintf(out, "too small for %s ", too_small_for);
+
 		/* skip to next extension header */
-		{
-			unsigned int elen =
-				IPSEC_PFKEYv2_LEN(se->sadb_ext_len);
-
-			if (elen < sizeof(struct sadb_ext)) {
-				fprintf(out, "illegal-length(%d) ", elen);
-				elen = sizeof(struct sadb_ext);
-			}
-
-			se = (struct sadb_ext *)(((unsigned char *)se) + elen);
-			len -= elen;
-		}
+		se = (struct sadb_ext *)(((unsigned char *)se) + elen);
+		len -= elen;
 	}
+
+	if (len > 0)
+		fprintf(out, "%u bytes left over", len);
+
 	fprintf(out, "\n");
 }
-
