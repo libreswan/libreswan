225f1a812f57aa2d8ba2a085289face43fa4b4cf
Paul Wouters <pwouters@redhat.com>
2014-01-09 23:02:39 -0500

    testing: updated psk-pluto-01

--- testing/pluto/psk-pluto-01/eastinit.sh
+++ testing/pluto/psk-pluto-01/eastinit.sh
@@ -2,6 +2,5 @@
 /usr/local/libexec/ipsec/_stackmanager start
 /usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add road--eastnet-psk
-ipsec auto --status
+ipsec auto --add road-eastnet-psk
 echo "initdone"
--- testing/pluto/psk-pluto-01/nicinit.sh
+++ testing/pluto/psk-pluto-01/nicinit.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # Display the table, so we know it's correct.
 iptables -t nat -L
 echo "initdone"
