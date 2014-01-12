5a763db86849dd1f6d57051f91e987adc9b5ab68
Paul Wouters <pwouters@redhat.com>
2014-01-12 02:07:44 -0500

    testing: about 25 test cases updated

--- testing/pluto/ikev2-12-transport-psk/westrun.sh
+++ testing/pluto/ikev2-12-transport-psk/westrun.sh
@@ -1,4 +1,3 @@
 ipsec auto --up  ipv4-psk-ikev2-transport
 ping -n -c 4 -I 192.1.2.45 192.1.2.23
-ipsec look
 echo done
--- testing/pluto/psk-pluto-06/eastinit.sh
+++ testing/pluto/psk-pluto-06/eastinit.sh
@@ -1,11 +1,5 @@
-#ipsec setup stop
-#umount /var/tmp; mount /var/tmp
-#umount /usr/local; mount /usr/local
-
-TESTNAME=psk-pluto-06
-source /testing/pluto/bin/eastlocal.sh
-
+/testing/guestbin/swan-prep
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
-ipsec auto --add road--eastnet-psk
+ipsec auto --add road-eastnet-psk
+echo "initdone"
