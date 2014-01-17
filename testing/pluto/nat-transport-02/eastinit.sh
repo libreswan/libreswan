545968a0f679b0b6b47d3273e9dbb4eb7b3a9198
Paul Wouters <pwouters@redhat.com>
2014-01-17 11:43:06 -0800

    testing: WIP update for nat-transport-02

--- testing/pluto/nat-transport-02/eastinit.sh
+++ testing/pluto/nat-transport-02/eastinit.sh
@@ -1,12 +1,7 @@
-: ==== start ====
-TESTNAME=nat-transport-02
-source /testing/pluto/bin/eastlocal.sh
-
-sh /etc/init.d/inetd restart
-
+/testing/guestbin/swan-prep
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
-ipsec auto --add north--east-port3
-ipsec whack --debug-control --debug-controlmore --debug-crypt
-
+ipsec auto --add north-east-port3
+nc -4 -l 192.1.2.23 2 &
+nc -4 -l 192.1.2.23 3 &
+echo "initdone"
