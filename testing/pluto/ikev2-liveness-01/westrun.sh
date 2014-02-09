9f50f53c76ee48210577af55c0204dc205f5286a
Paul Wouters <pwouters@redhat.com>
2014-02-09 17:02:31 -0500

    testing: minor change to ikev2-liveness-01

--- testing/pluto/ikev2-liveness-01/westrun.sh
+++ testing/pluto/ikev2-liveness-01/westrun.sh
@@ -2,7 +2,9 @@ ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
 ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec look
 # sleep for 60s to run a few liveness cycles 
-sleep 60
+sleep 20
+sleep 20
+sleep 20
 kill -9 `cat /run/pluto/pluto.pid`
 # sleep for timeout action
 sleep 30
