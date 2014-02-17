libreswan/libreswan

commit 2995415b24315ad492c453055e235ec8d6168724

Improve wire_chunk abstraction in pluto_crypto.h
- separate the wire_arena concept from the wire_chunk.
  This helps the C type system to catch errors
  and makes the code clearer

- include the space logically within the arena.
  This makes a number of parameters redundant
  leaving less opportunity for mistakes and
  confusion.

- add macros to make the notation more concise.

- unify functions with the same purpose

- rename functions to make their purpose clearer

- document the abstraction and conventions.

- make the function of the union within struct pluto_crypto_req clearer.

- sprinkse a few const qualifiers to clarify data flow

programs/pluto/crypt_ke.c
@@ -119,44 +119,40 @@ void calc_ke(struct pluto_crypto_req *r)
 		}
 	}
 
-	pluto_crypto_allocchunk(&kn->thespace, &kn->secret,
-				sizeof(SECKEYPrivateKey*));
+	ALLOC_WIRE_CHUNK(*kn, secret, sizeof(SECKEYPrivateKey*));
 	{
-		char *gip = wire_chunk_ptr(kn, &(kn->secret));
+		unsigned char *gip = wire_chunk_ptr(kn, &kn->secret);
 
 		memcpy(gip, &privk, sizeof(SECKEYPrivateKey *));
 	}
 
-	pluto_crypto_allocchunk(&kn->thespace, &kn->gi,
-				pubk->u.dh.publicValue.len);
+	ALLOC_WIRE_CHUNK(*kn, gi, pubk->u.dh.publicValue.len);
 	{
-		char *gip = wire_chunk_ptr(kn, &(kn->gi));
+		unsigned char *gip = wire_chunk_ptr(kn, &kn->gi);
 
 		memcpy(gip, pubk->u.dh.publicValue.data,
 		       pubk->u.dh.publicValue.len);
 	}
 
-	pluto_crypto_allocchunk(&kn->thespace, &kn->pubk,
-				sizeof(SECKEYPublicKey*));
+	ALLOC_WIRE_CHUNK(*kn, pubk, sizeof(SECKEYPublicKey*));
 	{
-		char *gip = wire_chunk_ptr(kn, &(kn->pubk));
+		unsigned char *gip = wire_chunk_ptr(kn, &kn->pubk);
 
 		memcpy(gip, &pubk, sizeof(SECKEYPublicKey*));
 	}
 
 	DBG(DBG_CRYPT, {
 		    DBG_dump("NSS: Local DH secret (pointer):\n",
-			     wire_chunk_ptr(kn, &(kn->secret)),
+			     wire_chunk_ptr(kn, &kn->secret),
 			     sizeof(SECKEYPrivateKey*));
 		    DBG_dump("NSS: Public DH value sent(computed in NSS):\n",
-			     wire_chunk_ptr(kn,
-					    &(kn->gi)),
+			     wire_chunk_ptr(kn, &kn->gi),
 			     pubk->u.dh.publicValue.len);
 	    });
 
 	DBG(DBG_CRYPT,
 	    DBG_dump("NSS: Local DH public value (pointer):\n",
-		     wire_chunk_ptr(kn, &(kn->pubk)),
+		     wire_chunk_ptr(kn, &kn->pubk),
 		     sizeof(SECKEYPublicKey*)));
 
 	/* clean up after ourselves */
@@ -181,7 +177,7 @@ void calc_nonce(struct pluto_crypto_req *r)
 {
 	struct pcr_kenonce *kn = &r->pcr_d.kn;
 
-	pluto_crypto_allocchunk(&kn->thespace, &kn->n, DEFAULT_NONCE_SIZE);
+	ALLOC_WIRE_CHUNK(*kn, n, DEFAULT_NONCE_SIZE);
 	get_rnd_bytes(wire_chunk_ptr(kn, &(kn->n)), DEFAULT_NONCE_SIZE);
 
 	DBG(DBG_CRYPT,

programs/pluto/crypt_start_dh.c
@@ -86,32 +86,25 @@ stf_status start_dh_secretiv(struct pluto_crypto_req_cont *cn,
 		    r.pcr_d.dhq.oakley_group, (int)r.pcr_len));
 
 	if (pss != NULL)
-		pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->pss,
-				       *pss);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->ni,
-			       st->st_ni);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->nr,
-			       st->st_nr);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->gi,
-			       st->st_gi);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->gr,
-			       st->st_gr);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space,
-			       &dhq->secret, st->st_sec_chunk);
+		WIRE_CLONE_CHUNK(*dhq, pss, *pss);
+	WIRE_CLONE_CHUNK(*dhq, ni, st->st_ni);
+	WIRE_CLONE_CHUNK(*dhq, nr, st->st_nr);
+	WIRE_CLONE_CHUNK(*dhq, gi, st->st_gi);
+	WIRE_CLONE_CHUNK(*dhq, gr, st->st_gr);
+	WIRE_CLONE_CHUNK(*dhq, secret, st->st_sec_chunk);
 
 	/*copying required encryption algo*/
 	/*dhq->encrypt_algo = st->st_oakley.encrypt;*/
 	dhq->encrypter = st->st_oakley.encrypter;
 	DBG(DBG_CRYPT,
 	    DBG_log("Copying DH pub key pointer to be sent to a thread helper"));
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->pubk,
-			       st->pubk);
+	WIRE_CLONE_CHUNK(*dhq, pubk, st->pubk);
 
-	pluto_crypto_allocchunk(&dhq->thespace, &dhq->icookie, COOKIE_SIZE);
+	ALLOC_WIRE_CHUNK(*dhq, icookie, COOKIE_SIZE);
 	memcpy(wire_chunk_ptr(dhq, &dhq->icookie),
 	       st->st_icookie, COOKIE_SIZE);
 
-	pluto_crypto_allocchunk(&dhq->thespace, &dhq->rcookie, COOKIE_SIZE);
+	ALLOC_WIRE_CHUNK(*dhq, rcookie, COOKIE_SIZE);
 	memcpy(wire_chunk_ptr(dhq, &dhq->rcookie),
 	       st->st_rcookie, COOKIE_SIZE);
 
@@ -189,32 +182,25 @@ stf_status start_dh_secret(struct pluto_crypto_req_cont *cn,
 	dhq->keysize = st->st_oakley.enckeylen / BITS_PER_BYTE;
 
 	if (pss != NULL)
-		pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->pss,
-				       *pss);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->ni,
-			       st->st_ni);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->nr,
-			       st->st_nr);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->gi,
-			       st->st_gi);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->gr,
-			       st->st_gr);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space,
-			       &dhq->secret, st->st_sec_chunk);
+		WIRE_CLONE_CHUNK(*dhq, pss, *pss);
+	WIRE_CLONE_CHUNK(*dhq, ni, st->st_ni);
+	WIRE_CLONE_CHUNK(*dhq, nr, st->st_nr);
+	WIRE_CLONE_CHUNK(*dhq, gi, st->st_gi);
+	WIRE_CLONE_CHUNK(*dhq, gr, st->st_gr);
+	WIRE_CLONE_CHUNK(*dhq, secret, st->st_sec_chunk);
 
 	/*copying required encryption algo*/
 	/* XXX Avesh: you commented this out on purpose or by accident ?? */
 	/*dhq->encrypter = st->st_oakley.encrypter;*/
 	DBG(DBG_CRYPT,
 	    DBG_log("Copying DH pub key pointer to be sent to a thread helper"));
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space,
-			       &dhq->pubk, st->pubk);
+	WIRE_CLONE_CHUNK(*dhq, pubk, st->pubk);
 
-	pluto_crypto_allocchunk(&dhq->thespace, &dhq->icookie, COOKIE_SIZE);
+	ALLOC_WIRE_CHUNK(*dhq, icookie, COOKIE_SIZE);
 	memcpy(wire_chunk_ptr(&r.pcr_d.dhq, &dhq->icookie),
 	       st->st_icookie, COOKIE_SIZE);
 
-	pluto_crypto_allocchunk(&dhq->thespace, &dhq->rcookie, COOKIE_SIZE);
+	ALLOC_WIRE_CHUNK(*dhq, rcookie, COOKIE_SIZE);
 	memcpy(wire_chunk_ptr(&r.pcr_d.dhq, &dhq->rcookie),
 	       st->st_rcookie, COOKIE_SIZE);
 
@@ -285,30 +271,24 @@ stf_status start_dh_v2(struct pluto_crypto_req_cont *cn,
 
 	passert(r.pcr_d.dhq.oakley_group != 0);
 
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->ni,
-			       st->st_ni);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->nr,
-			       st->st_nr);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->gi,
-			       st->st_gi);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space, &dhq->gr,
-			       st->st_gr);
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space,
-			       &dhq->secret, st->st_sec_chunk);
+	WIRE_CLONE_CHUNK(*dhq, ni, st->st_ni);
+	WIRE_CLONE_CHUNK(*dhq, nr, st->st_nr);
+	WIRE_CLONE_CHUNK(*dhq, gi, st->st_gi);
+	WIRE_CLONE_CHUNK(*dhq, gr, st->st_gr);
+	WIRE_CLONE_CHUNK(*dhq, secret, st->st_sec_chunk);
 
 	/*copying required encryption algo*/
 	/*dhq->encrypt_algo = st->st_oakley.encrypter->common.algo_v2id;*/
 	dhq->encrypter = st->st_oakley.encrypter;
 	DBG(DBG_CRYPT,
 	    DBG_log("Copying DH pub key pointer to be sent to a thread helper"));
-	pluto_crypto_copychunk(&dhq->thespace, dhq->space,
-			       &dhq->pubk, st->pubk);
+	WIRE_CLONE_CHUNK(*dhq, pubk, st->pubk);
 
-	pluto_crypto_allocchunk(&dhq->thespace, &dhq->icookie, COOKIE_SIZE);
+	ALLOC_WIRE_CHUNK(*dhq, icookie, COOKIE_SIZE);
 	memcpy(wire_chunk_ptr(dhq, &dhq->icookie),
 	       st->st_icookie, COOKIE_SIZE);
 
-	pluto_crypto_allocchunk(&dhq->thespace, &dhq->rcookie, COOKIE_SIZE);
+	ALLOC_WIRE_CHUNK(*dhq, rcookie, COOKIE_SIZE);
 	memcpy(wire_chunk_ptr(dhq, &dhq->rcookie),
 	       st->st_rcookie, COOKIE_SIZE);
 
@@ -336,9 +316,9 @@ stf_status start_dh_v2(struct pluto_crypto_req_cont *cn,
 }
 
 void finish_dh_v2(struct state *st,
-		  struct pluto_crypto_req *r)
+		  const struct pluto_crypto_req *r)
 {
-	struct pcr_skeycalc_v2 *dhv2 = &r->pcr_d.dhv2;
+	const struct pcr_skeycalc_v2_r *dhv2 = &r->pcr_d.dhv2;
 
 	clonetochunk(st->st_shared,   wire_chunk_ptr(dhv2, &(dhv2->shared)),
 		     dhv2->shared.len,   "calculated shared secret");

programs/pluto/ipsec_doi.c
@@ -125,22 +125,21 @@ void echo_hdr(struct msg_digest *md, bool enc, u_int8_t np)
  * Processing FOR KE values.
  */
 void unpack_KE(struct state *st,
-	       struct pluto_crypto_req *r,
+	       const struct pluto_crypto_req *r,
 	       chunk_t *g)
 {
-	struct pcr_kenonce *kn = &r->pcr_d.kn;
+	const struct pcr_kenonce *kn = &r->pcr_d.kn;
 
 	if (!st->st_sec_in_use) {
 		st->st_sec_in_use = TRUE;
 		freeanychunk(*g); /* happens in odd error cases */
 
-		clonetochunk(*g, wire_chunk_ptr(kn, &(kn->gi)),
+		clonetochunk(*g, wire_chunk_ptr(kn, &kn->gi),
 			     kn->gi.len, "saved gi value");
 		DBG(DBG_CRYPT,
 		    DBG_log("saving DH priv (local secret) and pub key into state struc"));
 		clonetochunk(st->st_sec_chunk,
-			     wire_chunk_ptr(kn,
-					    &(kn->secret)),
+			     wire_chunk_ptr(kn, &kn->secret),
 			     kn->secret.len,
 			     "pointer to DH private key (secret)");
 
@@ -178,9 +177,9 @@ notification_t accept_KE(chunk_t *dest, const char *val_name,
 	return NOTHING_WRONG;
 }
 
-void unpack_nonce(chunk_t *n, struct pluto_crypto_req *r)
+void unpack_nonce(chunk_t *n, const struct pluto_crypto_req *r)
 {
-	struct pcr_kenonce *kn = &r->pcr_d.kn;
+	const struct pcr_kenonce *kn = &r->pcr_d.kn;
 
 	freeanychunk(*n);
 	clonetochunk(*n, wire_chunk_ptr(kn, &(kn->n)),

commit 78a3643cc9e39ccd963a1aac3e0653ac9a2b6b17

fix bug introduced in 2995415b24315ad492c453055e235ec8d6168724 (improvement in wire_chunk abstraction in pluto_crypto.h)

programs/pluto/crypt_ke.c
@@ -121,14 +121,14 @@ void calc_ke(struct pluto_crypto_req *r)
 
 	ALLOC_WIRE_CHUNK(*kn, secret, sizeof(SECKEYPrivateKey *));
 	{
-		unsigned char *gip = wire_chunk_ptr(kn, &kn->secret);
+		unsigned char *gip = WIRE_CHUNK_PTR(*kn, secret);
 
 		memcpy(gip, &privk, sizeof(SECKEYPrivateKey *));
 	}
 
 	ALLOC_WIRE_CHUNK(*kn, gi, pubk->u.dh.publicValue.len);
 	{
-		unsigned char *gip = wire_chunk_ptr(kn, &kn->gi);
+		unsigned char *gip = WIRE_CHUNK_PTR(*kn, gi);
 
 		memcpy(gip, pubk->u.dh.publicValue.data,
 		       pubk->u.dh.publicValue.len);
@@ -136,23 +136,23 @@ void calc_ke(struct pluto_crypto_req *r)
 
 	ALLOC_WIRE_CHUNK(*kn, pubk, sizeof(SECKEYPublicKey *));
 	{
-		unsigned char *gip = wire_chunk_ptr(kn, &kn->pubk);
+		unsigned char *gip = WIRE_CHUNK_PTR(*kn, pubk);
 
 		memcpy(gip, &pubk, sizeof(SECKEYPublicKey *));
 	}
 
 	DBG(DBG_CRYPT, {
 		    DBG_dump("NSS: Local DH secret (pointer):\n",
-			     wire_chunk_ptr(kn, &kn->secret),
+			     WIRE_CHUNK_PTR(*kn, secret),
 			     sizeof(SECKEYPrivateKey*));
 		    DBG_dump("NSS: Public DH value sent(computed in NSS):\n",
-			     wire_chunk_ptr(kn, &kn->gi),
+			     WIRE_CHUNK_PTR(*kn, gi),
 			     pubk->u.dh.publicValue.len);
 	    });
 
 	DBG(DBG_CRYPT,
 	    DBG_dump("NSS: Local DH public value (pointer):\n",
-		     wire_chunk_ptr(kn, &kn->pubk),
+		     WIRE_CHUNK_PTR(*kn, pubk),
 		     sizeof(SECKEYPublicKey*)));
 
 	/* clean up after ourselves */
@@ -178,11 +178,11 @@ void calc_nonce(struct pluto_crypto_req *r)
 	struct pcr_kenonce *kn = &r->pcr_d.kn;
 
 	ALLOC_WIRE_CHUNK(*kn, n, DEFAULT_NONCE_SIZE);
-	get_rnd_bytes(wire_chunk_ptr(kn, &(kn->n)), DEFAULT_NONCE_SIZE);
+	get_rnd_bytes(WIRE_CHUNK_PTR(*kn, n), DEFAULT_NONCE_SIZE);
 
 	DBG(DBG_CRYPT,
 	    DBG_dump("Generated nonce:\n",
-		     wire_chunk_ptr(kn, &(kn->n)),
+		     WIRE_CHUNK_PTR(*kn, n),
 		     DEFAULT_NONCE_SIZE));
 }
 

programs/pluto/crypt_start_dh.c
@@ -60,7 +60,7 @@
 stf_status start_dh_secretiv(struct pluto_crypto_req_cont *cn,
 			     struct state *st,
 			     enum crypto_importance importance,
-			     enum phase1_role init,        /* TRUE=g_init,FALSE=g_r */
+			     enum phase1_role init,	/* TRUE=g_init,FALSE=g_r */
 			     u_int16_t oakley_group2)
 {
 	struct pluto_crypto_req r;
@@ -101,11 +101,11 @@ stf_status start_dh_secretiv(struct pluto_crypto_req_cont *cn,
 	WIRE_CLONE_CHUNK(*dhq, pubk, st->pubk);
 
 	ALLOC_WIRE_CHUNK(*dhq, icookie, COOKIE_SIZE);
-	memcpy(wire_chunk_ptr(dhq, &dhq->icookie),
+	memcpy(WIRE_CHUNK_PTR(*dhq, icookie),
 	       st->st_icookie, COOKIE_SIZE);
 
 	ALLOC_WIRE_CHUNK(*dhq, rcookie, COOKIE_SIZE);
-	memcpy(wire_chunk_ptr(dhq, &dhq->rcookie),
+	memcpy(WIRE_CHUNK_PTR(*dhq, rcookie),
 	       st->st_rcookie, COOKIE_SIZE);
 
 	passert(dhq->oakley_group != 0);
@@ -136,22 +136,22 @@ void finish_dh_secretiv(struct state *st,
 {
 	struct pcr_skeyid_r *dhr = &r->pcr_d.dhr;
 
-	clonetochunk(st->st_shared, wire_chunk_ptr(dhr, &dhr->shared),
+	clonetochunk(st->st_shared, WIRE_CHUNK_PTR(*dhr, shared),
 		     dhr->shared.len, "calculated shared secret");
-	clonetochunk(st->st_skeyid, wire_chunk_ptr(dhr, &dhr->skeyid),
+	clonetochunk(st->st_skeyid, WIRE_CHUNK_PTR(*dhr, skeyid),
 		     dhr->skeyid.len, "calculated skeyid secret");
-	clonetochunk(st->st_skeyid_d, wire_chunk_ptr(dhr, &dhr->skeyid_d),
+	clonetochunk(st->st_skeyid_d, WIRE_CHUNK_PTR(*dhr, skeyid_d),
 		     dhr->skeyid_d.len, "calculated skeyid_d secret");
-	clonetochunk(st->st_skeyid_a, wire_chunk_ptr(dhr, &dhr->skeyid_a),
+	clonetochunk(st->st_skeyid_a, WIRE_CHUNK_PTR(*dhr, skeyid_a),
 		     dhr->skeyid_a.len, "calculated skeyid_a secret");
-	clonetochunk(st->st_skeyid_e, wire_chunk_ptr(dhr, &dhr->skeyid_e),
+	clonetochunk(st->st_skeyid_e, WIRE_CHUNK_PTR(*dhr, skeyid_e),
 		     dhr->skeyid_e.len, "calculated skeyid_a secret");
-	clonetochunk(st->st_enc_key, wire_chunk_ptr(dhr, &dhr->enc_key),
+	clonetochunk(st->st_enc_key, WIRE_CHUNK_PTR(*dhr, enc_key),
 		     dhr->enc_key.len, "calculated key for phase 1");
 
 	passert(dhr->new_iv.len <= MAX_DIGEST_LEN);
 	passert(dhr->new_iv.len > 0);
-	memcpy(st->st_new_iv, wire_chunk_ptr(dhr, &dhr->new_iv),
+	memcpy(st->st_new_iv, WIRE_CHUNK_PTR(*dhr, new_iv),
 	       dhr->new_iv.len);
 	st->st_new_iv_len = dhr->new_iv.len;
 
@@ -197,11 +197,11 @@ stf_status start_dh_secret(struct pluto_crypto_req_cont *cn,
 	WIRE_CLONE_CHUNK(*dhq, pubk, st->pubk);
 
 	ALLOC_WIRE_CHUNK(*dhq, icookie, COOKIE_SIZE);
-	memcpy(wire_chunk_ptr(&r.pcr_d.dhq, &dhq->icookie),
+	memcpy(WIRE_CHUNK_PTR(*dhq, icookie),
 	       st->st_icookie, COOKIE_SIZE);
 
 	ALLOC_WIRE_CHUNK(*dhq, rcookie, COOKIE_SIZE);
-	memcpy(wire_chunk_ptr(&r.pcr_d.dhq, &dhq->rcookie),
+	memcpy(WIRE_CHUNK_PTR(*dhq, rcookie),
 	       st->st_rcookie, COOKIE_SIZE);
 
 	e = send_crypto_helper_request(&r, cn, &toomuch);
@@ -231,8 +231,8 @@ void finish_dh_secret(struct state *st,
 {
 	struct pcr_skeyid_r *dhr = &r->pcr_d.dhr;
 
-	clonetochunk(st->st_shared,   wire_chunk_ptr(dhr, &(dhr->shared)),
-		     dhr->shared.len,   "calculated shared secret");
+	clonetochunk(st->st_shared, WIRE_CHUNK_PTR(*dhr, shared),
+		     dhr->shared.len, "calculated shared secret");
 }
 
 /*
@@ -241,7 +241,7 @@ void finish_dh_secret(struct state *st,
 stf_status start_dh_v2(struct pluto_crypto_req_cont *cn,
 		       struct state *st,
 		       enum crypto_importance importance,
-		       enum phase1_role init,        /* TRUE=g_init,FALSE=g_r */
+		       enum phase1_role init,	/* TRUE=g_init,FALSE=g_r */
 		       u_int16_t oakley_group2)
 {
 	struct pluto_crypto_req r;
@@ -255,7 +255,7 @@ stf_status start_dh_v2(struct pluto_crypto_req_cont *cn,
 
 	DBG(DBG_CONTROLMORE,
 	    DBG_log("calculating skeyseed using prf=%s integ=%s cipherkey=%s",
-		    enum_name(&ikev2_trans_type_prf_names,   st->st_oakley.prf_hash),
+		    enum_name(&ikev2_trans_type_prf_names, st->st_oakley.prf_hash),
 		    enum_name(&ikev2_trans_type_integ_names,
 			      st->st_oakley.integ_hash),
 		    enum_name(&ikev2_trans_type_encr_names,
@@ -263,7 +263,7 @@ stf_status start_dh_v2(struct pluto_crypto_req_cont *cn,
 
 	/* convert appropriate data to dhq */
 	dhq->auth = st->st_oakley.auth;
-	dhq->prf_hash   = st->st_oakley.prf_hash;
+	dhq->prf_hash = st->st_oakley.prf_hash;
 	dhq->integ_hash = st->st_oakley.integ_hash;
 	dhq->oakley_group = oakley_group2;
 	dhq->init = init;
@@ -285,11 +285,11 @@ stf_status start_dh_v2(struct pluto_crypto_req_cont *cn,
 	WIRE_CLONE_CHUNK(*dhq, pubk, st->pubk);
 
 	ALLOC_WIRE_CHUNK(*dhq, icookie, COOKIE_SIZE);
-	memcpy(wire_chunk_ptr(dhq, &dhq->icookie),
+	memcpy(WIRE_CHUNK_PTR(*dhq, icookie),
 	       st->st_icookie, COOKIE_SIZE);
 
 	ALLOC_WIRE_CHUNK(*dhq, rcookie, COOKIE_SIZE);
-	memcpy(wire_chunk_ptr(dhq, &dhq->rcookie),
+	memcpy(WIRE_CHUNK_PTR(*dhq, rcookie),
 	       st->st_rcookie, COOKIE_SIZE);
 
 	passert(dhq->oakley_group != 0);
@@ -320,21 +320,21 @@ void finish_dh_v2(struct state *st,
 {
 	const struct pcr_skeycalc_v2_r *dhv2 = &r->pcr_d.dhv2;
 
-	clonetochunk(st->st_shared,   wire_chunk_ptr(dhv2, &(dhv2->shared)),
-		     dhv2->shared.len,   "calculated shared secret");
-	clonetochunk(st->st_skey_d,   wire_chunk_ptr(dhv2, &(dhv2->skeyid_d)),
-		     dhv2->skeyid_d.len,   "calculated skeyid secret");
-	clonetochunk(st->st_skey_ai, wire_chunk_ptr(dhv2, &(dhv2->skeyid_ai)),
+	clonetochunk(st->st_shared, WIRE_CHUNK_PTR(*dhv2, shared),
+		     dhv2->shared.len, "calculated shared secret");
+	clonetochunk(st->st_skey_d, WIRE_CHUNK_PTR(*dhv2, skeyid_d),
+		     dhv2->skeyid_d.len, "calculated skeyid secret");
+	clonetochunk(st->st_skey_ai, WIRE_CHUNK_PTR(*dhv2, skeyid_ai),
 		     dhv2->skeyid_ai.len, "calculated skeyid_ai secret");
-	clonetochunk(st->st_skey_ar, wire_chunk_ptr(dhv2, &(dhv2->skeyid_ar)),
+	clonetochunk(st->st_skey_ar, WIRE_CHUNK_PTR(*dhv2, skeyid_ar),
 		     dhv2->skeyid_ar.len, "calculated skeyid_ar secret");
-	clonetochunk(st->st_skey_pi, wire_chunk_ptr(dhv2, &(dhv2->skeyid_pi)),
+	clonetochunk(st->st_skey_pi, WIRE_CHUNK_PTR(*dhv2, skeyid_pi),
 		     dhv2->skeyid_pi.len, "calculated skeyid_pi secret");
-	clonetochunk(st->st_skey_pr, wire_chunk_ptr(dhv2, &(dhv2->skeyid_pr)),
+	clonetochunk(st->st_skey_pr, WIRE_CHUNK_PTR(*dhv2, skeyid_pr),
 		     dhv2->skeyid_pr.len, "calculated skeyid_pr secret");
-	clonetochunk(st->st_skey_ei, wire_chunk_ptr(dhv2, &(dhv2->skeyid_ei)),
+	clonetochunk(st->st_skey_ei, WIRE_CHUNK_PTR(*dhv2, skeyid_ei),
 		     dhv2->skeyid_ei.len, "calculated skeyid_ei secret");
-	clonetochunk(st->st_skey_er, wire_chunk_ptr(dhv2, &(dhv2->skeyid_er)),
+	clonetochunk(st->st_skey_er, WIRE_CHUNK_PTR(*dhv2, skeyid_er),
 		     dhv2->skeyid_er.len, "calculated skeyid_er secret");
 
 	st->hidden_variables.st_skeyid_calculated = TRUE;

programs/pluto/ipsec_doi.c
@@ -134,17 +134,17 @@ void unpack_KE(struct state *st,
 		st->st_sec_in_use = TRUE;
 		freeanychunk(*g); /* happens in odd error cases */
 
-		clonetochunk(*g, wire_chunk_ptr(kn, &kn->gi),
+		clonetochunk(*g, WIRE_CHUNK_PTR(*kn, gi),
 			     kn->gi.len, "saved gi value");
 		DBG(DBG_CRYPT,
 		    DBG_log("saving DH priv (local secret) and pub key into state struc"));
 		clonetochunk(st->st_sec_chunk,
-			     wire_chunk_ptr(kn, &kn->secret),
+			     WIRE_CHUNK_PTR(*kn, secret),
 			     kn->secret.len,
 			     "pointer to DH private key (secret)");
 
 		clonetochunk(st->pubk,
-			     wire_chunk_ptr(kn, &(kn->pubk)),
+			     WIRE_CHUNK_PTR(*kn, pubk),
 			     kn->pubk.len, "pointer to DH public key");
 	}
 }
@@ -182,7 +182,7 @@ void unpack_nonce(chunk_t *n, const struct pluto_crypto_req *r)
 	const struct pcr_kenonce *kn = &r->pcr_d.kn;
 
 	freeanychunk(*n);
-	clonetochunk(*n, wire_chunk_ptr(kn, &(kn->n)),
+	clonetochunk(*n, WIRE_CHUNK_PTR(*kn, n),
 		     DEFAULT_NONCE_SIZE, "initiator nonce");
 }
 

programs/pluto/pluto_crypt.h
@@ -62,8 +62,12 @@ typedef unsigned int pcr_req_id;
  * within the struct.  This is managed within an arena.
  *
  * Because of two limitations of C typing, the actual space must
- * be created as an array of unsigned char of the required size
- * (reduced by 1) right after the arena.
+ * be created in two tranches: a fixed amount in the arena's
+ * "space" field and the remaining amount in a buffer "more_space"
+ * that follows the arena.  This odd arrangement allows all arenas
+ * to be the same type.  A little bit of space is wasted by
+ * alignment padding at the end of the arena struct; this could
+ * be fixed at the cost of complexity.
  *
  * The macros with uppercase names are magic: they know
  * the field names "arena" and "space" in a way that a function
@@ -78,10 +82,12 @@ typedef struct wire_arena {
 
 #define DECLARE_WIRE_ARENA(size) \
 	wire_arena_t arena; \
-	unsigned char space[(size) - 1]
+	unsigned char more_space[(size) - 1]
 
-#define INIT_WIRE_ARENA(parent) \
-	{ (parent).arena.next = 0; (parent).arena.roof = sizeof((parent).space) + 1; }
+#define INIT_WIRE_ARENA(parent) { \
+		(parent).arena.next = 0; \
+		(parent).arena.roof = sizeof((parent).arena.space) + sizeof((parent).more_space); \
+	}
 
 typedef struct wire_chunk {
 	unsigned int start;
@@ -121,8 +127,8 @@ extern void wire_clone_chunk(wire_arena_t *arena,
  * It is assumed that the memory for the wired_chunk will persist unchanged
  * during the life of the chunk.
  */
-#define setchunk_from_wire(chunk, parent, wire) \
-	setchunk(chunk, wire_chunk_ptr(&(parent)->arena, (wire)), (wire)->len)
+#define setchunk_from_wire(chunk, parent_ptr, wire) \
+	setchunk(chunk, wire_chunk_ptr(&(parent_ptr)->arena, (wire)), (wire)->len)
 
 /* end of wire_chunk definitions */
 

testing/crypto/pk-dh-01/pk-dh.c
@@ -183,7 +183,7 @@ int main(int argc, char *argv[])
 
 #define dumpdat(field) \
 	libreswan_DBG_dump(#field,      \
-			   wire_chunk_ptr(skq, &skq->field), \
+			   WIRE_CHUNK_PTR(*skq, field), \
 			   skq->field.len);
 
 	dumpdat(icookie);
@@ -202,7 +202,7 @@ int main(int argc, char *argv[])
 	printf("\noutput:\n");
 
 	{
-		void *shared = wire_chunk_ptr(skr, &skr->shared);
+		void *shared = WIRE_CHUNK_PTR(*skr, shared);
 
 		libreswan_DBG_dump("shared", shared, skr->shared.len);
 	}

testing/crypto/pk-dh-01/tc2.c
@@ -41,7 +41,7 @@ static void perform_t2_test(void)
 
 #define dumpdat(field) \
 	libreswan_DBG_dump(#field,      \
-			   wire_chunk_ptr(skq, &skq->field), \
+			   WIRE_CHUNK_PTR(*skq, field), \
 			   skq->field.len);
 
 	dumpdat(icookie);
@@ -63,7 +63,7 @@ static void perform_t2_test(void)
 	fflush(stderr);
 
 	{
-		void *shared = wire_chunk_ptr(skr, &skr->shared);
+		void *shared = WIRE_CHUNK_PTR(*skr, shared);
 
 		libreswan_DBG_dump("shared", shared, skr->shared.len);
 	}

testing/crypto/v2-dh-06/pk-dh.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 
 #define dumpdat(field) \
 	libreswan_DBG_dump(#field,      \
-			   wire_chunk_ptr(skq, &skq->field), \
+			   WIRE_CHUNK_PTR(*skq, field), \
 			   skq->field.len);
 
 	dumpdat(icookie);
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
 	fflush(stdout);
 	fflush(stderr);
 
-#define dumpskr(FOO) { void *FOO = wire_chunk_ptr(skr, &skr->FOO); \
+#define dumpskr(FOO) { void *FOO = WIRE_CHUNK_PTR(*skr, FOO); \
 		       libreswan_DBG_dump(#FOO, FOO, skr->FOO.len); \
 }
 

testing/crypto/v2-dh-06/tc2.c
@@ -41,7 +41,7 @@ static void perform_t2_test(void)
 
 #define dumpdat(field) \
 	libreswan_DBG_dump(#field,      \
-			   wire_chunk_ptr(skq, &skq->field), \
+			   WIRE_CHUNK_PTR(*skq, field), \
 			   skq->field.len);
 
 	dumpdat(icookie);
@@ -63,7 +63,7 @@ static void perform_t2_test(void)
 	fflush(stderr);
 
 	{
-		void *shared = wire_chunk_ptr(skr, &skr->shared);
+		void *shared = WIRE_CHUNK_PTR(*skr, shared);
 
 		libreswan_DBG_dump("shared", shared, skr->shared.len);
 	}

testing/crypto/v2-dh-07/pk-dh.c
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
 
 #define dumpdat(field) \
 	libreswan_DBG_dump(#field,      \
-			   wire_chunk_ptr(skq, &skq->field), \
+			   WIRE_CHUNK_PTR(*skq, field), \
 			   skq->field.len);
 
 	dumpdat(icookie);
@@ -109,7 +109,7 @@ int main(int argc, char *argv[])
 	fflush(stdout);
 	fflush(stderr);
 
-#define dumpskr(FOO) { void *FOO = wire_chunk_ptr(skr, &skr->FOO); \
+#define dumpskr(FOO) { void *FOO = WIRE_CHUNK_PTR(*skr, FOO); \
 		       libreswan_DBG_dump(#FOO, FOO, skr->FOO.len); \
 }
 