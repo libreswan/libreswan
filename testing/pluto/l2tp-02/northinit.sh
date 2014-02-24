b8191e0e91b0de63e7473c70dedcf7b941727da1
Paul Wouters <pwouters@redhat.com>
2014-02-24 14:37:09 -0500

    testing: converted l2tp-02 to KVM

--- testing/pluto/l2tp-02/northinit.sh
+++ testing/pluto/l2tp-02/northinit.sh
@@ -1,30 +1,14 @@
-: ==== start ====
-TESTNAME=l2tp-01
-source /testing/pluto/bin/northlocal.sh
-
-if [ -f /var/run/l2tpd.pid ]; then kill `cat /var/run/l2tpd.pid`; fi
-
+/testing/guestbin/swan-prep
 iptables -F INPUT
 iptables -F OUTPUT
-ipsec setup stop
-
 # make sure that clear text does not get through
 iptables -A INPUT -i eth1 -s 192.1.2.23 -p udp --sport 1701 -j REJECT
 iptables -A OUTPUT -o eth1 -d 192.1.2.23 -p udp --dport 1701 -j REJECT
-
-ipsec setup restart
+ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add north--east-l2tp
 ipsec auto --add north--east-pass
-
 ipsec auto --route north--east-pass
-ipsec whack --debug-control --debug-controlmore --debug-natt
-
-if [ ! -f /etc/ppp/chap-secrets ]; then mount --bind /testing/pluto/l2tp-01 /etc/ppp; fi
-(cd /tmp && l2tpd -c /testing/pluto/l2tp-01/north.l2tpd.conf -D 2>/tmp/l2tpd.log ) &
-
+(cd /tmp && xl2tpd -D 2>/tmp/xl2tpd.log 1>&2) &
 ipsec auto --route north--east-l2tp
-
 echo done
-
