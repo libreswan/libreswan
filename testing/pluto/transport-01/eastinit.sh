731e1f17859c5f9fd2619a51d86ed0af792a06c4
Paul Wouters <pwouters@redhat.com>
2014-03-24 16:28:15 -0400

    testing: fixup transport-01

--- testing/pluto/transport-01/eastinit.sh
+++ testing/pluto/transport-01/eastinit.sh
@@ -1,11 +1,11 @@
 /testing/guestbin/swan-prep
-ipsec _stackmanager start
-/usr/local/libexec/ipsec/pluto --config /etc/ipsec.conf
+ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add west--east-port3
-ipsec auto --add west--east-pass
-ipsec auto --add west--east-pass2
+ipsec auto --add west-east-port3
+ipsec auto --add west-east-pass
+ipsec auto --add west-east-pass2
 nc -l 3 &
-ipsec auto --route west--east-pass
-ipsec auto --route west--east-pass2
+ipsec auto --route west-east-pass
+ipsec auto --route west-east-pass2
 ipsec eroute
+echo "initdone"
