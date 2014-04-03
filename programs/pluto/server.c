01ddb09760908c0e7953d97b58a9dd8e5d88ce95
Paul Wouters <pwouters@redhat.com>
2014-04-03 18:46:35 -0400

    pluto: print port in interface status
    We were seeing test result changes where we noticed a new duplicate
    entry in the interface list, eg:
    
     000 interface ipsec0/eth1 192.1.2.23
    +000 interface ipsec0/eth1 192.1.2.23
    
    This turned out to be caused by the new default enabling of NAT-T which
    also binds port 4500. By printing the port, the output now becomes:
    
     000 interface ipsec0/eth1 192.1.2.23:4500
     000 interface ipsec0/eth1 192.1.2.23:500

--- programs/pluto/server.c
+++ programs/pluto/server.c
@@ -420,9 +420,9 @@ void show_ifaces_status(void)
 	struct iface_port *p;
 
 	for (p = interfaces; p != NULL; p = p->next)
-		whack_log(RC_COMMENT, "interface %s/%s %s",
+		whack_log(RC_COMMENT, "interface %s/%s %s:%d",
 			  p->ip_dev->id_vname, p->ip_dev->id_rname,
-			  ip_str(&p->ip_addr));
+			  ip_str(&p->ip_addr), p->port);
 }
 
 void show_debug_status(void)
