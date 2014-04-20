ca51bf19879fd386e1e84c2dc8e5871a8583ec78
Antony Antony <antony@phenome.org>
2014-04-20 15:26:52 +0200

    real fix the previous one was a git accident

--- lib/libipsecconf/confread.c
+++ lib/libipsecconf/confread.c
@@ -658,7 +658,6 @@ static bool validate_end(struct ub_ctx *dnsctx,
 			    leftright, end->strings[KSCF_ADDRESSPOOL] );
 
 		er = ttorange(addresspool, 0, AF_INET, &end->pool_range);
-<<<<<<< HEAD
 		if (er != NULL)
 			ERR_FOUND("bad %saddresspool=%s [%s]", leftright,
 					addresspool, er);
@@ -671,23 +670,7 @@ static bool validate_end(struct ub_ctx *dnsctx,
 		if(ip_address_cmp(&end->pool_range.start, &end->pool_range.end)
 				< 0)
 			ERR_FOUND("invalid range in %addresspool=%s",
-=======
-		if (er != NULL) 
-			ERR_FOUND("bad %saddresspool=%s [%s]", leftright, 
-					addresspool, er);
-		if (ip_address_isany(&end->pool_range.start))
-			ERR_FOUND("bad start in %saddresspool=%s", 
 					leftright, addresspool);
-		if (ip_address_isany(&end->pool_range.end))
-			ERR_FOUND("bad end in %addresspool=%s", 
-					leftright, 
-					addresspool);
-		if(ip_address_cmp(&end->pool_range.start, 
-					&end->pool_range.end) < 0)
-			ERR_FOUND("invalid range in %addresspool=%s", 
->>>>>>> 4d248dad7fa334b3ecb9bbf234acc6367544e4a7
-					leftright, 
-					addresspool);
 	}
 
 	if (end->options_set[KNCF_XAUTHSERVER] ||
--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -257,7 +257,6 @@ void delete_connection(struct connection *c, bool relations)
 	if (c->kind == CK_GROUP)
 		delete_group(c);
 
-<<<<<<< HEAD
 	if ((c->pool != NULL) && (c->kind != CK_GOING_AWAY)) {
 		DBG(DBG_CONTROLMORE, DBG_log("unreference addresspool of conn "
 					"%s [%lu] kind %s refcnt %u",
@@ -265,20 +264,6 @@ void delete_connection(struct connection *c, bool relations)
 					enum_name(&connection_kind_names, 
 						c->kind), c->pool->refcnt));
 		unreference_addresspool(c->pool);
-=======
-#if 0
-	/* TODO:  this will be enabled in the next version */
-	if ((c->pool != NULL) && (c->kind == CK_TEMPLATE)) {
-		DBG(DBG_CONTROLMORE,
-			DBG_log("free addresspool entry for the conn %s "
-				"kind %s conn serial %d pool refcnt %u",
-				c->name, enum_name(&connection_kind_names,
-						c->kind),
-				c->instance_serial,
-				c->pool->refcnt));
-		unreference_addresspool(c->pool);
-		c->pool = NULL;
->>>>>>> 4d248dad7fa334b3ecb9bbf234acc6367544e4a7
 	}
 
 	/* free up any logging resources */
