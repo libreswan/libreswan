a0cdcf839ee7ebcecbe2df8179edb42a2a80b6c2
Paul Wouters <pwouters@redhat.com>
2014-04-26 11:08:32 -0400

    testing: added nhelper tests for ikev2 psk, raw rsa and x509

---
+++ testing/pluto/ikev2-03-basic-rawrsa-nhelpers0/eastinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep
+ipsec _stackmanager start 
+ipsec setup start:
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-ikev2
+ipsec auto --status
+echo "initdone"
