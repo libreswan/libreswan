ab1a11b0a224e5113dbf0f20fdbf36a94fde775a
Paul Wouters <pwouters@redhat.com>
2014-05-02 13:35:45 -0400

    testing: added xauth-pluto-05-netkey
    As it handles obtaining the IP differently from KLIPS/MAST
    (there is no changesource() function for NETKEY

---
+++ testing/pluto/xauth-pluto-05-netkey/roadinit.sh
@@ -0,0 +1,7 @@
+/testing/guestbin/swan-prep
+ifconfig eth0 192.1.3.194 netmask 255.255.255.0
+route add -net default gw 192.1.3.254
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add modecfg-road-eastnet-psk
+echo "initdone"
