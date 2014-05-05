d945ab733ad0d2c469f7f68a2ea390345b794933
Paul Wouters <pwouters@redhat.com>
2014-05-05 16:45:59 -0400

    testing: another update on known good test output

--- testing/pluto/algo-pluto-01/eastinit.sh
+++ testing/pluto/algo-pluto-01/eastinit.sh
@@ -2,5 +2,5 @@
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-aes256
-ipsec auto --status
+ipsec auto --status |grep westnet-eastnet-aes256
 echo "initdone"
--- testing/pluto/l2tp-02-netkey/eastinit.sh
+++ testing/pluto/l2tp-02-netkey/eastinit.sh
@@ -2,9 +2,9 @@
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 # pass non-l2tp plaintext, this also tests passthrough has higher priority
-ipsec auto --add any--east-l2tp
-ipsec auto --add north--east-pass
-ipsec auto --route north--east-pass
+ipsec auto --add any-east-l2tp
+ipsec auto --add north-east-pass
+ipsec auto --route north-east-pass
 # make sure that clear text does not get through
 iptables -A INPUT -i eth1 -d 192.1.2.23/32 -p udp --dport 1701 -j LOGDROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
