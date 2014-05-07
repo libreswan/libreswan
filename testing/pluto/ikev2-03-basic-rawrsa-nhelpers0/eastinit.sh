634bf78569824b93b8439c7cd612012ed72f66b5
Paul Wouters <pwouters@redhat.com>
2014-05-07 16:41:34 -0400

    testing: some more test cases updated

--- testing/pluto/ikev2-03-basic-rawrsa-nhelpers0/eastinit.sh
+++ testing/pluto/ikev2-03-basic-rawrsa-nhelpers0/eastinit.sh
@@ -1,8 +1,5 @@
 /testing/guestbin/swan-prep
-ipsec _stackmanager start 
-ipsec setup start:
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-ipsec auto --status
 echo "initdone"
