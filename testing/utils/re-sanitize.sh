04cb6e69d3fe61dbce6ad4059fac91c36461d68d
Paul Wouters <pwouters@redhat.com>
2014-05-23 15:50:45 -0400

    testing: utils/re-sanitize.sh can be re-run to test sanitizer changes
    eg: cd testing/pluto/basic-pluto-01/ ; ../../utils/re-sanitize.sh

---
+++ testing/utils/re-sanitize.sh
@@ -0,0 +1,65 @@
+#!/bin/bash 
+. ../../../kvmsetup.sh
+if [ -f ./testparams.sh ] ; then
+        . ./testparams.sh
+else
+        . ../../default-testparams.sh
+fi
+. ../setup.sh
+. ../../utils/functions.sh
+
+for host in $LIBRESWANHOSTS
+do
+   if [ -f "${host}.console.txt" ]
+   then
+	echo "re-sanitizing ${host}"
+	# sanitize last run
+	if [ -f OUTPUT/${host}.console.verbose.txt ]
+	then
+		cleanups="cat OUTPUT/${host}.console.verbose.txt "
+		for fixup in `echo $REF_CONSOLE_FIXUPS`
+		do
+
+			if [ -f $FIXUPDIR/$fixup ]
+			then
+	    			case $fixup in
+				*.sed) cleanups="$cleanups | sed -f $FIXUPDIR/$fixup";;
+				*.pl)  cleanups="$cleanups | perl $FIXUPDIR/$fixup";;
+				*.awk) cleanups="$cleanups | awk -f $FIXUPDIR/$fixup";;
+		    		*) echo Unknown fixup type: $fixup;;
+            			esac
+			elif [ -f $FIXUPDIR2/$fixup ]
+			then
+	    			case $fixup in
+				*.sed) cleanups="$cleanups | sed -f $FIXUPDIR2/$fixup";;
+				*.pl)  cleanups="$cleanups | perl $FIXUPDIR2/$fixup";;
+				*.awk) cleanups="$cleanups | awk -f $FIXUPDIR2/$fixup";;
+		    		*) echo Unknown fixup type: $fixup;;
+            			esac
+			else
+	    			echo Fixup $fixup not found.
+	    			return
+			fi
+		done
+
+		fixedoutput=OUTPUT/${host}.console.txt
+		rm -f $fixedoutput OUTPUT/${host}.console.diff
+		## debug echo $cleanups
+		eval $cleanups >$fixedoutput
+		# stick terminating newline in for fun.
+		echo >>$fixedoutput
+		if diff -N -u -w -b -B ${host}.console.txt $fixedoutput >OUTPUT/${host}.console.diff
+		then
+			echo "${host}Console output matched"
+		else
+			echo "${host}Console output differed"
+		fi
+		if [ -f OUTPUT/${host}.console.diff -a \! -s OUTPUT/${host}.console.diff ]
+		then
+			rm OUTPUT/${host}.console.diff
+		fi
+
+	fi
+   fi
+done
+
