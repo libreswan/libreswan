a8da8005ebb4e21a0fc2e608f80305fe53c1b663
Paul Wouters <pwouters@redhat.com>
2014-05-29 16:13:26 -0400

    testing: Added AES default key size tests
    IKEv1: ike-algo-02-aes-default (ike-algo-02 testwas was obsoleted and removed)
    IKEv2: ikev2-algo-05-aes-default
    
    The interop test interop-ikev2-strongswan-05-psk-aes was modified to include
    unspecified "aes" (as opposed to "aes128")

--- testing/pluto/ike-algo-02-aes-default/final.sh
+++ testing/pluto/ike-algo-02-aes-default/final.sh
@@ -1,6 +1,11 @@
 ipsec look
+grep -A 1 KEY_LENGTH /tmp/pluto.log
 : ==== cut ====
 ipsec auto --status
+: ==== tuc ====
+ipsec whack --shutdown
+: ==== cut ====
+ipsec look
 : ==== tuc ====
 if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* ./; fi
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
