ebcf2c358925283553ceee9e748ef3e045049fa7
Paul Wouters <pwouters@redhat.com>
2014-06-30 23:22:19 -0400

    testing: place core in OUTPUT/ instead of testdir itself

--- testing/pluto/basic-pluto-01-valgrind/final.sh
+++ testing/pluto/basic-pluto-01-valgrind/final.sh
@@ -3,6 +3,6 @@ ipsec look
 ipsec auto --status
 # ==== tuc ====
 ipsec whack --shutdown
-if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* ./; fi
+if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
 # ==== end ====
