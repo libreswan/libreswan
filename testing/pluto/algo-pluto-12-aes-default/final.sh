5630ecedc22e60283ac61c3383d3ea924fd77013
Paul Wouters <pwouters@redhat.com>
2014-07-16 16:23:55 -0400

    testing: remove ipsec whack --shutdown from some tests
    Those tests do not need it and makes some final.sh output
    volatile

--- testing/pluto/algo-pluto-12-aes-default/final.sh
+++ testing/pluto/algo-pluto-12-aes-default/final.sh
@@ -2,10 +2,6 @@ grep -A 1 KEY_LENGTH /tmp/pluto.log
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
-ipsec whack --shutdown
-: ==== cut ====
-ipsec look
-: ==== tuc ====
 if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
 : ==== end ====
