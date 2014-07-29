libreswan/libreswan

commit 3eda2666ddb763ee3d659e56ec862bfe93d16cef

* undo the close on whack_sock, as it is placed in the state.
Left a comment to avoid making this mistake again.

programs/pluto/ipsec_doi.c
@@ -464,7 +464,7 @@ ipsecdoi_replace(struct state *st
 #endif
 			 );
     }
-    close_any(whack_sock);
+    /* don't close whack_sock here as some caller above might have placed it in the state object */
 }
 
 /*

commit 3e6181938965c41197a863d2d8143b565a244e14

Simplify some code involved with closing whack socket. Fixes leak introduced in 3eda2666ddb763ee3d659e56ec862bfe93d16cef which in turn was a fix for a sometimes incorrect closing of a whack socket.

programs/pluto/ikev1_aggr.c
@@ -235,6 +235,7 @@ static stf_status aggr_inI1_outR1_common(struct msg_digest *md,
 		pb_stream pre_sa_pbs = sa_pd->pbs;
 		lset_t policy = preparse_isakmp_sa_body(&pre_sa_pbs) |
 				POLICY_AGGRESSIVE;
+
 		c = find_host_connection(&md->iface->ip_addr, pluto_port,
 					 (ip_address*)NULL, md->sender_port,
 					 policy);

programs/pluto/ipsec_doi.c
@@ -271,7 +271,9 @@ void ipsecdoi_initiate(int whack_sock,
 					 , uctx
 #endif
 					 );
-			return;
+		} else {
+			/* fizzle: whack_sock will be unused */
+			close_any(whack_sock);
 		}
 	} else if (HAS_IPSEC_POLICY(policy)) {
 
@@ -287,7 +289,6 @@ void ipsecdoi_initiate(int whack_sock,
 				    , uctx
 #endif
 				    );
-			return;
 		} else {
 			/* ??? we assume that peer_nexthop_sin isn't important:
 			 * we already have it from when we negotiated the ISAKMP SA!
@@ -299,12 +300,8 @@ void ipsecdoi_initiate(int whack_sock,
 					   , uctx
 #endif
 					   );
-			return;
 		}
 	}
-
-	/* fall through in the case of error */
-	close_any(whack_sock);
 }
 
 /* Replace SA with a fresh one that is similar
@@ -336,7 +333,7 @@ void ipsecdoi_replace(struct state *st,
 
 		initiator = pick_initiator(c, policy);
 		passert(!HAS_IPSEC_POLICY(policy));
-		if (initiator) {
+		if (initiator != NULL) {
 			(void) initiator(whack_sock, st->st_connection, st,
 					 policy,
 					 try, st->st_import
@@ -344,6 +341,9 @@ void ipsecdoi_replace(struct state *st,
 					 , st->sec_ctx
 #endif
 					 );
+		} else {
+			/* fizzle: whack_sock will be unused */
+			close_any(whack_sock);
 		}
 	} else {
 		/* Add features of actual old state to policy.  This ensures
@@ -380,7 +380,6 @@ void ipsecdoi_replace(struct state *st,
 #endif
 				  );
 	}
-	/* don't close whack_sock here as some caller above might have placed it in the state object */
 }
 
 /*