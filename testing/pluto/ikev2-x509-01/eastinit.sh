4b4bb80c198931f3730025673a07fee209a36e5c
Matt Rogers <mrogers@redhat.com>
2014-09-11 18:43:32 -0400

    testing: revive ikev2-x509-01
    update interop-ikev2-strongswan-19-x509-res-certreq description
    
    add tests to TESTLIST

--- testing/pluto/ikev2-x509-01/eastinit.sh
+++ testing/pluto/ikev2-x509-01/eastinit.sh
@@ -1,12 +1,5 @@
-: ==== start ====
-TESTNAME=ikev2-x509-01
-source /testing/pluto/bin/eastnlocal.sh
-
-iptables -A INPUT -i eth1 -s 192.0.3.0/24 -d 0.0.0.0/0 -j DROP
-
+/testing/guestbin/swan-prep --x509
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
-ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add ikev2-westnet-eastnet-x509-cr
 echo "initdone"
