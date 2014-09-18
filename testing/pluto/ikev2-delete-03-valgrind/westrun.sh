769b8a7be9bf31ca3e2423a9a255a2eabbff9304
Antony Antony <antony@phenome.org>
2014-09-18 18:26:23 +0300

    ikev2-delete-03-valgrind: fix the --up line

--- testing/pluto/ikev2-delete-03-valgrind/westrun.sh
+++ testing/pluto/ikev2-delete-03-valgrind/westrun.sh
@@ -1,4 +1,4 @@
-ipsec auto --add westnet-eastnet-ikev2
+ipsec auto --up westnet-eastnet-ikev2
 ping -n -c 2 -I 192.0.1.254 192.0.2.254
 ipsec auto --status
 echo "sleeping a bit.. 2"
@@ -8,6 +8,7 @@ echo "sleeping a bit.. 2"
 sleep 2
 ipsec auto --status
 echo "sleeping a bit.. 2"
+sleep 2
 ipsec whack --deletestate 1
 sleep 2
 ipsec auto --status
