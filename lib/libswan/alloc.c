libreswan/libreswan

commit 83f2e3da82f90b511f0812ed3d61fb671615f9f5

leak detective: when detecting, clear each chunk of memory that is freed

lib/libswan/alloc.c
@@ -145,6 +145,8 @@ void pfree(void *ptr)
 			}
 			pthread_mutex_unlock(&leak_detective_mutex);
 		}
+		/* stomp on memory!   Is another byte value better? */
+		memset(ptr, 0xEF, sizeof(union mhdr) + p->i.size);
 		p->i.magic = ~LEAK_MAGIC;
 		free(p);
 	} else {

commit 7161340cb0f6d1dd20e8fb8855c98964c82ac40a

pluto: fix pfree (broken in 83f2e3da82f90b511f0812ed3d61fb671615f9f5)

lib/libswan/alloc.c
@@ -146,7 +146,7 @@ void pfree(void *ptr)
 			pthread_mutex_unlock(&leak_detective_mutex);
 		}
 		/* stomp on memory!   Is another byte value better? */
-		memset(ptr, 0xEF, sizeof(union mhdr) + p->i.size);
+		memset(p, 0xEF, sizeof(union mhdr) + p->i.size);
 		p->i.magic = ~LEAK_MAGIC;
 		free(p);
 	} else {