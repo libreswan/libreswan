37637f774b3ba84eef43dcd5c14ce5d9648607e6
Paul Wouters <pwouters@redhat.com>
2014-09-25 17:25:40 -0400

    testing: IKEv1 and IKEv2 tests for isakmp-reserved-flags
    Tests ensure no ISAKMP reserved flags are copied from the received
    header into the response header

--- testing/pluto/ikev1-isakmp-reserved-flags-02/final.sh
+++ testing/pluto/ikev1-isakmp-reserved-flags-02/final.sh
@@ -1,5 +1,5 @@
 ipsec look
-# Should be 2 hits for both west (sending) and east (receiving)
+# Should be 4 hits (3 main mode, 1 quick mode)  for both west (sending) and east (receiving)
 grep ISAKMP_FLAG_MSG_RESERVED_BIT6 /tmp/pluto.log | wc -l
 : ==== cut ====
 ipsec auto --status
