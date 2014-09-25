3afe8421698d882faa067a7b9da96694c600934e
Matt Rogers <mrogers@redhat.com>
2014-09-25 00:05:35 -0400

    testing: add x509-chain-03

---
+++ testing/pluto/x509-chain-03/westinit.sh
@@ -0,0 +1,13 @@
+/testing/guestbin/swan-prep --x509 --certchain
+certutil -A -n "east_chain_intermediate_2" -d /etc/ipsec.d -t 'c,,' -a -i /testing/x509/cacerts/east_chain_intermediate_2.crt
+certutil -A -n "east_chain_intermediate_1" -d /etc/ipsec.d -t 'c,,' -a -i /testing/x509/cacerts/east_chain_intermediate_1.crt
+# confirm that the network is alive
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
+# make sure that clear text does not get through
+iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
+# confirm with a ping
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-x509-chain
+echo "initdone"
