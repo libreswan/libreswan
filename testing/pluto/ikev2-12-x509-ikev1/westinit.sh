c8a0d84c594605146c1b0a0ee497aef79403fbc8
Paul Wouters <pwouters@redhat.com>
2014-09-28 15:57:34 -0400

    testing: disagle efence banner

--- testing/pluto/ikev2-12-x509-ikev1/westinit.sh
+++ testing/pluto/ikev2-12-x509-ikev1/westinit.sh
@@ -6,6 +6,7 @@ ping -n -c 4 -I 192.0.1.254 192.0.2.254
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
 export PLUTO_EVENT_RETRANSMIT_DELAY=1
 export PLUTO_MAXIMUM_RETRANSMISSIONS_INITIAL=3
+export EF_DISABLE_BANNER=1
 ipsec pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
