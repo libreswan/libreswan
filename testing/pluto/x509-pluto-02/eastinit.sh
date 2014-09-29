482db6572a1977a3a17f83314cb256aae2bffec8
Paul Wouters <pwouters@redhat.com>
2014-09-29 18:22:42 -0400

    testing: updated x509-pluto-02 init.sh.  output needs updating

--- testing/pluto/x509-pluto-02/eastinit.sh
+++ testing/pluto/x509-pluto-02/eastinit.sh
@@ -3,6 +3,5 @@ certutil -D -n north -d /etc/ipsec.d
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add north-east-x509-pluto-02
-ipsec auto --status
-#ipsec auto --listall
+ipsec auto --status | grep north-east-x509-pluto-02
 echo "initdone"
