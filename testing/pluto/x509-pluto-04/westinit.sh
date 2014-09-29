82e0e85ff316e653addafa0b3fdc358a269ed3b3
Paul Wouters <pwouters@redhat.com>
2014-09-29 18:11:41 -0400

    testing: WIP: added x509-pluto-04 to test CA mismatch

--- testing/pluto/x509-pluto-04/westinit.sh
+++ testing/pluto/x509-pluto-04/westinit.sh
@@ -1,9 +1,10 @@
 /testing/guestbin/swan-prep --x509
-# remove west's cert so it must come via IKE
-certutil -D -n west -d /etc/ipsec.d
-certutil -D -n west-ec -d /etc/ipsec.d
+certutil -D -n east -d /etc/ipsec.d
+certutil -D -n east-ec -d /etc/ipsec.d
+ipsec import /testing/x509/pkcs12/otherca/signedbyotherca.p12 
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
+ipsec whack --debug-all --impair-retransmits
 ipsec auto --add westnet-eastnet-x509-cr
 ipsec auto --status | grep westnet-eastnet-x509-cr
 echo "initdone"
