3bd1909f56ccd22c3af470e0b31b71ebe29fe758
Paul Wouters <pwouters@redhat.com>
2014-10-17 10:25:23 -0400

    testing: updated x509-pluto-03

--- testing/pluto/x509-pluto-03/eastinit.sh
+++ testing/pluto/x509-pluto-03/eastinit.sh
@@ -1,15 +1,8 @@
-: ==== start ====
-TESTNAME=x509-pluto-03
-source /testing/pluto/bin/eastlocal.sh
-
-rm /tmp/$TESTNAME/ipsec.d/certs/west*
-rm /tmp/$TESTNAME/ipsec.d/crls/ca.crl
-
-iptables -A INPUT -i eth1 -s 192.0.3.0/24 -d 0.0.0.0/0 -j DROP
-
+/testing/guestbin/swan-prep --x509
+# remove west's cert so it must come via IKE
+certutil -D -n west -d /etc/ipsec.d
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add westnet-eastnet-x509-cr
-
-/testing/pluto/basic-pluto-01/eroutewait.sh trap
+ipsec auto --status | grep westnet-eastnet-x509-cr
+echo "initdone"
