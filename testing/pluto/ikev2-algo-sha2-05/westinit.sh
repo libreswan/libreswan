b87c6ebb3fc415dc798f131b0997d24efd1d877d
Paul Wouters <pwouters@redhat.com>
2014-10-19 21:10:18 -0400

    testing: add space for consistency

--- testing/pluto/ikev2-algo-sha2-05/westinit.sh
+++ testing/pluto/ikev2-algo-sha2-05/westinit.sh
@@ -3,5 +3,5 @@ ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --debug-all --impair-retransmits
 ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
-ipsec auto --status |grep westnet-eastnet-ipv4-psk-ikev2
+ipsec auto --status | grep westnet-eastnet-ipv4-psk-ikev2
 echo "initdone"
