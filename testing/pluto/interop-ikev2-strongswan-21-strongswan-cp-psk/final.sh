276a765f9ff5e443244946364603ec6aa50e0294
Antony Antony <antony@phenome.org>
2014-11-05 15:09:57 +0100

    testing: remove duplicate test interop-ikev2-strongswan-21-strongswan-cp-psk

--- testing/pluto/interop-ikev2-strongswan-21-strongswan-cp-psk/final.sh
+++
@@ -1,9 +0,0 @@
-if [ -f /var/run/pluto/pluto.pid ]; then ipsec look ; fi
-if [ -f /var/run/charon.pid ]; then strongswan statusall ; fi
-: ==== cut ====
-if [ -f /var/run/pluto/pluto.pid ]; then ipsec auto --status ; fi
-if [ -f /var/run/charon.pid ]; then strongswan statusall ; fi
-: ==== tuc ====
-if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
-if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
-: ==== end ====
