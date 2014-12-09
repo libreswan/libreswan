libreswan/libreswan

commit b892803c3ef1a0ed62526d25099d21f61a1b8a45

lib/libipsecconf: poke and prod
parser.l:

- Improve include processing and diagnostics.

- simplify lexical grammar

- correct signed/unsigned mixup in formats

- sprinkle "static"

- use bool where appropriate

- partialy tidy

parser.y:

- simplify grammar

- Get rid of a shift/reduce and a reduce/reduce conflict

- fix problem with EOF based on patch by lkundrak
  <https://github.com/lkundrak/libreswan/commit/9be0e2ab88b2573b742b24c404bc889f01c40a55>
  Handling of FIRST_SPACES EOL is still a bit kludgy

- detect overflow on numeric scaling

- properly bound snprintf output buffers

- sprinkle "static"

- partialy tidy

confread.c:

- slightly improve on message "starter: case KH_NOTSET: empty"

- partialy tidy

starterlog.c:

- simplify

lib/libipsecconf/confread.c
@@ -461,7 +461,8 @@ static bool validate_end(struct ub_ctx *dnsctx,
 		break;
 
 	case KH_NOTSET:
-		starter_log(LOG_LEVEL_DEBUG, "starter: case KH_NOTSET: empty");
+		ERR_FOUND("%s not specified", leftright);
+		starter_log(LOG_LEVEL_DEBUG, "starter: %s is KH_NOTSET", leftright);
 		break;
 	}
 
@@ -511,15 +512,14 @@ static bool validate_end(struct ub_ctx *dnsctx,
 				    !unbound_resolve(dnsctx, value,
 						strlen(value), AF_INET6,
 						&(end->nexthop)))
-					ERR_FOUND(
-						"bad value for %snexthop=%s\n",
+					ERR_FOUND("bad value for %snexthop=%s\n",
 						leftright, value);
 #else
 				er = ttoaddr(value, 0, family,
 						&(end->nexthop));
 				if (er != NULL)
-					ERR_FOUND(
-						"bad value for %snexthop=%s [%s]", leftright, value,
+					ERR_FOUND("bad value for %snexthop=%s [%s]",
+						leftright, value,
 						er);
 #endif
 			}
@@ -529,7 +529,6 @@ static bool validate_end(struct ub_ctx *dnsctx,
 #if 0
 		if (conn_st->policy & POLICY_OPPORTUNISTIC)
 			end->nexttype = KH_DEFAULTROUTE;
-
 #endif
 		anyaddr(family, &end->nexthop);
 
@@ -579,6 +578,7 @@ static bool validate_end(struct ub_ctx *dnsctx,
 	 */
 	if (end->strings_set[KSCF_SOURCEIP]) {
 		char *value = end->strings[KSCF_SOURCEIP];
+
 		if (tnatoaddr(value, strlen(value), AF_INET,
 			      &(end->sourceip)) != NULL &&
 		    tnatoaddr(value, strlen(value), AF_INET6,
@@ -597,13 +597,13 @@ static bool validate_end(struct ub_ctx *dnsctx,
 					  leftright, value);
 #else
 			er = ttoaddr(value, 0, family, &(end->sourceip));
-			if (er)
+			if (er != NULL)
 				ERR_FOUND("bad addr %ssourceip=%s [%s]",
 					  leftright, value, er);
 #endif
 		} else {
 			er = tnatoaddr(value, 0, family, &(end->sourceip));
-			if (er)
+			if (er != NULL)
 				ERR_FOUND("bad numerical addr %ssourceip=%s [%s]",
 					leftright, value, er);
 		}
@@ -612,10 +612,9 @@ static bool validate_end(struct ub_ctx *dnsctx,
 				    "%ssourceip= used but not %ssubnet= defined, defaulting %ssubnet to %s",
 				    leftright, leftright, leftright, value);
 			er = addrtosubnet(&end->sourceip, &end->subnet);
-			if (er) {
-				ERR_FOUND(
-					"attempt to default %ssubnet from %s failed: %s", leftright, value,
-					er);
+			if (er != NULL) {
+				ERR_FOUND("attempt to default %ssubnet from %s failed: %s",
+					leftright, value, er);
 			}
 			end->has_client = TRUE;
 			end->has_client_wildcard = FALSE;
@@ -639,7 +638,7 @@ static bool validate_end(struct ub_ctx *dnsctx,
 		ugh = ttoprotoport(value, 0, &end->protocol, &end->port,
 				   &end->has_port_wildcard);
 
-		if (ugh)
+		if (ugh != NULL)
 			ERR_FOUND("bad %sprotoport=%s [%s]", leftright, value,
 				  ugh);
 	}

lib/libipsecconf/parser.l
@@ -50,16 +50,11 @@ static int parser_y_eof(void);
 /* we want no actual output! */
 #define ECHO
 
-/* do not define this function, as it is not used, and fails -Werror */
-#if 0
-#define YY_NO_INPUT
-#endif
-
 struct ic_inputsource {
 	YY_BUFFER_STATE state;
 	FILE *file;
 	unsigned int line;
-	unsigned int once;	/* boolean */
+	bool once;
 	char *filename;
 	int fileglobcnt;
 	glob_t fileglob;
@@ -87,7 +82,7 @@ void parser_y_error(char *b, int size, const char *s)
 #if defined(SOMETHING_FOR_SOME_ARCH)
 	extern char *yytext;
 #endif
-	snprintf(b, size, "%s:%d: %s [%s]",
+	snprintf(b, size, "%s:%u: %s [%s]",
 		stacktop->filename == NULL ? "<none>" : stacktop->filename,
 		stacktop->line,
 		s, yytext);
@@ -97,7 +92,7 @@ void parser_y_init (const char *name, FILE *f)
 {
 	memset(&ic_private, 0, sizeof(ic_private));
 	ic_private.stack[0].line = 1;
-	ic_private.stack[0].once = 1;
+	ic_private.stack[0].once = TRUE;
 	ic_private.stack[0].file = f;
 	ic_private.stack[0].filename = strdup(name);
 	stacktop = &ic_private.stack[0];
@@ -161,7 +156,7 @@ static int parser_y_nextglobfile(struct ic_inputsource *iis)
 	}
 
 	iis->line = 1;
-	iis->once = 1;
+	iis->once = TRUE;
 	iis->filename = strdup(iis->fileglob.gl_pathv[fcnt]);
 
 	/* open the file */
@@ -191,14 +186,9 @@ int parser_y_include (const char *filename)
 
 	globbuf.gl_offs = 0;
 
-	try = filename;
-	newname[0] = '\0';
-	newname2[0] = '\0';
-
 	/*
-	 * order of operations is: try rootdir, and then rootdir2
-	 * only if it starts with /.
-	 * We never try plain name in that situation.
+	 * If the filename starts with /, we try to find it prefixed with
+	 * rootdir or rootdir2.
 	 *
 	 * glob() returns a match if that file exists, even without
 	 * having to do any globbing.
@@ -212,33 +202,59 @@ int parser_y_include (const char *filename)
 # define GB 0
 #endif
 
-	if (rootdir[0] != '\0' && filename[0] == '/') {
+	/*
+	 * If there is no rootdir, but there is a rootdir2, swap them.
+	 * This reduces the number of cases to be handled.
+	 */
+	if (rootdir[0] == '\0' && rootdir2[0] != '\0') {
+		strcpy(rootdir, rootdir2);
+		rootdir2[0] = '\0';
+	}
+
+	if (filename[0] != '/' || rootdir[0] == '\0') {
+		/* try plain name, with no rootdirs */
+		try = filename;
+		globresult = glob(try, GB, NULL, &globbuf);
+		if (globresult == GLOB_NOMATCH) {
+			/* no files found... */
+			starter_log(LOG_LEVEL_ERR,
+				"warning: could not open include filename: '%s'",
+				filename);
+			globfree(&globbuf);
+			return 0;
+		}
+	} else {
+		/* try prefixing with rootdir */
 		snprintf(newname, sizeof(newname), "%s%s", rootdir, filename);
 		try = newname;
 
 		globresult = glob(try, GB, NULL, &globbuf);
-		if (globresult == GLOB_NOMATCH && rootdir2[0] != '\0') {
-			/* now try with rootdir2 */
-			snprintf(newname2, sizeof(newname2),
-				 "%s%s", rootdir2, filename);
-			try = newname2;
-			globresult = glob(try, GB, NULL, &globbuf);
+		if (globresult == GLOB_NOMATCH) {
+			if (rootdir2[0] == '\0') {
+				starter_log(LOG_LEVEL_ERR,
+					"warning: could not open include filename '%s' (tried '%s')",
+					filename, newname);
+				globfree(&globbuf);
+				return 0;
+			} else {
+				/* try prefixing with rootdir2 */
+				snprintf(newname2, sizeof(newname2),
+					 "%s%s", rootdir2, filename);
+				try = newname2;
+				globresult = glob(try, GB, NULL, &globbuf);
+				if (globresult == GLOB_NOMATCH) {
+				starter_log(LOG_LEVEL_ERR,
+					"warning: could not open include filename: '%s' (tried '%s' and '%s')",
+					filename, newname, newname2);
+				globfree(&globbuf);
+				return 0;
+				}
+			}
 		}
-	} else {
-		/* try globbing plain name now.. */
-		globresult = glob(try, GB, NULL, &globbuf);
 	}
 
 #undef GB
 
-	if (globresult == GLOB_NOMATCH) {
-		/* no files found... */
-		starter_log(LOG_LEVEL_INFO,
-			"warning: could not open include filename: '%s' (tried %s and %s)",
-			filename, newname, newname2);
-		globfree(&globbuf);
-		return 0;
-	}
 
 	if (ic_private.stack_ptr >= MAX_INCLUDE_DEPTH) {
 		yyerror("max inclusion depth reached");
@@ -247,7 +263,7 @@ int parser_y_include (const char *filename)
 
 	if (lex_verbosity > 0) {
 		starter_log(LOG_LEVEL_DEBUG,
-			    "including file '%s'(%s) from line %s:%d\n"
+			    "including file '%s' ('%s') from %s:%u"
 			    , filename, try
 			    , stacktop->filename
 			    , stacktop->line);
@@ -264,7 +280,7 @@ int parser_y_include (const char *filename)
 	return parser_y_eof();
 }
 
-int parser_y_eof(void)
+static int parser_y_eof(void)
 {
 	if (stacktop->state != YY_CURRENT_BUFFER) {
 		yy_delete_buffer(YY_CURRENT_BUFFER);
@@ -275,12 +291,13 @@ int parser_y_eof(void)
 
 		if (lex_verbosity > 0) {
 			int stackp = ic_private.stack_ptr;
+
 			starter_log(LOG_LEVEL_DEBUG,
-				    "end of file %s\n", stacktop->filename);
+				    "end of file %s", stacktop->filename);
 
 			if (stackp > 0) {
 				starter_log(LOG_LEVEL_DEBUG,
-					"resuming %s line %d\n"
+					"resuming %s:%u"
 					, ic_private.stack[stackp-1].filename
 					, ic_private.stack[stackp-1].line);
 			}
@@ -305,10 +322,10 @@ int parser_y_eof(void)
 /* lexical states:
  *
  * INITIAL
- * USERDEF: after "="
- * BOOLEAN: after keyword with BOOLWORD attribute, until \n
- * COMMENTEQUAL: after keyword "x-comment"
- * COMMENTSTRING: after = in COMMENTEQUAL state, until \n
+ * USERDEF: after <INITIAL>"=" or <INITIAL>"conn"; returns to INITIAL after next token (integer or string)
+ * BOOLEAN: after keyword with BOOLWORD attribute; followed by '='; returns to INITIAL after next token (bool)
+ * COMMENTEQUAL: after keyword "x-comment"; followed by = and then switches to COMMENTSTRING
+ * COMMENTSTRING: after = in COMMENTEQUAL state; everything up to \n is a string; then returns to INITIAL
  */
 
 %x USERDEF BOOLEAN COMMENTEQUAL COMMENTSTRING
@@ -317,18 +334,23 @@ int parser_y_eof(void)
 
 <<EOF>>	{
 #if 0
-	printf("stacktop->filename = %s\n",
+	printf("EOF: stacktop->filename = %s\n",
 		stacktop->filename == NULL ? "NULL" : stacktop->filename);
 #endif
-	if (stacktop->file != NULL) {
-		char ch;
-		fseek(stacktop->file, -1L, SEEK_END);
-		ch = (char)fgetc(stacktop->file);
-		if (ch != '\n' && stacktop->once) {
-			stacktop->once = 0;
-			return EOL;
-		}
+	/*
+	 * Add a newline at the end of the file in case one was missing.
+	 * This code assumes that EOF is sticky:
+	 * that it can be detected repeatedly.
+	 */
+	if (stacktop->once) {
+		stacktop->once = FALSE;
+		return EOL;
 	}
+
+	/*
+	 * we've finished this file:
+	 * continue with the file it was included from (if any)
+	 */
 	if (parser_y_eof()) {
 		yyterminate();
 	}
@@ -346,9 +368,9 @@ int parser_y_eof(void)
 
 ^[\t ]+			return FIRST_SPACES;
 
-[\t ]+			/* ignore spaces in line */ ;
+<INITIAL,BOOLEAN>[\t ]+	/* ignore spaces in line */ ;
 
-<USERDEF>[0-9]+		{
+<INITIAL,USERDEF>[0-9]+	{
 				/* process a number */
 				yylval.num = strtoul(yytext, NULL, 10);
 				BEGIN INITIAL;
@@ -358,13 +380,6 @@ int parser_y_eof(void)
 <USERDEF>%forever	{
 				/* a number, really 0 */
 				yylval.num = 0;
-				BEGIN INITIAL;
-				return INTEGER;
-			}
-
-[0-9]+			{
-				/* process a number */
-				yylval.num = strtoul(yytext, NULL, 10);
 				BEGIN INITIAL;
 				return INTEGER;
 			}
@@ -409,6 +424,7 @@ int parser_y_eof(void)
 			}
 
 <USERDEF>\"[^\"\n]*\"	{
+				/* "string" */
 				char *s = yytext + 1;
 				int len = strlen(s);
 
@@ -424,6 +440,7 @@ int parser_y_eof(void)
 			}
 
 <USERDEF>\{[^\"\n]*\}	{
+				/* { string-without-quotes } */
 				char *s = yytext + 1;
 				int len = strlen(s);
 
@@ -439,12 +456,14 @@ int parser_y_eof(void)
 			}
 
 <USERDEF>[^\" \t\n]+	{
+				/* string-without-quotes-or-whitespace */
 				yylval.s = strdup(yytext);
 				BEGIN INITIAL;
 				return STRING;
 			}
 
 <USERDEF>[^\{} \t\n]+	{
+				/* string-without-braces-or-whitespace */
 				yylval.s = strdup(yytext);
 				BEGIN INITIAL;
 				return STRING;
@@ -456,10 +475,15 @@ int parser_y_eof(void)
 			}
 
 =			{ BEGIN USERDEF; return EQUAL; }
+
 version			return VERSION;
+
 config			return CONFIG;
+
 setup			return SETUP;
+
 conn			{ BEGIN USERDEF; return CONN; }
+
 include			return INCLUDE;
 
 [^\"= \t\n]+		{

lib/libipsecconf/parser.y
@@ -78,26 +78,22 @@ static struct starter_comments_list *_parser_comments;
  * Config file
  */
 
-config_file:
-        blanklines versionstmt blanklines sections
-        | blanklines sections
-        ;
+config_file: blanklines versionstmt sections ;
 
 /* check out the version number - this is optional (and we're phasing out its use) */
 /* we have configs shipped with version 2 (INTEGER) and with version 2.0 (STRING, now  NUMBER/float was removed */
 
 versionstmt: /* NULL */
-        | VERSION STRING EOL
-        | VERSION INTEGER EOL
+        | VERSION STRING EOL blanklines
+        | VERSION INTEGER EOL blanklines
 	;
 
 blanklines: /* NULL */
 	| blanklines EOL
-	| blanklines FIRST_SPACES EOL
 	;
 
 sections: /* NULL */
-	| sections section_or_include
+	| sections section_or_include blanklines
 	;
 
 section_or_include:
@@ -105,14 +101,14 @@ section_or_include:
 		_parser_kw = &(_parser_cfg->config_setup);
 		_parser_kw_last = NULL;
 		_parser_comments = &_parser_cfg->comments;
-		if(yydebug) fprintf(stderr, "\nconfig setup read\n");
+		if (yydebug)
+			fprintf(stderr, "\nconfig setup read\n");
 
 	} kw_sections
 	| CONN STRING EOL {
 		struct section_list *section;
 		section = (struct section_list *)malloc(sizeof(struct section_list));
-		if (section) {
-
+		if (section != NULL) {
 			section->name = $2;
 			section->kw = NULL;
 
@@ -126,10 +122,9 @@ section_or_include:
 			TAILQ_INIT(&section->comments);
 			_parser_comments = &section->comments;
 
-			if(yydebug) fprintf(stderr, "\nread conn %s\n", section->name);
-
-		}
-		else {
+			if(yydebug)
+				fprintf(stderr, "\nread conn %s\n", section->name);
+		} else {
 			_parser_kw = NULL;
 			_parser_kw_last = NULL;
 			yyerror("can't allocate memory in section_or_include/conn");
@@ -140,12 +135,12 @@ section_or_include:
 	}
 	;
 
-kw_sections:
-	kw_sections kw_section
-	| /* NULL */
+kw_sections: /* NULL */
+	| kw_sections kw_section
 	;
 
-kw_section: FIRST_SPACES statement_kw EOL ;
+kw_section: FIRST_SPACES statement_kw EOL
+	| FIRST_SPACES EOL;	/* kludge to ignore whitespace (without newline) at EOF */
 
 statement_kw:
 	KEYWORD EQUAL KEYWORD {
@@ -153,7 +148,7 @@ statement_kw:
 
 		assert(_parser_kw != NULL);
 		new = alloc_kwlist();
-		if (!new) {
+		if (new == NULL) {
 		    yyerror("can't allocate memory in statement_kw");
 		} else {
 		    struct keyword kw;
@@ -193,7 +188,7 @@ statement_kw:
 		    case kt_time:
 		    case kt_percent:
 			yyerror("keyword value is a keyword, but type not a string");
-			assert(!(kw.keydef->type == kt_bool));
+			assert(kw.keydef->type != kt_bool);
 			break;
 
            	    case kt_comment:
@@ -208,20 +203,20 @@ statement_kw:
 		    if (_parser_kw_last)
 			_parser_kw_last->next = new;
 		    _parser_kw_last = new;
-		    if (!*_parser_kw) *_parser_kw = new;
+		    if (*_parser_kw == NULL)
+			*_parser_kw = new;
 		}
 	}
 	| COMMENT EQUAL STRING {
 		struct starter_comments *new;
 
 		new = alloc_comment();
-		if (new) {
+		if (new == NULL) {
+		    yyerror("can't allocate memory in statement_kw");
+		} else {
 		    new->x_comment = strdup($1.string);
 		    new->commentvalue = strdup($3);
 	            TAILQ_INSERT_TAIL(_parser_comments, new, link);
-                }
-		else {
-		    yyerror("can't allocate memory in statement_kw");
 		}
 	}
 	| KEYWORD EQUAL STRING {
@@ -229,7 +224,7 @@ statement_kw:
 
 		assert(_parser_kw != NULL);
 		new = alloc_kwlist();
-		if (!new) {
+		if (new == NULL) {
 		    yyerror("can't allocate memory in statement_kw");
 		} else {
 		    struct keyword kw;
@@ -289,7 +284,9 @@ statement_kw:
 
 		assert(_parser_kw != NULL);
 		new = alloc_kwlist();
-		if (new) {
+		if (new == NULL) {
+		    yyerror("can't allocate memory in statement_kw");
+		} else {
 		    new->keyword = $1;
 		    new->number = $<num>3;  /* Should not be necessary! */
 		    new->next = NULL;
@@ -297,9 +294,6 @@ statement_kw:
 			_parser_kw_last->next = new;
 		    _parser_kw_last = new;
 		    if (!*_parser_kw) *_parser_kw = new;
-		}
-		else {
-		    yyerror("can't allocate memory in statement_kw");
 		}
 	}
 	| KEYWORD EQUAL INTEGER {
@@ -307,7 +301,9 @@ statement_kw:
 
 		assert(_parser_kw != NULL);
 		new = alloc_kwlist();
-		if (new) {
+		if (new == NULL) {
+		    yyerror("can't allocate memory in statement_kw");
+		} else {
 		    new->keyword = $1;
 		    new->number = $<num>3;  /* Should not be necessary! */
 		    new->next = NULL;
@@ -315,9 +311,6 @@ statement_kw:
 			_parser_kw_last->next = new;
 		    _parser_kw_last = new;
 		    if (!*_parser_kw) *_parser_kw = new;
-		}
-		else {
-		    yyerror("can't allocate memory in statement_kw");
 		}
 	}
 	| TIMEWORD EQUAL STRING {
@@ -336,42 +329,69 @@ statement_kw:
 		val = strtoul(str, &endptr, 10);
 
 		if(endptr == str) {
-                  snprintf(buf, 80, "bad duration value %s=%s", kw.keydef->keyname, str);
+                  snprintf(buf, sizeof(buf), "bad duration value %s=%s", kw.keydef->keyname, str);
                   yyerror(buf);
 		  fail = TRUE;
-
 		}
 
 		if(!fail)
                 {
-		  if(*endptr == '\0') { /* nothing */ }
-		  else if ((*endptr == 's') && (endptr[1] == '\0')) { }
-		  else if ((*endptr == 'm') && (endptr[1] == '\0')) { val *= secs_per_minute; }
-		  else if ((*endptr == 'h') && (endptr[1] == '\0')) { val *= secs_per_hour; }
-		  else if ((*endptr == 'd') && (endptr[1] == '\0')) { val *= secs_per_day; }
-		  else if ((*endptr == 'w') && (endptr[1] == '\0')) { val *= 7*secs_per_day; }
-		  else {
-                    snprintf(buf, 80, "bad duration multiplier '%c' on %s", *endptr, str);
-                    yyerror(buf);
-                    fail=TRUE;
-                  }
+			unsigned scale;
+
+			if (*endptr == '\0') {
+				/* seconds: no scaling */
+				scale = 1;
+			} else if (endptr[1] == '\0') {
+				/* single character suffix */
+				switch (*endptr) {
+				case 's': scale = 1; break;
+				case 'm': scale = secs_per_minute; break;
+				case 'h': scale = secs_per_hour; break;
+				case 'd': scale = secs_per_day; break;
+				case 'w': scale = 7*secs_per_day; break;
+				default:
+					snprintf(buf, sizeof(buf),
+						"bad duration multiplier '%c' on %s",
+						*endptr, str);
+					yyerror(buf);
+					fail=TRUE;
+				}
+			} else {
+				snprintf(buf, sizeof(buf),
+					"bad duration multiplier \"%s\" on %s",
+					endptr, str);
+				yyerror(buf);
+				fail=TRUE;
+			}
+
+			if (!fail) {
+				if (UINT_MAX / scale < val) {
+					snprintf(buf, sizeof(buf),
+						"overflow scaling %s",
+						str);
+					yyerror(buf);
+					fail=TRUE;
+				} else {
+					val *= scale;
+				}
+			}
                 }
 
 	        if(!fail)
                 {
 		  assert(_parser_kw != NULL);
 		  new = alloc_kwlist();
-		  if (new) {
+		  if (new == NULL) {
+		    yyerror("can't allocate memory in statement_kw");
+		  } else {
 		    new->keyword = $1;
 		    new->number = val;
 		    new->next = NULL;
 		    if (_parser_kw_last)
 			_parser_kw_last->next = new;
 		    _parser_kw_last = new;
-		    if (!*_parser_kw) *_parser_kw = new;
-		  }
-		  else {
-		    yyerror("can't allocate memory in statement_kw");
+		    if (*_parser_kw == NULL)
+			*_parser_kw = new;
 		  }
                 }
 	}
@@ -391,7 +411,7 @@ statement_kw:
 		val = strtoul(str, &endptr, 10);
 
 		if(endptr == str) {
-                  snprintf(buf, 80, "bad percent value %s=%s", kw.keydef->keyname, str);
+                  snprintf(buf, sizeof(buf), "bad percent value %s=%s", kw.keydef->keyname, str);
                   yyerror(buf);
 		  fail = TRUE;
 
@@ -401,7 +421,7 @@ statement_kw:
                 {
 		  if ((*endptr == '%') && (endptr[1] == '\0')) { }
 		  else {
-                    snprintf(buf, 80, "bad percentage multiplier '%c' on %s", *endptr, str);
+                    snprintf(buf, sizeof(buf), "bad percentage multiplier '%c' on %s", *endptr, str);
                     yyerror(buf);
                     fail=TRUE;
                   }
@@ -411,17 +431,17 @@ statement_kw:
                 {
 		  assert(_parser_kw != NULL);
 		  new = alloc_kwlist();
-		  if (new) {
+		  if (new == NULL) {
+		    yyerror("can't allocate memory in statement_kw");
+		  } else {
 		    new->keyword = $1;
 		    new->number = val;
 		    new->next = NULL;
 		    if (_parser_kw_last)
 			_parser_kw_last->next = new;
 		    _parser_kw_last = new;
-		    if (!*_parser_kw) *_parser_kw = new;
-		  }
-		  else {
-		    yyerror("can't allocate memory in statement_kw");
+		    if (*_parser_kw == NULL)
+			*_parser_kw = new;
 		  }
                 }
 	}
@@ -430,17 +450,17 @@ statement_kw:
 
 		assert(_parser_kw != NULL);
 		new = alloc_kwlist();
-		if (new) {
+		if (new != NULL) {
+		    yyerror("can't allocate memory in statement_kw");
+		} else {
 		    new->keyword = $1;
 		    new->number = $<num>3;  /* Should not be necessary! */
 		    new->next = NULL;
 		    if (_parser_kw_last)
 			_parser_kw_last->next = new;
 		    _parser_kw_last = new;
-		    if (!*_parser_kw) *_parser_kw = new;
-		}
-		else {
-		    yyerror("can't allocate memory in statement_kw");
+		    if (*_parser_kw == NULL)
+			*_parser_kw = new;
 		}
 	}
 	| KEYWORD EQUAL { /* this is meaningless, we ignore it */ }
@@ -454,7 +474,7 @@ void yyerror(const char *s)
 		parser_y_error(parser_errstring, ERRSTRING_LEN, s);
 }
 
-struct config_parsed *parser_load_conf (const char *file, err_t *perr)
+struct config_parsed *parser_load_conf(const char *file, err_t *perr)
 {
 	struct config_parsed *cfg=NULL;
 	int err = 0;
@@ -519,7 +539,7 @@ end:
 	return cfg;
 }
 
-static void parser_free_kwlist (struct kw_list *list)
+static void parser_free_kwlist(struct kw_list *list)
 {
 	while (list != NULL) {
 		struct kw_list *elt = list;
@@ -531,7 +551,7 @@ static void parser_free_kwlist (struct kw_list *list)
 	}
 }
 
-void parser_free_conf (struct config_parsed *cfg)
+void parser_free_conf(struct config_parsed *cfg)
 {
 	struct section_list *seci, *sec;
 	if (cfg) {
@@ -551,7 +571,7 @@ void parser_free_conf (struct config_parsed *cfg)
 	}
 }
 
-struct kw_list *alloc_kwlist(void)
+static struct kw_list *alloc_kwlist(void)
 {
 	struct kw_list *new;
 
@@ -560,7 +580,7 @@ struct kw_list *alloc_kwlist(void)
 	return new;
 }
 
-struct starter_comments *alloc_comment(void)
+static struct starter_comments *alloc_comment(void)
 {
 	struct starter_comments *new;
 

lib/libipsecconf/starterlog.c
@@ -38,13 +38,11 @@ static bool log_debugging = FALSE;
 static bool log_to_console = FALSE;
 static bool log_to_syslog = FALSE;
 
-static void do_print_info(int level, const char *buff)
+static void log_one_line(int level, const char *buff)
 {
-	if ((!log_debugging) && (level == LOG_LEVEL_DEBUG))
-		return;
-
 	if (log_to_console)
 		fprintf(stderr, "%s\n", buff);
+
 	if (log_to_syslog) {
 		if (level == LOG_LEVEL_ERR)
 			syslog(LOG_ERR, "%s\n", buff);
@@ -53,37 +51,11 @@ static void do_print_info(int level, const char *buff)
 	}
 }
 
-static void log_info_multiline(int level, const char *buff)
-{
-	char *copy, *b, *ptr, *end;
-
-	if (!buff)
-		return;
-
-	if ((!log_debugging) && (level == LOG_LEVEL_DEBUG))
-		return;
-
-	copy = strdup(buff);
-	if (!copy)
-		return;
-
-	end = copy + strlen(copy);
-	for (ptr = copy, b = copy; ptr <= end; ptr++) {
-		if (*ptr == '\n')
-			*ptr = '\0';
-		if (*ptr == '\0') {
-			if (b != end)
-				do_print_info(level, b);
-			b = ptr + 1;
-		}
-	}
-	free(copy);
-}
-
 void starter_log(int level, const char *fmt, ...)
 {
 	va_list args;
 	char buff[BUFF_SIZE];
+	char *b;
 
 	if (!log_debugging && level == LOG_LEVEL_DEBUG)
 		return;
@@ -91,7 +63,21 @@ void starter_log(int level, const char *fmt, ...)
 	va_start(args, fmt);
 	vsnprintf(buff, BUFF_SIZE - 1, fmt, args);
 	buff[BUFF_SIZE - 1] = '\0';
-	log_info_multiline(level, buff);
+
+	/* log each '\n'-terminated segment separately */
+	for (b = buff;;) {
+		char *p = strchr(b, '\n');
+
+		if (p == NULL)
+			break;
+		*p = '\0';
+		log_one_line(level, b);
+		b = p + 1;
+	}
+
+	/* log the '\0'- terminated segment */
+	log_one_line(level, b);
+
 	va_end(args);
 }
 

commit 8d5d396b3e867a2dc5f02650397a5a86c1b371c1

confread: don't print an error for a missing left or right - bug introduced in b892803c - also: improve debug logging for certain cases

lib/libipsecconf/confread.c
@@ -405,7 +405,7 @@ static bool validate_end(struct ub_ctx *dnsctx,
 
 	case KH_IFACE:
 		/* generally, this doesn't show up at this stage */
-		starter_log(LOG_LEVEL_DEBUG, "starter: case KH_IFACE empty");
+		starter_log(LOG_LEVEL_DEBUG, "starter: %s is KH_IFACE", leftright);
 		break;
 
 	case KH_IPADDR:
@@ -452,16 +452,15 @@ static bool validate_end(struct ub_ctx *dnsctx,
 	case KH_IPHOSTNAME:
 		/* generally, this doesn't show up at this stage */
 		starter_log(LOG_LEVEL_DEBUG,
-			    "starter: case KH_IPHOSTNAME empty");
+			    "starter: %s is KH_IPHOSTNAME", leftright);
 		break;
 
 	case KH_DEFAULTROUTE:
 		starter_log(LOG_LEVEL_DEBUG,
-			    "starter: case KH_DEFAULTROUTE: empty");
+			    "starter: %s is KH_DEFAULTROUTE", leftright);
 		break;
 
 	case KH_NOTSET:
-		ERR_FOUND("%s not specified", leftright);
 		starter_log(LOG_LEVEL_DEBUG, "starter: %s is KH_NOTSET", leftright);
 		break;
 	}