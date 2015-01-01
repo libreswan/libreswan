acb5c96501993c7de5338e44100c312c167bf3b5
D. Hugh Redelmeier <hugh@mimosa.com>
2015-01-01 00:40:58 -0500

    testing: fix runit.sh scripts in readwriteconf-26 and readwriteconf-27

--- testing/scripts/readwriteconf-26/runit.sh
+++ testing/scripts/readwriteconf-26/runit.sh
@@ -6,8 +6,8 @@
 #
 
 exe=${OBJDIRTOP}/programs/readwriteconf/readwriteconf
-conf="--config ${ROOTDIR}/testing/scripts/readwriteconf-25/ipv6.conf"
-args="--rootdir=${ROOTDIR}/testing/scripts/readwriteconf-25 $conf "
+conf="--config ${ROOTDIR}/testing/scripts/readwriteconf-26/noeol.conf"
+args="--rootdir=${ROOTDIR}/testing/scripts/readwriteconf-26 $conf "
 #args="$args --verbose --verbose --verbose --verbose"
 echo "file $exe" >.gdbinit
 echo "set args $args " >>.gdbinit
