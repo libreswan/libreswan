369b18570395fefe3d70c69e2f63ff35c0594e76
Andrew Cagney <andrew.cagney@gmail.com>
2015-01-01 18:19:43 -0500

    pluto: AES_CTR and test vector code
    (where Paul forgot to call 'git add' on the new files)
    
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

---
+++ programs/pluto/cbc_test_vectors.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2014 Andrew Cagney <andrew.cagney@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+int test_camellia_cbc(const struct encrypt_desc *encrypt_desc);
+int test_aes_cbc(const struct encrypt_desc *encrypt_desc);
---
+++ programs/pluto/test_buffer.c
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2014 Andrew Cagney <andrew.cagney@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include <stdlib.h>
+
+#include "defs.h"
+#include "constants.h"
+#include "lswalloc.h"
+#include "lswlog.h"
+
+#include "nss.h"
+#include "pk11pub.h"
+
+#include "test_buffer.h"
+
+static chunk_t zalloc_chunk(size_t length, const char *name)
+{
+	chunk_t chunk;
+	chunk.len = length;
+	chunk.ptr = alloc_bytes(length, name);
+	memset(chunk.ptr, 0, chunk.len);
+	return chunk;
+}
+
+/*
+ * Given a hex encode string, decode it into a chunk.
+ *
+ * If this function fails, crash and burn.  Its been fed static data
+ * so should never ever have a problem.
+ */
+static chunk_t decode_hex_to_chunk(const char *original, const char *string)
+{
+	/* The decoded buffer can't be bigger than the encoded string.  */
+	chunk_t chunk = zalloc_chunk(strlen(string), original);
+	chunk.len = 0;
+	const char *pos = string;
+	while (*pos != '\0') {
+		/* skip leading/trailing space */
+		while (*pos == ' ') {
+			pos++;
+		}
+		if (*pos == '\0') {
+			break;
+		}
+		/* Expecting <HEX><HEX>, at least *pos is valid.  */
+		char buf[3];
+		int i = 0;
+		do {
+			buf[i++] = *pos++;
+		} while (*pos != ' ' && *pos != '\0' && i < 2);
+		buf[i] = '\0';
+		if (i != 2) {
+			loglog(RC_INTERNALERR,
+			       "decode_hex_to_chunk: hex buffer \"%s\" contains unexpected space or NUL at \"%s\"\n", string, pos);
+			exit_pluto(1);
+		}
+		char *end;
+		chunk.ptr[chunk.len] = strtoul(buf, &end, 16);
+		if (end - buf != 2) {
+			loglog(RC_INTERNALERR,
+			       "decode_hex_to_chunk: hex buffer \"%s\" invalid hex character at \"%s\"\n", string, pos);
+			exit_pluto(1);
+		}
+		chunk.len++;
+	}
+	return chunk;
+}
+
+/*
+ * Given an ASCII string, convert it onto a buffer of bytes.  If the
+ * buffer is prefixed by 0x assume the contents are hex (with spaces)
+ * and decode it; otherwise it is assumed that the ascii (minus the
+ * NUL) should be copied.
+ */
+chunk_t decode_to_chunk(const char *prefix, const char *original)
+{
+	DBG(DBG_CRYPT, DBG_log("decode_to_chunk: %s: input \"%s\"",
+			       prefix, original));
+	chunk_t chunk;
+	if (startswith(original, "0x")) {
+		chunk = decode_hex_to_chunk(original, original + strlen("0x"));
+	} else {
+		chunk = zalloc_chunk(strlen(original), original);
+		memcpy(chunk.ptr, original, chunk.len);
+	}
+	DBG(DBG_CRYPT, DBG_dump_chunk("decode_to_chunk: output: ", chunk));
+	return chunk;
+}
+
+int compare_chunk(const char *prefix,
+		  chunk_t expected,
+		  u_char *actual)
+{
+	size_t i;
+	for (i = 0; i < expected.len; i++) {
+		u_char l = expected.ptr[i];
+		u_char r = actual[i];
+		if (l != r) {
+			/* Caller should issue the real log message.  */
+			DBG(DBG_CRYPT, DBG_log("compare_chunk: %s: bytes at %zd differ, expected %02x found %02x",
+					       prefix, i, l, r));
+			return 0;
+		}
+	}
+	DBG(DBG_CRYPT, DBG_log("compare_chunk: %s: ok", prefix));
+	return 1;
+}
+
+int compare_chunks(const char *prefix,
+		   chunk_t expected,
+		   chunk_t actual)
+{
+	if (expected.len != actual.len) {
+		DBG(DBG_CRYPT,
+		    DBG_log("compare_chunks: %s: expected length %zd but got %zd",
+			    prefix, expected.len, actual.len));
+		return 0;
+	}
+	return compare_chunk(prefix, expected, actual.ptr);
+}
+
+/*
+ * Turn the raw key into a SECItem and then SymKey.
+ *
+ * Since slots are referenced counted and ImportSymKey adds a
+ * reference, immediate freeing of the local slot is possible.
+ *
+ * ImportSymKey makes a copy of the key chunk so that can also be
+ * released.
+ */
+PK11SymKey *decode_to_key(CK_MECHANISM_TYPE cipher_mechanism,
+			  const char *encoded_key)
+{
+	PK11SlotInfo *slot = PK11_GetBestSlot(cipher_mechanism, NULL);
+	chunk_t raw_key = decode_to_chunk("Key: ", encoded_key);
+	SECItem key_item;
+	key_item.type = siBuffer;
+	key_item.data = raw_key.ptr; /* ptr to an array of key bytes */
+	key_item.len = raw_key.len; /* length of the array of key bytes */
+	PK11SymKey *sym_key = PK11_ImportSymKey(slot, cipher_mechanism,
+						PK11_OriginUnwrap,
+						CKA_ENCRYPT, &key_item, NULL);
+	freeanychunk(raw_key);
+	PK11_FreeSlot(slot);
+	return sym_key;
+}
