libreswan/libreswan

commit 451d8c5c141798db3b6a6d4939d471770581cea4

IKEv2: send_v2_notification() used hardcoded ISAKMP_v2_SA_INIT exchange
If an ID is mismatched, and we find out during IKE_AUTH, we sent
an error back using the wrong exchange type. The value is now part
of a switch, which still needs to be extended, but should handle
the error in IKE_AUTH now.

programs/pluto/ikev2_parent.c
@@ -2993,7 +2993,18 @@ void send_v2_notification(struct state *p1st,
 		if (rcookie != NULL) /* some responses are with zero rSPI */
 			memcpy(hdr.isa_rcookie, rcookie, COOKIE_SIZE);
 		memcpy(hdr.isa_icookie, icookie, COOKIE_SIZE);
-		hdr.isa_xchg = ISAKMP_v2_SA_INIT;
+
+		/* incomplete */
+		switch (p1st->st_state) {
+		case STATE_PARENT_R1:
+			hdr.isa_xchg = ISAKMP_v2_AUTH;
+			break;
+		default:
+			/* default to old behaviour of hardcoding ISAKMP_v2_SA_INIT */
+			hdr.isa_xchg = ISAKMP_v2_SA_INIT;
+			break;
+		}
+
 		hdr.isa_np = ISAKMP_NEXT_v2N;
 		/* XXX unconditionally clearing original initiator flag is wrong */
 		hdr.isa_flags &= ~ISAKMP_FLAGS_v2_IKE_I;

commit 33fb6ce39a8ebfdd76800fad5b8943bb66b00525

IKEv2: fixup exchange type (broke in commit 451d8c5)
IKE_AUTH should be used for STATE_PARENT_R2 not STATE_PARENT_R1

programs/pluto/ikev2_parent.c
@@ -2996,7 +2996,7 @@ void send_v2_notification(struct state *p1st,
 
 		/* incomplete */
 		switch (p1st->st_state) {
-		case STATE_PARENT_R1:
+		case STATE_PARENT_R2:
 			hdr.isa_xchg = ISAKMP_v2_AUTH;
 			break;
 		default: