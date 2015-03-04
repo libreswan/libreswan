94db45b443a26511434b84a5c6236d9d5d6e4a09
Tuomo Soini <tis@foobar.fi>
2015-03-04 20:37:46 +0200

    testing: use consistent shellscript coding style

--- testing/scripts/setup.sh
+++ testing/scripts/setup.sh
@@ -1,31 +1,26 @@
 
-if [ -z "$LIBRESWANSRCDIR" ]
-then
-	if [ -f ../../kvmsetup.sh ]
-	then
-	  LIBRESWANSRCDIR=`cd ../.. && pwd`
-	else 
-	  if [ -f ../../../kvmsetup.sh ]
-	  then 
-	    LIBRESWANSRCDIR=`cd ../../.. && pwd`
-	  fi
-        fi  	
+if [ -z "${LIBRESWANSRCDIR}" ]; then
+    if [ -f ../../kvmsetup.sh ]; then
+	LIBRESWANSRCDIR=$(cd ../.. && pwd)
+    else
+	if [ -f ../../../kvmsetup.sh ]; then
+	    LIBRESWANSRCDIR=$(cd ../../.. && pwd)
+	fi
+    fi  	
 fi
 
-if [ ! -f $LIBRESWANSRCDIR/kvmsetup.sh ]
-then
-	echo kvmsetup not found at LIBRESWANSRCDIR=$LIBRESWANSRCDIR.
-	echo Is LIBRESWANSRCDIR set correctly'?'
-	exit 5
+if [ ! -f ${LIBRESWANSRCDIR}/kvmsetup.sh ]; then
+    echo "kvmsetup not found at LIBRESWANSRCDIR=${LIBRESWANSRCDIR}."
+    echo "Is LIBRESWANSRCDIR set correctly?"
+    exit 5
 fi
 
-LIBRESWANSRCDIR=`cd ${LIBRESWANSRCDIR}; pwd`
+LIBRESWANSRCDIR=$(cd ${LIBRESWANSRCDIR}; pwd)
 export LIBRESWANSRCDIR
 
-FIXUPDIR=`cd ${LIBRESWANSRCDIR}/testing/scripts/fixups && pwd`
-FIXUPDIR2=`cd ${LIBRESWANSRCDIR}/testing/sanitizers && pwd`
+FIXUPDIR=$(cd ${LIBRESWANSRCDIR}/testing/scripts/fixups && pwd)
+FIXUPDIR2=$(cd ${LIBRESWANSRCDIR}/testing/sanitizers && pwd)
 TESTINGROOT=${LIBRESWANSRCDIR}/testing
-UTILS=`cd ${TESTINGROOT}/utils && pwd`
+UTILS=$(cd ${TESTINGROOT}/utils && pwd)
 
 TESTSUBDIR=scripts
-
