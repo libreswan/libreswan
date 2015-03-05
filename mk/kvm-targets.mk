9d4aaea6499854c534231dadc8bacf80793edbb6
Andrew Cagney <cagney@gnu.org>
2015-03-05 14:31:45 -0500

    building: add more debugging, clean up variable names
    Better handles case where there isn't a host (use KVM_EASTDIR)
    and the target may or may not be a pattern rule.

--- mk/kvm-targets.mk
+++ mk/kvm-targets.mk
@@ -20,50 +20,60 @@
 # XXX: For compatibility
 abs_top_srcdir ?= $(abspath ${LIBRESWANSRCDIR})
 
-KVM_RUN ?= $(abs_top_srcdir)/testing/utils/runkvm.py
-KVM_MOUNTS ?= $(abs_top_srcdir)/testing/utils/kvmmounts.sh
+KVM_RUN_COMMAND ?= $(abs_top_srcdir)/testing/utils/runkvm.py
+KVM_MOUNTS_COMMAND ?= $(abs_top_srcdir)/testing/utils/kvmmounts.sh
+KVM_SWANTEST_COMMAND ?= $(abs_top_srcdir)/testing/utils/swantest
 
 KVM_HOSTS = east west road north
 
 # NOTE: Use this from make rules only.  Determines the KVM path to
 # $(abs_top_srcdir).  If broken, override using Makefile.inc.local
 KVM_HOST = $(lastword $(subst -, ,$@))
-KVM_MOUNT ?= $(shell $(KVM_MOUNTS) $(KVM_HOST) swansource)
-KVM_SUBMOUNT ?= $(subst $(KVM_MOUNT),,$(abs_top_srcdir))
-KVM_SOURCEDIR ?= /source/$(patsubst /%,%,$(KVM_SUBMOUNT))
+KVM_BUILD_MOUNT ?= $(shell $(KVM_MOUNTS_COMMAND) $(1) swansource)
+KVM_BUILD_SUBDIR ?= $(subst $(call KVM_BUILD_MOUNT,$(1)),,$(abs_top_srcdir))
+KVM_TARGET_SOURCEDIR ?= $(patsubst %/,%,/source/$(patsubst /%,%,$(call KVM_BUILD_SUBDIR,$(1))))
+KVM_SOURCEDIR ?= $(call KVM_TARGET_SOURCEDIR,$(KVM_HOST))
+KVM_EASTDIR ?= $(call KVM_TARGET_SOURCEDIR,east)
 
-KVM_TEST_TARGETS = $(patsubst %,kvm-test-%,$(KVM_HOSTS))
-.PHONY: $(KVM_TEST_TARGETS)
-$(KVM_TEST_TARGETS):
-	: host: $(KVM_HOST)
-	: mount for $(KVM_HOST): $(KVM_MOUNT)
-	: submount for $(KVM_HOST): $(KVM_SUBMOUNT)
-	: sourcedir for $(KVM_HOST): $(KVM_SOURCEDIR)
-	: hosts: $(KVM_HOSTS)
-	: targets: $(KVM_TEST_TARGETS)
+KVM_PRINT_TARGETS = $(patsubst %,kvm-print-%,$(KVM_HOSTS))
+.PHONY: $(KVM_PRINT_TARGETS)
+$(KVM_PRINT_TARGETS): kvm-print
+	: $@:
+	:   KVM_HOST: $(KVM_HOST)
+	:   KVM_BUILD_MOUNT: $(call KVM_BUILD_MOUNT,$(KVM_HOST))
+	:   KVM_BUILD_SUBDIR: $(call KVM_BUILD_SUBDIR,$(KVM_HOST))
+	:   KVM_TARGET_SOURCEDIR: $(call KVM_TARGET_SOURCEDIR,$(KVM_HOST))
+	:   KVM_SOURCEDIR: $(KVM_SOURCEDIR)
+.PHONY: kvm-print
+kvm-print:
+	: $@:
+	:   PWD: $(PWD)
+	:   KVM_HOSTS: $(KVM_HOSTS)
+	:   KVM_PRINT_TARGETS: $(KVM_PRINT_TARGETS)
+	:   KVM_EASTDIR: $(KVM_EASTDIR)
 
 KVM_RUN_TARGETS = $(patsubst %,kvm-run-%,$(KVM_HOSTS))
 .PHONY: $(KVM_RUN_TARGETS)
 $(KVM_RUN_TARGETS):
 	test '$(RUN)' != ""
-	$(KVM_RUN) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --run '$(RUN)'
+	$(KVM_RUN_COMMAND) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --run '$(RUN)'
 
 KVM_REBOOT_TARGETS = $(patsubst %,kvm-reboot-%,$(KVM_HOSTS))
 .PHONY: $(KVM_REBOOT_TARGETS)
 $(KVM_REBOOT_TARGETS):
-	$(KVM_RUN) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --reboot
+	$(KVM_RUN_COMMAND) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --reboot
 
 KVM_COMPILE_TARGETS = $(patsubst %,kvm-compile-%,$(KVM_HOSTS))
 #.PHONY: $(KVM_COMPILE_TARGETS)
 #$(KVM_COMPILE_TARGETS):
-kvm-compile-%: kvm-reboot-%
-	$(KVM_RUN) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --compile
+kvm-compile-%: kvm-print-% kvm-reboot-%
+	$(KVM_RUN_COMMAND) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --compile
 
 KVM_INSTALL_TARGETS = $(patsubst %,kvm-install-%,$(KVM_HOSTS))
 #.PHONY: $(KVM_INSTALL_TARGETS)
 #$(KVM_INSTALL_TARGETS):
-kvm-install-%: kvm-reboot-%
-	$(KVM_RUN) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --install
+kvm-install-%: kvm-print-% kvm-reboot-%
+	$(KVM_RUN_COMMAND) --sourcedir $(KVM_SOURCEDIR) --hostname $(KVM_HOST) --install
 
 .PHONY: kvm-update
 kvm-update: kvm-compile-east | $(KVM_INSTALL_TARGETS)
