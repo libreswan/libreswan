129cc50644e9b00fbd790d16b18fe05b9398f1e9
Andrew Cagney <cagney@gnu.org>
2015-03-11 11:16:55 -0400

    building: enable -Werror
    Override with: WERROR_CFLAGS= or WARN_CFLAGS=.
    Note that WERROR is going the way of the dodo.

--- mk/config.mk
+++ mk/config.mk
@@ -227,13 +227,15 @@ GCC_LINT?=-DGCC_LINT
 # some defaults that cause most if not all warnings
 # eventually: -Wshadow -pedantic
 ifeq ($(origin WERROR_CFLAGS),undefined)
-#WERROR_CFLAGS ?= -Werror
+WERROR_CFLAGS = -Werror
 endif
 ifeq ($(origin WARNING_CFLAG),undefined)
-WARNING_CFLAGS ?= -Wall -Wextra -Wformat -Wformat-nonliteral -Wformat-security -Wundef -Wmissing-declarations -Wredundant-decls -Wnested-externs
+WARNING_CFLAGS = -Wall -Wextra -Wformat -Wformat-nonliteral -Wformat-security -Wundef -Wmissing-declarations -Wredundant-decls -Wnested-externs
 endif
-# WERROR may go away
-WERROR ?= $(WARNING_CFLAGS) $(WERROR_CFLAGS)
+ifeq ($(origin WERROR),undefined)
+# WERROR will go away
+WERROR = $(WARNING_CFLAGS) $(WERROR_CFLAGS)
+endif
 
 ### misc configuration, included here in hopes that other files will not
 ### have to be changed for common customizations.
