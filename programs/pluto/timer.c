cd1a26feb1e68995428ad72f3c238047d7a36e40
Antony Antony <antony@phenome.org>
2015-04-22 03:26:50 -0500

    fix typo. the previous commit brome make.

--- programs/pluto/timer.c
+++ programs/pluto/timer.c
@@ -142,7 +142,7 @@ static void retransmit_v1_msg(struct state *st)
 		delay_ms = c->r_interval;
 	}
 
-	if (delay_ms != 0) {
+	if (delay_ms != 0)
 		delay_ms =  retrans_delay(st, delay_ms);
 
 	if (delay_ms != 0) {
