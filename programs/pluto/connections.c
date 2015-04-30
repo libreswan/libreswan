f01e3de46b353c2bbe6219c679d3dd6927321ebb
Herbert Xu <herbert@gondor.apana.org.au>
2015-05-01 05:59:22 +0800

    pluto: Fix bogus "no RSA public key known for '%fromcert'"
    When refine_host_connection tests against a %fromcert RW connection
    followed by other right=%any connections with fixed IDs (e.g.,
    @hostname), it will lose the fromcert setting.  So when it does
    eventually return with the %fromcert RW connection fromcert will
    be set to false and therefore the actual certificate ID won't be
    copied into spd.that.id, resulting in a bogus "no RSA public key
    known for '%fromcert'".
    
    This error won't happen if the order of matching is reversed and
    the %fromcert connection gets tested last.  So that's why the
    conencton sometimes works but often fails with an authentication
    error.
    
    This patch fixes it by keeping the fromcert setting of the best
    match.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Matt Rogers <mrogers@redhat.com>

--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -2618,6 +2618,7 @@ struct connection *refine_host_connection(const struct state *st,
 	d = c->host_pair->connections;
 	for (wcpip = FALSE;; wcpip = TRUE) {
 		for (; d != NULL; d = d->hp_next) {
+			bool d_fromcert = FALSE;
 			bool match1 = match_id(peer_id, &d->spd.that.id,
 					&wildcards);
 			bool match2 = trusted_ca_nss(peer_ca, d->spd.that.ca,
@@ -2656,9 +2657,10 @@ struct connection *refine_host_connection(const struct state *st,
 			 * the %fromcert + peer id match result. - matt
 			 */
 			if (!match1) {
-			    *fromcert = id_kind(&d->spd.that.id) == ID_FROMCERT;
-			    if (!*fromcert)
-				continue;
+				d_fromcert = id_kind(&d->spd.that.id) ==
+					     ID_FROMCERT;
+				if (!d_fromcert)
+					continue;
 			}
 
 			/* if initiator, our ID must match exactly */
@@ -2761,8 +2763,10 @@ struct connection *refine_host_connection(const struct state *st,
 			 * We'll go with it if the Peer ID was an exact match.
 			 */
 			if (match1 && wildcards == 0 &&
-			    peer_pathlen == 0 && our_pathlen == 0)
+			    peer_pathlen == 0 && our_pathlen == 0) {
+				*fromcert = d_fromcert;
 				return d;
+			}
 
 			/*
 			 * If it was a non-exact (wildcard) match, we'll
@@ -2783,6 +2787,7 @@ struct connection *refine_host_connection(const struct state *st,
 						d->name,
 						wildcards, peer_pathlen,
 						our_pathlen));
+				*fromcert = d_fromcert;
 				best_found = d;
 				best_wildcards = wildcards;
 				best_peer_pathlen = peer_pathlen;
