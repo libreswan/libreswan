60715aadcbb20232d43a539b984ac60cb574d1bf
Andrew Cagney <cagney@gnu.org>
2015-05-07 16:20:05 -0400

    building: fix ../../mk/find.sh | xargs grep ...

--- mk/find.sh
+++ mk/find.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -e
 
 # Find anything remotely related to makefiles; used to find makefile
-# variable references.
+# variable references.  Assume this is being run as .../mk/find.sh.
 
-cd $(dirname $(dirname $0))
-
-find * \
+find $(dirname $(dirname $0))/* \
      -name '*~' -prune \
      -o \
-     -type f -path 'packaging/*' -print \
+     -type f -path '*/packaging/*' -print \
      -o \
      -type f -name 'Makefile*' -print \
      -o \
