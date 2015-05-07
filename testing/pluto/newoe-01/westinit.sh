62ebd9b3246bc59237c6798b975d00bab34f0881
Paul Wouters <pwouters@redhat.com>
2015-05-07 15:39:23 -0400

    testing: updated newoe-01
    Still noticing that we have two IPsec SA's between west and east ?

--- testing/pluto/newoe-01/westinit.sh
+++ testing/pluto/newoe-01/westinit.sh
@@ -1,7 +1,6 @@
 /testing/guestbin/swan-prep
 named -c /etc/bind/named.conf
 cp policies/* /etc/ipsec.d/policies/
-echo "192.1.2.45/32" >> /etc/ipsec.d/policies/clear
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 #ipsec whack --myid @west.testing.libreswan.org
