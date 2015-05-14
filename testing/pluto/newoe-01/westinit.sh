6ac4450003d8e2e226165c16cdf5b9992bf48106
Paul Wouters <pwouters@redhat.com>
2015-05-14 17:20:42 -0400

    testing: newoe-01 update
    - wait for OE groups to load/instantiate
    - no need for named in this test
    - now shows proper foodgroup is used for whack oppo command

--- testing/pluto/newoe-01/westinit.sh
+++ testing/pluto/newoe-01/westinit.sh
@@ -1,8 +1,7 @@
 /testing/guestbin/swan-prep
-named -c /etc/bind/named.conf
 cp policies/* /etc/ipsec.d/policies/
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-#ipsec whack --myid @west.testing.libreswan.org
-ipsec auto --status
+# give OE policies time to load
+sleep 5
 echo "initdone"
