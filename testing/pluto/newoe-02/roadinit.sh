3d819f7a981ae2804760be89d46c6f80eb7a9ffc
Paul Wouters <pwouters@redhat.com>
2015-05-14 19:05:33 -0400

    testing: newoe-02 cleanup and docs why this test is still failing
    see description.txt (reference consoles deleted because it is wrong)

--- testing/pluto/newoe-02/roadinit.sh
+++ testing/pluto/newoe-02/roadinit.sh
@@ -1,5 +1,6 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
+ping -n -c 4 -I 192.1.3.209 7.7.7.7
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 # give OE policies time to load
