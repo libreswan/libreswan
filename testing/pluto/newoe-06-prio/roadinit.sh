0cf1b49f8010915ffe6173ecabef309e879aea19
Paul Wouters <pwouters@redhat.com>
2015-06-02 23:07:06 -0400

    testing: some updates to the newoe-* tests with for updated sanitizers

--- testing/pluto/newoe-06-prio/roadinit.sh
+++ testing/pluto/newoe-06-prio/roadinit.sh
@@ -1,5 +1,7 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
+echo "192.1.2.0/24" >> /etc/ipsec.d/policies/private-or-clear
+ip -s xfrm monitor > /tmp/xfrm-monitor.out &
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 # give OE policies time to load
--- testing/pluto/newoe-06-prio/roadrun.sh
+++ testing/pluto/newoe-06-prio/roadrun.sh
@@ -1,4 +1,6 @@
 ipsec auto --route road-east-ikev2
 # hopefully trigger road-east-ikev2 - not the OE authnull conn
 ping -n -c 4 -I 192.1.3.209 192.1.2.23
+killall ip > /dev/null 2> /dev/null
+cp /tmp/xfrm-monitor.out OUTPUT/road.xfrm-monitor.txt
 echo done
