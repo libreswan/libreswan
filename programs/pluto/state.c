libreswan/libreswan

commit 4052d3b5112dbd854685fd1ac69ba092a95aa4cb

pluto: mark enum event_type allocations so leak detective reports event type

programs/pluto/state.c
@@ -1560,8 +1560,7 @@ void fmt_state(struct state *st, const monotime_t n,
 		 st->st_remoteport,
 		 enum_name(&state_names, st->st_state),
 		 enum_name(&state_stories, st->st_state),
-		 st->st_event ? enum_name(&timer_event_names,
-					  st->st_event->ev_type) : "none",
+		 enum_name(&timer_event_names, st->st_event->ev_type),
 		 delta,
 		 np1, np2, eo, dpdbuf,
 		 idlestr,

programs/pluto/timer.c
@@ -845,8 +845,8 @@ static unsigned long envnumber(const char *name,
  */
 static void event_schedule_tv(enum event_type type, const struct timeval delay, struct state *st)
 {
-	struct pluto_event *ev = alloc_thing(struct pluto_event,
-				"struct pluto_event in event_schedule()");
+	const char *en = enum_name(&timer_event_names, type);
+	struct pluto_event *ev = alloc_thing(struct pluto_event, en);
 
 	DBG(DBG_LIFECYCLE, DBG_log("event_schedule_tv called for about %lu seconds and change",
 		delay.tv_sec));
@@ -910,12 +910,12 @@ static void event_schedule_tv(enum event_type type, const struct timeval delay, 
 	DBG(DBG_CONTROL, {
 			if (st == NULL) {
 				DBG_log("inserting event %s, timeout in %lu.%06lu seconds",
-					enum_show(&timer_event_names, type),
+					en,
 					(unsigned long)delay.tv_sec,
 					(unsigned long)delay.tv_usec);
 			} else {
 				DBG_log("inserting event %s, timeout in %lu.%06lu seconds for #%lu",
-					enum_show(&timer_event_names, type),
+					en,
 					(unsigned long)delay.tv_sec,
 					(unsigned long)delay.tv_usec,
 					ev->ev_state->st_serialno);

commit 227c079228adc54999223d5fe3568bce7123063f

pluto: fix bug introduced in 4052d3b5112dbd854685fd1ac69ba092a95aa4cb

programs/pluto/state.c
@@ -1560,7 +1560,8 @@ void fmt_state(struct state *st, const monotime_t n,
 		 st->st_remoteport,
 		 enum_name(&state_names, st->st_state),
 		 enum_name(&state_stories, st->st_state),
-		 enum_name(&timer_event_names, st->st_event->ev_type),
+		 st->st_event == NULL ? "none" :
+			enum_name(&timer_event_names, st->st_event->ev_type),
 		 delta,
 		 np1, np2, eo, dpdbuf,
 		 idlestr,