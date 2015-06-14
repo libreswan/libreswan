896957f821355ddbfab834b6cbd4d1944c81035c
Paul Wouters <pwouters@redhat.com>
2015-06-14 15:32:07 -0400

    testing: updated most kev2-liveness-0* tests
    But not ikev2-liveness-07 bcause I think that still shows a problem

--- testing/pluto/ikev2-liveness-02/westrun.sh
+++ testing/pluto/ikev2-liveness-02/westrun.sh
@@ -2,8 +2,11 @@ ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
 ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec look
 # sleep for 60s to run a few liveness cycles 
-sleep 60
-kill -9 `cat /run/pluto/pluto.pid`
+sleep 30
+sleep 30
+ipsec whack --debug-all --impair-send-no-delete
+ipsec auto --delete westnet-eastnet-ipv4-psk-ikev2
+ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
 # sleep for timeout action
 sleep 30
 echo done
