7ae55637bd172bec9970350840ea8d299bb04d59
Paul Wouters <pwouters@redhat.com>
2015-06-16 10:22:21 -0400

    testing: udpated ikev2-frag-02-ipv6 that shows the "out of room" issue

--- testing/pluto/ikev2-frag-02-ipv6/westinit.sh
+++ testing/pluto/ikev2-frag-02-ipv6/westinit.sh
@@ -1,11 +1,4 @@
 /testing/guestbin/swan-prep --46 --x509 --x509name key4096
-# confirm that the network is alive
-ping6 -n -c 4 -I 2001:db8:1:2::45 2001:db8:1:2::23
-# make sure that clear text does not get through
-ip6tables -A INPUT -i eth1 -s 2001:db8:1:2::23 -p ipv6-icmp --icmpv6-type echo-reply -j DROP
-ip6tables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
-# confirm with a ping
-ping6 -n -c 4 -I 2001:db8:1:2::45 2001:db8:1:2::23
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add v6-tunnel
