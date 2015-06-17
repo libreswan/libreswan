aee342c9a15abb9debac3279abf6f60529455813
Antony Antony <antony@phenome.org>
2015-06-17 03:17:55 -0500

    testing: ikev2 ipsec sa delte for netkey

--- testing/pluto/newoe-10-expire-inactive/roadrun.sh
+++ testing/pluto/newoe-10-expire-inactive/roadrun.sh
@@ -3,8 +3,14 @@ ping -n -c 4 -I 192.1.3.209 192.1.2.23
 sleep 5
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
+# wait for IPSEC SA to expire due to inactivity.
 sleep 60
 sleep 60
+# should be empty
+ipsec whack --trafficstatus
+ipsec look
+ping -n -c 4 -I 192.1.3.209 192.1.2.23
+#establish a new one
 ipsec whack --trafficstatus
 ipsec look
 echo done
