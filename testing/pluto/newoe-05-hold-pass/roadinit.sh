1c7eb0d6a883760f26c9bcc15dad70501d62a2b5
Paul Wouters <pwouters@redhat.com>
2015-06-16 22:42:56 -0400

    testing: add missing newoe-05-hold-pass and newoe-01-whack consoles

---
+++ testing/pluto/newoe-05-hold-pass/roadinit.sh
@@ -0,0 +1,11 @@
+/testing/guestbin/swan-prep
+cp policies/* /etc/ipsec.d/policies/
+ping -n -c 4 -I 192.1.3.209 7.7.7.7
+ipsec setup start
+# ensure for tests acquires expire before our failureshunt=2m
+echo 30 > /proc/sys/net/core/xfrm_acq_expires
+/testing/pluto/bin/wait-until-pluto-started
+# give OE policies time to load
+sleep 5
+ip -s xfrm monitor > /tmp/xfrm-monitor.out &
+echo "initdone"
