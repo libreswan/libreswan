350bfa1766e11cf2df701a70978d188328000e98
Paul Wouters <pwouters@redhat.com>
2015-06-23 13:49:10 -0300

    testing: minoew newoe updates

--- testing/pluto/newoe-04-01/roadinit.sh
+++
@@ -1,9 +0,0 @@
-/testing/guestbin/swan-prep
-named -c /etc/bind/named.conf
-cp policies/* /etc/ipsec.d/policies/
-#echo "192.1.3.209/32" >> /etc/ipsec.d/policies/clear
-ping -n -c 2 -I 192.1.3.209 7.7.7.7
-ipsec setup start
-/testing/pluto/bin/wait-until-pluto-started
-ipsec auto --status
-echo "initdone"
