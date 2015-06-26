1bc1f1336c8f1944fa23daf78f6764d1b2e21e88
Antony Antony <antony@phenome.org>
2015-06-26 15:52:51 +0200

    testing: newoe-10-expire-inactive when IPSEC SA is inactive expire parent and the child.

--- testing/pluto/newoe-10-expire-inactive/roadrun.sh
+++ testing/pluto/newoe-10-expire-inactive/roadrun.sh
@@ -1,16 +1,15 @@
 ping -n -c 4 -I 192.1.3.209 192.1.2.23
-# wait on OE retransmits and rekeying
 sleep 5
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
 # wait for IPSEC SA to expire due to inactivity.
 sleep 60
 sleep 60
-# should be empty
+# expired #1  and #2. trafficstatus  should be empty
 ipsec whack --trafficstatus
 ipsec look
-ping -n -c 4 -I 192.1.3.209 192.1.2.23
 #establish a new one
+ping -n -c 4 -I 192.1.3.209 192.1.2.23
 ipsec whack --trafficstatus
 ipsec look
 echo done
