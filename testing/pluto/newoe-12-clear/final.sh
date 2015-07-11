4d6c67b087137d823958ba8d20de14e1db6d15eb
Antony Antony <antony@phenome.org>
2015-07-11 20:50:18 +0200

    testing: newoe-12-clear, newoe-13-block

---
+++ testing/pluto/newoe-12-clear/final.sh
@@ -0,0 +1,5 @@
+ipsec look
+ipsec auto --status
+if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
