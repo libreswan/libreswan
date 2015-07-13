5d09f34f676f5c9089154e70d4b9fe5cdb998419
Paul Wouters <pwouters@redhat.com>
2015-07-13 11:49:17 -0400

    testing: changed newoe-12-clear to use private-or-clear on east
    This better matches what we look for in the test. The test case
    now also does not use symmetrical policies/ but proper ones based
    on a base policies/* dir and specific entries added in the init.sh
    
    (I did not change the use of the global ikev2-oe.conf but I do think
     it is better to have per-test ikev2-oe.conf files)

--- testing/pluto/newoe-12-clear/eastinit.sh
+++ testing/pluto/newoe-12-clear/eastinit.sh
@@ -1,5 +1,6 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
+echo "192.1.3.209/32" >> /etc/ipsec.d/policies/clear-or-private
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 # give OE policies time to load
--- testing/pluto/newoe-12-clear/roadinit.sh
+++ testing/pluto/newoe-12-clear/roadinit.sh
@@ -1,5 +1,6 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
+echo "192.1.2.23/32" >> /etc/ipsec.d/policies/clear
 ipsec setup start
 # ensure for tests acquires expire before our failureshunt=2m
 echo 30 > /proc/sys/net/core/xfrm_acq_expires
