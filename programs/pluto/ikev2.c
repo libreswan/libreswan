libreswan/libreswan

commit 2b87cd33dd45e7987a248f1ac60b8486dbe8f489

ikev2: cleanup comments. add serial # log message. '#1 complete v2 state transition with'

programs/pluto/ikev2.c
@@ -1688,7 +1688,8 @@ void complete_v2_state_transition(struct msg_digest **mdp,
 		/* FALL THROUGH */
 	case STF_IGNORE:
 		DBG(DBG_CONTROL,
-		    DBG_log("complete v2 state transition with %s",
+		    DBG_log("#%lu complete v2 state transition with %s",
+			    cur_state->st_serialno,
 			    enum_show(&stfstatus_name, result)));
 		return;
 
@@ -1727,8 +1728,8 @@ void complete_v2_state_transition(struct msg_digest **mdp,
 	 */
 
 	DBG(DBG_CONTROL,
-	    DBG_log("complete v2 state transition from %s with %s",
-		from_state_name,
+	    DBG_log("#%lu complete v2 state transition from %s with %s",
+		st->st_serialno, from_state_name,
 		result > STF_FAIL ?
 		    enum_name(&ikev2_notify_names, result - STF_FAIL) :
 		    enum_name(&stfstatus_name, result)));

programs/pluto/ikev2_parent.c
@@ -3698,10 +3698,10 @@ stf_status ikev2_child_inIoutR(struct msg_digest *md)
 
 	RETURN_STF_FAILURE(accept_v2_nonce(md, &st->st_ni, "Ni"));
 
-	/* calculate new nonce and the KE */
+	/* calculate new nonce. a new KE is not supported yet */
 	{
 		struct pluto_crypto_req_cont *qke = new_pcrc(
-			ikev2_child_inIoutR_continue, "IKEv2 CHILD KE AND NONCE",
+			ikev2_child_inIoutR_continue, "IKEv2 CHILD NONCE",
 			st, md);
 		stf_status e;
 		enum crypto_importance ci;
@@ -3709,10 +3709,6 @@ stf_status ikev2_child_inIoutR(struct msg_digest *md)
 		ci = pcim_ongoing_crypto;
 		if (ci < st->st_import)
 			ci = st->st_import;
-		/*
-		 * ??? I'm not sure of the logic of this bit.
-		 * For one thing, no KE, despite mentions above.
-		 */
 
 		if (!st->st_sec_in_use) {
 			DBG(DBG_CONTROLMORE, DBG_log("Generate new nonce for CREATE_CHILD_SA exchange."));

commit f823738b474aeefaa24b715439014014b40afe0b

pluto: remove crashing code from complete_v2_state_transition Introduced in 2b87cd33dd45e7987a248f1ac60b8486dbe8f489. Simply checking for a null pointer won't save this. I have a core where md->st == 0xedededededededed (STF_INLINE).

programs/pluto/ikev2.c
@@ -1688,8 +1688,7 @@ void complete_v2_state_transition(struct msg_digest **mdp,
 		/* FALL THROUGH */
 	case STF_IGNORE:
 		DBG(DBG_CONTROL,
-		    DBG_log("#%lu complete v2 state transition with %s",
-			    cur_state->st_serialno,
+		    DBG_log("complete v2 state transition with %s",
 			    enum_show(&stfstatus_name, result)));
 		return;
 