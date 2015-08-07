ec3375984c932523b96cc4d1bf143c650620d0f5
Paul Wouters <pwouters@redhat.com>
2015-08-07 07:24:48 -0400

    testing: remove an unneeded policygroup  entry in newoe-08-restart

--- testing/pluto/newoe-08-restart/eastinit.sh
+++ testing/pluto/newoe-08-restart/eastinit.sh
@@ -1,6 +1,5 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
-echo 192.0.2.0/24 >> /etc/ipsec.d/policies/clear-or-private
 echo 192.1.3.0/24 >> /etc/ipsec.d/policies/clear-or-private
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
--- testing/pluto/newoe-08-restart/roadinit.sh
+++ testing/pluto/newoe-08-restart/roadinit.sh
@@ -1,6 +1,5 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
-echo 192.0.2.0/24 >> /etc/ipsec.d/policies/private-or-clear
 echo 192.1.3.0/24 >> /etc/ipsec.d/policies/private-or-clear
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
