libreswan/libreswan

commit 6cce0301a12390a7211df35d98114cbe8f956918

* pluto/rsasigkey: Allow specifying seedbits for NSS RNG
* Added --seedbits <numbits> option to pluto and rsasigkey
* Added seedbits= to ipsec.conf "config setup" section

There is a BSI requirement that we seed at least 440bits of random
from /dev/random into the NSS via PK11_RandomUpdate() before we
are allowed to pull random from PK11_GenerateRandom() despite the
fact that NSS already deals on it own with initialising its PRNG.

Since this can seriously stall startup on low-entropy machines,
we do not inflict this upon everyone.

rsasigkey already fed 480 bits into NSS, so I turned this into
an option with 480 as default. pluto's default is to not do this.

A big warning was added to the code using /dev/random in plutomain.c
to ensure people don't change this in the future and to ensure the
function to read from /dev/random is never exported to other parts of
the code.

programs/pluto/plutomain.c
@@ -111,6 +111,7 @@ static bool fork_desired = TRUE;
 /* pulled from main for show_setup_plutomain() */
 static const struct lsw_conf_options *oco;
 static char *coredir;
+static int pluto_nss_seedbits;
 static int nhelpers = -1;
 
 libreswan_passert_fail_t libreswan_passert_fail = passert_fail;
@@ -314,6 +315,49 @@ static void set_cfg_string(char **target, char *value)
 	*target = strdup(value);
 }
 
+/*
+ * This function MUST NOT be used for anything else!
+ * It is used to seed the NSS PRNG based on --seedbits pluto argument
+ * or the seedbits= * config setup option in ipsec.conf.
+ * Everything else that needs random MUST use get_rnd_bytes()
+ * This function MUST NOT be changed to use /dev/urandom.
+ */
+static void get_bsi_random(size_t nbytes, unsigned char *buf)
+{
+	size_t ndone;
+	int dev;
+	ssize_t got;
+	const char *device = "/dev/random";
+
+	dev = open(device, 0);
+	if (dev < 0) {
+		loglog(RC_LOG_SERIOUS, "could not open %s (%s)\n",
+			device, strerror(errno));
+		exit_pluto(PLUTO_EXIT_NSS_FAIL);
+	}
+
+	ndone = 0;
+		DBG(DBG_CONTROL,DBG_log("need %d bits random for extra seeding of the NSS PRNG",
+			(int) nbytes * BITS_PER_BYTE));
+
+	while (ndone < nbytes) {
+		got = read(dev, buf + ndone, nbytes - ndone);
+		if (got < 0) {
+			loglog(RC_LOG_SERIOUS,"read error on %s (%s)\n",
+				device, strerror(errno));
+			exit_pluto(PLUTO_EXIT_NSS_FAIL);
+		}
+		if (got == 0) {
+			loglog(RC_LOG_SERIOUS,"EOF on %s!?!\n",  device);
+			exit_pluto(PLUTO_EXIT_NSS_FAIL);
+		}
+		ndone += got;
+	}
+	close(dev);
+	DBG(DBG_CONTROL,DBG_log("read %ld bytes from /dev/random for NSS PRNG",
+		nbytes));
+}
+
 static void pluto_init_nss(char *confddir)
 {
 	SECStatus nss_init_status;
@@ -327,6 +371,23 @@ static void pluto_init_nss(char *confddir)
 	} else {
 		libreswan_log("NSS Initialized");
 		PK11_SetPasswordFunc(getNSSPassword);
+	}
+
+	/*
+	 * This exists purely to make the BSI happy.
+	 * We do not infliect this on other users
+	 */
+	if (pluto_nss_seedbits != 0) {
+		SECStatus rv;
+		int seedbytes = BYTES_FOR_BITS(pluto_nss_seedbits);
+		unsigned char *buf = alloc_bytes(seedbytes,"TLA seedmix");
+
+		get_bsi_random(seedbytes, buf); /* much TLA, very blocking */
+		rv = PK11_RandomUpdate(buf, seedbytes);
+		libreswan_log("seeded %d bytes into the NSS PRNG", seedbytes);
+		passert(rv == SECSuccess);
+		zero(&buf);
+		pfree(buf);
 	}
 }
 
@@ -428,6 +489,7 @@ static const struct option long_opts[] = {
 	{ "virtual_private\0_", required_argument, NULL, '6' },	/* _ */
 	{ "virtual-private\0<network_list>", required_argument, NULL, '6' },
 	{ "nhelpers\0<number>", required_argument, NULL, 'j' },
+	{ "seedbits\0<number>", required_argument, NULL, 'c' },
 #ifdef HAVE_LABELED_IPSEC
 	/* ??? really an attribute type, not a value */
 	{ "secctx_attr_value\0_", required_argument, NULL, 'w' },	/* obsolete name; _ */
@@ -700,6 +762,14 @@ int main(int argc, char **argv)
 					break;
 
 				nhelpers = u;
+			}
+			continue;
+		case 'c':	/* --seedbits */
+			pluto_nss_seedbits = atoi(optarg);
+			if (pluto_nss_seedbits == 0) {
+				printf("pluto: seedbits must be an integer > 0");
+				/* not exit_pluto because we are not initialized yet */
+				exit(PLUTO_EXIT_NSS_FAIL);
 			}
 			continue;
 
@@ -979,6 +1049,7 @@ int main(int argc, char **argv)
 				}
 			}
 
+			pluto_nss_seedbits = cfg->setup.options[KBF_SEEDBITS];
 			pluto_nat_port =
 				cfg->setup.options[KBF_NATIKEPORT];
 			keep_alive = cfg->setup.options[KBF_KEEPALIVE];
@@ -1483,3 +1554,4 @@ void show_setup_plutomain()
 	whack_log(RC_COMMENT, "secctx-attr-type=<unsupported>");
 #endif
 }
+

programs/rsasigkey/rsasigkey.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2003-2008 Michael C Richardson <mcr@xelerance.com>
  * Copyright (C) 2003-2009 Paul Wouters <paul@xelerance.com>
  * Copyright (C) 2009 Avesh Agarwal <avagarwa@redhat.com>
- * Copyright (C) 2012-2013 Paul Wouters <paul@libreswan.org>
+ * Copyright (C) 2012-2015 Paul Wouters <paul@libreswan.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -64,26 +64,22 @@
 #define MIN_KEYBIT 2192
 
 #ifndef DEVICE
-/* To the openwrt people: Do not change /dev/random to /dev/urandom. The
- * /dev/random device is ONLY used for generating long term keys, which
- * should NEVER be done with /dev/urandom. If people use X.509, PSK or
- * even raw RSA keys generated on other systems, changing this will have
- * 0 effect. It's better to fail or bail out of generating a key, then
- * generate a bad one.
- */
 # define DEVICE  "/dev/random"
 #endif
 #ifndef MAXBITS
 # define MAXBITS 20000
 #endif
 
+#define DEFAULT_SEED_BITS 60 /* 480 bits of random seed */
+
 #define E       3               /* standard public exponent */
 /* #define F4	65537 */	/* possible future public exponent, Fermat's 4th number */
 
 #define NSSDIR "/etc/ipsec.d"
 
+char *progname;
 char usage[] =
-	"rsasigkey [--verbose] [--random <device>] [--configdir <dir>] [--password <password>] [--hostname host] [<nbits>]";
+	"rsasigkey [--verbose] [--random <device>] [--configdir <dir>] [--password <password>] [--hostname host] [--seedbits bits] [<keybits>]";
 struct option opts[] = {
 	{ "rounds",    1,      NULL,   'p', }, /* obsoleted */
 	{ "noopt",     0,      NULL,   'n', }, /* obsoleted */
@@ -96,6 +92,7 @@ struct option opts[] = {
 	{ "configdir",        1,      NULL,   'c' },
 	{ "configdir2",        1,      NULL,   'd' }, /* nss tools use -d */
 	{ "password", 1,      NULL,   'P' },
+	{ "seedbits", 1,      NULL,   's' },
 	{ 0,           0,      NULL,   0, }
 };
 int verbose = 0;                /* narrate the action? */
@@ -106,14 +103,13 @@ char outputhostname[NS_MAXDNAME];  /* hostname for output */
 char me[] = "ipsec rsasigkey";  /* for messages */
 
 /* forwards */
-void rsasigkey(int nbits, char *configdir, char *password);
+void rsasigkey(int nbits, int seedbits, char *configdir, char *password);
 void getrandom(size_t nbytes, unsigned char *buf);
 static const unsigned char *bundle(int e, mpz_t n, size_t *sizep);
 static const char *conv(const unsigned char *bits, size_t nbytes, int format);
 static const char *hexout(mpz_t var);
 void report(char *msg);
 
-#define RAND_BUF_SIZE 60
 
 /* getModulus - returns modulus of the RSA public key */
 static SECItem *getModulus(SECKEYPublicKey *pk)
@@ -163,15 +159,17 @@ static const char *hexOut(SECItem *data)
 }
 
 /* UpdateRNG - Updates NSS's PRNG with user generated entropy. */
-static void UpdateNSS_RNG(void)
+static void UpdateNSS_RNG(int seedbits)
 {
 	SECStatus rv;
-	unsigned char buf[RAND_BUF_SIZE];
+	int seedbytes = BYTES_FOR_BITS(seedbits);
+	unsigned char *buf = alloc_bytes(seedbytes,"TLA seedmix");
 
-	getrandom(RAND_BUF_SIZE, buf);
-	rv = PK11_RandomUpdate(buf, sizeof buf);
+	getrandom(seedbytes, buf);
+	rv = PK11_RandomUpdate(buf, seedbytes);
 	assert(rv == SECSuccess);
 	zero(&buf);
+	pfree(buf);
 }
 
 /*  Returns the password passed in in the text file.
@@ -303,6 +301,7 @@ int main(int argc, char *argv[])
 {
 	int opt;
 	int nbits = 0;
+	int seedbits = DEFAULT_SEED_BITS;
 	char *configdir = NULL; /* where the NSS databases reside */
 	char *password = NULL;  /* password for token authentication */
 
@@ -343,6 +342,16 @@ int main(int argc, char *argv[])
 			break;
 		case 'P':       /* token authentication password */
 			password = optarg;
+			break;
+		case 's': /* seed bits */
+			seedbits = atoi(optarg);
+			if (PK11_IsFIPS()) {
+				if (seedbits < DEFAULT_SEED_BITS) {
+					fprintf(stderr, "%s: FIPS mode does not allow < %d seed bits\n",
+						me, DEFAULT_SEED_BITS);
+					exit(1);
+				}
+			}
 			break;
 		case '?':
 		default:
@@ -393,7 +402,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	rsasigkey(nbits, configdir, password);
+	rsasigkey(nbits, seedbits, configdir, password);
 	exit(0);
 }
 
@@ -405,7 +414,7 @@ int main(int argc, char *argv[])
  * real speed advantages.
  * See also: https://www.imperialviolet.org/2012/03/16/rsae.html
  */
-void rsasigkey(int nbits, char *configdir, char *password)
+void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
 {
 	SECStatus rv;
 	PK11RSAGenParams rsaparams = { nbits, (long) E };
@@ -483,7 +492,7 @@ void rsasigkey(int nbits, char *configdir, char *password)
 #endif /* 0 */
 
 	/* Do some random-number initialization. */
-	UpdateNSS_RNG();
+	UpdateNSS_RNG(seedbits);
 	/* Log in to the token */
 	if (password) {
 		rv = PK11_Authenticate(slot, PR_FALSE, &pwdata);
@@ -553,9 +562,7 @@ void rsasigkey(int nbits, char *configdir, char *password)
 /*
    - getrandom - get some random bytes from /dev/random (or wherever)
  */
-void getrandom(nbytes, buf)
-size_t nbytes;
-unsigned char *buf;                     /* known to be big enough */
+void getrandom(size_t nbytes, unsigned char *buf)
 {
 	size_t ndone;
 	int dev;
@@ -570,8 +577,8 @@ unsigned char *buf;                     /* known to be big enough */
 
 	ndone = 0;
 	if (verbose) {
-		fprintf(stderr, "getting %d random bytes from %s...\n",
-			(int) nbytes,
+		fprintf(stderr, "getting %d random seed bytes for NSS from %s...\n",
+			(int) nbytes * BITS_PER_BYTE,
 			device);
 	}
 	while (ndone < nbytes) {
@@ -683,3 +690,12 @@ char *msg;
 
 	fprintf(stderr, "%s\n", msg);
 }
+/* exit_tool() is needed if the library was compiled with DEBUG, even if we are not.
+ * The odd-looking parens are to prevent macro expansion:
+ * lswlog.h without DEBUG define a macro exit_tool().
+ */
+void (exit_tool)(int x)
+{
+        exit(x);
+}
+

include/ipsecconf/keywords.h
@@ -83,6 +83,7 @@ enum keyword_numeric_config_field {
 	KBF_STRICTCRLPOLICY,
 	KBF_SEND_CA,
 	KBF_NATIKEPORT,
+	KBF_SEEDBITS,
 	KBF_KEEPALIVE,
 	KBF_PLUTORESTARTONCRASH,
 	KBF_CRLCHECKINTERVAL,

lib/libipsecconf/confread.c
@@ -99,6 +99,10 @@ void ipsecconf_default_values(struct starter_config *cfg)
 
 	cfg->setup.options[KBF_KEEPALIVE] = 0;                  /* config setup */
 	cfg->setup.options[KBF_NATIKEPORT] = NAT_IKE_UDP_PORT;
+
+	/* Don't inflict BSI requirements on everyone */
+	cfg->setup.options[KBF_SEEDBITS] = 0;
+
 #ifdef HAVE_LABELED_IPSEC
 	cfg->setup.options[KBF_SECCTX] = SECCTX;
 #endif

lib/libipsecconf/keywords.c
@@ -370,6 +370,7 @@ const struct keyword_def ipsec_conf_keywords_v2[] = {
 	{ "virtual-private", kv_config, kt_string,     KSF_VIRTUALPRIVATE, NOT_ENUM },
 	{ "nat_ikeport",   kv_config | kv_alias, kt_number,      KBF_NATIKEPORT, NOT_ENUM },	/* obsolete _ */
 	{ "nat-ikeport",   kv_config, kt_number,      KBF_NATIKEPORT, NOT_ENUM },
+	{ "seedbits",   kv_config, kt_number,      KBF_SEEDBITS, NOT_ENUM },
 	{ "keep_alive", kv_config | kv_alias, kt_number,    KBF_KEEPALIVE, NOT_ENUM },	/* obsolete _ */
 	{ "keep-alive", kv_config, kt_number,    KBF_KEEPALIVE, NOT_ENUM },
 	{ "nat_traversal", kv_config, kt_obsolete_quiet, KBF_WARNIGNORE, NOT_ENUM },

programs/rsasigkey/Makefile
@@ -16,7 +16,7 @@ LIBRESWANSRCDIR?=$(shell cd ../..; pwd)
 include ${LIBRESWANSRCDIR}/Makefile.inc
 
 PROGRAM=rsasigkey
-LIBS=${LIBRESWANLIB} -lgmp
+LIBS=${LSWLOGLIB} ${LIBRESWANLIB} -lgmp
 LIBS+=${NSSLIBS}
 
 ifeq ($(USE_FIPSCHECK),true)

commit 23f1bfe040999206149a2d1f59fb8510ada1d9e7

pluto, rsasigkey: fix two messed up calls to messup
- cannot use messup() (or zero()) with a pointer to first element of an array

- fix: introduce and use messupn;
  add comments to zero() to make its limitations clearer.

- Paul noticed one instance of the bug and rendered it non-fatal in
  4218390f66a7a87a7888c067e86cdd80d276caf8 (not actually a fix, but
  no problem will be observable).

- The bug was introduced in 6cce0301a12390a7211df35d98114cbe8f956918
  when an auto buffer array was replaced by a heap-allocated buffer
  represented by a pointer to the first element.
  Neither zero() nor messup() can deal with this new representation.

include/constants.h
@@ -124,8 +124,17 @@ typedef int bool;
 /*
  * zero an object given a pointer to it.
  *
- * Note: this won't work on an array without an explicit &
- * (it will appear to work but it will only zero the first element).
+ * Note: this won't work on a pointer to the first element of an
+ * array since sizeof() will only give the length of the first element.
+ * Unfortunately, no compiler diagnostic will flag this.
+ * Any array will have to be prefixed with an & to yield a pointer
+ * to the whole array.  The normal representation for a string or pointer
+ * to a raw buffer is a pointer to the first element, so they cannot be zeroed.
+ *
+ * Simple form of this rule:
+ * The argument to zero must be prefixed by & unless it is a pointer
+ * to the object you wish to zero.  A pointer to an object must be
+ * a pointer to the whole object, not just the first element.
  *
  * Note also that zeroing a pointer is not guaranteed to make it NULL
  * (read the C standard).  This problem is mostly theoretical since
@@ -147,7 +156,8 @@ typedef int bool;
  * secret that might be leaked).
  */
 
-#define messup(x) memset((x), 0xFB, sizeof(*(x)))	/* set all bytes to wrong value */
+#define messupn(x, n) memset((x), 0xFB, (n))	/* set n bytes to wrong value */
+#define messup(x) messupn((x), sizeof(*(x)))	/* set all bytes to wrong value */
 
 /* routines to copy C strings to fixed-length buffers */
 extern char *jam_str(char *dest, size_t size, const char *src);

programs/pluto/plutomain.c
@@ -398,7 +398,7 @@ static bool pluto_init_nss(char *nssdb)
 		rv = PK11_RandomUpdate(buf, seedbytes);
 		libreswan_log("seeded %d bytes into the NSS PRNG", seedbytes);
 		passert(rv == SECSuccess);
-		messup(&buf);
+		messupn(buf, seedbytes);
 		pfree(buf);
 	}
 

programs/rsasigkey/rsasigkey.c
@@ -169,7 +169,7 @@ static void UpdateNSS_RNG(int seedbits)
 	getrandom(seedbytes, buf);
 	rv = PK11_RandomUpdate(buf, seedbytes);
 	assert(rv == SECSuccess);
-	messup(buf);
+	messupn(buf, seedbytes);
 	pfree(buf);
 }
 