372381fadc13ce92dbeecd8386651bd97497313a
Antony Antony <antony@phenome.org>
2015-08-13 09:20:20 +0200

    testing newoe-06-prio/final.sh use trafficstatus to check which SA was used. connection

--- testing/pluto/newoe-06-prio/final.sh
+++ testing/pluto/newoe-06-prio/final.sh
@@ -1,3 +1,4 @@
+ipsec whack --trafficstatus
 # this should show IKE and IPsec state for "road-east-ikev2" and not an OE group
 ipsec status | grep road-east | sed 's/"road-east-ikev2".*/"road-east-ikev2" --- cut ---/' | grep "#"
 : ==== cut ====
