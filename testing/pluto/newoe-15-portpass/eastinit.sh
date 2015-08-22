5fe593a8cb34fb44d11df1f130b166ebb79eafff
Paul Wouters <pwouters@redhat.com>
2015-08-22 19:29:00 -0400

    testing: added newoe-15-portpass

---
+++ testing/pluto/newoe-15-portpass/eastinit.sh
@@ -0,0 +1,12 @@
+/testing/guestbin/swan-prep
+cp policies/* /etc/ipsec.d/policies/
+echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/private-or-clear
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+# give OE policies time to load
+sleep 5
+ipsec auto --add passthrough-in
+ipsec auto --route passthrough-in
+ipsec auto --add passthrough-out
+ipsec auto --route passthrough-out
+echo "initdone"
