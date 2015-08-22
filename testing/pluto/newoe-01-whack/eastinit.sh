f66b722de954cd91f928e23e6c048fb50034455b
Paul Wouters <pwouters@redhat.com>
2015-08-21 22:15:51 -0400

    testing: newoe testcase re-sync, cleanup and clear-or-private avoiding

--- testing/pluto/newoe-01-whack/eastinit.sh
+++ testing/pluto/newoe-01-whack/eastinit.sh
@@ -1,6 +1,6 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
-echo "192.0.2.0/24" >> /etc/ipsec.d/policies/clear-or-private
+echo "192.1.2.0/24" >> /etc/ipsec.d/policies/private-or-clear
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 # give OE policies time to load
