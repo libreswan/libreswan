140d03be9a56ccdbfd9d49faaebed816d1e8e47c
Paul Wouters <pwouters@redhat.com>
2015-08-22 19:58:38 -0400

    testing: two new test cases to test for /32 policy in /24 policy
    (/32 clear in /24 block works, but /24 block inside /32 clear fails)

---
+++ testing/pluto/newoe-14-clear-in-block-clear/final.sh
@@ -0,0 +1,9 @@
+ipsec look
+# should not show any hits
+grep "fos_" /tmp/pluto.log
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
