5d7d591e19c1caae180367f38ee79f4045e5bb58
Paul Wouters <pwouters@redhat.com>
2015-08-24 15:27:46 -0400

    testing: added ikev1-impair-gx-01 ikev1-impair-gx-02

---
+++ testing/pluto/ikev1-impair-gx-02/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec whack --debug-all --impair-send-zero-gx
+ipsec auto --add westnet-eastnet-ipv4-psk
+echo "initdone"
