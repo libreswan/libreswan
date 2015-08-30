448f72b82402ac787d1b2b89b4c072847af97f0a
Paul Wouters <pwouters@redhat.com>
2015-08-30 19:00:21 -0400

    testing: added xauth-pluto-20-pam

---
+++ testing/pluto/xauth-pluto-20-pam/eastinit.sh
@@ -0,0 +1,9 @@
+/testing/guestbin/swan-prep
+gcc -fPIC -fno-stack-protector -c mypam.c
+ld -x --shared -o /lib64/security/mypam.so mypam.o
+mv /etc/pam.d/pluto /etc/pam.d/pluto.stock
+cp pluto.pam /etc/pam.d/pluto
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add xauth-road-eastnet
+echo "initdone"
