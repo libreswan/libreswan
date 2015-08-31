libreswan/libreswan

commit f8e3ba3f29bbb7d42bcfe51dd5ec2ddbf7bbfcb1

ikev2 : PAM autherization support.  "pam-authorize=yes" per connection. ipsec whack --deleteid --name <id> bring down the connection with <id>

programs/pluto/ikev1_xauth.c
@@ -77,6 +77,7 @@
 #include "ikev1_xauth.h"
 #include "virtual.h"	/* needs connections.h */
 #include "addresspool.h"
+#include "pam_conv.h"
 
 /* forward declarations */
 static stf_status xauth_client_ackstatus(struct state *st,
@@ -114,11 +115,6 @@ struct xauth_thread_arg {
 	char *connname;
 	st_jbuf_t *ptr;
 };
-
-#ifdef XAUTH_HAVE_PAM
-static int xauth_pam_conv(int num_msg, const struct pam_message **msgm,
-			  struct pam_response **response, void *appdata_ptr);
-#endif
 
 /*
  * pointer to an array of st_jbuf_t elements.
@@ -1022,61 +1018,6 @@ static stf_status xauth_send_status(struct state *st, int status)
 }
 
 #ifdef XAUTH_HAVE_PAM
-/** XAUTH PAM conversation
- *
- * @param num_msg Int.
- * @param msgm Pam Message Struct
- * @param response Where PAM will put the results
- * @param appdata_ptr Pointer to data struct (as we are using threads)
- * @return int PAM Return Code (possibly fudged)
- */
-static int xauth_pam_conv(int num_msg,
-			  const struct pam_message **msgm,
-			  struct pam_response **response,
-			  void *appdata_ptr)
-{
-	struct xauth_thread_arg *const arg = appdata_ptr;
-	int count = 0;
-	struct pam_response *reply;
-
-	if (num_msg <= 0)
-		return PAM_CONV_ERR;
-
-	reply = alloc_bytes(
-			num_msg * sizeof(struct pam_response), "pam_response");
-
-	for (count = 0; count < num_msg; ++count) {
-		const char *s = NULL;
-
-		switch (msgm[count]->msg_style) {
-		case PAM_PROMPT_ECHO_OFF:
-			s = arg->password;
-			break;
-		case PAM_PROMPT_ECHO_ON:
-			s = arg->name;
-			break;
-		}
-
-		if (s != NULL) {
-			/*
-			 * Add s to list of responses.
-			 * According to pam_conv(3), our caller will
-			 * use free(3) to free these arguments so
-			 * we must allocate them with malloc,
-			 * not our own allocators.
-			 */
-			size_t len = strlen(s) + 1;
-			char *t = malloc(len);	/* must be malloced */
-
-			memcpy(t, s, len);
-			reply[count].resp_retcode = 0;
-			reply[count].resp = t;
-		}
-	}
-
-	*response = reply;
-	return PAM_SUCCESS;
-}
 
 /** Do authentication via PAM (Plugable Authentication Modules)
  *
@@ -1100,7 +1041,7 @@ static bool do_pam_authentication(void *varg)
 	do {
 		ipstr_buf ra;
 
-		conv.conv = xauth_pam_conv;
+		conv.conv = pam_conv;
 		conv.appdata_ptr = varg;
 
 		what = "pam_start";

programs/pluto/ikev2_parent.c
@@ -25,6 +25,7 @@
  *
  */
 
+#include <pthread.h>    /* Must be the first include file */
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
@@ -33,6 +34,7 @@
 #include <gmp.h>
 
 #include <libreswan.h>
+#include <errno.h>
 
 #include "sysdep.h"
 #include "constants.h"
@@ -61,6 +63,7 @@
 #include "pending.h"
 #include "kernel.h"
 #include "nat_traversal.h"
+#include "pam_conv.h"
 #include "alg_info.h" /* for ALG_INFO_IKE_FOREACH */
 
 #include "key.h" /* for SECKEY_DestroyPublicKey */
@@ -74,6 +77,23 @@
 		else \
 			send_v2_notification_from_md(md, t, NULL); \
 	}
+
+struct ikev2_pam_helper {
+	struct pam_thread_arg pam;	/* writable inside thread */
+	bool pam_status;		/* set inside the thread */
+	pthread_t tid;                  /* set before thread */
+	bool in_use;                    /* set before and inside thread */
+	struct timeval start_time;      /* set before thread */
+	struct timeval done_time;       /* set inside thread */
+	struct ikev2_pam_helper *next;  /* set outside thread */
+	int master_fd;                  /* master's fd (-1 if none) */
+	int helper_fd;                  /* helper's fd */
+	struct event *evm;              /* callback event on master_fd. */
+};
+
+static stf_status ikev2_parent_inI2outR2_auth_tail( struct msg_digest *md, bool pam_status);
+
+static struct ikev2_pam_helper *pluto_v2_pam_helpers = NULL;
 
 static crypto_req_cont_func ikev2_parent_outI1_continue;	/* type assertion */
 
@@ -2074,6 +2094,217 @@ static stf_status ikev2_parent_inR1outI2_tail(
 	return STF_OK;
 }
 
+#ifdef XAUTH_HAVE_PAM
+/* IN AN AUTHENTICAL THREAD */
+static void *ikev2_pam_autherize_thread (void *x)
+{
+	struct ikev2_pam_helper *p = (struct ikev2_pam_helper *) x;
+	struct timeval done_delta;
+	FILE *in = fdopen(p->helper_fd, "rb");
+	FILE *out = fdopen(p->helper_fd, "wb");
+	size_t sz;
+
+	/* threads will go quietly if the master cancel it */
+	pthread_setcanceltype  (PTHREAD_CANCEL_ASYNCHRONOUS,  NULL);
+	pthread_setcancelstate (PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+	p->pam_status = ikev2_do_pam_authentication(&p->pam);
+        gettimeofday(&p->done_time, NULL);
+	timersub(&p->done_time, &p->start_time, &done_delta);
+
+	DBG(DBG_CONTROL, DBG_log("#%lu %s[%lu] IKEv2 PAM helper thread"
+				" finished work. status %s elapsed time"
+				" %lu.%06lu '%s'",
+				p->pam.st_serialno, p->pam.c_name,
+				p->pam.c_instance_serial,
+				p->pam_status ? "SUCCESS" : "FAIL",
+				(unsigned long)done_delta.tv_sec,
+				(unsigned long)(done_delta.tv_usec * 1000000),
+				p->pam.name));
+	p->in_use = FALSE;
+	sz = fwrite(p, sizeof(char), sizeof(p), out);
+	fflush(out);
+
+	if (sz != sizeof(p)) {
+		if (ferror(out) != 0) {
+			/* ??? is strerror(ferror(out)) correct? */
+			char errbuf[1024];       /* ??? how big is big enough? */
+
+			strerror_r(errno, errbuf, sizeof(errbuf));
+			loglog(RC_LOG_SERIOUS,
+					"IKEv2 PAM helper failed to write answer: %s", errbuf);
+		} else {
+			/* short write -- fatal */
+			loglog(RC_LOG_SERIOUS,
+					"IKEv2 PAM helper error: write truncated: %zu instead of %zu",
+					sz, sizeof(p));
+		}
+	}
+
+	fclose(in);
+	fclose(out);
+
+	return NULL;
+}
+
+static void free_pam_thread_entry(struct ikev2_pam_helper *p)
+{
+	pfreeany(p->pam.name);
+	pfreeany(p->pam.password);
+	pfreeany(p->pam.c_name);
+	pfreeany(p->pam.ra);
+	pthread_cancel(p->tid);
+	event_free(p->evm);
+	if (p->master_fd != NULL_FD)
+		close(p->master_fd);
+	if (p->helper_fd != NULL_FD)
+		close(p->helper_fd);
+	pfreeany(p);
+	p = NULL;
+}
+
+static void ikev2_pam_continue(struct ikev2_pam_helper *p)
+{
+	stf_status stf;
+	struct msg_digest *md;
+	struct ikev2_pam_helper *x;
+	struct timeval served_time;
+	struct timeval served_delta;
+	struct timeval done_delta;
+	struct state *st = state_with_serialno(p->pam.st_serialno);
+
+	gettimeofday(&served_time, NULL);
+	timersub(&served_time, &p->start_time, &served_delta);
+	timersub(&p->done_time, &p->start_time, &done_delta);
+
+	if (st == NULL) {
+		DBG(DBG_CONTROL, DBG_log("IKEv2 PAM helper thread calls"
+					" state #%lu, %s[%lu]."
+				        " The state is gone. elapsed time %lu.%06lu",
+					p->pam.st_serialno, p->pam.c_name,
+					p->pam.c_instance_serial,
+					(unsigned long)served_delta.tv_sec,
+					(unsigned long)(served_delta.tv_usec * 1000000)));
+		return;
+	}
+	read(p->master_fd, (void *)&x, sizeof(p));
+
+	DBG(DBG_CONTROL, DBG_log("#%lu %s[%lu] IKEv2 PAM helper thread"
+				" can continue. PAM status %s."
+				" elapsed time %lu.%06lu"
+				" PAM auth time %lu.%06lu U='%s'",
+				p->pam.st_serialno, p->pam.c_name,
+				p->pam.c_instance_serial,
+				p->pam_status ? "SUCCESS" : "FAIL",
+				(unsigned long)served_delta.tv_sec,
+				(unsigned long)(served_delta.tv_usec * 1000000),
+				(unsigned long)done_delta.tv_sec,
+				(unsigned long)(done_delta.tv_usec * 1000000),
+				p->pam.name));
+
+
+	md = st->st_suspended_md;
+	unset_suspended(md->st);
+	st->has_pam_thread = FALSE;
+
+	if(p->pam_status) {
+		/* This is a hardcoded continue, convert this to micro state. */
+		stf = ikev2_parent_inI2outR2_auth_tail(md, p->pam_status);
+	} else {
+		stf = STF_FAIL + v2N_AUTHENTICATION_FAILED;
+	}
+
+	free_pam_thread_entry(p);
+
+	complete_v2_state_transition(&md, stf);
+	release_any_md(&md);
+	reset_globals();
+}
+
+static event_callback_routine ikev2_pam_continue_cb;
+static void ikev2_pam_continue_cb(evutil_socket_t fd UNUSED, const short event UNUSED, void *arg)
+{
+	ikev2_pam_continue((struct ikev2_pam_helper *)arg);
+}
+
+/*
+ * in the middle of IKEv2 AUTH exchange, the AUTH payload is verified succsfully,
+ * now invoke PAM helper to authrorize connection.
+ * when pam helper is done state will be woken up and continue
+ */
+
+static stf_status ikev2_start_pam_authorize(struct msg_digest *md)
+{
+	struct ikev2_pam_helper *p = alloc_thing(struct ikev2_pam_helper, "v2 pam helper");
+	struct state *st = md->st;
+	int thread_status;
+	int fds[2];
+	pthread_attr_t pattr;
+	ipstr_buf ra;
+	char thatid[IDTOA_BUF];
+
+	p->master_fd = NULL_FD;
+	set_suspended(md->st, md);
+	zero(p);
+	p->in_use = TRUE;
+	gettimeofday(&p->start_time, NULL);
+
+	if (socketpair(PF_UNIX, SOCK_STREAM, 0, fds) != 0) {
+		loglog(RC_LOG_SERIOUS, "could not create socketpair for ikev2 pam authorize: %s",
+				strerror(errno));
+		return STF_INTERNAL_ERROR;
+	}
+	p->master_fd = fds[0];
+	p->helper_fd = fds[1];
+
+	idtoa(&st->st_connection->spd.that.id, thatid, sizeof(thatid));
+	p->pam.name = clone_str(thatid, "pam name thatid");
+
+	/* ??? if password is always "password" (seems odd) then wny is a copy needed? */
+	p->pam.password = clone_str("password", "password");
+	p->pam.c_name = clone_str(st->st_connection->name, "connection name, ikev2 pam");
+	p->pam.ra = clone_str(ipstr(&st->st_remoteaddr, &ra), "st remote address");
+	p->pam.c_instance_serial = st->st_connection->instance_serial;
+	p->pam.st_serialno = st->st_serialno;
+
+	p->next = pluto_v2_pam_helpers;
+	pluto_v2_pam_helpers = p;
+
+	DBG(DBG_CONTROL, DBG_log("#%lu, %s[%lu] start IKEv2 PAM helper thread U='%s' P='%s'",
+				p->pam.st_serialno, p->pam.c_name,
+				p->pam.c_instance_serial, p->pam.name,
+				p->pam.password));
+
+	pthread_attr_init(&pattr);
+	pthread_attr_setdetachstate(&pattr, PTHREAD_CREATE_DETACHED);
+	thread_status = pthread_create(&p->tid, NULL,
+			ikev2_pam_autherize_thread, (void *)p);
+	if (thread_status != 0) {
+		loglog(RC_LOG_SERIOUS, "#%lu  %s[%lu] failed to start IKEv2 PAM"
+				"helper thread error = %d '%s'",
+				p->pam.st_serialno, p->pam.c_name,
+				p->pam.c_instance_serial,
+				thread_status, p->pam.name);
+		close(fds[1]);
+		close(fds[0]);
+		p->master_fd = NULL_FD;
+		return STF_INTERNAL_ERROR;
+
+	}
+	DBG(DBG_CONTROLMORE, DBG_log("#%lu %s[%lu] started IKEv2 PAM helper thread '%s'",
+				p->pam.st_serialno, p->pam.c_name,
+				p->pam.c_instance_serial, p->pam.name));
+	st->has_pam_thread = TRUE;
+	pthread_attr_destroy(&pattr);
+
+	DBG(DBG_CONTROL, DBG_log("setup IKEv2 PAM authorize helper callback"
+				" for master fd %d", p->master_fd));
+	p->evm = pluto_event_new(p->master_fd, EV_READ, ikev2_pam_continue_cb,p, NULL);
+
+	return STF_SUSPEND;
+}
+#endif /* XAUTH_HAVE_PAM */
+
 /*
  *
  ***************************************************************
@@ -2099,6 +2330,7 @@ static crypto_req_cont_func ikev2_parent_inI2outR2_continue;
 static stf_status ikev2_parent_inI2outR2_tail(
 	struct pluto_crypto_req_cont *dh,
 	struct pluto_crypto_req *r);
+
 
 stf_status ikev2parent_inI2outR2(struct msg_digest *md)
 {
@@ -2179,10 +2411,7 @@ static stf_status ikev2_parent_inI2outR2_tail(
 	struct msg_digest *md = dh->pcrc_md;
 	struct state *const st = md->st;
 	struct connection *c = st->st_connection;
-	unsigned char idhash_in[MAX_DIGEST_LEN], idhash_out[MAX_DIGEST_LEN];
-	unsigned char *authstart;
-	unsigned int np;
-	int v2_notify_num = 0;
+	unsigned char idhash_in[MAX_DIGEST_LEN];
 
 	/* extract calculated values from r */
 	finish_dh_v2(st, r);
@@ -2282,6 +2511,31 @@ static stf_status ikev2_parent_inI2outR2_tail(
 			      enum_name(&ikev2_auth_names,
 					md->chain[ISAKMP_NEXT_v2AUTH]->payload.
 					v2a.isaa_type));
+		return STF_FATAL;
+	}
+
+#ifdef XAUTH_HAVE_PAM
+	if (c->policy & POLICY_IKEV2_PAM_AUTHORIZE)
+		return ikev2_start_pam_authorize(md);
+#endif
+	return ikev2_parent_inI2outR2_auth_tail(md, TRUE);
+}
+
+static stf_status ikev2_parent_inI2outR2_auth_tail( struct msg_digest *md,
+		bool pam_status)
+{
+	struct state *const st = md->st;
+	struct connection *c = st->st_connection;
+	unsigned char idhash_out[MAX_DIGEST_LEN];
+	unsigned char *authstart;
+	unsigned int np;
+
+	if (!pam_status) {
+		/*
+		 * TBD: send this notification encrypted because
+		 * because AUTH payload succeed
+		 */
+		SEND_V2_NOTIFICATION(v2N_AUTHENTICATION_FAILED);
 		return STF_FATAL;
 	}
 
@@ -2465,7 +2719,7 @@ static stf_status ikev2_parent_inI2outR2_tail(
 				DBG(DBG_CONTROL,
 				    DBG_log("ikev2_child_sa_respond returned STF_FAIL with %s",
 					    enum_name(&ikev2_notify_names,
-						      v2_notify_num)));
+						      ret - STF_FAIL)));
 				np = ISAKMP_NEXT_v2NONE; /* use some day if we built a complete packet */
 				return ret; /* we should continue building a valid reply packet */
 			} else if (ret != STF_OK) {
@@ -4169,8 +4423,30 @@ bool ikev2_delete_out(struct state *st)
  */
 static int build_ikev2_version()
 {
-/* TODO: if bumping, we should also set the Version flag in the ISAKMP haeder */
-return ((IKEv2_MAJOR_VERSION + (DBGP(IMPAIR_MAJOR_VERSION_BUMP) ? 1 : 0))
-	<< ISA_MAJ_SHIFT) | (IKEv2_MINOR_VERSION +
-	(DBGP(IMPAIR_MINOR_VERSION_BUMP) ? 1 : 0));
+	/* TODO: if bumping, we should also set the Version flag in the ISAKMP header */
+	return ((IKEv2_MAJOR_VERSION + (DBGP(IMPAIR_MAJOR_VERSION_BUMP) ? 1 : 0))
+			<< ISA_MAJ_SHIFT) | (IKEv2_MINOR_VERSION +
+				(DBGP(IMPAIR_MINOR_VERSION_BUMP) ? 1 : 0));
 }
+
+#ifdef XAUTH_HAVE_PAM
+void state_deletion_cleanup(so_serial_t st_serialno)
+{
+	struct ikev2_pam_helper **pp;
+	struct ikev2_pam_helper *p;
+
+	/* search for finished pam threads */
+	for (pp = &pluto_v2_pam_helpers; (p = *pp) != NULL; pp = &p->next) {
+		if (p->pam.st_serialno == st_serialno) {
+			DBG(DBG_CONTROL, DBG_log("Deleting IKEv2 PAM helper thread for #%lu,"
+						" %s[%lu] status %s '%s'",
+						p->pam.st_serialno, p->pam.c_name,
+						p->pam.c_instance_serial,
+						p->pam_status ? "SUCCESS" : "FAIL",
+						p->pam.name));
+			free_pam_thread_entry(p);
+		}
+	}
+}
+
+#endif

programs/pluto/pam_conv.c
@@ -1,0 +1,179 @@
+/* PAM Authentication and Autherization related functions
+ *
+ * Copyright (C) 2001-2002 Colubris Networks
+ * Copyright (C) 2003 Sean Mathews - Nu Tech Software Solutions, inc.
+ * Copyright (C) 2003-2004 Xelerance Corporation
+ * Copyright (C) 2009 Ken Wilson <Ken_Wilson@securecomputing.com>
+ * Copyright (C) 2009 Avesh Agarwal <avagarwa@redhat.com>
+ * Copyright (C) 2010,2013 D. Hugh Redelmeier <hugh@mimosa.com>
+ * Copyright (C) 2012 Wes Hardaker <opensource@hardakers.net>
+ * Copyright (C) 2012-2013 Paul Wouters <pwouters@redhat.com>
+ * Copyright (C) 2012-2013 Philippe Vouters <philippe.vouters@laposte.net>
+ * Copyright (C) 2013 David McCullough <ucdevel@gmail.com>
+ * Copyright (C) 2013 Antony Antony <antony@phenome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * This code originally written by Colubris Networks, Inc.
+ * Extraction of patch and porting to 1.99 codebases by Xelerance Corporation
+ * Porting to 2.x by Sean Mathews
+ */
+
+#ifdef XAUTH_HAVE_PAM
+#include <string.h>
+#include <stdlib.h>
+#include <security/pam_appl.h> /* needed for pam_handle_t */
+
+#include "defs.h"
+#include "lswlog.h"
+#include "pam_conv.h"
+
+/* BEWARE: This code is multi-threaded.
+ *
+ * Any static object is likely shared and probably has to be protected by
+ * a lock.
+ * Any other shared object needs to be protected.
+ * Beware of calling functions that are not thread-safe.
+ *
+ * Non-thread-safe functions:
+ * - ??? pam_*?
+ */
+
+/*
+ * PAM conversation
+ *
+ * @param num_msg Int.
+ * @param msgm Pam Message Struct
+ * @param response Where PAM will put the results
+ * @param appdata_ptr Pointer to data struct (as we are using threads)
+ * @return int PAM Return Code (possibly fudged)
+ */
+int pam_conv(int num_msg,
+		const struct pam_message **msgm,
+		struct pam_response **response,
+		void *appdata_ptr)
+{
+	struct pam_thread_arg *const arg = appdata_ptr;
+	int count = 0;
+	struct pam_response *reply;
+
+	if (num_msg <= 0)
+		return PAM_CONV_ERR;
+
+	/*
+	 *   According to pam_conv(3), caller will free(3) reply
+	 *   so we must allocate it with malloc.
+	 */
+	reply = malloc(num_msg * sizeof(struct pam_response));
+
+	for (count = 0; count < num_msg; ++count) {
+		const char *s = NULL;
+
+		switch (msgm[count]->msg_style) {
+		case PAM_PROMPT_ECHO_OFF:
+			s = arg->password;
+			break;
+		case PAM_PROMPT_ECHO_ON:
+			s = arg->name;
+			break;
+		}
+
+		reply[count].resp_retcode = 0;
+		reply[count].resp = NULL;       /* for unhandled case */
+
+		if (s != NULL) {
+			/*
+			 * Add s to list of responses.
+			 * According to pam_conv(3), our caller will
+			 * use free(3) to free these arguments so
+			 * we must allocate them with malloc,
+			 * not our own allocators.
+			 */
+			size_t len = strlen(s) + 1;
+			char *t = malloc(len);	/* must be malloced */
+
+			memcpy(t, s, len);
+			reply[count].resp = t;
+		}
+	}
+
+	*response = reply;
+	return PAM_SUCCESS;
+}
+
+/* 
+ * Do IKEv2 second authentication via PAM (Plugable Authentication Modules)
+ *
+ * @return bool success
+ */
+/* IN AN AUTH THREAD */
+bool ikev2_do_pam_authentication(void *varg)
+{
+	struct pam_thread_arg *arg = varg;
+	int retval;
+	pam_handle_t *pamh = NULL;
+	struct pam_conv conv;
+	const char *what;
+
+	/* This do-while structure is designed to allow a logical cascade
+	 * without excessive indentation.  No actual looping happens.
+	 * Failure is handled by "break".
+	 */
+	do {
+		conv.conv = pam_conv;
+		conv.appdata_ptr = varg;
+
+		what = "pam_start";
+		retval = pam_start("pluto", arg->name, &conv, &pamh);
+		if (retval != PAM_SUCCESS)
+			break;
+
+		DBG(DBG_CONTROL, DBG_log("pam_start SUCCESS"));
+
+		/* Send the remote host address to PAM */
+		what = "pam_set_item";
+		retval = pam_set_item(pamh, PAM_RHOST, arg->ra);
+		if (retval != PAM_SUCCESS)
+			break;
+
+		DBG(DBG_CONTROL, DBG_log("pam_set_item SUCCESS"));
+
+		/* Two factor authentication - Check that the user is valid,
+		 * and then check if they are permitted access
+		 */
+		what = "pam_authenticate";
+		retval = pam_authenticate(pamh, PAM_SILENT); /* is user really user? */
+
+		if (retval != PAM_SUCCESS)
+			break;
+
+		DBG(DBG_CONTROL, DBG_log("pam_authenticate SUCCESS"));
+
+		what = "pam_acct_mgmt";
+		retval = pam_acct_mgmt(pamh, 0); /* permitted access? */
+		if (retval != PAM_SUCCESS)
+			break;
+
+		/* success! */
+		libreswan_log("IKEv2: PAM_SUCCESS");
+		pam_end(pamh, PAM_SUCCESS);
+		return TRUE;
+	} while (FALSE);
+
+	/* common failure code */
+
+	DBG(DBG_CONTROL,
+	    DBG_log("%s failed with '%s", what, pam_strerror(pamh, retval)));
+	libreswan_log("IKEv2 : %s failed with '%s'", what, pam_strerror(pamh, retval));
+	pam_end(pamh, retval);
+	return FALSE;
+}
+#endif /* XAUTH_HAVE_PAM */

programs/pluto/pam_conv.h
@@ -1,0 +1,32 @@
+/* PAM Authentication and Autherization related 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * This code originally written by Colubris Networks, Inc.
+ * Extraction of patch and porting to 1.99 codebases by Xelerance Corporation
+ * Porting to 2.x by Sean Mathews
+ */
+
+#ifdef XAUTH_HAVE_PAM
+struct pam_thread_arg {
+	char *name;
+	char *password;
+	char *c_name;
+	char *ra;
+	so_serial_t st_serialno;
+	unsigned long c_instance_serial;
+};
+
+extern bool ikev2_do_pam_authentication(void *varg);
+int pam_conv(int num_msg, const struct pam_message **msgm,
+		                          struct pam_response **response, void
+					  *appdata_ptr);
+#endif /* XAUTH_HAVE_PAM */

include/ipsecconf/keywords.h
@@ -116,6 +116,7 @@ enum keyword_numeric_config_field {
 	KBF_FORCEENCAP,
 	KBF_IKEv2,
 	KBF_IKEv2_ALLOW_NARROWING,
+	KBF_IKEv2_PAM_AUTHORIZE,
 	KBF_CONNADDRFAMILY,
 	KBF_FORCEBUSY, /* obsoleted for KBF_DDOS_MODE */
 	KBF_DDOS_IKE_TRESHOLD,

commit 3f14d347d8218fb5ff00c796948f96f8bfac03b3

pluto: fix ikev1 xauth pam, a bug introduced in f8e3ba3f2 consolidate ikev1 and ikev2 do_pam_authentication to one function. log elapsed time for PAM. improve debug messages in do_pam_authentication to track multiple clients.

programs/pluto/ikev1_xauth.c
@@ -1001,81 +1001,6 @@ static stf_status xauth_send_status(struct state *st, int status)
 	return STF_OK;
 }
 
-#ifdef XAUTH_HAVE_PAM
-
-/** Do authentication via PAM (Plugable Authentication Modules)
- *
- * We try to authenticate the user in our own PAM session.
- *
- * @return bool success
- */
-/* IN AN AUTH THREAD */
-static bool do_pam_authentication(void *varg)
-{
-	struct xauth_thread_arg *arg = varg;
-	int retval;
-	pam_handle_t *pamh = NULL;
-	struct pam_conv conv;
-	const char *what;
-
-	/* This do-while structure is designed to allow a logical cascade
-	 * without excessive indentation.  No actual looping happens.
-	 * Failure is handled by "break".
-	 */
-	do {
-		ipstr_buf ra;
-
-		conv.conv = pam_conv;
-		conv.appdata_ptr = varg;
-
-		what = "pam_start";
-		retval = pam_start("pluto", arg->name, &conv, &pamh);
-		if (retval != PAM_SUCCESS)
-			break;
-
-		DBG(DBG_CONTROL, DBG_log("pam_start SUCCESS"));
-
-		/* Send the remote host address to PAM */
-		what = "pam_set_item";
-		retval = pam_set_item(pamh, PAM_RHOST,
-				      ipstr(&arg->st->st_remoteaddr, &ra));
-		if (retval != PAM_SUCCESS)
-			break;
-
-		DBG(DBG_CONTROL, DBG_log("pam_set_item SUCCESS"));
-
-		/* Two factor authentication - Check that the user is valid,
-		 * and then check if they are permitted access
-		 */
-		what = "pam_authenticate";
-		retval = pam_authenticate(pamh, PAM_SILENT); /* is user really user? */
-
-		if (retval != PAM_SUCCESS)
-			break;
-
-		DBG(DBG_CONTROL, DBG_log("pam_authenticate SUCCESS"));
-
-		what = "pam_acct_mgmt";
-		retval = pam_acct_mgmt(pamh, 0); /* permitted access? */
-		if (retval != PAM_SUCCESS)
-			break;
-
-		/* success! */
-		libreswan_log("XAUTH: PAM_SUCCESS");
-		pam_end(pamh, PAM_SUCCESS);
-		return TRUE;
-	} while (FALSE);
-
-	/* common failure code */
-
-	DBG(DBG_CONTROL,
-	    DBG_log("%s failed with '%s", what, pam_strerror(pamh, retval)));
-	libreswan_log("XAUTH: %s failed with '%s'", what, pam_strerror(pamh, retval));
-	pam_end(pamh, retval);
-	return FALSE;
-}
-#endif /* XAUTH_HAVE_PAM */
-
 /** Do authentication via /etc/ipsec.d/passwd file using MD5 passwords
  *
  * Structure is one entry per line.
@@ -1256,6 +1181,43 @@ static bool do_file_authentication(void *varg)
 	return win;
 }
 
+/* IN AN AUTH THREAD */
+static bool ikev1_do_pam_authentication(const struct xauth_thread_arg *arg)
+{
+	struct state *st = arg->st;
+	libreswan_log("XAUTH: pam authentication being called to authenticate user %s",
+			arg->name);
+	struct pam_thread_arg parg;
+	ipstr_buf ra;
+	struct timeval start_time;
+	struct timeval served_time;
+	struct timeval served_delta;
+	bool results = FALSE;
+
+	parg.name = arg->name;
+	parg.password =  arg->password;
+	parg.c_name = arg->connname;
+	parg.ra = clone_str(ipstr(&st->st_remoteaddr, &ra), "st remote address");
+	parg.st_serialno = st->st_serialno;
+	parg.c_instance_serial = st->st_connection->instance_serial;
+	parg.atype = "XAUTH";
+	gettimeofday(&start_time, NULL);
+	results = do_pam_authentication(&parg);
+	gettimeofday(&served_time, NULL);
+	timersub(&served_time, &start_time, &served_delta);
+	DBG(DBG_CONTROL, DBG_log("XAUTH PAM helper thread call "
+				"state #%lu, %s[%lu] user=%s %s. "
+				"elapsed time %lu.%06lu",
+				parg.st_serialno, parg.c_name,
+				parg.c_instance_serial, parg.name,
+				results ? "SUCCESS" : "FAIL",
+				(unsigned long)served_delta.tv_sec,
+				(unsigned long)(served_delta.tv_usec * 1000000)));
+
+	pfreeany(parg.ra);
+	return (results);
+}
+
 /*
  * Main authentication routine will then call the actual compiled-in
  * method to verify the user/password
@@ -1330,10 +1292,7 @@ static void *do_authentication(void *varg)
 	switch (st->st_connection->xauthby) {
 #ifdef XAUTH_HAVE_PAM
 	case XAUTHBY_PAM:
-		libreswan_log(
-			"XAUTH: pam authentication being called to authenticate user %s",
-			arg->name);
-		results = do_pam_authentication(varg);
+		results = ikev1_do_pam_authentication(arg);
 		break;
 #endif
 	case XAUTHBY_FILE:

programs/pluto/ikev2_parent.c
@@ -2382,7 +2382,7 @@ static void *ikev2_pam_autherize_thread (void *x)
 	pthread_setcanceltype  (PTHREAD_CANCEL_ASYNCHRONOUS,  NULL);
 	pthread_setcancelstate (PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
-	p->pam_status = ikev2_do_pam_authentication(&p->pam);
+	p->pam_status = do_pam_authentication(&p->pam);
 	gettimeofday(&p->done_time, NULL);
 	timersub(&p->done_time, &p->start_time, &done_delta);
 
@@ -2545,6 +2545,7 @@ static stf_status ikev2_start_pam_authorize(struct msg_digest *md)
 	p->pam.ra = clone_str(ipstr(&st->st_remoteaddr, &ra), "st remote address");
 	p->pam.c_instance_serial = st->st_connection->instance_serial;
 	p->pam.st_serialno = st->st_serialno;
+	p->pam.atype = "IKEv2";
 
 	p->next = pluto_v2_pam_helpers;
 	pluto_v2_pam_helpers = p;

programs/pluto/pam_conv.c
@@ -10,7 +10,7 @@
  * Copyright (C) 2012-2013 Paul Wouters <pwouters@redhat.com>
  * Copyright (C) 2012-2013 Philippe Vouters <philippe.vouters@laposte.net>
  * Copyright (C) 2013 David McCullough <ucdevel@gmail.com>
- * Copyright (C) 2013 Antony Antony <antony@phenome.org>
+ * Copyright (C) 2013-2015 Antony Antony <antony@phenome.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -109,15 +109,25 @@ int pam_conv(int num_msg,
 	return PAM_SUCCESS;
 }
 
+static void log_pam_step(const struct pam_thread_arg *arg, const char *what,
+		const char *how)
+{
+	DBG(DBG_CONTROL, DBG_log("%s helper thread %s %s for "
+                                "state #%lu, %s[%lu] user=%s.",
+				arg->atype, what, how,
+				arg->st_serialno, arg->c_name,
+				arg->c_instance_serial, arg->name));
+
+}
+
 /*
  * Do IKEv2 second authentication via PAM (Plugable Authentication Modules)
  *
  * @return bool success
  */
 /* IN AN AUTH THREAD */
-bool ikev2_do_pam_authentication(void *varg)
+bool do_pam_authentication(struct pam_thread_arg *arg)
 {
-	struct pam_thread_arg *arg = varg;
 	int retval;
 	pam_handle_t *pamh = NULL;
 	struct pam_conv conv;
@@ -129,22 +139,20 @@ bool ikev2_do_pam_authentication(void *varg)
 	 */
 	do {
 		conv.conv = pam_conv;
-		conv.appdata_ptr = varg;
+		conv.appdata_ptr = arg;
 
 		what = "pam_start";
 		retval = pam_start("pluto", arg->name, &conv, &pamh);
 		if (retval != PAM_SUCCESS)
 			break;
-
-		DBG(DBG_CONTROL, DBG_log("pam_start SUCCESS"));
+		log_pam_step(arg, what, "SUCCESS");
 
 		/* Send the remote host address to PAM */
 		what = "pam_set_item";
 		retval = pam_set_item(pamh, PAM_RHOST, arg->ra);
 		if (retval != PAM_SUCCESS)
 			break;
-
-		DBG(DBG_CONTROL, DBG_log("pam_set_item SUCCESS"));
+		log_pam_step(arg, what, "SUCCESS");
 
 		/* Two factor authentication - Check that the user is valid,
 		 * and then check if they are permitted access
@@ -154,16 +162,16 @@ bool ikev2_do_pam_authentication(void *varg)
 
 		if (retval != PAM_SUCCESS)
 			break;
+		log_pam_step(arg, what, "SUCCESS");
 
-		DBG(DBG_CONTROL, DBG_log("pam_authenticate SUCCESS"));
-
-		what = "pam_acct_mgmt";
 		retval = pam_acct_mgmt(pamh, 0); /* permitted access? */
 		if (retval != PAM_SUCCESS)
 			break;
 
+		what = "pam";
+		log_pam_step(arg, what, "SUCCESS");
+
 		/* success! */
-		libreswan_log("IKEv2: PAM_SUCCESS");
 		pam_end(pamh, PAM_SUCCESS);
 		return TRUE;
 	} while (FALSE);

programs/pluto/pam_conv.h
@@ -23,9 +23,10 @@ struct pam_thread_arg {
 	char *ra;
 	so_serial_t st_serialno;
 	unsigned long c_instance_serial;
+	char *atype;  /* string XAUTH or IKEv2 */
 };
 
-extern bool ikev2_do_pam_authentication(void *varg);
+extern bool do_pam_authentication(struct pam_thread_arg *arg);
 int pam_conv(int num_msg, const struct pam_message **msgm,
 					  struct pam_response **response, void
 					  *appdata_ptr);