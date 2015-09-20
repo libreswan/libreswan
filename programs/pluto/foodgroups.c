7f2ab7dffe3f52fda54b2410e5ef729eb47e36cb
Antony Antony <antony@phenome.org>
2015-09-20 23:14:34 +0200

    pluto: replace function ipcmp with addrcmp, to support both v4 and v6 ipcmp is not used else where so get rid of it.

--- programs/pluto/foodgroups.c
+++ programs/pluto/foodgroups.c
@@ -74,28 +74,6 @@ static struct fg_targets *targets = NULL;
 
 static struct fg_targets *new_targets;
 
-/* ipcmp compares the two ip_address values a and b.
- * It returns -1, 0, or +1 if a is, respectively,
- * less than, equal to, or greater than b.
- */
-static int ipcmp(ip_address *a, ip_address *b)
-{
-	if (addrtypeof(a) != addrtypeof(b)) {
-		return addrtypeof(a) < addrtypeof(b) ? -1 : 1;
-	} else if (sameaddr(a, b)) {
-		return 0;
-	} else {
-		const struct sockaddr *sa = sockaddrof(a),
-		*sb = sockaddrof(b);
-
-		passert(addrtypeof(a) == AF_INET); /* not yet implemented IPv6 version :-( */
-		return (ntohl(((const struct sockaddr_in *)sa)->sin_addr.s_addr)
-			<
-			ntohl(((const struct sockaddr_in *)sb)->sin_addr.s_addr))
-		       ?
-		       -1 : 1;
-	}
-}
 
 /* subnetcmp compares the two ip_subnet values a and b.
  * It returns -1, 0, or +1 if a is, respectively,
@@ -110,9 +88,9 @@ static int subnetcmp(const ip_subnet *a, const ip_subnet *b)
 	maskof(a, &maska);
 	networkof(b, &netb);
 	maskof(b, &maskb);
-	r = ipcmp(&neta, &netb);
+	r = addrcmp(&neta, &netb);
 	if (r == 0)
-		r = ipcmp(&maska, &maskb);
+		r = addrcmp(&maska, &maskb);
 	return r;
 }
 
