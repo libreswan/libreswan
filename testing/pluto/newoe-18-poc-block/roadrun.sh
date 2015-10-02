68934e9ddded489d3269d64a6377aa094da3750b
Paul Wouters <pwouters@redhat.com>
2015-10-02 10:33:26 -0400

    testing: fixup two more newoe tests

--- testing/pluto/newoe-18-poc-block/roadrun.sh
+++ testing/pluto/newoe-18-poc-block/roadrun.sh
@@ -1,7 +1,7 @@
 ping -w 2 -n -c 1 -I 192.1.3.209 192.1.2.23
 # wait on OE retransmits and rekeying
 sleep 5
-# should show no bare shunts or tunnels
+# should show pass bare shunt
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
 ipsec look
