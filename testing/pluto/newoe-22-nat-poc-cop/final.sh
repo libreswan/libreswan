dbd8fa47ee9c0839da9c78b634ecc073a6b59272
Paul Wouters <pwouters@redhat.com>
2015-10-03 17:04:11 -0400

    testing: Add newoe-22-nat-poc-cop

---
+++ testing/pluto/newoe-22-nat-poc-cop/final.sh
@@ -0,0 +1,9 @@
+ipsec look
+# a tunnel should have established
+grep "negotiated tunnel" /tmp/pluto.log
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
