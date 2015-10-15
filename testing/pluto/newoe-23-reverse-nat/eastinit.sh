ba7dc9c45af6ca5d627dd9b38955801dbfb1c36d
Paul Wouters <pwouters@redhat.com>
2015-10-15 02:55:04 -0400

    testing: skip sending deletes in newoe-23-reverse-nat

--- testing/pluto/newoe-23-reverse-nat/eastinit.sh
+++ testing/pluto/newoe-23-reverse-nat/eastinit.sh
@@ -4,6 +4,7 @@ echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/private-or-clear
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 # disable sending deletes to test if road deletes incomplete states
+ipsec whack --debugall --impair-no-send-delete
 # give OE policies time to load
 sleep 5
 echo "initdone"
