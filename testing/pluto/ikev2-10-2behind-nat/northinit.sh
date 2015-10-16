3909e47617c721d3782210e8ab095745237891c6
Antony Antony <antony@phenome.org>
2015-10-16 12:10:02 +0200

    testing: ikev1-hub-spoke ikev2-10-2behind-nat missing files

---
+++ testing/pluto/ikev2-10-2behind-nat/northinit.sh
@@ -0,0 +1,7 @@
+/testing/guestbin/swan-prep --x509
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add north
+ipsec auto --up north
+ping -n -c 4 192.0.2.254
+echo "initdone"
