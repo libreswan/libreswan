b6fdbf348907edbfa6a8be8b8826660b33cb2d8f
Antony Antony <antony@phenome.org>
2015-10-16 10:19:53 +0200

    testing: fix ikev1-hub-spoke ikev2-49-hub-spoke and add to TESTLIST output was old "negotiated tunnel"

--- testing/pluto/ikev1-hub-spoke/northinit.sh
+++ testing/pluto/ikev1-hub-spoke/northinit.sh
@@ -2,5 +2,6 @@
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add northnet-westnet-ipv4-psk
+ipsec auto --up northnet-westnet-ipv4-psk
 ipsec auto --status
 echo "initdone"
