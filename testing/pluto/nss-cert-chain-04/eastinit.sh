c882872fd69becf7cf0691453a679d95c6d0c773
Matt Rogers <mrogers@redhat.com>
2015-12-07 15:36:14 -0500

    testing: nss-cert-chain-04 changes

--- testing/pluto/nss-cert-chain-04/eastinit.sh
+++ testing/pluto/nss-cert-chain-04/eastinit.sh
@@ -1,10 +1,11 @@
-/testing/guestbin/swan-prep --x509 --certchain
+/testing/guestbin/swan-prep --x509
+pk12util -i /testing/x509/pkcs12/otherca/signedbyother.p12 -d sql:/etc/ipsec.d -W "foobar"
 certutil -A -i /testing/x509/certs/west_chain_int_1.crt -n "west_chain_1" -d sql:/etc/ipsec.d -t "CT,,"
 certutil -A -i /testing/x509/certs/west_chain_int_2.crt -n "west_chain_2" -d sql:/etc/ipsec.d -t "CT,,"
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add chain-A
-ipsec auto --add chain-B
-ipsec auto --status |grep chain
+ipsec auto --add road-chain-B
+ipsec auto --add road-A
+ipsec auto --status |grep road
 certutil -L -d sql:/etc/ipsec.d
 echo "initdone"
--- testing/pluto/nss-cert-chain-04/westinit.sh
+++ testing/pluto/nss-cert-chain-04/westinit.sh
@@ -1,7 +1,8 @@
 /testing/guestbin/swan-prep --x509 --certchain
-pk12util -i /testing/x509/pkcs12/mainca/west.p12 -d sql:/etc/ipsec.d -W "foobar"
-certutil -A -i /testing/x509/certs/east_chain_int_1.crt -n "east_chain_1" -d sql:/etc/ipsec.d -t "CT,,"
-certutil -A -i /testing/x509/certs/east_chain_int_2.crt -n "east_chain_2" -d sql:/etc/ipsec.d -t "CT,,"
+certutil -A -i /testing/x509/cacerts/otherca.crt -n "otherca" -d sql:/etc/ipsec.d -t "CT,,"
+#pk12util -i /testing/x509/pkcs12/mainca/west.p12 -d sql:/etc/ipsec.d -W "foobar"
+#certutil -A -i /testing/x509/certs/east_chain_int_1.crt -n "east_chain_1" -d sql:/etc/ipsec.d -t "CT,,"
+#certutil -A -i /testing/x509/certs/east_chain_int_2.crt -n "east_chain_2" -d sql:/etc/ipsec.d -t "CT,,"
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add chain-A
