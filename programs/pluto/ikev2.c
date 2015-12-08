9d314dd0f048b69b82e3b10d559b38b4a73ee097
Antony Antony <antony@phenome.org>
2015-12-08 22:26:39 +0100

    ikev2: OE before when IKE SA is about to expire check last usage. If it is not used let it expire.

--- programs/pluto/ikev2.c
+++ programs/pluto/ikev2.c
@@ -1422,9 +1422,12 @@ time_t ikev2_replace_delay(struct state *st, enum event_type *pkind,
 			*pkind = kind = EVENT_SA_EXPIRE;
 		}
 
-		if ((c->policy & POLICY_OPPORTUNISTIC) &&
-				(IS_CHILD_SA_ESTABLISHED(st))) {
-			*pkind = kind = EVENT_v2_SA_REPLACE_IF_USED;
+		if (c->policy & POLICY_OPPORTUNISTIC) {
+			if (IS_PARENT_SA_ESTABLISHED(st)) {
+				*pkind = kind = EVENT_v2_SA_REPLACE_IF_USED_IKE;
+			} else if (IS_CHILD_SA_ESTABLISHED(st)) {
+				*pkind = kind = EVENT_v2_SA_REPLACE_IF_USED;
+			}
 		} else if (c->policy & POLICY_DONT_REKEY) {
 			*pkind = kind = EVENT_SA_EXPIRE;
 		}
