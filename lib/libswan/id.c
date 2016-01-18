libreswan/libreswan

commit 16820ca64ab5c40642a68e647c13cc12e39ef6e1

simplify match_rdn, match_dn_unordered, refine_host_connection by shrinking scope of autos

lib/libswan/id.c
@@ -544,56 +544,47 @@ void duplicate_id(struct id *dst, const struct id *src)
 
 static bool match_rdn(CERTRDN *rdn_a, CERTRDN *rdn_b, bool *has_wild)
 {
-	CERTAVA **avas_a;
-	CERTAVA **avas_a_head;
-	CERTAVA *ava_a;
-	CERTAVA **avas_b;
-	CERTAVA *ava_b;
-	SECItem *val_b;
-	int tag_b, tag_a;
-	int matched = 0;
-	int ava_num = 0;
-	bool ret = FALSE;
-
 	if (rdn_a == NULL || rdn_b == NULL)
 		return FALSE;
 
-	avas_a_head = rdn_a->avas;
-	avas_b = rdn_b->avas;
+	int matched = 0;
+	int ava_num = 0;
 
-	while ((ava_b = *avas_b++) != NULL) {
+	CERTAVA **avas_b;
+	for (avas_b = rdn_b->avas; *avas_b != NULL; avas_b++) {
+		CERTAVA *const ava_b = *avas_b;
+		const SECOidTag tag_b = CERT_GetAVATag(ava_b);
+
 		ava_num++;
-		tag_b = CERT_GetAVATag(ava_b);
-		avas_a = avas_a_head;
-		while ((ava_a = *avas_a++) != NULL) {
-			tag_a = CERT_GetAVATag(ava_a);
-			if (tag_a == tag_b) {
-				val_b = CERT_DecodeAVAValue(&ava_b->value);
-				if (has_wild && val_b != NULL &&
-						val_b->len == 1 &&
-						val_b->data[0] == '*') {
-					*has_wild = TRUE;
-					matched++;
-					SECITEM_FreeItem(val_b, PR_TRUE);
-					break;
-				} else if (NSSCERT_CompareAVA(ava_a, ava_b) == SECEqual) {
-					matched++;
-					if (val_b != NULL)
-						SECITEM_FreeItem(val_b, PR_TRUE);
-					break;
-				}
+
+		CERTAVA **avas_a;
+		for (avas_a = rdn_a->avas; *avas_a != NULL; avas_a++) {
+			CERTAVA *const ava_a = *avas_a;
+
+			if (CERT_GetAVATag(ava_a) == tag_b) {
+				SECItem *val_b = CERT_DecodeAVAValue(&ava_b->value);
+
+				/* XXX Can CERT_DecodeAVAValue() return NULL? No man page :( */
 				if (val_b != NULL) {
-					/* XXX Can CERT_DecodeAVAValue() return NULL? No man page :( */
+					if (has_wild != NULL &&
+					    val_b->len == 1 &&
+					    val_b->data[0] == '*') {
+						*has_wild = TRUE;
+						matched++;
+						SECITEM_FreeItem(val_b, PR_TRUE);
+						break;
+					}
 					SECITEM_FreeItem(val_b, PR_TRUE);
+				}
+				if (NSSCERT_CompareAVA(ava_a, ava_b) == SECEqual) {
+					matched++;
+					break;
 				}
 			}
 		}
 	}
 
-	if ((matched > 0 && ava_num > 0) && matched == ava_num)
-		ret = TRUE;
-
-	return ret;
+	return matched > 0 && matched == ava_num;
 }
 
 /*
@@ -604,13 +595,6 @@ static bool match_dn_unordered(chunk_t a, chunk_t b, int *wildcards)
 {
 	char abuf[ASN1_BUF_LEN];
 	char bbuf[ASN1_BUF_LEN];
-	CERTName *a_name = NULL;
-	CERTName *b_name = NULL;
-	CERTRDN **rdns_a;
-	CERTRDN **rdns_a_head;
-	CERTRDN *rdn_a;
-	CERTRDN **rdns_b;
-	CERTRDN *rdn_b;
 	int rdn_num = 0;
 	int matched = 0;
 
@@ -619,20 +603,24 @@ static bool match_dn_unordered(chunk_t a, chunk_t b, int *wildcards)
 
 	DBG(DBG_CONTROL,
 	    DBG_log("%s A: %s, B: %s", __FUNCTION__, abuf, bbuf));
-	a_name = CERT_AsciiToName(abuf);
-	b_name = CERT_AsciiToName(bbuf);
+
+	CERTName *a_name = CERT_AsciiToName(abuf);
+	CERTName *b_name = CERT_AsciiToName(bbuf);
 
 	if (a_name == NULL || b_name == NULL)
 		return FALSE;
 
-	rdns_a_head = a_name->rdns;
-	rdns_b = b_name->rdns;
+	CERTRDN **rdns_b;
+	for (rdns_b = b_name->rdns; *rdns_b != NULL; rdns_b++) {
+		CERTRDN *rdn_b = *rdns_b;
 
-	while ((rdn_b = *rdns_b++) != NULL) {
 		rdn_num++;
-		rdns_a = rdns_a_head;
-		while ((rdn_a = *rdns_a++) != NULL) {
+
+		CERTRDN **rdns_a;
+		for (rdns_a = a_name->rdns; *rdns_a != NULL; rdns_a++) {
+			CERTRDN *rdn_a = *rdns_a++;
 			bool has_wild = FALSE;
+
 			if (match_rdn(rdn_a, rdn_b,
 				      wildcards != NULL ? &has_wild : NULL)) {
 				matched++;
@@ -652,12 +640,13 @@ static bool match_dn_unordered(chunk_t a, chunk_t b, int *wildcards)
 		    rdn_num,
 		    wildcards ? *wildcards : 0));
 
-	return ((matched > 0 && rdn_num > 0) && matched == rdn_num);
+	return matched > 0 && rdn_num > 0 && matched == rdn_num;
 }
 
 bool same_dn_any_order(chunk_t a, chunk_t b)
 {
 	bool ret = match_dn(a, b, NULL);
+
 	if (!ret) {
 		DBG(DBG_CONTROL,
 		    DBG_log("%s: not an exact match, now checking any RDN order",
@@ -671,6 +660,7 @@ bool same_dn_any_order(chunk_t a, chunk_t b)
 bool match_dn_any_order_wild(chunk_t a, chunk_t b, int *wildcards)
 {
 	bool ret = match_dn(a, b, wildcards);
+
 	if (!ret) {
 		DBG(DBG_CONTROL,
 		    DBG_log("%s: not an exact match, now checking any RDN order with %d wildcards",

programs/pluto/connections.c
@@ -1707,7 +1707,7 @@ struct connection *instantiate(struct connection *c, const ip_address *him,
 	c->instance_serial++;
 	d = clone_thing(*c, "instantiated connection");
 	if (his_id != NULL) {
-		int wildcards;
+		int wildcards;	/* ??? ignored? */
 
 		passert(d->spd.that.id.kind == ID_FROMCERT || match_id(his_id, &d->spd.that.id, &wildcards));
 		d->spd.that.id = *his_id;
@@ -2735,16 +2735,6 @@ struct connection *refine_host_connection(const struct state *st,
 					bool *fromcert)
 {
 	struct connection *c = st->st_connection;
-	struct connection *best_found = NULL;
-	const struct RSA_private_key *my_RSA_pri = NULL;
-	bool wcpip; /* wildcard Peer IP? */
-	int wildcards = 0,
-		best_wildcards = 0;
-	int our_pathlen = 0,
-		best_our_pathlen = 0,
-		peer_pathlen = 0,
-		best_peer_pathlen = 0;
-	const chunk_t *psk = NULL;
 	generalName_t *requested_ca = st->st_requested_ca;
 
 	*fromcert = FALSE;
@@ -2761,21 +2751,28 @@ struct connection *refine_host_connection(const struct state *st,
 
 	chunk_t peer_ca = get_peer_ca(peer_id);
 
-	if (same_id(&c->spd.that.id, peer_id) &&
-		peer_ca.ptr != NULL &&
-		trusted_ca_nss(peer_ca, c->spd.that.ca, &peer_pathlen) &&
-		peer_pathlen == 0 &&
-		match_requested_ca(requested_ca, c->spd.this.ca,
-				&our_pathlen) &&
-		our_pathlen == 0) {
+	{
+		int opl;
+		int ppl;
 
-		DBG(DBG_CONTROLMORE,
-			DBG_log("refine_host_connection: happy with starting point: %s",
-				c->name));
+		if (same_id(&c->spd.that.id, peer_id) &&
+			peer_ca.ptr != NULL &&
+			trusted_ca_nss(peer_ca, c->spd.that.ca, &ppl) &&
+			ppl == 0 &&
+			match_requested_ca(requested_ca, c->spd.this.ca, &opl) &&
+			opl == 0) {
 
-		/* peer ID matches current connection -- look no further */
-		return c;
+			DBG(DBG_CONTROLMORE,
+				DBG_log("refine_host_connection: happy with starting point: %s",
+					c->name));
+
+			/* peer ID matches current connection -- look no further */
+			return c;
+		}
 	}
+
+	const chunk_t *psk = NULL;
+	const struct RSA_private_key *my_RSA_pri = NULL;
 
 	if (auth_policy & POLICY_PSK) {
 		if (initiator) {
@@ -2822,13 +2819,21 @@ struct connection *refine_host_connection(const struct state *st,
 
 	struct connection *d = c->host_pair->connections;
 
+	int best_our_pathlen = 0;
+	int best_peer_pathlen = 0;
+	struct connection *best_found = NULL;
+	int best_wildcards = 0;
+
+	bool wcpip; /* wildcard Peer IP? */
 	for (wcpip = FALSE;; wcpip = TRUE) {
 		for (; d != NULL; d = d->hp_next) {
-			bool d_fromcert = FALSE;
+			int wildcards = 0;
 			bool match1 = match_id(peer_id, &d->spd.that.id,
 					&wildcards);
+			int peer_pathlen;
 			bool match2 = trusted_ca_nss(peer_ca, d->spd.that.ca,
 						&peer_pathlen);
+			int our_pathlen;
 			bool match3 = match_requested_ca(requested_ca,
 							d->spd.this.ca,
 							&our_pathlen);
@@ -2844,7 +2849,7 @@ struct connection *refine_host_connection(const struct state *st,
 					fmt_conn_instance(c, b1),
 					d->name,
 					fmt_conn_instance(d, b2),
-					best_found ?
+					best_found != NULL ?
 						best_found->name : "(none)",
 					match1 && match2 && match3,
 					match1, match2, match3);});
@@ -2862,6 +2867,7 @@ struct connection *refine_host_connection(const struct state *st,
 			 * Check for the match but also check to see if it's
 			 * the %fromcert + peer id match result. - matt
 			 */
+			bool d_fromcert = FALSE;
 			if (!match1) {
 				d_fromcert = id_kind(&d->spd.that.id) ==
 					ID_FROMCERT;
@@ -2912,8 +2918,7 @@ struct connection *refine_host_connection(const struct state *st,
 				char b1[CONN_INST_BUF];
 				char b2[CONN_INST_BUF];
 
-				DBG_log("refine_host_connection: checked %s%s "
-					"against %s%s, now for see if best",
+				DBG_log("refine_host_connection: checked %s%s against %s%s, now for see if best",
 					c->name,
 					fmt_conn_instance(c, b1),
 					d->name,

commit a90351b878f5a9af4be22a2a921281d8951a9a32

lib/libswan/id.c: fix bug introduced in 16820ca64ab5c40642a68e647c13cc12e39ef6e1; sprinkle const

lib/libswan/id.c
@@ -542,7 +542,7 @@ void duplicate_id(struct id *dst, const struct id *src)
 	clonetochunk(dst->name, src->name.ptr, src->name.len, "copy of id");
 }
 
-static bool match_rdn(CERTRDN *rdn_a, CERTRDN *rdn_b, bool *has_wild)
+static bool match_rdn(const CERTRDN *const rdn_a, const CERTRDN *const rdn_b, bool *const has_wild)
 {
 	if (rdn_a == NULL || rdn_b == NULL)
 		return FALSE;
@@ -550,14 +550,14 @@ static bool match_rdn(CERTRDN *rdn_a, CERTRDN *rdn_b, bool *has_wild)
 	int matched = 0;
 	int ava_num = 0;
 
-	CERTAVA **avas_b;
+	CERTAVA *const *avas_b;
 	for (avas_b = rdn_b->avas; *avas_b != NULL; avas_b++) {
 		CERTAVA *const ava_b = *avas_b;
 		const SECOidTag tag_b = CERT_GetAVATag(ava_b);
 
 		ava_num++;
 
-		CERTAVA **avas_a;
+		CERTAVA *const *avas_a;
 		for (avas_a = rdn_a->avas; *avas_a != NULL; avas_a++) {
 			CERTAVA *const ava_a = *avas_a;
 
@@ -591,7 +591,7 @@ static bool match_rdn(CERTRDN *rdn_a, CERTRDN *rdn_b, bool *has_wild)
  * match an equal number of RDNs, in any order
  * if wildcards != NULL, wildcard matches are enabled
  */
-static bool match_dn_unordered(chunk_t a, chunk_t b, int *wildcards)
+static bool match_dn_unordered(const chunk_t a, const chunk_t b, int *const wildcards)
 {
 	char abuf[ASN1_BUF_LEN];
 	char bbuf[ASN1_BUF_LEN];
@@ -604,21 +604,21 @@ static bool match_dn_unordered(chunk_t a, chunk_t b, int *wildcards)
 	DBG(DBG_CONTROL,
 	    DBG_log("%s A: %s, B: %s", __FUNCTION__, abuf, bbuf));
 
-	CERTName *a_name = CERT_AsciiToName(abuf);
-	CERTName *b_name = CERT_AsciiToName(bbuf);
+	CERTName *const a_name = CERT_AsciiToName(abuf);
+	CERTName *const b_name = CERT_AsciiToName(bbuf);
 
 	if (a_name == NULL || b_name == NULL)
 		return FALSE;
 
-	CERTRDN **rdns_b;
+	CERTRDN *const *rdns_b;
 	for (rdns_b = b_name->rdns; *rdns_b != NULL; rdns_b++) {
-		CERTRDN *rdn_b = *rdns_b;
+		CERTRDN *const rdn_b = *rdns_b;
 
 		rdn_num++;
 
-		CERTRDN **rdns_a;
+		CERTRDN *const *rdns_a;
 		for (rdns_a = a_name->rdns; *rdns_a != NULL; rdns_a++) {
-			CERTRDN *rdn_a = *rdns_a++;
+			CERTRDN *const rdn_a = *rdns_a;
 			bool has_wild = FALSE;
 
 			if (match_rdn(rdn_a, rdn_b,