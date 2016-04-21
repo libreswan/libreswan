270101866845e2b074f15b3df36fbd6fc51dd284
Andrew Cagney <cagney@gnu.org>
2016-04-21 15:19:18 -0400

    testing: use wait-until-live in tests that don't have their output checked

--- testing/pluto/ikev2-algo-esn-02/westinit.sh
+++ testing/pluto/ikev2-algo-esn-02/westinit.sh
@@ -1,6 +1,6 @@
 /testing/guestbin/swan-prep
 # confirm that the network is alive
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
+../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add replay
