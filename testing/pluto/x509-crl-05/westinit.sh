f3cd0205e38722475ccbbffc3773bf03c12de577
Andrew Cagney <cagney@gnu.org>
2016-04-21 13:33:35 -0400

    testing: use wait-until-alive in tests missing from TESTLIST

--- testing/pluto/x509-crl-05/westinit.sh
+++ testing/pluto/x509-crl-05/westinit.sh
@@ -1,7 +1,7 @@
 /testing/guestbin/swan-prep --x509 --revoked
 certutil -D -n east -d sql:/etc/ipsec.d
 # confirm that the network is alive
-ping -n -c 4 192.0.2.254
+../../pluto/bin/wait-until-alive 192.0.2.254
 # make sure that clear text does not get through
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 # confirm with a ping
