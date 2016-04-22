ccc4816f0187cc0ec5c5e3a62487c3b96a5875bb
Andrew Cagney <cagney@gnu.org>
2016-04-22 10:06:19 -0400

    kvm-test: fix "make kvm-test KVM_TESTS="something containing new lines"
    For instance:
    
    $ make kvm-test "KVM_TESTS=$(./testing/utils/kvmresults.py --quick testing/pluto | awk '/output-different/ { print $1 }' )"

--- mk/kvm-targets.mk
+++ mk/kvm-targets.mk
@@ -214,29 +214,35 @@ kvm-shutdown-%:
 # yet passed?  Since each alternative has merit, let the user decide.
 
 KVM_TESTS = testing/pluto
-KVM_TESTS2 = $(strip $(KVM_TESTS))
+
+# Given a make command like:
+#
+#     make kvm-test "KVM_TESTS=$(./testing/utils/kvmresults.py --quick testing/pluto | awk '/output-different/ { print $1 }' )"
+#
+# then KVM_TESTS ends up containing new lines, strip them out.
+STRIPPED_KVM_TESTS = $(strip $(KVM_TESTS))
 
 # "check" runs any test that has not yet passed (that is: failed,
 # incomplete, and not started).  This is probably the safest option.
 .PHONY: kvm-check kvm-check-good kvm-check-all
 kvm-check: kvm-check-good
 kvm-check-good: $(KVM_KEYS)
-	: KVM_TESTS = $(KVM_TESTS2)
-	$(KVMRUNNER) --retry  1 --test-result "good"     $(KVM_TESTS2)
+	: KVM_TESTS = $(STRIPPED_KVM_TESTS)
+	$(KVMRUNNER) --retry  1 --test-result "good"     $(STRIPPED_KVM_TESTS)
 kvm-check-all: $(KVM_KEYS)
-	: KVM_TESTS = $(KVM_TESTS2)
-	$(KVMRUNNER) --retry  1 --test-result "good|wip" $(KVM_TESTS2)
+	: KVM_TESTS = $(STRIPPED_KVM_TESTS)
+	$(KVMRUNNER) --retry  1 --test-result "good|wip" $(STRIPPED_KVM_TESTS)
 
 # "test" runs tests regardless.  It is best used with the KVM_TESTS
 # varible.
 .PHONY: kvm-test-good kvm-test-all
 kvm-test: kvm-test-good
 kvm-test: $(KVM_KEYS)
-	: KVM_TESTS = $(KVM_TESTS2)
-	$(KVMRUNNER) --retry -1 --test-result "good"     $(KVM_TESTS2)
+	: KVM_TESTS = $(STRIPPED_KVM_TESTS)
+	$(KVMRUNNER) --retry -1 --test-result "good"     $(STRIPPED_KVM_TESTS)
 kvm-test-all: $(KVM_KEYS)
-	: KVM_TESTS = $(KVM_TESTS2)
-	$(KVMRUNNER) --retry -1 --test-result "good|wip" $(KVM_TESTS2)
+	: KVM_TESTS = $(STRIPPED_KVM_TESTS)
+	$(KVMRUNNER) --retry -1 --test-result "good|wip" $(STRIPPED_KVM_TESTS)
 
 # clean up
 .PHONY: kvm-clean-check kvm-clean-test
