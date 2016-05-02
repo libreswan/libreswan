cb65dcedcd7981439d8de63b4b758d36891ad2bd
Paul Wouters <pwouters@redhat.com>
2016-05-01 20:00:49 -0400

    libcrypto: remove unused CBC_IMPL_BLK8() and CBC_DECL()

--- lib/libcrypto/include/cbc_generic.h
+++ lib/libcrypto/include/cbc_generic.h
@@ -74,57 +74,5 @@
 		} \
 		return ret; \
 	}
-#define CBC_IMPL_BLK8(name, ctx_type, addr_type,  enc_func, dec_func) \
-	int name(ctx_type * ctx, u_int8_t * in, u_int8_t * out, int ilen, \
-		 const u_int8_t * iv, int encrypt) { \
-		int ret = ilen, pos; \
-		const u_int32_t *iv_i; \
-		if ((ilen) % 8) \
-			return 0; \
-		if (encrypt) { \
-			pos = 0; \
-			while (pos < ilen) { \
-				if (pos == 0) \
-					iv_i = (const u_int32_t*) iv; \
-				else \
-					iv_i = (const u_int32_t*) (out - 8); \
-				*((u_int32_t *)(&out[ 0])) = iv_i[0] ^ \
-							     *((const u_int32_t \
-								*)(&in[ 0])); \
-				*((u_int32_t *)(&out[ 4])) = iv_i[1] ^ \
-							     *((const u_int32_t \
-								*)(&in[ 4])); \
-				enc_func(ctx, (addr_type)out, (addr_type)out); \
-				in += 8; \
-				out += 8; \
-				pos += 8; \
-			} \
-		} else { \
-			pos = ilen - 8; \
-			in += pos; \
-			out += pos; \
-			while (pos >= 0) { \
-				dec_func(ctx, (const addr_type)in, \
-					 (addr_type)out); \
-				if (pos == 0) \
-					iv_i = (const u_int32_t*) (iv); \
-				else \
-					iv_i = (const u_int32_t*) (in - 8); \
-				*((u_int32_t *)(&out[ 0])) ^= iv_i[0]; \
-				*((u_int32_t *)(&out[ 4])) ^= iv_i[1]; \
-				in -= 8; \
-				out -= 8; \
-				pos -= 8; \
-			} \
-		} \
-		return ret; \
-	}
-#define CBC_DECL(name, ctx_type) \
-	int name(ctx_type * ctx, u_int8_t * in, u_int8_t * out, int ilen, \
-		 const u_int8_t * iv, int encrypt)
-/*
-   Eg.:
-   CBC_IMPL_BLK16(AES_cbc_encrypt, aes_context, u_int8_t *, aes_encrypt, aes_decrypt);
-   CBC_DECL(AES_cbc_encrypt, aes_context);
- */
+
 #endif /* _CBC_GENERIC_H */
