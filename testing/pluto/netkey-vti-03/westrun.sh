58b02112f50da9b8579ef1258b46fd3e8585a3b1
Paul Wouters <pwouters@redhat.com>
2016-05-05 16:59:31 -0400

    WIP: fixup

--- testing/pluto/netkey-vti-03/westrun.sh
+++ testing/pluto/netkey-vti-03/westrun.sh
@@ -1,10 +1,9 @@
 ipsec auto --up  westnet-eastnet-vti-01
 ipsec auto --up  westnet-eastnet-vti-02
-ip tunnel add name ipsec0 mode vti local 192.1.2.45 remote 0.0.0.0 ikey 2 okey 1  dev eth0
-#ip tunnel add name ipsec0 mode vti local 192.1.2.45 remote 0.0.0.0 ikey 2 okey 1  dev eth1
+ip tunnel add name ipsec0 mode vti local 192.1.2.45 remote 0.0.0.0 ikey 2 okey 1 
 ip link set ipsec0 up
 sysctl -w net.ipv4.conf.ipsec0.disable_policy=1
-sysctl -w net.ipv4.conf.ipsec0.disable_xfrm=1
+#sysctl -w net.ipv4.conf.ipsec0.disable_xfrm=1
 sysctl -w net.ipv4.conf.ipsec0.rp_filter=0
 sysctl -w net.ipv4.conf.ipsec0.forwarding=1
 #ip addr add 192.0.1.0/24 remote 192.0.2.0/24 dev ipsec0
