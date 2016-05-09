c4e10b79fd534b694a561cc7a861cab5ef9e2e3f
Paul Wouters <pwouters@redhat.com>
2016-05-09 14:12:05 -0400

    testing: convert and add netkey-psk-pluto-06 to kvm

--- testing/pluto/netkey-psk-pluto-06/eastinit.sh
+++ testing/pluto/netkey-psk-pluto-06/eastinit.sh
@@ -1,13 +1,5 @@
-: ==== start ====
-TESTNAME=netkey-psk-pluto-06
-/testing/guestbin/swan-prep --testname $TESTNAME
-
-ipsec setup stop
-/usr/local/libexec/ipsec/_stackmanager stop
-rm -fr /var/run/pluto/pluto.pid
+/testing/guestbin/swan-prep
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-
 ipsec auto --add road--eastnet-psk
-ipsec auto --status
 echo "initdone"
