libreswan/libreswan

commit e3ce63f7b241f1f597a10293352b6802a831ba41

pluto: straighten out some confusion about attribute names and labeled IPSec

lib/libswan/constants.c
@@ -850,12 +850,11 @@ static const char *const ipsec_attr_name[] = {
 	"COMPRESS_DICT_SIZE",
 	"COMPRESS_PRIVATE_ALG",
 #ifdef HAVE_LABELED_IPSEC
-	"ECN_TUNNEL",
+	"ECN_TUNNEL or old SECCTX",
 #endif
 };
 
-#if 0
-/* this is not used - which is a little strange */
+/* these are the only attributes with which we support variable length values */
 static const char *const ipsec_var_attr_name[] = {
 	"SA_LIFE_DURATION (variable length)",
 	NULL,
@@ -869,7 +868,6 @@ static const char *const ipsec_var_attr_name[] = {
 	"NULL", /* ECN TUNNEL */
 #endif
 };
-#endif
 
 #ifdef HAVE_LABELED_IPSEC
 static const char *const ipsec_private_attr_name[] = {
@@ -894,11 +892,11 @@ static enum_names ipsec_private_attr_names = {
 static enum_names ipsec_attr_desc_tv = {
 	SA_LIFE_TYPE + ISAKMP_ATTR_AF_TV,
 #ifdef HAVE_LABELED_IPSEC
-	ECN_TUNNEL + ISAKMP_ATTR_AF_TV,
+	ECN_TUNNEL_or_old_SECCTX + ISAKMP_ATTR_AF_TV,
 #else
 	COMPRESS_PRIVATE_ALG + ISAKMP_ATTR_AF_TV,
 #endif
-	ipsec_attr_name,
+	ipsec_var_attr_name,
 #ifdef HAVE_LABELED_IPSEC
 	&ipsec_private_attr_names
 #else
@@ -907,21 +905,13 @@ static enum_names ipsec_attr_desc_tv = {
 };
 
 enum_names ipsec_attr_names = {
-#ifdef HAVE_LABELED_IPSEC
 	SA_LIFE_TYPE,
-#else
-	SA_LIFE_DURATION,
-#endif
 #ifdef HAVE_LABELED_IPSEC
-	ECN_TUNNEL,
+	ECN_TUNNEL_or_old_SECCTX,
 #else
 	COMPRESS_PRIVATE_ALG,
 #endif
-#ifdef HAVE_LABELED_IPSEC
 	ipsec_attr_name,
-#else
-	ipsec_var_attr_name,
-#endif
 	&ipsec_attr_desc_tv
 };
 
@@ -938,7 +928,7 @@ enum_names *const ipsec_attr_val_descs[] = {
 	NULL,	/* COMPRESS_DICT_SIZE */
 	NULL,	/* COMPRESS_PRIVATE_ALG */
 #ifdef HAVE_LABELED_IPSEC
-	NULL,	/* ECN_TUNNEL */
+	NULL,	/* ECN_TUNNEL_or_old_SECCTX */
 #endif
 };
 

include/ietf_constants.h
@@ -976,6 +976,7 @@ enum ikev1_ipsec_attr {
 	COMPRESS_DICT_SIZE = 8,
 	COMPRESS_PRIVATE_ALG = 9, /* B/V */
 	ECN_TUNNEL = 10, /*B*/ /*RFC 3168*/ /* Originally mistakenly grabbed for SECCTX */
+	ECN_TUNNEL_or_old_SECCTX = ECN_TUNNEL,
 	SECCTX = 32001, /* B/V */ /* chosen from private range as in RFC 2407 */
 };
 

programs/pluto/plutomain.c
@@ -823,7 +823,7 @@ int main(int argc, char **argv)
 			ugh = ttoulb(optarg, 0, 0, 0xFFFF, &u);
 			if (ugh != NULL)
 				break;
-			if (u != SECCTX && u != 10) {
+			if (u != SECCTX && u != ECN_TUNNEL_or_old_SECCTX) {
 				ugh = "must be a positive 32001 (default) or 10 (for backward compatibility)";
 				break;
 			}

commit bbb9daea1cfd92b99fed1dfbf4c9f8ebc5791cc6

pluto: fix bug introduced in e3ce63f7b241f1f597a10293352b6802a831ba41

lib/libswan/constants.c
@@ -896,7 +896,7 @@ static enum_names ipsec_attr_desc_tv = {
 #else
 	COMPRESS_PRIVATE_ALG + ISAKMP_ATTR_AF_TV,
 #endif
-	ipsec_var_attr_name,
+	ipsec_attr_name,
 #ifdef HAVE_LABELED_IPSEC
 	&ipsec_private_attr_names
 #else
@@ -911,7 +911,7 @@ enum_names ipsec_attr_names = {
 #else
 	COMPRESS_PRIVATE_ALG,
 #endif
-	ipsec_attr_name,
+	ipsec_var_attr_name,
 	&ipsec_attr_desc_tv
 };
 