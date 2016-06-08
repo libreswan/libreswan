ff7419bdca194b530837dc37c9070d21a981799f
Antony Antony <antony@phenome.org>
2016-06-08 16:13:31 +0200

    testing: ikev2-51-rw-nat-ikeport newoe-25-cat-5

---
+++ testing/pluto/newoe-25-cat-5/eastinit.sh
@@ -0,0 +1,9 @@
+/testing/guestbin/swan-prep
+cp policies/* /etc/ipsec.d/policies/
+echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
+cp ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+# give OE policies time to load
+sleep 2
+echo "initdone"
