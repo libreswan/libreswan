1a16b91e5b0c69b98903f6df9591e506ed0287c3
Andrew Cagney <cagney@gnu.org>
2016-06-10 23:36:48 -0400

    testing: another attempt at fixing --x509 --nsspw and --fips
    Wasn't setting the password when x509.
    Was setting the wrong password when FIPS.
    Was allowing the wrong password when FIPS.
    
    fips-09-ikev2-gcm - --fips - tweaked to pass
    ikev2-04-basic-x509 - --x509 - passes unmodified
    ikev2-04-basic-x509-nsspw - --x509 -nsspw - added and passes
    
    Other FIPS tests appear to "pass" but their output is out-of-date
    and needs careful review.

---
+++ testing/pluto/ikev2-04-basic-x509-nsspw/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep --x509 --nsspw
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-ikev2
+ipsec auto --status | grep westnet-eastnet-ikev2
+echo "initdone"
