fe744305a133c62e86f013a74d236ac11f73d930
Andrew Cagney <cagney@gnu.org>
2016-06-14 20:58:31 -0400

    rsasigkey: switch to lsw_nss_get_password et.al.
    Fixes --fips an --nsspw cases when there's an nsspassword.
    Performs early check on DB accessable.
    
    Deletes a bunch of cloned code.

--- programs/rsasigkey/rsasigkey.c
+++ programs/rsasigkey/rsasigkey.c
@@ -78,7 +78,6 @@
 #define E       3               /* standard public exponent */
 /* #define F4	65537 */	/* possible future public exponent, Fermat's 4th number */
 
-char *progname;
 char usage[] =
 	"rsasigkey [--verbose] [--seeddev <device>] [--configdir <dir>] [--password <password>] [--hostname host] [--seedbits bits] [<keybits>]";
 struct option opts[] = {
@@ -97,18 +96,16 @@ struct option opts[] = {
 	{ "seedbits", 1,      NULL,   's' },
 	{ 0,           0,      NULL,   0, }
 };
-int verbose = 0;                /* narrate the action? */
 char *device = DEVICE;          /* where to get randomness */
 int nrounds = 30;               /* rounds of prime checking; 25 is good */
 char outputhostname[NS_MAXDNAME];  /* hostname for output */
 
-char me[] = "ipsec rsasigkey";  /* for messages */
+char *progname = "ipsec rsasigkey";  /* for messages */
 
 /* forwards */
-void rsasigkey(int nbits, int seedbits, char *configdir, char *password);
+void rsasigkey(int nbits, int seedbits, const struct lsw_conf_options *oco);
 void getrandom(size_t nbytes, unsigned char *buf);
 static const char *conv(const unsigned char *bits, size_t nbytes, int format);
-void report(char *msg);
 
 /*
  * bundle - bundle e and n into an RFC2537-format chunk_t
@@ -132,7 +129,7 @@ static char *base64_bundle(int e, chunk_t modulus)
 	err_t err = rsa_pubkey_to_base64(exponent, modulus, &bundle);
 	if (err) {
 		fprintf(stderr, "%s: can't-happen bundle convert error `%s'\n",
-			me, err);
+			progname, err);
 		exit(1);
 	}
 
@@ -153,133 +150,6 @@ static void UpdateNSS_RNG(int seedbits)
 	pfree(buf);
 }
 
-/*  Returns the password passed in in the text file.
- *  Uses the password once and nulls it out to prevent
- *  PKCS11 from calling us forever.
- */
-static char *GetFilePasswd(PK11SlotInfo *slot, PRBool retry, void *arg)
-{
-	char *phrases, *phrase;
-	PRFileDesc *fd;
-	PRInt32 nb;
-	const char *pwFile = (const char *)arg;
-	int i;
-	const long maxPwdFileSize = 4096;
-	char *tokenName = NULL;
-	int tokenLen = 0;
-
-	if (pwFile == NULL)
-		return NULL;
-
-	if (retry)
-		return NULL;  /* no good retrying - the files contents will be the same */
-
-	phrases = PORT_ZAlloc(maxPwdFileSize);
-
-	if (phrases == NULL)
-		return NULL; /* out of memory */
-
-	fd = PR_Open(pwFile, PR_RDONLY, 0);
-	if (fd == NULL) {
-		fprintf(stderr, "%s: No password file \"%s\" exists.\n", me, pwFile);
-		PORT_Free(phrases);
-		return NULL;
-	}
-	nb = PR_Read(fd, phrases, maxPwdFileSize);
-
-	PR_Close(fd);
-
-	if (nb == 0) {
-		fprintf(stderr, "%s: password file contains no data\n", me);
-		PORT_Free(phrases);
-		return NULL;
-	}
-
-	if (slot != NULL) {
-		tokenName = PK11_GetTokenName(slot);
-		if (tokenName != NULL)
-			tokenLen = PORT_Strlen(tokenName);
-	}
-	i = 0;
-	do {
-		int startphrase = i;
-		int phraseLen;
-		/* handle the Windows EOL case */
-		while (phrases[i] != '\r' && phrases[i] != '\n' && i < nb)
-			i++;
-		/* terminate passphrase */
-		phrases[i++] = '\0';
-		/* clean up any EOL before the start of the next passphrase */
-		while ( (i < nb) && (phrases[i] == '\r' || phrases[i] == '\n'))
-			phrases[i++] = '\0';
-		/* now analyze the current passphrase */
-		phrase = &phrases[startphrase];
-		if (tokenName == NULL)
-			break;
-		if (PORT_Strncmp(phrase, tokenName, tokenLen))
-			continue;
-		phraseLen = PORT_Strlen(phrase);
-		if (phraseLen < (tokenLen + 1))
-			continue;
-		if (phrase[tokenLen] != ':')
-			continue;
-		phrase = &phrase[tokenLen + 1];
-		break;
-	} while (i < nb);
-
-	phrase = PORT_Strdup((char*)phrase);
-	PORT_Free(phrases);
-	return phrase;
-}
-
-static char *GetModulePassword(PK11SlotInfo *slot, PRBool retry, void *arg)
-{
-	secuPWData *pwdata = (secuPWData *)arg;
-	secuPWData pwnull = { PW_NONE, 0 };
-	char *pw;
-
-	if (pwdata == NULL)
-		pwdata = &pwnull;
-
-	if (PK11_ProtectedAuthenticationPath(slot)) {
-		/*
-		 * Anyone know what this case is?
-		 */
-		fprintf(stderr, "%s: protected authentication paths not supported\n",
-			me);
-		return NULL;
-	}
-	if (retry && pwdata->source != PW_NONE) {
-		fprintf(stderr, "%s: Incorrect password/PIN entered.\n", me);
-		return NULL;
-	}
-
-	switch (pwdata->source) {
-	case PW_FROMFILE:
-		/* Instead of opening and closing the file every time, get the pw
-		 * once, then keep it in memory (duh).
-		 */
-		pw = GetFilePasswd(slot, retry, pwdata->data);
-		pwdata->source = PW_PLAINTEXT;
-		pwdata->data = strdup(pw);
-		/* it's already been dup'ed */
-		return pw;
-
-	case PW_PLAINTEXT:
-		return strdup(pwdata->data);
-
-	default: /* cases PW_NONE not supported */
-		fprintf(stderr,
-			"%s: Unknown or unsupported case in GetModulePassword\n",
-			me);
-		break;
-	}
-
-	fprintf(stderr, "%s: Password check failed:  No password found.\n",
-		me);
-	return NULL;
-}
-
 /*
    - main - mostly argument parsing
  */
@@ -289,23 +159,24 @@ int main(int argc, char *argv[])
 	int opt;
 	int nbits = 0;
 	int seedbits = DEFAULT_SEED_BITS;
-	char *configdir = oco->confddir; /* where the NSS databases reside */
-	char *password = NULL;  /* password for token authentication */
+
+	log_to_stderr = FALSE;
+	tool_init_log();
 
 	while ((opt = getopt_long(argc, argv, "", opts, NULL)) != EOF)
 		switch (opt) {
 		case 'n':
 		case 'p':
 			fprintf(stderr, "%s: --noopt and --rounds options have been obsoleted - ignored\n",
-				me);
+				progname);
 			break;
 		case 'v':       /* verbose description */
-			verbose = 1;
+			log_to_stderr = TRUE;
 			break;
 
 		case 'r':
 			fprintf(stderr, "%s: Warning: --random is obsoleted for --seeddev. It no longer specifies the random device used for obtaining random key material",
-				me);
+				progname);
 			/* FALLTHROUGH */
 		case 'S':       /* nonstandard random device for seed */
 			device = optarg;
@@ -326,22 +197,22 @@ int main(int argc, char *argv[])
 			exit(0);
 			break;
 		case 'V':       /* version */
-			printf("%s %s\n", me, ipsec_version_code());
+			printf("%s %s\n", progname, ipsec_version_code());
 			exit(0);
 			break;
 		case 'c':       /* nss configuration directory */
 		case 'd':       /* -d is used for configdir with nss tools */
-			configdir = optarg;
+			lsw_conf_confddir(optarg);
 			break;
 		case 'P':       /* token authentication password */
-			password = optarg;
+			lsw_conf_nsspassword(optarg);
 			break;
 		case 's': /* seed bits */
 			seedbits = atoi(optarg);
 			if (PK11_IsFIPS()) {
 				if (seedbits < DEFAULT_SEED_BITS) {
 					fprintf(stderr, "%s: FIPS mode does not allow < %d seed bits\n",
-						me, DEFAULT_SEED_BITS);
+						progname, DEFAULT_SEED_BITS);
 					exit(1);
 				}
 			}
@@ -355,7 +226,7 @@ int main(int argc, char *argv[])
 	if (outputhostname[0] == '\0') {
 		if (gethostname(outputhostname, sizeof(outputhostname)) < 0) {
 			fprintf(stderr, "%s: gethostname failed (%s)\n",
-				me,
+				progname,
 				strerror(errno));
 			exit(1);
 		}
@@ -371,7 +242,7 @@ int main(int argc, char *argv[])
 
 		if (ugh != NULL) {
 			fprintf(stderr, "%s: keysize specification is malformed: %s\n",
-				me, ugh);
+				progname, ugh);
 			exit(1);
 		}
 		nbits = u;
@@ -379,19 +250,19 @@ int main(int argc, char *argv[])
 
 	if (nbits < MIN_KEYBIT ) {
 		fprintf(stderr, "%s: requested RSA key size of %d is too small - use %d or more\n",
-			me, nbits, MIN_KEYBIT);
+			progname, nbits, MIN_KEYBIT);
 		exit(1);
 	} else if (nbits > MAXBITS) {
-		fprintf(stderr, "%s: overlarge bit count (max %d)\n", me,
+		fprintf(stderr, "%s: overlarge bit count (max %d)\n", progname,
 			MAXBITS);
 		exit(1);
 	} else if (nbits % (BITS_PER_BYTE * 2) != 0) {
-		fprintf(stderr, "%s: bit count (%d) not multiple of %d\n", me,
+		fprintf(stderr, "%s: bit count (%d) not multiple of %d\n", progname,
 			nbits, (int)BITS_PER_BYTE * 2);
 		exit(1);
 	}
 
-	rsasigkey(nbits, seedbits, configdir, password);
+	rsasigkey(nbits, seedbits, oco);
 	exit(0);
 }
 
@@ -403,37 +274,17 @@ int main(int argc, char *argv[])
  * real speed advantages.
  * See also: https://www.imperialviolet.org/2012/03/16/rsae.html
  */
-void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
+void rsasigkey(int nbits, int seedbits, const struct lsw_conf_options *oco)
 {
-	SECStatus rv;
 	PK11RSAGenParams rsaparams = { nbits, (long) E };
-	secuPWData pwdata = { PW_NONE, NULL };
 	PK11SlotInfo *slot = NULL;
 	SECKEYPrivateKey *privkey = NULL;
 	SECKEYPublicKey *pubkey = NULL;
 	realtime_t now = realnow();
 
-	if (password == NULL) {
-		pwdata.source = PW_NONE;
-	} else {
-		/* check if passwd == configdir/nsspassword */
-		size_t cdl = strlen(configdir);
-		size_t pwl = strlen(password);
-		static const char suf[] = "/nsspassword";
-
-		if (pwl == cdl + sizeof(suf) - 1 &&
-			memeq(password, configdir, cdl) &&
-			memeq(password + cdl, suf, sizeof(suf)))
-			pwdata.source = PW_FROMFILE;
-		else
-			pwdata.source = PW_PLAINTEXT;
-	}
-	pwdata.data = password;
-
 	lsw_nss_buf_t err;
-	if (!lsw_nss_setup(configdir, LSW_NSS_SKIP_PR_CLEANUP|LSW_NSS_SKIP_AUTH,
-			   GetModulePassword, err)) {
-		fprintf(stderr, "%s: %s\n", me, err);
+	if (!lsw_nss_setup(oco->confddir, 0, lsw_nss_get_password, err)) {
+		fprintf(stderr, "%s: %s\n", progname, err);
 		exit(1);
 	}
 
@@ -445,52 +296,26 @@ void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
 	}
 #endif
 
-	if (PK11_IsFIPS() && password == NULL) {
-		fprintf(stderr,
-			"%s: On FIPS mode a password is required\n",
-			me);
+	/* Good for now but someone may want to use a hardware token */
+	slot = lsw_nss_get_authenticated_slot(err);
+	if (slot == NULL) {
+		fprintf(stderr, "%s: %s\n", progname, err);
+		lsw_nss_shutdown();
 		exit(1);
 	}
 
-	/* Good for now but someone may want to use a hardware token */
-	slot = PK11_GetInternalKeySlot();
-	/* In which case this may be better */
-	/* slot = PK11_GetBestSlot(CKM_RSA_PKCS_KEY_PAIR_GEN, password ? &pwdata : NULL); */
-	/* or the user may specify the name of a token. */
-
-#if 0
-	if (PK11_IsFIPS() || !PK11_IsInternal(slot)) {
-		rv = PK11_Authenticate(slot, PR_FALSE, &pwdata);
-		if (rv != SECSuccess) {
-			fprintf(stderr, "%s: could not authenticate to token '%s'\n",
-				me, PK11_GetTokenName(slot));
-			return;
-		}
-	}
-#endif /* 0 */
-
 	/* Do some random-number initialization. */
 	UpdateNSS_RNG(seedbits);
-	/* Log in to the token */
-	if (password != NULL) {
-		rv = PK11_Authenticate(slot, PR_FALSE, &pwdata);
-		if (rv != SECSuccess) {
-			fprintf(stderr,
-				"%s: could not authenticate to token '%s'\n",
-				me, PK11_GetTokenName(slot));
-			return;
-		}
-	}
 	privkey = PK11_GenerateKeyPair(slot,
 				       CKM_RSA_PKCS_KEY_PAIR_GEN,
 				       &rsaparams, &pubkey,
 				       PR_TRUE,
-				       password != NULL? PR_TRUE : PR_FALSE,
-				       &pwdata);
+				       PK11_IsFIPS() ? PR_TRUE : PR_FALSE,
+				       lsw_return_nss_password_file_info());
 	/* inTheToken, isSensitive, passwordCallbackFunction */
 	if (privkey == NULL) {
 		fprintf(stderr,
-			"%s: key pair generation failed: \"%d\"\n", me,
+			"%s: key pair generation failed: \"%d\"\n", progname,
 			PORT_GetError());
 		return;
 	}
@@ -508,7 +333,7 @@ void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
 	{
 		SECItem *ckaid = PK11_GetLowLevelKeyIDForPrivateKey(privkey);
 		if (ckaid == NULL) {
-			fprintf(stderr, "%s: 'CKAID' calculation failed\n", me);
+			fprintf(stderr, "%s: 'CKAID' calculation failed\n", progname);
 			exit(1);
 		}
 		hex_ckaid = strdup(conv(ckaid->data, ckaid->len, 16));
@@ -521,7 +346,7 @@ void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
 		hex_ckaid);
 
 	/* and the output */
-	report("output...\n");  /* deliberate extra newline */
+	libreswan_log("output...\n");  /* deliberate extra newline */
 	printf("\t# RSA %d bits   %s   %s", nbits, outputhostname,
 		ctime(&now.real_secs));
 	/* ctime provides \n */
@@ -561,26 +386,23 @@ void getrandom(size_t nbytes, unsigned char *buf)
 
 	dev = open(device, 0);
 	if (dev < 0) {
-		fprintf(stderr, "%s: could not open %s (%s)\n", me,
+		fprintf(stderr, "%s: could not open %s (%s)\n", progname,
 			device, strerror(errno));
 		exit(1);
 	}
 
 	ndone = 0;
-	if (verbose) {
-		fprintf(stderr, "getting %d random seed bytes for NSS from %s...\n",
-			(int) nbytes * BITS_PER_BYTE,
-			device);
-	}
+	libreswan_log("getting %d random seed bytes for NSS from %s...\n",
+		      (int) nbytes * BITS_PER_BYTE, device);
 	while (ndone < nbytes) {
 		got = read(dev, buf + ndone, nbytes - ndone);
 		if (got < 0) {
-			fprintf(stderr, "%s: read error on %s (%s)\n", me,
+			fprintf(stderr, "%s: read error on %s (%s)\n", progname,
 				device, strerror(errno));
 			exit(1);
 		}
 		if (got == 0) {
-			fprintf(stderr, "%s: eof on %s!?!\n", me, device);
+			fprintf(stderr, "%s: eof on %s!?!\n", progname, device);
 			exit(1);
 		}
 		ndone += got;
@@ -600,26 +422,14 @@ static const char *conv(const unsigned char *bits, size_t nbytes, int format)
 
 	n = datatot(bits, nbytes, format, convbuf, sizeof(convbuf));
 	if (n == 0) {
-		fprintf(stderr, "%s: can't-happen convert error\n", me);
+		fprintf(stderr, "%s: can't-happen convert error\n", progname);
 		exit(1);
 	}
 	if (n > sizeof(convbuf)) {
 		fprintf(stderr,
 			"%s: can't-happen convert overflow (need %d)\n",
-			me, (int) n);
+			progname, (int) n);
 		exit(1);
 	}
 	return convbuf;
-}
-
-/*
-   - report - report progress, if indicated
- */
-void report(msg)
-char *msg;
-{
-	if (!verbose)
-		return;
-
-	fprintf(stderr, "%s\n", msg);
 }
