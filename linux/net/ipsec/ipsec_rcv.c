755f26b40c6b8efeb8ffe027910b91da2b53a9d9
D. Hugh Redelmeier <hugh@mimosa.com>
2016-06-20 17:09:29 -0400

    ipsec_rcv.c: improve readability of ipsec_rcv_auth_decap

--- linux/net/ipsec/ipsec_rcv.c
+++ linux/net/ipsec/ipsec_rcv.c
@@ -1500,22 +1500,21 @@ static enum ipsec_rcv_value ipsec_rcv_auth_decap(struct ipsec_rcv_state *irs)
 		return IPSEC_RCV_BADLEN;
 	}
 
-	if (irs->authfuncs ||
+	if (irs->authfuncs
 #ifdef CONFIG_KLIPS_OCF
-	    irs->ipsp->ocf_in_use ||
+	    || irs->ipsp->ocf_in_use
 #endif
 #ifdef CONFIG_KLIPS_ALG
-	    irs->ixt_a ||
+	    || irs->ixt_a
 #endif
-	    0) {
+	    ) {
 		if (irs->proto_funcs->rcv_setup_auth) {
-			return (*irs->proto_funcs->rcv_setup_auth)(irs,
-								   irs->skb,
-								   &irs->replay,
-								   &irs->
-								   authenticator);
+			return (*irs->proto_funcs->rcv_setup_auth)(
+					irs,
+					irs->skb,
+					&irs->replay,
+					&irs->authenticator);
 		}
-
 	}
 	return IPSEC_RCV_OK;
 }
