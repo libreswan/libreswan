libreswan/libreswan

commit 36955a9d57245d2d46f1b3da2f5bc9153d2989e2

pluto fetch.c: tidy
- use FOR statement where clearer (hightlights the induction variable
  and brings the control of it to one place)

- simplify by eliminating variables

- improve comments

- reduce scope of variables

- combine declaration with first assignment where simpler.
  This makes it evidently impossible to refer to it before initialization.

- remove assignments embedded in tests

- locking now matches the static structure of the program

- eliminate some deep returns

programs/pluto/fetch.c
@@ -519,52 +519,48 @@ void free_crl_fetch(void)
 }
 
 /*
- * add additional distribution points
+ * Add additional distribution points.
+ * Note: clones anything it needs to keep.
  */
 void add_distribution_points(generalName_t *newPoints,
 			     generalName_t **distributionPoints)
 {
-	while (newPoints != NULL) {
-		bool add = TRUE;
-		generalName_t *gn = *distributionPoints;
+	for (; newPoints != NULL; newPoints = newPoints->next) {
+		generalName_t *gn;
 
-		while (gn != NULL) {
+		for (gn = *distributionPoints; ; gn = gn->next) {
+			if (gn == NULL) {
+				/*
+				 * End of list; not found.
+				 * Clone additional distribution point.
+				 */
+				generalName_t *ngn = clone_thing(*newPoints, "generalName");
+				clonetochunk(ngn->name, newPoints->name.ptr,
+					     newPoints->name.len,
+					     "crl uri");
+
+				/* insert additional CRL distribution point */
+				ngn->next = *distributionPoints;
+				*distributionPoints = ngn;
+				break;
+			}
 			if (gn->kind == newPoints->kind &&
 			    gn->name.len == newPoints->name.len &&
 			    memeq(gn->name.ptr, newPoints->name.ptr,
 				   gn->name.len)) {
-				/* distribution point already present, skip to next entry */
-				add = FALSE;
+				/* newPoint already present */
 				break;
 			}
-			gn = gn->next;
 		}
-
-		if (add) {
-			/* clone additional distribution point */
-			gn = clone_thing(*newPoints, "generalName");
-			clonetochunk(gn->name, newPoints->name.ptr,
-				     newPoints->name.len,
-				     "crl uri");
-
-			/* insert additional CRL distribution point */
-			gn->next = *distributionPoints;
-			*distributionPoints = gn;
-		}
-		newPoints = newPoints->next;
 	}
 }
 
 /*
- * add a crl fetch request to the chained list
+ * Add a crl fetch request to the chained list.
+ * Note: clones anything that needs to persist.
  */
 void add_crl_fetch_request_nss(SECItem *issuer_dn, generalName_t *end_dp)
 {
-	CERTCertificate *ca = NULL;
-	generalName_t *new_dp = NULL;
-	fetch_req_t *req;
-	chunk_t idn;
-
 	DBG(DBG_CONTROL, DBG_log("attempting to add a new CRL fetch request"));
 
 	if (issuer_dn == NULL || issuer_dn->data == NULL ||
@@ -574,99 +570,102 @@ void add_crl_fetch_request_nss(SECItem *issuer_dn, generalName_t *end_dp)
 		return;
 	}
 
-	if ((ca = CERT_FindCertByName(CERT_GetDefaultCertDB(),
-							issuer_dn)) == NULL) {
+	CERTCertificate *ca = CERT_FindCertByName(CERT_GetDefaultCertDB(),
+						issuer_dn);
+	if (ca == NULL) {
 		DBG_log("no CA cert found to add fetch request: [%d]",
 							       PORT_GetError());
 		return;
 	}
 
-	idn = secitem_to_chunk(*issuer_dn);
+	chunk_t idn = secitem_to_chunk(*issuer_dn);
 
+	/* LOCK: matching unlock is at end of loop -- must be executed */
 	lock_crl_fetch_list("add_crl_fetch_request");
-	req = crl_fetch_reqs;
 
-	while (req != NULL) {
+	fetch_req_t *req;
+
+	for (req = crl_fetch_reqs; ; req = req->next) {
+		if (req == NULL) {
+			/* end of list; no match found */
+
+			generalName_t *new_dp = gndp_from_nss_cert(ca);
+
+			if (new_dp == NULL) {
+				if (end_dp == NULL) {
+					DBG(DBG_CONTROL,
+						DBG_log("no distribution point available for new fetch request"));
+					break;
+				}
+				DBG(DBG_CONTROL,
+					DBG_log("no CA crl DP available; using provided DP"));
+				new_dp = end_dp;
+			}
+
+			/* create a new fetch request */
+			fetch_req_t *nr = alloc_thing(fetch_req_t, "fetch request");
+
+			*nr = empty_fetch_req;
+
+			/* note current time */
+			nr->installed = realnow();
+
+			/* clone issuer */
+			clonetochunk(nr->issuer, idn.ptr, idn.len, "issuer dn");
+
+			/* copy distribution points */
+			add_distribution_points(new_dp, &nr->distributionPoints);
+
+			/* insert new fetch request at the head of the queue */
+			nr->next = crl_fetch_reqs;
+			crl_fetch_reqs = nr;
+
+			DBG(DBG_CONTROL,
+			    DBG_log("crl fetch request added"));
+			break;
+		}
 		if (same_dn(idn, req->issuer)) {
 			/* there is already a fetch request */
 			DBG(DBG_CONTROL,
 			    DBG_log("crl fetch request already exists"));
 
 			/* there might be new distribution points */
-			if ((new_dp = gndp_from_nss_cert(ca)) != NULL) {
-				DBG(DBG_CONTROL,
-				    DBG_log("new distribution point available"));
-				add_distribution_points(new_dp,
-						      &req->distributionPoints);
-			} else if (end_dp != NULL) {
-				DBG(DBG_CONTROL,
-					DBG_log("no CA crl DP available, adding provided DP"));
-				add_distribution_points(end_dp, &req->distributionPoints);
-			}
-			unlock_crl_fetch_list("add_crl_fetch_request");
-			if (ca != NULL)
-				CERT_DestroyCertificate(ca);
-			return;
-		}
-		req = req->next;
-	}
+			generalName_t *new_dp = gndp_from_nss_cert(ca);
 
-	if (new_dp == NULL) {
-		if ((new_dp = gndp_from_nss_cert(ca)) == NULL) {
-			if (end_dp != NULL) {
+			if (new_dp == NULL) {
+				if (end_dp == NULL) {
+					DBG(DBG_CONTROL,
+						DBG_log("no CA crl DP available"));
+					break;
+				}
 				DBG(DBG_CONTROL,
-					DBG_log("no CA crl DP available, using provided DP"));
+				    DBG_log("no CA crl DP available; using provided DP"));
 				new_dp = end_dp;
-			} else {
-				DBG(DBG_CONTROL,
-					DBG_log("no distribution point available for new fetch request"));
-				unlock_crl_fetch_list("add_crl_fetch_request");
-				if (ca != NULL)
-					CERT_DestroyCertificate(ca);
-				return;
 			}
+
+			add_distribution_points(end_dp, &req->distributionPoints);
+			DBG(DBG_CONTROL,
+			    DBG_log("crl fetch request augmented"));
+			break;
 		}
 	}
-
-	/* create a new fetch request */
-	req = alloc_thing(fetch_req_t, "fetch request");
-	*req = empty_fetch_req;
-
-	/* note current time */
-	req->installed = realnow();
-
-	/* clone issuer */
-	clonetochunk(req->issuer, idn.ptr, idn.len, "issuer dn");
-
-	/* copy distribution points */
-	add_distribution_points(new_dp, &req->distributionPoints);
-
-	/* insert new fetch request at the head of the queue */
-	req->next = crl_fetch_reqs;
-	crl_fetch_reqs = req;
-
-	DBG(DBG_CONTROL,
-	    DBG_log("crl fetch request added"));
+	/* UNLOCK: matching lock is before loop */
 	unlock_crl_fetch_list("add_crl_fetch_request");
 
-	if (ca != NULL)
-		CERT_DestroyCertificate(ca);
-
+	CERT_DestroyCertificate(ca);
 }
 
 /*
  * list all distribution points
  */
-void list_distribution_points(const generalName_t *gn)
+void list_distribution_points(const generalName_t *first_gn)
 {
-	bool first_gn = TRUE;
-
-	while (gn != NULL) {
-		whack_log(RC_COMMENT, "       %s '%.*s'", (first_gn) ? "distPts:" :
-			  "        ", (int)gn->name.len,
-			  gn->name.ptr);
-		first_gn = FALSE;
-		gn = gn->next;
+	const generalName_t *gn;
+	for (gn = first_gn; gn != NULL; gn = gn->next) {
+		whack_log(RC_COMMENT, "       %s '%.*s'",
+			gn == first_gn ? "distPts:" : "        ",
+			(int)gn->name.len,
+			gn->name.ptr);
 	}
 }
 
@@ -701,6 +700,6 @@ void list_crl_fetch_requests(bool utc)
 	unlock_crl_fetch_list("list_crl_fetch_requests");
 }
 
-#else
+#else /* defined(LIBCURL) || defined(LDAP_VER) */
 /* we'll just ignore for now - this is all going away anyway */
 #endif

commit 14886f8f188f0c40408f60a7e77afddf1e94cfbf

pluto: changes inspired by scan-build (clang diagnostics)
- fix add_crl_fetch_request_nss
  (broken in 36955a9d57245d2d46f1b3da2f5bc9153d2989e2)

- add a missing guard to handle NULL case

- elminate redundant and confusing-to-clang test for NULL

- fix spelling (not caught by clang, but DHR's gaze directed there)

- replace improper use of snprintf result (it is length of
  output if there had been an sufficient buffr)

- add comment about newly unearthed mystery

programs/pluto/fetch.c
@@ -644,7 +644,7 @@ void add_crl_fetch_request_nss(SECItem *issuer_dn, generalName_t *end_dp)
 				new_dp = end_dp;
 			}
 
-			add_distribution_points(end_dp, &req->distributionPoints);
+			add_distribution_points(new_dp, &req->distributionPoints);
 			DBG(DBG_CONTROL,
 			    DBG_log("crl fetch request augmented"));
 			break;

programs/pluto/ikev1.c
@@ -2475,6 +2475,7 @@ void complete_v1_state_transition(struct msg_digest **mdp, stf_status result)
 				pexpect(s != STF_FAIL);
 				if (s == STF_FAIL)
 					result = STF_FAIL; /* ??? fall through !?! */
+				/* ??? result not subsequently used */
 			}
 		}
 

programs/pluto/initiate.c
@@ -882,7 +882,7 @@ static void initiate_ondemand_body(struct find_oppo_bundle *b,
 				}
 			}
 
-			if (c != NULL && ((c->policy & POLICY_RSASIG) == LEMPTY)) {
+			if ((c->policy & POLICY_RSASIG) == LEMPTY) {
 				ipstr_buf b1;
 
 				/* no dns queries to find the gateway. create one here */
@@ -1076,7 +1076,7 @@ static void initiate_ondemand_body(struct find_oppo_bundle *b,
 			/* We've finished last DNS queries: IPSECKEY for his client.
 			 * Using the information, try to instantiate a connection
 			 * and start negotiating.
-			 * We now know the peer.  The chosing of "c" ignored this,
+			 * We now know the peer.  The choosing of "c" ignored this,
 			 * so we will disregard its current value.
 			 * !!! We need to randomize the entry in gw that we choose.
 			 */
@@ -1117,11 +1117,12 @@ static void initiate_ondemand_body(struct find_oppo_bundle *b,
 						b->failure_shunt, /* if not from conn, where did this come from? */
 						b->transport_proto,
 						"no suitable connection")) {
-							DBG(DBG_OPPO, DBG_log("replaced negotiatinshunt with failurehunt=hold because no connection was found"));
+							DBG(DBG_OPPO, DBG_log("replaced negotiationshunt with failurehunt=hold because no connection was found"));
 					} else {
-						libreswan_log("failed to replace negotiatinshunt with failurehunt=hold");
+						libreswan_log("failed to replace negotiationshunt with failurehunt=hold");
 					}
 				}
+				/* ??? c == NULL -- what can we do? */
 			} else {
 				/* If we are to proceed asynchronously, b->whackfd will be NULL_FD. */
 				passert(c->kind == CK_INSTANCE);
@@ -1167,16 +1168,17 @@ static void initiate_ondemand_body(struct find_oppo_bundle *b,
 
 		/* the second chunk: initiate the next DNS query (if any) */
 		DBG(DBG_OPPO | DBG_CONTROL, {
-			ipstr_buf b1;
-			ipstr_buf b2;
-			/* ??? CLANG 3.5 thinks c might be NULL */
-			DBG_log("initiate on demand using %s from %s to %s new state: %s%s%s",
-				(c->policy & POLICY_AUTH_NULL) ? "AUTH_NULL" : "RSASIG",
-				ipstr(&b->our_client, &b1),
-				ipstr(&b->peer_client, &b2),
-				oppo_step_name[b->step],
-				ugh ? " - error:" : "",
-				ugh ? ugh : "");
+			if (c != NULL) {
+				ipstr_buf b1;
+				ipstr_buf b2;
+				DBG_log("initiate on demand using %s from %s to %s new state: %s%s%s",
+					(c->policy & POLICY_AUTH_NULL) ? "AUTH_NULL" : "RSASIG",
+					ipstr(&b->our_client, &b1),
+					ipstr(&b->peer_client, &b2),
+					oppo_step_name[b->step],
+					ugh ? " - error:" : "",
+					ugh ? ugh : "");
+			}
 		});
 
 		if (c == NULL) {
@@ -1185,7 +1187,7 @@ static void initiate_ondemand_body(struct find_oppo_bundle *b,
 			 * This case has been handled already.
 			 */
 		} else if (ugh != NULL) {
-			/* i dont think this can happen without DNS, and then these value are already set */
+			/* I dont think this can happen without DNS, and then these value are already set */
 			b->policy_prio = c->prio;
 			b->negotiation_shunt = (c->policy & POLICY_NEGO_PASS) ? SPI_PASS : SPI_HOLD;
 			b->failure_shunt = shunt_policy_spi(c, FALSE);

programs/pluto/kernel.c
@@ -453,13 +453,13 @@ int fmt_common_shell_out(char *buf, int blen, const struct connection *c,
 		catstr[0] = '\0';
 
 	connmarkstr[0] = '\0';
-	int inend = 0;
 	if (c->sa_marks.in.val != 0) {
-		inend = snprintf(connmarkstr, sizeof(connmarkstr), "CONNMARK_IN=%"PRIu32"/%#010x ",
+		snprintf(connmarkstr, sizeof(connmarkstr), "CONNMARK_IN=%"PRIu32"/%#010x ",
 			c->sa_marks.in.val, c->sa_marks.in.mask);
 	}
 	if (c->sa_marks.out.val != 0) {
-		inend = snprintf(connmarkstr+inend, sizeof(connmarkstr)-inend, "CONNMARK_OUT=%"PRIu32"/%#010x ",
+		size_t inend = strlen(connmarkstr);
+		snprintf(connmarkstr+inend, sizeof(connmarkstr)-inend, "CONNMARK_OUT=%"PRIu32"/%#010x ",
 			c->sa_marks.out.val, c->sa_marks.out.mask);
 	}
 