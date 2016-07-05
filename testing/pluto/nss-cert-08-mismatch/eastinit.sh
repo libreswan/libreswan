2761d3720859c114b90c8dae80212b6542a23663
Paul Wouters <pwouters@redhat.com>
2016-07-05 19:03:36 +0300

    testing: add nss-cert-08-mismatch testing for responder ID mismatch

---
+++ testing/pluto/nss-cert-08-mismatch/eastinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep --x509
+certutil -D -d sql:/etc/ipsec.d -n road
+certutil -D -d sql:/etc/ipsec.d -n west
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add nss-cert
+ipsec auto --status |grep nss-cert
+echo "initdone"
