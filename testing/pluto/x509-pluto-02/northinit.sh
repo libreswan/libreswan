3f3884390a64572089450a40344cd5b5827ee0b7
Paul Wouters <pwouters@redhat.com>
2016-07-08 23:43:53 +0300

    testing: update of various reference output

--- testing/pluto/x509-pluto-02/northinit.sh
+++ testing/pluto/x509-pluto-02/northinit.sh
@@ -4,7 +4,7 @@ setenforce 0
 iptables -A INPUT -i eth0 -s 192.0.2.0/24 -j LOGDROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
 # confirm with a ping
-ping -n -c 4 192.0.2.254
+ping -n -c 4 -I 192.0.3.254 192.0.2.254
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add north-east-x509-pluto-02
