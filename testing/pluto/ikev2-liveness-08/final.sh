2616888d1a83f45a7a941fa40677dbae0c8e3453
Antony Antony <antony@phenome.org>
2016-08-02 11:47:20 +0200

    testing: ikev2-liveness-08 testing two rw gateways behind same nat

---
+++ testing/pluto/ikev2-liveness-08/final.sh
@@ -0,0 +1,8 @@
+# should show there are no tunnels up
+ipsec whack --trafficstatus
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; core=`ls /tmp/core* 2>/dev/null`; gdb --batch /usr/local/libexec/ipsec/pluto $core; mv /tmp/core* OUTPUT/; fi
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
