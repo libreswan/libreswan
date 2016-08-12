06761aa2faba83ad59aaa574d1ca536e9a7e3621
Andrew Cagney <cagney@gnu.org>
2016-08-12 11:03:57 -0400

    kvmresults: when --json dump something vaguely resembling json

--- testing/utils/kvmresults.py
+++ testing/utils/kvmresults.py
@@ -17,6 +17,7 @@
 import argparse
 import sys
 import os
+import re
 from enum import Enum
 
 from fab import testsuite
@@ -26,6 +27,7 @@ from fab import stats
 from fab import skip
 from fab import ignore
 from fab import argutil
+from fab import jsonutil
 
 
 class Print(argutil.List):
@@ -42,6 +44,12 @@ class Print(argutil.List):
     saved_output_directory = "saved-output-directory"
     scripts = "scripts"
     errors = "errors"
+    runtime = "runtime"
+    total_time = "total-time"
+    start_time = "start-time"
+    end_time = "end-time"
+    script_time = "script-time"
+    boot_time = "boot-time"
 
 
 class Prefix(Enum):
@@ -106,6 +114,9 @@ def main():
     parser.add_argument("baseline", metavar="BASELINE-DIRECTORY", nargs="?",
                         help="an optional testsuite directory (contains a TESTLIST file) containing output from a previous test run")
 
+    parser.add_argument("--json", action="store_true",
+                        help="output each result as an individual json object (pipe the output through 'jq -s .' to convert it to a well formed json list")
+
     testsuite.add_arguments(parser)
     logutil.add_arguments(parser)
     skip.add_arguments(parser)
@@ -126,6 +137,7 @@ def main():
         logger.info("  Print: %s", args.print)
         logger.info("  Prefix: %s", args.prefix)
         logger.info("  Baseline: %s", args.baseline)
+        logger.info("  Json: %s", args.json)
         testsuite.log_arguments(logger, args)
         logutil.log_arguments(logger, args)
         skip.log_arguments(logger, args)
@@ -186,6 +198,83 @@ def stderr_log(fmt, *args):
     sys.stderr.write("\n")
 
 
+class JsonOutput:
+    def __init__(self):
+        self.table = {}
+    def prefix(self, key, value):
+        self.add(key, value)
+    def add(self, *keyval, string=None):
+        keys = [key.replace("-","_") for key in keyval[0:-1]]
+        value = keyval[-1]
+        # Only suppress non-existant values.
+        if value is None:
+            return
+        table = self.table
+        for key in keys[0:-1]:
+            key = key.replace("-","_")
+            if not key in table:
+                table[key] = {}
+            table = table[key]
+        table[keys[-1]] = value
+    def flush(self):
+        jsonutil.dump(self.table, sys.stdout)
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+class PrintOutput:
+    def prefix(self, key, value):
+        sys.stdout.write(value)
+    def add(self, *keyval, string=lambda x: x and " " + str(x) or ""):
+        # By default, suppress anything Null or empty/false/...
+        keys = keyval[0:-1]
+        value = keyval[-1]
+        sys.stdout.write(string(value))
+    def flush(self):
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+
+class ResultCache:
+    """Load the results on-demand"""
+
+    def __init__(self, logger, test, args, baseline, result_stats):
+        self.cached_debug_log = None
+        self.cached_result = None
+        self.logger = logger
+        self.test = test
+        self.args = args
+        self.baseline = baseline
+        self.result_stats = result_stats
+
+    def result(self, reason):
+        if self.cached_result:
+            return self.cached_result
+        self.logger.debug("loading results for %s", reason)
+        self.cached_result = post.mortem(self.test, self.args, baseline=self.baseline,
+                                         output_directory=self.test.saved_output_directory,
+                                         skip_sanitize=self.args.quick or self.args.quick_sanitize,
+                                         skip_diff=self.args.quick or self.args.quick_diff,
+                                         update=self.args.update,
+                                         update_sanitize=self.args.update_sanitize,
+                                         update_diff=self.args.update_diff)
+        self.result_stats.add_result(self.cached_result)
+        return self.cached_result
+
+    def grub(self, regex, cast=lambda x: x):
+        """Grub around in the debug log to find useful stuff"""
+        if self.cached_debug_log is None:
+            debug_log = os.path.join(self.test.output_directory, "debug.log")
+            if os.path.isfile(debug_log):
+                with open(debug_log) as f:
+                    self.cached_debug_log = f.read()
+            else:
+                self.cached_debug_log = ""
+        match = re.search(regex, self.cached_debug_log, re.MULTILINE)
+        if not match:
+            return None
+        return cast(match.group(1))
+
+
 def results(logger, tests, baseline, args, result_stats):
 
     for test in tests:
@@ -201,21 +290,14 @@ def results(logger, tests, baseline, args, result_stats):
                 continue
 
             # Filter out tests that have not been run
-            result = None
-            if args.skip in args.print or Print.result in args.print or Print.diffs in args.print or Print.errors in args.print:
-                result = post.mortem(test, args, baseline=baseline,
-                                     output_directory=test.saved_output_directory,
-                                     skip_sanitize=args.quick or args.quick_sanitize,
-                                     skip_diff=args.quick or args.quick_diff,
-                                     update=args.update,
-                                     update_sanitize=args.update_sanitize,
-                                     update_diff=args.update_diff)
-                result_stats.add_result(result)
+            result_cache = ResultCache(logger, test, args, baseline, result_stats)
+            if args.skip:
+                result = result_cache.result("skip")
                 if skip.result(logger, args, result):
                     result_stats.add_skipped(result)
                     continue
 
-            sep = ""
+            b = args.json and JsonOutput() or PrintOutput()
 
             # Print the test's name/path
             if not args.prefix:
@@ -223,93 +305,76 @@ def results(logger, tests, baseline, args, result_stats):
                 # explicitly specifies a test's output directory
                 # (found in TEST.SAVED_OUTPUT_DIRECTORY), print that;
                 # otherwise print the path to the test's directory.
-                print(sep, end="")
-                print((test.saved_output_directory
-                       and test.saved_output_directory
-                       or test.directory), end="")
-                sep = " "
+                if test.saved_output_directory:
+                    b.prefix(Print.saved_output_directory,
+                             test.saved_output_directory)
+                else:
+                    b.prefix(Print.directory, test.directory)
             else:
                 # Print the test name/path per command line
                 if args.prefix is Prefix.name:
-                    print(sep, end="")
-                    print(test.name, end="")
-                    sep = " "
+                    b.prefix(Print.name, test.name)
                 elif args.prefix is Prefix.test_directory:
-                    print(sep, end="")
-                    print(test.directory, end="")
-                    sep = " "
+                    b.prefix(Print.directory, test.directory)
                 elif args.prefix is Prefix.output_directory:
-                    print(sep, end="")
-                    print((test.saved_output_directory
-                           and test.saved_output_directory
-                           or test.output_directory), end="")
-                    sep = " "
+                    if test.saved_output_directory:
+                        b.prefix(Print.saved_output_directory,
+                                 test.saved_output_directory)
+                    else:
+                        b.prefix(Print.output_directory,
+                                 test.output_directory)
 
             for p in args.print:
                 if p is Print.diffs:
                     continue
                 elif p is Print.directory:
-                    print(sep, end="")
-                    print(test.directory, end="")
-                    sep = " "
+                    b.add(p, test.directory)
                 elif p is Print.expected_result:
-                    print(sep, end="")
-                    print(test.expected_result, end="")
-                    sep = " "
+                    b.add(p, test.expected_result)
                 elif p is Print.full_name:
-                    print(sep, end="")
-                    print(test.full_name, end="")
-                    sep = " "
+                    b.add(p, test.full_name)
                 elif p is Print.host_names:
-                    for name in test.host_names:
-                        print(sep, end="")
-                        print(name, end="")
-                        sep = ","
-                    sep = " "
+                    b.add(p, test.host_names,
+                          string=lambda host_names: " " + ",".join(host_names))
                 elif p is Print.kind:
-                    print(sep, end="")
-                    print(test.kind, end="")
-                    sep = " "
+                    b.add(p, test.kind)
                 elif p is Print.name:
-                    print(sep, end="")
-                    print(test.name, end="")
-                    sep = " "
+                    b.add(p, test.name)
                 elif p is Print.output_directory:
-                    print(sep, end="")
-                    print(test.output_directory, end="")
-                    sep = " "
+                    b.add(p, test.output_directory)
                 elif p is Print.result:
-                    print(" ", result, end="", sep="")
+                    b.add(p, result_cache.result(p))
                 elif p is Print.errors:
-                    if result.errors:
-                        print(" ", result.errors, end="", sep="")
+                    b.add(p, result_cache.result(p).errors)
                 elif p is Print.sanitize_directory:
-                    print(sep, end="")
-                    print(test.sanitize_directory, end="")
-                    sep = " "
+                    b.add(p, test.sanitize_directory)
                 elif p is Print.saved_output_directory:
-                    print(sep, end="")
-                    print(test.saved_output_directory, end="")
-                    sep = " "
+                    b.add(p, test.saved_output_directory)
                 elif p is Print.scripts:
-                    for host, script in test.host_script_tuples:
-                        print(sep, end="")
-                        print("%s:%s" % (host, script), end="")
-                        sep = ","
-                    sep = " "
+                    b.add(p, [{ "host": h, "script": s} for h, s in test.host_script_tuples],
+                          string=lambda scripts: " " + ",".join([script["host"] + ":" + script["script"] for script in scripts]))
+                elif p is Print.start_time:
+                    b.add(p, result_cache.grub(r"starting debug log at (.*)$"))
+                elif p is Print.end_time:
+                    b.add(p, result_cache.grub(r"ending debug log at (.*)$"))
+                elif p is Print.runtime:
+                    b.add(p, result_cache.grub(r": stop testing .* after (.*) second", float))
+                elif p is Print.boot_time:
+                    b.add(p, result_cache.grub(r": stop booting domains after (.*) second", float))
+                elif p is Print.script_time:
+                    b.add(p, result_cache.grub(r": stop running scripts .* after (.*) second", float))
+                elif p is Print.total_time:
+                    b.add(p, result_cache.grub(r": stop processing test .* after (.*) second", float))
                 else:
-                    print()
                     raise Exception("unhandled print option %s" % p)
 
-            print()
-
             if Print.diffs in args.print:
+                result = result_cache.result(Print.diffs)
                 for domain in result.diffs:
-                    for line in result.diffs[domain]:
-                        if line:
-                            print(line)
+                    b.add(Print.diffs, domain, result.diffs[domain],
+                          string=lambda diff: diff and "\n" + "\n".join(diff) or "")
 
-            sys.stdout.flush()
+            b.flush()
 
 
 if __name__ == "__main__":
