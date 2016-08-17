libreswan/libreswan

commit 868ea2f8be3bbc9062ab4339eb5b6c9f67cae02e

libswan: move/merge rfc2537/rfc3110 code into secrets.[hc]
Remember to compute the CKAID when unpacking the blob into an RSA_public_key.

programs/rsasigkey/rsasigkey.c
@@ -106,31 +106,36 @@ char me[] = "ipsec rsasigkey";  /* for messages */
 /* forwards */
 void rsasigkey(int nbits, int seedbits, char *configdir, char *password);
 void getrandom(size_t nbytes, unsigned char *buf);
-static const unsigned char *bundle(int e, chunk_t n, size_t *sizep);
 static const char *conv(const unsigned char *bits, size_t nbytes, int format);
 void report(char *msg);
 
 /*
- * hexOut - prepare hex output, guaranteeing even number of digits.
- * (The current Libreswan conversion routines expect an even digit count.)
- *
- * NOTE: result is a pointer into a STATIC buffer.
+ * bundle - bundle e and n into an RFC2537-format chunk_t
  */
-static const char *hexOut(chunk_t data)
+static char *base64_bundle(int e, chunk_t modulus)
 {
-	unsigned i;
-	static char hexbuf[3 + BYTES_FOR_BITS(MAXBITS) * 2];
-	char *hexp = hexbuf;
+	/*
+	 * Pack the single-byte exponent into a byte array.
+	 */
+	assert(e <= 255);
+	u_char exponent_byte = 1;
+	chunk_t exponent = {
+		.ptr = &exponent_byte,
+		.len = 1,
+	};
 
-	if (data.len > BYTES_FOR_BITS(MAXBITS))
-		return "[too many bytes]";
+	/*
+	 * Create the resource record.
+	 */
+	char *bundle;
+	err_t err = rsa_pubkey_to_base64(exponent, modulus, &bundle);
+	if (err) {
+		fprintf(stderr, "%s: can't-happen bundle convert error `%s'\n",
+			me, err);
+		exit(1);
+	}
 
-	*hexp++ = '0';
-	*hexp++ = 'x';
-	for (i = 0; i < data.len; i++, hexp += 2)
-		sprintf(hexp, "%02x", data.ptr[i]);
-
-	return hexbuf;
+	return bundle;
 }
 
 /* UpdateRNG - Updates NSS's PRNG with user generated entropy. */
@@ -403,8 +408,6 @@ void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
 	PK11SlotInfo *slot = NULL;
 	SECKEYPrivateKey *privkey = NULL;
 	SECKEYPublicKey *pubkey = NULL;
-	const unsigned char *bundp = NULL;
-	size_t bs;
 	realtime_t now = realnow();
 
 	if (password == NULL) {
@@ -526,8 +529,12 @@ void rsasigkey(int nbits, int seedbits, char *configdir, char *password)
 
 	printf("\t#ckaid=%s\n", hex_ckaid);
 
-	bundp = bundle(E, public_modulus, &bs);
-	printf("\t#pubkey=%s\n", conv(bundp, bs, 's')); /* RFC2537ish format */
+	/* RFC2537/RFC3110-ish format */
+	{
+		char *bundle = base64_bundle(E, public_modulus);
+		printf("\t#pubkey=%s\n", bundle);
+		pfree(bundle);
+	}
 
 	printf("\tModulus: 0x%s\n", conv(public_modulus.ptr, public_modulus.len, 16));
 	printf("\tPublicExponent: 0x%s\n", conv(public_exponent.ptr, public_exponent.len, 16));
@@ -581,39 +588,6 @@ void getrandom(size_t nbytes, unsigned char *buf)
 	}
 
 	close(dev);
-}
-
-/*
-   - bundle - bundle e and n into an RFC2537-format lump
- * Note, calls hexOut.
- *
- * NOTE: returns a pointer into a STATIC buffer
- */
-static const unsigned char *bundle(int e, chunk_t n, size_t *sizep)
-{
-	const char *hexp = hexOut(n);
-	static unsigned char bundbuf[2 + BYTES_FOR_BITS(MAXBITS)];
-	const char *er;
-	size_t size;
-
-	assert(e <= 255);
-	bundbuf[0] = 1;
-	bundbuf[1] = e;
-	er = ttodata(hexp, 0, 0, (char *)bundbuf + 2, sizeof(bundbuf) - 2,
-		     &size);
-	if (er != NULL) {
-		fprintf(stderr, "%s: can't-happen bundle convert error `%s'\n",
-			me, er);
-		exit(1);
-	}
-	if (size > sizeof(bundbuf) - 2) {
-		fprintf(stderr, "%s: can't-happen bundle overflow (need %d)\n",
-			me, (int) size);
-		exit(1);
-	}
-	if (sizep != NULL)
-		*sizep = size + 2;
-	return bundbuf;
 }
 
 /*

include/secrets.h
@@ -60,13 +60,21 @@ struct RSA_public_key {
 };
 
 struct RSA_private_key {
-	struct RSA_public_key pub;	/* must be at start for RSA_show_public_key */
+	struct RSA_public_key pub;
 };
 
 extern void free_RSA_public_content(struct RSA_public_key *rsa);
 
-extern err_t unpack_RSA_public_key(struct RSA_public_key *rsa,
-				   const chunk_t *pubkey);
+err_t rsa_pubkey_to_rfc_resource_record(chunk_t exponent, chunk_t modulus, chunk_t *rr);
+err_t rfc_resource_record_to_rsa_pubkey(chunk_t rr, chunk_t *exponent, chunk_t *modulus);
+
+err_t rsa_pubkey_to_base64(chunk_t exponent, chunk_t modulus, char **rr);
+err_t base64_to_rsa_pubkey(const char *rr, chunk_t *exponent, chunk_t *modulus);
+
+err_t pack_RSA_public_key(const struct RSA_public_key *rsa, chunk_t *pubkey);
+err_t unpack_RSA_public_key(struct RSA_public_key *rsa, const chunk_t *pubkey);
+
+void DBG_log_RSA_public_key(const struct RSA_public_key *rsa);
 
 struct private_key_stuff {
 	enum PrivateKeyKind kind;

lib/libswan/Makefile
@@ -42,6 +42,7 @@ SRCS=addrtoa.c addrtot.c addrtypeof.c alg_info.c anyaddr.c \
 	subnettypeof.c ttoaddr.c ttodata.c ttoprotoport.c \
 	ttorange.c ttosa.c ttosubnet.c ttoul.c ultot.c \
 	secitem_chunk.c \
+	base64_rsa_pubkey.c \
 	$(X509LIB)
 
 ifeq ($(USE_DNSSEC),true)

lib/libswan/base64_rsa_pubkey.c
@@ -1,0 +1,208 @@
+/*
+ * convert rsa pubkeys to/from RFC2537/RFC3110 resource records, for libreswan
+ *
+ * Copyright (C) 1998-2004  D. Hugh Redelmeier.
+ * Copyright (C) 2005 Michael Richardson <mcr@xelerance.com>
+ * Copyright (C) 2009-2012 Avesh Agarwal <avagarwa@redhat.com>
+ * Copyright (C) 2012-2015 Paul Wouters <paul@libreswan.org>
+ * Copyright (C) 2016 Andrew Cagney <cagney@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include <nss.h>
+
+#include "libreswan.h"
+#include "lswalloc.h"
+#include "lswlog.h"
+#include "secrets.h"
+
+/*
+ * Deal with RFC Resource Records as defined in rfc3110 (nee rfc2537).
+ */
+
+err_t rsa_pubkey_to_rfc_resource_record(chunk_t exponent, chunk_t modulus, chunk_t *rr)
+{
+	*rr = empty_chunk;
+
+	/*
+	 * Since exponent length field is either 1 or 3 bytes in size,
+	 * just allocate 3 extra bytes.
+	 */
+	size_t rrlen = exponent.len + modulus.len + 3;
+	u_char *buf = alloc_bytes(rrlen, "buffer for rfc3110");
+	u_char *p = buf;
+
+	if (exponent.len <= 255) {
+		*p++ = exponent.len;
+	} else if (exponent.len <= 0xffff) {
+		*p++ = 0;
+		*p++ = (exponent.len >> 8) & 0xff;
+		*p++ = exponent.len & 0xff;
+	} else {
+		pfree(buf);
+		return "RSA public key exponent too long for resource record";
+	}
+
+	memcpy(p, exponent.ptr, exponent.len);
+	p += exponent.len;
+	memcpy(p, modulus.ptr, modulus.len);
+	p += modulus.len;
+
+	*rr = (chunk_t) {
+		.ptr = buf,
+		.len = p - buf,
+	};
+
+	return NULL;
+}
+
+err_t rfc_resource_record_to_rsa_pubkey(chunk_t rr, chunk_t *e, chunk_t *n)
+{
+	*e = empty_chunk;
+	*n = empty_chunk;
+
+	/*
+	 * Step 1: find the bounds of the exponent and modulus within
+	 * the recource record and verify that they are sane.
+	 */
+
+	chunk_t exponent;
+	if (rr.len >= 2 && rr.ptr[0] != 0x00) {
+		/*
+		 * Exponent length is one-byte, followed by that many
+		 * exponent bytes
+		 */
+		exponent = (chunk_t) {
+			.ptr = rr.ptr + 1,
+			.len = rr.ptr[0]
+		};
+	} else if (rr.len >= 3 && rr.ptr[0] == 0x00) {
+		/*
+		 * Exponent length is 0x00 followed by 2 bytes of
+		 * length (big-endian), followed by that many exponent
+		 * bytes
+		 */
+		exponent = (chunk_t) {
+			.ptr = rr.ptr + 3,
+			.len = (rr.ptr[1] << BITS_PER_BYTE) + rr.ptr[2],
+		};
+	} else {
+		/* not even room for length! */
+		return "RSA public key resource record way too short";
+	}
+
+	/*
+	 * Does the exponent fall off the end of the resource record?
+	 */
+	u_char *const exponent_end = exponent.ptr + exponent.len;
+	u_char *const rr_end = rr.ptr + rr.len;
+	if (exponent_end > rr_end) {
+		return "truncated RSA public key resource record exponent";
+	}
+
+	/*
+	 * What is left over forms the modulus.
+	 */
+	chunk_t modulus = (chunk_t) {
+		.ptr = exponent_end,
+		.len = rr_end - exponent_end,
+	};
+
+	if (modulus.len < RSA_MIN_OCTETS_RFC) {
+		return "RSA public key resource record modulus too short";
+	}
+	if (modulus.len < RSA_MIN_OCTETS) {
+		return RSA_MIN_OCTETS_UGH;
+	}
+	if (modulus.len > RSA_MAX_OCTETS) {
+		return RSA_MAX_OCTETS_UGH;
+	}
+
+	/*
+	 * Step 2: all looks good, clone the bits
+	 */
+	*e = chunk_clone(exponent, "e");
+	*n = chunk_clone(modulus, "n");
+	return NULL;
+}
+
+err_t rsa_pubkey_to_base64(chunk_t exponent, chunk_t modulus, char **base64_rr)
+{
+	*base64_rr = NULL;
+
+	chunk_t rr_chunk;
+	err_t err = rsa_pubkey_to_rfc_resource_record(exponent, modulus, &rr_chunk);
+	if (err) {
+		return err;
+	}
+
+	/*
+	 * A byte is 8-bits, base64 uses 6-bits (2^6=64).  Plus some
+	 * for 0s.  Plus some for \0.  Plus some extra for rounding.
+	 */
+	size_t rr_len = rr_chunk.len * 8 / 6 + 2 + 1 + 10;
+	char *rr = alloc_bytes(rr_len, "base64 resource record");
+	size_t n = datatot(rr_chunk.ptr, rr_chunk.len, 's', rr, rr_len);
+	if (n >= rr_len) {
+		freeanychunk(rr_chunk);
+		return "base64 encoded RSA public key resource record larger than expected";
+	}
+
+	*base64_rr = rr;
+	freeanychunk(rr_chunk);
+	return NULL;
+}
+
+#if 0
+err_t base64_to_rsa_pubkey(const char *rr, chunk_t *exponent, chunk_t *modulus)
+{
+	return "not implemented";
+}
+#endif
+
+#if 0
+err_t pack_RSA_public_key(const struct RSA_public_key *rsa, chunk_t *rr)
+{
+	return rsa_pubkey_to_rfc_resource_record(rsa->e, rsa->n, rr);
+}
+#endif
+
+err_t unpack_RSA_public_key(struct RSA_public_key *rsa, const chunk_t *pubkey)
+{
+	err_t err;
+
+	/* unpack */
+	chunk_t exponent;
+	chunk_t modulus;
+	err = rfc_resource_record_to_rsa_pubkey(*pubkey, &exponent, &modulus);
+	if (err) {
+		return err;
+	}
+
+	chunk_t ckaid;
+	err = form_rsa_ckaid(modulus, &ckaid);
+	if (err) {
+		freeanychunk(exponent);
+		freeanychunk(modulus);
+		return err;
+	}
+
+	keyblobtoid(pubkey->ptr, pubkey->len, rsa->keyid, sizeof(rsa->keyid));
+	rsa->k = modulus.len;
+	rsa->e = exponent;
+	rsa->n = modulus;
+	rsa->ckaid = ckaid;
+
+	DBG(DBG_PRIVATE, DBG_log_RSA_public_key(rsa));
+	/* generate the CKAID */
+	return NULL;
+}

lib/libswan/secrets.c
@@ -110,7 +110,7 @@ static void lsw_process_secret_records(struct secret **psecrets);
 static void lsw_process_secrets_file(struct secret **psecrets,
 				const char *file_pat);
 
-static void RSA_show_public_key(struct RSA_public_key *k)
+void DBG_log_RSA_public_key(const struct RSA_public_key *k)
 {
 	DBG_log(" keyid: *%s", k->keyid);
 	DBG_dump_chunk("n", k->n);
@@ -1193,80 +1193,6 @@ void free_public_keys(struct pubkey_list **keys)
 {
 	while (*keys != NULL)
 		*keys = free_public_keyentry(*keys);
-}
-
-/*
- * decode of RSA pubkey chunk
- * - format specified in RFC 2537 RSA/MD5 Keys and SIGs in the DNS
- * - exponent length in bytes (1 or 3 octets)
- *   + 1 byte if in [1, 255]
- *   + otherwise 0x00 followed by 2 bytes of length (big-endian)
- * - exponent (of specified length)
- * - modulus (the rest of the pubkey chunk)
- */
-err_t unpack_RSA_public_key(struct RSA_public_key *rsa, const chunk_t *pubkey)
-{
-	chunk_t exponent;
-	chunk_t mod;
-
-	rsa->keyid[0] = '\0';	/* in case of keyblobtoid failure */
-
-	if (pubkey->len < 3)
-		return "RSA public key blob way too short";	/*
-								 * not even
-								 * room for
-								 * length!
-								 */
-
-	/* exponent */
-	if (pubkey->ptr[0] != 0x00) {
-		/* one-byte length, followed by that many exponent bytes */
-		setchunk(exponent, pubkey->ptr + 1, pubkey->ptr[0]);
-	} else {
-		/*
-		 * 0x00 followed by 2 bytes of length (big-endian),
-		 * followed by that many exponent bytes
-		 */
-		setchunk(exponent, pubkey->ptr + 3,
-			(pubkey->ptr[1] << BITS_PER_BYTE) + pubkey->ptr[2]);
-	}
-
-	/*
-	 * check that exponent fits within pubkey and leaves room for
-	 * a reasonable modulus.
-	 * Take care to avoid overflow in this check.
-	 */
-	if (pubkey->len -
-		(exponent.ptr - pubkey->ptr) < exponent.len +
-		RSA_MIN_OCTETS_RFC)
-		return "RSA public key blob too short";
-
-	/* modulus: all that's left in pubkey */
-	mod.ptr = exponent.ptr + exponent.len;
-	mod.len = &pubkey->ptr[pubkey->len] - mod.ptr;
-
-	if (mod.len < RSA_MIN_OCTETS)
-		return RSA_MIN_OCTETS_UGH;
-
-	if (mod.len > RSA_MAX_OCTETS)
-		return RSA_MAX_OCTETS_UGH;
-
-	rsa->e = chunk_clone(exponent, "e");
-	rsa->n = chunk_clone(mod, "n");
-
-	keyblobtoid(pubkey->ptr, pubkey->len, rsa->keyid, sizeof(rsa->keyid));
-
-	DBG(DBG_PRIVATE, RSA_show_public_key(rsa));
-
-	rsa->k = rsa->n.len;
-
-	if (rsa->k != mod.len) {
-		freeanychunk(rsa->e);
-		freeanychunk(rsa->n);
-		return "RSA modulus shorter than specified";
-	}
-
-	return NULL;
 }
 
 bool same_RSA_public_key(const struct RSA_public_key *a,

programs/showhostkey/showhostkey.c
@@ -203,40 +203,16 @@ static struct secret *pick_key(struct secret *host_secrets,
 	return s;
 }
 
-static unsigned char *pubkey_to_rfc3110(const struct RSA_public_key *pub,
-				 unsigned int *keybuflen)
+static char *pubkey_to_rfc3110_base64(const struct RSA_public_key *pub)
 {
-	unsigned char *buf;
-	unsigned char *p;
-	unsigned int elen;
-
-	/* XXX: this has always leaked memory */
-	chunk_t e = chunk_clone(pub->e, "e");
-	chunk_t n = chunk_clone(pub->n, "n");
-	elen = e.len;
-
-	buf = alloc_bytes(e.len + n.len + 3, "buffer for rfc3110");
-	p = buf;
-
-	if (elen <= 255) {
-		*p++ = elen;
-	} else if ((elen & ~0xffff) == 0) {
-		*p++ = 0;
-		*p++ = (elen >> 8) & 0xff;
-		*p++ = elen & 0xff;
-	} else {
-		pfree(buf);
-		return 0; /* unrepresentable exponent length */
+	char* base64;
+	err_t err = rsa_pubkey_to_base64(pub->e, pub->n, &base64);
+	if (err) {
+		fprintf(stderr, "%s: unexpected error encoing RSA public key '%s'\n",
+			progname, err);
+		exit(5);
 	}
-
-	memcpy(p, e.ptr, e.len);
-	p += e.len;
-	memcpy(p, n.ptr, n.len);
-	p += n.len;
-
-	*keybuflen = (p - buf);
-
-	return buf;
+	return base64;
 }
 
 static void show_dnskey(struct secret *s,
@@ -244,11 +220,8 @@ static void show_dnskey(struct secret *s,
 			char *gateway)
 {
 	char qname[256];
-	char base64[8192];
 	int gateway_type = 0;
 	const struct private_key_stuff *pks = lsw_get_pks(s);
-	unsigned char *keyblob;
-	unsigned int keybloblen = 0;
 
 	gethostname(qname, sizeof(qname));
 
@@ -258,9 +231,7 @@ static void show_dnskey(struct secret *s,
 		exit(5);
 	}
 
-	keyblob = pubkey_to_rfc3110(&pks->u.RSA_private_key.pub, &keybloblen);
-
-	datatot(keyblob, keybloblen, 's', base64, sizeof(base64));
+	char *base64 = pubkey_to_rfc3110_base64(&pks->u.RSA_private_key.pub);
 
 	if (gateway != NULL) {
 		ip_address test;
@@ -280,15 +251,13 @@ static void show_dnskey(struct secret *s,
 	printf("%s.    IN    IPSECKEY  %d %d 2 %s %s\n",
 	       qname, precedence, gateway_type,
 	       (gateway == NULL) ? "." : gateway, base64 + sizeof("0s") - 1);
+	pfree(base64);
 }
 
 static void show_confkey(struct secret *s,
 			 char *side)
 {
-	char base64[8192];
 	const struct private_key_stuff *pks = lsw_get_pks(s);
-	unsigned char *keyblob;
-	unsigned int keybloblen = 0;
 
 	if (pks->kind != PPK_RSA) {
 		char *enumstr = "gcc is crazy";
@@ -307,14 +276,14 @@ static void show_confkey(struct secret *s,
 		exit(5);
 	}
 
-	keyblob = pubkey_to_rfc3110(&pks->u.RSA_private_key.pub, &keybloblen);
-
-	datatot(keyblob, keybloblen, 's', base64, sizeof(base64));
+	char *base64 = pubkey_to_rfc3110_base64(&pks->u.RSA_private_key.pub);
 
 	printf("\t# rsakey %s\n",
 	       pks->u.RSA_private_key.pub.keyid);
 	printf("\t%srsasigkey=%s\n", side,
 	       base64);
+
+	pfree(base64);
 }
 
 int main(int argc, char *argv[])

commit 1c364828804c300bd3be156eaf62f675c1108725

rsasigkey: fix the printed pubkey=... - print "e", not 1
Most likely caused by 868ea2f8be3bbc9062ab4339eb5b6c9f67cae02e,

programs/rsasigkey/rsasigkey.c
@@ -116,7 +116,7 @@ static char *base64_bundle(int e, chunk_t modulus)
 	 * Pack the single-byte exponent into a byte array.
 	 */
 	assert(e <= 255);
-	u_char exponent_byte = 1;
+	u_char exponent_byte = e;
 	chunk_t exponent = {
 		.ptr = &exponent_byte,
 		.len = 1,