c9f282a75e64bb5bd58a32d9d0f65525d995120b
Andrew Cagney <cagney@gnu.org>
2016-08-19 10:53:22 -0400

    web: git-advance-master.sh: only try to advance HEAD
    i.e., don't fetch new changes (let something else do that)
    and only succeed when head advances

--- testing/web/git-advance-master.sh
+++ testing/web/git-advance-master.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 git checkout master
-git fetch origin
 next=$(git rev-list --first-parent master..origin/master | tail -n 1)
-test -n "${next}" || exit 0
+test -n "${next}" || exit 111
 git rebase ${next}
+
+cat <<EOF
+
+Pending commits:
+
+EOF
+git log origin/master ^HEAD
+
+cat <<EOF
+
+New commit:
+
+EOF
+git log HEAD ^HEAD^
