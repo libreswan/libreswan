ee4ffba48d82f99991bd8be5b7759d26c7ab0dea
Andrew Cagney <cagney@gnu.org>
2016-08-27 11:17:25 -0400

    web: add passed[+failed[+unresolved[+untested]]] lines
    and make the points clickable and hoverable (if that is a word).

--- testing/web/graph.js
+++ testing/web/graph.js
@@ -5,10 +5,33 @@ function graph(graph) {
 
             var parseDate = d3.utcParse("%Y-%m-%d %H:%M");
 
+	    var names = [
+		"passed",
+		"failed",
+		"unresolved",
+		"untested"
+	    ]
+
             // Clean up the fields, make date a date.
             data.forEach(function(d) {
                 d.date = parseDate(d.date)
                 d.passed = +d.passed
+		var results = []
+		for (var i = 0; i < names.length; i++) {
+		    if (d.hasOwnProperty(names[i])) {
+			results.push(+d[names[i]])
+		    } else {
+			results.push(0)
+		    }
+		}
+		var totals = []
+		var total = 0
+		for (i = 0; i < results.length; i++) {
+		    total += results[i]
+		    totals[i] = total
+		}
+		d.results = results
+		d.totals = totals
             })
 
             // In date order.
@@ -21,20 +44,15 @@ function graph(graph) {
 		height = 500 - margin.top - margin.bottom;
 
             var x = d3.scaleTime()
-		.range([0, width]);
+		.domain(d3.extent(data, function(d) { return d.date; }))
+		.range([0, width])
             var y = d3.scaleLinear()
-		.range([height, 0]);
+		.domain([d3.min(data, function(d) { return 0.95 * d.totals[0];}),
+			 d3.max(data, function(d) { return 1.05 * d.totals[d.totals.length-1];})])
+		.range([height, 0])
 
             var xAxis = d3.axisBottom(x)
             var yAxis = d3.axisLeft(y)
-
-            x.domain(d3.extent(data, function(d) { return d.date; }));
-            y.domain([d3.min(data, function(d) { return 0.95 * d.passed;}),
-                      d3.max(data, function(d) { return 1.05 * d.passed;})]);
-
-            var passedline = d3.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d.passed); });
 
             // should grab div with id=graph
             var svg = d3.select("div").insert("svg")
@@ -48,14 +66,6 @@ function graph(graph) {
 		.attr("transform", "translate(0," + height + ")")
 		.call(xAxis);
 
-            // Add the scatterplot
-            svg.selectAll("dot")
-		.data(data)
-		.enter().append("circle")
-		.attr("r", 3.5)
-		.attr("cx", function(d) { return x(d.date); })
-		.attr("cy", function(d) { return y(d.passed); });
-
             svg.append("g")
 		.attr("class", "y axis")
 		.call(yAxis)
@@ -66,10 +76,44 @@ function graph(graph) {
 		.style("text-anchor", "end")
 		.text("Passed");
 
-            svg.append("path")
-		.datum(data)
-		.attr("class", "line")
-		.attr("d", passedline)
+	    var iso_date = function(d) {
+		d = d.toISOString()
+		d = d.match("([^T]*)T([0-9]*:[0-9]*)")
+		return d[1] + " " + d[2]
+	    }
+
+	    var dots = svg
+		.selectAll(".dot")
+		.data(data)
+		.enter()
+
+	    for (var i = 0; i < names.length; i++) {
+		var line = d3.line()
+		    .x(function(d) { return x(d.date); })
+		    .y(function(d) { return y(d.totals[i]); });
+		svg.append("path")
+		    .datum(data)
+		    .attr("class", "line")
+		    .attr("d", line)
+		// Overlay scatterplot
+		dots.append("circle")
+		    .attr("class", names[i])
+		    .attr("r", 3.5)
+		    .attr("cx", function(d) { return x(d.date); })
+		    .attr("cy", function(d) { return y(d.totals[i]); })
+		    .on("click", function(d) {
+			window.location = d.directory
+		    })
+		    .append("title")
+		    .text(function(d) {
+			var text = iso_date(d.date)
+			for (j = 0; j <= i; j++) {
+				text += "\n" + names[j] + ": " + d.results[j]
+			}
+			text += "\n" + "total: " + d.totals[i]
+			return text
+		    })
+	    }
 	})
     });
 }
