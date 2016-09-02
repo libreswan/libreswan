43a5b28d6ab2fa2281b6ccb772ceb669f0b28e7a
Paul Wouters <pwouters@redhat.com>
2016-09-02 12:59:50 -0400

    testing: multienet-02 deleted
    it only tests alternative syntax which is now tested on east in
    multinet-01

--- testing/pluto/multinet-02/eastinit.sh
+++
@@ -1,9 +0,0 @@
-: ==== start ====
-TESTNAME=multinet-02
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-
-ipsec auto --add westnet-eastnet-subnets
-
