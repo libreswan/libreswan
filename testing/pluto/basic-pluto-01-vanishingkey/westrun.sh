4f21793ebed2d4d92372e7fec7818a08bcba505f
Paul Wouters <pwouters@redhat.com>
2016-09-17 13:09:55 -0400

    testing: added basic-pluto-01-vanishingkey

---
+++ testing/pluto/basic-pluto-01-vanishingkey/westrun.sh
@@ -0,0 +1,5 @@
+# this is expected to fail to to our own misconfigured key
+ipsec auto --up  westnet-eastnet
+# we should NOT see more than one of our own outcoming attempts
+ipsec status
+echo done
