f47a21f1f73a330f55818c76d308f6a78c026170
Paul Wouters <pwouters@redhat.com>
2016-09-22 19:12:28 -0400

    testing: updated nat-pluto-03 to show encap status in final.sh

--- testing/pluto/nat-pluto-03/roadinit.sh
+++ testing/pluto/nat-pluto-03/roadinit.sh
@@ -4,4 +4,5 @@ route add -net default gw 192.1.3.254
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add road-eastnet-forceencaps
+ipsec status |grep encaps:
 echo "initdone"
