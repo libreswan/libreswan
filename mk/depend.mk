libreswan/libreswan

commit 55e01e95a8d3fc06625da575bc55449615ea8d5f

building: create $(builddir) via a GNU make order-only dependency
Avoid redundant mkdir -p $(builddir) commands.

mk/depend.mk
@@ -36,8 +36,7 @@ endif
 
 mk.depend.file := $(lastword $(MAKEFILE_LIST))
 mk.depend.dependencies.file := $(builddir)/Makefile.depend.mk
-$(mk.depend.dependencies.file): $(srcdir)/Makefile $(mk.depend.file)
-	mkdir -p $(builddir)
+$(mk.depend.dependencies.file): $(srcdir)/Makefile $(mk.depend.file) | $(builddir)
 	set -e ; \
 	for f in $(MK_DEPEND_FILES) ; do \
 		case $$f in \

mk/dirs.mk
@@ -190,3 +190,11 @@ OBJDIRTOP?=$(abs_top_builddir)
 	@echo SRCDIR=$(SRCDIR)
 	@echo OBJDIRTOP=$(OBJDIRTOP)
 	@echo LIBRESWANSRCDIR=$(LIBRESWANSRCDIR)
+
+# Targets needing the builddir should add:
+#
+#     | $(builddir)
+#
+# as a soft/order-only dependency.
+$(builddir):
+	mkdir -p $(builddir)

Makefile
@@ -210,9 +210,8 @@ OBJDIRTOP=${ABSOBJDIR}
 
 .PHONY: config
 config: ${OBJDIR}/Makefile
-${OBJDIR}/Makefile: $(srcdir)/Makefile packaging/utils/makeshadowdir
+${OBJDIR}/Makefile: $(srcdir)/Makefile packaging/utils/makeshadowdir | $(builddir)
 	@echo Setting up for OBJDIR=${OBJDIR}
-	mkdir -p $(builddir)
 	packaging/utils/makeshadowdir `cd $(srcdir); pwd` ${OBJDIR} "${SUBDIRS}"
 
 # Recursive clean dealt with elsewhere.

mk/manpages.mk
@@ -65,8 +65,7 @@ local-clean-manpages:
 # $(buildpath) (when .xml isn't generated, they both do not).  The
 # $(notdir $@) in the test is to take care of both possibilities.
 
-%: %.xml
-	@mkdir -p $(builddir)
+%: %.xml | $(builddir)
 	${TRANSFORM_VARIABLES} < $< > $(builddir)/$(notdir $@).tmp
 	: ignoring seemingly bogus $(XMLTO) exit status
 	cd $(builddir) && $(XMLTO) man $(notdir $@).tmp || true

programs/configs/Makefile
@@ -35,8 +35,7 @@ endif
 
 xmlsources := $(patsubst d.ipsec.conf/%,${srcdir}/d.ipsec.conf/%,$(shell cat ${srcdir}/d.ipsec.conf/order.txt))
 
-$(builddir)/ipsec.conf.5.xml: $(xmlsources)
-	@mkdir -p $(builddir)
+$(builddir)/ipsec.conf.5.xml: $(xmlsources) | $(builddir)
 	cat $(xmlsources) > $@
 
 local-clean-manpages: local-clean-xml-manpages

testing/cavp/Makefile
@@ -5,14 +5,12 @@ default: check
 check: ikev2.cavp ikev1_dsa.cavp ikev1_psk.cavp
 
 .PRECIOUS: %.fax
-%.fax:
-	mkdir -p $(builddir)
+%.fax: | $(builddir)
 	curl -o $(builddir)/$@.bz2 https://download.libreswan.org/cavs/$@.bz2
 	bunzip2 $(builddir)/$@.bz2
 
 .PRECIOUS: %.out
-%.out: %.fax $(top_builddir)/programs/pluto/cavp
-	mkdir -p $(builddir)
+%.out: %.fax $(top_builddir)/programs/pluto/cavp | $(builddir)
 	$(top_builddir)/programs/pluto/cavp \
 		$(builddir)/$*.fax \
 		> $(builddir)/$*.tmp

commit 94b2ed0f313d49bbc441741503ed9da325a4fa55

building: move $(builddir) rule to depend.mk
In dirs.mk it was the default rule.  Regression from
55e01e95a8d3fc06625da575bc55449615ea8d5f.

mk/depend.mk
@@ -47,6 +47,14 @@ $(mk.depend.dependencies.file): $(srcdir)/Makefile $(mk.depend.file) | $(builddi
 	done > $@.tmp
 	mv $@.tmp $@
 
+# Targets needing the builddir should add:
+#
+#     | $(builddir)
+#
+# as a soft/order-only dependency.
+$(builddir):
+	mkdir -p $(builddir)
+
 clean: mk.depend.clean
 .PHONY: mk.depend.clean
 mk.depend.clean:
@@ -54,4 +62,3 @@ mk.depend.clean:
 	rm -f $(builddir)/*.d
 
 -include $(mk.depend.dependencies.file)
-

mk/dirs.mk
@@ -190,11 +190,3 @@ OBJDIRTOP?=$(abs_top_builddir)
 	@echo SRCDIR=$(SRCDIR)
 	@echo OBJDIRTOP=$(OBJDIRTOP)
 	@echo LIBRESWANSRCDIR=$(LIBRESWANSRCDIR)
-
-# Targets needing the builddir should add:
-#
-#     | $(builddir)
-#
-# as a soft/order-only dependency.
-$(builddir):
-	mkdir -p $(builddir)