4b1f0d5a3ce27b1591281b4f92f858c64c68d0db
Paul Wouters <pwouters@redhat.com>
2016-10-21 13:06:22 -0400

    testing: add delete-sa-06

---
+++ testing/pluto/delete-sa-06/westrun.sh
@@ -0,0 +1,13 @@
+# wait for east to initiate to us
+sleep 10
+ipsec whack --trafficstatus
+# sending delete/notify should cause east to re-initiate 
+ipsec auto --down west-east-auto
+# give Delete/Notify some time
+sleep 5
+# A new IPsec SA should be established (without patch takes 30 seconds)
+ipsec whack --trafficstatus
+sleep 20
+ipsec whack --trafficstatus
+sleep 10
+ipsec whack --trafficstatus
