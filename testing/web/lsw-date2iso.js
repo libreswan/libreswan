8f6e30e43caaf7fdb5112bff8eda700c1e23eb86
Andrew Cagney <cagney@gnu.org>
2016-11-17 09:56:48 -0500

    web: include seconds in displayed dates
    Some commits happen less than a minute apart, including seconds
    ensures that they correctly sort.
    For instance: "2016-11-15 16:39:11" and "2016-11-15 16:39:20"

--- testing/web/lsw-date2iso.js
+++ testing/web/lsw-date2iso.js
@@ -2,6 +2,6 @@
 function lsw_date2iso(d) {
     // Force to UTC?
     d = d.toISOString()
-    d = d.match("([^T]*)T([0-9]*:[0-9]*)")
+    d = d.match("([^T]*)T([0-9]*:[0-9]*:[0-9]*)")
     return d[1] + " " + d[2]
 }
