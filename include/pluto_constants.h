libreswan/libreswan

commit dc436423e64e7b66c578d1e2ed77c8164144bfc2

pluto: the pending code fpr phase2 misfired for IKEv2 connections
There is an EVENT_PENDING_PHASE2 that is scheduled every 2 minutes.
It checks if there are pending IPsec SA negotiations that are waiting
for an IKE negotiation to complete. If this takes longer than 120s,
it shoots the IKE negotiation and starts a new one.

When it was looking for pending IKE negotiations, it used the macro
PHASE1_INITIATOR_STATES which did not include STATE_PARENT_I1 or
STATE_PARENT_I2. So it would never find the currently failing IKEv2
state, and call ipsec_doi to start a new one, duplicating the connections.

This meant we were duplicating failed IKEv2 negotiations every 120
seconds.

include/pluto_constants.h
@@ -453,7 +453,11 @@ enum original_role {
 				  LELEM(STATE_AGGR_I2) | \
 				  LELEM(STATE_XAUTH_I0) | \
 				  LELEM(STATE_XAUTH_I1) | \
-				  LELEM(STATE_MODE_CFG_I1))
+				  LELEM(STATE_MODE_CFG_I1) | \
+                                 LELEM(STATE_MODE_CFG_I1) | \
+                                 LELEM(STATE_PARENT_I1) | \
+                                 LELEM(STATE_PARENT_I2))
+
 
 #define IS_PHASE1_INIT(s) ((LELEM(s) & PHASE1_INITIATOR_STATES) != LEMPTY)
 

programs/pluto/pending.c
@@ -48,7 +48,7 @@
 #include "ikev1_quick.h"
 #include "timer.h"
 
-/* struct pending, the structure representing Quick Mode
+/* struct pending, the structure representing IPsec SA
  * negotiations delayed until a Keying Channel has been negotiated.
  * Essentially, a pending call to quick_outI1.
  */
@@ -68,7 +68,10 @@ struct pending {
 	struct pending *next;
 };
 
-/* queue a Quick Mode negotiation pending completion of a suitable Main Mode */
+/*
+ * queue an IPsec SA negotiation pending completion of a
+ * suitable phase 1 (IKE SA)
+ */
 void add_pending(int whack_sock,
 		 struct state *isakmp_sa,
 		 struct connection *c,
@@ -82,14 +85,14 @@ void add_pending(int whack_sock,
 {
 	struct pending *p, **pp;
 
-	/* look for duplicate pending phase #2, skip add operation */
+	/* look for duplicate pending IPsec SA's, skip add operation */
 	pp = host_pair_first_pending(c);
 
 	for (p = pp ? *pp : NULL; p != NULL; p = p->next) {
 		if (p->connection == c && p->isakmp_sa == isakmp_sa) {
 			DBG(DBG_CONTROL, {
 				ipstr_buf b;
-				DBG_log("Ignored already queued up pending Quick Mode with %s \"%s\"",
+				DBG_log("Ignored already queued up pending IPsec SA negotiation with %s \"%s\"",
 					ipstr(&c->spd.that.host_addr, &b),
 					c->name);
 			});
@@ -117,7 +120,7 @@ void add_pending(int whack_sock,
 	if (uctx != NULL) {
 		p->uctx = clone_thing(*uctx, "pending security context");
 		DBG(DBG_CONTROL,
-		    DBG_log("pending phase 2 with security context %s, %d",
+		    DBG_log("pending IPsec SA negotiation with security context %s, %d",
 			    p->uctx->sec_ctx_value,
 			    p->uctx->ctx.ctx_len));
 	}
@@ -159,7 +162,7 @@ void release_pending_whacks(struct state *st, err_t story)
 				passert(whack_log_fd == NULL_FD);
 				whack_log_fd = p->whack_sock;
 				whack_log(RC_COMMENT,
-					  "%s for ISAKMP SA, but releasing whack for pending IPSEC SA",
+					  "%s for IKE SA, but releasing whack for pending IPSEC SA",
 					  story);
 				whack_log_fd = NULL_FD;
 			}

commit 7cfd94723e0e892353004545d87ebc61c700c779

ikev2: fix ipsec auto --start | --up for aliased connection.
The commit dc436423e broke this while fixing "phase2 misfired for IKEv2
connections". This patch address it in a different way.

include/pluto_constants.h
@@ -509,9 +509,7 @@ enum original_role {
 				  LELEM(STATE_AGGR_I2) | \
 				  LELEM(STATE_XAUTH_I0) | \
 				  LELEM(STATE_XAUTH_I1) | \
-				  LELEM(STATE_MODE_CFG_I1) | \
-				  LELEM(STATE_PARENT_I1) | \
-				  LELEM(STATE_PARENT_I2))
+				  LELEM(STATE_MODE_CFG_I1))
 
 
 #define IS_PHASE1_INIT(s) ((LELEM(s) & PHASE1_INITIATOR_STATES) != LEMPTY)
@@ -534,6 +532,9 @@ enum original_role {
 #define IS_ISAKMP_AUTHENTICATED(s) (STATE_MAIN_R3 <= (s) \
 				    && STATE_AGGR_R0 != (s) \
 				    && STATE_AGGR_I1 != (s))
+
+#define IKEV2_ISAKMP_INITIATOR_STATES (LELEM(STATE_PARENT_I1) |\
+					LELEM(STATE_PARENT_I2))
 
 #define ISAKMP_SA_ESTABLISHED_STATES  (LELEM(STATE_MAIN_R3) | \
 				       LELEM(STATE_MAIN_I4) | \

programs/pluto/initiate.c
@@ -1533,12 +1533,13 @@ void connection_check_phase2(void)
 			char cib[CONN_INST_BUF];
 
 			libreswan_log(
-				"pending Quick Mode with %s \"%s\"%s took too long -- replacing phase 1",
+				"pending IPsec SA negotiation with %s \"%s\"%s took too long -- replacing phase 1",
 				ipstr(&c->spd.that.host_addr, &b),
 				c->name, fmt_conn_instance(c, cib));
 
 			p1st = find_phase1_state(c,
 						 ISAKMP_SA_ESTABLISHED_STATES |
+						 IKEV2_ISAKMP_INITIATOR_STATES |
 						 PHASE1_INITIATOR_STATES);
 
 			if (p1st != NULL) {

programs/pluto/ipsec_doi.c
@@ -249,8 +249,9 @@ void ipsecdoi_initiate(int whack_sock,
 #endif
 		       )
 {
-	/* If there's already an ISAKMP SA established, use that and
-	 * go directly to Quick Mode.  We are even willing to use one
+	/*
+         * If there's already an IKEv1 ISAKMP SA established, use that and
+         * go directly to Quick Mode.  We are even willing to use one
 	 * that is still being negotiated, but only if we are the Initiator
 	 * (thus we can be sure that the IDs are not going to change;
 	 * other issues around intent might matter).