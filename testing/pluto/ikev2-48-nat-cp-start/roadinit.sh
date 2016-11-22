6098f7a9c27a34d63666a88e2340904589b7429c
Antony Antony <antony@phenome.org>
2016-11-22 09:15:06 +0000

    testing update ikev2-48-nat-cp and add ikev2-48-nat-cp-start

--- testing/pluto/ikev2-48-nat-cp-start/roadinit.sh
+++ testing/pluto/ikev2-48-nat-cp-start/roadinit.sh
@@ -2,5 +2,4 @@
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --debug-all --impair-retransmits
-ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
 echo "initdone"
--- testing/pluto/ikev2-48-nat-cp-start/roadrun.sh
+++ testing/pluto/ikev2-48-nat-cp-start/roadrun.sh
@@ -1,7 +1,5 @@
-ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
-# AA see really weired route, I have to remove that
+sleep 4
 route -n
-route del -net 192.1.2.23 netmask 255.255.255.255
 # ipsec will configure 192.0.2.1 on eth0
 ip addr show  dev eth0
 ping -n -c 2 -I 192.0.2.1 192.1.2.23
