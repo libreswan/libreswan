3eab0d2f2071f577cd531ebd8a1b45f83f0d0258
Andrew Cagney <cagney@gnu.org>
2016-12-18 20:53:14 -0500

    web: speed up earliest-commit.sh by using 'git merge-base --octopus'
    and don't stumble over unknown commits.

--- testing/web/earliest-commit.sh
+++ testing/web/earliest-commit.sh
@@ -7,6 +7,9 @@ Usage:
 
     $0 <repodir> <summarydir>
 
+Identify the git revision of the earliest test result (based on commit
+rank) in <summarydir>.
+
 EOF
     exit 1
 fi
@@ -16,9 +19,30 @@ webdir=$(dirname $0)
 repodir=$1 ; shift
 summarydir=$1 ; shift
 
-for dir in ${summarydir}/*-g*-* ; do
-    test -d ${dir} || continue
-    hash=$(${webdir}/gime-git-rev.sh ${dir})
-    rank=$(${webdir}/gime-git-rank.sh ${repodir} ${hash})
-    echo ${rank} ${hash}
-done | sort -r -n | tail -1 | awk '{print $2}'
+# Create a list of the earliest hashes.  Use xargs to keep the length
+# under control.
+#
+# Most likely this is just one hash, but as results get bigger you
+# never know.
+
+hashes=$(ls ${summarydir} \
+	    | xargs --no-run-if-empty \
+		    ${webdir}/gime-git-rev.sh \
+	    | xargs --no-run-if-empty \
+		    git show --ignore-missing --no-patch --format=%h \
+	    | xargs --no-run-if-empty \
+		    git merge-base --octopus )
+if test -z "${hashes}" ; then
+    echo "No results in ${summarydir}" 1>&2
+    exit 0
+fi
+
+# determine the first of them; need to pretty print the result (git
+# merge-base doesn't handle --format).
+#
+# Assume that ${hashes} isn't so long that more than two iterations of
+# merge-base are needed.  If that turns out to be false then "fixing"
+# merge-base to take stdin would be better.
+
+git show --no-patch --format=%h \
+    $(git merge-base --octopus ${hashes})
