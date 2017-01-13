e4afb623bbc7ee31e278af58903679994e473167
Paul Wouters <pwouters@redhat.com>
2017-01-13 14:44:16 -0500

    testing: delete-sa-09-rhbz1311360-c32 fixup

--- testing/pluto/delete-sa-09-rhbz1311360-c32/westrun.sh
+++ testing/pluto/delete-sa-09-rhbz1311360-c32/westrun.sh
@@ -3,7 +3,4 @@ ipsec auto --up  westnet-eastnet
 sleep 1
 ipsec status |grep STATE
 ipsec auto --down  west-east
-sleep 1
-# Expecting the IKE SA of west-east and the IPsec SA of westnet-eastnet
-ipsec status |grep STATE
 echo done
