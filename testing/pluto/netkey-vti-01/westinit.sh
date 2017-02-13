32f27cb470e97f48e3b12b4f2ac704cb5cc10441
Paul Wouters <pwouters@redhat.com>
2017-02-13 13:25:43 -0500

    testing: fixup netkey-vti-01

--- testing/pluto/netkey-vti-01/westinit.sh
+++ testing/pluto/netkey-vti-01/westinit.sh
@@ -1,4 +1,9 @@
 /testing/guestbin/swan-prep
+# We want to test without a specific existing non-device route,
+# so we remove the regular route for 192.0.2.0/24, and add default route 
+ip route del 192.0.2.0/24
+ip route del default
+ip route add 0.0.0.0/0 via 192.1.2.23 dev eth1
 # confirm that the network is alive
 ../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # make sure that clear text does not get through
@@ -9,6 +14,4 @@ ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-vti
-# remove the regular route for 192.0.2.0/24
-#ip route del 192.0.2.0/24
 echo "initdone"
