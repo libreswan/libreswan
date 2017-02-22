e55c7991b9b7912733024dcc24bbc0c3f1aa10cf
Paul Wouters <pwouters@redhat.com>
2017-02-22 12:51:44 -0500

    Revert "pluto: add support for "~" to whack_log()"
    This reverts commit c23a58933e4d577eb3d4890439a47459e95d661a.
    
    Unexptedly reduces too many prefixes :(

--- programs/pluto/log.c
+++ programs/pluto/log.c
@@ -563,11 +563,7 @@ void whack_log(int mess_no, const char *message, ...)
 	if (wfd != NULL_FD || dying_breath) {
 		va_list args;
 		char m[LOG_WIDTH]; /* longer messages will be truncated */
-		int prelen = 0;
-
-		/* support to suppress numeral prefix using ~ */
-		if (message[0] != '~')
-			prelen = snprintf(m, sizeof(m), "%03d ", mess_no);
+		int prelen = snprintf(m, sizeof(m), "%03d ", mess_no);
 
 		passert(prelen >= 0);
 
