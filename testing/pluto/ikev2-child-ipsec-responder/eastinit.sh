80e1d60f68cb93b40c262f0c7f40fbca24ec2151
Antony Antony <antony@phenome.org>
2017-02-24 22:25:36 +0100

    testing: add ikev2-child-ipsec-responder

---
+++ testing/pluto/ikev2-child-ipsec-responder/eastinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep
+ip addr add 192.0.200.254/24 dev eth0:1
+ip route add 192.0.100.0/24 via 192.1.2.45  dev eth1
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+# ipsec auto --add westnet-eastnet-ikev2a # this conn has auto=start
+ipsec auto --add westnet-eastnet-ikev2b
+echo "initdone"
