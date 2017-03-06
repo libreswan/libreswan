c483d2d7dad359f71243d52948f723f22a1f1b34
Antony Antony <antony@phenome.org>
2017-03-06 13:55:22 +0100

    testing: update testcase certoe-12-nat-server

--- testing/pluto/certoe-12-nat-server/final.sh
+++ testing/pluto/certoe-12-nat-server/final.sh
@@ -1,5 +1,5 @@
 # A tunnel should have established with non-zero byte counters
-ping -n -c 4 192.1.2.23
+ip addr show scope global
 # jacob two two for east?
 ipsec whack --trafficstatus 
 ipsec whack --trafficstatus 
@@ -8,6 +8,9 @@ ipsec look
 grep IKEv2_AUTH_ /tmp/pluto.log 
 : ==== cut ====
 ipsec auto --status
+ipsec stop
+#check the ip extra ip address/sourceip address is removed
+ip addr show scope global
 : ==== tuc ====
 if [ -n "`ls /tmp/core* 2>/dev/null`" ]; then echo CORE FOUND; mv /tmp/core* OUTPUT/; fi
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
