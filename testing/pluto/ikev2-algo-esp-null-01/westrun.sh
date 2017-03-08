953d7326ce4141347733cc117ba5d5f401ea3947
Andrew Cagney <cagney@gnu.org>
2017-03-08 13:44:04 -0500

    testing: add ikev2-algo-esp-null-01 and interop-ikev2-strongswan-34-esp-null-responder

---
+++ testing/pluto/ikev2-algo-esp-null-01/westrun.sh
@@ -0,0 +1,4 @@
+../bin/libreswan-up-down.sh westnet-eastnet-esp-null-md5 -I 192.0.1.254 192.0.2.254
+../bin/libreswan-up-down.sh westnet-eastnet-esp-null-sha1 -I 192.0.1.254 192.0.2.254
+
+echo done
