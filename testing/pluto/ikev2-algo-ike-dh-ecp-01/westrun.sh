fd05d6f6ba44b721c3c6c84f6c15c8068dd3ae7b
Andrew Cagney <cagney@gnu.org>
2017-03-13 19:46:13 -0400

    testing: use libreswan-up-down.sh in ikev2-algo-ike-dh-ecp-01

--- testing/pluto/ikev2-algo-ike-dh-ecp-01/westrun.sh
+++ testing/pluto/ikev2-algo-ike-dh-ecp-01/westrun.sh
@@ -1,20 +1,5 @@
-
-ipsec auto --add westnet-eastnet-ikev2-dh19
-ipsec auto --up  westnet-eastnet-ikev2-dh19
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
-ipsec auto --down  westnet-eastnet-ikev2-dh19
-ipsec auto --delete  westnet-eastnet-ikev2-dh19
-
-ipsec auto --add westnet-eastnet-ikev2-dh20
-ipsec auto --up  westnet-eastnet-ikev2-dh20
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
-ipsec auto --down  westnet-eastnet-ikev2-dh20
-ipsec auto --delete  westnet-eastnet-ikev2-dh20
-
-ipsec auto --add westnet-eastnet-ikev2-dh21
-ipsec auto --up  westnet-eastnet-ikev2-dh21
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
-ipsec auto --down  westnet-eastnet-ikev2-dh21
-ipsec auto --delete  westnet-eastnet-ikev2-dh21
+../bin/libreswan-up-down.sh ikev2-ike=aes128-sha1-dh19 -I 192.0.1.254 192.0.2.254
+../bin/libreswan-up-down.sh ikev2-ike=aes128-sha1-dh20 -I 192.0.1.254 192.0.2.254
+../bin/libreswan-up-down.sh ikev2-ike=aes128-sha1-dh21 -I 192.0.1.254 192.0.2.254
 
 echo done
