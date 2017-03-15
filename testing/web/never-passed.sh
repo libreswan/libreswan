88a9789476072bd945171adc99b82fc0436edbfc
Andrew Cagney <cagney@gnu.org>
2017-03-14 20:18:09 -0400

    web: in never-passed.sh print #nr of last test to pass

--- testing/web/never-passed.sh
+++ testing/web/never-passed.sh
@@ -21,14 +21,14 @@ cd $1
 # ./run/TESTLIST
 runs=$(find . -name TESTLIST -print -prune | cut -d/ -f2 | sort -n -r)
 
-# Need to ignore first directory as that is "in-progress"; should just
-# trab directories[1]
+# Only finished test runs have a TESTLIST file?
 TESTLIST=$(set - ${runs} ; echo $1)/TESTLIST
 if test -z "${TESTLIST}" ; then
     echo $TESTLIST not found 1>&2
     exit 1
 fi
 echo ${TESTLIST} 1>&2
+echo sort -k 1n -k 4nr 1>&2
 
 while read kind test expectation junk ; do
     case "${kind}" in
@@ -42,6 +42,7 @@ while read kind test expectation junk ; do
     passed=0
     failed=0
     total=0
+    last=0
     for run in ${runs} ; do
 	result=${run}/${test}/OUTPUT/RESULT
 	if test ! -r ${result} ; then
@@ -52,11 +53,12 @@ while read kind test expectation junk ; do
 	total=$((${total} + 1))
 	if grep -e '"result" *: *"passed"' < ${result} > /dev/null ; then
 	    passed=$((${passed} + 1))
+	    last=${total}
 	else
 	    failed=$((${failed} + 1))
 	fi
     done
     # ouput in a format that is friendly to commands like 'sort -n -r'
     # and 'sort -k 1n -k 3nr'
-    echo ${passed} ${failed} ${total} ${test}
+    echo ${passed} ${last} ${failed} ${total} ${test}
 done < ${TESTLIST}
