3f607d178929996931dc56299e2e0816eece2f89
Andrew Cagney <cagney@gnu.org>
2017-04-05 09:21:27 -0400

    pluto: delete an unused definition of RETURN_STF_FAILURE

--- programs/pluto/ipsec_doi.c
+++ programs/pluto/ipsec_doi.c
@@ -83,19 +83,6 @@
 
 #include "pluto_stats.h"
 
-/* MAGIC: perform f, a function that returns notification_t
- * and return from the ENCLOSING stf_status returning function if it fails.
- */
-/* ??? why are there so many copies of this routine (ikev2.h, ikev1_continuations.h, ipsec_doi.c).
- * Sometimes more than one copy is defined!
- */
-#define RETURN_STF_FAILURE(f) { \
-	notification_t res = (f); \
-	if (res != NOTHING_WRONG) { \
-		  return STF_FAIL + res; \
-	} \
-}
-
 /*
  * Process KE values.
  */
