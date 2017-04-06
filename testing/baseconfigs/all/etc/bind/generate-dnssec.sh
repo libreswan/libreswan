707f94761ac085fa7c3ee48683c695f15c132447
Paul Wouters <pwouters@redhat.com>
2017-04-06 00:35:09 -0400

    testing: added generate-dnssec.sh
    This script in testing/baseconfigs/all/etc/bind/ generates DNSSEC
    keys and signs all the zones.

---
+++ testing/baseconfigs/all/etc/bind/generate-dnssec.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DIR=$(dirname $0)
+pushd $DIR
+
+rm -f keys/K* signed/*.signed dsset/dsset*
+
+for zone in *arpa *libreswan.org;
+do
+dnssec-keygen -K keys -b 1024        -a RSASHA256 -n ZONE $zone;
+dnssec-keygen -K keys -b 2048 -f KSK -a RSASHA256 -n ZONE $zone;
+dnssec-signzone -S -K keys -x -f signed/${zone}.signed $zone
+mv dsset-* dsset/
+done
+popd
+
