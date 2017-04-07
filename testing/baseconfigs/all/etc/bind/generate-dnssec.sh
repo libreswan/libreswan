184648dc7dad16e6d09d8bced970119c9b1547d2
Paul Wouters <pwouters@redhat.com>
2017-04-06 22:14:40 -0400

    testing: create dsset/dsset.all to include with unbound
    unbound doesn't support wildecards for loading trust keys :/

--- testing/baseconfigs/all/etc/bind/generate-dnssec.sh
+++ testing/baseconfigs/all/etc/bind/generate-dnssec.sh
@@ -11,6 +11,7 @@ dnssec-keygen -K keys -b 1024        -a RSASHA256 -n ZONE $zone;
 dnssec-keygen -K keys -b 2048 -f KSK -a RSASHA256 -n ZONE $zone;
 dnssec-signzone -S -K keys -x -f signed/${zone}.signed $zone
 mv dsset-* dsset/
+cat dsset/dsset-* >> dsset/dsset.all
 done
 popd
 
