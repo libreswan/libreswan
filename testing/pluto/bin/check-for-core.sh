7b16bb207adeb89c446fec6669581bef15725bc7
Andrew Cagney <cagney@gnu.org>
2017-04-06 20:27:10 -0400

    testing: replace check-for-core with check-for-core.sh
    use in addconn-01

---
+++ testing/pluto/bin/check-for-core.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ls /tmp/core* 2>/dev/null | while read core ; do
+
+    echo
+    echo CORE FOUND: $core
+
+    exe=$(echo $core | cut -d. -f3)
+    if test -r /usr/local/libexec/ipsec/${exe} ; then
+	prog=/usr/local/libexec/ipsec/${exe}
+    else
+	prog=""
+    fi
+
+    if test -n "${prog}" ; then
+	echo
+	gdb -q ${prog} ${core} <<EOF
+bt
+quit
+EOF
+	echo
+    fi
+
+    mv -f $core OUTPUT/
+done
