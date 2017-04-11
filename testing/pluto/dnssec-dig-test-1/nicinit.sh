1997f70057015e4f0bee9a989294a06d764cba24
Antony Antony <antony@phenome.org>
2017-04-11 14:38:48 +0200

    testing: dnssec-dig-test-1 check if unbound.service is copied over

--- testing/pluto/dnssec-dig-test-1/nicinit.sh
+++ testing/pluto/dnssec-dig-test-1/nicinit.sh
@@ -1,9 +1,9 @@
 iptables -t nat -F
 iptables -F
-iptables -t nat -L
-time /testing/guestbin/swan-prep --dnssec
-ps -ax
+time /testing/guestbin/swan-prep --dnssec 2>&1
 ls -lt /var/lib/unbound/
 grep "root.key" /etc/unbound/unbound.conf
+ls -lt /etc/systemd/system/unbound.service
+ps -ax
 echo done
 : ==== end ====
