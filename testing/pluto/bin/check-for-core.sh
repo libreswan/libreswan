5637a8ce4de31227ce8914e44c68399ccb1479ac
Andrew Cagney <cagney@gnu.org>
2017-04-13 12:41:13 -0400

    testing: restore 'CORE FOUND: ...' message in check-for-core.sh
    The sed used for 2993cb0e70804cca9c58852bb30d9669fb7b7606 was
    a tad too agressive.

--- testing/pluto/bin/check-for-core.sh
+++ testing/pluto/bin/check-for-core.sh
@@ -3,6 +3,11 @@
 ls /tmp/core* 2>/dev/null | while read core ; do
 
     echo
+
+    # Note: Post-mortem scripts use the text "CORE FOUND" in the
+    # console output as a marker for a test failing with a core dump.
+    echo CORE FOUND: $core
+
     exe=$(echo $core | cut -d. -f3)
     if test -r /usr/local/libexec/ipsec/${exe} ; then
 	prog=/usr/local/libexec/ipsec/${exe}
