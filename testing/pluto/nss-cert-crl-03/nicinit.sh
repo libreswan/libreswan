ba8202ee11db2434e9bfa1159ebbe130a1d79345
Andrew Cagney <cagney@gnu.org>
2017-05-01 12:54:10 -0400

    testing: add script to start the CRL server in OUTPUT/ and wait for it to start
    Fixes a race where the CRL http server's starting message randomly
    appeared in the output and the sources ended up containing revoked.crl.

--- testing/pluto/nss-cert-crl-03/nicinit.sh
+++ testing/pluto/nss-cert-crl-03/nicinit.sh
@@ -1,8 +1,4 @@
 # set up the crl server
-cp /testing/x509/crls/cacrlvalid.crl /testing/pluto/nss-cert-crl-03/revoked.crl
-cd /testing/pluto/nss-cert-crl-03
-# in case httpd runs for freeipa tests, shut down httpd
-service httpd stop > /dev/null 2> /dev/null
-/usr/bin/python -m SimpleHTTPServer 80 &
-echo "done."
+cp ../../x509/crls/cacrlvalid.crl OUTPUT/revoked.crl
+../bin/simple-http-server.sh OUTPUT 80
 : ==== end ====
