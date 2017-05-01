bd42083a10d59d35cdfc9e21fa744d2c769ed817
Paul Wouters <pwouters@redhat.com>
2017-04-30 21:46:15 -0400

    testing: remove obsoleted tests

--- testing/pluto/phase1-expire-01-reconnect-klips/eastinit.sh
+++
@@ -1,9 +0,0 @@
-: ==== start ====
-TESTNAME=phase1-expire-01-reconnect-klips
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-
-ipsec auto --add westnet-eastnet
-ipsec whack --debug-control --debug-controlmore --debug-crypt
