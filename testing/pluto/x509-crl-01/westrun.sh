7e33f1afbb07fb082286b32af9127be7f3c54686
Paul Wouters <pwouters@redhat.com>
2017-05-01 15:44:50 -0400

    testing: remove old x509-crl* tests (see nss-cert-crl-* for new tests)

--- testing/pluto/x509-crl-01/westrun.sh
+++
@@ -1,3 +0,0 @@
-ipsec auto --up  westnet-eastnet-x509-cr
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
-ipsec whack --trafficstatus
--- testing/pluto/x509-crl-05/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep --x509
-cp /testing/x509/crls/cacrlvalid.crl /etc/ipsec.d/crls
-iptables -A INPUT -i eth1 -s 192.0.1.0/24 -d 0.0.0.0/0 -j DROP
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add east-west-crlstrict
