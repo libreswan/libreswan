c21b16b3c8c712d7ed10274c458351efb5bd6e41
Paul Wouters <pwouters@redhat.com>
2017-04-30 21:49:01 -0400

    testing: removed obsoleted tests

--- testing/pluto/algo-pluto-05/eastinit.sh
+++
@@ -1,9 +0,0 @@
-: ==== start ====
-TESTNAME=algo-pluto-05
-source /testing/pluto/bin/eastlocal.sh
-
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-
-ipsec auto --add westnet-eastnet-both
-echo done
