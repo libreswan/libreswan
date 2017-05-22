9da04e4ec56634d34e6b0bcf0266975c626433c1
Antony Antony <antony@phenome.org>
2017-05-22 20:04:39 +0200

    testing: update from ikev2-fetch-ipseckey

---
+++ testing/pluto/ikev2-dnssec-fqdn-01/roadinit.sh
@@ -0,0 +1,9 @@
+/testing/guestbin/swan-prep
+# make sure that clear text does not get through
+iptables -A INPUT -i eth0 -s 192.0.2.0/24 -j LOGDROP
+iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add rad-eastnet-fqdn-ikev2
+ipsec auto --status | grep rad-eastnet-fqdn-ikev2
+echo "initdone"
