c2141531a1a69406ef3c5baff129345048446ad7
Antony Antony <antony@phenome.org>
2017-05-22 19:56:31 +0200

    dnssec: update include/dnssec.h

--- include/dnssec.h
+++ include/dnssec.h
@@ -1,17 +1,35 @@
-
-/*
- * We should not need to include any DNSSEC keys, but we do not (yet) want to depend on that.
- * These keys are valid now (2012 Q1) and should remain valid for many years
- */
-
 #ifndef _DNSSEC_H
 # define _DNSSEC_H
 
+#include <event2/event.h>
 #include <unbound.h>
 
-extern struct ub_ctx *unbound_init(bool do_dnssec, const char *rootfile, const char *trusted);
 
-extern bool unbound_resolve(struct ub_ctx *dnsctx, char *src, size_t srclen,
-			   int af, ip_address *ipaddr);
+extern void unbound_ctx_free(void);
+extern void unbound_sync_init(bool do_dnssec, const char *rootfile,
+			 const char *trusted);
+
+extern void unbound_event_init(struct event_base *eb, bool do_dnssec,
+		const char *rootfile, const char *trusted);
+
+extern bool unbound_resolve(char *src, size_t srclen, int af,
+				ip_address *ipaddr);
+
+extern struct ub_ctx * get_unbound_ctx(void);
+
+/*
+ * returned in callback of ub_resolve_event
+ * with sec: 0 if insecure, 1 if bogus, 2 if DNSSEC secure.
+ * note these constants are only for ub_resolve_event. other resolv function
+ * my have different values for secure.
+ *
+ *  Seperate from this is ub_result.secure which is true or falsea,
+ *  returned by ub_resolve or ub_resove_async
+ */
+enum lswub_resolve_event_secure_kind {
+	UB_EVENT_INSECURE	= 0,
+	UB_EVENT_BOGUS		= 1,
+	UB_EVNET_SECURE		= 2,
+};
 
 #endif
