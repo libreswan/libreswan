2638a7bcc5865b4f3ea97e38484ee95f5f19e3a9
Paul Wouters <pwouters@redhat.com>
2017-05-21 23:50:09 -0400

    pluto: remember st->st_peer_alt_id across state machine runs

--- programs/pluto/ikev1.c
+++ programs/pluto/ikev1.c
@@ -2843,7 +2843,6 @@ bool ikev1_decode_peer_id(struct msg_digest *md, bool initiator, bool aggrmode)
 			} else {
 				libreswan_log("Peer ID matches and no better connection found - continuing with existing connection");
 				r = c;
-				md->st->st_peer_alt_id = FALSE; /* no longer used but better safe than sorry */
 			}
 		}
 
