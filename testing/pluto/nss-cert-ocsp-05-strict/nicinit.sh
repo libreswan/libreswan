1632adb209cee364cd1ee5971f3b573c65608716
Paul Wouters <pwouters@redhat.com>
2017-05-23 23:09:26 -0400

    testing: fixup crl/ocsp tests

--- testing/pluto/nss-cert-ocsp-05-strict/nicinit.sh
+++ testing/pluto/nss-cert-ocsp-05-strict/nicinit.sh
@@ -5,7 +5,7 @@ cp /testing/x509/cacerts/mainca.crt /etc/ocspd/certs/mainca.pem
 cp /testing/x509/ocspd.conf /etc/ocspd/
 openssl crl -inform DER -in /testing/x509/crls/cacrlvalid.crl -outform PEM -out /etc/ocspd/crls/revoked_crl.pem
 #stock ocspd.conf, used separate ones for different configs
-restoreon -R /etc/ocspd
+restorecon -R /etc/ocspd
 ocspd -v -d -c /etc/ocspd/ocspd.conf
 echo "done."
 : ==== end ====
