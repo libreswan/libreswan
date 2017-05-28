24852926da2521806787acb355fb4cb093529056
Antony Antony <antony@phenome.org>
2017-05-28 09:58:58 +0000

    testing: update test, scripts, for 4a3dd3289
    ikev2-liveness-05, commit 4a3dd3289, put a hold rule on the TEMPLATE.

--- testing/pluto/ikev2-liveness-05/westrun.sh
+++ testing/pluto/ikev2-liveness-05/westrun.sh
@@ -14,17 +14,14 @@ sleep 30
 # But shuntstatus only shows bare shunts, not connection shunts :(
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
-# no packets should be caught in firewall and no icmp replies should happen
+# packets should be caught in firewall and no icmp replies should happen
 ping -w 2 -q -n -c 3 -I 192.0.1.254 192.0.2.254
 # Remove the Blockage
 iptables -D INPUT -s 192.1.2.23/32 -d 0/0 -j DROP
 iptables -D OUTPUT -d 192.1.2.23/32 -s 0/0 -j DROP
-# Sleep 90
-sleep 60
-sleep 30
+# trigger traffic
+ping -W 1 -w 1 -q -n -c 1 -I 192.0.1.254 192.0.2.254
+sleep 2
+ping -W 1 -w 4 -q -n -c 4 -I 192.0.1.254 192.0.2.254
 # Tunnel should be back up now even without triggering traffic
-ipsec whack --trafficstatus
-ipsec whack --shuntstatus
-# ping should reply
-ping -q -n -c 4 -I 192.0.1.254 192.0.2.254
 echo done
