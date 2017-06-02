f9ec7c35629a13432c0680a5c99658d081e15a13
Andrew Cagney <cagney@gnu.org>
2017-06-02 10:25:48 -0400

    testing: almost get dh negotiated PFS working in fips-11-ikev2-esp-dh
    except the teardown dumps core

--- testing/pluto/fips-11-ikev2-esp-dh/westrun.sh
+++ testing/pluto/fips-11-ikev2-esp-dh/westrun.sh
@@ -1,3 +1,5 @@
-../bin/libreswan-up-down.sh ikev2-esp=aes-sha1-modp2048 -I 192.0.1.254 192.0.2.254
-../bin/libreswan-up-down.sh ikev2-esp=aes-sha1-modp1024 -I 192.0.1.254 192.0.2.254
-echo done
+ipsec auto --add ikev2-base
+ipsec auto --up ikev2-base
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
+../bin/libreswan-up-down.sh ikev2-esp=aes-sha1-modp4096 -I 192.0.100.254 192.0.200.254
+ipsec auto --delete ikev2-base
