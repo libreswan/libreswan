fd89ccca6e23b871f30f15c06ae963aefe98c66f
Eric Curtin <ericcurtin17@gmail.com>
2017-06-12 22:38:49 +0100

    Updates to dockerfiles & travis build scripts
    Build, push, pull, list and run dockers. Also, update build script to
    use with Travis CI. Enable docker in .travis.yml file also.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,24 @@
 
 set -eu
 
-if [ $(uname) == "Linux" ]; then
-  sudo apt -qq update
-  sudo apt install -y libnspr4-dev
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$dir/.."
+
+if [ "$(uname)" != "Linux" ]; then
+  make -j3 all
+  make -j3 install
+  exit 0
 fi
 
-make all
-make install
+dockerfiles/docker.sh pull
+
+for doc in $(dockerfiles/docker.sh list); do
+  git clean -fdx
+  name=$(printf $doc | sed "s#curtine/##" | sed "s/:/-/")
+  docker run -v "$PWD":"$PWD" -h "$name" --name "$name" "$doc" \
+             /bin/bash -c "cd $PWD && \
+                           export CC=$CC && \
+                           make -j3 all && \
+                           make -j3 install"
+done
 
