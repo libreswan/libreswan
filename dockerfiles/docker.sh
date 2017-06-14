f80634d39f9565425964031dd8a00388f1d6ab7d
Eric Curtin <ericcurtin17@gmail.com>
2017-06-14 10:48:18 -0400

    building: Updates to dockerfiles & travis build scripts
    Build, push, pull, list and run dockers. Also, update build script to
    use with Travis CI. Enable docker in .travis.yml file also.
    
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

--- dockerfiles/docker.sh
+++ dockerfiles/docker.sh
@@ -2,11 +2,31 @@
 
 set -e
 
-thisDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $thisDir
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base="$(basename "$0")"
 
-if [ "$1" != "build" ] && [ "$1" != "pull" ] && [ "$1" != "push" ]; then
-  echo "Usage: $0 DIRECTORY" >&2
+cd "$dir"
+
+if [ "$1" != "build" ] && [ "$1" != "pull" ] && [ "$1" != "push" ] && \
+   [ "$1" != "list" ]; then
+  echo -e "Usage: $base arg\n" \
+          "arg should be build, pull, push or list"  >&2
   exit 1
 fi
+
+for dir in */*; do
+  docImg=$(printf "$dir" | sed 's#/#:#' | sed 's#^#curtine/libreswan-#')
+
+  if [ "$1" == "list" ]; then
+    printf "$docImg\n"
+    continue
+  fi
+
+  if [ "$1" == "build" ]; then
+    docker build -t "$docImg" "$dir"
+    continue
+  fi
+
+  docker "$1" "$docImg"
+done
 
