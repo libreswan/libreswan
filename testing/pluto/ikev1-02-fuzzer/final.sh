bf836f3c49f032fd1adeb2b144a74eae373edcb4
Paul Wouters <pwouters@redhat.com>
2017-06-14 00:25:36 -0400

    testing: added ikev1-02-fuzzer

---
+++ testing/pluto/ikev1-02-fuzzer/final.sh
@@ -0,0 +1,8 @@
+# shutting down, so output will change if we had already crashed
+ipsec whack --shutdown
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
