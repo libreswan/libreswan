23db1ab15faaeb4d1335b38fca78456d8e851d5f
Andrew Cagney <cagney@gnu.org>
2017-06-16 14:11:23 -0400

    algparse: move to programs/ directory
    and the test output to basic-pluto-00 where it is used
    (kvm tests can't reach into the source tree)

--- testing/pluto/basic-pluto-00/westrun.sh
+++ testing/pluto/basic-pluto-00/westrun.sh
@@ -1,4 +1,4 @@
 # diffs should be empty
-/usr/local/libexec/ipsec/algparse -v | diff -u - ../../algparse/OUTPUT.algparse.txt
+/usr/local/libexec/ipsec/algparse -v | diff -u OUTPUT.algparse.txt -
 /usr/local/libexec/ipsec/enumcheck | diff -u - ../../enumcheck/OUTPUT.enumcheck.txt
 echo done
