11a83ab12d65294dc2b16ef96b11cc220e3f2018
Antony Antony <antony@phenome.org>
2017-07-04 09:09:17 +0200

    testing: remove rsasig2-0X it, rsasigkey2 is not fully supported

--- testing/pluto/rsasig2-01/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-certutil -F -n 61559973d3acef7d3a370e3e82ad92c18a8225f1 -d sql:/etc/ipsec.d
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-2
-echo "initdone"
