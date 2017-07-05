16dc89656f26d51a2d0cb40d619d514269999a34
D. Hugh Redelmeier <hugh@mimosa.com>
2017-07-04 23:51:17 -0400

    testing: ikev2-16-alias-whack-start spell "north" correctly

--- testing/pluto/ikev2-16-alias-whack-start/eastinit.sh
+++ testing/pluto/ikev2-16-alias-whack-start/eastinit.sh
@@ -2,6 +2,6 @@
 ip addr show dev eth0 | grep 192.0.22.254 || (ip addr add 192.0.22.254/24 dev eth0)
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add norhtnet-eastnets
-ipsec auto --status | grep norhtnet-eastnets
+ipsec auto --add northnet-eastnets
+ipsec auto --status | grep northnet-eastnets
 echo "initdone"
