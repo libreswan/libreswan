libreswan/libreswan

commit 6806420bfec59c8fa6e44d0a95a52d6878c10a6e

pluto: Perform stricter SubjectAltName checks on configured ID's
Just validating a certificate is not enough if NOT using rightid=%fromcert

If an explicit ID is used, such as rightid=1.2.3.4 or rightid=@FQDN or
rightid=user@fqdn then those IDs MUST be present as SubjectAltName on
the received/configured certificate.

programs/pluto/x509.c
@@ -301,42 +301,13 @@ bool trusted_ca_nss(chunk_t a, chunk_t b, int *pathlen)
  */
 void select_nss_cert_id(CERTCertificate *cert, struct id *end_id)
 {
-	bool use_dn = FALSE;	/* ID is subject DN */
-
-	/* check for cert email addr first */
-	if (end_id->kind == ID_USER_FQDN) {
-		char email[IDTOA_BUF];
-
-		idtoa(end_id, email, IDTOA_BUF);
-		if (cert->emailAddr == NULL || !streq(cert->emailAddr, email)) {
-			DBG(DBG_X509,
-			    DBG_log("no email \'%s\' for cert, using ASN1 subjectName",
-						email));
-			use_dn = TRUE;
-		}
-	}
-
-	if (end_id->kind == ID_DER_ASN1_DN) {
-		chunk_t certdn = same_secitem_as_chunk(cert->derSubject);
-
-		if (!same_dn_any_order(end_id->name, certdn)) {
-			char idb[IDTOA_BUF];
-
-			idtoa(end_id, idb, IDTOA_BUF);
-			DBG(DBG_X509,
-			    DBG_log("no subject \'%s\' for cert, using ASN1 subjectName \'%s\'",
-						idb, cert->subjectName));
-			use_dn = TRUE;
-		}
-	}
-
-	if (end_id->kind == ID_FROMCERT || end_id->kind == ID_NONE || use_dn) {
+	if (end_id->kind == ID_FROMCERT) {
 		DBG(DBG_X509,
-		    DBG_log("setting ID to ID_DER_ASN1_DN: \'%s\'",
-			    cert->subjectName));
+                    DBG_log("setting ID to ID_DER_ASN1_DN: \'%s\'", cert->subjectName));
 		end_id->name = same_secitem_as_chunk(cert->derSubject);
 		end_id->kind = ID_DER_ASN1_DN;
 	}
+
 }
 
 static char *make_crl_uri_str(chunk_t *uri)
@@ -756,6 +727,9 @@ static bool pluto_process_certs(struct state *st, chunk_t *certs,
 #endif
 	bool cont = TRUE;
 	bool rev_opts[RO_SZ];
+	char namebuf[IDTOA_BUF];
+	char ipstr[IDTOA_BUF];
+	char sbuf[ASN1_BUF_LEN];
 
 	rev_opts[RO_OCSP] = ocsp_enable;
 	rev_opts[RO_OCSP_S] = ocsp_strict;
@@ -772,10 +746,88 @@ static bool pluto_process_certs(struct state *st, chunk_t *certs,
 	}
 
 	if ((ret & VERIFY_RET_OK) && end_cert != NULL) {
-		libreswan_log("certificate %s OK", end_cert->subjectName);
-		c->spd.that.cert.u.nss_cert = end_cert;
-		c->spd.that.cert.ty = CERT_X509_SIGNATURE;
-		add_rsa_pubkey_from_cert(&c->spd.that.id, end_cert);
+		libreswan_log("certificate verified OK: %s", end_cert->subjectName);
+		DBG(DBG_X509, DBG_log("Verifying configured ID matches certificate"));
+
+		switch(c->spd.that.id.kind) {
+		case ID_IPV4_ADDR:
+		case ID_IPV6_ADDR:
+			idtoa(&c->spd.that.id, ipstr, sizeof(ipstr));
+			if (cert_VerifySubjectAltName(end_cert, ipstr)) {
+				st->st_peer_alt_id = TRUE;
+				DBG(DBG_X509, DBG_log("ID_IP '%s' matched", ipstr));
+			} else {
+				loglog(RC_LOG_SERIOUS, "certificate does not contain ID_IP subjectAltName=%s",
+						ipstr);
+				return FALSE;
+			}
+			break;
+
+		case ID_FQDN:
+			/* We need to skip the "@" prefix from our configured FQDN */
+			idtoa(&c->spd.that.id, namebuf, sizeof(namebuf));
+
+			 if (cert_VerifySubjectAltName(end_cert, namebuf + 1)) {
+				st->st_peer_alt_id = TRUE;
+				DBG(DBG_X509, DBG_log("ID_FQDN '%s' matched", namebuf+1));
+			} else {
+				loglog(RC_LOG_SERIOUS, "certificate does not contain subjectAltName=%s",
+					namebuf + 1);
+				return FALSE;
+			}
+			break;
+
+		case ID_USER_FQDN:
+			idtoa(&c->spd.that.id, namebuf, sizeof(namebuf));
+			if (cert_VerifySubjectAltName(end_cert, namebuf)) {
+				st->st_peer_alt_id = TRUE;
+				DBG(DBG_X509, DBG_log("ID_USER_FQDN '%s' matched", namebuf));
+			} else {
+				loglog(RC_LOG_SERIOUS, "certificate does not contain ID_USER_FQDN subjectAltName=%s",
+					namebuf);
+				return FALSE;
+			}
+			break;
+
+		case ID_FROMCERT:
+			/* We don't care about ID type DN, since the CA already verified it */
+			st->st_peer_alt_id = TRUE;
+			idtoa(&c->spd.that.id, namebuf, sizeof(namebuf));
+			DBG(DBG_X509, DBG_log("ID_DER_ASN1_DN '%s' does not need further ID verification", namebuf));
+			break;
+
+		case ID_DER_ASN1_DN:
+			idtoa(&c->spd.that.id, namebuf, sizeof(namebuf));
+			dntoasi(sbuf, sizeof(sbuf), end_cert->derSubject);
+			DBG(DBG_X509, DBG_log("ID_DER_ASN1_DN '%s' needs further ID comparison against '%s'",
+				sbuf, namebuf));
+
+
+			struct id certid;
+			int wildcards; /* ignored? */
+			certid.name = same_secitem_as_chunk(end_cert->derSubject);
+			if (!match_id(&certid, &c->spd.that.id, &wildcards)) {
+				DBG(DBG_X509, DBG_log("ID_DER_ASN1_DN '%s' matched our ID", namebuf));
+				st->st_peer_alt_id = TRUE;
+			} else {
+				loglog(RC_LOG_SERIOUS, "ID_DER_ASN1_DN '%s' does not match expected '%s'",
+					end_cert->subjectName, namebuf);
+			}
+			break;
+		default:
+			loglog(RC_LOG_SERIOUS, "Unhandled ID type %d: %s",
+				c->spd.that.id.kind,
+				enum_show(&ike_idtype_names, c->spd.that.id.kind));
+				return FALSE;
+		}
+
+		if (st->st_peer_alt_id) {
+			DBG(DBG_X509, DBG_log("SAN ID matched, updating that.cert and adding pubkey from cert"));
+			c->spd.that.cert.u.nss_cert = end_cert;
+			c->spd.that.cert.ty = CERT_X509_SIGNATURE;
+			add_rsa_pubkey_from_cert(&c->spd.that.id, end_cert);
+			return TRUE;
+		}
 	} else if (ret & VERIFY_RET_REVOKED) {
 		libreswan_log("certificate revoked!");
 		cont = FALSE;
@@ -822,6 +874,7 @@ bool ikev1_decode_cert(struct msg_digest *md)
 	bool ret = TRUE;
 	int der_num = 0;
 
+	DBG(DBG_X509, DBG_log("checking for CERT payloads"));
 	for (p = md->chain[ISAKMP_NEXT_CERT]; p != NULL; p = p->next) {
 		struct isakmp_cert *const cert = &p->payload.cert;
 
@@ -837,6 +890,7 @@ bool ikev1_decode_cert(struct msg_digest *md)
 	}
 
 	if (der_num > 0) {
+		DBG(DBG_X509, DBG_log("found at last one CERT payload, calling pluto_process_certs()"));
 		if (!pluto_process_certs(st, der_list, der_num)) {
 			libreswan_log("Peer public key is not available for this exchange");
 			ret = FALSE;
@@ -850,7 +904,6 @@ bool ikev1_decode_cert(struct msg_digest *md)
 }
 
 /* Decode IKEV2 CERT Payload */
-
 bool ikev2_decode_cert(struct msg_digest *md)
 {
 	struct state *st = md->st;
@@ -859,6 +912,7 @@ bool ikev2_decode_cert(struct msg_digest *md)
 	bool ret = TRUE;
 	int der_num = 0;
 
+	DBG(DBG_X509, DBG_log("checking for CERT payloads"));
 	for (p = md->chain[ISAKMP_NEXT_v2CERT]; p != NULL; p = p->next) {
 		struct ikev2_cert *const v2cert = &p->payload.v2cert;
 
@@ -874,6 +928,7 @@ bool ikev2_decode_cert(struct msg_digest *md)
 	}
 
 	if (der_num > 0) {
+		DBG(DBG_X509, DBG_log("found at last one CERT payload, calling pluto_process_certs()"));
 		if (!pluto_process_certs(st, der_list, der_num)) {
 			libreswan_log("Peer public key is not available for this exchange");
 			ret = FALSE;

programs/pluto/ikev1.c
@@ -2750,9 +2750,11 @@ bool ikev1_decode_peer_id(struct msg_digest *md, bool initiator, bool aggrmode)
 			      enum_show(&ike_idtype_names, id->isaid_idtype), buf);
 	}
 
-	/* check for certificates */
-	if (!ikev1_decode_cert(md))
-		return FALSE;
+	if (!aggrmode) {
+		/* check for certificates */
+		if (!ikev1_decode_cert(md))
+			return FALSE;
+	}
 
 	/* Now that we've decoded the ID payload, let's see if we
 	 * need to switch connections.
@@ -2765,8 +2767,10 @@ bool ikev1_decode_peer_id(struct msg_digest *md, bool initiator, bool aggrmode)
 		return TRUE;
 
 	if (initiator) {
-		if (!same_id(&st->st_connection->spd.that.id, &peer) &&
-		     id_kind(&st->st_connection->spd.that.id) != ID_FROMCERT) {
+		if (!st->st_peer_alt_id &&
+			!same_id(&st->st_connection->spd.that.id, &peer) &&
+			id_kind(&st->st_connection->spd.that.id) != ID_FROMCERT) {
+
 			char expect[IDTOA_BUF],
 			     found[IDTOA_BUF];
 
@@ -2818,20 +2822,28 @@ bool ikev1_decode_peer_id(struct msg_digest *md, bool initiator, bool aggrmode)
 
 		struct connection *r = NULL;
 
-		if ((auth_policy & ~POLICY_AGGRESSIVE) != LEMPTY) {
-			r = refine_host_connection(st, &peer, initiator,
+		/* aggresive mode already pinned ID in first packet */
+		if (!aggrmode) {
+			r = refine_host_connection(st, &peer, FALSE /* initiator */,
 				auth_policy, AUTH_UNSET /* ikev2 only */, &fromcert);
-			pexpect(r != NULL);
 		}
 
 		if (r == NULL) {
 			char buf[IDTOA_BUF];
 
 			idtoa(&peer, buf, sizeof(buf));
-			loglog(RC_LOG_SERIOUS,
-			       "no suitable connection for peer '%s'",
-			       buf);
-			return FALSE;
+			DBG(DBG_CONTROL, DBG_log(
+			       "no more suitable connection for peer '%s'", buf));
+			/* can we continue with what we had? */
+			if (!md->st->st_peer_alt_id &&
+				!same_id(&c->spd.that.id, &peer) &&
+				id_kind(&c->spd.that.id) != ID_FROMCERT) {
+					libreswan_log("Peer mismatch on first found connection and no better connection found");
+					return FALSE;
+			} else {
+				DBG(DBG_CONTROL, DBG_log("Peer ID matches and no better connection found - continuing with existing connection"));
+				r = c;
+			}
 		}
 
 		DBG(DBG_CONTROL, {

programs/pluto/ikev1_aggr.c
@@ -270,7 +270,7 @@ stf_status aggr_inI1_outR1(struct msg_digest *md)
 	}
 
 	/* warn for especially dangerous Aggressive Mode and PSK */
-	if ((c->policy & (POLICY_PSK & POLICY_AGGRESSIVE)) != LEMPTY) {
+	if (LIN(POLICY_PSK, c->policy) && LIN(POLICY_AGGRESSIVE, c->policy)) {
 		loglog(RC_LOG_SERIOUS,
 			"IKEv1 Aggressive Mode with PSK is vulnerable to dictionary attacks and is cracked on large scale by TLA's");
 	}
@@ -1165,7 +1165,7 @@ stf_status aggr_outI1(int whack_sock,
 	struct state *st;
 	struct spd_route *sr;
 
-	if ((c->policy & (POLICY_PSK & POLICY_AGGRESSIVE)) != LEMPTY) {
+	if (LIN(POLICY_PSK, c->policy) && LIN(POLICY_AGGRESSIVE, c->policy)) {
 		loglog(RC_LOG_SERIOUS,
 			"IKEv1 Aggressive Mode with PSK is vulnerable to dictionary attacks and is cracked on large scale by TLA's");
 	}

programs/pluto/ikev1_main.c
@@ -1696,13 +1696,13 @@ stf_status oakley_id_and_auth(struct msg_digest *md, bool initiator,
 	 * ID Payload in.
 	 * Note: this may switch the connection being used!
 	 */
-	if (!ikev1_decode_peer_id(md, initiator, aggrmode))
+	if (!st->st_peer_alt_id  && !ikev1_decode_peer_id(md, initiator, aggrmode))
 		return STF_FAIL + INVALID_ID_INFORMATION;
 
 	/*
 	 * process any CERT payloads if aggrmode
 	 */
-	if (aggrmode && !ikev1_decode_cert(md)) {
+	if (!st->st_peer_alt_id && aggrmode && !ikev1_decode_cert(md)) {
 		return STF_FAIL + INVALID_ID_INFORMATION;
 	}
 

programs/pluto/ikev2.c
@@ -1390,9 +1390,9 @@ bool ikev2_decode_peer_id_and_certs(struct msg_digest *md)
 	 * - if opportunistic, we'll lose our HOLD info
 	 */
 	if (initiator) {
-		if (!same_id(&st->st_connection->spd.that.id, &peer_id) &&
-			id_kind(&st->st_connection->spd.that.id) !=
-			ID_FROMCERT) {
+		if (!md->st->st_peer_alt_id &&
+			!same_id(&c->spd.that.id, &peer_id) &&
+			id_kind(&st->st_connection->spd.that.id) != ID_FROMCERT) {
 
 			char expect[IDTOA_BUF],
 			     found[IDTOA_BUF];
@@ -1436,11 +1436,9 @@ bool ikev2_decode_peer_id_and_certs(struct msg_digest *md)
 			struct connection *r = NULL;
 
 			if (authby != AUTH_NULL) {
-				/* should never return NULL */
 				r = refine_host_connection(
 				md->st, &peer_id, FALSE /*initiator*/,
 				LEMPTY /* auth_policy */, authby, &fromcert);
-				pexpect(r != NULL);
 			}
 
 			if (r == NULL) {
@@ -1448,8 +1446,18 @@ bool ikev2_decode_peer_id_and_certs(struct msg_digest *md)
 
 				idtoa(&peer_id, buf, sizeof(buf));
 				DBG(DBG_CONTROL, DBG_log(
-					"no refined connection for peer '%s'", buf));
-				r = c; /* ??? is this safe? */
+					"no suitable connection for peer '%s'", buf));
+				/* can we continue with what we had? */
+				if (!md->st->st_peer_alt_id &&
+					!same_id(&c->spd.that.id, &peer_id) &&
+					id_kind(&c->spd.that.id) != ID_FROMCERT) {
+						libreswan_log("Peer mismatch on first found connection and no better connection found");
+						return FALSE;
+				} else {
+					DBG(DBG_CONTROL, DBG_log("Peer ID matches and no better connection found - continuing with existing connection"));
+					r = c;
+				}
+
 			}
 
 			if (r != c) {

programs/pluto/ikev2_parent.c
@@ -3535,6 +3535,7 @@ stf_status ikev2_parent_inI2outR2_id_tail(struct msg_digest *md)
 	struct state *const st = md->st;
 	unsigned char idhash_in[MAX_DIGEST_LEN];
 
+	/* calculate hash of IDi for AUTH below */
 	{
 		struct hmac_ctx id_ctx;
 		const pb_stream *id_pbs = &md->chain[ISAKMP_NEXT_v2IDi]->pbs;
@@ -3542,8 +3543,6 @@ stf_status ikev2_parent_inI2outR2_id_tail(struct msg_digest *md)
 		unsigned int idlen = pbs_room(id_pbs) - NSIZEOF_isakmp_generic;
 
 		hmac_init(&id_ctx, st->st_oakley.prf, st->st_skey_pi_nss);
-
-		/* calculate hash of IDi for AUTH below */
 		DBG(DBG_CRYPT, DBG_dump("idhash verify I2", idstart, idlen));
 		hmac_update(&id_ctx, idstart, idlen);
 		hmac_final(idhash_in, &id_ctx);

programs/pluto/nss_cert_vfy.c
@@ -1,6 +1,7 @@
 /* pluto NSS certificate verification routines
  *
  * Copyright (C) 2015 Matt Rogers <mrogers@libreswan.org>
+ * Copyright (C) 2017 Paul Wouters <pwouters@redhat.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -491,3 +492,95 @@ int verify_and_cache_chain(chunk_t *ders, int num_ders, CERTCertificate **ee_out
 
 	return ret;
 }
+
+bool cert_VerifySubjectAltName(const CERTCertificate *cert, const char *name)
+{
+	SECStatus rv;
+	SECItem	subAltName;
+	PLArenaPool *arena = NULL;
+	CERTGeneralName *nameList = NULL;
+	CERTGeneralName *current = NULL;
+	bool san_ip = FALSE;
+	unsigned int len = strlen(name);
+	ip_address myip;
+
+	rv = CERT_FindCertExtension(cert, SEC_OID_X509_SUBJECT_ALT_NAME,
+			&subAltName);
+	if (rv != SECSuccess) {
+		DBG(DBG_X509, DBG_log("certificate contains no subjectAltName extension"));
+		return FALSE;
+	}
+
+	if (tnatoaddr(name, 0, AF_UNSPEC, &myip) == NULL)
+		san_ip = TRUE;
+
+	arena = PORT_NewArena(DER_DEFAULT_CHUNKSIZE);
+	passert(arena != NULL);
+
+	nameList = current = CERT_DecodeAltNameExtension(arena, &subAltName);
+	passert(current != NULL);
+
+	do
+	{
+		switch(current->type) {
+		case certDNSName:
+		case certRFC822Name:
+			if (san_ip)
+				break;
+			if (current->name.other.len == len) {
+				if (memcmp(current->name.other.data, name, len) == 0) {
+					DBG(DBG_X509, DBG_log("subjectAltname %s found in certificate", name));
+					PORT_FreeArena(arena, PR_FALSE);
+					return TRUE;
+				}
+			}
+
+			if (current->name.other.len != 0 && current->name.other.len < IDTOA_BUF) {
+				char osan[IDTOA_BUF];
+
+				memcpy(osan,current->name.other.data, current->name.other.len);
+				osan[current->name.other.len] = '\0';
+				DBG(DBG_X509, DBG_log("subjectAltname (len=%d) %s not match %s", current->name.other.len, osan, name));
+			} else {
+				DBG(DBG_X509, DBG_log("subjectAltname <TOO BIG TO PRINT> does not match %s", name));
+			}
+			break;
+
+		case certIPAddress:
+			if (!san_ip)
+				break;
+			if ((current->name.other.len == 4) && (addrtypeof(&myip) == AF_INET)) {
+				if (memcmp(current->name.other.data, &myip.u.v4.sin_addr.s_addr, 4) == 0) {
+					DBG(DBG_X509, DBG_log("subjectAltname IPv4 matches %s", name));
+					PORT_FreeArena(arena, PR_FALSE);
+					return TRUE;
+				} else {
+					DBG(DBG_X509, DBG_log("subjectAltname IPv4 does not match %s", name));
+					break;
+				}
+			}
+			if ((current->name.other.len == 16) && (addrtypeof(&myip) == AF_INET6)) {
+				if (memcmp(current->name.other.data, &myip.u.v6.sin6_addr.s6_addr, 16) == 0) {
+					DBG(DBG_X509, DBG_log("subjectAltname IPv6 matches %s", name));
+					PORT_FreeArena(arena, PR_FALSE);
+					return TRUE;
+				} else {
+					DBG(DBG_X509, DBG_log("subjectAltname IPv6 does not match %s", name));
+					break;
+				}
+			}
+			DBG(DBG_X509, DBG_log("subjectAltnamea IP address family mismatch for %s", name));
+			break;
+
+		default:
+			break;
+		}
+		current = CERT_GetNextGeneralName(current);
+	} while (current != nameList);
+
+	loglog(RC_LOG_SERIOUS, "No matching subjectAltName found");
+	/* Don't free nameList, it's part of the arena. */
+	PORT_FreeArena(arena, PR_FALSE);
+	return FALSE;
+}
+

programs/pluto/nss_cert_vfy.h
@@ -24,6 +24,8 @@ extern int verify_and_cache_chain(chunk_t *ders, int num_ders,
 						 CERTCertificate **ee_out,
 						 bool *rev_opts);
 
+extern bool cert_VerifySubjectAltName(const CERTCertificate *cert, const char *name);
+
 /* rev_opts index */
 #define RO_OCSP 0
 #define RO_OCSP_S 1

commit d75f65e308e9561d01f1746cd697716d866484b2

X509: fixup pluto_process_certs()'s return code.
The code uses a return of TRUE to indicate some failure modes that
must result in calling refine_host_connection() to look for a
better match. This was broken in my commit 6806420bfec

I added a warning to the function's top comment to warn about this.

programs/pluto/x509.c
@@ -713,7 +713,10 @@ static bool find_fetch_dn(SECItem *dn, struct connection *c,
 }
 #endif
 
-/* returns FALSE for a REVOKED cert or internal failure. returns
+/*
+ * WARNING: This function's bool return case is not what you expect!
+ *
+ * returns FALSE for a REVOKED cert or internal failure. returns
  * TRUE for a good cert or a failed verify (for continuing with
  * connection refining)
  */
@@ -758,7 +761,7 @@ static bool pluto_process_certs(struct state *st, chunk_t *certs,
 			} else {
 				loglog(RC_LOG_SERIOUS, "certificate does not contain ID_IP subjectAltName=%s",
 						ipstr);
-				return FALSE;
+				return TRUE; /* signal connswitch */
 			}
 			break;
 
@@ -772,7 +775,7 @@ static bool pluto_process_certs(struct state *st, chunk_t *certs,
 			} else {
 				loglog(RC_LOG_SERIOUS, "certificate does not contain subjectAltName=%s",
 					namebuf + 1);
-				return FALSE;
+				return TRUE; /* signal conn switch */
 			}
 			break;
 
@@ -784,7 +787,7 @@ static bool pluto_process_certs(struct state *st, chunk_t *certs,
 			} else {
 				loglog(RC_LOG_SERIOUS, "certificate does not contain ID_USER_FQDN subjectAltName=%s",
 					namebuf);
-				return FALSE;
+				return TRUE; /* signal conn switch */
 			}
 			break;
 