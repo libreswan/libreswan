4cec6fe15b048737affeb04f06733b0fa6a3e038
Antony Antony <antony@phenome.org>
2017-07-18 22:02:31 +0200

    testing: rename fedora25 to fedora26. F26 is out
    F25 guest VM were not very stable. Lets work on F26.

--- testing/libvirt/fedora26.mk
+++ testing/libvirt/fedora26.mk
@@ -1,5 +1,5 @@
-KVM_ISO_URL = http://fedora.bhs.mirrors.ovh.net/linux/releases/25/Server/x86_64/iso/Fedora-Server-dvd-x86_64-25-1.3.iso
-KVM_KICKSTART_FILE = testing/libvirt/fedora25.ks
+KVM_ISO_URL = http://fedora.bhs.mirrors.ovh.net/linux/releases/26/Server/x86_64/iso/Fedora-Server-dvd-x86_64-26-1.5.iso
+KVM_KICKSTART_FILE = testing/libvirt/fedora26.ks
 
 KVM_PACKAGE_INSTALL = dnf install -y
 KVM_DEBUGINFO_INSTALL = dnf  debuginfo-install -y
