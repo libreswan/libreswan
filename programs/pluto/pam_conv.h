da85c1bfe6b96257d6cbba01f13b73afdbdfd9bc
Andrew Cagney <cagney@gnu.org>
2017-07-25 13:20:13 -0400

    xauth: make struct pam_thread_arg .atype const

--- programs/pluto/pam_conv.h
+++ programs/pluto/pam_conv.h
@@ -23,7 +23,7 @@ struct pam_thread_arg {
 	char *ra;
 	so_serial_t st_serialno;
 	unsigned long c_instance_serial;
-	char *atype;  /* string XAUTH or IKEv2 */
+	const char *atype;  /* string XAUTH or IKEv2 */
 };
 
 extern bool do_pam_authentication(struct pam_thread_arg *arg);
