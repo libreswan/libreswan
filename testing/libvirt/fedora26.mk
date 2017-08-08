eb3684761e5c17c7d29eb9909429b480e47e62ba
Antony Antony <antony@phenome.org>
2017-08-08 14:00:47 +0000

    testing: F26 pexpect rpm fix

--- testing/libvirt/fedora26.mk
+++ testing/libvirt/fedora26.mk
@@ -5,11 +5,11 @@ KVM_PACKAGE_INSTALL = dnf install -y
 KVM_DEBUGINFO_INSTALL = dnf  debuginfo-install -y
 KVM_INSTALLE_RPM_LIST = rpm -aq > /var/tmp/rpm-qa-fedora-updates.log
 
-# fedora 25 hack. run swan-transmogrify to initialze Network
+# fedora 24..26 hack. run swan-transmogrify to initialze Network
 # interscace It does not seems to run the first time when called from
 # /etc/rc.d/rc.local This slows down installation. If you 7 prefixes
 # it could cost 40 min:)
-KVM_F25_HACK=$(KVMSH) --shutdown $(1)$(2) '/testing/guestbin/swan-transmogrify'
+KVM_F26_HACK=$(KVMSH) --shutdown $(1)$(2) '/testing/guestbin/swan-transmogrify'
 
 KVM_PACKAGES = \
     ElectricFence \
@@ -47,7 +47,7 @@ KVM_PACKAGES = \
     pam-devel \
     pexpect \
     psmisc \
-    python-pexpect \
+    python3-pexpect \
     python-setproctitle \
     racoon2 \
     screen \
