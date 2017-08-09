46bc269f632e9447f69eb13d898fa73c3d5a17e8
Andrew Cagney <cagney@gnu.org>
2017-08-09 10:24:18 -0400

    testing: update F22 URL, fedora.bhs.mirrors.ovh.net is missing F22

--- testing/libvirt/fedora22.mk
+++ testing/libvirt/fedora22.mk
@@ -1,4 +1,8 @@
-KVM_ISO_URL = http://fedora.bhs.mirrors.ovh.net/linux/releases/22/Server/x86_64/iso/Fedora-Server-DVD-x86_64-22.iso
+# The domain download.fedoraproject.org can redirect to a mirror, such
+# as fedora.bhs.mirrors.ovh.net as of 2017-08-09, that doesn't carry
+# F22.
+KVM_ISO_URL = https://muug.ca/mirror/linux/releases/22/Server/x86_64/iso/Fedora-Server-DVD-x86_64-22.iso
+
 KVM_KICKSTART_FILE = testing/libvirt/fedora22.ks
 
 KVM_PACKAGE_INSTALL = dnf install -y
