4a25c6a8e70783280ae49271471bb47b81d7e03f
Antony Antony <antony@phenome.org>
2017-08-15 14:30:03 +0200

    pluto: remove traces of HOST_NAME_MAX

--- ports/win2k/include/sysdep.h
+++ ports/win2k/include/sysdep.h
@@ -13,9 +13,3 @@ typedef unsigned short sa_family_t;
 #define IPPROTO_COMP    108
 
 #define TimeZoneOffset _timezone
-
-/* Not entirely sure if win32 defines this */
-#ifndef HOST_NAME_MAX           /* POSIX 1003.1-2001 says <unistd.h> defines this */
-# define HOST_NAME_MAX 255      /* upper bound, according to SUSv2 */
-#endif
-
