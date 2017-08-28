272301a82178ea1a2c8afd39f26e2e024ef21853
Sahana Prasad <sahana.prasad07@gmail.com>
2017-08-28 19:52:18 -0400

    testing: Added test cases for RFC 7427 functionality and interop
    Test cases with impairments are added to test interoperability with
    clients that do not support RFC 7427
    
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

--- testing/pluto/certoe-10-symetric-cert-whack/final.sh
+++ testing/pluto/certoe-10-symetric-cert-whack/final.sh
@@ -1,7 +1,7 @@
 # A tunnel should have established with non-zero byte counters
 ipsec whack --trafficstatus 
 grep "negotiated connection" /tmp/pluto.log
-# you should see only RSA
+# you should see only Digital Signatures which supports only RSA now
 grep IKEv2_AUTH_ OUTPUT/*pluto.log 
 : ==== cut ====
 ipsec auto --status
