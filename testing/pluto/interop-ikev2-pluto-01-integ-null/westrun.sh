ce4cf47df1dac01ff30430e496fcfac6104ce3c6
Andrew Cagney <cagney@gnu.org>
2017-09-22 13:06:11 -0400

    testing: use --impair <class> in ikev2-algo-15-esp-null-none and interop-ikev2-pluto-01-integ-null
    to show it working

--- testing/pluto/interop-ikev2-pluto-01-integ-null/westrun.sh
+++ testing/pluto/interop-ikev2-pluto-01-integ-null/westrun.sh
@@ -1,6 +1,6 @@
 ipsec whack --debug-all
 ../bin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
-ipsec whack --debug-all --impair-ikev2-include-integ-none
+ipsec whack --debug-all --impair ikev2-include-integ-none
 ../bin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
-ipsec whack --debug-all --impair-ikev2-exclude-integ-none
+ipsec whack --debug-all --impair ikev2-exclude-integ-none
 ../bin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
