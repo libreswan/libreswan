libreswan/libreswan

commit 0d66b0632c5e903c7eb0dc76cbd165bef44bf7fc

logging: when AH, audit-log the actual AH integrity algorithm (and not NONE)
This code still a mash up of algorithm names:

- IKE uses struct ike_alg .officname

- ESP/AH use the IKEv1 name tables esp_transformid_names and
  auth_alg_names (which would be missing IKEv2 entries)

some cleanup is required.  For instance, everything could use
.officname (if we knew what that really was, tcpdump?), or the
.fqn, or even a new dedicated .audit_name.

programs/pluto/linux_audit.c
@@ -229,6 +229,7 @@ void linux_audit_conn(const struct state *st, enum linux_audit_kind op)
 
 	case LAK_CHILD_START:
 	case LAK_CHILD_DESTROY:
+	{
 		snprintf(head, sizeof(head), "op=%s %s connstate=%lu, satype=%s samode=%s",
 			op == LAK_CHILD_START ? "start" : "destroy",
 			conn_encode,
@@ -236,17 +237,37 @@ void linux_audit_conn(const struct state *st, enum linux_audit_kind op)
 			st->st_esp.present ? "ipsec-esp" : (st->st_ah.present ? "ipsec-ah" : "ipsec-policy"),
 			c->policy & POLICY_TUNNEL ? "tunnel" : "transport");
 
-		snprintf(cipher_str, sizeof(cipher_str),
-			"cipher=%s ksize=%d integ=%s",
-			st->st_esp.present ?
-				enum_show_shortb(&esp_transformid_names,
-					st->st_esp.attrs.transattrs.ta_ikev1_encrypt, &esb) :
-				"none",
-			st->st_esp.present ?
-				st->st_esp.attrs.transattrs.enckeylen :
-				0,
-			enum_show_shortb(&auth_alg_names,
-				st->st_esp.attrs.transattrs.ta_ikev1_integ_hash, &esb2));
+		/*
+		 * XXX: Instead of IKEv1_ESP_ID, this should use
+		 * ->common.fqn or ->common.officname; however that
+		 * means changing the output.  So leave it roughly as
+		 * is for now.
+		 */
+
+		const struct encrypt_desc *encrypt;
+		const struct integ_desc *integ;
+		unsigned enckeylen;
+		if (st->st_esp.present) {
+			encrypt = st->st_esp.attrs.transattrs.ta_encrypt;
+			integ = st->st_esp.attrs.transattrs.ta_integ;
+			enckeylen = 0;
+		} else if (st->st_ah.present) {
+			encrypt = NULL;
+			integ = st->st_ah.attrs.transattrs.ta_integ;
+			enckeylen = 0;
+		} else {
+			encrypt = NULL;
+			integ = NULL;
+			enckeylen = 0;
+		}
+		snprintf(cipher_str, sizeof(cipher_str), "cipher=%s ksize=%u integ=%s",
+			 (encrypt == NULL ? "none" :
+			  enum_show_shortb(&esp_transformid_names,
+					   encrypt->common.id[IKEv1_ESP_ID], &esb)),
+			 enckeylen,
+			 (integ == NULL ? "none" :
+			  enum_show_shortb(&auth_alg_names,
+					   integ->common.id[IKEv1_ESP_ID], &esb2)));
 
 		snprintf(spi_str, sizeof(spi_str),
 		"in-spi=%lu(0x%08lx) out-spi=%lu(0x%08lx) in-ipcomp=%lu(0x%08lx) out-ipcomp=%lu(0x%08lx)",
@@ -263,6 +284,7 @@ void linux_audit_conn(const struct state *st, enum linux_audit_kind op)
 		st->st_ipcomp.present ? (unsigned long)ntohl(st->st_ipcomp.our_spi) : (unsigned long)0,
 		st->st_ipcomp.present ? (unsigned long)ntohl(st->st_ipcomp.our_spi) : (unsigned long)0);
 		break;
+	}
 	default:
 		bad_case(op);
 	}

commit 25e158bbfa8ce2f717033ed6137d63f437e45742

pluto: Fix audit logging of ESP encrkeylen, broken in commit 0d66b0632c5e903c

programs/pluto/linux_audit.c
@@ -192,8 +192,8 @@ void linux_audit_conn(const struct state *st, enum linux_audit_kind op)
 
 		if (st->st_oakley.ta_integ == &ike_alg_integ_none) {
 			if (!st->st_ikev2) {
-				/* ikev1 takes integ from prf, ecept of cause gcm */
-				/* but we don't support gcm in ikev1 for now */
+				/* IKE takes integ from prf, except of course gcm */
+				/* but IANA doesn't define gcm for IKE, only for ESP */
 				jam_str(integname, sizeof(integname), prfname);
 			} else {
 				snprintf(integname, sizeof(integname), "none");
@@ -208,8 +208,8 @@ void linux_audit_conn(const struct state *st, enum linux_audit_kind op)
 			 * XXX: dead code path?
 			 */
 			if (!st->st_ikev2) {
-				/* ikev1 takes integ from prf, ecept of cause gcm */
-				/* but we don't support gcm in ikev1 for now */
+				/* IKE takes integ from prf, except of course gcm */
+				/* but IANA doesn't define gcm for IKE, only for ESP */
 				jam_str(integname, sizeof(integname), prfname);
 			} else {
 				snprintf(integname, sizeof(integname), "none");
@@ -247,7 +247,7 @@ void linux_audit_conn(const struct state *st, enum linux_audit_kind op)
 		if (st->st_esp.present) {
 			encrypt = st->st_esp.attrs.transattrs.ta_encrypt;
 			integ = st->st_esp.attrs.transattrs.ta_integ;
-			enckeylen = 0;
+			enckeylen = st->st_esp.attrs.transattrs.enckeylen;
 		} else if (st->st_ah.present) {
 			encrypt = NULL;
 			integ = st->st_ah.attrs.transattrs.ta_integ;