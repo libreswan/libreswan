libreswan/libreswan

commit 66f8e13d4ff538e2227243c93f6196633f2f7f4e

linux/net/ipsec/prng.c: note puzzle fo dangling else in macro definitions

lib/libbsdpfkey/pfkey.c
@@ -54,8 +54,8 @@
 
 unsigned int bsdpfkey_lib_debug = 0;
 libreswan_keying_debug_func_t pfkey_debug_func;
-#define PFKEY_DEBUG if (bsdpfkey_lib_debug) \
-		(*pfkey_debug_func)
+#define PFKEY_DEBUG { if (bsdpfkey_lib_debug) \
+		(*pfkey_debug_func); }
 
 static int findsupportedmap(int);
 static int setsupportedmap(const struct sadb_supported *sup, int properlen);

linux/include/libreswan.h
@@ -185,7 +185,7 @@ static inline deltatime_t monotimediff(monotime_t a, monotime_t b) {
 #include <linux/ctype.h>
 #include <libreswan/ipsec_kversion.h>
 #include <libreswan/ipsec_param.h>
-#define user_assert(foo)  /* nothing */
+#define user_assert(foo)  { } /* nothing */
 
 #else /* NOT in kernel */
 #include <sys/types.h>
@@ -391,10 +391,10 @@ typedef uint32_t IPsecSAref_t;
 #define IPSEC_SA_REF_MASK           ((1u << IPSEC_SA_REF_TABLE_IDX_WIDTH) - 1u)
 #define IPSEC_NFMARK_IS_SAREF_BIT 0x80000000u
 
-#define IPsecSAref2NFmark(x) (((x) & \
-			       IPSEC_SA_REF_MASK) << IPSEC_SA_REF_TABLE_OFFSET)
-#define NFmark2IPsecSAref(x) (((x) >> \
-			       IPSEC_SA_REF_TABLE_OFFSET) & IPSEC_SA_REF_MASK)
+#define IPsecSAref2NFmark(x) \
+	(((x) & IPSEC_SA_REF_MASK) << IPSEC_SA_REF_TABLE_OFFSET)
+#define NFmark2IPsecSAref(x) \
+	(((x) >> IPSEC_SA_REF_TABLE_OFFSET) & IPSEC_SA_REF_MASK)
 
 #define IPSEC_SAREF_NULL ((IPsecSAref_t)0u)
 /* Not representable as an nfmark */

linux/include/libreswan/pfkey_debug.h
@@ -43,16 +43,17 @@ extern unsigned int pfkey_lib_debug;  /* bits selecting what to report */
 extern int (*pfkey_debug_func)(const char *message, ...) PRINTF_LIKE(1);
 extern int (*pfkey_error_func)(const char *message, ...) PRINTF_LIKE(1);
 
-#define DEBUGGING(level, args ...)  if (pfkey_lib_debug & level) { \
+#define DEBUGGING(level, args ...)  { if (pfkey_lib_debug & (level)) { \
 		if (pfkey_debug_func != NULL) { \
 			(*pfkey_debug_func)("pfkey_lib_debug:" args); \
 		} else { \
 			printf("pfkey_lib_debug:" args); \
-		} }
+		} } }
 
-#define ERROR(args ...)      if (pfkey_error_func != NULL) { \
-		(*pfkey_error_func)("pfkey_lib_debug:" args); \
-}
+#define ERROR(args ...)      { \
+		if (pfkey_error_func != NULL) \
+			(*pfkey_error_func)("pfkey_lib_debug:" args); \
+	}
 
 # define MALLOC(size) malloc(size)
 # define FREE(obj) free(obj)

linux/net/ipsec/pfkey_v2_parse.c
@@ -194,8 +194,8 @@ DEBUG_NO_STATIC int pfkey_sa_parse(struct sadb_ext *pfkey_ext)
 		SENDERR(EINVAL);
 	}
 
-	if (!((pfkey_sa->sadb_sa_exttype ==  K_SADB_EXT_SA) ||
-	      (pfkey_sa->sadb_sa_exttype ==  K_SADB_X_EXT_SA2))) {
+	if (pfkey_sa->sadb_sa_exttype !=  K_SADB_EXT_SA &&
+	    pfkey_sa->sadb_sa_exttype !=  K_SADB_X_EXT_SA2) {
 		ERROR("pfkey_sa_parse: "
 			"unknown exttype=%d, expecting K_SADB_EXT_SA=%d or K_SADB_X_EXT_SA2=%d.\n",
 			pfkey_sa->sadb_sa_exttype,
@@ -207,12 +207,12 @@ DEBUG_NO_STATIC int pfkey_sa_parse(struct sadb_ext *pfkey_ext)
 	if (pfkey_sa->sadb_sa_len > sizeof(struct sadb_sa) /
 	    IPSEC_PFKEYv2_ALIGN) {
 		if (pfkey_sa->sadb_x_sa_ref == IPSEC_SAREF_NULL ||
-		    pfkey_sa->sadb_x_sa_ref == ~(IPSEC_SAREF_NULL))
+		    pfkey_sa->sadb_x_sa_ref == ~IPSEC_SAREF_NULL)
 			pfkey_sa->sadb_x_sa_ref = IPSEC_SAREF_NULL;
 	}
 
-	if ((IPSEC_SAREF_NULL != pfkey_sa->sadb_x_sa_ref) &&
-	    (pfkey_sa->sadb_x_sa_ref >= (1 << IPSEC_SA_REF_TABLE_IDX_WIDTH))) {
+	if (IPSEC_SAREF_NULL != pfkey_sa->sadb_x_sa_ref &&
+	    pfkey_sa->sadb_x_sa_ref >= (1 << IPSEC_SA_REF_TABLE_IDX_WIDTH)) {
 		ERROR("pfkey_sa_parse: "
 			"SAref=%d must be (SAref == IPSEC_SAREF_NULL(%d) || SAref < IPSEC_SA_REF_TABLE_NUM_ENTRIES(%d)).\n",
 			pfkey_sa->sadb_x_sa_ref,

contrib/ldsaref/saref.c
@@ -13,7 +13,7 @@ typedef int (*socket_fn)(int, int, int);
 static socket_fn real_socket = NULL;
 static int saref = 0;
 #define UNASSIGNED_SAREF 0
-#define INVALID_SAREF -1
+#define INVALID_SAREF (-1)
 
 int socket(int domain, int type, int protocol)
 {

contrib/lucent/UDP501encap.c
@@ -27,11 +27,11 @@ RJZ - LNX UDP501 # cat UDP501encap.c
 #define BOOL int
 
 #define DstPort 501
-	 #define DstPortHi DstPort >> 8
-	 #define DstPortLo DstPort & 0x00FF
+	 #define DstPortHi (DstPort >> 8)
+	 #define DstPortLo (DstPort & 0x00FF)
 #define SrcPort 501
-	 #define SrcPortHi SrcPort >> 8
-	 #define SrcPortLo SrcPort & 0x00FF
+	 #define SrcPortHi (SrcPort >> 8)
+	 #define SrcPortLo (SrcPort & 0x00FF)
 
 typedef unsigned short u16;
 typedef unsigned long u32;

contrib/sarefnc/generic.h
@@ -47,7 +47,7 @@
 /* bmc@telebase and others have suggested these macros if a box *does* have
    rand48.  Will consider for later if we're doing something that really
    requires stronger random numbers, but netcat and such certainly doesn't.
-   #define srandom(seed) srand48((long) seed)
+   #define srandom(seed) srand48((long) (seed))
  #define random()      lrand48() */
 
 /* if your machine doesn't have lstat(), it should have stat() [dos...] */

contrib/sarefnc/netcat.c
@@ -198,7 +198,7 @@ unsigned char o_tos = 0;
 /* Debug macro: squirt whatever message and sleep a bit so we can see it go
    by.  need to call like Debug ((stuff)) [with no ; ] so macro args match!
    Beware: writes to stdOUT... */
-#define Debug(x) printf x; printf("\n"); fflush(stdout); sleep(1);
+#define Debug(x) { printf x; printf("\n"); fflush(stdout); sleep(1); }
 
 /* support routines -- the bulk of this thing.  Placed in such an order that
    we don't have to forward-declare anything: */

commit 80ce2f10fae1380cc41fad56c7bcc1a622c5cecb

pfkey: fix bug introduced in 66f8e13d4ff538e2227243c93f6196633f2f7f4e
- 66f8e13d4ff538e2227243c93f6196633f2f7f4e broke code that misused the
  preprocessor.  It is not clear why the compiler didn't complain.
  But Cagney caught it.  Fixed without abuse.

- tidy pfkey debugging code definitions

lib/libbsdpfkey/pfkey.c
@@ -53,9 +53,9 @@
 #define CALLOC(size, cast) (cast)calloc(1, (size))
 
 unsigned int bsdpfkey_lib_debug = 0;
-libreswan_keying_debug_func_t pfkey_debug_func;
-#define PFKEY_DEBUG { if (bsdpfkey_lib_debug) \
-		(*pfkey_debug_func); }
+libreswan_keying_debug_func_t pfkey_debug_func = NULL;
+#define PFKEY_DEBUG(args...) { if (bsdpfkey_lib_debug) \
+		(*pfkey_debug_func)(args); }
 
 static int findsupportedmap(int);
 static int setsupportedmap(const struct sadb_supported *sup, int properlen);

linux/include/libreswan.h
@@ -446,7 +446,7 @@ typedef uint32_t IPsecSAref_t;
  * function to log stuff from libraries that may be used in multiple
  * places.
  */
-typedef int (*libreswan_keying_debug_func_t)(const char *message, ...);
+typedef int (*libreswan_keying_debug_func_t)(const char *message, ...) PRINTF_LIKE(1);
 
 /*
  * new IPv6-compatible functions

linux/include/libreswan/pfkey_debug.h
@@ -40,8 +40,8 @@ extern unsigned int pfkey_lib_debug;  /* bits selecting what to report */
 
 #else
 
-extern int (*pfkey_debug_func)(const char *message, ...) PRINTF_LIKE(1);
-extern int (*pfkey_error_func)(const char *message, ...) PRINTF_LIKE(1);
+extern libreswan_keying_debug_func_t pfkey_debug_func;
+extern libreswan_keying_debug_func_t pfkey_error_func;
 
 #define DEBUGGING(level, args ...)  { if (pfkey_lib_debug & (level)) { \
 		if (pfkey_debug_func != NULL) { \

linux/net/ipsec/pfkey_v2_parse.c
@@ -69,8 +69,8 @@ extern int debug_pfkey;
 #include <libreswan/pfkey_debug.h>
 
 unsigned int pfkey_lib_debug = PF_KEY_DEBUG_PARSE_NONE;
-int (*pfkey_debug_func)(const char *message, ...) PRINTF_LIKE(1);
-int (*pfkey_error_func)(const char *message, ...) PRINTF_LIKE(1);
+libreswan_keying_debug_func_t pfkey_debug_func = NULL;
+libreswan_keying_debug_func_t pfkey_error_func = NULL;
 
 #define SENDERR(_x) do { error = -(_x); goto errlab; } while (0)
 