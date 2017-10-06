317403578333352edc3281387faad855a3ab9d10
wolfgang@linogate.de <wolfgang@linogate.de>
2017-10-06 21:29:33 +0200

    testing: xauth-pluto-22 to reproduce lsw#299
    Signed-off-by: Antony Antony <antony@phenome.org>

--- testing/pluto/xauth-pluto-22/eastinit.sh
+++ testing/pluto/xauth-pluto-22/eastinit.sh
@@ -1,4 +1,6 @@
 /testing/guestbin/swan-prep
+echo "use4:xOzlFlqtwJIu2:east-any" >> /etc/ipsec.d/passwd
+echo "use5:xOzlFlqtwJIu2:east-any:192.0.2.100" >> /etc/ipsec.d/passwd
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add east-any
--- testing/pluto/xauth-pluto-22/northinit.sh
+++ testing/pluto/xauth-pluto-22/northinit.sh
@@ -3,7 +3,7 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ../bin/block-non-ipsec.sh
 ipsec auto --add north-east
-ipsec whack --xauthname 'use3' --xauthpass 'use1pass' --name north-east --initiate
+ipsec whack --xauthname 'use4' --xauthpass 'use1pass' --name north-east --initiate
 ping -n -c 4 -w 4 -I 192.0.2.100 192.0.2.254
 ipsec whack --trafficstatus
 echo initdone
