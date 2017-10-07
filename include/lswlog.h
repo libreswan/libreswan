e307cfa937cf6584b491291f1eabeaba410ca9cd
Andrew Cagney <cagney@gnu.org>
2017-10-07 09:07:23 -0400

    logging: add lswlog_source_line()

--- include/lswlog.h
+++ include/lswlog.h
@@ -217,7 +217,11 @@ size_t lswlogf(struct lswlog *log, const char *format, ...) PRINTF_LIKE(2);
 size_t lswlogs(struct lswlog *log, const char *string);
 size_t lswlogl(struct lswlog *log, struct lswlog *buf);
 
+/* <error-string> (<error> 0x<error>) */
 size_t lswlog_pr_error(struct lswlog *log);
+/* _(in FUNC() at FILE:LINE) */
+size_t lswlog_source_line(struct lswlog *log, const char *func,
+			  const char *file, unsigned long line);
 
 /*
  * The logging output streams used by library code.
