e3ef271359f6eb9ef6cac8e5fc6f8876dc94086b
Andrew Cagney <cagney@gnu.org>
2017-10-08 13:01:22 -0400

    testing: add x509-ikev2-frag-01-ike-aes_gcm - check aes_gcm encrypted auth fragments

---
+++ testing/pluto/x509-ikev2-frag-01-ike-aes_gcm/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep --x509
+iptables -I INPUT -p udp -m length --length 0x5dc:0xffff -j LOGDROP
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add x509
+echo "initdone"
---
+++ testing/pluto/x509-ikev2-frag-01-ike-aes_gcm/final.sh
@@ -0,0 +1,7 @@
+grep -e 'fragment number:' -e 'total fragments:' /tmp/pluto.log
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
