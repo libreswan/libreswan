5d5646a34f0cf0c206cdadb7996d4622431fa45a
Paul Wouters <pwouters@redhat.com>
2017-10-21 17:17:41 -0400

    testing: fixup xauth-pluto-23
    let road and north wait for five seconds in final.sh to prevent a
    race between east and north/road with shutdown, as this varies
    the trafficstatus line on east.
    
    Also north was missing its northrun console output, possibly due
    to difference of kvmrunner.py vs swantest ?

--- testing/pluto/xauth-pluto-23/final.sh
+++ testing/pluto/xauth-pluto-23/final.sh
@@ -1,3 +1,6 @@
+# let road and north wait for east to show tunnels before shutting down
+hostname | grep road > /dev/null && sleep 5
+hostname | grep north > /dev/null && sleep 5
 hostname | grep east > /dev/null && ipsec whack --trafficstatus
 : ==== cut ====
 ipsec look
