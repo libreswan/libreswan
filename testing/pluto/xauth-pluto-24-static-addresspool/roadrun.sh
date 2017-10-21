6dc3fcf10ea323f604cc3588eab10127c48564a6
Paul Wouters <pwouters@redhat.com>
2017-10-21 17:19:41 -0400

    testing: xauth-pluto-24-static-addresspool - skip road ping
    the road ping sanitizing seems to have changed? Also, the ping would
    never work anyway since we fail to setup the IPsec SA due to the
    overlap of ip, which is already visible in the console output.

--- testing/pluto/xauth-pluto-24-static-addresspool/roadrun.sh
+++ testing/pluto/xauth-pluto-24-static-addresspool/roadrun.sh
@@ -1,5 +1,4 @@
 ipsec whack --debug-all --impair retransmits
+# expected to fail due to overlap of IP
 ipsec whack --xauthname 'xroad' --xauthpass 'use1pass' --name road-east --initiate
-ping -q -w 4 -n -c 4 192.0.2.254
-ipsec whack --trafficstatus
 echo done
