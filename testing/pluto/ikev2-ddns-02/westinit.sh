6ed1e594b284968240056e244a463cfe3b00124a
Paul Wouters <pwouters@redhat.com>
2017-10-21 21:49:14 -0400

    testing: fixup ikev2-ddns-02

--- testing/pluto/ikev2-ddns-02/westinit.sh
+++ testing/pluto/ikev2-ddns-02/westinit.sh
@@ -3,7 +3,7 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 cp resolv.conf /etc
 # need to disable ipv6 and activate auto-interface
-cp west-unbound.conf /etc/unbound/
+cp west-unbound.conf /etc/unbound/unbound.conf
 # will throw an error about bad unresolvable name
 ipsec auto --add named
 echo "initdone"
