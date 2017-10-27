4354d326975ebf30eccce7c69a7a329096f37ead
Andrew Cagney <cagney@gnu.org>
2017-10-27 11:57:10 -0400

    testing: drop --debug-all option from some impaired tests
    since --impair ... on its own will 'do the right thing'

--- testing/pluto/ikev2-algo-15-esp-null-none/westrun.sh
+++ testing/pluto/ikev2-algo-15-esp-null-none/westrun.sh
@@ -1,7 +1,7 @@
 # should fail
 ipsec auto --add westnet-eastnet-esp-null
 # enable impair
-ipsec whack --debug-all --impair allow-null-null --impair ikev2-include-integ-none
+ipsec whack --impair allow-null-null --impair ikev2-include-integ-none
 ipsec auto --add westnet-eastnet-esp-null
 ipsec auto --up westnet-eastnet-esp-null
 ping -c4 -I 192.0.1.254 192.0.2.254
