352fdef6e356826d2f4c593c7b9b309a56001f21
Andrew Cagney <cagney@gnu.org>
2017-10-27 14:39:43 -0400

    testing: use --debug ... instead of --debug-...
    Although it turns out that only old tests are doing this.

--- testing/pluto/ike-des128-01/westinit.sh
+++ testing/pluto/ike-des128-01/westinit.sh
@@ -6,7 +6,7 @@ ipsec setup restart
 /testing/pluto/bin/wait-until-pluto-started
 
 ipsec auto --add westnet-eastnet
-ipsec whack --debug-control --debug-controlmore --debug-parsing --debug-crypt
+ipsec whack --debug none --debug control --debug controlmore --debug parsing --debug crypt
 
 echo done
 
