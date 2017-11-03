778d54ad9e655f7fc86c4c0f338e46718ab143af
Antony Antony <antony@phenome.org>
2017-11-03 15:21:05 +0100

    testing: dpd-09-shared white space

--- testing/pluto/dpd-09-shared/northrun.sh
+++ testing/pluto/dpd-09-shared/northrun.sh
@@ -3,9 +3,9 @@ ipsec auto --up north-a-dpd
 ping -n -c 2 -I 192.0.3.254 192.0.2.254
 ping -n -c 2 -I 192.0.3.254 192.0.22.254
 ipsec whack --trafficstatus
-# 
+#
 ip route add unreachable 192.1.2.23
-#sleep 40 
+# sleep 40
 sleep 20
 sleep 20
 ipsec status | grep north-
