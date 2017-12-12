80d5baae5dfbf9170978ca3f2af38a430c451d20
Antony Antony <antony@phenome.org>
2017-12-12 13:18:11 +0100

    testing: add mk/docker-targets.mk

---
+++ mk/docker-targets.mk
@@ -0,0 +1,83 @@
+# docker make/start/stop targets, for Libreswan testing
+#
+# Copyright (C) 2016-2017 Antony Antony <antony@phenome.org>
+# 
+# make DISTRO=fedora DISTRO_REL=27 docker-image
+
+# These variable could be changed
+#
+DOCKER_CMD ?= sudo docker
+D ?= testing/docker
+
+W1 = $(firstword $(subst -, ,$1))
+W2 = $(or $(word 2, $(subst -, ,$1)), $(value 2))
+W3 = $(or $(word 3, $(subst -, ,$1)), $(value 2))
+
+FIRST_TARGET ?=$@	# keep track of original target
+DISTRO ?= fedora	# default distro
+DISTRO_REL ?= 27 	# default release
+
+DI_T ?= swanbase 	#docker image tag
+
+# for travis
+#ifeq ($(DISTRO), travis)
+#	BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
+#	DISTRO =  $(call W2, $(BRANCH), '')
+#	DISTRO_REL = $(call W3, $(BRANCH), '')
+#else
+#	BRANCH =
+#endif
+
+# end of configurable variables 
+
+DI = $(DISTRO)-$(DISTRO_REL)
+DOCKER_BASE = $(DI)-base
+DOCKER_PACKAGES = $(DI)-packages
+DOCKER_SSH = $(DI)-ssh
+DOCKER_START = $(DI)-start
+DOCKERFILE ?= $(D)/dockerfile
+
+.PHONY: dcokerfile
+dockerfile: DOCKERFILE_PKG = $(D)/Dockerfile-$(DISTRO)-min-packages
+dockerfile: $(DOCKERFILE_BASE) $(DOCKERFILE_PKG)
+	echo "FROM $(DISTRO):$(DISTRO_REL) " >  $(DOCKERFILE)
+	echo "ENV container docker\n"  >> $(DOCKERFILE)
+	echo 'MAINTAINER "Antony Antony" <antony@phenome.org>' >> $(DOCKERFILE)
+	cat $(DOCKERFILE_BASE)  $(DOCKERFILE_PKG) >> $(DOCKERFILE)
+
+.PHONY docker-image:
+docker-image: $(DI)
+
+.PHONY travis-docker-image:
+travis-image:
+	$(MAKE) $(BRANCH)
+
+.PHONY: travis-ubuntu-xenial
+travis-ubuntu-xenial: ubuntu-xenial-packages
+	$(DOCKER_CMD) run -h $(DI_T) --privileged --name $(DI_T) \
+	-v $(PWD):/home/build/libreswan  \
+	-v /sys/fs/cgroup:/sys/fs/cgroup:ro -d ubuntu-xenial-packages
+	$(DOCKER_CMD) ps -a
+
+$(DOCKER_SSH): DOCKERFILE_SSH = $(D)/Dockerfile-swan-ssh
+$(DOCKER_SSH): dcokerfile $(DOCKERFILE_SSH)
+	@echo "make $@" 
+	cat $(DOCKERFILE_SSH) >>  $(DOCKERFILE)
+	$(DOCKER_CMD) build -t $(DI_T) -f $(DOCKERFILE) .
+
+.PHONY: $(DI)
+$(DI): dockerfile $(DOCKER_SSH)
+$(DI):
+	@echo "DISTRO $(DISTRO) $(DISTRO_REL)"
+	@echo "make $@"
+
+.PHONY: docker-start
+docker-start:
+	$(DOCKER_CMD) run -h $(DI_T) --privileged --name $(DI_T) \
+	-v /home/build/libreswan:/home/build/libreswan  \
+	-v /sys/fs/cgroup:/sys/fs/cgroup:ro -d $(DI_T)
+
+.PHONY: docker-stop
+docker-stop:
+	$(DOCKER_CMD) stop $(DI_T)
+	$(DOCKER_CMD) rm $(DI_T)
