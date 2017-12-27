55e5ad1415c0264d7a5a05d4721255dd5d9bc756
Andrew Cagney <cagney@gnu.org>
2017-12-27 15:19:54 -0500

    testing: update fedora22's ISO url; point it at archives.fedoraproject.org

--- testing/libvirt/fedora22.mk
+++ testing/libvirt/fedora22.mk
@@ -1,7 +1,7 @@
 # The domain download.fedoraproject.org can redirect to a mirror, such
 # as fedora.bhs.mirrors.ovh.net as of 2017-08-09, that doesn't carry
 # F22.
-KVM_ISO_URL = https://muug.ca/mirror/fedora/linux/releases/22/Server/x86_64/iso/Fedora-Server-DVD-x86_64-22.iso
+KVM_ISO_URL = http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/22/Server/x86_64/iso/Fedora-Server-DVD-x86_64-22.iso
 KVM_OS_VARIANT = fedora22
 
 KVM_KICKSTART_FILE = testing/libvirt/fedora22.ks
