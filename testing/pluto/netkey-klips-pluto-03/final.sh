fe37c9d5a901644871105491a5a10c016c2131f1
Paul Wouters <pwouters@redhat.com>
2018-01-22 09:59:13 -0500

    testing: various minor fixups

--- testing/pluto/netkey-klips-pluto-03/final.sh
+++ testing/pluto/netkey-klips-pluto-03/final.sh
@@ -2,10 +2,6 @@ ipsec look
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
-ipsec whack --shutdown
-: ==== cut ====
-ipsec look
-: ==== tuc ====
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
 : ==== end ====
