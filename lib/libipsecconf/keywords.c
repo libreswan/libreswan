libreswan/libreswan

commit 8b449d14554f7ff709570405f22169c1d4b418e9

IKEv2: Add support for modecfgdns= and modecfgdomains=
This adds support for INTERNAL_IP4_DNS and INTERNAL_IP6_DNS
and passes these options to updown similar to IKEv1, so that
resolv.conf or the running unbound DNS server are updated.

This adds support for INTERNAL_DNS_DOMAIN as defined by
draft-ietf-ipsecme-split-dns-03

It does not yet add suppor tfor INTERNAL_DNSSEC_TA.

Note this renames the option modecfgdns1= and modecfgdns2= to
modecfgdns= and modecfgdomain= to modecfgdomains=

The new options take a comma or space seperated list.

The old config options are retained as aliases. This does have the
effect that modecfgdns2= overwrites modecfgdns1= as only the last
option is actually stored in modecfgdns=

This also moves the received DNS / DOMAIN / BANNER options from
struct connection to struct state where it belongs.

lib/libipsecconf/keywords.c
@@ -581,14 +581,11 @@ const struct keyword_def ipsec_conf_keywords[] = {
   { "xauthby",  kv_conn,  kt_enum,  KBF_XAUTHBY,  &kw_xauthby, NULL, },
   { "xauthfail",  kv_conn,  kt_enum,  KBF_XAUTHFAIL,  &kw_xauthfail, NULL, },
   { "modecfgpull",  kv_conn,  kt_invertbool,  KBF_MODECONFIGPULL, NULL, NULL, },
-  /*
-   * these are really kt_ipaddr,  but we handle them as string
-   * until we load them into a whack message
-   */
-  { "modecfgdns1",  kv_conn,  kt_string,  KSCF_MODECFGDNS1, NULL, NULL, },
-  { "modecfgdns2",  kv_conn,  kt_string,  KSCF_MODECFGDNS2, NULL, NULL, },
-
-  { "modecfgdomain",  kv_conn,  kt_string,  KSCF_MODECFGDOMAIN, NULL, NULL, },
+  { "modecfgdns",  kv_conn,  kt_string,  KSCF_MODECFGDNS, NULL, NULL, },
+  { "modecfgdns1",  kv_conn,  kt_string | kv_alias, KSCF_MODECFGDNS, NULL, NULL, }, /* obsolete */
+  { "modecfgdns2",  kv_conn,  kt_string | kv_alias, KSCF_MODECFGDNS, NULL, NULL, }, /* obsolete */
+  { "modecfgdomains",  kv_conn,  kt_string,  KSCF_MODECFGDOMAINS, NULL, NULL, },
+  { "modecfgdomain",  kv_conn | kv_alias,  kt_string,  KSCF_MODECFGDOMAINS, NULL, NULL, }, /* obsolete */
   { "modecfgbanner",  kv_conn,  kt_string,  KSCF_MODECFGBANNER, NULL, NULL, },
   { "mark",  kv_conn,  kt_string,  KSCF_CONN_MARK_BOTH, NULL, NULL, },
   { "mark-in",  kv_conn,  kt_string,  KSCF_CONN_MARK_IN, NULL, NULL, },

include/ietf_constants.h
@@ -1206,7 +1206,9 @@ enum ikev2_cp_attribute_type {
 	IKEv2_P_CSCF_IP6_ADDRESS = 21,
 	IKEv2_FTT_KAT = 22,
 	IKEv2_EXTERNAL_SOURCE_IP4_NAT_INFO = 23,
-	IKEv2_TIMEOUT_PERIOD_FOR_LIVENESS_CHECK = 24
+	IKEv2_TIMEOUT_PERIOD_FOR_LIVENESS_CHECK = 24,
+	IKEv2_INTERNAL_DNS_DOMAIN = 25,
+	/* IKEv2_INTERNAL_DNSSEC_TA = 26 expected */
 };
 
 

include/ipsecconf/confread.h
@@ -105,9 +105,8 @@ struct starter_conn {
 
 	char *esp;
 	char *ike;
-	char *modecfg_dns1;
-	char *modecfg_dns2;
-	char *modecfg_domain;
+	char *modecfg_dns;
+	char *modecfg_domains;
 	char *modecfg_banner;
 	char *policy_label;
 	char *conn_mark_both;

include/ipsecconf/keywords.h
@@ -194,9 +194,8 @@ enum keyword_string_conn_field {
 	KSCF_USERNAME,
 	KSCF_SUBNETS,
 	KSCF_ADDRESSPOOL,
-	KSCF_MODECFGDNS1,
-	KSCF_MODECFGDNS2,
-	KSCF_MODECFGDOMAIN,
+	KSCF_MODECFGDNS,
+	KSCF_MODECFGDOMAINS,
 	KSCF_MODECFGBANNER,
 	KSCF_IKE,
 	KSCF_ESP,

include/whack.h
@@ -300,10 +300,9 @@ struct whack_message {
 	/* for connalias string */
 	char *connalias;
 
-	/* for MODECFG */
-	ip_address modecfg_dns1;
-	ip_address modecfg_dns2;
-	char *modecfg_domain;
+	/* for IKEv1 MODECFG and IKEv2 CP */
+	char *modecfg_dns;
+	char *modecfg_domains;
 	char *modecfg_banner;
 
 	char *conn_mark_both;

lib/libipsecconf/confread.c
@@ -1236,9 +1236,8 @@ static bool load_conn(
 #endif
 
 	str_to_conn(ike, KSCF_IKE);
-	str_to_conn(modecfg_dns1, KSCF_MODECFGDNS1);
-	str_to_conn(modecfg_dns2, KSCF_MODECFGDNS2);
-	str_to_conn(modecfg_domain, KSCF_MODECFGDOMAIN);
+	str_to_conn(modecfg_dns, KSCF_MODECFGDNS);
+	str_to_conn(modecfg_domains, KSCF_MODECFGDOMAINS);
 	str_to_conn(modecfg_banner, KSCF_MODECFGBANNER);
 
 	str_to_conn(conn_mark_both, KSCF_CONN_MARK_BOTH);
@@ -1408,9 +1407,8 @@ static void conn_default(struct starter_conn *conn,
 	conn->esp = clone_str(def->esp, "conn default esp");
 	conn->ike = clone_str(def->ike, "conn default ike");
 
-	conn->modecfg_dns1 = clone_str(def->modecfg_dns1, "conn default dns1");
-	conn->modecfg_dns2 = clone_str(def->modecfg_dns2, "conn default dns2");
-	conn->modecfg_domain = clone_str(def->modecfg_domain, "conn default domain");
+	conn->modecfg_dns = clone_str(def->modecfg_dns, "conn default dns");
+	conn->modecfg_domains = clone_str(def->modecfg_domains, "conn default domains");
 	conn->modecfg_banner = clone_str(def->modecfg_banner, "conn default banner");
 	conn->conn_mark_both = clone_str(def->conn_mark_both, "conn default conn_mark_both");
 	conn->conn_mark_in = clone_str(def->conn_mark_in, "conn default conn_mark_in");
@@ -1564,9 +1562,6 @@ static void confread_free_conn(struct starter_conn *conn)
 
 	pfreeany(conn->esp);
 	pfreeany(conn->ike);
-
-	pfreeany(conn->modecfg_dns1);
-	pfreeany(conn->modecfg_dns2);
 
 	pfreeany(conn->left.virt);
 	pfreeany(conn->right.virt);

lib/libipsecconf/starterwhack.c
@@ -665,9 +665,12 @@ static int starter_whack_basic_add_conn(struct starter_config *cfg,
 
 #endif
 
-	msg.modecfg_domain = conn->modecfg_domain;
-	starter_log(LOG_LEVEL_DEBUG, "conn: \"%s\" modecfgdomain=%s",
-		conn->name, msg.modecfg_domain);
+	msg.modecfg_dns = conn->modecfg_dns;
+	starter_log(LOG_LEVEL_DEBUG, "conn: \"%s\" modecfgdns=%s",
+		conn->name, msg.modecfg_dns);
+	msg.modecfg_domains = conn->modecfg_domains;
+	starter_log(LOG_LEVEL_DEBUG, "conn: \"%s\" modecfgdomains=%s",
+		conn->name, msg.modecfg_domains);
 	msg.modecfg_banner = conn->modecfg_banner;
 	starter_log(LOG_LEVEL_DEBUG, "conn: \"%s\" modecfgbanner=%s",
 		conn->name, msg.modecfg_banner);
@@ -694,23 +697,6 @@ static int starter_whack_basic_add_conn(struct starter_config *cfg,
 		msg.xauthby = conn->options[KBF_XAUTHBY];
 	if (conn->options_set[KBF_XAUTHFAIL])
 		msg.xauthfail = conn->options[KBF_XAUTHFAIL];
-
-	if (conn->modecfg_dns1 != NULL) {
-		if (!tnatoaddr(conn->modecfg_dns1, 0, AF_INET,
-				&(msg.modecfg_dns1)) &&
-			!tnatoaddr(conn->modecfg_dns1, 0, AF_INET6,
-				&(msg.modecfg_dns1)))
-			starter_log(LOG_LEVEL_ERR,
-				"Ignoring modecfgdns1= entry, it is not a valid IPv4 or IPv6 address");
-	}
-	if (conn->modecfg_dns2 != NULL) {
-		if (!tnatoaddr(conn->modecfg_dns2, 0, AF_INET,
-				&(msg.modecfg_dns2)) &&
-			!tnatoaddr(conn->modecfg_dns2, 0, AF_INET6,
-				&(msg.modecfg_dns2)))
-			starter_log(LOG_LEVEL_ERR,
-				"Ignoring modecfgdns2= entry, it is not a valid IPv4 or IPv6 address");
-	}
 
 	set_whack_end("left",  &msg.left, &conn->left);
 	set_whack_end("right", &msg.right, &conn->right);

lib/libswan/constants.c
@@ -1362,12 +1362,15 @@ static const char *const ikev2_cp_attribute_type_name[] = {
 	"IKEv2_P_CSCF_IP6_ADDRESS",
 	"IKEv2_FTT_KAT",
 	"IKEv2_EXTERNAL_SOURCE_IP4_NAT_INFO", /* 3gpp */
-	"IKEv2_TIMEOUT_PERIOD_FOR_LIVENESS_CHECK" /* 3gpp */
+	"IKEv2_TIMEOUT_PERIOD_FOR_LIVENESS_CHECK", /* 3gpp */
+	"IKEv2_INTERNAL_DNS_DOMAIN", /* draft-ietf-ipsecme-split-dns */
+	/* "IKEv2_INTERNAL_DNSSEC_TA", draft-ietf-ipsecme-split-dns, no Code Point yet */
 };
 
 enum_names ikev2_cp_attribute_type_names = {
 	IKEv2_CP_ATTR_RESERVED,
-	IKEv2_TIMEOUT_PERIOD_FOR_LIVENESS_CHECK,
+	IKEv2_INTERNAL_DNS_DOMAIN,
+	/* IKEv2_INTERNAL_DNSSEC_TA, */
 	ARRAY_REF(ikev2_cp_attribute_type_name),
 	NULL, /* prefix */
 	NULL

commit d8d4572e2a928ca8b748948a9a46c7668df1c9d9

pluto: fixup modecfgdns[12] alias processing
This was incorrectly added in commit 8b449d14 which broke configurations
that were using the old modecfgdns1/modecfgdns2 keywords

lib/libipsecconf/keywords.c
@@ -584,8 +584,8 @@ const struct keyword_def ipsec_conf_keywords[] = {
   { "xauthfail",  kv_conn,  kt_enum,  KBF_XAUTHFAIL,  &kw_xauthfail, NULL, },
   { "modecfgpull",  kv_conn,  kt_invertbool,  KBF_MODECONFIGPULL, NULL, NULL, },
   { "modecfgdns",  kv_conn,  kt_string,  KSCF_MODECFGDNS, NULL, NULL, },
-  { "modecfgdns1",  kv_conn,  kt_string | kv_alias, KSCF_MODECFGDNS, NULL, NULL, }, /* obsolete */
-  { "modecfgdns2",  kv_conn,  kt_string | kv_alias, KSCF_MODECFGDNS, NULL, NULL, }, /* obsolete */
+  { "modecfgdns1",  kv_conn | kv_alias, kt_string, KSCF_MODECFGDNS, NULL, NULL, }, /* obsolete */
+  { "modecfgdns2",  kv_conn | kv_alias, kt_string, KSCF_MODECFGDNS, NULL, NULL, }, /* obsolete */
   { "modecfgdomains",  kv_conn,  kt_string,  KSCF_MODECFGDOMAINS, NULL, NULL, },
   { "modecfgdomain",  kv_conn | kv_alias,  kt_string,  KSCF_MODECFGDOMAINS, NULL, NULL, }, /* obsolete */
   { "modecfgbanner",  kv_conn,  kt_string,  KSCF_MODECFGBANNER, NULL, NULL, },