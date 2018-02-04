db3f004f66126a998746553249e5d493525b04e3
Paul Wouters <pwouters@redhat.com>
2018-02-04 15:29:52 -0500

    testing: added newoe-18-private-private-32
    confirm lsw#305 is resolved.

---
+++ testing/pluto/newoe-18-private-private-32/roadinit.sh
@@ -0,0 +1,11 @@
+/testing/guestbin/swan-prep
+cp policies/* /etc/ipsec.d/policies/
+echo "192.1.2.23/32"  >> /etc/ipsec.d/policies/private
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+# ensure for tests acquires expire before our failureshunt=2m
+echo 30 > /proc/sys/net/core/xfrm_acq_expires
+# give OE policies time to load
+sleep 5
+ip -s xfrm monitor > /tmp/xfrm-monitor.out &
+echo "initdone"
