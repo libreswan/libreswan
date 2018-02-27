ed9ac455a4b2c07552e9c18663c15e4cf1e18418
Andrew Cagney <cagney@gnu.org>
2018-02-27 09:48:21 -0500

    building: when F22 set USE_GLIBC_KERN_FLIP_HEADERS
    The kernel headers saga continues ...

--- mk/defaults/linux.mk
+++ mk/defaults/linux.mk
@@ -20,14 +20,32 @@ BISONOSFLAGS=-g --verbose
 
 USE_LABELED_IPSEC?=true
 
-# Detect linux variants and releases (for now just fedora and assume
-# >= 22).
-ifneq ($(wildcard /etc/fedora-release),)
-LINUX_VARIANT?=fedora
+# Detect linux variants and releases.
+
+# So that the sub-shell is invoked only once, ":=" is used.  This in
+# turn means 'ifndef' is needed as := is unconditional.
+
+ifndef LINUX_VARIANT
+  ifneq ($(wildcard /etc/os-release),)
+    LINUX_VARIANT:=$(shell sed -n -e 's/^ID=//p' /etc/os-release)
+  endif
 endif
+#(info LINUX_VARIANT=$(LINUX_VARIANT))
+
+ifndef LINUX_VARIANT_VERSION
+  ifneq ($(wildcard /etc/os-release),)
+    LINUX_VARIANT_VERSION:=$(shell sed -n -e 's/^VERSION_ID=//p' /etc/os-release)
+  endif
+endif
+#(info LINUX_VARIANT_VERSION=$(LINUX_VARIANT_VERSION))
 
 ifeq ($(LINUX_VARIANT),fedora)
-USE_FIPSCHECK?=true
-USE_LINUX_AUDIT?=true
-USE_SECCOMP?=true
+  USE_FIPSCHECK?=true
+  USE_LINUX_AUDIT?=true
+  USE_SECCOMP?=true
+  # Assume that fedora 22 (used by test VMs) needs the hack
+  ifeq ($(LINUX_VARIANT_VERSION),22)
+    USE_GLIBC_KERN_FLIP_HEADERS=true
+  endif
 endif
+#(info USE_GLIBC_KERN_FLIP_HEADERS=$(USE_GLIBC_KERN_FLIP_HEADERS))
