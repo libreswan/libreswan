9be57296c71ac772d67654601913b983f4176427
Andrew Cagney <cagney@gnu.org>
2018-02-28 11:50:44 -0500

    logging: split debug and impair definitions into separate files

--- lib/libswan/impair.c
+++ lib/libswan/impair.c
@@ -1,6 +1,6 @@
-/* debug set constants, for libreswan
+/* impair constants, for libreswan
  *
- * Copyright (C) 2017 Andrew Cagney
+ * Copyright (C) 2017-2018 Andrew Cagney
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -23,52 +23,6 @@
  * See plutomain.c for what the extra "\0" is all about (hint it is a
  * hack for encoding what to do with flags).
  */
-
-#define D(N,A) [N##_IX] = LELEM(N##_IX) == N ? A "\0" : NULL
-
-static const char *debug_strings[] = {
-	D(DBG_RAW, "debug-raw"),
-	D(DBG_CRYPT, "debug-crypt"),
-	D(DBG_PARSING, "debug-parsing"),
-	D(DBG_EMITTING, "debug-emitting"),
-	D(DBG_CONTROL, "debug-control"),
-	D(DBG_LIFECYCLE, "debug-lifecycle"),
-	D(DBG_KERNEL, "debug-kernel"),
-	D(DBG_DNS, "debug-dns"),
-	D(DBG_OPPO, "debug-oppo"),
-	D(DBG_CONTROLMORE, "debug-controlmore"),
-	D(DBG_PFKEY, "debug-pfkey"),
-	D(DBG_NATT, "debug-nattraversal"),
-	D(DBG_X509, "debug-x509"),
-	D(DBG_DPD, "debug-dpd"),
-	D(DBG_XAUTH, "debug-xauth"),
-	D(DBG_RETRANSMITS, "debug-retransmits"),
-	D(DBG_OPPOINFO, "debug-oppoinfo"),
-	D(DBG_WHACKWATCH, "debug-whackwatch"),
-	D(DBG_PRIVATE, "debug-private"),
-	D(DBG_ADD_PREFIX, "debug-add-prefix"),
-	D(DBG_ALGORITHM_PARSER, "debug-algorithm-parser"),
-};
-
-const struct enum_names debug_names = {
-	DBG_floor_IX, DBG_roof_IX - 1,
-	ARRAY_REF(debug_strings),
-	"debug-",
-	NULL,
-};
-
-struct lmod_compat debug_compat[] = {
-       { "klips",    DBG_KERNEL },
-       { "netkey",    DBG_KERNEL },
-       { NULL, LEMPTY, },
-};
-
-const struct lmod_info debug_lmod_info = {
-	.names = &debug_names,
-	.all = DBG_ALL,
-	.mask = DBG_MASK,
-	.compat = debug_compat,
-};
 
 #define I(N,A) [N##_IX - DBG_roof_IX] = LELEM(N##_IX) == N ? A "\0" : NULL
 
