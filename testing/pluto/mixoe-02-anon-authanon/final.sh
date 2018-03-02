6dbe08b11700765af3848925ca48ef5b241f2215
Paul Wouters <pwouters@redhat.com>
2018-03-01 21:22:56 -0500

    testing: updated OE test cases
    - add mixoe test cases for OE fallback
    - add missing dnsoe consoles
    - fixup certoe test cases
    - update newoe test cases

---
+++ testing/pluto/mixoe-02-anon-authanon/final.sh
@@ -0,0 +1,8 @@
+# Authentication should be AUTH_NULL
+hostname | grep nic > /dev/null || grep Authenticated /tmp/pluto.log
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
