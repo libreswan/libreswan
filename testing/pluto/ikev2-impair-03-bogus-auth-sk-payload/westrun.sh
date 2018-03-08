4990611ee483672b82083106a954100cb55033b4
Andrew Cagney <cagney@gnu.org>
2018-03-08 14:32:13 -0500

    testing: test unknown payload inside of AUTH's SK payload

---
+++ testing/pluto/ikev2-impair-03-bogus-auth-sk-payload/westrun.sh
@@ -0,0 +1,11 @@
+ipsec whack --impair none
+ipsec whack --impair send-no-retransmits # one packet
+ipsec whack --impair add-bogus-payload-to-auth-sk
+: good
+../bin/libreswan-up-down.sh westnet-eastnet-ipv4-psk-ikev2 -I 192.0.1.254 192.0.2.254
+: bad
+ipsec whack --impair none
+ipsec whack --impair delete-on-retransmit
+ipsec whack --impair add-bogus-payload-to-auth-sk,bogus-payload-critical
+../bin/libreswan-up-down.sh westnet-eastnet-ipv4-psk-ikev2 -I 192.0.1.254 192.0.2.254
+echo done
