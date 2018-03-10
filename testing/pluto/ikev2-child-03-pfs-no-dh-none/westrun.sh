da02c64330c15420e017a52ec7e35032e62c38b6
Andrew Cagney <cagney@gnu.org>
2018-03-09 20:03:54 -0500

    testing: interop pfs={yes,no} with aes=...;none

---
+++ testing/pluto/ikev2-child-03-pfs-no-dh-none/westrun.sh
@@ -0,0 +1,24 @@
+ipsec auto --up westnet-eastnet-ikev2
+../../pluto/bin/one-ping.sh -I 192.0.1.254 192.0.2.254
+
+# remote pfs=no dh=none
+
+# pfs=no dh= - connect
+ipsec auto --up westnet-eastnet-ikev2-00
+../../pluto/bin/one-ping.sh -I 192.0.100.254 192.0.200.254
+
+# pfs=no dh=none - connect
+ipsec auto --up westnet-eastnet-ikev2-01
+../../pluto/bin/one-ping.sh -I 192.0.101.254 192.0.201.254
+
+# pfs=yes dh= - fail
+# ipsec auto --up westnet-eastnet-ikev2-10
+# ../../pluto/bin/one-ping.sh -I 192.0.110.254 192.0.210.254
+
+# pfs=yes dh=none - connect
+ipsec auto --up westnet-eastnet-ikev2-11
+../../pluto/bin/one-ping.sh -I 192.0.111.254 192.0.211.254
+
+ipsec whack --trafficstatus
+
+echo done
