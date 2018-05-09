275799aeae8afd7450850b775f80aa3a10ff829e
Paul Wouters <pwouters@redhat.com>
2018-05-09 17:01:12 -0400

    testing: newoe-25-cat-5 prevent race condition visible in status output

--- testing/pluto/newoe-25-cat-5/roadinit.sh
+++ testing/pluto/newoe-25-cat-5/roadinit.sh
@@ -5,5 +5,7 @@ echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair retransmits
+# give OE policies time to load
+sleep 5
 ipsec auto --status
 echo "initdone"
