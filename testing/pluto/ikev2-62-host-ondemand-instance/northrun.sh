b72a0994250def02e0045a438392ab674d701af8
Andrew Cagney <cagney@gnu.org>
2018-05-09 13:13:25 -0400

    testing: rename grep-for-ipsec-whack-trafficstatus.sh to wait-for-whack-trafficstatus.sh
    Hopefully slightly more meaningful ...

--- testing/pluto/ikev2-62-host-ondemand-instance/northrun.sh
+++ testing/pluto/ikev2-62-host-ondemand-instance/northrun.sh
@@ -1,6 +1,6 @@
 # one ping to trigger IKE
 ../../pluto/bin/one-ping.sh -I 192.0.3.254 192.0.2.254
-../../pluto/bin/grep-for-ipsec-whack-trafficstatus.sh north-east
+../../pluto/bin/wait-for-whack-trafficstatus.sh north-east
 # success
 ping -q -w 4 -n -c 4 -I 192.0.3.254 192.0.2.254
 ipsec whack --trafficstatus
