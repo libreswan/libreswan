8eb1952d074dd229b674c5ecea7c9bcbb293a538
Paul Wouters <pwouters@redhat.com>
2018-05-12 15:42:28 -0400

    testing: added ikev*-hostpair-01

---
+++ testing/pluto/ikev2-hostpair-01/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep
+# confirm that the network is alive
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add roadnet-eastnet-ipv4-psk-ikev2
+echo "initdone"
