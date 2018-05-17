b97706b777d829fe6adc81a8649adba9b6d4eff0
Andrew Cagney <cagney@gnu.org>
2018-05-17 11:40:34 -0400

    testing: add ikev1-x509-15-pkcs7

---
+++ testing/pluto/ikev1-x509-15-pkcs7/final.sh
@@ -0,0 +1,8 @@
+grep PKCS7 /tmp/pluto.log
+ipsec look
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
