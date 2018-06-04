b7f40c43ae61a64f2891ae62b7f98bc00138ac27
Paul Wouters <pwouters@redhat.com>
2018-06-04 15:01:33 -0400

    testing: rename ipv6-v6-through-v6-netkey-netkey to ipv6-6in6-01-ikev1

--- testing/pluto/ipv6-v6-through-v6-netkey-netkey/eastinit.sh
+++
@@ -1,5 +0,0 @@
-/testing/guestbin/swan-prep --46
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-6in6
-echo "initdone"
--- testing/pluto/ipv6-v6-through-v6-netkey-netkey/westrun.sh
+++
@@ -1,3 +0,0 @@
-ipsec auto --up westnet-eastnet-6in6
-ping6 -n -c 4 -I 2001:db8:0:1::254 2001:db8:0:2::254
-echo done
