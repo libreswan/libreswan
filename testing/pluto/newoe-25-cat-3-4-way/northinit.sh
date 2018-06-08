314f0a4f61b180ed602a3ec0e3af28b19f4a117a
Paul Wouters <pwouters@redhat.com>
2018-06-08 09:31:19 -0400

    testing: newoe-25-cat-3-4-way add missing delay before ipsec status

--- testing/pluto/newoe-25-cat-3-4-way/northinit.sh
+++ testing/pluto/newoe-25-cat-3-4-way/northinit.sh
@@ -5,6 +5,8 @@ echo "192.1.3.209/32" >> /etc/ipsec.d/policies/private-or-clear
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair retransmits
+# give OE policies time to load
+sleep 5
 ipsec auto --status
 #these will create passthroguh shunts
 ping -n -c 4 -I 192.1.3.33 192.1.2.23
