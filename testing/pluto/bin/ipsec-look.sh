230618250cd6530d2839298cae32f00e88ce0887
Paul Wouters <pwouters@redhat.com>
2018-06-18 00:03:56 -0400

    testing: ipsec-look.sh don't try to run on nic

--- testing/pluto/bin/ipsec-look.sh
+++ testing/pluto/bin/ipsec-look.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# don't run on nic, it does not have ipsec installed
+hostname | grep "nic" > /dev/null && exit
+
 # dump raw version of what ../../pluto/bin/ipsec-look.sh manges
 echo ==== cut ====
 echo "start raw xfrm state:"
