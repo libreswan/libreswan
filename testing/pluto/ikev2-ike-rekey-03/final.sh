7a5b005a90d259be400c301a6596b4b9b7549608
Paul Wouters <pwouters@redhat.com>
2018-06-22 17:39:57 -0400

    testing: change ikev2-ike-rekey-03 to sort states for more consistent output

--- testing/pluto/ikev2-ike-rekey-03/final.sh
+++ testing/pluto/ikev2-ike-rekey-03/final.sh
@@ -1,5 +1,5 @@
 ipsec whack --trafficstatus
-ipsec status |grep STATE_
+ipsec status |grep STATE_ | sort
 # there should be only one IKE_INIT exchange
 grep "STATE_PARENT_I1 with STF_OK" /tmp/pluto.log
 grep "STATE_PARENT_R1 with STF_OK" /tmp/pluto.log
