4649e839baa6cf97b0fa8c83beeb4d6e81861e88
Andrew Cagney <andrew.cagney@gmail.com>
2018-06-28 17:09:37 -0400

    building: cleanup netbsd.mk

--- mk/defaults/netbsd.mk
+++ mk/defaults/netbsd.mk
@@ -1,25 +1,26 @@
-
-PORTLASTINCLUDE=
-PORTLASTINCLUDE+=-isystem /usr/local/include
-PORTLASTINCLUDE+=-I/usr/pkg/include
-
-PORTDEFINE+=-DHAS_SUN_LEN
-
-PORTLDFLAGS=-L/usr/pkg/lib
-
-# no KLIPS, we will be using FreeBSD copy of pfkey code.
-USE_KLIPS=false
-USE_KERNEL26=false
-USE_PFKEYv2=false
+USERLAND_CFLAGS += -I/usr/pkg/include
+USERLAND_CFLAGS += -DHAS_SUN_LEN
+USERLAND_CFLAGS += -DNEED_SIN_LEN
+USERLAND_CFLAGS += -DHAVE_NETINET6_IN_H
+USERLAND_CFLAGS += -DHAVE_UDPFROMTO
+USERLAND_CFLAGS += -DHAVE_IP_RECVDSTADDR
+USERLAND_CFLAGS += -Ds6_addr16=__u6_addr.__u6_addr16
+USERLAND_CFLAGS += -Ds6_addr32=__u6_addr.__u6_addr32
 
 USE_BSDKAME=true
+USE_LIBCAP_NG=false
 
-NEEDS_GETOPT=true
+LDFLAGS +=  -lipsec -L/usr/pkg/lib -Wl,-rpath,/usr/pkg/lib
 
-# build modules, etc. for KLIPS.
-BUILD_KLIPS=false
+NSSFLAGS = -I/usr/pkg/include/nspr -I/usr/pkg/include/nss/nss
+NSS_LDFLAGS = -L/usr/pkg/lib/nss -Wl,-rpath,/usr/pkg/lib/nss -lnss3 -lfreebl3 -lssl3
+NSPR_LDFLAGS = -L/usr/pkg/lib/nspr -Wl,-rpath,/usr/pkg/lib/nspr -lnspr4
 
-CFLAGS+= -DSCANDIR_HAS_CONST
+CRYPT_LDFLAGS =
 
-USERLINK=-L/usr/local/lib -lcrypt
+INITSYSTEM=
 
+FINAL_PAM_D_DIR=/usr/local/etc/pam.d
+FINALSYSCONFDIR=/usr/local/etc
+FINALRUNDIR=/usr/local/run/pluto
+FINALNSSDIR=/usr/local/etc/ipsec.d
