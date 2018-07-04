2d174c8cc322762f78b00ddd33f1d4029039d9ab
Antony Antony <antony@phenome.org>
2018-07-05 00:48:03 +0200

    testing: fix iptable ikev2-32-nat-rw-rekey ikev2-31-nat-rw-no-rekey

--- testing/pluto/ikev2-31-nat-rw-no-rekey/nicinit.sh
+++ testing/pluto/ikev2-31-nat-rw-no-rekey/nicinit.sh
@@ -5,6 +5,6 @@ iptables -t nat -L
 iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -p udp --sport 4500 -j SNAT --to-source 192.1.2.254:3500-3700
 iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -p udp --sport 500 -j SNAT --to-source 192.1.2.254:2500-2700
 iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j SNAT --to-source 192.1.2.254
-iptables -I FORWARD 1 --proto 50 -j LOGDROP
+iptables -I FORWARD 1 --proto 50 -j DROP
 echo done
 : ==== end ====
