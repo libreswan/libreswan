e6059873feba314f4c4b1ef00745ebcc6e220abb
Andrew Cagney <cagney@gnu.org>
2018-07-06 16:22:36 -0400

    includes: also #define SADB_X_AALG_NULL in include/linux/pfkeyv2.h
    part of the sorry saga of pluto having both
       include/linux/pfkeyv2.h
       linux/include/libreswan/pfkeyv2.h
    define stuff

--- include/linux/pfkeyv2.h
+++ include/linux/pfkeyv2.h
@@ -284,6 +284,7 @@ struct sadb_x_nat_t_port {
 #define SADB_X_AALG_AH_AES_192_GMAC     12
 #define SADB_X_AALG_AH_AES_256_GMAC     13
 
+#define SADB_X_AALG_AES_CMAC_96         250             /* used internally */
 #define SADB_X_AALG_NULL		251	/* kame */
 #define SADB_X_AALG_SHA2_256HMAC_TRUNCBUG	252
 #define SADB_AALG_MAX			255	/* while the AUTH_ALGORITHM is two octets, what is the SADB_AALG? */
