libreswan/libreswan

commit d23fb67075a07e6a7a2a1b4c09589c60534431e8

pluto: cut down repetition in building V2 proposals
- replace ikev2_proposals_from_alg_info_ike with ikev2_need_ike_proposals
  and ikev2_proposals_from_alg_info_esp with ikev2_need_esp_or_ah_proposals

- the replacements have fewer arguments so calls are simpler

- moved a passert from after each ikev2_proposals_from_alg_info_esp call to
  inside ikev2_proposals_from_alg_info_esp.

programs/pluto/ikev2_spdb_struct.c
@@ -1921,41 +1921,40 @@ static struct ikev2_proposals default_ikev2_ike_proposals = {
 };
 
 /*
- * Transform an alg_info_ike into an array of ikev2 proposals.
+ * Ensure c->ike_proposals is filled in.  If not, build it.
+ *
+ * ??? if c->ike_proposals was set for v1 we won't change it.
  *
  * WARNING: alg_info_ike is IKEv1
  *
  * If alg_info_ike includes unknown algorithms those get dropped,
  * which can lead to no proposals.
- * *result will not be NULL (see passert).
+ * c->ike_proposals will not be NULL (see passert).
  */
-void ikev2_proposals_from_alg_info_ike(const char *connection_name,
-				       const char *why,
-				       struct alg_info_ike *alg_info_ike,
-				       struct ikev2_proposals **result)
-{
-	if (*result != NULL) {
+
+void ikev2_need_ike_proposals(struct connection *c, const char *why) {
+	if (c->ike_proposals != NULL) {
 		DBGF(DBG_CONTROL, "already determined local IKE proposals for %s (%s)",
-		     connection_name, why);
+		     c->name, why);
 		return;
 	}
 
 	const char *notes;
-	if (alg_info_ike == NULL) {
+	if (c->alg_info_ike == NULL) {
 		DBGF(DBG_CONTROL, "selecting default local IKE proposals for %s (%s)",
-		     connection_name, why);
-		*result = &default_ikev2_ike_proposals;
+		     c->name, why);
+		c->ike_proposals = &default_ikev2_ike_proposals;
 		notes = " (default)";
 	} else {
 		DBGF(DBG_CONTROL, "constructing local IKE proposals for %s (%s)",
-		     connection_name, why);
+		     c->name, why);
 		struct ikev2_proposals *proposals = alloc_thing(struct ikev2_proposals, "proposals");
-		int proposals_roof = alg_info_ike->ai.alg_info_cnt + 1;
+		int proposals_roof = c->alg_info_ike->ai.alg_info_cnt + 1;
 		proposals->proposal = alloc_things(struct ikev2_proposal, proposals_roof, "propsal");
 		proposals->on_heap = TRUE;
 		proposals->roof = 1;
 
-		FOR_EACH_IKE_INFO(alg_info_ike, ike_info) {
+		FOR_EACH_IKE_INFO(c->alg_info_ike, ike_info) {
 			LSWDBGP(DBG_CONTROL, buf) {
 				lswlogs(buf, "converting ike_info ");
 				lswlog_proposal_info(buf, ike_info);
@@ -1972,17 +1971,17 @@ void ikev2_proposals_from_alg_info_ike(const char *connection_name,
 				proposals->roof++;
 			}
 		}
-		*result = proposals;
+		c->ike_proposals = proposals;
 		notes = "";
 	}
 
 	LSWLOG(buf) {
 		lswlogf(buf, "local IKE proposals for %s (%s): ",
-			connection_name, why);
-		print_proposals(buf, *result);
+			c->name, why);
+		print_proposals(buf, c->ike_proposals);
 		lswlogs(buf, notes);
 	}
-	passert(*result != NULL);
+	passert(c->ike_proposals != NULL);
 }
 
 static struct ikev2_proposal default_ikev2_esp_proposal_missing_esn[] = {
@@ -2072,24 +2071,21 @@ static void add_esn_transforms(struct ikev2_proposal *proposal, lset_t policy)
 	}
 }
 
-void ikev2_proposals_from_alg_info_esp(const char *connection_name,
-				       const char *why,
-				       struct alg_info_esp *alg_info_esp,
-				       lset_t policy,
-				       const struct oakley_group_desc *default_dh,
-				       struct ikev2_proposals **result)
+void ikev2_need_esp_or_ah_proposals(struct connection *c,
+				    const char *why,
+				    const struct oakley_group_desc *default_dh)
 {
-	if (*result != NULL) {
+	if (c->ike_proposals != NULL) {
 		DBGF(DBG_CONTROL, "already determined local ESP/AH proposals for %s (%s)",
-		     connection_name, why);
+		     c->name, why);
 		return;
 	}
 
 	const char *notes;
-	if (alg_info_esp == NULL) {
+	if (c->alg_info_esp == NULL) {
 		DBGF(DBG_CONTROL, "selecting default local ESP/AH proposals for %s (%s)",
-		     connection_name, why);
-		lset_t esp_ah = policy & (POLICY_ENCRYPT | POLICY_AUTHENTICATE);
+		     c->name, why);
+		lset_t esp_ah = c->policy & (POLICY_ENCRYPT | POLICY_AUTHENTICATE);
 		struct ikev2_proposals *default_proposals_missing_esn;
 		switch (esp_ah) {
 		case POLICY_ENCRYPT:
@@ -2112,7 +2108,7 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 		 * off and/or this is the AUTH exchange and DH is
 		 * excluded by &unset_group.
 		 */
-		bool add_empty_msdh_duplicates = (policy & POLICY_MSDH_DOWNGRADE) &&
+		bool add_empty_msdh_duplicates = (c->policy & POLICY_MSDH_DOWNGRADE) &&
 			default_dh != NULL && default_dh != &unset_group;
 
 		/*
@@ -2143,7 +2139,7 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 		int propnum;
 		struct ikev2_proposal *proposal;
 		FOR_EACH_PROPOSAL(propnum, proposal, proposals) {
-			add_esn_transforms(proposal, policy);
+			add_esn_transforms(proposal, c->policy);
 		}
 		if (default_dh != NULL && default_dh != &unset_group) {
 			DBGF(DBG_CONTROL, "adding dh %s to default proposals",
@@ -2157,7 +2153,7 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 					break;
 			}
 		}
-		*result = proposals;
+		c->ike_proposals = proposals;
 		notes = " (default)";
 	} else {
 
@@ -2170,7 +2166,7 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 			} else {
 				lswlogf(buf, "default DH %s", default_dh->common.name);
 			}
-			lswlogf(buf, "  for %s (%s)", connection_name, why);
+			lswlogf(buf, "  for %s (%s)", c->name, why);
 		}
 
 		/*
@@ -2180,15 +2176,15 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 		 * Even when DEFAULT_DH is NULL, DH may be added
 		 * (found in alg-info).  Deal with that below.
 		 */
-		bool add_empty_msdh_duplicates = (policy & POLICY_MSDH_DOWNGRADE) &&
+		bool add_empty_msdh_duplicates = (c->policy & POLICY_MSDH_DOWNGRADE) &&
 			default_dh != &unset_group;
 
 		struct ikev2_proposals *proposals = alloc_thing(struct ikev2_proposals,
 								"ESP/AH proposals");
-		int proposals_roof = alg_info_esp->ai.alg_info_cnt + 1;
+		int proposals_roof = c->alg_info_esp->ai.alg_info_cnt + 1;
 		if (add_empty_msdh_duplicates) {
 			/* make space for everything duplicated */
-			proposals_roof += alg_info_esp->ai.alg_info_cnt;
+			proposals_roof += c->alg_info_esp->ai.alg_info_cnt;
 		}
 		proposals->proposal = alloc_things(struct ikev2_proposal, proposals_roof,
 						   "ESP/AH proposal");
@@ -2196,7 +2192,7 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 		proposals->roof = 1;
 
 		enum ikev2_sec_proto_id protoid;
-		switch (policy & (POLICY_ENCRYPT | POLICY_AUTHENTICATE)) {
+		switch (c->policy & (POLICY_ENCRYPT | POLICY_AUTHENTICATE)) {
 		case POLICY_ENCRYPT:
 			protoid = IKEv2_SEC_PROTO_ESP;
 			break;
@@ -2204,11 +2200,11 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 			protoid = IKEv2_SEC_PROTO_AH;
 			break;
 		default:
-			bad_case(policy);
+			bad_case(c->policy);
 		}
 
 		for (int dup = 0; dup < (add_empty_msdh_duplicates ? 2 : 1); dup++) {
-			FOR_EACH_ESP_INFO(alg_info_esp, esp_info) {
+			FOR_EACH_ESP_INFO(c->alg_info_esp, esp_info) {
 				LSWDBGP(DBG_CONTROL, log) {
 					lswlogf(log, "converting proposal ");
 					lswlog_proposal_info(log, esp_info);
@@ -2231,7 +2227,7 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 									  proposals,
 									  dup ? &unset_group : default_dh);
 				if (proposal != NULL) {
-					add_esn_transforms(proposal, policy);
+					add_esn_transforms(proposal, c->policy);
 					DBG(DBG_CONTROL,
 					    DBG_log_ikev2_proposal("... ", proposal));
 					proposals->roof++;
@@ -2239,16 +2235,17 @@ void ikev2_proposals_from_alg_info_esp(const char *connection_name,
 			}
 		}
 
-		*result = proposals;
+		c->ike_proposals = proposals;
 		notes = "";
 	}
 
 	LSWLOG(buf) {
 		lswlogf(buf, "local ESP/AH proposals for %s (%s): ",
-			connection_name, why);
-		print_proposals(buf, *result);
+			c->name, why);
+		print_proposals(buf, c->ike_proposals);
 		lswlogs(buf, notes);
 	}
+	passert(c->ike_proposals != NULL);
 }
 
 struct ipsec_proto_info *ikev2_child_sa_proto_info(struct state *st, lset_t policy)

programs/pluto/ikev2.h
@@ -94,17 +94,11 @@ void DBG_log_ikev2_proposal(const char *prefix, struct ikev2_proposal *proposal)
 void free_ikev2_proposal(struct ikev2_proposal **proposal);
 void free_ikev2_proposals(struct ikev2_proposals **proposals);
 
-void ikev2_proposals_from_alg_info_ike(const char *connection_name,
-				       const char *why,
-				       struct alg_info_ike *alg_info_ike,
-				       struct ikev2_proposals **proposals);
+void ikev2_need_ike_proposals(struct connection *c, const char *why);
 
-void ikev2_proposals_from_alg_info_esp(const char *connection_name,
-				       const char *why,
-				       struct alg_info_esp *alg_info_esp,
-				       lset_t policy,
-				       const struct oakley_group_desc *default_dh,
-				       struct ikev2_proposals **proposals);
+void ikev2_need_esp_or_ah_proposals(struct connection *c,
+				    const char *why,
+				    const struct oakley_group_desc *default_dh);
 
 bool ikev2_emit_sa_proposal(pb_stream *pbs,
 			    struct ikev2_proposal *proposal,

programs/pluto/ikev2_parent.c
@@ -803,10 +803,7 @@ void ikev2_parent_outI1(int whack_sock,
 	 * Initialize st->st_oakley, including the group number.
 	 * Grab the DH group from the first configured proposal and build KE.
 	 */
-	ikev2_proposals_from_alg_info_ike(c->name,
-					  "IKE SA initiator selecting KE",
-					  c->alg_info_ike,
-					  &c->ike_proposals);
+	ikev2_need_ike_proposals(c, "IKE SA initiator selecting KE");
 	st->st_oakley.ta_dh = ikev2_proposals_first_dh(c->ike_proposals);
 	if (st->st_oakley.ta_dh == NULL) {
 		libreswan_log("proposals do not contain a valid DH");
@@ -943,10 +940,7 @@ static stf_status ikev2_parent_outI1_common(struct msg_digest *md UNUSED,
 	}
 	/* SA out */
 	{
-		ikev2_proposals_from_alg_info_ike(c->name,
-						  "IKE SA initiator emitting local proposals",
-						  c->alg_info_ike,
-						  &c->ike_proposals);
+		ikev2_need_ike_proposals(c, "IKE SA initiator emitting local proposals");
 		/*
 		 * Since this is an initial IKE exchange, the SPI is
 		 * emitted as is part of the packet header and not the
@@ -1300,9 +1294,7 @@ stf_status ikev2_parent_inI1outR1(struct state *null_st, struct msg_digest *md)
 	}
 
 	/* Get the proposals ready.  */
-	ikev2_proposals_from_alg_info_ike(c->name, "IKE SA responder matching remote proposals",
-					  c->alg_info_ike,
-					  &c->ike_proposals);
+	ikev2_need_ike_proposals(c, "IKE SA responder matching remote proposals");
 
 	/*
 	 * Select the proposal.
@@ -1814,10 +1806,8 @@ stf_status ikev2_IKE_SA_process_SA_INIT_response_notification(struct state *st,
 			pstats(invalidke_recv_s, sg.sg_group);
 			pstats(invalidke_recv_u, st->st_oakley.ta_dh->group);
 
-			ikev2_proposals_from_alg_info_ike(c->name,
-							  "IKE SA initiator validating remote's suggested KE",
-							  c->alg_info_ike,
-							  &c->ike_proposals);
+			ikev2_need_ike_proposals(c, "IKE SA initiator validating remote's suggested KE");
+
 			if (ikev2_proposals_include_modp(c->ike_proposals, sg.sg_group)) {
 				DBG(DBG_CONTROLMORE, DBG_log("Suggested modp group is acceptable"));
 				/*
@@ -2086,10 +2076,8 @@ stf_status ikev2_parent_inR1outI2(struct state *st, struct msg_digest *md)
 		/* SA body in and out */
 		struct payload_digest *const sa_pd =
 			md->chain[ISAKMP_NEXT_v2SA];
-		ikev2_proposals_from_alg_info_ike(c->name,
-						  "IKE SA initiator accepting remote proposal",
-						  c->alg_info_ike,
-						  &c->ike_proposals);
+		ikev2_need_ike_proposals(c, "IKE SA initiator accepting remote proposal");
+
 		stf_status ret = ikev2_process_sa_payload("IKE initiator (accepting)",
 							  &sa_pd->pbs,
 							  /*expect_ike*/ TRUE,
@@ -3526,12 +3514,9 @@ static stf_status ikev2_parent_inR1outI2_tail(struct state *pst, struct msg_dige
 		 * used.
 		 */
 		free_ikev2_proposals(&cc->esp_or_ah_proposals);
-		ikev2_proposals_from_alg_info_esp(cc->name,
-						  "IKE SA initiator emitting ESP/AH proposals",
-						  cc->alg_info_esp,
-						  cc->policy, &unset_group,
-						  &cc->esp_or_ah_proposals);
-		passert(cc->esp_or_ah_proposals != NULL);
+		ikev2_need_esp_or_ah_proposals(cc,
+					       "IKE SA initiator emitting ESP/AH proposals",
+					       &unset_group);
 
 		ikev2_emit_sa_proposals(&e_pbs_cipher, cc->esp_or_ah_proposals,
 					&local_spi, ISAKMP_NEXT_v2TSi);
@@ -4420,12 +4405,7 @@ stf_status ikev2_process_child_sa_pl(struct msg_digest *md,
 		free_ikev2_proposals(&c->esp_or_ah_proposals);
 	}
 
-	ikev2_proposals_from_alg_info_esp(c->name, what,
-					  c->alg_info_esp,
-					  c->policy,
-					  default_dh,
-					  &c->esp_or_ah_proposals);
-	passert(c->esp_or_ah_proposals != NULL);
+	ikev2_need_esp_or_ah_proposals(c, what, default_dh);
 
 	ret = ikev2_process_sa_payload(what,
 			&sa_pd->pbs,
@@ -5352,10 +5332,7 @@ static stf_status ikev2_child_add_ike_payloads(struct msg_digest *md,
 		local_nonce = st->st_ni;
 
 		free_ikev2_proposals(&c->ike_proposals);
-		ikev2_proposals_from_alg_info_ike(c->name,
-				"IKE SA initiating rekey",
-				c->alg_info_ike,
-				&c->ike_proposals);
+		ikev2_need_ike_proposals(c, "IKE SA initiating rekey");
 
 		/* send v2 IKE SAs*/
 		if (!ikev2_emit_sa_proposals(outpbs,
@@ -5426,9 +5403,8 @@ static notification_t process_ike_rekey_sa_pl_response(struct msg_digest *md,
 	struct payload_digest *const sa_pd = md->chain[ISAKMP_NEXT_v2SA];
 
 	/* Get the proposals ready.  */
-	ikev2_proposals_from_alg_info_ike(c->name, "IKE SA accept response to rekey",
-					  c->alg_info_ike,
-					  &c->ike_proposals);
+	ikev2_need_ike_proposals(c, "IKE SA accept response to rekey");
+
 	stf_status ret = ikev2_process_sa_payload("IKE initiator (accepting)",
 						  &sa_pd->pbs,
 						  /*expect_ike*/ TRUE,
@@ -5473,9 +5449,8 @@ static notification_t process_ike_rekey_sa_pl(struct msg_digest *md, struct stat
 	struct payload_digest *const sa_pd = md->chain[ISAKMP_NEXT_v2SA];
 
 	/* Get the proposals ready.  */
-	ikev2_proposals_from_alg_info_ike(c->name, "IKE SA responding to rekey",
-					  c->alg_info_ike,
-					  &c->ike_proposals);
+	ikev2_need_ike_proposals(c, "IKE SA responding to rekey");
+
 	stf_status ret = ikev2_process_sa_payload("IKE Rekey responder child",
 			&sa_pd->pbs,
 			/*expect_ike*/ TRUE,
@@ -6697,33 +6672,33 @@ void ikev2_initiate_child_sa(struct pending *p)
 		 */
 		free_ikev2_proposals(&c->esp_or_ah_proposals);
 		const struct oakley_group_desc *default_dh =
-			((c->policy & POLICY_PFS) != LEMPTY) ? ike->sa.st_oakley.ta_dh : NULL;
-		ikev2_proposals_from_alg_info_esp(c->name, "ESP/AH initiator emitting proposals",
-				c->alg_info_esp,
-				c->policy, default_dh,
-				&c->esp_or_ah_proposals);
-		passert(c->esp_or_ah_proposals != NULL);
+			c->policy & POLICY_PFS ? ike->sa.st_oakley.ta_dh : NULL;
+
+		ikev2_need_esp_or_ah_proposals(c,
+					       "ESP/AH initiator emitting proposals",
+					       default_dh);
+
 		st->st_pfs_group = ikev2_proposals_first_dh(c->esp_or_ah_proposals);
 
 		DBG(DBG_CONTROLMORE, {
-				const char *pfsgroupname = st->st_pfs_group == NULL ?
-				"no-pfs" : st->st_pfs_group->common.name;
+			const char *pfsgroupname = st->st_pfs_group == NULL ?
+			"no-pfs" : st->st_pfs_group->common.name;
 
-				DBG_log("#%lu schedule %s IPsec SA %s%s using IKE# %lu pfs=%s",
-						st->st_serialno,
-						rst != NULL ? "rekey initiate" : "initiate",
-						prettypolicy(p->policy),
-						replacestr,
-						ike->sa.st_serialno,
-						pfsgroupname);
-				});
+			DBG_log("#%lu schedule %s IPsec SA %s%s using IKE# %lu pfs=%s",
+				st->st_serialno,
+				rst != NULL ? "rekey initiate" : "initiate",
+				prettypolicy(p->policy),
+				replacestr,
+				ike->sa.st_serialno,
+				pfsgroupname);
+		});
 	} else {
 		DBG(DBG_CONTROLMORE, {
-				DBG_log("#%lu schedule initiate IKE Rekey SA %s to replace IKE# %lu",
-						st->st_serialno,
-						prettypolicy(p->policy),
-						ike->sa.st_serialno);
-				});
+			DBG_log("#%lu schedule initiate IKE Rekey SA %s to replace IKE# %lu",
+				st->st_serialno,
+				prettypolicy(p->policy),
+				ike->sa.st_serialno);
+		});
 	}
 
 	event_force(EVENT_v2_INITIATE_CHILD, st);

commit 157af04c882710935c4282dc7d8b78a0895203c8

pluto: fix ikev2_need_esp_or_ah_proposals broken in d23fb67075a07e6a7a2a1b4c09589c60534431e8

programs/pluto/ikev2_spdb_struct.c
@@ -2083,7 +2083,7 @@ void ikev2_need_esp_or_ah_proposals(struct connection *c,
 				    const char *why,
 				    const struct oakley_group_desc *default_dh)
 {
-	if (c->ike_proposals != NULL) {
+	if (c->esp_or_ah_proposals != NULL) {
 		DBGF(DBG_CONTROL, "already determined local ESP/AH proposals for %s (%s)",
 		     c->name, why);
 		return;
@@ -2161,7 +2161,7 @@ void ikev2_need_esp_or_ah_proposals(struct connection *c,
 					break;
 			}
 		}
-		c->ike_proposals = proposals;
+		c->esp_or_ah_proposals = proposals;
 		notes = " (default)";
 	} else {
 
@@ -2243,17 +2243,17 @@ void ikev2_need_esp_or_ah_proposals(struct connection *c,
 			}
 		}
 
-		c->ike_proposals = proposals;
+		c->esp_or_ah_proposals = proposals;
 		notes = "";
 	}
 
 	LSWLOG(buf) {
 		lswlogf(buf, "local ESP/AH proposals for %s (%s): ",
 			c->name, why);
-		print_proposals(buf, c->ike_proposals);
+		print_proposals(buf, c->esp_or_ah_proposals);
 		lswlogs(buf, notes);
 	}
-	passert(c->ike_proposals != NULL);
+	passert(c->esp_or_ah_proposals != NULL);
 }
 
 struct ipsec_proto_info *ikev2_child_sa_proto_info(struct state *st, lset_t policy)