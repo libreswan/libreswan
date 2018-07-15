9f8edb14e70bcdca8e5afbe366bc6ba7319b0481
D. Hugh Redelmeier <hugh@mimosa.com>
2018-07-15 16:05:28 -0400

    pluto: minor format improvement

--- programs/pluto/ikev2.c
+++ programs/pluto/ikev2.c
@@ -2098,14 +2098,12 @@ bool ikev2_decode_peer_id_and_certs(struct msg_digest *md)
 	 */
 	if (initiator) {
 		if (!md->st->st_peer_alt_id &&
-			!same_id(&c->spd.that.id, &peer_id) &&
-			c->spd.that.id.kind != ID_FROMCERT) {
-
+		    !same_id(&c->spd.that.id, &peer_id) &&
+		    c->spd.that.id.kind != ID_FROMCERT) {
 			char expect[IDTOA_BUF],
 			     found[IDTOA_BUF];
 
-			idtoa(&c->spd.that.id, expect,
-				sizeof(expect));
+			idtoa(&c->spd.that.id, expect, sizeof(expect));
 			idtoa(&peer_id, found, sizeof(found));
 			loglog(RC_LOG_SERIOUS,
 				"we require IKEv2 peer to have ID '%s', but peer declares '%s'",
