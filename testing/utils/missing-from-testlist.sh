d180117d71d4e4b6e05ec1cb558e192e541a38bf
Andrew Cagney <cagney@gnu.org>
2018-08-01 12:12:44 -0400

    testing: add missing-from-testlist.sh hack

---
+++ testing/utils/missing-from-testlist.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# hack to print the test directories that don't appear in TESTLIST.
+
+nf=18
+df=10
+
+{
+    # an approx list of test directories
+    ls -ld testing/pluto/*/ | cut -d/ -f3
+} | {
+    # filter out any in TESTLIST
+    while read t ; do
+	grep "[^a-z0-9]$t[^-a-z0-9]" testing/pluto/TESTLIST > /dev/null || echo $t
+    done
+} | {
+    # agument each test with author et.al.
+    while read t ; do
+	h=$(git log --format="%h" testing/pluto/$t | tail -1)
+	d=$(git log --date=short --format="%ad" $h -- testing/pluto/$t)
+	n=$(git log --format="%an" $h -- testing/pluto/$t)
+	echo ${n}:${d}:${h}:${t}
+    done
+} | {
+    # sort by name then date
+    sort -t : -k 1,1 -k 2,2V
+} | {
+    # pretty print
+    IFS=:
+    pn=
+    while read n d h t ; do
+	if test "${pn}" != "${n}" ; then
+	    echo
+	    echo "$n"
+	    echo
+	    pn=${n}
+	fi
+	echo ${d} ${h} ${t}
+    done
+}
