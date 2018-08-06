10733a97308503c9867722459e721cbe772da288
Andrew Cagney <cagney@gnu.org>
2018-08-06 12:09:02 -0400

    testing: s/ip ro li/ip route list/
    So it is easier for the sanitizer and humans to understand.

--- testing/pluto/netkey-vti-03/westrun.sh
+++ testing/pluto/netkey-vti-03/westrun.sh
@@ -1,7 +1,7 @@
 ipsec auto --up  westnet-eastnet-vti-01
 ipsec auto --up  westnet-eastnet-vti-02
 # our two ranges should orute into the vti device
-ip ro li
+ip route list
 ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ping -n -c 4 -I 10.0.1.254 10.0.2.254
 ipsec whack --trafficstatus
