ba084547a2350bb236f2330a4f50bddf67ba7f11
Paul Wouters <pwouters@redhat.com>
2018-08-11 15:35:27 -0400

    testing: clarify westrun.sh in ikev2-child-re-up-01 (test still fails)

--- testing/pluto/ikev2-child-re-up-01/westrun.sh
+++ testing/pluto/ikev2-child-re-up-01/westrun.sh
@@ -5,6 +5,8 @@ sleep 5
 # this should cause a rekey-or-reauth
 ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
 sleep 3
-# there should not be two tunnels EVENT_SA_REPLACE?
+ping -n -c 4 -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+# there should not be two tunnels in EVENT_SA_REPLACE? One should be in EVENT_SA_EXPIRE ?
 ipsec status |grep STATE
 echo done
