ec86a4f732b90ae7a180a89c060cd6026d8c926a
D. Hugh Redelmeier <hugh@mimosa.com>
2018-08-12 13:12:40 -0400

    testing: switch to using newer version of strongswan RPM
    Use https://ftp.nohats.ca/strongswan/strongswan-5.6.3-1.fc22.x86_64.rpm
    as specified by Paul on mailing list.

--- testing/libvirt/fedora28.mk
+++ testing/libvirt/fedora28.mk
@@ -7,7 +7,7 @@ KVM_PACKAGE_INSTALL = dnf install -y
 KVM_DEBUGINFO_INSTALL = dnf  debuginfo-install -y
 KVM_INSTALL_RPM_LIST = 'rpm -aq > /var/tmp/rpm-qa-fedora-updates.log'
 
-KVM_STRONGSWAN_PACKAGE ?= https://download.nohats.ca/strongswan/strongswan-5.6.0-1.fc22.x86_64.rpm
+KVM_STRONGSWAN_PACKAGE ?= https://ftp.nohats.ca/strongswan/strongswan-5.6.3-1.fc22.x86_64.rpm
 KVM_LIBFAKETIME_PACKAGE ?= https://download.nohats.ca/libfaketime/libfaketime-0.9.6-4.fc22.x86_64.rpm
 
 KVM_PACKAGES = \
