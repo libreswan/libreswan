e8678ba78112c75ab1d873f34907ab263123d695
Paul Wouters <pwouters@redhat.com>
2018-08-16 13:44:12 -0400

    testing: fixup strongswan tests, remove cast tests (as strongswan dropped support)

--- testing/pluto/interop-ikev1-strongswan-08-strongswan-cast/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep --userland strongswan
-modprobe cast6_generic
-modprobe cast5_generic
-modprobe cast_common
-../../pluto/bin/strongswan-start.sh
-echo "initdone"
--- testing/pluto/interop-ikev2-strongswan-09-dh22-initiator/final.sh
+++
@@ -1,9 +0,0 @@
-if [ -f /var/run/pluto/pluto.pid ]; then ipsec whack --trafficstatus ; fi
-if [ -f /var/run/charon.pid ]; then strongswan status ; fi
-: ==== cut ====
-if [ -f /var/run/pluto/pluto.pid ]; then ipsec auto --status ; fi
-if [ -f /var/run/charon.pid ]; then strongswan statusall ; fi
-: ==== tuc ====
-../bin/check-for-core.sh
-if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
-: ==== end ====
