8c6d5fdc1dc57945e695f41bfb5528978e704b7f
Paul Wouters <pwouters@redhat.com>
2018-08-27 23:07:46 -0400

    testing: update xauth-pluto-28-twobehindnat
    This test still fails. it seems road connecting causes east's policy
    rules for north to get mangled, leaving only 4 of 6 ip xfrm policy
    rules in place.

--- testing/pluto/xauth-pluto-28-twobehindnat/final.sh
+++ testing/pluto/xauth-pluto-28-twobehindnat/final.sh
@@ -1,3 +1,4 @@
+# on east this should show 2 sets of in/fwd/out policies
 ../../pluto/bin/ipsec-look.sh
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
--- testing/pluto/xauth-pluto-28-twobehindnat/northinit.sh
+++ testing/pluto/xauth-pluto-28-twobehindnat/northinit.sh
@@ -3,8 +3,7 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ../bin/block-non-ipsec.sh
 ipsec auto --add north-east
-# re-uses same username, should not cause problems
-ipsec whack --xauthname 'xroad' --xauthpass 'use1pass' --name north-east --initiate
-ping -n -c 4 -I 192.0.2.201 192.0.2.254
+ipsec whack --xauthname 'use1' --xauthpass 'use1pass' --name north-east --initiate
+ping -n -c 4 -I 192.0.2.101 192.0.2.254
 ipsec whack --trafficstatus
 echo initdone
