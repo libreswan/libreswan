45961a3563e52ccb3625c1c0bc36558732664c48
Andrew Cagney <cagney@gnu.org>
2018-09-04 17:00:47 -0400

    testing: use --impair delete-on-retransmit to supress another IKEv1 retransmit
    The more nuanced --impair retransmits only used by IKEv2 tests.

--- testing/pluto/basic-pluto-15-no-retransmit/westinit.sh
+++ testing/pluto/basic-pluto-15-no-retransmit/westinit.sh
@@ -1,6 +1,7 @@
 /testing/guestbin/swan-prep
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
+ipsec whack --impair delete-on-retransmit
 ipsec whack --impair retransmits
 ipsec auto --add westnet-eastnet
 ipsec auto --add westnet-eastnet-ikev2
