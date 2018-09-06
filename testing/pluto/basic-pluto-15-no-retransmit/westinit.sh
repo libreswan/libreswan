58bf7e2c86f1516e2bfd9689ea27ab61e3df2210
Andrew Cagney <cagney@gnu.org>
2018-09-06 16:21:33 -0400

    testing: in basic-pluto-15-no-retransmit, just use impair delete-on-retransmit
    since things should die quickly

--- testing/pluto/basic-pluto-15-no-retransmit/westinit.sh
+++ testing/pluto/basic-pluto-15-no-retransmit/westinit.sh
@@ -1,8 +1,8 @@
 /testing/guestbin/swan-prep
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
+# suffer a quick death
 ipsec whack --impair delete-on-retransmit
-ipsec whack --impair retransmits
 ipsec auto --add westnet-eastnet
 ipsec auto --add westnet-eastnet-ikev2
 echo "initdone"
