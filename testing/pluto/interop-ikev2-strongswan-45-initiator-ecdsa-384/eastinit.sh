8ed3a605ad19c10d38e593dc7afdc3f643ece91b
Paul Wouters <pwouters@redhat.com>
2018-09-10 20:00:14 -0400

    testing: updated ECDSA test cases (removed one obsolete one, added new one)

--- testing/pluto/interop-ikev2-strongswan-45-initiator-ecdsa-384/eastinit.sh
+++ testing/pluto/interop-ikev2-strongswan-45-initiator-ecdsa-384/eastinit.sh
@@ -1,12 +1,8 @@
 /testing/guestbin/swan-prep 
-# next two line should not be needed
-certutil -A -i /testing/x509/strongswan/strongEastCert.der -n "strongEast" -t "P,," -d sql:/etc/ipsec.d/
-certutil -A -i /testing/x509/strongswan/strongWestCert.der -n "strongWest" -t "P,," -d sql:/etc/ipsec.d/
 /usr/bin/pk12util -i /testing/x509/strongswan/strongEast.p12 -d sql:/etc/ipsec.d -w /testing/x509/nss-pw
 # Tuomo: why doesn't ipsec checknss --settrust work here?
 certutil -M -d sql:/etc/ipsec.d -n "strongSwan CA - strongSwan" -t CT,,
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-#tcpdump -w OUTPUT/swan12.pcap -i eth0 -c 1000
 echo "initdone"
