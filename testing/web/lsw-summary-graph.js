83f9ddd8073d3e34056f324f66935c062c6e37bb
Andrew Cagney <cagney@gnu.org>
2018-09-13 20:12:16 -0400

    web: use the full hash when matching test runs with commits
    ... instead of the abbreviated hash (which is git version dependent).
    
    Fixes a bug where a test run created on a remote system (and different
    git version) would be discarded because the run's abbreviated commit
    hash didn't match the abbreviated commit hash generated by the local
    git.

--- testing/web/lsw-summary-graph.js
+++ testing/web/lsw-summary-graph.js
@@ -102,7 +102,7 @@ function lsw_summary_graph(graph_id, table_id, summary) {
 	})
 	// finally grand total as "untested"
 	sum.push(test_run.total)
-	sums[test_run.commit.abbreviated_commit_hash] = sum
+	sums[test_run.commit.hash] = sum
     })
 
     //
@@ -139,7 +139,7 @@ function lsw_summary_graph(graph_id, table_id, summary) {
 	.domain([
 	    d3.min(full_test_runs, function(d) {
 		// very first accumulative value
-		return 0.8 * sums[d.commit.abbreviated_commit_hash][0]
+		return 0.8 * sums[d.commit.hash][0]
 	    }),
 	    d3.max(full_test_runs, function(d) {
 		return 1.02 * d.total
@@ -234,7 +234,7 @@ function lsw_summary_graph(graph_id, table_id, summary) {
 		return x(test_run.commit.committer.date)
 	    })
 	    .y(function(test_run) {
-		return y(sums[test_run.commit.abbreviated_commit_hash][sum_index])
+		return y(sums[test_run.commit.hash][sum_index])
 	    })
 	svg.append("path")
 	    .datum(good_first_parent_test_runs)
@@ -258,7 +258,7 @@ function lsw_summary_graph(graph_id, table_id, summary) {
 		return x(test_run.commit.committer.date)
 	    })
 	    .attr("cy", function(test_run) {
-		return y(sums[test_run.commit.abbreviated_commit_hash][sum_index])
+		return y(sums[test_run.commit.hash][sum_index])
 	    })
 	    .on("click", function(test_run) {
 		lsw_summary_graph_click_test_run(table_id, test_run)
@@ -275,7 +275,7 @@ function lsw_summary_graph(graph_id, table_id, summary) {
 	if (newest_first_parent_test_run) {
 	    keys.push({
 		x: keys_x,
-		y: y(sums[newest_first_parent_test_run.commit.abbreviated_commit_hash][sum_index]),
+		y: y(sums[newest_first_parent_test_run.commit.hash][sum_index]),
 		klass: sum_klass[sum_index],
 		text: sum_text[sum_index],
 	    })
