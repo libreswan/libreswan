7c9001fe40fe8282b70252c6b5470a684626f3ae
Paul Wouters <pwouters@redhat.com>
2018-09-17 01:06:26 -0400

    libswan: asn1.c should use %zu for size_t

--- lib/libswan/asn1.c
+++ lib/libswan/asn1.c
@@ -164,7 +164,7 @@ size_t asn1_length_signature(chunk_t *blob , chunk_t *sig_val)
 		sig_val->len = len_r;
 		/* XXX: need to check len_r and len_s fits in this */
 	        sig_val->ptr = alloc_bytes(len_r * 2, "ec points");
-		DBG(DBG_PARSING, DBG_log(" sig_val  len is %ld",sig_val->len));
+		DBG(DBG_PARSING, DBG_log(" sig_val  len is %zu",sig_val->len));
 		/* copy the values of r into signature */
 		memcpy(sig_val->ptr,blob->ptr,len_r);
 
@@ -184,7 +184,7 @@ size_t asn1_length_signature(chunk_t *blob , chunk_t *sig_val)
 			}
 			DBG(DBG_PARSING, DBG_log("  len_s is %d",len_s));
 			sig_val->len += len_s;
-			DBG(DBG_PARSING, DBG_log(" sig_val total len is %ld",sig_val->len));
+			DBG(DBG_PARSING, DBG_log(" sig_val total len is %zu",sig_val->len));
 			/* copy the values of r into signature */
 			memcpy(sig_val->ptr+len_r,blob->ptr,len_s);
 		}
