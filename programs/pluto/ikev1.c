fa004e7d4b83fbeaa8d0f6d8430a96aed97a97b9
D. Hugh Redelmeier <hugh@mimosa.com>
2018-09-19 12:51:01 -0400

    pluto: ikev1.c: when rejecting an unexpected payload, log the state name

--- programs/pluto/ikev1.c
+++ programs/pluto/ikev1.c
@@ -1967,9 +1967,10 @@ void process_packet_tail(struct msg_digest **mdp)
 					      LELEM(ISAKMP_NEXT_CR) |
 					      LELEM(ISAKMP_NEXT_CERT))) {
 					loglog(RC_LOG_SERIOUS,
-						"%smessage ignored because it contains an unexpected payload type (%s)",
+						"%smessage ignored because it contains a payload type (%s) unexpected by state %s",
 						excuse,
-						enum_show(&ikev1_payload_names, np));
+						enum_show(&ikev1_payload_names, np),
+						st->st_state_name);
 					SEND_NOTIFICATION(INVALID_PAYLOAD_TYPE);
 					return;
 				}
