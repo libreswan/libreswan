243ae4b255b9c581b36a1dceb149f3e1c1729573
Paul Wouters <pwouters@redhat.com>
2018-09-21 16:46:52 -0400

    testing: extend delete-sa-01 to test for bogus empty informational message

--- testing/pluto/delete-sa-01/final.sh
+++ testing/pluto/delete-sa-01/final.sh
@@ -1,4 +1,6 @@
 ../../pluto/bin/ipsec-look.sh
+# up to 3.26 we printed a bogus message, this is checking that no longer happens
+grep "received and ignored empty informational" /tmp/pluto.log
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
