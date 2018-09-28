19eae41cf6042690d4c8ae113c376ec966cf21af
Paul Wouters <pwouters@redhat.com>
2018-09-28 13:13:44 -0400

    Documentation: Another pass on the GPL / LGPL license links
    We are actually GPLv2+ and LGPLv2+. The latest license versions are only 3+ and not 2+
    
    This was verified all the way back to the freeswan license:
    
    https://download.libreswan.org/old-freeswan/freeswan-2.06/COPYING

--- programs/klipsdebug/klipsdebug.c
+++ programs/klipsdebug/klipsdebug.c
@@ -7,7 +7,7 @@
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.  See <https://www.gnu.org/licenses/gpl.txt>.
+ * option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
@@ -66,7 +66,7 @@ char copyright[] =
    This program is free software; you can redistribute it and/or modify it\n\
    under the terms of the GNU General Public License as published by the\n\
    Free Software Foundation; either version 2 of the License, or (at your\n\
-   option) any later version.  See <https://www.gnu.org/licenses/gpl.txt>.\n\
+   option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.\n\
 \n\
    This program is distributed in the hope that it will be useful, but\n\
    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY\n\
