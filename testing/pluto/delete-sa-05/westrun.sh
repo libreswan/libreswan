574cb62e43c9536e1da9f34f8d4d4a55c004f4f5
Paul Wouters <pwouters@redhat.com>
2018-09-30 19:26:22 -0400

    testing: delete-sa-05: add a sleep to prevent race condition in output

--- testing/pluto/delete-sa-05/westrun.sh
+++ testing/pluto/delete-sa-05/westrun.sh
@@ -3,6 +3,6 @@ sleep 30
 ipsec whack --trafficstatus
 # delete instead of down so it won't re-establish due to east auto=start
 ipsec auto --delete west-east-auto
+sleep 2
 # We should still have the ISAKMP SA for west-east-auto2
 ipsec status |grep west-east | grep STATE_
-
