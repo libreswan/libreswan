054026dba22d531392479ec39f59e3502da5a137
Paul Wouters <pwouters@redhat.com>
2018-10-08 15:37:51 -0400

    testing: minor updates

--- testing/pluto/ikev2-ike-rekey-03/final.sh
+++ testing/pluto/ikev2-ike-rekey-03/final.sh
@@ -1,8 +1,7 @@
 ipsec whack --trafficstatus
 ipsec status |grep STATE_ | sort
-# there should be only one IKE_INIT exchange
-grep "STATE_PARENT_I1 with STF_OK" /tmp/pluto.log
-grep "STATE_PARENT_R1 with STF_OK" /tmp/pluto.log
+# there should be only one IKE_INIT exchange created
+grep "STATE_UNDEFINED(ignore) => STATE_PARENT" /tmp/pluto.log  |grep parent
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
