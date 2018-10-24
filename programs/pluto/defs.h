libreswan/libreswan

commit aa6b8949ee79da4246c981ff43da8ad24afc6507

packets: change type of Message ID to ft_nat (network byte ordered natural) as expected by IKEv2
The debug-log lines change to:
    Message ID: 1 (0x1)
it might be nice for all ft_nat fields to be instead be logged as:
    Message ID: 1 (00 00 00 01)

IKEv2: Message ID is a network ordered natural number

By pre-emptively converting the Message ID network <-> host order in
the packet parser, byte-swap code litering IKEv2 can be eliminated.

The field .msgid_received field _should_ be redundant (as you can tell by
this comment, things instead break because wierd msgid games are being played).

IKEv1: Message ID is either 0 or a random value

Since internal Message IDs are all re-ordered code testing for
equality or zero continues to work (and logging the value as an
integer no longer needs to convert).

The got-ya is in IKEv1's auth code where the host byte-ordered
Messsage ID needs to be converted to network order before being
fed into the hmac.

programs/pluto/ikev1_quick.c
@@ -677,16 +677,18 @@ static size_t quick_mode_hash12(u_char *dest, const u_char *start,
 	DBG_dump("hash key", st->st_skeyid_a.ptr, st->st_skeyid_a.len);
 #endif
 	hmac_init(&ctx, st->st_oakley.ta_prf, st->st_skeyid_a_nss);
-	hmac_update(&ctx, (const void *) msgid, sizeof(msgid_t));
+	passert(sizeof(msgid_t) == sizeof(uint32_t));
+	msgid_t raw_msgid = htonl(*msgid);
+	hmac_update(&ctx, (const void *)&raw_msgid, sizeof(raw_msgid));
 	if (hash2)
 		hmac_update_chunk(&ctx, st->st_ni); /* include Ni_b in the hash */
 	hmac_update(&ctx, start, roof - start);
 	hmac_final(dest, &ctx);
 
 	DBG(DBG_CRYPT, {
-		    DBG_log("HASH(%d) computed:", hash2 + 1);
-		    DBG_dump("", dest, ctx.hmac_digest_len);
-	    });
+			DBG_log("HASH(%d) computed:", hash2 + 1);
+			DBG_dump("", dest, ctx.hmac_digest_len);
+		});
 	return ctx.hmac_digest_len;
 
 #   undef hmac_update
@@ -704,7 +706,9 @@ static size_t quick_mode_hash3(u_char *dest, struct state *st)
 
 	hmac_init(&ctx, st->st_oakley.ta_prf, st->st_skeyid_a_nss);
 	hmac_update(&ctx, (const u_char *)"\0", 1);
-	hmac_update(&ctx, (u_char *) &st->st_msgid, sizeof(st->st_msgid));
+	passert(sizeof(msgid_t) == sizeof(uint32_t));
+	msgid_t raw_msgid = htonl(st->st_msgid);
+	hmac_update(&ctx, (const void*)&raw_msgid, sizeof(raw_msgid));
 	hmac_update_chunk(&ctx, st->st_ni);
 	hmac_update_chunk(&ctx, st->st_nr);
 	hmac_final(dest, &ctx);

programs/pluto/ikev2.c
@@ -1092,7 +1092,7 @@ static struct state *process_v2_child_ix(struct msg_digest *md,
 	if (is_msg_request(md)) {
 		/* this an IKE request and not a response */
 		if (resp_state_with_msgid(pst->st_serialno,
-					  htonl(md->msgid_received)) != NULL) {
+					  md->msgid_received) != NULL) {
 			what = "CREATE_CHILD_SA Request retransmission ignored";
 			st = NULL;
 		} else if (md->from_state == STATE_V2_CREATE_R) {
@@ -1100,21 +1100,21 @@ static struct state *process_v2_child_ix(struct msg_digest *md,
 			st = ikev2_duplicate_state(pexpect_ike_sa(pst), IPSEC_SA,
 						   SA_RESPONDER);
 			change_state(st, STATE_V2_CREATE_R);
-			st->st_msgid = htonl(md->msgid_received);
+			st->st_msgid = md->msgid_received;
 			insert_state(st); /* needed for delete - we are duplicating early */
 		} else {
 			what = "IKE Rekey Request";
 			st = ikev2_duplicate_state(pexpect_ike_sa(pst), IKE_SA,
 						   SA_RESPONDER);
 			change_state(st, STATE_V2_REKEY_IKE_R); /* start with this */
-			st->st_msgid = htonl(md->msgid_received);
+			st->st_msgid = md->msgid_received;
 			/* can not call insert_state yet. no IKE cookies yet */
 		}
 	} else  {
 		/* this a response */
 		what = "Child SA Response";
 		st = state_with_parent_msgid(pst->st_serialno,
-				htonl(md->msgid_received));
+				md->msgid_received);
 		if (st == NULL) {
 			switch (md->from_state) {
 			case STATE_V2_CREATE_I:
@@ -1230,7 +1230,7 @@ static bool processed_retransmit(struct state *st,
 				st->st_msgid_lastreplied);
 		}
 		struct state *cst =  resp_state_with_msgid(st->st_serialno,
-							   htonl(st->st_msgid_lastrecv));
+							   st->st_msgid_lastrecv);
 		if (cst == NULL) {
 			/* XXX: why? */
 			return false; /* process the re-transtmited message */
@@ -1303,7 +1303,7 @@ void ikev2_process_packet(struct msg_digest **mdp)
 	 * 2) is it initiator or not?
 	 */
 
-	md->msgid_received = ntohl(md->hdr.isa_msgid);
+	md->msgid_received = md->hdr.isa_msgid;
 	const enum isakmp_xchg_types ix = md->hdr.isa_xchg;
 	md->message_role = (md->hdr.isa_flags & ISAKMP_FLAGS_v2_MSG_R) ? MESSAGE_RESPONSE : MESSAGE_REQUEST;
 	const bool sent_by_ike_initiator = (md->hdr.isa_flags & ISAKMP_FLAGS_v2_IKE_I) != 0;

programs/pluto/ikev2_parent.c
@@ -3267,7 +3267,7 @@ static stf_status ikev2_parent_inR1outI2_tail(struct state *pst, struct msg_dige
 	/* XXX because the early child state ends up with the try counter check, we need to copy it */
 	cst->st_try = pst->st_try;
 
-	cst->st_msgid = htonl(pst->st_msgid_nextuse); /* PAUL: note ordering */
+	cst->st_msgid = pst->st_msgid_nextuse;
 	insert_state(cst);
 	md->st = cst;
 
@@ -5687,12 +5687,12 @@ static stf_status ikev2_child_out_tail(struct msg_digest *md)
 		memcpy(hdr.isa_rcookie, pst->st_rcookie, COOKIE_SIZE);
 		memcpy(hdr.isa_icookie, pst->st_icookie, COOKIE_SIZE);
 		if (IS_CHILD_SA_RESPONDER(st)) {
-			hdr.isa_msgid = htonl(md->msgid_received);
+			hdr.isa_msgid = md->msgid_received;
 			hdr.isa_flags = ISAKMP_FLAGS_v2_MSG_R; /* response on */
 		} else {
-			hdr.isa_msgid = htonl(pst->st_msgid_nextuse);
+			hdr.isa_msgid = pst->st_msgid_nextuse;
 			/* store it to match response */
-			st->st_msgid = htonl(pst->st_msgid_nextuse);
+			st->st_msgid = pst->st_msgid_nextuse;
 		}
 
 		if (pst->st_original_role == ORIGINAL_INITIATOR) {
@@ -6270,7 +6270,7 @@ stf_status process_encrypted_informational_ikev2(struct state *st,
 				.isa_version = build_ikev2_version(),
 				.isa_xchg = ISAKMP_v2_INFORMATIONAL,
 				.isa_flags = ISAKMP_FLAGS_v2_MSG_R,
-				.isa_msgid = htonl(md->msgid_received),
+				.isa_msgid = md->msgid_received,
 				.isa_length = 0, /* filled in on pbs close */
 			};
 

programs/pluto/ikev2_send.c
@@ -387,10 +387,10 @@ pb_stream open_v2_message(pb_stream *reply,
 	 * this is for the IKE SA or a CHILD SA).
 	 */
 	if (md != NULL) {
-		hdr.isa_msgid = htonl(md->msgid_received);
+		hdr.isa_msgid = md->msgid_received;
 	} else {
 		passert(ike != NULL);
-		hdr.isa_msgid = htonl(ike->sa.st_msgid_nextuse);
+		hdr.isa_msgid = ike->sa.st_msgid_nextuse;
 	}
 
 	return open_output_struct_pbs(reply, &hdr, &isakmp_hdr_desc);
@@ -660,7 +660,7 @@ void send_v2_delete(struct state *const st)
 	/* increase message ID for next delete message */
 	/* ikev2_update_msgid_counters need an md */
 	ike->sa.st_msgid_nextuse++;
-	st->st_msgid = htonl(ike->sa.st_msgid_nextuse);
+	st->st_msgid = ike->sa.st_msgid_nextuse;
 }
 
 /*

programs/pluto/isakmp_hdr.h
@@ -51,6 +51,6 @@ struct isakmp_hdr {
 	uint8_t isa_version;	/* high-order 4 bits: Major; low order 4: Minor */
 	uint8_t isa_xchg;	/* Exchange type */
 	uint8_t isa_flags;
-	uint32_t isa_msgid;	/* Message ID (RAW) */
+	uint32_t isa_msgid;	/* Message ID */
 	uint32_t isa_length;	/* Length of message */
 };

programs/pluto/packet.c
@@ -60,7 +60,7 @@ static field_desc isa_fields[] = {
 	{ ft_loose_enum, 8 / BITS_PER_BYTE, "ISAKMP version", &version_names },
 	{ ft_enum, 8 / BITS_PER_BYTE, "exchange type", &exchange_names_ikev1orv2 },
 	{ ft_set, 8 / BITS_PER_BYTE, "flags", isakmp_flag_names },
-	{ ft_raw, 32 / BITS_PER_BYTE, "message ID", NULL },
+	{ ft_nat, 32 / BITS_PER_BYTE, "Message ID", NULL },
 	{ ft_len, 32 / BITS_PER_BYTE, "length", NULL },
 	{ ft_end, 0, NULL, NULL }
 };

programs/pluto/state.c
@@ -674,7 +674,7 @@ struct state *resp_state_with_msgid(so_serial_t psn, msgid_t st_msgid)
 	});
 	DBG(DBG_CONTROL,
 		DBG_log("no waiting child state matching pst #%lu msg id %u",
-			psn, ntohs(st_msgid)));
+			psn, st_msgid));
 	return NULL;
 }
 
@@ -694,7 +694,7 @@ struct state *state_with_parent_msgid(so_serial_t psn, msgid_t st_msgid)
 	});
 	DBG(DBG_CONTROL,
 		DBG_log("no waiting child state matching pst #%lu msg id %u",
-			psn, ntohs(st_msgid)));
+			psn, st_msgid));
 	return NULL;
 }
 
@@ -1614,8 +1614,8 @@ struct state *find_state_ikev1(const uint8_t *icookie,
 			DBG(DBG_CONTROL,
 			    DBG_log("v1 peer and cookies match on #%lu, provided msgid %08" PRIx32 " == %08" PRIx32,
 				    st->st_serialno,
-				    ntohl(msgid),
-				    ntohl(st->st_msgid)));
+				    msgid,
+				    st->st_msgid));
 			if (msgid == st->st_msgid)
 				break;
 		}
@@ -1643,8 +1643,8 @@ struct state *find_state_ikev1_init(const uint8_t *icookie,
 			DBG(DBG_CONTROL,
 			    DBG_log("v1 peer and icookie match on #%lu, provided msgid %08" PRIx32 " == %08" PRIx32,
 				    st->st_serialno,
-				    ntohl(msgid),
-				    ntohl(st->st_msgid)));
+				    msgid,
+				    st->st_msgid));
 			if (msgid == st->st_msgid)
 				break;
 		}
@@ -1844,9 +1844,9 @@ struct state *ikev1_find_info_state(const u_char *icookie,
 		DBG(DBG_CONTROL,
 		    DBG_log("peer and cookies match on #%lu; msgid=%08" PRIx32 " st_msgid=%08" PRIx32 " st_msgid_phase15=%08" PRIx32,
 			    st->st_serialno,
-			    ntohl(msgid),
-			    ntohl(st->st_msgid),
-			    ntohl(st->st_msgid_phase15)));
+			    msgid,
+			    st->st_msgid,
+			    st->st_msgid_phase15));
 		if ((st->st_msgid_phase15 != v1_MAINMODE_MSGID &&
 		     msgid == st->st_msgid_phase15) ||
 		    msgid == st->st_msgid)

testing/pluto/ikev2-liveness-01/final.sh
@@ -1,6 +1,6 @@
 # should be empty if east triggered
 hostname | grep west > /dev/null || ipsec whack --trafficstatus
-grep "message ID:" /tmp/pluto.log
+grep "Message ID:" /tmp/pluto.log
 # grep on east
 hostname | grep west > /dev/null || grep -A 1 "has not responded in" /tmp/pluto.log
 : ==== cut ====

commit 20fe1c6a8170b119ac0f254b09ea0bf707e65033

ikev1: compute the phase 2 IV using the network-ordered Message ID
Broken by aa6b8949ee79da4246c981ff43da8ad24afc6507

programs/pluto/defs.h
@@ -55,7 +55,7 @@ extern const char *check_expiry(realtime_t expiration_date,
 extern volatile bool exiting_pluto;
 extern void exit_pluto(int /*status*/) NEVER_RETURNS;
 
-typedef uint32_t msgid_t;      /* Network order for ikev1, host order for ikev2 */
+typedef uint32_t msgid_t;      /* Host byte ordered */
 
 /* are all bytes 0? */
 extern bool all_zero(const unsigned char *m, size_t len);

programs/pluto/ikev1_quick.c
@@ -736,7 +736,9 @@ void init_phase2_iv(struct state *st, const msgid_t *msgid)
 	struct crypt_hash *ctx = crypt_hash_init(h, "IV", DBG_CRYPT);
 	crypt_hash_digest_bytes(ctx, "PH1_IV", st->st_ph1_iv, st->st_ph1_iv_len);
 	passert(*msgid != 0);
-	crypt_hash_digest_bytes(ctx, "MSGID", (const u_char *)msgid, sizeof(*msgid));
+	passert(sizeof(msgid_t) == sizeof(uint32_t));
+	msgid_t raw_msgid = htonl(*msgid);
+	crypt_hash_digest_bytes(ctx, "MSGID", (void*) &raw_msgid, sizeof(raw_msgid));
 	crypt_hash_final_bytes(&ctx, st->st_new_iv, st->st_new_iv_len);
 
 	DBG_cond_dump(DBG_CRYPT, "computed Phase 2 IV:",