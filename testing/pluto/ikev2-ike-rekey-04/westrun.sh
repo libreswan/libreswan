3a7274b42e5e9447cf3ad4d72fd09a60164c702b
Antony Antony <antony@phenome.org>
2018-11-08 14:51:20 +0100

    testing: ikev2-ike-rekey-04 fix the test, still a wip test

--- testing/pluto/ikev2-ike-rekey-04/westrun.sh
+++ testing/pluto/ikev2-ike-rekey-04/westrun.sh
@@ -1,2 +1,19 @@
 ipsec auto --up westnet-eastnet-ikev2a
+ipsec auto --up westnet-eastnet-ikev2b
+ipsec auto --up westnet-eastnet-ikev2c
+# ike #1 IPsec #2 #3 #4
+ipsec status |grep STATE_
+sleep 30
+# ipsec should be rekeyed
+# ike #1 IPsec #5 #6 #7
+ipsec status |grep STATE_
+sleep 20
+# ike sa should be rekeyed
+# ike #8 IPsec #5 #6 #7
+ipsec status |grep STATE_
+sleep 30
+# second rekey of IPsec SAs
+ipsec status |grep STATE_
+# ike #8 IPsec #9 #10 #11
+sleep 20
 echo done
