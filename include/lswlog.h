41a773747e69768238a67c03f047702665844e15
Andrew Cagney <cagney@gnu.org>
2018-11-13 10:15:58 -0500

    debugging: add dbg(FMT, ...)
    debug-logs when ever any debug-logging is enabled
    intended as replacement for the most common cases:
      DBGF(..., FMT, ...);
      DBG(..., DBG_log(FMT, ...));
    not intended for crypto (dbg_crypto()?) and/or too-much-info
    (dbg_more()?).

--- include/lswlog.h
+++ include/lswlog.h
@@ -297,6 +297,8 @@ void libreswan_exit_log_errno(int e, const char *message, ...) PRINTF_LIKE(2) NE
 
 extern lset_t cur_debugging;	/* current debugging level */
 
+void dbg(const char *fmt, ...) PRINTF_LIKE(1);
+
 #define DBGP(cond)	(cur_debugging & (cond))
 
 #define DEBUG_PREFIX "| "
