libreswan/libreswan

commit 906e43e0eae29d8d45f150c5b33e4ae51385feb7

IKE SPI: replace get_cookie(bool,uint8_t*) with fill_ike_{initiator,responder}_spi(ike_sa,...)
Uses IKEv2 names.  Split into explicit initiator/responder functions
(don't rely on 'bool' and 'uint8_*' being correctly aligned).

Uses a local secret_of_the_day; with size increased to match
SHA2 algorithm.

New code lives in ike_spi.[hc].  Bunch of unnecessary #includes
deleted as a side effect.

Also add, but don't yet use, zero_ike_spi, and ike_spi_is_zero().

programs/pluto/ike_spi.c
@@ -1,0 +1,96 @@
+/* IKE SPI generation routines, for libreswan
+ *
+ * Copyright (C) 1997 Angelos D. Keromytis.
+ * Copyright (C) 1998-2002  D. Hugh Redelmeier.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#include "lswlog.h"
+#include "ike_alg_hash.h"
+
+#include "defs.h"	/* for all_zero */
+#include "state.h"	/* for ike_sa */
+#include "ike_spi.h"
+#include "rnd.h"
+#include "crypt_hash.h"
+
+const uint8_t zero_cookie[IKE_SA_SPI_SIZE];  /* guaranteed 0 */
+
+bool is_zero_cookie(const uint8_t spi[IKE_SA_SPI_SIZE])
+{
+	return memeq(spi, zero_cookie, IKE_SA_SPI_SIZE);
+}
+
+const ike_spi_t zero_ike_spi;  /* guaranteed 0 */
+
+bool ike_spi_is_zero(const ike_spi_t *spi)
+{
+	return memeq(spi, &zero_ike_spi, IKE_SA_SPI_SIZE);
+}
+
+static uint8_t ike_spi_secret[SHA2_256_DIGEST_SIZE];
+
+void refresh_ike_spi_secret(void)
+{
+	get_rnd_bytes(ike_spi_secret, sizeof(ike_spi_secret));
+}
+
+/*
+ * Generate the IKE Initiator's SPI.
+ */
+void fill_ike_initiator_spi(struct ike_sa *ike)
+{
+	do {
+		/* not sizeof(spi) as a pointer */
+		get_rnd_bytes(ike->sa.st_icookie, IKE_SA_SPI_SIZE);
+	} while (is_zero_cookie(ike->sa.st_icookie)); /* probably never loops */
+}
+
+/*
+ * Generate the IKE Responder's SPI.
+ *
+ * As responder, we use a hashing method to get a pseudo random
+ * value instead of using our own random pool. It will prevent
+ * an attacker from gaining raw data from our random pool and
+ * it will prevent an attacker from depleting our random pool
+ * or entropy.
+ */
+void fill_ike_responder_spi(struct ike_sa *ike, const ip_address *addr)
+{
+	do {
+		static uint32_t counter = 0; /* STATIC */
+
+		struct crypt_hash *ctx = crypt_hash_init(&ike_alg_hash_sha2_256,
+							 "IKE Responder SPI",
+							 DBG_CRYPT);
+
+		const unsigned char *addr_ptr;
+		size_t addr_length = addrbytesptr_read(addr, &addr_ptr);
+		crypt_hash_digest_bytes(ctx, "addr",
+					addr_ptr, addr_length);
+
+		crypt_hash_digest_bytes(ctx, "sod",
+					ike_spi_secret,
+					sizeof(ike_spi_secret));
+		counter++;
+		crypt_hash_digest_bytes(ctx, "counter",
+					(const void *) &counter,
+					sizeof(counter));
+
+		u_char buffer[SHA2_256_DIGEST_SIZE];
+		crypt_hash_final_bytes(&ctx, buffer, SHA2_256_DIGEST_SIZE);
+		/* cookie size is smaller than hash output size */
+		passert(IKE_SA_SPI_SIZE <= SHA2_256_DIGEST_SIZE);
+		memcpy(ike->sa.st_rcookie, buffer, IKE_SA_SPI_SIZE);
+
+	} while (is_zero_cookie(ike->sa.st_rcookie)); /* probably never loops */
+}

programs/pluto/ike_spi.h
@@ -1,0 +1,54 @@
+/* IKE SPI generation routines, for libreswan
+ *
+ * Copyright (C) 1998-2002  D. Hugh Redelmeier.
+ * Copyright (C) 2018  Andrew Cagney
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef IKE_SPI_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ietf_constants.h"	/* for IKE_SA_SPI_SIZE */
+#include "ip_address.h"
+
+struct ike_sa;
+
+/*
+ * New and old.
+ *
+ * Switching from spi[IKE_SA_SPI_SIZE] to ike_spi_t will take time as
+ * it churns the code.  First with a cookie->ike_spi rename and second
+ * with a switch to an explicit reference parameter (old code worked
+ * without '&' because parameter was passed by array reference).
+ */
+
+typedef struct {
+	uint8_t ike_spi[IKE_SA_SPI_SIZE];
+} ike_spi_t;
+extern const ike_spi_t zero_ike_spi;
+bool ike_spi_is_zero(const ike_spi_t *ike_spi);
+
+extern const uint8_t zero_cookie[IKE_SA_SPI_SIZE]; /* use zero_ike_spi */
+bool is_zero_cookie(const uint8_t ike_spi[IKE_SA_SPI_SIZE]); /* use ike_spi_is_zero() */
+
+/*
+ * Since these work on IKE SA's they take that as a parameter.
+ */
+
+void fill_ike_initiator_spi(struct ike_sa *ike);
+void fill_ike_responder_spi(struct ike_sa *ike, const ip_address *addr);
+
+void refresh_ike_spi_secret(void);
+
+#endif

programs/pluto/ikev2_parent.c
@@ -62,7 +62,7 @@
 #include "ipsec_doi.h"
 #include "vendor.h"
 #include "timer.h"
-#include "cookie.h"
+#include "ike_spi.h"
 #include "rnd.h"
 #include "pending.h"
 #include "kernel.h"
@@ -771,7 +771,7 @@ void ikev2_parent_outI1(fd_t whack_sock,
 	st = new_state();
 
 	/* set up new state */
-	get_cookie(TRUE, st->st_icookie, &c->spd.that.host_addr);
+	fill_ike_initiator_spi(ike_sa(st));
 	initialize_new_state(st, c, policy, try, whack_sock);
 	st->st_ikev2 = TRUE;
 	change_state(st, STATE_PARENT_I0);
@@ -1378,9 +1378,10 @@ stf_status ikev2_parent_inI1outR1(struct state *null_st, struct msg_digest *md)
 	 */
 	struct state *st = new_state();
 	/* set up new state */
-	memcpy(st->st_icookie, md->hdr.isa_icookie, COOKIE_SIZE);
 	/* initialize_new_state expects valid icookie/rcookie values, so create it now */
-	get_cookie(FALSE, st->st_rcookie, &md->sender);
+	memcpy(st->st_icookie, md->hdr.isa_icookie, IKE_SA_SPI_SIZE);
+	fill_ike_responder_spi(ike_sa(st), &md->sender);
+
 	initialize_new_state(st, c, policy, 0, null_fd);
 	update_ike_endpoints(st, md);
 	st->st_ikev2 = TRUE;
@@ -4590,7 +4591,7 @@ static notification_t process_ike_rekey_sa_pl(struct msg_digest *md, struct stat
 	st->st_accepted_ike_proposal = accepted_ike_proposal;
 
 	ikev2_copy_cookie_from_sa(accepted_ike_proposal, st->st_icookie);
-	get_cookie(TRUE, st->st_rcookie, &md->sender);
+	fill_ike_responder_spi(ike_sa(st), &md->sender);
 	insert_state(st); /* needed for delete - we are duplicating early */
 
 	return STF_OK;
@@ -5680,7 +5681,7 @@ void ikev2_initiate_child_sa(struct pending *p)
 	} else {
 		st = ikev2_duplicate_state(ike, IKE_SA, SA_INITIATOR);
 		st->st_oakley = ike->sa.st_oakley;
-		get_cookie(TRUE, st->st_icookie, &c->spd.that.host_addr);
+		fill_ike_initiator_spi(ike_sa(st));
 		st->st_ike_pred = ike->sa.st_serialno;
 	}
 

include/ike_alg_hash.h
@@ -19,8 +19,8 @@
 extern const struct hash_desc ike_alg_hash_sha1;
 #endif
 
-#ifdef USE_SHA2
 extern const struct hash_desc ike_alg_hash_sha2_256;
+#ifdef USE_SHA2
 extern const struct hash_desc ike_alg_hash_sha2_384;
 extern const struct hash_desc ike_alg_hash_sha2_512;
 #endif

programs/pluto/Makefile
@@ -201,7 +201,8 @@ OBJS += ctr_test_vectors.o
 OBJS += gcm_test_vectors.o
 OBJS += prf_test_vectors.o
 OBJS += test_buffer.o
-OBJS += pending.o cookie.o crypto.o defs.o
+OBJS += pending.o crypto.o defs.o
+OBJS += ike_spi.o
 OBJS += foodgroups.o log.o state.o plutomain.o plutoalg.o server.o
 OBJS += peerlog.o
 OBJS += hash_table.o list_entry.o

programs/pluto/cookie.c
@@ -1,78 +1,0 @@
-/* cookie generation/verification routines.
- * Copyright (C) 1997 Angelos D. Keromytis.
- * Copyright (C) 1998-2002  D. Hugh Redelmeier.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- */
-
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#include <libreswan.h>
-
-#include "constants.h"
-#include "defs.h"
-#include "lswlog.h"
-#include "rnd.h"
-#include "cookie.h"
-#include "ike_alg_hash.h"
-#include "crypt_hash.h"
-
-const u_char zero_cookie[COOKIE_SIZE];  /* guaranteed 0 */
-
-/*
- * Generate a cookie (aka SPI)
- * First argument is true if we're to create an Initiator cookie.
- *
- * As responder, we use a hashing method to get a pseudo random
- * value instead of using our own random pool. It will prevent
- * an attacker from gaining raw data from our random pool and
- * it will prevent an attacker from depleting our random pool
- * or entropy.
- *
- * TODO: This use of SHA2 should be allowed even if we have USE_SHA2=false
- */
-void get_cookie(bool initiator, uint8_t cookie[COOKIE_SIZE],
-		const ip_address *addr)
-{
-	do {
-		if (initiator) {
-			get_rnd_bytes(cookie, COOKIE_SIZE);
-		} else {
-			static uint32_t counter = 0; /* STATIC */
-
-			struct crypt_hash *ctx = crypt_hash_init(&ike_alg_hash_sha2_256,
-								 "cookie", DBG_CRYPT);
-
-			const unsigned char *addr_ptr;
-			size_t addr_length = addrbytesptr_read(addr, &addr_ptr);
-			crypt_hash_digest_bytes(ctx, "addr",
-						addr_ptr, addr_length);
-
-			crypt_hash_digest_bytes(ctx, "sod",
-						secret_of_the_day,
-						sizeof(secret_of_the_day));
-			counter++;
-			crypt_hash_digest_bytes(ctx, "counter",
-						(const void *) &counter,
-						sizeof(counter));
-
-			u_char buffer[SHA2_256_DIGEST_SIZE];
-			crypt_hash_final_bytes(&ctx, buffer, SHA2_256_DIGEST_SIZE);
-			/* cookie size is smaller than hash output size */
-			passert(COOKIE_SIZE <= SHA2_256_DIGEST_SIZE);
-			memcpy(cookie, buffer, COOKIE_SIZE);
-		}
-	} while (is_zero_cookie(cookie)); /* probably never loops */
-}

programs/pluto/cookie.h
@@ -1,22 +1,0 @@
-/* cookie generation/verification routines.
- * Copyright (C) 1998-2002  D. Hugh Redelmeier.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- */
-
-#include <libreswan.h>
-
-extern const u_char zero_cookie[COOKIE_SIZE];   /* guaranteed 0 */
-
-extern void get_cookie(bool initiator, uint8_t cookie[COOKIE_SIZE],
-		       const ip_address *addr);
-
-#define is_zero_cookie(cookie) all_zero((cookie), COOKIE_SIZE)

programs/pluto/demux.c
@@ -49,7 +49,6 @@
 #include "lswlog.h"
 
 #include "defs.h"
-#include "cookie.h"
 #include "id.h"
 #include "x509.h"
 #include "certs.h"

commit f91c7191e4d06a3b30fe41be1a23f9f1ce378ef0

ikev2: fix ike rekey, commit 906e43e0e broke it.
calling fill_ike_initiator_spi() while rekeying IKE SA seems  wrong.
The call overwrote ike, parent sa's, SPI incorrectly.
The rekey message would send with wrong SPI.

testcase to check ikev2-ike-rekey-01

This is a temp fix. For a better fix check how this commit affect ikev1
quick more?

programs/pluto/ike_spi.c
@@ -55,6 +55,15 @@ void fill_ike_initiator_spi(struct ike_sa *ike)
 	} while (is_zero_cookie(ike->sa.st_icookie)); /* probably never loops */
 }
 
+void ikev2_fill_ike_rekey_initiator_spi(uint8_t spi[IKE_SA_SPI_SIZE])
+{
+	do {
+		/* not sizeof(spi) as a pointer */
+		get_rnd_bytes(spi, IKE_SA_SPI_SIZE);
+	} while (is_zero_cookie(spi)); /* probably never loops */
+}
+
+
 /*
  * Generate the IKE Responder's SPI.
  *

programs/pluto/ike_spi.h
@@ -47,6 +47,7 @@ bool is_zero_cookie(const uint8_t ike_spi[IKE_SA_SPI_SIZE]); /* use ike_spi_is_z
  */
 
 void fill_ike_initiator_spi(struct ike_sa *ike);
+void ikev2_fill_ike_rekey_initiator_spi(uint8_t spi[IKE_SA_SPI_SIZE]);
 void fill_ike_responder_spi(struct ike_sa *ike, const ip_address *addr);
 
 void refresh_ike_spi_secret(void);

programs/pluto/ikev2_parent.c
@@ -5660,7 +5660,7 @@ void ikev2_initiate_child_sa(struct pending *p)
 	} else {
 		st = ikev2_duplicate_state(ike, IKE_SA, SA_INITIATOR);
 		st->st_oakley = ike->sa.st_oakley;
-		fill_ike_initiator_spi(ike_sa(st));
+		ikev2_fill_ike_rekey_initiator_spi(st->st_icookie);
 		st->st_ike_pred = ike->sa.st_serialno;
 	}
 