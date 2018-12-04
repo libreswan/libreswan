f48425918e69c15edd8d526cbe4b396373d010d3
Paul Wouters <pwouters@redhat.com>
2018-12-04 12:30:51 -0500

    pluto: calc_skeyseed_v2() don't set next_byte when it won't be used anymore

--- programs/pluto/ikev2_prf.c
+++ programs/pluto/ikev2_prf.c
@@ -186,7 +186,6 @@ static void calc_skeyseed_v2(struct pcr_dh_v2 *sk,
 	SK_pr_k = key_from_symkey_bytes(finalkey, next_byte, skp_bytes);
 	/* store copy of SK_pr_k for later use in authnull */
 	chunk_SK_pr = chunk_from_symkey("chunk_SK_pr", SK_pr_k);
-	next_byte += skp_bytes;	/* next_byte not subsequently used */
 
 	DBG(DBG_CRYPT,
 	    DBG_log("NSS ikev2: finished computing individual keys for IKEv2 SA"));
