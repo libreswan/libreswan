416a087f248991c4e2e240d60767c57bcfc38cd5
Paul Wouters <pwouters@redhat.com>
2018-12-19 15:09:30 -0500

    testing: update ikev2-04-basic-x509
    we delete hardcoded certs from other end, so it depends on CA verification.
    This changes the output. Note this should really be done in swan-prep.
    
    We also have to specify our own cert in conn for each endpoint. This needs
    fixing in more test cases.

--- testing/pluto/ikev2-04-basic-x509/eastinit.sh
+++ testing/pluto/ikev2-04-basic-x509/eastinit.sh
@@ -1,4 +1,5 @@
 /testing/guestbin/swan-prep --x509
+certutil -d sql:/etc/ipsec.d -D -n west
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
--- testing/pluto/ikev2-04-basic-x509/westinit.sh
+++ testing/pluto/ikev2-04-basic-x509/westinit.sh
@@ -1,4 +1,5 @@
 /testing/guestbin/swan-prep --x509
+certutil -d sql:/etc/ipsec.d -D -n east
 # confirm that the network is alive
 ../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # ensure that clear text does not get through
