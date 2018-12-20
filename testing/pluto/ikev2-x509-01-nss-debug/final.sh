90652bbe236075c6543bbe21c85a9d67085d8d0f
Paul Wouters <pwouters@redhat.com>
2018-12-20 12:32:55 -0500

    testing: ikev2-x509-01-nss-debug supply empty secrets
    This avoids loading a raw RSA key unrelated to the test.

--- testing/pluto/ikev2-x509-01-nss-debug/final.sh
+++ testing/pluto/ikev2-x509-01-nss-debug/final.sh
@@ -3,6 +3,7 @@
 ipsec auto --status
 ipsec whack --shutdown
 : ==== tuc ====
+grep Totals /tmp/nss.log
 cat /tmp/nss.log | grep C_ | sort -n -r -k 5 
 echo "for nspr logs, look at the verbose console log in OUTPUT"
 : ==== cut ====
