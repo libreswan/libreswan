e03853f86d6deb1078a59515329ed7cbf136cfad
Paul Wouters <pwouters@redhat.com>
2018-12-26 22:28:10 -0500

    Revert "testing: add ip-xfrm.sed to default sanitizer list. Remove now obsolete test-params.sh files"
    This reverts commit d23faa28e19ebe6fdffe4435e4a815b64e4bd99b.
    
    The sanitizer is kinda awful and destroys everything :/

--- testing/pluto/interop-ikev2-strongswan-38-mobike-pool/testparams.sh
+++ testing/pluto/interop-ikev2-strongswan-38-mobike-pool/testparams.sh
@@ -2,3 +2,5 @@
 
 . ../../default-testparams.sh
 ROAD_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS strongswan.sed"
+ROAD_CONSOLE_FIXUPS="$ROAD_CONSOLE_FIXUPS ip-xfrm.sed"
+EAST_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ip-xfrm.sed"
--- testing/pluto/netkey-vti-09/testparams.sh
+++ testing/pluto/netkey-vti-09/testparams.sh
@@ -2,4 +2,3 @@
 
 . ../../default-testparams.sh
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ip-xfrm.sed"
-NORTH_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS strongswan.sed"
