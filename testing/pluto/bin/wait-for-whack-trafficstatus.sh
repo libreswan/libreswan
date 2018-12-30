7d4ffb7a83689f7c2d6e88e26f3f213cb53dd0c9
Andrew Cagney <cagney@gnu.org>
2018-12-30 16:35:50 -0500

    testing: fix wait-for-whack-trafficstatus.sh --timeout

--- testing/pluto/bin/wait-for-whack-trafficstatus.sh
+++ testing/pluto/bin/wait-for-whack-trafficstatus.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
+timeout=30
+
 if test $# -eq 0; then
     cat <<EOF 1>&2
+
 Usage: $0 [--timeout N] <pattern>
 
 Repeatedly grep the output of:
+
 	   whack --trafficstatus
-until it matches <pattern>.
+
+until it matches <pattern> or until the approximate timeout (default
+${timeout}) expires.
+
 EOF
     exit 1
 fi
+
 
 if test "$1" = "--timeout"; then
     shift ; timeout=$1 ; shift
 fi
 
-timeout=30
 count=0
 while true ; do
     if ipsec whack --trafficstatus | grep "$@" ; then
@@ -23,6 +30,7 @@ while true ; do
     fi
     count=$(expr ${count} + 1)
     if test ${count} -ge ${timeout} ; then
+	echo timeout after ${timeout} seconds
 	exit 1
     fi
     sleep 1
