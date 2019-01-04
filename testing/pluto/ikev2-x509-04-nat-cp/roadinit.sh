95f21a5533dcbaf84662b9b5d20ab618456ab592
Paul Wouters <pwouters@redhat.com>
2019-01-04 17:08:54 -0500

    WIP: test with roadwarrior, NAT, CP and certificates

---
+++ testing/pluto/ikev2-x509-04-nat-cp/roadinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep --x509
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec whack --impair suppress-retransmits
+ipsec auto --add rw
+echo "initdone"
---
+++ testing/pluto/ikev2-x509-04-nat-cp/roadrun.sh
@@ -0,0 +1,9 @@
+ipsec auto --up rw
+# ipsec will configure 100.64.0.x on eth0
+ip addr show  dev eth0
+ping -n -c 2 192.0.2.254
+ipsec whack --trafficstatus
+ipsec auto --down rw
+#check if the address is removed
+ip addr show  dev eth0
+echo done
