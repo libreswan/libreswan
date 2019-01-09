f98bb41e658a5c516163b7910df43dc06bf34fbe
Andrew Cagney <cagney@gnu.org>
2019-01-09 09:58:13 -0500

    testing: update enumcheck-01

--- testing/pluto/enumcheck-01/west.sh
+++ testing/pluto/enumcheck-01/west.sh
@@ -1,2 +1,11 @@
 /testing/guestbin/swan-prep
-/usr/local/libexec/ipsec/enumcheck | diff -u ../../enumcheck/OUTPUT.enumcheck.txt -
+
+# Use CMP not DIFF.  When there's a difference, the output from diff
+# mixed in with the output from comparing console.txt files looks too
+# much like console.txt needs updating when it doesn't.
+
+# To update OUTPUT.enumcheck.txt run something like:
+# $ ./OBJ.linux.x86_64/testing/enumcheck/enumcheck > testing/enumcheck/OUTPUT.enumcheck.txt
+# $ git diff
+
+/usr/local/libexec/ipsec/enumcheck | cmp ../../enumcheck/OUTPUT.enumcheck.txt - || echo "Does the file OUTPUT.enumcheck.txt need updating?"
