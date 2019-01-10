89a4cc268c9fa8d5609e9c606382d5a3cf304f73
Paul Wouters <pwouters@redhat.com>
2019-01-10 00:35:45 -0500

    testing: add ikev2-x509-namespaces-01 with 100 plutos on road

--- testing/pluto/ikev2-x509-namespaces-01/final.sh
+++ testing/pluto/ikev2-x509-namespaces-01/final.sh
@@ -1,5 +1,4 @@
-: ==== cut ====
-: ==== tuc ====
+hostname | grep east > /dev/null && ipsec whack --trafficstatus
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
 : ==== end ====
--- testing/pluto/ikev2-x509-namespaces-01/roadrun.sh
+++ testing/pluto/ikev2-x509-namespaces-01/roadrun.sh
@@ -1,7 +1,6 @@
-#ipsec auto --up rw
-#for i in `seq 1 10`; do ip netnet exec $name$i ipsec whack --asynchronous --rundir /tmp/run$i --name user$i  --initiate; done
-#sleep 20
-#
-# to get a root inside the namespace, run this on road:
-# ip netns exec $space1 bash
+num=99
+# the --asynchronous causes all output to be invisible here, but you can always check /tmp/pluto*log on road after the tets
+for i in `seq 1 $num`; do ip netns exec space$i ipsec whack --asynchronous --rundir /tmp/run$i --name user$i  --initiate; done
+# to get a root inside the namespace, like space5, run this on road:
+# ip netns exec space5 bash
 echo done
