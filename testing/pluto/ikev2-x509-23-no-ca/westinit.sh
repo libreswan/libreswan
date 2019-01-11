c91907204b7ecf25674aff92ef8d12e43c988829
Paul Wouters <pwouters@redhat.com>
2019-01-11 11:53:01 -0500

    testing: added ikev2-x509-23-no-ca

---
+++ testing/pluto/ikev2-x509-23-no-ca/westinit.sh
@@ -0,0 +1,14 @@
+/testing/guestbin/swan-prep --x509
+certutil -D -n east -d sql:/etc/ipsec.d
+# confirm that the network is alive
+../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
+# ensure that clear text does not get through
+iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
+iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+# confirm clear text does not get through
+../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec whack --impair suppress-retransmits
+ipsec auto --add ikev2-westnet-eastnet-x509-cr
+echo "initdone"
