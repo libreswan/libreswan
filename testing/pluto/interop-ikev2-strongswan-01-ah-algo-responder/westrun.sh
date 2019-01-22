05c122a14cd0a5ce8eec9695c413c47d0dbd3e08
Paul Wouters <pwouters@redhat.com>
2019-01-21 22:39:37 -0500

    testing: interop-ikev2-strongswan-01-ah-algo-responder add AH rekey test

--- testing/pluto/interop-ikev2-strongswan-01-ah-algo-responder/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-01-ah-algo-responder/westrun.sh
@@ -1,4 +1,14 @@
 ../bin/libreswan-up-down.sh ah=md5 -I 192.0.1.254 192.0.2.254
 ../bin/libreswan-up-down.sh ah=sha1 -I 192.0.1.254 192.0.2.254
-
+# Test rekey
+ipsec auto --add ah=sha1
+ipsec auto --up ah=sha1
+ping -c2 -I 192.0.1.254 192.0.2.254
+sleep 1
+ipsec auto --up ah=sha1
+sleep 1
+ping -c2 -I 192.0.1.254 192.0.2.254
+# since bofh AH tunnels are still there, check if they all got traffic, meaning new ones was used
+# use weird spacing to avoid sanitizer
+ip xfrm     s | grep anti-replay
 echo done
