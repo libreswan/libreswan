f6817d27e2f70c694009b112c1282ce2b1d6e176
Paul Wouters <pwouters@redhat.com>
2019-01-22 21:34:31 -0500

    pluto: ikev2_parent_inR1outI2_tail() should event_schedule() a EVENT_SA_REPLACE, not EVENT_SA_EXPIRE

--- programs/pluto/ikev2_parent.c
+++ programs/pluto/ikev2_parent.c
@@ -2209,7 +2209,7 @@ static stf_status ikev2_parent_inR1outI2_tail(struct state *pst, struct msg_dige
 
 	pexpect(md->svm->timeout_event == EVENT_RETRANSMIT); /* for CST */
 	delete_event(pst);
-	event_schedule(EVENT_SA_EXPIRE, deltatime(PLUTO_HALFOPEN_SA_LIFE), pst);
+	event_schedule(EVENT_SA_REPLACE, deltatime(PLUTO_HALFOPEN_SA_LIFE), pst);
 	change_state(pst, STATE_PARENT_I2);
 
 	/*
