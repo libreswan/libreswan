dd4f2396d975c774a1697684eb6c57ed170ab7c0
Paul Wouters <pwouters@redhat.com>
2019-01-23 14:54:33 -0500

    testing: remove interop-ikev2-strongswan-04-x509-responder-certreq
    As a responder, you must always send CERTREQ if you want initiator
    to send a CERT.

--- testing/pluto/interop-ikev2-strongswan-04-x509-responder-certreq/eastinit.sh
+++
@@ -1,3 +0,0 @@
-/testing/guestbin/swan-prep --userland strongswan --x509
-../../pluto/bin/strongswan-start.sh
-echo "initdone"
--- testing/pluto/interop-ikev2-strongswan-04-x509-responder-certreq/westrun.sh
+++
@@ -1,4 +0,0 @@
-ipsec auto --up  westnet-eastnet-ikev2
-ping -n -c4 -I 192.0.1.254 192.0.2.254
-ipsec whack --trafficstatus
-echo done
