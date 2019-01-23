b1fa96d13bea27f481d06eebaeacb3ef47162f13
Paul Wouters <pwouters@redhat.com>
2019-01-22 21:38:39 -0500

    testing: ikev2-26-keyingtries now passes
    The test now confirms a new keying try is performed when we time out in
    receiving IKE_AUTH answer.

--- testing/pluto/ikev2-26-keyingtries/westrun.sh
+++ testing/pluto/ikev2-26-keyingtries/westrun.sh
@@ -1,5 +1,9 @@
-# this should fail, release whack socket but a state should be trying in the background
-ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
-sleep 10
-ipsec status |grep STATE_ > /dev/null || echo "test failed, all states went away"
+# this should fail, our testing cannot wait for whack to release, so use async
+ipsec auto --up --asynchronous westnet-eastnet-ipv4-psk-ikev2
+sleep 30
+sleep 30
+sleep 30
+# the state should have been deleted, but replaced via EVENT_SA_REPLACE with a new state trying
+# if no STATE_s show up, this test failed
+ipsec status |grep STATE_  || echo "test failed, all states went away"
 echo done
