8a01d8eea47a53c613ed29e3b2e28399d8c6d1f6
Paul Wouters <pwouters@redhat.com>
2019-01-23 22:33:53 -0500

    testing: fixup newoe-27-replace-sa-authnull and newoe-27-replace-sa
    the test cases were swapped with their name (eg authnull was authenticated
    and the other one was authnull)

--- testing/pluto/newoe-27-replace-sa-authnull/eastinit.sh
+++ testing/pluto/newoe-27-replace-sa-authnull/eastinit.sh
@@ -3,7 +3,6 @@ cp policies/* /etc/ipsec.d/policies/
 echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/private-or-clear
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add authenticated
 # give OE policies time to load
 sleep 5
 echo "initdone"
