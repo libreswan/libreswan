5e4a06fc9b859ac61065887a52d460b66a858227
Paul Wouters <pwouters@redhat.com>
2019-01-25 13:19:26 -0500

    testing: updated testing/pluto/ikev2-algo-16-newer-dh for new defaults
    Since we do not have a DH group that we support but do not have in the default
    proposal set, this test cannot test for INVALID_KE anymore. But we have other
    tests that do for non-default configurations.

--- testing/pluto/ikev2-algo-16-newer-dh/westrun.sh
+++ testing/pluto/ikev2-algo-16-newer-dh/westrun.sh
@@ -5,6 +5,7 @@ ipsec auto --add westnet-eastnet-ikev2-modp3072
 ipsec auto --add westnet-eastnet-ikev2-modp4096
 ipsec auto --add westnet-eastnet-ikev2-modp8192
 ipsec auto --add westnet-eastnet-ikev2-dh19
+ipsec auto --add westnet-eastnet-ikev2-dh31
 ipsec auto --add westnet-eastnet-ikev2-dh20-fallback
 ipsec auto --up westnet-eastnet-ikev2-modp2048
 ipsec auto --delete westnet-eastnet-ikev2-modp2048
@@ -16,10 +17,14 @@ ipsec auto --up westnet-eastnet-ikev2-modp8192
 ipsec auto --delete westnet-eastnet-ikev2-modp8192
 ipsec auto --up westnet-eastnet-ikev2-dh19
 ipsec auto --delete westnet-eastnet-ikev2-dh19
+ipsec auto --up westnet-eastnet-ikev2-dh31
+ipsec auto --delete westnet-eastnet-ikev2-dh31
+# NOTE: we cannot do this test anymore as we support everything now in the default proposal list
 # Next one should work after INVALID_KE suggestion by east to change dh20 to modp2048
-ipsec auto --up westnet-eastnet-ikev2-dh20-fallback
-ipsec auto --delete westnet-eastnet-ikev2-dh20-fallback
-# the last one is no longer in the default list and should fail
+# ipsec auto --up westnet-eastnet-ikev2-dh20-fallback
+# ipsec auto --delete westnet-eastnet-ikev2-dh20-fallback
+#
+# MODP1536 is no longer in the default list and should fail
 ipsec whack --impair delete-on-retransmit
 ipsec auto --up westnet-eastnet-ikev2-modp1536
 echo done
