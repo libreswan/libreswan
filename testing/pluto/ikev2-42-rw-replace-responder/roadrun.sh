9b0bab184184ef21300c3188b46ec6eabf88d597
Andrew Cagney <cagney@gnu.org>
2019-02-02 15:11:14 -0500

    testing: in ikev2-42-rw-replace-responder only grep for debug lines with a state

--- testing/pluto/ikev2-42-rw-replace-responder/roadrun.sh
+++ testing/pluto/ikev2-42-rw-replace-responder/roadrun.sh
@@ -6,6 +6,6 @@ ipsec status
 sleep 60
 sleep 60
 ping -q -n -c 4 -I 192.0.2.100 192.1.2.23
-grep -E  'EVENT_SA_EXPIRE|EVENT_SA_REPLACE' OUTPUT/road.pluto.log | grep -v pe@ | head -9
+grep -E  'EVENT_SA_EXPIRE|EVENT_SA_REPLACE' OUTPUT/road.pluto.log | grep -e '#[0-9]' | grep -v pe@
 ipsec status
 echo done
