43e8b886cc97c6002478483fe58f3b07173cc38e
Paul Wouters <pwouters@redhat.com>
2019-02-21 18:06:55 -0500

    testing: WIP added certoe-13-poc-del-slash24 and certoe-14-poc-del-slash32
    The first shows a /32 in a /24 re-establishment. It is almost right,
    but for some reason delays showing OE state on road.
    
    The seconds shows a /32 in a /32 re-establishment. It shows a bug in
    where the groupinstance is /32 and the OE tunnel itself (also /32)
    is removed, no groupinstance policy is re-instated.

---
+++ testing/pluto/certoe-13-poc-del-slash24/5-road-redo.sh
@@ -0,0 +1,17 @@
+# confirm received delete was processed - should show no tunnel
+ipsec whack --trafficstatus
+# try triggering again, ondemand policy should re-trigger OE
+ip xfrm state
+ip xfrm pol
+# we use a different trigger so we do not hit original left over larval state
+hping -c 1 --udp  192.1.2.23
+# wait on OE to re-establish IPsec SA
+sleep 5
+ip xfrm state
+ip xfrm pol
+# should show established tunnel and no bare shunts
+ipsec whack --trafficstatus
+ipsec whack --shuntstatus
+# ping should succeed through tunnel
+ping -n -c 2 -I 192.1.3.209 192.1.2.23
+ipsec whack --trafficstatus
---
+++ testing/pluto/certoe-14-poc-del-slash32/4-east-del.sh
@@ -0,0 +1,9 @@
+# confirm tunnel is up
+ipsec whack --trafficstatus
+# east sends a delete by restarting
+ipsec restart
+# give OE conns time to load
+sleep 5
+# should be empty
+ipsec status |grep STATE_
+echo waiting on road to re-initiate
