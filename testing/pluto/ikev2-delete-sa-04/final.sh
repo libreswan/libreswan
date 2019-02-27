de83b498ab690e349bfd706c50624c238587e0e7
Paul Wouters <pwouters@redhat.com>
2019-02-26 23:59:53 -0500

    testing: WIP update ikev2-delete-sa-04
    This requires a not-yet-pushed patch to pass

--- testing/pluto/ikev2-delete-sa-04/final.sh
+++ testing/pluto/ikev2-delete-sa-04/final.sh
@@ -1,10 +1,7 @@
-# wait over one minute to ensure no EVENT_SA_REPLACE lingered and started something
-sleep 45
-sleep 30
 # There should be no IKE SA and no IPsec SA
 ipsec whack --trafficstatus
 # east howvever, should be attempting to connect to west because it has auto=start
-ipsec status |grep EVENT_RETRANSMIT | sed "s/EVENT_RETRANSMIT in .*$/EVENT_RETRANSMIT in .../"
+ipsec status |grep STATE_
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
