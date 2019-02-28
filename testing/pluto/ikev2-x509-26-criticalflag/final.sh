bc4f8fffd73efde153ace49af47db3b36c70e145
D. Hugh Redelmeier <hugh@mimosa.com>
2019-02-28 17:17:04 -0500

    pluto/ikev2-x509-26-criticalflag: fix reference logs
    - remove spurious trailing whitespace on routing tables
    
    - reflect some changes in logging (STATE_* removed?)
    
    - reflect change in logging by verify_end_cert

--- testing/pluto/ikev2-x509-26-criticalflag/final.sh
+++ testing/pluto/ikev2-x509-26-criticalflag/final.sh
@@ -1,6 +1,6 @@
 ../../pluto/bin/ipsec-look.sh
 # on east, it should show it failed the NSS IPsec profile and used the NSS TLS profile
-hostname | grep east > /dev/null && grep "retrying verification" /tmp/pluto.log
+hostname | grep east > /dev/null && grep "verify_end_cert trying profile" /tmp/pluto.log
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
