5f4278268d04579c872adf211032047859aefc0e
Andrew Cagney <cagney@gnu.org>
2019-03-02 14:12:06 -0500

    testing: WIP: play with IKEv1's HASH playload

---
+++ testing/pluto/impair-09-protected-ikev1/payloads.sed
@@ -0,0 +1,42 @@
+/IMPAIR:/ p
+
+# mark start of some packets
+/parse ISAKMP Message:/,/length:/ {
+	/next payload type: ISAKMP_NEXT_D/ {
+		i\
+-- start message (D)
+       	     p
+	}
+	/next payload type: ISAKMP_NEXT_HASH/ {
+		i\
+-- start message (HASH)
+       	     p
+	}
+	/next payload type: ISAKMP_NEXT_ID/ {
+		i\
+-- start message (ID)
+       	     p
+	}
+}
+
+# payloads that need authentication
+
+/parse ISAKMP Delete Payload:/,/number of SPIs/ p
+/parse ISAKMP Hash Payload:/,/length:/ p
+/parse ISAKMP Signature Payload:/,/length:/ p
+
+# authentication message
+
+# G;p appends the empty-hold-space + NL to pattern space; and then
+# prints it
+
+/^[^|].* Authenticated using RSA/ {G;p}
+/received .* message HASH[^ ]* data / {G;p}
+/received .* message SIG[^ ]* data / {G;p}
+/^[^|].* message for STATE_INFO_PROTECTED is missing payloads ISAKMP_NEXT_HASH/ {G;p}
+/received Hash Payload does not match computed value/ {G;p}
+/Informational Exchange is for an unknown/ {
+	s/MSGID:0x.*/MSGID:0x..../
+	G
+	p
+}
---
+++ testing/pluto/impair-09-protected-ikev1/westrun.sh
@@ -0,0 +1,67 @@
+# Proper test connection, should work
+ipsec whack --impair none
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+
+# Quick:
+ipsec whack --impair none
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
+ipsec whack --impair v1-hash-exchange:quick
+# HASH payload omited - should fail
+ipsec whack --impair v1-hash-payload:omit
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload empty - should fail
+ipsec whack --impair v1-hash-payload:empty
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload badly calculated - should fail
+ipsec whack --impair v1-hash-payload:0
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+echo done
+
+# Delete
+ipsec whack --impair none
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
+ipsec whack --impair v1-hash-exchange:delete
+# HASH payload omited - should fail
+ipsec whack --impair v1-hash-payload:omit
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload empty - should fail
+ipsec whack --impair v1-hash-payload:empty
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload badly calculated - should fail
+ipsec whack --impair v1-hash-payload:0
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+echo done
+
+# XAUTH:
+ipsec whack --impair none
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
+ipsec whack --impair v1-hash-exchange:xauth
+# HASH payload omited - should fail
+ipsec whack --impair v1-hash-payload:omit
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload empty - should fail
+ipsec whack --impair v1-hash-payload:empty
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload badly calculated - should fail
+ipsec whack --impair v1-hash-payload:0
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+echo done
+
+# INFO
+ipsec whack --impair none
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
+ipsec whack --impair v1-hash-exchange:notification
+# HASH payload omited - should fail
+ipsec whack --impair v1-hash-payload:omit
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload empty - should fail
+ipsec whack --impair v1-hash-payload:empty
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+# HASH payload badly calculated - should fail
+ipsec whack --impair v1-hash-payload:0
+../bin/libreswan-up-down.sh  westnet-eastnet -I 192.0.1.254 192.0.2.254
+echo done
