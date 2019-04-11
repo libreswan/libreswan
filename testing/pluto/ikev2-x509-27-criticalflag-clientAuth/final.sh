fa2bc8188789ef79fa4c8974715380cc0a78c7e1
Paul Wouters <pwouters@redhat.com>
2019-04-11 12:11:41 +0200

    testing: added ikev2-x509-27-criticalflag-clientAuth

---
+++ testing/pluto/ikev2-x509-27-criticalflag-clientAuth/final.sh
@@ -0,0 +1,9 @@
+../../pluto/bin/ipsec-look.sh
+# on east, it should show it failed the NSS IPsec profile and used the NSS TLS profile
+hostname | grep east > /dev/null && grep "verify_end_cert trying profile" /tmp/pluto.log
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
