d60cc6ab87ca279b7ffcf2e86386929b12749190
Andrew Cagney <cagney@gnu.org>
2019-04-23 10:17:46 -0400

    testing: check for core dump in libipsecconf-02; merge scripts

--- testing/pluto/libipsecconf-02/west.sh
+++ testing/pluto/libipsecconf-02/west.sh
@@ -1,9 +1,13 @@
+/testing/guestbin/swan-prep
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
-# These should load 
+
+# These should load
 ipsec auto --add base
 ipsec auto --add subnet4
 ipsec auto --add subnet6-good
+
 # this one should fail to load
 ipsec auto --add subnet6-bad
-echo done
+
+../bin/check-for-core.sh
