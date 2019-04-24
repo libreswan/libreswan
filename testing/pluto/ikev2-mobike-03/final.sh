cfc12b4fd1455d1c482106f5c2965ffb5f498b87
Paul Wouters <pwouters@redhat.com>
2019-04-24 19:53:36 +0200

    testing: ikev2-mobike-0[34]  restore road's real IP address.
    Due to a bug somewhere in the test harnass, road sometimes is not
    properly restarted to get back its proper IP address. This is a
    workaround to restore its IP address after these two test cases
    change it for the MOBIKE testing.

--- testing/pluto/ikev2-mobike-03/final.sh
+++ testing/pluto/ikev2-mobike-03/final.sh
@@ -2,6 +2,9 @@ ipsec whack --trafficstatus
 ../../pluto/bin/ipsec-look.sh
 : ==== cut ====
 ipsec auto --status
+: ==== restore IPs to prevent leaking into other tests that dont reboot first ===
+ip addr show dev eth0 | grep 192.1.33.222 && ip addr del 192.1.33.222/24 dev eth0
+ip addr show dev eth0 | grep 192.1.3.209 || ip addr add 192.1.3.209/24 dev eth0
 : ==== tuc ====
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
