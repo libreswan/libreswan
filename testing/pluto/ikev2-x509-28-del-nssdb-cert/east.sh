9f0cedb14a64c95109d87cf9a11f35f6beb866d9
Andrew Cagney <cagney@gnu.org>
2019-04-26 09:42:54 -0400

    testing: try yanking a certificate from under pluto's feet
    See bug 339.

---
+++ testing/pluto/ikev2-x509-28-del-nssdb-cert/east.sh
@@ -0,0 +1,27 @@
+/testing/guestbin/swan-prep --x509
+
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+
+# list certs in NSS DB
+ipsec whack --listcerts | grep east
+
+# add / remove 'test'
+ipsec auto --add test
+ipsec auto --delete test
+
+# delete certificate east
+certutil -d sql:/etc/ipsec.d -D -n east
+# whack should not show certificate
+ipsec whack --listcerts | grep east
+
+# try a load; should fail
+ipsec auto --add test
+ipsec auto --delete test
+
+# put east back
+certutil -A -i ../../x509/certs/east.crt -d sql:/etc/ipsec.d -n east -t "P,,"
+
+# re-load should not dump core
+ipsec auto --add test
+ipsec auto --delete test
