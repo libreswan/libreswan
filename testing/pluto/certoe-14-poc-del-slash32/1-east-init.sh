96b61cac205ea6b7a1e7edccc3891afd10708b7b
Paul Wouters <pwouters@redhat.com>
2019-04-30 15:50:36 -0400

    testing: fixup certoe-14-poc-del-slash32
    This fixes the routing so that packets reach road and east, over the
    non-default route. They both need explicit subnet routes for that to
    work, and their IP hardcoded in their configs.

--- testing/pluto/certoe-14-poc-del-slash32/1-east-init.sh
+++ testing/pluto/certoe-14-poc-del-slash32/1-east-init.sh
@@ -1,13 +1,13 @@
 /testing/guestbin/swan-prep  --x509
 ip route del default
 ip route add default via 192.9.2.254
+ip route add 192.1.3.0/24 via 192.1.2.254
 certutil -D -n road -d sql:/etc/ipsec.d
 certutil -D -n east -d sql:/etc/ipsec.d
 cp east-ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
 cp policies/* /etc/ipsec.d/policies/
-#echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/clear-or-private
-#echo "192.1.3.209/32"  >> /etc/ipsec.d/policies/clear-or-private
 echo "192.1.3.209/32"  >> /etc/ipsec.d/policies/private
+restorecon -R /etc/ipsec.d
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits
