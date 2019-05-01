d5f43ae9e5ead8adb1ae7e736fefa0cb65bc1c9e
Paul Wouters <pwouters@redhat.com>
2019-04-30 21:45:44 -0400

    testing: update to certoe-15-poc-east-west
    (still requires a fix for hitting a passert() which is being looked at)

--- testing/pluto/certoe-15-poc-east-west/1-east-init.sh
+++ testing/pluto/certoe-15-poc-east-west/1-east-init.sh
@@ -1,14 +1,11 @@
 /testing/guestbin/swan-prep  --x509
-setenforce 0
 ip route del default
-ip route add default via 192.9.4.1
+ip route add default via 192.9.2.1
 certutil -D -n west -d sql:/etc/ipsec.d
 cp east-ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
 cp policies/* /etc/ipsec.d/policies/
-#echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/clear-or-private
-#echo "192.1.3.209/32"  >> /etc/ipsec.d/policies/clear-or-private
-#echo "192.1.3.209/32"  >> /etc/ipsec.d/policies/private
 echo "192.1.2.45/32"  >> /etc/ipsec.d/policies/private
+restorecon -R /etc/ipsec.d
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits
