ac20f608a2138f99604aa6a925bd0f475eb92887
Paul Wouters <pwouters@redhat.com>
2019-05-06 12:36:32 -0400

    testing: xauth-pluto-20-pam re-add sleep to avoid revive issues

--- testing/pluto/xauth-pluto-20-pam/roadrun.sh
+++ testing/pluto/xauth-pluto-20-pam/roadrun.sh
@@ -1,6 +1,7 @@
 ipsec whack --impair suppress-retransmits
 ipsec whack --xauthname 'baduser' --xauthpass 'use1pass' --name xauth-road-eastnet --initiate
-#sleep 3
+# prevent revive race
+sleep 3
 ipsec whack --xauthname 'gooduser' --xauthpass 'use1pass' --name xauth-road-eastnet --initiate
 ping -n -c4 192.0.2.254
 ipsec trafficstatus
