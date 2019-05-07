6b13506ebb4cb3fbfbeaa15916588126b2b19e45
Andrew Cagney <cagney@gnu.org>
2019-05-07 12:41:47 -0400

    testing: strip out 'message arrived ...' lines when grubbing in pluto.log

--- testing/pluto/ikev2-impair-02-replay-forward/final.sh
+++ testing/pluto/ikev2-impair-02-replay-forward/final.sh
@@ -1,5 +1,5 @@
 ../../pluto/bin/ipsec-look.sh
-sed -n -e '/IMPAIR: start processing replay forward/,/IMPAIR: stop processing replay forward/ { /^[^|]/ p }' /tmp/pluto.log
+sed -n -e '/IMPAIR: start processing replay forward/,/IMPAIR: stop processing replay forward/ { /^[^|]/ p }' /tmp/pluto.log | grep -v 'message arrived'
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
