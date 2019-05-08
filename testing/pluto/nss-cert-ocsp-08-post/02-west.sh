da885bcd1653a92ecb5ad8227e93898c355212cb
Paul Wouters <pwouters@redhat.com>
2019-05-08 14:25:16 -0400

    testing: add ipsec whack --impair revival to ocsp failure test cases
    Prevents output changes depending on revival attempts

--- testing/pluto/nss-cert-ocsp-08-post/02-west.sh
+++ testing/pluto/nss-cert-ocsp-08-post/02-west.sh
@@ -3,6 +3,7 @@ certutil -d sql:/etc/ipsec.d -D -n east
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair delete-on-retransmit
+ipsec whack --impair revival
 ipsec auto --add nss-cert-ocsp
 ipsec auto --status |grep nss-cert-ocsp
 echo "initdone"
