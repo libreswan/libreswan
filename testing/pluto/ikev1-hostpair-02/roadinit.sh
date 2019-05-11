ac1e643e953fe661e9d06ea44db8252f7ef46510
Paul Wouters <pwouters@redhat.com>
2019-05-11 17:16:31 -0400

    testing: ikev1-hostpair-02 impair revival to match up state numbers

--- testing/pluto/ikev1-hostpair-02/roadinit.sh
+++ testing/pluto/ikev1-hostpair-02/roadinit.sh
@@ -2,5 +2,6 @@
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair delete-on-retransmit
+ipsec whack --impair revival
 ipsec auto --add westnet-eastnet-ipv4-psk-ikev1
 echo "initdone"
