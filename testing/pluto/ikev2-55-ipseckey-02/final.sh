e00d380a767887ba01a3e71f899bbc46efb33048
Paul Wouters <pwouters@redhat.com>
2019-05-17 14:50:49 -0400

    testing: re-enegineer ikev2-55-ipseckey-02
    Due to a bug, likely in the dns handling on east, this test failed.
    
    But this test's primary purpose was to test two keys in DNS where road
    can change key at any time and still work. So this test is redone to
    focus on the road test. This new test failure is copied as a new test
    case ikev2-55-ipseckey-09

--- testing/pluto/ikev2-55-ipseckey-02/final.sh
+++
@@ -1,9 +0,0 @@
-# east should have two public keys. including road fetched from dns
-ipsec auto --listpubkeys
-ipsec whack --trafficstatus
-: ==== cut ====
-ipsec auto --status
-: ==== tuc ====
-../bin/check-for-core.sh
-if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
-: ==== end ====
--- testing/pluto/ikev2-55-ipseckey-02/roadinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-ipsec whack --impair suppress-retransmits
-ipsec auto --add road-east-1
-echo "initdone"
