libreswan/libreswan

commit d745bf5656955abf0da6353cfe65759770746038

shell scripts: a few more improvements inspired by Coverity SHELLCHECK_WARNING
- replace `foo` with $(foo)

- elimintate "for dummy" construct

- eliminate some wc invocations

- much more to do

mk/kvm-targets.mk
@@ -423,7 +423,7 @@ $(KVM_KEYS_CLEAN_TARGETS):
 # For moment don't force keys to be re-built.
 .PHONY: kvm-keys-up-to-date
 kvm-keys-up-to-date:
-	@if test $$($(KVM_KEYS_EXPIRED) | wc -l) -gt 0 ; then \
+	@if [ -n $$($(KVM_KEYS_EXPIRED) ] ; then \
 		echo "The following keys are more than $(KVM_KEYS_EXPIRATION_DAY) days old:" ; \
 		$(KVM_KEYS_EXPIRED) | sed -e 's/^/  /' ; \
 		echo "run 'make kvm-keys-clean kvm-keys' to force an update" ; \

packaging/utils/patcher
@@ -30,7 +30,7 @@ umask 022
 
 verbose=
 modifier=patch
-for dummy
+while [ $# != 0 ]
 do
 	case "$1" in
 	-v)	verbose=yes		;;

programs/_realsetup.bsd/_realsetup.in
@@ -91,7 +91,7 @@ case "$1" in
 
 	# announcement
 	# (Warning, changes to this log message may affect barf.)
-	version="`ipsec --version | awk 'NR == 1 { print $(3) }' | sed -e 's/^U\(.*\)\/K(.*/\1/'`"
+	version="$(ipsec --version | awk 'NR == 1 { print $(3) }' | sed -e 's/^U\(.*\)\/K(.*/\1/')"
 	case "$1" in
 	start|--start)	perform echo "\"Starting Libreswan IPsec $version...\""	;;
 	_autostart)	perform echo "\"Restarting Libreswan IPsec $version...\""	;;
@@ -106,7 +106,7 @@ case "$1" in
 			echo "\"...unable to start Libreswan IPsec, no $f!\"" ";" \
 			exit 1 ";" \
 			"}"
-	    done
+	done
 
 	# the meaning of $$ at a different runtime is questionable!
 	perform echo '$$' ">" $lock
@@ -119,7 +119,7 @@ case "$1" in
 
 	# misc pre-Pluto setup
 
-	perform test -d `dirname $subsyslock` "&&" touch $subsyslock
+	perform test -d $(dirname $subsyslock) "&&" touch $subsyslock
 
 	# Pluto
 	case "$1" in
@@ -191,7 +191,7 @@ case "$1" in
 		lsmod 2>&1 | grep "^xfrm_user" > /dev/null && rmmod xfrm_user
 	fi
 
-	perform test -d `dirname $subsyslock` "&&" rm -f $subsyslock
+	perform test -d $(dirname $subsyslock) "&&" rm -f $subsyslock
 
 	perform rm -f $lock $plutopid
 	perform echo "...Libreswan IPsec stopped" "|" $LOGONLY
@@ -217,7 +217,7 @@ case "$1" in
 
 	if test -s $plutopid
 	then
-		if ps -p `cat $plutopid` >/dev/null
+		if ps -p $(cat $plutopid) >/dev/null
 		then
 			plutokind=normal
 		elif ps -C pluto >/dev/null
@@ -233,21 +233,21 @@ case "$1" in
 
 	if test -r /proc/net/ipsec_eroute
 	then
-		if test " `wc -l < /proc/net/ipsec_eroute 2> /dev/null `" -gt 0
+		if test " $(wc -l /proc/net/ipsec_eroute 2> /dev/null )" -gt 0
 		then
-			eroutes=`wc -l < /proc/net/ipsec_eroute 2> /dev/null | sed s/\ //g`
+			eroutes=$(wc -l /proc/net/ipsec_eroute 2> /dev/null | sed -e 's/ //g')
 		fi
 	else
-		eroutes=`ipsec eroute 2> /dev/null | wc -l | sed s/\ //g`
+		eroutes=$(ipsec eroute 2> /dev/null | wc -l | sed -e 's/\ //g')
 	fi
 
 	if test "$haslock"
 	then
-		echo "IPsec running"  " - pluto pid: `cat $plutopid`"
+		echo "IPsec running"  " - pluto pid: $(cat $plutopid)"
 		# might not be a subsystem lock dir, ignore that issue
 		if test "$plutokind" = "normal" -a \( "$klips" = "yes" -o "$lk26sec" = "yes" \)
 		then
-			echo "pluto pid `cat $plutopid`"
+			echo "pluto pid $(cat $plutopid)"
                         case "$eroutes" in
                         0)      echo "No tunnels up"    ;;
                         *)      echo "$eroutes tunnels up"      ;;
@@ -257,7 +257,7 @@ case "$1" in
 		echo "but..."
 		if test "$plutokind" != "normal"
 		then
-			echo "$plutokind Pluto running" " - pluto pid: `cat $plutopid`"
+			echo "$plutokind Pluto running" " - pluto pid: $(cat $plutopid)"
 		fi
 		case $klips in
 		maybe)	echo "KLIPS module is not loaded!"	;;

programs/_updown.netkey/_updown.netkey.in
@@ -446,7 +446,7 @@ delsource() {
         return ${st}
     fi
     # Remove source ip if it's not used any more.
-    if [ $(ip -o route list src ${PLUTO_MY_SOURCEIP} | wc -l) -eq 0 ]; then
+    if [ -z $(ip -o route list src ${PLUTO_MY_SOURCEIP}) ]; then
 	if [ -n "${VTI_IFACE}" -a "${VTI_ROUTING}" = yes ]; then
 	    interface="${VTI_IFACE}"
 	fi

programs/auto/auto.in
@@ -42,7 +42,7 @@ utc=
 verbose=
 remote=
 
-for dummy
+while [ $# != 0 ]
 do
     case "$1" in
 	--help)

programs/barf/barf.in
@@ -19,12 +19,13 @@ LC_ALL=C export LC_ALL
 LOGS=${LOGS-/var/log}
 me="ipsec barf"
 # Max lines to use for things like 'route -n'
+# ??? no longer used?
 maxlines=100
 
 # kludge to produce no barf output mentioning policygroups if none are present.
 # This will not catch ".file" policygroups.
 PREPOLICIES=@IPSEC_CONFDDIR@/policies
-if [ `ls $PREPOLICIES 2> /dev/null | wc -l` -ne 0 ]
+if [ -n $(ls $PREPOLICIES 2> /dev/null) ]
 then
 	POLICIES=$PREPOLICIES
 fi
@@ -71,7 +72,7 @@ findlog() {		# findlog string fallbackstring possiblefile ...
 		fi
 	done
 	# nope, resort to a search, newest first, of uncompressed logs
-	for f in `ls -t $LOGS | egrep -v lastlog | egrep -v tmp | egrep -v '^mail' | egrep -v '\.(gz|Z)$'`
+	for f in $(ls -t $LOGS | egrep -v lastlog | egrep -v tmp | egrep -v '^mail' | egrep -v '\.(gz|Z)$')
 	do
 		if test -f $LOGS/$f -a ! -d $LOGS/$f && egrep -q "$s" $LOGS/$f
 		then
@@ -82,7 +83,7 @@ findlog() {		# findlog string fallbackstring possiblefile ...
 			return 0
 		fi
 	done
-	for f in `ls -t $LOGS | egrep -v lastlog | egrep -v tmp | egrep -v '^mail' | egrep -v '\.(gz|Z)$'`
+	for f in $(ls -t $LOGS | egrep -v lastlog | egrep -v tmp | egrep -v '^mail' | egrep -v '\.(gz|Z)$')
 	do
 		if test -s $LOGS/$f -a -f $LOGS/$f && egrep -q "$t" $LOGS/$f
 		then
@@ -121,7 +122,7 @@ fi
 # /lib/modules examiner
 modulegoo() {
 	set +x
-	for d in `ls /lib/modules`
+	for d in $(ls /lib/modules)
 	do
 		if test -d /lib/modules/$d
 		then
@@ -244,7 +245,7 @@ _________________________ nss/crls
 crlutil -L -d sql:@IPSEC_NSSDIR@
 if [ $POLICIES ]
 then
-	for policy in $POLICIES/*; do base=`basename $policy`;
+	for policy in $POLICIES/*; do base=$(basename $policy);
 	   _________________________ ipsec/policies/$base
 	   cat $policy
 	done
@@ -292,7 +293,7 @@ then
 else
 	if test -r /proc/net/xfrm_stat
 	then
-		echo "NETKEY (`uname -r`) support detected "
+		echo "NETKEY ($(uname -r)) support detected "
 	else
 		echo "no KLIPS or NETKEY support detected"
 	fi
@@ -355,9 +356,9 @@ _________________________ usr/src/linux/.config
 if test -f /proc/config.gz
 then
 	zcat /proc/config.gz | egrep 'CONFIG_IPSEC|CONFIG_KLIPS|CONFIG_NET_KEY|CONFIG_INET|CONFIG_IP|CONFIG_HW_RANDOM|CONFIG_CRYPTO_DEV|_XFRM'
-elif test -f /lib/modules/`uname -r`/build/.config
+elif test -f /lib/modules/$(uname -r)/build/.config
 then
-	cat /lib/modules/`uname -r`/build/.config | egrep 'CONFIG_IPSEC|CONFIG_KLIPS|CONFIG_NET_KEY|CONFIG_INET|CONFIG_IP|CONFIG_HW_RANDOM|CONFIG_CRYPTO_DEV|_XFRM'
+	cat /lib/modules/$(uname -r)/build/.config | egrep 'CONFIG_IPSEC|CONFIG_KLIPS|CONFIG_NET_KEY|CONFIG_INET|CONFIG_IP|CONFIG_HW_RANDOM|CONFIG_CRYPTO_DEV|_XFRM'
 else
 	echo "no .config file found, cannot list kernel properties"
 fi

programs/newhostkey/newhostkey.in
@@ -29,7 +29,7 @@ seeddev="--seeddev /dev/random"
 output=
 nssdir="@IPSEC_NSSDIR@"
 password=
-for dummy; do
+while [ $# != 0 ] ; do
     case "$1" in
 	--bits)
 	    bits="${2}"

commit 7797d2cbed7844c43f08daa473fb68ae19f3ff28

testing: fix builds
Syntax error introduced by d745bf5656955abf0da6353cfe65759770746038.

mk/kvm-targets.mk
@@ -423,7 +423,7 @@ $(KVM_KEYS_CLEAN_TARGETS):
 # For moment don't force keys to be re-built.
 .PHONY: kvm-keys-up-to-date
 kvm-keys-up-to-date:
-	@if [ -n $$($(KVM_KEYS_EXPIRED) ] ; then \
+	@if test $$($(KVM_KEYS_EXPIRED) | wc -l) -gt 0 ; then \
 		echo "The following keys are more than $(KVM_KEYS_EXPIRATION_DAY) days old:" ; \
 		$(KVM_KEYS_EXPIRED) | sed -e 's/^/  /' ; \
 		echo "run 'make kvm-keys-clean kvm-keys' to force an update" ; \