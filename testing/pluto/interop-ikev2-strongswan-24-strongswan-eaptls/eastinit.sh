0802ee06e99e7005fba3d3c523dd343a683bd89d
Paul Wouters <pwouters@redhat.com>
2019-06-06 19:00:18 -0400

    testing: add interop-ikev2-strongswan-24-strongswan-eaptls

---
+++ testing/pluto/interop-ikev2-strongswan-24-strongswan-eaptls/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep --userland strongswan
+cp /testing/x509/keys/east.key /etc/strongswan/ipsec.d/private/
+cp /testing/x509/certs/east.crt /etc/strongswan/ipsec.d/certs/
+cp /testing/x509/cacerts/mainca.crt /etc/strongswan/ipsec.d/cacerts/
+../../pluto/bin/strongswan-start.sh
+echo "initdone"
