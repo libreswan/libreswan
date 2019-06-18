4724862e437c972c64f1ca24677dfb8e5f8d6979
Paul Wouters <pwouters@redhat.com>
2019-06-18 00:44:58 -0400

    testing: update linux-audit-01 to include IKE SA and IPsec SA failure test
    Output also slightly changed due to selinux message log changes.

--- testing/pluto/linux-audit-01/eastinit.sh
+++ testing/pluto/linux-audit-01/eastinit.sh
@@ -1,4 +1,6 @@
 /testing/guestbin/swan-prep
+setenforce 1
+echo ': PSK "test"' >> /etc/ipsec.secrets
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add ikev1
--- testing/pluto/linux-audit-01/westinit.sh
+++ testing/pluto/linux-audit-01/westinit.sh
@@ -1,8 +1,13 @@
 /testing/guestbin/swan-prep
+setenforce 1
+echo '@psk-west-v2 @psk-east-v2: PSK "ThisIsHereToMisMatch"' >> /etc/ipsec.secrets
+echo ': PSK "test"' >> /etc/ipsec.secrets
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add ikev1
 ipsec auto --add ikev1-aggr
 ipsec auto --add ikev2
-ipsec whack --impair suppress-retransmits
+ipsec auto --add ikev2-failtest
+ipsec auto --add ipsec-failtest
+ipsec whack --impair delete-on-retransmit
 echo "initdone"
