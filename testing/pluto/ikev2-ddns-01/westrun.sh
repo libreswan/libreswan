55f68c3271bab45c9d571774792d384ce906a496
Paul Wouters <pwouters@redhat.com>
2019-07-02 23:25:48 -0400

    testing: update ikev2-ddns-01 and ikev2-ddns-02
    These test cases depended on the bogus behaviour that DDNS updates
    would initiate a connection. The tests are modified to no longer
    expect this, for which they need to be run with auto=start now.
    
    ikev2-ddns-03 tests the auto=add case does not initiate.

--- testing/pluto/ikev2-ddns-01/westrun.sh
+++ testing/pluto/ikev2-ddns-01/westrun.sh
@@ -1,9 +1,9 @@
 sleep 5
+ipsec status |grep "===" # should show %dns for pending resolving
 echo "192.1.2.23 right.libreswan.org" >> /etc/hosts
-# wait for DDNS event
-# Oddly designed connection_check_ddns() will trigger --up
-sleep 30
-sleep 30
-sleep 30
+# trigger DDNS event (saves us from waiting)
+ipsec whack --ddns
+# give conn time to establish by itself
+sleep 3
 # tunnel should show up in final.sh
 echo done
--- testing/pluto/ikev2-ddns-02/westinit.sh
+++ testing/pluto/ikev2-ddns-02/westinit.sh
@@ -5,5 +5,4 @@ cp resolv.conf /etc
 # need to disable ipv6 and activate auto-interface
 cp west-unbound.conf /etc/unbound/unbound.conf
 # will throw an error about bad unresolvable name
-ipsec auto --add named
 echo "initdone"
