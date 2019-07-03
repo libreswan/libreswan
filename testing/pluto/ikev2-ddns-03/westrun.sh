38eed3e8a43d9156fa3b3d6053a41e4b13e608d0
Paul Wouters <pwouters@redhat.com>
2019-07-02 23:18:26 -0400

    testing: Added ikev2-ddns-03
    This test case confirms an auto=add connection will no longer
    spontaniously initiate based on a DDNS update.

---
+++ testing/pluto/ikev2-ddns-03/westrun.sh
@@ -0,0 +1,10 @@
+sleep 5
+echo "192.1.2.23 right.libreswan.org" >> /etc/hosts
+# trigger DDNS event (saves us from waiting)
+ipsec whack --ddns
+# there should be no states
+ipsec status |grep STATE_
+ipsec status | grep "===" # should no longer show %dns as resolving completed
+# confirm it all resolved by bringing the conn up manually
+ipsec auto --up named
+echo done
