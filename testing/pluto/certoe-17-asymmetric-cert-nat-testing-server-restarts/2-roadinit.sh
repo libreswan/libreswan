7a04b4a2ab2b9a80f8264d92787a689b62fff6fa
Rishabh04-02 <rishabh0402@gmail.com>
2019-07-26 04:43:37 -0500

    test cases reference updated
    test cases working with success

--- testing/pluto/certoe-17-asymmetric-cert-nat-testing-server-restarts/2-roadinit.sh
+++ testing/pluto/certoe-17-asymmetric-cert-nat-testing-server-restarts/2-roadinit.sh
@@ -3,7 +3,7 @@ certutil -D -n east -d sql:/etc/ipsec.d
 cp road-ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
 cp policies/* /etc/ipsec.d/policies/
 echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
-ipsec start
+ipsec restart
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits
 # give OE policies time to load
