2952fa5d7eb4795676bd5b40e139aeeab48e4434
Andrew Cagney <cagney@gnu.org>
2019-07-28 10:05:07 -0400

    interfaces: in starter_iface_find() clearly handle IPv6
    Instead of relying on the sin_addr and sin6_addr mapping
    to the same struct offset, explictly convert the sockaddr
    to an endpoint, and from the to an address.
    
    Should fix: ??? what should happen for IPv6?

--- lib/libipsecconf/interfaces.c
+++ lib/libipsecconf/interfaces.c
@@ -27,50 +27,68 @@
 
 #include "sysdep.h"
 #include "constants.h"
+#include "ip_endpoint.h"
+#include "ip_address.h"
 #include "socketwrapper.h"
 #include "libreswan/ipsec_tunnel.h"
-
+#include "libreswan/passert.h"
 #include "ipsecconf/interfaces.h"
 #include "ipsecconf/exec.h"
 #include "ipsecconf/starterlog.h"
+#include "lswlog.h"	/* for pexpect() */
 
 bool starter_iface_find(const char *iface, int af, ip_address *dst, ip_address *nh)
 {
+	/* XXX: danger REQ is recycled by ioctl() callss */
 	struct ifreq req;
-	struct sockaddr_in *sa = (struct sockaddr_in *)(&req.ifr_addr);
-	int sock;
 
 	if (iface == NULL)
-		return FALSE;	/* ??? can this ever happen? */
+		return false;	/* ??? can this ever happen? */
 
-	sock = safe_socket(af, SOCK_DGRAM, 0);
+	int sock = safe_socket(af, SOCK_DGRAM, 0);
 	if (sock < 0)
-		return FALSE;
+		return false;
 
 	fill_and_terminate(req.ifr_name, iface, IFNAMSIZ);
 
+	/* UP? */
 	if (ioctl(sock, SIOCGIFFLAGS, &req) != 0 ||
 	    (req.ifr_flags & IFF_UP) == 0x0) {
 		close(sock);
-		return FALSE;
+		return false;
 	}
 
+	/*
+	 * convert the sockaddr to an endpoint (ADDRESS:PORT, but
+	 * expect PORT==0)) and then extract just the address
+	 */
+
+	/* get NH */
 	if ((req.ifr_flags & IFF_POINTOPOINT) != 0x0 && nh != NULL &&
 	    (ioctl(sock, SIOCGIFDSTADDR, &req) == 0)) {
-		/* ??? what should happen for IPv6? */
-		if (sa->sin_family == af) {
-			initaddr((const void *)&sa->sin_addr,
-				 sizeof(struct in_addr), af, nh);
+		struct sockaddr *sa = &req.ifr_addr;
+		if (sa->sa_family == af) {
+			/* XXX: sizeof right? */
+			ip_endpoint nhe;
+			happy(sockaddr_as_endpoint(sa, sizeof(*sa), &nhe));
+			pexpect(endpoint_port(&nhe) == 0);
+			*nh = endpoint_address(&nhe);
 		}
 	}
+
+	/* get DST */
 	if (dst != NULL && ioctl(sock, SIOCGIFADDR, &req) == 0) {
-		/* ??? what should happen for IPv6? */
-		if (sa->sin_family == af) {
-			initaddr((const void *)&sa->sin_addr,
-				 sizeof(struct in_addr), af, dst);
+		struct sockaddr *sa = &req.ifr_addr;
+		if (sa->sa_family == af) {
+			/* XXX: sizeof right? */
+			ip_endpoint dste;
+			happy(sockaddr_as_endpoint(sa, sizeof(*sa), &dste));
+			pexpect(endpoint_port(&dste) == 0);
+			*dst = endpoint_address(&dste);
 		}
 	}
+
 	close(sock);
-	return TRUE;
+	return true;
 }
 
