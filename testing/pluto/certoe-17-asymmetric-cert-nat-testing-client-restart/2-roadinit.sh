58338e035fd216732684a8dbc7ebc0223892876c
Rishabh04-02 <rishabh0402@gmail.com>
2019-08-07 11:58:40 -0500

    testing: client restarts cleanly test case added

---
+++ testing/pluto/certoe-17-asymmetric-cert-nat-testing-client-restart/2-roadinit.sh
@@ -0,0 +1,11 @@
+/testing/guestbin/swan-prep --x509
+certutil -D -n east -d sql:/etc/ipsec.d
+cp road-ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
+cp policies/* /etc/ipsec.d/policies/
+echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec whack --impair suppress-retransmits
+# give OE policies time to load
+sleep 3
+echo "initdone"
---
+++ testing/pluto/certoe-17-asymmetric-cert-nat-testing-client-restart/4-road-ipsec-restart.sh
@@ -0,0 +1,6 @@
+# restarting ipsec
+ipsec restart
+# give OE conns time to load
+sleep 5
+# should be empty
+ipsec status |grep STATE_
