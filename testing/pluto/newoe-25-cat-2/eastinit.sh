5ccfe4c1522f3c1403931e216e50bad0f64e2380
Paul Wouters <pwouters@redhat.com>
2019-08-06 22:14:54 -0400

    testing: small misc update

--- testing/pluto/newoe-25-cat-2/eastinit.sh
+++ testing/pluto/newoe-25-cat-2/eastinit.sh
@@ -1,4 +1,7 @@
 /testing/guestbin/swan-prep
+# prevent stray DNS packets hitting OE - DNS not used on east in this test
+rm /etc/resolv.conf
+touch /etc/resolv.conf
 cp policies/* /etc/ipsec.d/policies/
 echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
 cp ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
