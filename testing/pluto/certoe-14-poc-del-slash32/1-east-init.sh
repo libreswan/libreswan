6aafe17fa0aaa6b13c1653b21338f1e72df6f7da
Paul Wouters <pwouters@redhat.com>
2019-09-01 18:49:01 -0400

    testing: fixup certoe-14-poc-del-slash32 for removed 192.9 network

--- testing/pluto/certoe-14-poc-del-slash32/1-east-init.sh
+++ testing/pluto/certoe-14-poc-del-slash32/1-east-init.sh
@@ -1,6 +1,8 @@
 /testing/guestbin/swan-prep  --x509
 ip route del default
-ip route add default via 192.9.2.254
+ip tuntap add mode tun tun0
+ifconfig tun0 10.13.13.13/24
+ip route add default via 10.13.13.1
 ip route add 192.1.3.0/24 via 192.1.2.254
 certutil -D -n road -d sql:/etc/ipsec.d
 certutil -D -n east -d sql:/etc/ipsec.d
