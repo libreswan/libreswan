c1ce36945755ce4687ed00fbf0898e6fee58a42e
Paul Wouters <pwouters@redhat.com>
2019-09-05 23:37:54 -0400

    testing: add/update tests for wildcard ID's
    This tests whether leftid/rightid=%any allows overriding the missing SAN
    and also confirms if not present to not allow a missing SAN.

---
+++ testing/pluto/ikev1-x509-19-id-notany-responder/westrun.sh
@@ -0,0 +1,3 @@
+# should fail
+ipsec auto --up ikev2-westnet-eastnet-x509-cr
+echo "done"
---
+++ testing/pluto/ikev2-x509-33-id-notany/westinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep --x509
+certutil -D -n east -d sql:/etc/ipsec.d
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add ikev2-westnet-eastnet-x509-cr
+ipsec status | grep idtype
+ipsec whack --impair suppress-retransmits
+echo "initdone"
