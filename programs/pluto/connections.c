a8559eac47ca9d0ac7c95ba8bd207817df985eb3
Andrew Cagney <cagney@gnu.org>
2019-09-11 11:29:48 -0400

    ikev1: simplify "responding to Main Mode from unknown peer..." log lines
    Two log lines generate the message:
    
    - "responding to Main Mode from unknown peer THAT.HOST_ADDR:THAT.HOST_PORT"
    
      this would be logged if INSTANCE && HOST_PORT_SPECIFIC, but, based
      on the tests, this doesn't happen (the only test to even set
      HOST_PORT_SPECIFIC is ikev2-51-rw-nat-ikeport, and not with
      INSTANCE).
    
    - "responding to Main Mode from unknown peer THAT.HOST_ADDR on port MD.SENDER.PORT"
    
      this does happen, but why use THAT.HOST_ADDR to print the unknown
      peer when MD.SENDER has the value (I'm guessing history, I'm pretty
      sure the two are identical).
    
    The new log line looks like:
    
      "responding to Main Mode from unknown peer MD.SENDER"
    
    where MD.SENDER is correctly formatted using str_sanitize_endpoint().
    
    (when !INSTANCE, the even shorter message "resonding to Main Mode" is
    logged, wonder why that doesn't include "from peer MD.SENDER").
    
    Also; always debug-log the cookie, and debug-log HOST_PORT_SPECIFIC.

--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -490,9 +490,14 @@ size_t format_end(char *buf,
 	}
 
 	host_port[0] = '\0';
-	if (this->host_port_specific)
+	if (this->host_port_specific) {
+		endpoint_buf b;
+		dbg("in %s() this %s port %d is host_port_specific",
+		    __func__, str_endpoint(&this->host_addr, &b),
+		    this->host_port);
 		snprintf(host_port, sizeof(host_port), ":%u",
 			 this->host_port);
+	}
 
 	/* payload portocol and port */
 	protoport[0] = '\0';
@@ -776,6 +781,10 @@ static int extract_end(struct end *dst, const struct whack_end *src,
 	if (src->host_port != pluto_port) {
 		dst->host_port = src->host_port;
 		dst->host_port_specific = TRUE;
+		endpoint_buf b;
+		dbg("in %s() end %s port %d is host_port_specific",
+		    __func__, str_endpoint(&dst->host_addr, &b),
+		    dst->host_port);
 	}
 
 	dst->sendcert =  src->sendcert;
