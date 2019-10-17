7901e74cd622cd4b50e478b9393a9e81db965512
Andrew Cagney <cagney@gnu.org>
2019-10-17 15:22:10 +0200

    fragments: merge should_fragment_v2_ike_msg() into v2_record_outbound_fragments()
    simplify - ikev2 always fragments

--- programs/pluto/ikev2_message.c
+++ programs/pluto/ikev2_message.c
@@ -1022,34 +1022,6 @@ static stf_status v2_record_outbound_fragments(struct state *st,
 	return STF_OK;
 }
 
-static bool should_fragment_v2_ike_msg(struct state *st, size_t len, bool resending)
-{
-	if (st->st_interface != NULL && st->st_interface->ike_float)
-		len += NON_ESP_MARKER_SIZE;
-
-	/* This condition is complex.  Formatting is meant to help reader.
-	 *
-	 * Hugh thinks his banished style would make this earlier version
-	 * a little clearer:
-	 * len + natt_bonus
-	 *    >= (st->st_connection->addr_family == AF_INET
-	 *       ? ISAKMP_FRAG_MAXLEN_IPv4 : ISAKMP_FRAG_MAXLEN_IPv6)
-	 * && ((  resending
-	 *        && (st->st_connection->policy & POLICY_IKE_FRAG_ALLOW)
-	 *        && st->st_seen_fragvid)
-	 *     || (st->st_connection->policy & POLICY_IKE_FRAG_FORCE)
-	 *     || st->st_seen_fragments))
-	 *
-	 * ??? the following test does not account for natt_bonus
-	 */
-	return len >= endpoint_type(&st->st_remote_endpoint)->ikev1_max_fragment_size &&
-	    (   (resending &&
-			(st->st_connection->policy & POLICY_IKE_FRAG_ALLOW) &&
-			st->st_seen_fragvid) ||
-		(st->st_connection->policy & POLICY_IKE_FRAG_FORCE) ||
-		st->st_seen_fragments   );
-}
-
 /*
  * Record the message ready for sending.  If needed, first fragment
  * it.
@@ -1064,9 +1036,14 @@ stf_status record_outbound_v2SK_msg(struct state *msg_sa,
 				    v2SK_payload_t *sk,
 				    const char *what)
 {
+	size_t len = pbs_offset(msg);
+	if (!pexpect(sk->ike->sa.st_interface != NULL) &&
+	    sk->ike->sa.st_interface->ike_float)
+		len += NON_ESP_MARKER_SIZE;
+
+	/* This condition is broken; it uses the IKEv1 constant */
 	stf_status ret;
-	if (should_fragment_v2_ike_msg(&sk->ike->sa, pbs_offset(msg),
-				    true/*IKEv1 retransmit*/)) {
+	if (len >= endpoint_type(&sk->ike->sa.st_remote_endpoint)->ikev1_max_fragment_size) {
 		ret = v2_record_outbound_fragments(msg_sa, msg, sk, what);
 	} else {
 		ret = encrypt_v2SK_payload(sk);
