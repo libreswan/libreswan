e57f74496cd577929f4639f311840c05749388e6
Andrew Cagney <cagney@gnu.org>
2019-10-17 23:57:02 +0200

    testing: update IKEv2 fragmentation tests
    This is round one - the tests that set out to test the IKEv2
    fragmentation code. None of these were affected but a few did need
    an expected output update.
    
    Round two will be all those tests unintentionally texting fragmentation.
    
    Follow-up 258654c8ff9b6216d5f9eab7f622c774d04098c4.

--- testing/pluto/ikev2-frag-03-retrans/final.sh
+++ testing/pluto/ikev2-frag-03-retrans/final.sh
@@ -1,3 +1,4 @@
+# On east this shows the duplicates on west there is nothing.
 grep "received duplicate [^ ]* message request .* fragment" /tmp/pluto.log
 ../../pluto/bin/ipsec-look.sh
 : ==== cut ====
