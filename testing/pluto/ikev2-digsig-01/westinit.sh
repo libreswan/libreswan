6f7577b70f9017a7b033a0a4d6607f119526325a
Paul Wouters <pwouters@redhat.com>
2019-10-22 23:44:21 +0200

    testing: update ikev2-digsig-01

--- testing/pluto/ikev2-digsig-01/westinit.sh
+++ testing/pluto/ikev2-digsig-01/westinit.sh
@@ -10,4 +10,5 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits
 ipsec auto --add westnet-eastnet-ikev2
+ipsec status | grep westnet-eastnet-ikev2 | grep policy: | grep -v modecfg
 echo "initdone"
