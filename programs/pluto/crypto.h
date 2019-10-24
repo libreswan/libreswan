c6212b2e07c6e0197b1a873286edea529fb0a894
Andrew Cagney <cagney@gnu.org>
2019-10-24 09:42:01 -0400

    crypto: delete hmac_*() interface, no longer used

--- programs/pluto/crypto.h
+++ programs/pluto/crypto.h
@@ -75,30 +75,6 @@ extern void init_crypto(void);
 	memcpy((st)->st_new_iv, (tmp), (tmp_len)); \
     }
 
-/*
- * HMAC package (new code should use crypt_prf).
- */
-
-struct crypt_prf;
-struct prf_desc;        /* opaque */
-
-struct hmac_ctx {
-	struct crypt_prf *prf;
-	size_t hmac_digest_len;
-};
-
-extern void hmac_init(struct hmac_ctx *ctx,
-		      const struct prf_desc *prf_desc,
-		      /*const*/ PK11SymKey *symkey);
-
-extern void hmac_update(struct hmac_ctx *ctx,
-			const u_char *data,
-			size_t data_len);
-
-#define hmac_update_chunk(ctx, ch) hmac_update((ctx), (ch).ptr, (ch).len)
-
-extern void hmac_final(u_char *output, struct hmac_ctx *ctx);
-
 struct connection;
 
 void ike_alg_show_connection(const struct connection *c, const char *instance);
