9e31f3a3e94ca3803e9977b4367210d037834db5
Andrew Cagney <cagney@gnu.org>
2019-10-29 15:57:31 -0400

    testing: in seccomp-03-updown, give background whack time to do its job
    as in:
       ipsec whack --seccomp-crashtest & sleep 2
    there was a race between whack's output from pluto and the follow-on commands.

--- testing/pluto/seccomp-03-updown/roadrun.sh
+++ testing/pluto/seccomp-03-updown/roadrun.sh
@@ -1,9 +1,12 @@
 ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
 ipsec whack --trafficstatus
 sleep 5
-# confirm we updated unbound - should not be empty once DNS CP is added 
+
+# confirm we updated unbound - should not be empty once DNS CP is
+# added
 unbound-control list_forwards
 ipsec auto --down westnet-eastnet-ipv4-psk-ikev2
+
 # try some things to trigger new syscalls
 ipsec status > /dev/null
 ipsec whack --listen > /dev/null
@@ -12,12 +15,19 @@ ipsec whack --shuntstatus > /dev/null
 ipsec secrets > /dev/null
 ipsec auto --delete westnet-eastnet-ipv4-psk-ikev2
 ipsec stop
+
 # ensure seccomp did not kill pluto - should not show any hits
 ausearch -ts recent -i -m SECCOMP
-# and also test seccomp actually works
+
+# Test seccomp actually works - that it records pluto crashing ....
 ipsec start
-sleep 3
-ipsec whack --seccomp-crashtest &
-# should show 1 entry now
+/testing/pluto/bin/wait-until-pluto-started
+
+# Since whack hangs - it does not know that pluto died - run it in the
+# background.  Give it a few seconds to do its job.
+ipsec whack --seccomp-crashtest & sleep 2
+
+# should show 1 entry of pluto crashing now
 ausearch -ts recent -i -m SECCOMP | sed -e "s/ ip=[^ ]* / ip=XXX /" -e "s/ pid=[^ ]* / pid=XXX /" -e "s/msg=audit(.*) /msg=audit(XXX) /"
+
 echo done
