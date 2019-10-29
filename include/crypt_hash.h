937ad7e3029da28233da83ac73f2db8c2e2397b4
Andrew Cagney <cagney@gnu.org>
2019-10-29 09:21:39 -0400

    crypto: make crypt_*_hunk() macros more robust

--- include/crypt_hash.h
+++ include/crypt_hash.h
@@ -42,7 +42,13 @@ void crypt_hash_digest_byte(struct crypt_hash *hash,
 void crypt_hash_digest_bytes(struct crypt_hash *hash,
 			     const char *bytes_name, const void *bytes,
 			     size_t sizeof_bytes);
-#define crypt_hash_digest_hunk(HASH, NAME, HUNK) crypt_hash_digest_bytes(HASH, NAME, (HUNK).ptr, (HUNK).len)
+#define crypt_hash_digest_hunk(HASH, NAME, HUNK)			\
+	{								\
+		typeof(HUNK) hunk_ = HUNK; /* evaluate once */		\
+		crypt_hash_digest_bytes(HASH, NAME,			\
+					hunk_.ptr, hunk_.len);		\
+	}
+
 #define crypt_hash_digest_thing(HASH, NAME, THING) crypt_hash_digest_bytes(HASH, NAME, &THING, sizeof(THING))
 
 
