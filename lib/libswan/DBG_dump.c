66ca3b765bb6a61cd059ce4b335688699f19bae8
Andrew Cagney <cagney@gnu.org>
2019-10-29 10:21:39 -0400

    logging: simplify DBG_dump(prefix, ...) - always put the prefix on its own log line
    Rather than flip-flopping based on a magic prefix length.

--- lib/libswan/DBG_dump.c
+++ lib/libswan/DBG_dump.c
@@ -26,44 +26,24 @@
 
 #include "lswlog.h"
 
-/* dump raw bytes in hex to stderr (for lack of any better destination) */
+/*
+ * dump raw bytes; when LABEL is non-NULL prefix the dump with a log
+ * line containing the label.
+ */
 
 void DBG_dump(const char *label, const void *p, size_t len)
 {
-#define DUMP_LABEL_WIDTH 20  /* arbitrary modest boundary */
-#define DUMP_WIDTH   (4 * (1 + 4 * 3) + 1)
-	char buf[DUMP_LABEL_WIDTH + DUMP_WIDTH];
-	char *bp, *bufstart;
-	const unsigned char *cp = p;
-
-	bufstart = buf;
-
-	if (label != NULL && label[0] != '\0') {
-		/* Handle the label.  Care must be taken to avoid buffer overrun. */
-		size_t llen = strlen(label);
-
-		if (llen + 1 > sizeof(buf)) {
-			DBG_log("%s", label);
-		} else {
-			strcpy(buf, label);
-			if (buf[llen - 1] == '\n') {
-				buf[llen - 1] = '\0'; /* get rid of newline */
-				DBG_log("%s", buf);
-			} else if (llen < DUMP_LABEL_WIDTH) {
-				bufstart = buf + llen;
-			} else {
-				DBG_log("%s", buf);
-			}
-		}
+	if (label != NULL) {
+		DBG_log("%s", label);
 	}
-
-	bp = bufstart;
+	const uint8_t *cp = p;
 	do {
-		int i, j;
-
-		for (i = 0; len != 0 && i != 4; i++) {
+		/* 4 * ' xx xx xx xx' + '\0' */
+		char buf[4 * (1 + 4 * 3) + 1];
+		char *bp = buf;
+		for (int  i = 0; len != 0 && i != 4; i++) {
 			*bp++ = ' ';
-			for (j = 0; len != 0 && j != 4; len--, j++) {
+			for (int j = 0; len != 0 && j != 4; len--, j++) {
 				static const char hexdig[] =
 					"0123456789abcdef";
 
@@ -74,9 +54,7 @@ void DBG_dump(const char *label, const void *p, size_t len)
 			}
 		}
 		*bp = '\0';
+		passert(bp < buf + elemsof(buf));
 		DBG_log("%s", buf);
-		bp = bufstart;
 	} while (len != 0);
-#undef DUMP_LABEL_WIDTH
-#undef DUMP_WIDTH
 }
