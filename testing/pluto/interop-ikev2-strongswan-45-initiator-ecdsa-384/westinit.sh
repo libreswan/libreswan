ef24d67ab7f5612204846fb4743b0b502b0ecc5a
Paul Wouters <pwouters@redhat.com>
2019-11-05 23:16:00 -0500

    testing: WIP fixing up interop-ikev2-strongswan-45-initiator-ecdsa-384
    we need to use our own openssl generated certs, as strongswan pki
    tool cannot set the (extended) key usage flags that NSS requires.

--- testing/pluto/interop-ikev2-strongswan-45-initiator-ecdsa-384/westinit.sh
+++ testing/pluto/interop-ikev2-strongswan-45-initiator-ecdsa-384/westinit.sh
@@ -2,9 +2,9 @@
 rm -f /etc/strongswan/ipsec.d/cacerts/*
 rm -f /etc/strongswan/ipsec.d/certs/*
 rm -f /etc/strongswan/ipsec.d/private/*
-cp /testing/x509/strongswan/strongCAcert.der /etc/strongswan/ipsec.d/cacerts/
-cp /testing/x509/strongswan/strongWestCert.der /etc/strongswan/ipsec.d/certs/
-cp /testing/x509/strongswan/strongWestKey.der /etc/strongswan/ipsec.d/private/
+cp /testing/x509/cacerts/curveca.crt /etc/strongswan/ipsec.d/cacerts/
+cp /testing/x509/certs/west-ec.crt /etc/strongswan/ipsec.d/certs/
+cp /testing/x509/keys/west-ec.key /etc/strongswan/ipsec.d/private/
 chmod 600 /etc/strongswan/ipsec.d/private/*
 ../../pluto/bin/strongswan-start.sh
 echo "initdone"
