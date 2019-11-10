dc382ffd9e6c0201af1beea5eac490db5ad66b18
Paul Wouters <pwouters@redhat.com>
2019-11-09 23:06:50 -0500

    testing: batch1 for mostly digsig updates.

--- testing/pluto/ikev2-delete-04/westinit.sh
+++ testing/pluto/ikev2-delete-04/westinit.sh
@@ -2,7 +2,6 @@
 # confirm that the network is alive
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add west-east-delete1
 ipsec auto --status | grep west-east
 echo "initdone"
