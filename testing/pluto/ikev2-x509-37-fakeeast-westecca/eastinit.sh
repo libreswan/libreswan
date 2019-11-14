123480df8b27afe878b65267dc1071c216eae95e
Andrew Cagney <cagney@gnu.org>
2019-11-13 21:03:17 -0500

    testing x509: add ikev2-x509-37-fakeeast-westecca - west has an EC cert

---
+++ testing/pluto/ikev2-x509-37-fakeeast-westecca/eastinit.sh
@@ -0,0 +1,15 @@
+/testing/guestbin/swan-prep
+
+# import real west+mainca
+pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing/x509/pkcs12/mainca/west.p12
+# delete real main CA
+certutil -D -d sql:/etc/ipsec.d -n "Libreswan test CA for mainca - Libreswan"
+# import fake east cert and fake main CA
+pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing/x509/fake/pkcs12/mainca/east.p12
+# remove main CA - so real-west can not be verified - rely on cert=west
+certutil -D -d sql:/etc/ipsec.d -n "Libreswan test CA for mainca - Libreswan"
+
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add ikev2-westnet-eastnet-x509-cr
+echo "initdone"
---
+++ testing/pluto/ikev2-x509-37-fakeeast-westecca/westinit.sh
@@ -0,0 +1,21 @@
+/testing/guestbin/swan-prep --eccert
+
+# import real west+mainca
+pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing/x509/pkcs12/mainca/west.p12
+# delete real main CA
+certutil -D -d sql:/etc/ipsec.d -n "Libreswan test CA for mainca - Libreswan"
+# so only CA is bogus
+pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing/x509/fake/pkcs12/mainec/mainec.p12
+
+# confirm that the network is alive
+../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
+# ensure that clear text does not get through
+iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
+iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+# confirm clear text does not get through
+../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add ikev2-westnet-eastnet-x509-cr
+ipsec whack --impair suppress-retransmits
+echo "initdone"
