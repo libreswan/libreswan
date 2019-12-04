bcb9d3f3ffe87812870932b0aa01c79619461bc1
Antony Antony <antony@phenome.org>
2019-12-04 12:36:16 +0000

    testing: tests to us new reke-ike-now and rekey-ipsec-now

--- testing/pluto/ikev2-child-rekey/westrun.sh
+++ testing/pluto/ikev2-child-rekey/westrun.sh
@@ -1,11 +1,21 @@
 ipsec auto --up west
 ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
-echo "sleep 31"
-sleep 31
+echo "sleep 9"
+sleep 9
+ipsec whack --rekey-ipsec-now --name west
+sleep 2
+# expect #1 IKE #3 IPsec
+ipsec status |grep STATE_
 ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
-echo "sleep 31"
-sleep 20
+echo "sleep 11"
+sleep 11
 ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
+ipsec whack --rekey-ipsec-now --name west
+sleep 2
+# expect #1 IKE #4 IPsec
+ipsec status |grep STATE_
+ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
 echo done
--- testing/pluto/ikev2-ike-rekey-01/westrun.sh
+++ testing/pluto/ikev2-ike-rekey-01/westrun.sh
@@ -1,12 +1,17 @@
 ipsec auto --up west
 ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
-echo "sleep 31"
-sleep 31
+echo "sleep 13"
+sleep 13
+ipsec whack --rekey-ike-now --name west
 ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
 ipsec status |grep STATE_
-echo "sleep 31"
-sleep 20
+# expect #2 IPsec #3 IKE
+echo "sleep 21"
+sleep 21
+ipsec whack --rekey-ike-now --name west
 ping -W 4 -n -c 4 -I 192.0.1.254 192.0.2.254
+ipsec status |grep STATE_
+# expect #2 IPsec #4 IKE
 echo done
