3bf84e17a531bd1ac87a050317092adc96abdfd1
Andrew Cagney <cagney@gnu.org>
2019-12-16 13:53:38 -0500

    demux: delete msg_digest .next, not used
    Follow-up b3d7263c4db9f935bdad56f31b68d5b22d76104d.

--- programs/pluto/demux.h
+++ programs/pluto/demux.h
@@ -78,7 +78,6 @@ struct payload_summary {
  */
 
 struct msg_digest {
-	struct msg_digest *next;		/* for free list */
 	chunk_t raw_packet;			/* (v1) if encrypted, received packet before decryption */
 	const struct iface_port *iface;		/* interface on which message arrived */
 	ip_endpoint sender;			/* address:port where message came from */
