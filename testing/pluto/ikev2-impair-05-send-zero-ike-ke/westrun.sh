93710d8622f2973573a433af7113c0fda2ab9912
Andrew Cagney <cagney@gnu.org>
2019-12-16 16:34:34 -0500

    testing: add --impair revival to ikev2-impair-05-send-zero-ike-ke
    Stops any race between the whack messages and pluto, and stops the
    seemlingly silent fail (what's actually happening is that pluto
    sees the connection is trying to stay up and so disconnects whack).

--- testing/pluto/ikev2-impair-05-send-zero-ike-ke/westrun.sh
+++ testing/pluto/ikev2-impair-05-send-zero-ike-ke/westrun.sh
@@ -1,13 +1,18 @@
-# send KE:0
+# send KE:0 -- which is invalid
+
+ipsec whack --impair none
+ipsec whack --impair revival
 ipsec whack --impair ke-payload:0
 ipsec whack --impair suppress-retransmits
-# should fail
+# should fail with syntax-error response
 ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
+
+# receive KE:0 -- which is invalid
+
 ipsec whack --impair none
-# receive KE:0
+ipsec whack --impair revival
 ipsec whack --impair suppress-retransmits
-# using --add to reset whack fd, otherwise console output lost :/
-ipsec auto --add  westnet-eastnet-ipv4-psk-ikev2
-# should fail
+# should fail fail with syntax error
 ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
+
 echo done
