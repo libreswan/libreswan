cd6146678c82fbb266f74bd708ba0d0a42c56aa4
Paul Wouters <pwouters@redhat.com>
2019-12-16 15:28:15 -0500

    testing: fixup ikev2-impair-05-send-zero-ike-ke
    make errors more clear, updated for new error output

--- testing/pluto/ikev2-impair-05-send-zero-ike-ke/westrun.sh
+++ testing/pluto/ikev2-impair-05-send-zero-ike-ke/westrun.sh
@@ -1,13 +1,13 @@
 # send KE:0
-
 ipsec whack --impair ke-payload:0
 ipsec whack --impair suppress-retransmits
+# should fail
 ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
 ipsec whack --impair none
-
 # receive KE:0
-
 ipsec whack --impair suppress-retransmits
+# using --add to reset whack fd, otherwise console output lost :/
+ipsec auto --add  westnet-eastnet-ipv4-psk-ikev2
+# should fail
 ipsec auto --up  westnet-eastnet-ipv4-psk-ikev2
-
 echo done
