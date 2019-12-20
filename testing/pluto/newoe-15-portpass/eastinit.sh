2ae5b476f01d9bfd209c20f85c965092ad9dff52
Paul Wouters <pwouters@redhat.com>
2019-12-19 20:24:47 -0500

    testing: newoe-* updates mostly for priority changes.

--- testing/pluto/newoe-15-portpass/eastinit.sh
+++ testing/pluto/newoe-15-portpass/eastinit.sh
@@ -1,4 +1,5 @@
 /testing/guestbin/swan-prep
+#pidof sshd || (/usr/sbin/sshd -p 22 > /dev/null 2>/dev/null) > /dev/null 2>/dev/null # for namespaces
 cp policies/* /etc/ipsec.d/policies/
 echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/private-or-clear
 ipsec start
