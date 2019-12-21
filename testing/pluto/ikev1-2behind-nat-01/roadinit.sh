8c792cbd2ba72eb6e05cf8c7642ebf2968e88ff8
Paul Wouters <pwouters@redhat.com>
2019-12-21 16:42:22 -0500

    testing: clarify ikev1-2behind-nat-01 in its description
    looks like this test can never work as "intended"

--- testing/pluto/ikev1-2behind-nat-01/roadinit.sh
+++ testing/pluto/ikev1-2behind-nat-01/roadinit.sh
@@ -1,5 +1,4 @@
 /testing/guestbin/swan-prep --x509
-echo -n "foobar" > /tm/pw
 pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing/x509/pkcs12/mainca/north.p12
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
--- testing/pluto/ikev1-2behind-nat-01/roadrun.sh
+++ testing/pluto/ikev1-2behind-nat-01/roadrun.sh
@@ -1,6 +1,7 @@
 ipsec auto --up road
 ping -n -c 4 192.0.2.254
-killall -9 pluto
+ipsec whack --impair send-no-delete
+ipsec stop
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add north
