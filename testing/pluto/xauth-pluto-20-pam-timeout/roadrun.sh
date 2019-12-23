6d9d20d8833ff2e6e8b0f6a774bbcc5675e0e2bf
Paul Wouters <pwouters@redhat.com>
2019-12-22 19:10:38 -0500

    testing: rework xauth-pluto-20-pam* to avoid false positives
    it would change depending on whether it was deleting a I1 or I2
    during a expected to-fail connection.

--- testing/pluto/xauth-pluto-20-pam-timeout/roadrun.sh
+++ testing/pluto/xauth-pluto-20-pam-timeout/roadrun.sh
@@ -1,7 +1,8 @@
 # next one will fail because server will timeout for this user
 ipsec whack --xauthname 'gooduser90' --xauthpass 'use1pass' --name xauth-road-eastnet --initiate
 # next one should succed and ping pass throguh
-ipsec auto --add xauth-road-eastnet
+# prevent false positive on deleting I1 or I2 by redirecting to /dev/null
+ipsec auto --add xauth-road-eastnet > /dev/null
 ipsec whack --xauthname 'gooduser' --xauthpass 'use1pass' --name xauth-road-eastnet --initiate
 ping -w 4 -n -c4 192.0.2.254
 ipsec whack --trafficstatus
--- testing/pluto/xauth-pluto-20-pam/roadrun.sh
+++ testing/pluto/xauth-pluto-20-pam/roadrun.sh
@@ -1,7 +1,8 @@
 ipsec whack --impair suppress-retransmits
 ipsec whack --xauthname 'baduser' --xauthpass 'use1pass' --name xauth-road-eastnet --initiate
 # prevent revive race, establishing two connections
-ipsec auto --add xauth-road-eastnet
+# prevent false positive on deleting I1 or I2 by redirecting to /dev/null
+ipsec auto --add xauth-road-eastnet > /dev/null
 ipsec whack --xauthname 'gooduser' --xauthpass 'use1pass' --name xauth-road-eastnet --initiate
 ping -n -c4 192.0.2.254
 ipsec trafficstatus
