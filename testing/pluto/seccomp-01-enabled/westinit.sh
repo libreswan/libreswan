2fda02987a63ce42f32ecd520133e788cad429a7
Paul Wouters <pwouters@redhat.com>
2019-12-26 23:10:58 -0500

    testing: seccomp-01-enabled fixup for sanitized audit service

--- testing/pluto/seccomp-01-enabled/westinit.sh
+++ testing/pluto/seccomp-01-enabled/westinit.sh
@@ -1,6 +1,6 @@
 /testing/guestbin/swan-prep --x509
 ipsec _stackmanager start
-service auditd stop
+service auditd stop > /dev/null 2> /dev/null
 rm  -f /var/log/audit/audit.log
 systemctl start auditd.service
 mkdir -p /var/run/pluto
