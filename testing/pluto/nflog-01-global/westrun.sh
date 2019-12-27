41da5fb389b50d7409d69ba6c9ff04f20957c6ea
Paul Wouters <pwouters@redhat.com>
2019-12-26 23:22:08 -0500

    testing: fixup nflog-* tests to avoid race condition in tcpdump process output

--- testing/pluto/nflog-01-global/westrun.sh
+++ testing/pluto/nflog-01-global/westrun.sh
@@ -2,7 +2,7 @@
 iptables -L -n
 ipsec auto --up westnet-eastnet-ikev2
 rm -fr /tmp/nflog-50.pcap
-tcpdump -c 8 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 &
+(tcpdump -c 8 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 >/dev/null 2>/dev/null &)
 ping -n -c 5 -I 192.0.1.254 192.0.2.254
 cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
 tcpdump -n -r OUTPUT/nflog-50.pcap
--- testing/pluto/nflog-03-conns/westrun.sh
+++ testing/pluto/nflog-03-conns/westrun.sh
@@ -4,7 +4,7 @@ iptables -L -n
 ipsec auto --up west-east-nflog
 iptables -L -n
 rm -fr /tmp/nflog-50.pcap
-tcpdump -c 8 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 &
+(tcpdump -c 8 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 >/dev/null 2>/dev/null &)
 ping -n -c 2 -I 192.1.2.45 192.1.2.23
 ping -n -c 2 -I 192.0.1.254 192.0.2.254
 ping -n -c 2 -I 192.1.2.45 192.1.2.23
