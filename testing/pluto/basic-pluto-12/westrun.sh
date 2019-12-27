aeefd8b8389f51a44237988407ca9ad342aef79a
Paul Wouters <pwouters@redhat.com>
2019-12-27 14:41:23 -0500

    testing: various fixes for namespaces
    test for being in a namespace and start sshd on east, required for
    these tests. Some sport/dport fixes. ssh error fixups for difference
    between f30 and f31

--- testing/pluto/basic-pluto-12/westrun.sh
+++ testing/pluto/basic-pluto-12/westrun.sh
@@ -6,5 +6,6 @@ echo take-passthrough-unencrpted | nc -s 192.0.1.254 192.0.2.254 22
 ipsec whack --trafficstatus
 echo take-conn-encrypted | nc -s 192.0.1.254 192.0.2.254 222
 # this moved through the tunnel, so non-zero
-ipsec whack --trafficstatus
+# workaround for diff err msg between fedora versions resulting in diff byte count
+ipsec whack --trafficstatus | grep -v "inBytes=0" | sed "s/type=ESP.*$/[...]/"
 echo done
