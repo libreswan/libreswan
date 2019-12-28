1c1f64dd1019416edcf189eed0d8baec1a4ed45e
Paul Wouters <pwouters@redhat.com>
2019-12-27 21:33:12 -0500

    testing: ikev2-x509-25-missing-ca updated for new NSS output related to missing CAs

--- testing/pluto/ikev2-x509-25-missing-ca/eastinit.sh
+++ testing/pluto/ikev2-x509-25-missing-ca/eastinit.sh
@@ -4,5 +4,7 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 # remove CA cert
 certutil -D -d sql:/etc/ipsec.d -n "Libreswan test CA for mainca - Libreswan"
+# insert a different CAcert to avoid NSS aborting for having no CA at all
+pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing//x509/pkcs12/badca/badeast.p12
 ipsec auto --add ikev2-westnet-eastnet-x509-cr
 echo "initdone"
