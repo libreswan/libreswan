636aa1fcb9f935b279400d40471139858dec712f
Paul Wouters <pwouters@redhat.com>
2019-12-28 15:06:22 -0500

    testing: don't confirm ping in nat-pluto-* tests
    The f30/f31 output for failed ping is quite different and not easilly
    sanitized.

--- testing/pluto/nat-pluto-10/northinit.sh
+++ testing/pluto/nat-pluto-10/northinit.sh
@@ -1,8 +1,6 @@
 /testing/guestbin/swan-prep --x509
 # ensure that clear text does not get through
 iptables -A INPUT -i eth1 -s 192.0.2.254/32 -j LOGDROP
-# confirm clear text does not get through
-ping -c 4 -n -I 192.0.3.254 192.0.2.254
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add northnet-eastnet-nat
