8f605710830a179e7ff869dac24598e79991f647
Paul Wouters <pwouters@redhat.com>
2019-12-28 17:48:55 -0500

    testing: update check for charon.pid to also check in /var/run/strongswan/
    Once everyone has upgraded to 5.8.2-2 we can remove the check for the old
    location (/var/run/) and only check for the new location.

--- testing/pluto/interop-ikev2-strongswan-07-strongswan/final.sh
+++ testing/pluto/interop-ikev2-strongswan-07-strongswan/final.sh
@@ -1,10 +1,10 @@
 ip xfrm pol
 ip xfrm state
 if [ -f /var/run/pluto/pluto.pid ]; then ../../pluto/bin/ipsec-look.sh ; fi
-if [ -f /var/run/charon.pid ]; then strongswan statusall ; fi
+if [ -f /var/run/charon.pid -o -f /var/run/strongswan/charon.pid ]; then strongswan statusall ; fi
 : ==== cut ====
 if [ -f /var/run/pluto/pluto.pid ]; then ipsec auto --status ; fi
-if [ -f /var/run/charon.pid ]; then strongswan statusall ; fi
+if [ -f /var/run/charon.pid -o -f /var/run/strongswan/charon.pid ]; then strongswan statusall ; fi
 : ==== tuc ====
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
