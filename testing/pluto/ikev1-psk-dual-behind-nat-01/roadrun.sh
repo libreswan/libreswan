ab92f28a46a5188cfeecf3d4b88743d220f6e3cc
Paul Wouters <pwouters@redhat.com>
2019-12-28 15:26:10 -0500

    testing: a few minor ping/ports fixups

--- testing/pluto/ikev1-psk-dual-behind-nat-01/roadrun.sh
+++ testing/pluto/ikev1-psk-dual-behind-nat-01/roadrun.sh
@@ -1,4 +1,4 @@
 ipsec whack --xauthname 'xroad' --xauthpass 'use1pass' --name road-east --initiate
 ping -n -c 4 -I 192.0.2.102 192.0.2.254
-ipsec whack --trafficstatus
+ipsec whack --trafficstatus | sed "s/port [0-9]* /port EPHEMERAL /"
 echo done
