e00324f754c4defdb112b671c5eb6c277bfe5ced
Paul Wouters <pwouters@redhat.com>
2019-12-27 20:50:53 -0500

    testing: newoe-25-cat-5 fixup for ephemeral ports in ip xfrm output

--- testing/pluto/newoe-25-cat-5/nicinit.sh
+++ testing/pluto/newoe-25-cat-5/nicinit.sh
@@ -1,7 +1,7 @@
 iptables -t nat -F
 iptables -F
 # NAT UDP 500,4500 to NICs address with sport
-iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -p udp --sport 4500  -j SNAT --to-source 192.1.2.254:2500-2700
-iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -p udp --sport 500  -j SNAT --to-source 192.1.2.254:3500-3700
+iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -p udp --sport 4500  -j SNAT --to-source 192.1.2.254:32768-33000
+iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -p udp --sport 500  -j SNAT --to-source 192.1.2.254:33001-33101
 iptables -t nat -A POSTROUTING -s 192.1.3.0/24 -j SNAT --to-source 192.1.2.254
 : ==== end ====
