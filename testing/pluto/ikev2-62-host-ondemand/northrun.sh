49df52ab66c35b0e719c1cb9bd30e68cb282199a
Andrew Cagney <cagney@gnu.org>
2019-12-29 11:27:03 -0500

    testing: eliminate one-ping.sh - use ping-once.sh

--- testing/pluto/ikev2-62-host-ondemand/northrun.sh
+++ testing/pluto/ikev2-62-host-ondemand/northrun.sh
@@ -1,5 +1,5 @@
 # one ping to trigger IKE
-../../pluto/bin/one-ping.sh -I 192.0.3.254 192.0.2.254
+../../pluto/bin/ping-once.sh --forget -I 192.0.3.254 192.0.2.254
 ../../pluto/bin/wait-for-whack-trafficstatus.sh north-east
 # success
 ping -q -w 4 -n -c 4 -I 192.0.3.254 192.0.2.254
