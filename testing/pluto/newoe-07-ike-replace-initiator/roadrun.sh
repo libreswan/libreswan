c950ea0a4f8f144d11c8af6dfbc16b073c4528c0
Paul Wouters <pwouters@redhat.com>
2020-01-06 18:18:13 -0500

    testing: newoe-07-ike-replace-initiator  use whack instead of short timers
    This avoids various race conditions and false positives in this test.
    
    For rekey, it uses: ipsec whack --rekey-ike --name 1

--- testing/pluto/newoe-07-ike-replace-initiator/roadrun.sh
+++ testing/pluto/newoe-07-ike-replace-initiator/roadrun.sh
@@ -1,16 +1,13 @@
+# first ping will be lost in triggering OE
 ping -n -c 4 -I 192.1.3.209 192.1.2.23
-# wait on OE retransmits and rekeying
-sleep 20
-ping -n -c 4 -I 192.1.3.209 192.1.2.23
+sleep 1
 ipsec status | grep "STATE_" 
-sleep 20
-ping -n -c 4 -I 192.1.3.209 192.1.2.23
-sleep 20
-ping -n -c 4 -I 192.1.3.209 192.1.2.23
-sleep 20
-ping -n -c 4 -I 192.1.3.209 192.1.2.23
-#parent state must be #3
-grep "STATE_" OUTPUT/road.console.verbose.txt
+# issue a manual rekey of parent
+ipsec whack --rekey-ike --name 1
+# wait two seconds to ensure old parent has expired
+sleep 2
+# parent state must be #3 and the latest ISAKMP
+ipsec status | grep STATE_
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
 echo done
