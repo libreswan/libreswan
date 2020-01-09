6857521f8bcafb66a83353aaf76104aaced5f308
Paul Wouters <pwouters@redhat.com>
2020-01-09 10:54:07 -0500

    testing: make ikev2-55-ipseckey-09 visibly fail better by changing console and roadrun.sh
    Looks like the 2nd key in DNS is not found by east ?

--- testing/pluto/ikev2-55-ipseckey-09/roadrun.sh
+++ testing/pluto/ikev2-55-ipseckey-09/roadrun.sh
@@ -13,6 +13,7 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits --impair revival
 ipsec auto --add road-east-2
+# expected to succeed !
 ipsec auto --up road-east-2
 ping -n -c 4 -I 192.1.3.209 192.1.2.23
 echo done
