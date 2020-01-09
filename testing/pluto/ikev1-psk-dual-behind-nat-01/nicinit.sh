fdde625f14ad92c6e648937c804098d621dbab26
Paul Wouters <pwouters@redhat.com>
2020-01-09 11:23:50 -0500

    testing: ikev1-psk-dual-behind-nat-01 fix ephemeral ports

--- testing/pluto/ikev1-psk-dual-behind-nat-01/nicinit.sh
+++ testing/pluto/ikev1-psk-dual-behind-nat-01/nicinit.sh
@@ -2,7 +2,7 @@
 iptables -t nat -F
 iptables -F
 # NAT
-iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j SNAT --to-source 192.1.2.254
+iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j SNAT -p udp --to-source 192.1.2.254:33000-33100
 # make sure that we never acidentially let ESP through.
 iptables -N LOGDROP
 iptables -A LOGDROP -j LOG
