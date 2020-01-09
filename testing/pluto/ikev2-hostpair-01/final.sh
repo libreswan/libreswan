b3c9b816f0549e2098446a5661258dc29e1561c4
Paul Wouters <pwouters@redhat.com>
2020-01-08 23:17:31 -0500

    testing: fixup ikev2-hostpair-01

--- testing/pluto/ikev2-hostpair-01/final.sh
+++ testing/pluto/ikev2-hostpair-01/final.sh
@@ -1,6 +1,8 @@
 ipsec status | grep eastnet
 # should show no hits
 grep INVALID_IKE_SPI /tmp/pluto.log
+grep MSG_TRUNC /tmp/pluto.log
+grep "cannot route" /tmp/pluto.log
 : ==== cut ====
 : ==== tuc ====
 ../bin/check-for-core.sh
