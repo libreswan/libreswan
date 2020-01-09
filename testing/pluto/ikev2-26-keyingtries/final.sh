6804358827da60eee84a4344ff4b3ca690da7027
Paul Wouters <pwouters@redhat.com>
2020-01-09 11:07:42 -0500

    testing: minor updates

--- testing/pluto/ikev2-26-keyingtries/final.sh
+++ testing/pluto/ikev2-26-keyingtries/final.sh
@@ -1,7 +1,3 @@
-../../pluto/bin/ipsec-look.sh
-: ==== cut ====
-ipsec auto --status
-: ==== tuc ====
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
 : ==== end ====
--- testing/pluto/ikev2-26-keyingtries/westrun.sh
+++ testing/pluto/ikev2-26-keyingtries/westrun.sh
@@ -1,9 +1,8 @@
-# this should fail, our testing cannot wait for whack to release, so use async
-ipsec auto --up --asynchronous westnet-eastnet-ipv4-psk-ikev2
-sleep 30
-sleep 30
-sleep 30
+# this should fail
+ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
 # the state should have been deleted, but replaced via EVENT_SA_REPLACE with a new state trying
 # if no STATE_s show up, this test failed
 ipsec status |grep STATE_  || echo "test failed, all states went away"
+# only one pending CHILD SA event should show up
+ipsec status |egrep "STATE_|pending"
 echo done
