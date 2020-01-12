0a7368c279955c52c048e293cc2e7285794d73aa
Paul Wouters <pwouters@redhat.com>
2020-01-11 21:34:12 -0500

    testing: remove ikev2-x509-21-shared-ip
    it does not test what it should, and we already have another test
    that uses west and road's cert to do exactly this, but without
    IKE ID not matching SAN due to made up ID to hit other conn.

--- testing/pluto/ikev2-x509-21-shared-ip/final.sh
+++
@@ -1,8 +0,0 @@
-# on west, we should only see main, on east we should other and main
-ipsec whack --trafficstatus
-: ==== cut ====
-ipsec auto --status
-: ==== tuc ====
-../bin/check-for-core.sh
-if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
-: ==== end ====
--- testing/pluto/ikev2-x509-21-shared-ip/westinit.sh
+++
@@ -1,4 +0,0 @@
-/testing/guestbin/swan-prep --x509
-certutil -D -n east -d sql:/etc/ipsec.d
-pk12util -W foobar -K '' -d sql:/etc/ipsec.d -i /testing/x509/pkcs12/otherca/otherwest.p12
-echo "initdone"
