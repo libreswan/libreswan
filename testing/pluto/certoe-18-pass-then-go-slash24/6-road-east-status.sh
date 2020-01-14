ac183ee01e0c566bc363ee47b8f19461f052d391
Paul Wouters <pwouters@redhat.com>
2020-01-13 20:16:51 -0500

    testing: add note to certoe-18-pass-then-go-slash24/6-road-east-status.sh

--- testing/pluto/certoe-18-pass-then-go-slash24/6-road-east-status.sh
+++ testing/pluto/certoe-18-pass-then-go-slash24/6-road-east-status.sh
@@ -1,5 +1,6 @@
 # OE has been triggered.
-# there should be no %pass shunts on either side and an active tunnel
+# there should be no %pass shunts on either side and an active tunnel and no partial states
+ipsec status |grep STATE_
 ipsec trafficstatus
 ipsec shuntstatus
 ../../pluto/bin/ipsec-look.sh
