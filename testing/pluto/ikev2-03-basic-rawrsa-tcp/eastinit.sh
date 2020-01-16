88f2113cd3a75ee0dcfcfa49c8429ee2f92c85da
Andrew Cagney <cagney@gnu.org>
2020-01-16 17:56:17 -0500

    testing: update WIP tcp tests to better match current output

--- testing/pluto/ikev2-03-basic-rawrsa-tcp/eastinit.sh
+++ testing/pluto/ikev2-03-basic-rawrsa-tcp/eastinit.sh
@@ -2,5 +2,4 @@
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-ipsec auto --status
 echo "initdone"
--- testing/pluto/ikev2-04-basic-x509-tcp/eastinit.sh
+++ testing/pluto/ikev2-04-basic-x509-tcp/eastinit.sh
@@ -2,5 +2,4 @@
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-ipsec auto --status | grep westnet-eastnet-ikev2
 echo "initdone"
