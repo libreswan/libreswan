a849c460e36908baa1a66a656a24f8989621a0ea
Andrew Cagney <cagney@gnu.org>
2020-01-22 11:09:51 -0500

    testing: add extract-console-from-debug-log.sh
    As in:
    
    $ ./testing/utils/extract-console-from-debug-log.sh testing/pluto/basic-pluto-01/OUTPUT/debug.log east 2>&1 | head
    error: The domain is not running
    
    error: The domain is not running
    
    Connected to domain east
    Escape character is ^]
    [    0.000000] Linux version 5.2.5-200.fc30.x86_64 (mockbuild@bkernel04.phx2.fedoraproject.org) (gcc version 9.1.1 20190503 (Red Hat 9.1.1-1) (GCC)) #1 SMP Wed Jul 31 14:37:17 UTC 2019
    [    0.000000] Command line: BOOT_IMAGE=(hd0,msdos1)/boot/vmlinuz-5.2.5-200.fc30.x86_64 root=UUID=fad14278-c6fa-44c1-a4f5-fb8ef547e9e9 ro console=tty0 rd_NO_PLYMOUTH biosdevname=0 net.ifnames=0 console=ttyS0,115200
    [    0.000000] x86/fpu: x87 FPU will use FXSAVE

---
+++ testing/utils/extract-console-from-debug-log.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if test $# -ne 2; then
+    cat <<EOF
+Usage:
+
+  $0 <debug.log> <host>
+
+Grub through <debug.log> extracting the console messages for <host>.
+Specifically the boot messages.  For instance:
+
+  $0 testing/pluto/basic-pluto-01/OUTPUT/debug.log east
+
+(this script extracts the text by grubbing for low-level debug-log
+message)
+
+EOF
+    exit 1
+fi
+
+debug=$1 ; shift
+host=$1 ; shift
+
+{
+    cat ${debug}
+} | {
+    grep -e "shell ${host} .* read <<" \
+	 -e "shell ${host} .* send <<"
+} | {
+    # extract the text read from the host's console
+    sed -e "s/^.* read <*[\"']//" \
+	-e "s/^.* send <*[\"']//" \
+	-e "s/[\"']>*$//"
+} | {
+    # turn the text into a single very long line (the true \r\n were
+    # escaped).
+    tr -d '\n'
+} | {
+    # convert the escaped \r and \n into line breaks
+    sed -e 's/\\r\\r\\n/\n/g' -e 's/\\r\\n/\n/g' -e 's/\\n/\n/g'
+} | {
+    # strip out magic escape characters used to fancy print on the
+    # console (can this just be disabled).  Apparently \u2026 is
+    # horizontal ellipsis.
+    sed -e 's/\\x1b\[0;1;3[0-9]m//g' \
+	-e 's/\\x1b\[0;3[0-9]m//g' \
+	-e 's/\\x1b\[0m//g' \
+	-e 's/\\x1bM\\r\\x1b\[K//' \
+	-e 's/\\u2026/.../'
+}
