b066e03a64a208e7da74f75c550d0164415507c6
Andrew Cagney <cagney@gnu.org>
2020-01-23 21:02:56 -0500

    testing tcp: tweak ikev2-03-basic-rawrsa-tcp
    - spell out all firewall rules
    - don't refer to testing's config
    - only allow TCP
    - expect ping to work

--- testing/pluto/ikev2-03-basic-rawrsa-tcp/westinit.sh
+++ testing/pluto/ikev2-03-basic-rawrsa-tcp/westinit.sh
@@ -2,8 +2,13 @@
 # confirm that the network is alive
 ../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # make sure that clear text does not get through
+iptables -F
+iptables -X
+iptables -N LOGDROP
+iptables -A LOGDROP -j LOG
+iptables -A LOGDROP -j DROP
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
-iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+iptables -A OUTPUT -o eth1 -p tcp --dport 4500 -j ACCEPT
 # confirm with a ping
 ../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 ipsec start
