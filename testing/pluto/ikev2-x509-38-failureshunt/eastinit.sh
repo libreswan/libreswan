aba50e3a12e3c5a99a3750a436cc50c8574a815c
Antony Antony <antony@phenome.org>
2020-01-26 22:33:32 +0000

    testing: ikev2-x509-38-failureshunt simplify the config

--- testing/pluto/ikev2-x509-38-failureshunt/eastinit.sh
+++ testing/pluto/ikev2-x509-38-failureshunt/eastinit.sh
@@ -1,7 +1,7 @@
 /testing/guestbin/swan-prep --x509
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet
 ipsec auto --add failureshunt
+ipsec auto --add westnet-eastnet
 ipsec status
 echo "initdone"
