54869ae7b45dadf8ebdfc2ca4af2b8fd161c523a
Andrew Cagney <cagney@gnu.org>
2020-01-27 17:20:28 -0500

    ip: merge satot() into jam_said(), simplify

--- lib/libswan/ip_said.c
+++ lib/libswan/ip_said.c
@@ -16,13 +16,9 @@
  */
 
 #include "ip_said.h"
-#include <string.h>
-#include <stdio.h>
-
-#include "ip_said.h"
 #include "ip_info.h"
-#include "jambuf.h"
-#include "libreswan/passert.h"
+
+#include "lswlog.h"	/* for bad_case() */
 
 ip_said said3(const ip_address *address, ipsec_spi_t spi,
 	      const struct ip_protocol *proto)
@@ -35,133 +31,55 @@ ip_said said3(const ip_address *address, ipsec_spi_t spi,
 	return said;
 }
 
+
 /*
-   - satot - convert SA to text "ah507@1.2.3.4"
+ * convert SA to text "ah507@1.2.3.4"
  */
-static size_t                          /* space needed for full conversion */
-satot(sa, format, dst, dstlen)
-const ip_said * sa;
-int format;                     /* character */
-char *dst;                      /* need not be valid if dstlen is 0 */
-size_t dstlen;
+
+void jam_said(jambuf_t *buf, const ip_said *sa)
 {
-	size_t len = 0;         /* 0 means "not recognized yet" */
-	int base;
-	int showversion;        /* use delimiter to show IP version? */
-	char *p;
-	char buf[10 + 1 + ULTOT_BUF + ADDRTOT_BUF];
-
-	switch (format) {
-	case 0:
-		base = 16;
-		showversion = 1;
-		break;
-	case 'f':
-		base = 17;
-		showversion = 1;
-		break;
-	case 'x':
-		base = 'x';
-		showversion = 0;
-		break;
-	case 'd':
-		base = 10;
-		showversion = 0;
-		break;
-	default:
-		if (dstlen > 0) {
-			strncpy(dst, "(error)", dstlen-1);
-			dst[dstlen-1] = '\0';
-		}
-		return 0;
-
-		break;
-	}
-
-	memset(buf, 0, sizeof(buf));
-
-	/* const ip_protocol *proto = sa->proto; */
 	const struct ip_protocol *proto = sa->proto;
 	const char *pre = (proto == NULL ? "unk" : proto->prefix);
 
 	if (strcmp(pre, PASSTHROUGHTYPE) == 0 &&
 	    sa->spi == PASSTHROUGHSPI &&
 	    isanyaddr(&sa->dst)) {
-		strcpy(buf, (said_type(sa) == &ipv4_info) ?
-		       PASSTHROUGH4NAME :
-		       PASSTHROUGH6NAME);
-		len = strlen(buf);
-	}
-
-	if (sa->proto == SA_INT) {
-		char intunk[10];
-
+		jam_string(buf, (said_type(sa) == &ipv4_info ?
+				 PASSTHROUGH4NAME :
+				 PASSTHROUGH6NAME));
+	} else if (sa->proto == SA_INT) {
 		switch (ntohl(sa->spi)) {
 		case SPI_PASS:
-			p = "%pass";
+			jam_string(buf, "%pass");
 			break;
 		case SPI_DROP:
-			p = "%drop";
+			jam_string(buf, "%drop");
 			break;
 		case SPI_REJECT:
-			p = "%reject";
+			jam_string(buf, "%reject");
 			break;
 		case SPI_HOLD:
-			p = "%hold";
+			jam_string(buf, "%hold");
 			break;
 		case SPI_TRAP:
-			p = "%trap";
+			jam_string(buf, "%trap");
 			break;
 		case SPI_TRAPSUBNET:
-			p = "%trapsubnet";
+			jam_string(buf, "%trapsubnet");
 			break;
 		default:
-			snprintf(intunk, sizeof(intunk), "%%unk-%d",
-				 ntohl(sa->spi));
-			p = intunk;
+			jam(buf, "%s-%d", "%unk", ntohl(sa->spi));
 			break;
 		}
-		if (p != NULL) {
-			strcpy(buf, p);
-			len = strlen(buf);
-		}
+	} else {
+		/* general case needed */
+		jam_string(buf, pre);
+		/* .SPI */
+		jam_char(buf, (said_type(sa) == &ipv4_info ? '.' : ':'));
+		jam(buf, "%x", ntohl(sa->spi));;
+		jam_char(buf, '@');
+		jam_address(buf, &sa->dst);
 	}
-
-	if (len == 0) {                 /* general case needed */
-		strcpy(buf, pre);
-		len = strlen(buf);
-		if (showversion) {
-			*(buf +
-			  len) = (said_type(sa) == &ipv4_info) ?
-				'.' : ':';
-			len++;
-			*(buf + len) = '\0';
-		}
-		len += ultot(ntohl(sa->spi), base, buf + len, sizeof(buf) - len);
-		*(buf + len - 1) = '@';
-		/* jambuf's are always '\0' terminated */
-		jambuf_t b = array_as_jambuf(buf + len, sizeof(buf) - len);
-		jam_address(&b, &sa->dst);
-		/* "pos" is always '\0' */
-		const char *end = jambuf_cursor(&b);
-		passert(*end == '\0');
-		/* *tot() functions lengh includes the NULL */
-		len = end-buf+1;
-	}
-
-	if (dst != NULL) {
-		if (len > dstlen)
-			*(buf + dstlen - 1) = '\0';
-		strcpy(dst, buf);
-	}
-	return len;
-}
-
-void jam_said(jambuf_t *buf, const ip_said *said)
-{
-	char t[SATOT_BUF];
-	satot(said, 0, t, sizeof(t));
-	jam_string(buf, t);
 }
 
 const char *str_said(const ip_said *said, said_buf *buf)
