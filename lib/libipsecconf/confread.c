libreswan/libreswan

commit 5d2283092dc8063b94f2f7c64b991ab9f1321f2e

libipsecconf: Add support for authby=foo,bar

lib/libipsecconf/confread.c
@@ -1174,11 +1174,9 @@ static bool load_conn(
 	KW_POLICY_FLAG(KBF_PFS, POLICY_PFS);
 
 	/* reset authby= flags */
-	if (conn->options_set[KBF_AUTHBY]) {
+	if (conn->options_set[KSCF_AUTHBY]) {
 
 		conn->policy &= ~POLICY_ID_AUTH_MASK;
-		conn->policy |= conn->options[KBF_AUTHBY];
-
 	}
 
 	KW_POLICY_NEGATIVE_FLAG(KBF_IKEPAD, POLICY_NO_IKEPAD);
@@ -1337,6 +1335,40 @@ static bool load_conn(
 
 		case sat_no:
 			break;
+		}
+	}
+
+	/* read in the authby string and translate to policy bits
+	 * this is the symmetric (left+right) version
+	 * there is also leftauthby/rightauthby version stored in 'end'
+	 *
+	 * authby=secret|rsasig|null|never|rsa-HASH
+	 */
+	if (conn->strings_set[KSCF_AUTHBY]) {
+		char *val = strtok(conn->strings[KSCF_AUTHBY], ", ");
+
+		while (val != NULL) {
+
+			/* Supported for IKEv1 and IKEv2 */
+			if (streq(val, "secret"))
+				conn->policy |= POLICY_PSK;
+			else if (streq(val, "rsasig") || streq(val, "rsa")) {
+				conn->policy |= POLICY_RSASIG;
+			}
+			else if (streq(val, "never"))
+				conn->policy |= POLICY_AUTH_NEVER;
+			/* everything else is only supported for IKEv2 */
+			else if (conn->policy & POLICY_IKEV1_ALLOW) {
+				*perr = "connection allowing ikev1 must use authby= of rsasig,secret or never ";
+				return TRUE;
+			}
+			else if (streq(val, "null")) {
+				conn->policy |= POLICY_AUTH_NULL;
+			} else {
+				*perr = "connection authby= value is unknown";
+				return TRUE;
+			}
+			val = strtok(NULL, ", ");
 		}
 	}
 

include/ipsecconf/keywords.h
@@ -109,7 +109,6 @@ enum keyword_numeric_config_field {
 	KBF_DPDTIMEOUT,
 	KBF_METRIC,
 	KBF_PHASE2,
-	KBF_AUTHBY,
 	KBF_KEYEXCHANGE,
 	KBF_AUTO,
 	KBF_PFS,
@@ -185,6 +184,7 @@ enum keyword_string_conn_field {
 	KSCF_NEXTHOP, /* loose_enum */
 	KSCF_UPDOWN,
 	KSCF_ID,
+	KSCF_AUTHBY, /* formerly enum */
 	KSCF_RSAKEY1, /* loose_enum */
 	KSCF_RSAKEY2, /* loose_enum */
 	KSCF_CERT,

lib/libipsecconf/keywords.c
@@ -130,18 +130,6 @@ static const struct keyword_enum_value kw_seccomp_values[] = {
 static const struct keyword_enum_values kw_seccomp_list = VALUES_INITIALIZER(kw_seccomp_values);
 #endif
 
-/*
- * Values for authby={never, rsasig, secret, null}
- */
-static const struct keyword_enum_value kw_authby_values[] = {
-        { "secret",    POLICY_PSK },
-        { "rsasig",    POLICY_RSASIG },
-        { "never",     POLICY_AUTH_NEVER },
-        { "null",      POLICY_AUTH_NULL },
-        { "secret|rsasig",    POLICY_PSK | POLICY_RSASIG},
-};
-static const struct keyword_enum_values kw_authby_list = VALUES_INITIALIZER(kw_authby_values);
-
 static const struct keyword_enum_value kw_auth_lr_values[] = {
        { "never",     AUTH_NEVER },
        { "secret",    AUTH_PSK },
@@ -525,7 +513,7 @@ const struct keyword_def ipsec_conf_keywords[] = {
   { "alsoflip",  kv_conn,  kt_string,  KSCF_ALSOFLIP, NULL, NULL, },
   { "connaddrfamily",  kv_conn,  kt_enum,  KBF_CONNADDRFAMILY,  &kw_connaddrfamily_list, NULL, },
   { "type",  kv_conn,  kt_enum,  KBF_TYPE,  &kw_type_list, NULL, },
-  { "authby",  kv_conn,  kt_enum,  KBF_AUTHBY,  &kw_authby_list, NULL, },
+  { "authby",  kv_conn,  kt_string,  KSCF_AUTHBY, NULL, NULL, },
   { "keyexchange",  kv_conn,  kt_enum,  KBF_KEYEXCHANGE,  &kw_keyexchange_list, NULL, },
   { "ikev2",  kv_conn | kv_processed,  kt_enum,  KBF_IKEv2,  &kw_fourvalued_list, NULL, },
   { "ppk", kv_conn | kv_processed, kt_enum, KBF_PPK, &kw_fourvalued_list, NULL, },

programs/pluto/connections.c
@@ -1771,14 +1771,14 @@ void add_connection(const struct whack_message *wm)
 		default_end(&c->spd.that, &c->spd.this.host_addr);
 
 		/*
-		 * If both left/rightauth is unset, fill it in with symmetric policy
+		 * If both left/rightauth is unset, fill it in with (prefered) symmetric policy
 		 */
 		if (wm->left.authby == AUTH_UNSET && wm->right.authby == AUTH_UNSET) {
 			if (c->policy & POLICY_RSASIG)
 				c->spd.this.authby = c->spd.that.authby = AUTH_RSASIG;
-			if (c->policy & POLICY_PSK)
+			else if (c->policy & POLICY_PSK)
 				c->spd.this.authby = c->spd.that.authby = AUTH_PSK;
-			if (c->policy & POLICY_AUTH_NULL)
+			else if (c->policy & POLICY_AUTH_NULL)
 				c->spd.this.authby = c->spd.that.authby = AUTH_NULL;
 		}
 

commit f2967f3bffd186192b3e9487219f883e0690863b

pluto: fix do not set sighash_policy = NONE when failureshunt was set
Fixes: 5d2283092dc
check was treating AUTHBY as enum, while it is string, and checking wrong array.
KNCF_FAILURESHUNT 18 KSCF_AUTHBY 18

lib/libipsecconf/confread.c
@@ -1236,7 +1236,7 @@ static bool load_conn(
 	KW_POLICY_FLAG(KNCF_PFS, POLICY_PFS);
 
 	/* reset authby= flags */
-	if (conn->options_set[KSCF_AUTHBY]) {
+	if (conn->strings_set[KSCF_AUTHBY]) {
 
 		conn->policy &= ~POLICY_ID_AUTH_MASK;
 		conn->sighash_policy = LEMPTY;