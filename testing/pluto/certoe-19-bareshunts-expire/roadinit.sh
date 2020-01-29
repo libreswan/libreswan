6e5474c971c60e55fbc77778bc1f6a508811104b
Paul Wouters <pwouters@redhat.com>
2020-01-29 19:23:09 +0100

    testing: added certoe-19-bareshunts-expire

---
+++ testing/pluto/certoe-19-bareshunts-expire/roadinit.sh
@@ -0,0 +1,13 @@
+/testing/guestbin/swan-prep --x509
+certutil -D -n east -d sql:/etc/ipsec.d
+cp road-ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
+cp policies/* /etc/ipsec.d/policies/
+echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
+echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/private
+echo "192.1.4.66/32"  >> /etc/ipsec.d/policies/private-or-clear
+# scan every 10s
+ipsec pluto --config /etc/ipsec.conf --expire-shunt-interval 10
+/testing/pluto/bin/wait-until-pluto-started
+# give OE policies time to load
+sleep 5
+echo "initdone"
