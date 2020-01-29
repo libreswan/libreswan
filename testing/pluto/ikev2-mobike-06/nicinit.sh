791a3278b003b48509effcff6b4c007024621ab9
Antony Antony <antony@phenome.org>
2020-01-29 07:22:40 +0000

    testing: mobike tests imporove the NAT sport dport sanitizing

--- testing/pluto/ikev2-mobike-06/nicinit.sh
+++ testing/pluto/ikev2-mobike-06/nicinit.sh
@@ -3,8 +3,12 @@ ip addr show dev eth1 | grep 192.1.2.250 || ip addr add 192.1.3.250/24 dev eth0
 iptables -t nat -F
 iptables -F
 iptables -X
-iptables -t nat -A POSTROUTING -p udp --sport 4500 -j SNAT --to-source 192.1.2.254:3503-3504
-iptables -t nat -A POSTROUTING -p udp --sport 500 -j SNAT --to-source 192.1.2.254:2503-2504
+# port for the first address
+iptables -t nat -A POSTROUTING -s 192.1.3.209 -p udp --sport 4500 -j SNAT --to-source 192.1.2.254:3503-3509
+iptables -t nat -A POSTROUTING -s 192.1.3.209 -p udp --sport 500 -j SNAT --to-source 192.1.2.254:2503-2509
+# for the second address use different port range
+iptables -t nat -A POSTROUTING -s 192.1.33.222 -p udp --sport 4500 -j SNAT --to-source 192.1.2.254:6503-6509
+iptables -t nat -A POSTROUTING -s 192.1.33.222 -p udp --sport 500 -j SNAT --to-source 192.1.2.254:5503-5509
 iptables -t nat -A POSTROUTING --destination 0.0.0.0/0 -j SNAT --to-source 192.1.2.254
 iptables -t nat -L -n
 iptables -L -n
--- testing/pluto/ikev2-mobike-06/roadinit.sh
+++ testing/pluto/ikev2-mobike-06/roadinit.sh
@@ -1,6 +1,4 @@
 /testing/guestbin/swan-prep
-echo "192.0.2.252/30" >> /etc/ipsec.d/policies/clear
-echo "192.1.3.252/30" >> /etc/ipsec.d/policies/clear
 ip route del default
 ip addr del 192.1.33.222/24 dev eth0 2>/dev/null
 sleep 2
