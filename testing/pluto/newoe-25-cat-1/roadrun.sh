b5a354f5d96f9ac1770c7b0ebd039bb8a92bef12
Andrew Cagney <cagney@gnu.org>
2020-02-04 13:35:53 -0500

    testing: update tests using wait-for-whack-trafficstatus.sh
    Switch to wait-for.sh (and delete wait-for-whack-trafficstatus.sh).
    Replace ping -c 4 with ping-once.sh; and update expected counters.

--- testing/pluto/newoe-25-cat-1/roadrun.sh
+++ testing/pluto/newoe-25-cat-1/roadrun.sh
@@ -5,7 +5,7 @@ ipsec whack --shuntstatus
 ../../pluto/bin/ipsec-look.sh
 # trigger OE
 ../../pluto/bin/ping-once.sh --down -I 192.1.3.209 192.1.2.23
-../../pluto/bin/wait-for-whack-trafficstatus.sh '"private-or-clear#192.1.2.0/24"'
+../../pluto/bin/wait-for.sh --match '"private-or-clear#192.1.2.0/24"' -- ipsec whack --trafficstatus
 # ping should succeed through tunnel
 ../../pluto/bin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
 ipsec whack --trafficstatus
