1987d81fba4ff4922c3a3b57bc699a356787525a
Andrew Cagney <cagney@gnu.org>
2020-02-04 21:56:03 -0500

    testing: cleanup newoe-05-hold-pass
    Use ping-once.sh --down ; wait-for.sh traffic status; ping-once.sh
    --up to bring up the OE tunnel.
    
    Replace wait 60 with wait-for.sh --match 7.7.7.7 shuntstatus.
    
    Add note wondering why the test ends with a bunch of trafficstatus /
    looks - each 60 seconds apart - that serve no obvious purpose.

--- testing/pluto/newoe-05-hold-pass/roadrun.sh
+++ testing/pluto/newoe-05-hold-pass/roadrun.sh
@@ -1,18 +1,24 @@
-ping -n -c 4 -I 192.1.3.209 192.1.2.23
-ping -n -c 2 -I 192.1.3.209 7.7.7.7
-# wait on OE retransmits and rekeying - shuntstatus is empty because shunt not bare
+# trigger OE
+../../pluto/bin/ping-once.sh --down -I 192.1.3.209 192.1.2.23
+../../pluto/bin/wait-for.sh --match '"private-or-clear#192.1.2.0/24"' -- ipsec whack --trafficstatus
+../../pluto/bin/ping-once.sh --up   -I 192.1.3.209 192.1.2.23
+# try shunt
+../../pluto/bin/ping-once.sh --down -I 192.1.3.209 7.7.7.7
+ipsec whack --shuntstatus
+# wait on OE retransmits and rekeying - shuntstatus is empty because
+# shunt 7.7.7.7 is not bare and its conn negotiationshunt=hold, so
+# ping should fail
 sleep 3
 ipsec whack --shuntstatus
 ipsec whack --trafficstatus
-# 7.7.7.7 shunt is not bare and its conn negotiationshunt=hold, so ping should fail
-ping -n -c 2 -I 192.1.3.209 7.7.7.7
-# letting acquire time out
-sleep 60
+../../pluto/bin/ping-once.sh --down -I 192.1.3.209 7.7.7.7
+# let acquire time out; shunt is now failureshunt=pass and should show
+# up, ping will work
+../../pluto/bin/wait-for.sh --timeout 60 --match 7.7.7.7 -- ipsec whack --shuntstatus
+../../pluto/bin/ping-once.sh --up -I 192.1.3.209 7.7.7.7
 ../../pluto/bin/ipsec-look.sh
-# conn timed out, shunt is now failureshunt=pass and should show up, ping will work
-ipsec whack --shuntstatus
-ping -n -c 2 -I 192.1.3.209 7.7.7.7
 # let failureshunt expire - both from bare shunt list as as kernel policy
+# XXX: what is below looking for?  Output doesn't seem to change.
 sleep 60
 ../../pluto/bin/ipsec-look.sh
 ipsec whack --shuntstatus
