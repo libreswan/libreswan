cf5d1b2e5747419a7a174e350226097dc91aac85
Paul Wouters <pwouters@redhat.com>
2020-02-06 19:19:41 -0500

    testing: fixup and document change for netkey-vti-06

--- testing/pluto/netkey-vti-06/final.sh
+++ testing/pluto/netkey-vti-06/final.sh
@@ -1,3 +1,4 @@
+# 4 dropped packets should be visible on east because road leaked them
 hostname | grep nic > /dev/null || ipsec whack --trafficstatus
 grep -v -P "\t0$" /proc/net/xfrm_stat
 : ==== cut ====
