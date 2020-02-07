b19dfceb557735b76c12cd17493a0dae9cc79a29
Paul Wouters <pwouters@redhat.com>
2020-02-07 15:05:12 -0500

    testing: removed newoe-23-reverse-nat
    The test was for pre-CAT and does not work anyway.

--- testing/pluto/newoe-23-reverse-nat/final.sh
+++
@@ -1,9 +0,0 @@
-# No tunnels should have established but a shunt should exist
-hostname | grep nic > /dev/null || ipsec whack --trafficstatus
-hostname | grep nic > /dev/null || ipsec whack --shuntstatus
-: ==== cut ====
-ipsec auto --status
-: ==== tuc ====
-../bin/check-for-core.sh
-if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
-: ==== end ====
