f50a079fd1c474fe99515d14259d268be8bbef1f
Paul Wouters <pwouters@redhat.com>
2020-02-07 16:21:32 -0500

    testing: minor fixups

--- testing/pluto/ikev2-24-cryptoload/final.sh
+++ testing/pluto/ikev2-24-cryptoload/final.sh
@@ -1,7 +1,5 @@
 ../../pluto/bin/ipsec-look.sh
 ipsec stop
-# on east ipsec stop will not work pluto is not started with ipsec start
-pidof pluto && kill `pidof pluto`
 grep -e leak /tmp/pluto.log | grep -v -e '|'
 : ==== cut ====
 ipsec auto --status
