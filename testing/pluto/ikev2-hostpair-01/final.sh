f901b828e089b426500ccbe885f092253fc9e3f6
Paul Wouters <pwouters@redhat.com>
2020-02-06 19:58:40 -0500

    testing: fixup ikev2-hostpair-01 to filter out ephemeral ike port displayed in ipsec status

--- testing/pluto/ikev2-hostpair-01/final.sh
+++ testing/pluto/ikev2-hostpair-01/final.sh
@@ -1,4 +1,4 @@
-ipsec status | grep eastnet
+ipsec status | grep eastnet | sed "s/192.1.2.254:[0-9]* STATE_/192.1.2.254:PORT STATE_/"
 # should show no hits
 grep INVALID_IKE_SPI /tmp/pluto.log
 grep MSG_TRUNC /tmp/pluto.log
