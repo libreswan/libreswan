ae05c8f900896f06e1ed36a79f6f3f9262331200
Paul Wouters <pwouters@redhat.com>
2020-02-07 11:22:08 -0500

    testing: clarify and fixup ikev2-ike-rekey-04

--- testing/pluto/ikev2-ike-rekey-04/westrun.sh
+++ testing/pluto/ikev2-ike-rekey-04/westrun.sh
@@ -1,19 +1,18 @@
 ipsec auto --up westnet-eastnet-ikev2a
 ipsec auto --up westnet-eastnet-ikev2b
 ipsec auto --up westnet-eastnet-ikev2c
-# ike #1 IPsec #2 #3 #4
+# expect: ike #1 IPsec #2 #3 #4
 ipsec status |grep STATE_
 sleep 30
 # ipsec should be rekeyed
-# ike #1 IPsec #5 #6 #7
+# expect: ike #1 IPsec #5 #6 #7
 ipsec status |grep STATE_
 sleep 20
 # ike sa should be rekeyed
-# ike #8 IPsec #5 #6 #7
+# expect: ike #8 IPsec #9 #10 #11
 ipsec status |grep STATE_
 sleep 30
 # second rekey of IPsec SAs
+# expect: ike #8 IPsec #12 #13 #14
 ipsec status |grep STATE_
-# ike #8 IPsec #9 #10 #11
-sleep 20
 echo done
