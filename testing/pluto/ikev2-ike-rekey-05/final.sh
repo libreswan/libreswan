4f159e6d0a36b9949ac885e0ad337322ce0500c1
Paul Wouters <pwouters@redhat.com>
2020-02-07 12:19:21 -0500

    testing: fixup ikev2-ike-rekey-05

--- testing/pluto/ikev2-ike-rekey-05/final.sh
+++ testing/pluto/ikev2-ike-rekey-05/final.sh
@@ -1,8 +1,8 @@
 ipsec whack --trafficstatus
 ipsec status |grep STATE_
 # there should be only one IKE_INIT exchange
-grep "STATE_PARENT_I1 with STF_OK" /tmp/pluto.log
-grep "STATE_PARENT_R1 with STF_OK" /tmp/pluto.log
+grep "STATE_PARENT_I1 to " /tmp/pluto.log
+grep "PARENT_R1 with status STF_OK" /tmp/pluto.log
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
