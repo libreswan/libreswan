d80e3589146d9df803daed15c216a53b0f37032c
Antony Antony <antony@phenome.org>
2020-02-13 10:13:58 +0000

    testing: ipv6-addresspool*  updown has support remove add addr
    now updown script has support for doroute6 remove dated comments and
    manual adding address and route.

--- testing/pluto/ipv6-addresspool-02-ikev2/roadrun.sh
+++ testing/pluto/ipv6-addresspool-02-ikev2/roadrun.sh
@@ -1,6 +1,3 @@
 ipsec auto --up road
-# ping will fail until we fix  up-client-v6 and add source address.
-ip -6  addr add 2001:db8:0:3:1::0/128 dev lo
-ip -6 route add 2001:db8:0:2::/64 via  2001:db8:1:3::254 src 2001:db8:0:3:1::0
 ping6 -c 2 -w 5 -I 2001:db8:0:3:1::0 2001:db8:0:2::254
 echo done
