5691b3d90a4085746a28b1747be7326846a6604e
Antony Antony <antony@phenome.org>
2020-02-16 09:40:17 +0000

    testing: fixes testoutput ikev2-55-ipseckey-* and dnssec

--- testing/pluto/dnssec-dig-test-1/westrun.sh
+++ testing/pluto/dnssec-dig-test-1/westrun.sh
@@ -1,6 +1,6 @@
-ping -q -c 1 192.1.2.254
-time dig east.testing.libreswan.org
-time dig east.testing.libreswan.org IPSECKEY
+../../pluto/bin/wait-until-alive 192.1.2.254
+dig east.testing.libreswan.org
+time dig +short east.testing.libreswan.org IPSECKEY
 time dig +short @192.1.2.254 east.testing.libreswan.org
 dig +short @192.1.2.254 chaos version.server txt
 dig +short @192.1.2.254 -p 5353 east.testing.libreswan.org
