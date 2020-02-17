1797ff8748a4734c5e5d404ca7178a42dc1123c6
Antony Antony <antony@phenome.org>
2020-02-17 15:12:30 +0000

    pluto: add Licence and Copyright to kernel_xfrm_interface.h

--- programs/pluto/kernel_xfrm_interface.h
+++ programs/pluto/kernel_xfrm_interface.h
@@ -1,3 +1,19 @@
+/*
+ * xfrmi declarations, linux kernel IPsec interface/device
+ *
+ * Copyright (C) 2018-2020 Antony Antony <antony@phenome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <https://www.gnu.org/licenses/gpl2.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
 #include <linux/if_link.h>
 #if defined(USE_XFRM_INTERFACE_IFLA_HEADER)
 /* kernel header linux/if_link.h < 4.19 may need this extra */
