11078ff830e6fa6bdf7f8789013b844680cdb3b1
Antony Antony <antony@phenome.org>
2020-02-20 15:46:38 +0000

    testing: ikev2-xfrmi-02-responder fix tcpdump permssion issues on kvm

--- testing/pluto/ikev2-xfrmi-02-responder/eastinit.sh
+++ testing/pluto/ikev2-xfrmi-02-responder/eastinit.sh
@@ -3,5 +3,6 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add east
 ipsec whack --impair revival
-tcpdump -s 0 -i eth1 -w OUTPUT/east.eth1.cap & echo $! > OUTPUT/east.tcpdump.pid
+rm -fr /tmp/east.ikev2-xfrmi-02-responder.pcap
+tcpdump -s 0 -i eth1 -w /tmp/east.ikev2-xfrmi-02-responder.pcap > /dev/nulll & echo $! > /tmp/east.ikev2-xfrmi-02-responder.pid
 echo "initdone"
--- testing/pluto/ikev2-xfrmi-02-responder/final.sh
+++ testing/pluto/ikev2-xfrmi-02-responder/final.sh
@@ -4,8 +4,10 @@ ip -s link show ipsec1
 ip rule show
 ip route show table 50
 ip route
-hostname | grep east > /dev/null && kill -TERM -p $(cat OUTPUT/east.tcpdump.pid) 
-hostname | grep east > /dev/null && tcpdump -n -r OUTPUT/east.eth1.cap not arp and not ip6
+: ==== cut ====
+hostname | grep east > /dev/null && kill -TERM -p $(cat /tmp/east.ikev2-xfrmi-02-responder.pid) ; (sleep 5; sync; cp /tmp/east.ikev2-xfrmi-02-responder.pcap OUTPUT/)
+: ==== tuc ====
+hostname | grep east > /dev/null && tcpdump -n -r OUTPUT/east.ikev2-xfrmi-02-responder.pcap not arp and not ip6 and not stp
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
