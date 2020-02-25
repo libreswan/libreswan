85ef2aeea748f13961c96ed80fc4e09a07541b77
Paul Wouters <pwouters@redhat.com>
2020-02-25 15:08:26 -0500

    testing: add a few ikev1-xfrmi-* test cases.

---
+++ testing/pluto/ikev1-xfrmi-04/final.sh
@@ -0,0 +1,11 @@
+ipsec whack --trafficstatus
+ip -s link show ipsec1
+ip rule show
+ip route show table 50
+ip route
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
