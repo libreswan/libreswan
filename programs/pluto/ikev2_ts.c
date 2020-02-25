libreswan/libreswan

commit ccd16ad4b62a26b369c725fcd3d3a3f489ac70e1

packets: merge pbs_in_address_{in,in6}() into pbs_in_address(...,ip_info,...)

programs/pluto/ikev2_ts.c
@@ -31,6 +31,7 @@
 #include "demux.h"
 #include "virtual.h"
 #include "hostpair.h"
+#include "ip_info.h"
 
 /*
  * While the RFC seems to suggest that the traffic selectors come in
@@ -292,34 +293,26 @@ static bool v2_parse_ts(struct payload_digest *const ts_pd,
 		if (!in_struct(&ts1, &ikev2_ts1_desc, &ts_pd->pbs, &addr))
 			return false;
 
+		const struct ip_info *ipv;
 		switch (ts1.isat1_type) {
 		case IKEv2_TS_IPV4_ADDR_RANGE:
-		{
-			ts->ts_type = IKEv2_TS_IPV4_ADDR_RANGE;
-			if (!pbs_in_address_in(&ts->net.start, &addr, "ipv4 ts low")) {
-				return false;
-			}
-			if (!pbs_in_address_in(&ts->net.end, &addr, "ipv4 ts high")) {
-				return false;
-			}
+			ipv = &ipv4_info;
 			break;
-		}
 		case IKEv2_TS_IPV6_ADDR_RANGE:
-		{
-			ts->ts_type = IKEv2_TS_IPV6_ADDR_RANGE;
-			if (!pbs_in_address_in6(&ts->net.start, &addr, "ipv4 ts low")) {
-				return false;
-			}
-			if (!pbs_in_address_in6(&ts->net.end, &addr, "ipv4 ts high")) {
-				return false;
-			}
+			ipv = &ipv6_info;
 			break;
-		}
-
 		default:
 			return false;
 		}
 
+		ts->ts_type = IKEv2_TS_IPV6_ADDR_RANGE;
+		if (!pbs_in_address(&ts->net.start, ipv, &addr, "TS low")) {
+			return false;
+		}
+		if (!pbs_in_address(&ts->net.end, ipv, &addr, "TS high")) {
+			return false;
+		}
+		/* XXX: does this matter? */
 		if (pbs_left(&addr) != 0)
 			return false;
 

programs/pluto/ikev1_xauth.c
@@ -1563,7 +1563,7 @@ static stf_status modecfg_inI2(struct msg_digest *md, pb_stream *rbody)
 			char caddr[SUBNETTOT_BUF];
 
 			ip_address a;
-			if (!pbs_in_address_in(&a, &strattr, "addr")) {
+			if (!pbs_in_address(&a, &ipv4_info, &strattr, "addr")) {
 				return STF_FATAL;
 			}
 			addrtosubnet(&a, &c->spd.this.client);
@@ -1721,7 +1721,7 @@ stf_status modecfg_inR1(struct state *st, struct msg_digest *md)
 				char caddr[SUBNETTOT_BUF];
 
 				ip_address a;
-				if (!pbs_in_address_in(&a, &strattr, "addr")) {
+				if (!pbs_in_address(&a, &ipv4_info, &strattr, "addr")) {
 					return STF_FATAL;
 				}
 				addrtosubnet(&a, &c->spd.this.client);
@@ -1751,7 +1751,7 @@ stf_status modecfg_inR1(struct state *st, struct msg_digest *md)
 				ipstr_buf b;
 
 				ip_address a;
-				if (!pbs_in_address_in(&a, &strattr, "addr")) {
+				if (!pbs_in_address(&a, &ipv4_info, &strattr, "addr")) {
 					return STF_FATAL;
 				}
 
@@ -1764,7 +1764,7 @@ stf_status modecfg_inR1(struct state *st, struct msg_digest *md)
 			case INTERNAL_IP4_DNS | ISAKMP_ATTR_AF_TLV:
 			{
 				ip_address a;
-				if (!pbs_in_address_in(&a, &strattr, "addr")) {
+				if (!pbs_in_address(&a, &ipv4_info, &strattr, "addr")) {
 					return STF_FATAL;
 				}
 

programs/pluto/packet.c
@@ -28,7 +28,7 @@
 #include "lswlog.h"
 #include "lswalloc.h"
 #include "impair.h"
-
+#include "ip_info.h"		/* used by pbs_in_address() */
 #include "packet.h"
 
 const pb_stream empty_pbs;
@@ -2724,24 +2724,31 @@ void close_output_pbs(pb_stream *pbs)
 		pbs->container->cur = pbs->cur; /* pass space utilization up */
 }
 
-bool pbs_in_address_in(ip_address *address, struct pbs_in *in_pbs, const char *what)
+bool pbs_in_address(ip_address *address, const struct ip_info *ipv,
+		    struct pbs_in *input_pbs, const char *what)
 {
-	struct in_addr ip;
-	if (!in_raw(&ip, sizeof(ip), in_pbs, what)) {
-		*address = address_invalid;
-		return false;
+	switch (ipv->af) {
+	case AF_INET:
+	{
+		struct in_addr ip;
+		if (!in_raw(&ip, sizeof(ip), input_pbs, what)) {
+			*address = address_invalid;
+			return false;
+		}
+		*address = address_from_in_addr(&ip);
+		return true;
 	}
-	*address = address_from_in_addr(&ip);
-	return true;
-}
-
-bool pbs_in_address_in6(ip_address *address, struct pbs_in *in_pbs, const char *what)
-{
-	struct in6_addr ip;
-	if (!in_raw(&ip, sizeof(ip), in_pbs, what)) {
-		*address = address_invalid;
-		return false;
+	case AF_INET6:
+	{
+		struct in6_addr ip;
+		if (!in_raw(&ip, sizeof(ip), input_pbs, what)) {
+			*address = address_invalid;
+			return false;
+		}
+		*address = address_from_in6_addr(&ip);
+		return true;
 	}
-	*address = address_from_in6_addr(&ip);
-	return true;
+	default:
+		bad_case(ipv->af);
+	}
 }

programs/pluto/packet.h
@@ -28,6 +28,8 @@
 #include "chunk.h"
 #include "ip_address.h"
 
+struct ip_info;
+
 /* a struct_desc describes a structure for the struct I/O routines.
  * This requires arrays of field_desc values to describe struct fields.
  */
@@ -221,13 +223,15 @@ extern bool in_struct(void *struct_ptr, struct_desc *sd,
 extern bool in_raw(void *bytes, size_t len, pb_stream *ins, const char *name) MUST_USE_RESULT;
 
 /*
- * Utilities, is there a better home?
+ * Utilities:
+ *
+ * XXX: is there a better place to be adding these in functions that
+ * build on the primitives?
  */
 
-bool pbs_in_address_in(ip_address *address, struct pbs_in *in_pbs,
-		       const char *what) MUST_USE_RESULT;
-bool pbs_in_address_in6(ip_address *address, struct pbs_in *in_pbs,
-			const char *what) MUST_USE_RESULT;
+bool pbs_in_address(ip_address *address, const struct ip_info *af,
+		    struct pbs_in *input_pbs,
+		    const char *WHAT) MUST_USE_RESULT;
 
 /*
  * Output PBS

commit d7701cab283cb669a4ff40d73392b6b58e549460

ikev2: fix ikev2 rekey
ikev2 child sa fail to rekey with strongswan and other implemenatnons

Fixes: ccd16ad4b (packets: merge pbs_in_address_{in,in6}())
Reported-by: Tuomo Soini <tis@foobar.fi>

programs/pluto/ikev2_ts.c
@@ -269,16 +269,17 @@ static bool v2_parse_ts(struct payload_digest *const ts_pd,
 		const struct ip_info *ipv;
 		switch (ts1.isat1_type) {
 		case IKEv2_TS_IPV4_ADDR_RANGE:
+			ts->ts_type = IKEv2_TS_IPV4_ADDR_RANGE;
 			ipv = &ipv4_info;
 			break;
 		case IKEv2_TS_IPV6_ADDR_RANGE:
+			ts->ts_type = IKEv2_TS_IPV6_ADDR_RANGE;
 			ipv = &ipv6_info;
 			break;
 		default:
 			return false;
 		}
 
-		ts->ts_type = IKEv2_TS_IPV6_ADDR_RANGE;
 		if (!pbs_in_address(&ts->net.start, ipv, &addr, "TS low")) {
 			return false;
 		}