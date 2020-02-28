916679807160342943a4856dd11545d39a7d5b95
Antony Antony <antony@phenome.org>
2020-02-28 10:42:51 +0000

    testing: fix interop-ikev2-strongswan-35-initiator-rekey
    use ipsec start instead of ipsec pluto
    stablize output with tactical sleep(15) for strongswan to delete old Child

--- testing/pluto/interop-ikev2-strongswan-35-initiator-rekey/westinit.sh
+++ testing/pluto/interop-ikev2-strongswan-35-initiator-rekey/westinit.sh
@@ -7,7 +7,7 @@ iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
 # confirm clear text does not get through
 ../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
-ipsec pluto
+ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair revival
 ipsec auto --add westnet-eastnet
--- testing/pluto/interop-ikev2-strongswan-35-initiator-rekey/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-35-initiator-rekey/westrun.sh
@@ -1,9 +1,12 @@
 ipsec auto --up westnet-eastnet
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
+ping -w 4 -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus 
 # does not work on 3.29 for now leave it timer based rekey
 ipsec whack --rekey-ipsec --name westnet-eastnet
-sleep 25
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
+sleep 15
+ping -w 4 -n -c 4 -I 192.0.1.254 192.0.2.254
+# only #3 should be there with traffic
 ipsec trafficstatus 
 ipsec status | grep westnet-eastnet
+# give time to strongswan to expire old one
+sleep 53
