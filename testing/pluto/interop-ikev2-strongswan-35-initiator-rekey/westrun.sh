0944ffa25fd7eb6afac402ee892d043ea0c0d26d
Antony Antony <antony@phenome.org>
2020-02-28 06:09:31 +0000

    testing: uncomment rekey interop-ikev2-strongswan-35-initiator-rekey

--- testing/pluto/interop-ikev2-strongswan-35-initiator-rekey/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-35-initiator-rekey/westrun.sh
@@ -2,7 +2,7 @@ ipsec auto --up westnet-eastnet
 ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus 
 # does not work on 3.29 for now leave it timer based rekey
-# ipsec whack --rekey-ipsec --name westnet-eastnet
+ipsec whack --rekey-ipsec --name westnet-eastnet
 sleep 25
 ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus 
