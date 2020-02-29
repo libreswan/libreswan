65d359d408302a1b1c91f252013c63da83f94fab
Andrew Cagney <cagney@gnu.org>
2020-02-29 11:21:57 -0500

    testing: don't expect a private key that isn't there
    In nss-cert-nosecret ipsec.secrets specifies the private key
    with CKAID:
    |   61 55 99 73  d3 ac ef 7d  3a 37 0e 3e  82 ad 92 c1
    |   8a 82 25 f1
    but that isn't in the NSS DB.

--- testing/pluto/nss-cert-nosecret/eastinit.sh
+++ testing/pluto/nss-cert-nosecret/eastinit.sh
@@ -1,5 +1,7 @@
 /testing/guestbin/swan-prep --x509
 certutil -d sql:/etc/ipsec.d -D -n west
+certutil -K -d sql:/etc/ipsec.d
+ipsec showhostkey --list
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add nss-cert
