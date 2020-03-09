9ede68112925c9a8a92c4086f05858979c1bcae8
Andrew Cagney <cagney@gnu.org>
2020-03-09 10:42:42 -0400

    testing: expect state #2 when responding to strongswan, state #1 was INVALID_KE

--- testing/pluto/interop-ikev2-strongswan-37-initiator-no-digsig/final.sh
+++ testing/pluto/interop-ikev2-strongswan-37-initiator-no-digsig/final.sh
@@ -1,4 +1,5 @@
 if [ -f /var/run/pluto/pluto.pid ]; then ../../pluto/bin/ipsec-look.sh ; fi
+# expect state #2, state #1 responded with INVALID_KE
 if [ -f /var/run/pluto/pluto.pid ]; then grep "authenticated using RSA" /tmp/pluto.log ; fi
 if [ -f /var/run/charon.pid -o -f /var/run/strongswan/charon.pid ]; then strongswan status ; fi
 : ==== cut ====
