a5c72337496f5dddb216dc83745a77740baaf37d
Antony Antony <antony@phenome.org>
2020-04-01 19:56:17 +0000

    testing: cleanup ikev2-68-sa-clones-null-iperf rename to ikev2-67-esp-null-iperf

--- testing/pluto/ikev2-67-esp-null-iperf/eastinit.sh
+++ testing/pluto/ikev2-67-esp-null-iperf/eastinit.sh
@@ -7,9 +7,9 @@ ipsec whack --impair ikev2-include-integ-none
 ipsec auto --add west-east
 ipsec auto --status | grep west-east
 ipsec whack --impair suppress-retransmits
-taskset 0x1 iperf3 -s -p 5001 & echo "$!"  > $(hostname)-iperf3-5001.pid
+taskset 0x1 iperf3 -s -p 5001 & echo "$!"  > OUTPUT/$(hostname)-iperf3-5001.pid
 cd /var/tmp/
-perf record -g -e cycles --call-graph dwarf & echo "$!"  > $(hostname)-perf.pid
-#taskset 0x2 iperf3 -s -p 5002 &
-#taskset 0x3 iperf3 -s -p 5003 &
+# perf record -g -e cycles --call-graph dwarf & echo "$!"  > $(hostname)-perf.pid
+# taskset 0x1 iperf3 -s -p 5002 &
+# taskset 0x3 iperf3 -s -p 5003 &
 echo "initdone"
---
+++ testing/pluto/ikev2-67-esp-null-iperf/westrun.sh
@@ -0,0 +1,16 @@
+ipsec auto --status | grep west-east
+ipsec auto --up west-east
+# taskset 0x1 ping -n -c 2 192.1.2.23
+ipsec trafficstatus
+# base line singe flow
+taskset 0x1 iperf3 -t 45 -i 2 -c 192.1.2.23 -p 5001
+# cd /var/tmp/
+# perf record -g -e cycles --call-graph dwarf & echo "$!"  > OUTPUT/$(hostname)-perf.pid
+# iperf3 -t 180 -i 2 -c 192.1.2.23 -p 5001
+ipsec status | grep west-east
+ipsec auto --down west-east
+ipsec auto --delete west-east
+ipsec auto --initiate west-east-null
+taskset 0x1 iperf3 -t 45 -i 2 -c 192.1.2.23 -p 5001
+ipsec status | grep west-east-null
+echo done
