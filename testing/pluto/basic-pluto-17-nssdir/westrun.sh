c011adf7ec496d5c1176993c6b6298b62fd17279
Paul Wouters <pwouters@redhat.com>
2020-04-01 21:15:01 -0400

    testing: added basic-pluto-17-nssdir

---
+++ testing/pluto/basic-pluto-17-nssdir/westrun.sh
@@ -0,0 +1,11 @@
+IPSEC_DIR=/etc/ipsec.d/
+IPSEC_TEST_DIR=/tmp/TESTipsec.d
+mkdir ${IPSEC_TEST_DIR}
+rm -f ${IPSEC_DIR}/*.db
+ipsec initnss --nssdir ${IPSEC_TEST_DIR}
+ls ${IPSEC_DIR} | egrep '*.db' # should show no db files
+ls ${IPSEC_TEST_DIR} | egrep '*.db' # should show db files
+ipsec checknss --nssdir ${IPSEC_TEST_DIR}
+ls ${IPSEC_DIR} | egrep '*.db' # should show no db files
+ls ${IPSEC_TEST_DIR} | egrep '*.db' # should show db files
+rm -rf ${IPSEC_TEST_DIR}
