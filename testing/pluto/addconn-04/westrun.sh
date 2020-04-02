fe9b5b2f5c3cd8a3d47084d847cf330a8837cd8e
Paul Wouters <pwouters@redhat.com>
2020-04-02 16:25:52 -0400

    testing: extend addconn-04 with test for infinite recursion.

--- testing/pluto/addconn-04/westrun.sh
+++ testing/pluto/addconn-04/westrun.sh
@@ -4,4 +4,11 @@ ipsec status | grep westnet-eastnet-ipv4-psk-ikev2 | grep "eroute owner"
 ipsec status | grep configfile
 # rhbz#1645137 test
 ipsec addconn --config /tmp/test.conf longike
+ipsec whack --shutdown # stop will cause reading /etc/ipsec.conf
+# rhbz#1625303 test
+cp bomb.conf /etc/ipsec.conf
+cp include-bomb.conf /etc/ipsec.d/
+restorecon /etc/ipsec.conf /etc/ipsec.d/include-bomb.conf
+# should fail properly at maxdepth recursion
+ipsec addconn --verbose --config /etc/ipsec.conf --checkconfig
 echo done
