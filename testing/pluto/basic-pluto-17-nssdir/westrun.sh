eb0110e6805428b815fd9de625a4157515fd066e
Paul Wouters <pwouters@redhat.com>
2020-04-01 21:41:29 -0400

    testing: basic-pluto-17-nssdir also test for F4 modulus for new raw keys

--- testing/pluto/basic-pluto-17-nssdir/westrun.sh
+++ testing/pluto/basic-pluto-17-nssdir/westrun.sh
@@ -8,4 +8,7 @@ ls ${IPSEC_TEST_DIR} | egrep '*.db' # should show db files
 ipsec checknss --nssdir ${IPSEC_TEST_DIR}
 ls ${IPSEC_DIR} | egrep '*.db' # should show no db files
 ls ${IPSEC_TEST_DIR} | egrep '*.db' # should show db files
+ipsec newhostkey --nssdir ${IPSEC_TEST_DIR} --output ${IPSEC_TEST_DIR}/test.secrets > /dev/null 2> /dev/null
+# should show F4 (0x010001) is used for new keys
+grep "PublicExponent: 0x010001" ${IPSEC_TEST_DIR}/test.secrets
 rm -rf ${IPSEC_TEST_DIR}
