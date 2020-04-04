9d6040831c59f36f1d485879234ca4b44fb94e91
Antony Antony <antony@phenome.org>
2020-04-04 18:11:24 +0000

    testing: ikev2-child-rekey-06-deadlock remove iptable LOG
    -j LOG is inconsitant between namespace and kvm testrun.
    It is not really necessary, so comment it. It could be handy to run the
    test manually.

--- testing/pluto/ikev2-child-rekey-06-deadlock/westinit.sh
+++ testing/pluto/ikev2-child-rekey-06-deadlock/westinit.sh
@@ -10,7 +10,7 @@ ipsec auto --add west
 # Drop the second IPsec rekey message, which is IKE Message ID: 4
 # Message ID : 0 = IKE_INIT, 1 = IKE_AUTH, 2 = REKEY (First one let it go)
 # 3 : DELETE, 4 = REKEY (DROP)
-iptables -A INPUT -m u32 --u32 '0x6 & 0xFF = 0x11 && 0x30 & 0xFFFFFFFF = 0x4' -j LOG --log-prefix  "ike"
+# iptables -A INPUT -m u32 --u32 '0x6 & 0xFF = 0x11 && 0x30 & 0xFFFFFFFF = 0x4' -j LOG --log-prefix  "ike"
 iptables -A INPUT -m u32 --u32 '0x6 & 0xFF = 0x11 && 0x30 & 0xFFFFFFFF = 0x4' -j DROP
 sleep 4
 echo "initdone"
