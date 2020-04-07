f0af14d03ef0675ba881019fae516c78d500e299
Paul Wouters <pwouters@redhat.com>
2020-04-07 16:37:41 -0400

    testing: add ikev2-x509-39-OU-comma

---
+++ testing/pluto/ikev2-x509-39-OU-comma/westinit.sh
@@ -0,0 +1,9 @@
+/testing/guestbin/swan-prep
+rm -f /etc/ipsec.d/*.*
+ipsec initnss > /dev/null 2> /dev/null
+pk12util -i /testing/x509/nss/client.p12 -d sql:/etc/ipsec.d -K 'foobar' -W 'foobar'
+certutil -M -d sql:/etc/ipsec.d -n cacert -t 'CT,,'
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add x509
+echo "initdone"
---
+++ testing/pluto/ikev2-x509-39-OU-comma/westrun.sh
@@ -0,0 +1,4 @@
+ipsec status |grep OU=
+ipsec auto --listall |grep OU=
+ipsec auto --up x509
+echo "done"
