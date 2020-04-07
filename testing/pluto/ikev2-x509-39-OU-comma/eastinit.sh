3cc8d3adfe2192f0c879811af68b82e6867e9291
Paul Wouters <pwouters@redhat.com>
2020-04-07 17:09:36 -0400

    testing: ikev2-x509-39-OU-comma fixup

--- testing/pluto/ikev2-x509-39-OU-comma/eastinit.sh
+++ testing/pluto/ikev2-x509-39-OU-comma/eastinit.sh
@@ -1,7 +1,7 @@
 /testing/guestbin/swan-prep
 # always zap and recreate special comma certs
-/testing/x509/nss-certs.sh
 rm /etc/ipsec.d/*.*
+/testing/x509/nss-certs.sh
 ipsec initnss > /dev/null 2> /dev/null
 pk12util -i /testing/x509/nss/server.p12 -d sql:/etc/ipsec.d -K 'foobar' -W 'foobar'
 certutil -M -d sql:/etc/ipsec.d -n cacert -t 'CT,,'
