2cda4e9651d78f4e0e47db75b511419b21a39e27
Antony Antony <antony@phenome.org>
2020-04-18 10:08:43 +0000

    testing: add ikev2-child-rekey-09-windows

---
+++ testing/pluto/ikev2-child-rekey-09-windows/roadinit.sh
@@ -0,0 +1,7 @@
+/testing/guestbin/swan-prep --x509
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add road-east-x509-ipv4
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
+echo "initdone"
