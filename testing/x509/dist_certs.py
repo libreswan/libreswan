751559afcc0da9f525c572307aa13cdf35cbf753
Paul Wouters <pwouters@redhat.com>
2020-04-21 00:13:13 -0400

    testing: add some different size keys to dist_certs.py

--- testing/x509/dist_certs.py
+++ testing/x509/dist_certs.py
@@ -446,13 +446,15 @@ def create_mainca_end_certs(mainca_end_certs):
     for name in mainca_end_certs:
         # put special cert handling here
         print(" - creating %s"% name)
+        keysize = 3072
         if name == 'smallkey':
             keysize = 1024
-        else:
-            if name == 'key4096':
-                keysize = 4096
-            else:
-                keysize = 3072
+        if name == 'mediumkey':
+            keysize = 2048
+        if name == 'key2032':
+            keysize = 2032
+        if name == 'key4096':
+            keysize = 4096
 
         if name == 'notyetvalid':
             startdate = dates['FUTURE']
@@ -890,7 +892,7 @@ def run_dist_certs():
                         'west-ekuCritical-eku-emailProtection', 'east-ekuCritical-eku-emailProtection', # Should still work
                         'usage-server', 'usage-client', 'usage-both',
                         'nic-noext', 'nic-nourl',
-                        'smallkey', 'key4096',
+                        'smallkey', 'mediumkey', 'key2032', 'key4096',
                         'notyetvalid','notvalidanymore',
                         'signedbyother','otherwest','othereast','wrongdnorg',
                         'unwisechar','spaceincn','hashsha1',
