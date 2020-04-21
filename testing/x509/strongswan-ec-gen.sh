71c94a23ccde1c1f2cd79b52fb02bb8cecde0f70
Paul Wouters <pwouters@redhat.com>
2020-04-21 13:44:39 -0400

    testing: fixup x509/strongswan-ec-gen.sh

--- testing/x509/strongswan-ec-gen.sh
+++ testing/x509/strongswan-ec-gen.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+PKI="/usr/libexec/strongswan/pki"
+#PKI="/usr/local/strongswan/bin/pki"
+
 libreswandir=$(cd $(dirname $(realpath -m $0)); cd ../..; pwd)
 cd ${libreswandir}/testing/x509
 rm -f strongswan/*
 mkdir -p strongswan
 cd strongswan
-echo "ignore strongswan.conf errors"
-strongswan pki --gen --type ecdsa --size 384 > strongCAkey.der
-strongswan pki --self --in strongCAkey.der --dn "C=CH, O=strongSwan, CN=strongSwan CA" --ca > strongCAcert.der
-strongswan pki --gen --type ecdsa --size 384 > strongWestKey.der
-strongswan pki --pub --in strongWestKey.der | strongswan pki --issue --cacert strongCAcert.der --cakey strongCAkey.der --dn "C=CH, O=strongSwan, CN=strongWest" --flag serverAuth --san west.testing.libreswan.org > strongWestCert.der
-strongswan pki --gen --type ecdsa --size 384 > strongEastKey.der
-strongswan pki --pub --in strongEastKey.der | strongswan pki --issue --cacert strongCAcert.der --cakey strongCAkey.der --dn "C=CH, O=strongSwan, CN=strongEast" --flag serverAuth --san east.testing.libreswan.org > strongEastCert.der
+
+$PKI --gen --type ecdsa --size 384 > strongCAkey.der
+$PKI --self --in strongCAkey.der --dn "C=CH, O=strongSwan, CN=strongSwan CA" --ca > strongCAcert.der
+$PKI --gen --type ecdsa --size 384 > strongWestKey.der
+$PKI --pub --in strongWestKey.der | $PKI --issue --cacert strongCAcert.der --cakey strongCAkey.der --dn "C=CH, O=strongSwan, CN=strongWest" --flag serverAuth --san west.testing.libreswan.org > strongWestCert.der
+$PKI --gen --type ecdsa --size 384 > strongEastKey.der
+$PKI --pub --in strongEastKey.der | $PKI --issue --cacert strongCAcert.der --cakey strongCAkey.der --dn "C=CH, O=strongSwan, CN=strongEast" --flag serverAuth --san east.testing.libreswan.org > strongEastCert.der
 openssl x509 -inform der -outform pem -in strongCAcert.der -out strongCAcert.pem
 openssl x509 -inform der -outform pem -in strongWestCert.der -out strongWestCert.pem
 openssl x509 -inform der -outform pem -in strongEastCert.der -out strongEastCert.pem
