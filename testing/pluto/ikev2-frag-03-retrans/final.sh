42b35d7d71e0e8c2a3994c1c6b86f5be5d27f30d
Paul Wouters <pwouters@redhat.com>
2020-04-23 17:08:31 -0400

    testing: fixup ikev2-frag-03-retrans for last_contact addition.

--- testing/pluto/ikev2-frag-03-retrans/final.sh
+++ testing/pluto/ikev2-frag-03-retrans/final.sh
@@ -1,5 +1,5 @@
 # On east this shows the duplicates on west there is nothing.
-grep "received duplicate [^ ]* message request .* fragment" /tmp/pluto.log
+grep "received duplicate [^ ]* message request .* fragment" /tmp/pluto.log | sed "s/last_contact=.[^ ]* /last_contact=XXX /g"
 ../../pluto/bin/ipsec-look.sh
 : ==== cut ====
 ipsec auto --status
