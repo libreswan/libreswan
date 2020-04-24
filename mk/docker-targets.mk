libreswan/libreswan

commit b3f3c3806e1fc68e5170172005eaa0c44605adf0

testing: travis podman fixes to build on Fedora 32

mk/docker-targets.mk
@@ -31,6 +31,8 @@ D_USE_NSS_AVA_COPY ?= true
 
 DOCKERFILE ?= $(D)/dockerfile
 
+NOGPGPCHECK ?= false
+
 SUDO_CMD ?= sudo
 PKG_CMD = $(shell test -f /usr/bin/dnf && echo /usr/bin/dnf || (test -f /usr/bin/yum && echo /usr/bin/yum || echo "no yum or dnf found" && exit 1))
 PKG_BUILDDEP = $(shell test -f /usr/bin/dnf && echo "/usr/bin/dnf builddep" || echo /usr/bin/yum-builddep )
@@ -97,7 +99,8 @@ endif
 
 ifeq ($(DISTRO), fedora)
 	ifeq ($(DISTRO_REL), rawhide)
-		TWEAKS += rawhide-remove-dnf-update
+		# TWEAKS += rawhide-remove-dnf-update
+		TWEAKS += dnf-nogpgcheck
 	endif
 
 	MAKE_BASE = base
@@ -105,10 +108,19 @@ ifeq ($(DISTRO), fedora)
 	LOCAL_MAKE_FLAGS =
 endif
 
+ifeq ($(NOGPGPCHECK), true)
+	TWEAKS += dnf-nogpgcheck
+endif
+
 .PHONY: rawhide-remove-dnf-update
 rawhide-remove-dnf-update:
 	# on rawhide RUN dnf -y update could be a bad idea
 	$(shell sed -i '/RUN dnf -y update/d' testing/docker/dockerfile)
+
+.PHONY: dnf-nogpgcheck
+dnf-nogpgcheck:
+	$(shell sed -i 's/dnf install -y /dnf install --nogpgcheck -y /' testing/docker/dockerfile)
+	$(shell sed -i 's/dnf update -y/dnf update -y --nogpgcheck /' testing/docker/dockerfile)
 
 .PHONY: dockerfile-remove-libreswan-spec
 dockerfile-remove-libreswan-spec:
@@ -199,8 +211,9 @@ travis-ubuntu-xenial: ubuntu-xenial-packages
 .PHONY: docker-build
 docker-build: dockerfile
 	# --volume is only in podman
-	# $(DOCKER_CMD) build -t $(DI_T) --volume /home/build/libreswan:/home/build/libreswan -f $(DOCKERFILE) .
-	$(DOCKER_CMD) build -t $(DI_T) -f $(DOCKERFILE) .
+	$(DOCKER_CMD) build -t $(DI_T) --volume /home/build/libreswan:/home/build/libreswan -f $(DOCKERFILE) .
+	# for docker
+	# $(DOCKER_CMD) build -t $(DI_T) -f $(DOCKERFILE) .
 
 .PHONY: docker-ssh-image
 docker-ssh-image: DOCKERFILE_SSH = $(D)/Dockerfile-swan-ssh

testing/docker/Dockerfile-fedora-min-packages
@@ -1,11 +1,12 @@
 # FROM fedora:27
 # MAINTAINER "Antony Antony" <antony@phenome.org>
 # ENV container docker
-RUN dnf -y update
+RUN dnf update -y
 # RUN mkdir -p /home/build/libreswan
 RUN mkdir -p /home/build/
-COPY . /home/build/libreswan
-RUN dnf -y install systemd; \
+# next line for docker not for podman
+# COPY . /home/build/libreswan
+RUN dnf install -y systemd; \
 (cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done); \
 rm -f /lib/systemd/system/multi-user.target.wants/*;\
 rm -f /etc/systemd/system/*.wants/*;\

testing/libvirt/rawhide.mk
@@ -82,7 +82,6 @@ KVM_UPGRADE_PACKAGES ?= \
     pam-devel \
     patch \
     perf \
-    pexpect \
     policycoreutils-python-utils \
     psmisc \
     python3-pyOpenSSL \

commit cedd2e9cd4ae420c5f3bd6ec82df5063f8375cc8

testing: fix travis broke in b3f3c3806e1fc

mk/docker-targets.mk
@@ -211,7 +211,7 @@ travis-ubuntu-xenial: ubuntu-xenial-packages
 .PHONY: docker-build
 docker-build: dockerfile
 	# --volume is only in podman
-	$(DOCKER_CMD) build -t $(DI_T) --volume /home/build/libreswan:/home/build/libreswan -f $(DOCKERFILE) .
+	$(DOCKER_CMD) build -t $(DI_T) --volume $(PWD):/home/build/libreswan -f $(DOCKERFILE) .
 	# for docker
 	# $(DOCKER_CMD) build -t $(DI_T) -f $(DOCKERFILE) .
 