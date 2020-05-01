f66b9410ed4fb163a66ffe5b382d70b711dcf989
Andrew Cagney <cagney@gnu.org>
2020-04-30 21:41:50 -0400

    testing: give liveness tests more sleep time

--- testing/pluto/ikev2-liveness-01/westrun.sh
+++ testing/pluto/ikev2-liveness-01/westrun.sh
@@ -1,16 +1,13 @@
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
-# sleep for 30s to run a few liveness cycles
-sleep 15
-sleep 15
-sleep 15
-sleep 15
-sleep 15
+# sleep for a few seconds to run a few liveness cycles
+sleep 20
 # kill pluto; host may send ICMP unreachble. with iptables it won't
 ipsec whack --impair send-no-delete
 ipsec stop
-# give east time to trigger dpd, which shows up in final.sh
+# give dpdtimeout=30 time to trigger dpd, which shows up in final.sh
 sleep 20
 sleep 20
+../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 echo done
--- testing/pluto/ikev2-liveness-03/westrun.sh
+++ testing/pluto/ikev2-liveness-03/westrun.sh
@@ -1,11 +1,14 @@
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
-# sleep for 60s to run a few liveness cycles
-sleep 30
-sleep 30
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+# sleep for a bit to run a few liveness cycles
+sleep 20
 ipsec whack --impair send-no-delete
 ipsec auto --delete westnet-eastnet-ipv4-psk-ikev2
 ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
-# sleep for timeout action
-sleep 30
+# sleep for more than timeout action - 60 seconds
+sleep 20
+sleep 20
+sleep 20
+sleep 20
+../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 echo done
