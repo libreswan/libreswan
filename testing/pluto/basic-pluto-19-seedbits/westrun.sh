594b1975950773932d96159eb63c6590f3bec76a
Paul Wouters <pwouters@redhat.com>
2020-05-01 11:08:37 -0400

    testing: add relto checking to basic-pluto-19-seedbits

--- testing/pluto/basic-pluto-19-seedbits/westrun.sh
+++ testing/pluto/basic-pluto-19-seedbits/westrun.sh
@@ -13,3 +13,6 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec stop
 egrep "bits random|bytes from|seeded" /tmp/pluto.log
+test -f /usr/local/libexec/ipsec/pluto && PLUTOBIN="/usr/local/libexec/ipsec/pluto"
+test -f /usr/libexec/ipsec/pluto && PLUTOBIN="/usr/libexec/ipsec/pluto"
+./checksec.sh --file $PLUTOBIN
