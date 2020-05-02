96764885e9f867fcf2b724017a66a502604cfd80
Andrew Cagney <cagney@gnu.org>
2020-05-02 10:03:31 -0400

    ikev2 msgids: don't scribble on the kill event when queueing a request
    Fixes bug where queueing a liveness request would stomp on an IKE SA's
    DISCARD (per connection) changing it to REPLACE with a short timeout
    (test?).
    
    The call is old.  Assume it was to triger a timeout when the
    pending exchange failed?  The retransmit code should handle that now.

--- programs/pluto/ikev2_msgid.c
+++ programs/pluto/ikev2_msgid.c
@@ -623,8 +623,6 @@ void v2_msgid_queue_initiator(struct ike_sa *ike, struct state *st,
 	 * v2_CREATE_CHILD_SA append to last the task.
 	 * v2_INFORMATIONAL v2D append to the last v2D task.
 	 */
-	delete_event(st);
-	event_schedule(EVENT_SA_REPLACE, MAXIMUM_RESPONDER_WAIT_DELAY, st);
 	/* find the end; small list? */
 	struct v2_msgid_pending **pp = &initiator->pending;
 	while (*pp != NULL) {
