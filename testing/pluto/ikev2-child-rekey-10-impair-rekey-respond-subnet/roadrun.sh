88b9dd76af93009c9584f5ace5361c713f4ec919
Paul Wouters <pwouters@redhat.com>
2020-05-04 22:28:31 -0400

    testing: extract fixups from 743cb8de5 for impair-rekey tests

--- testing/pluto/ikev2-child-rekey-10-impair-rekey-respond-subnet/roadrun.sh
+++ testing/pluto/ikev2-child-rekey-10-impair-rekey-respond-subnet/roadrun.sh
@@ -1,8 +1,8 @@
-ping -q -n -c 2 192.1.2.23
+../../pluto/bin/ping-once.sh --up 192.1.2.23
 ipsec auto --up road-east-x509-ipv4
-ping -q -n -c 4 -I 192.0.2.100 192.1.2.23
+../../pluto/bin/ping-once.sh --up -I 192.0.2.100 192.1.2.23
 ipsec whack --trafficstatus
-ipsec whack --rekey-ipsec --name road-east-x509-ipv4
+ipsec whack --rekey-ipsec --name road-east-x509-ipv4 --async
 echo "sleep 40 seconds"
 sleep 40
 echo done
