192e8fa8f3c91ed342e816340221687eff88d335
Paul Wouters <pwouters@redhat.com>
2020-05-25 14:59:12 -0400

    testing: fixup basic-pluto-19-seedbits
    also added test for type=block, which we surprisingly didn't have anywhere

--- testing/pluto/basic-pluto-19-seedbits/westrun.sh
+++ testing/pluto/basic-pluto-19-seedbits/westrun.sh
@@ -11,6 +11,9 @@ egrep "bits random|bytes from|seeded" /tmp/pluto.log
 sed -i "s/seedbits=.*$/seedbits=2048/" /etc/ipsec.conf
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
+sleep 10
+# this ping should fail due to the type=block connection
+ping -c 1 192.1.2.23
 ipsec stop
 egrep "bits random|bytes from|seeded" /tmp/pluto.log
 test -f /usr/local/libexec/ipsec/pluto && PLUTOBIN="/usr/local/libexec/ipsec/pluto"
