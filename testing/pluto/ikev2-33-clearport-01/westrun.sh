bb7b8e2bb30fa9e31f4fbecf62ff5b2758f8b369
Paul Wouters <pwouters@redhat.com>
2020-05-25 23:15:32 -0400

    testing: cleanup ikev2-33-clearport-01, show ip xfrm pol

--- testing/pluto/ikev2-33-clearport-01/westrun.sh
+++ testing/pluto/ikev2-33-clearport-01/westrun.sh
@@ -2,6 +2,7 @@
 ipsec auto --route west-east
 # poke a hole to port 222, those packets will be allowed cleartext
 ipsec auto --route pass-222
+ip xfrm pol
 # send packet over the clear exception - should return connection refused
 echo 'test' | nc -v -w 5 192.1.2.23 222
 # send packet over the 'tunnel' that's negotiating - shoudl get blocked
