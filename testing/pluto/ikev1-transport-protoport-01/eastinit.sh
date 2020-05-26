9950d8748db1704155005dc5f2a08716a0c16cd6
Paul Wouters <pwouters@redhat.com>
2020-05-26 00:01:31 -0400

    testing: added ikev1-transport-protoport-01

---
+++ testing/pluto/ikev1-transport-protoport-01/eastinit.sh
@@ -0,0 +1,5 @@
+/testing/guestbin/swan-prep
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add ipv4-psk-ikev2-transport
+echo "initdone"
---
+++ testing/pluto/ikev1-transport-protoport-01/westrun.sh
@@ -0,0 +1,2 @@
+ipsec auto --up ipv4-psk-ikev2-transport
+echo done
