4318249f3a7daee23aec43af63e28b2bb68be9df
Paul Wouters <pwouters@redhat.com>
2020-05-26 22:19:36 -0400

    testing: use fips-11-ikev2-esp-dh to test sha2_512 which wasn't tested elsewhere in fips mode

--- testing/pluto/fips-11-ikev2-esp-dh/westrun.sh
+++ testing/pluto/fips-11-ikev2-esp-dh/westrun.sh
@@ -1,5 +1,6 @@
 ipsec auto --add ikev2-base
 ipsec auto --up ikev2-base
 ping -n -c 4 -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
 ../bin/libreswan-up-down.sh ikev2-esp=aes-sha1-modp4096 -I 192.0.100.254 192.0.200.254
 ipsec auto --delete ikev2-base
