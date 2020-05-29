93decb1c17022347d6f7ac94b45e4ea465771884
Paul Wouters <pwouters@redhat.com>
2020-05-28 23:47:22 -0400

    testing: libipsecconf-04 - added some %fromcert testing

--- testing/pluto/libipsecconf-04/westrun.sh
+++ testing/pluto/libipsecconf-04/westrun.sh
@@ -2,4 +2,8 @@ ipsec auto --add first
 ipsec auto --add second
 # conn second should inherit the conn %default values with 3des-md5
 ipsec status |grep "algorithms:"
+# connection should fail to load - don't accept %fromcert without cert
+ipsec auto --add cert-complain
+# this one should work as %fromcert means for the CERT received with IKE
+ipsec auto --add cert-allow
 echo done
