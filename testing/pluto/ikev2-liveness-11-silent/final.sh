ebb3c9f20042c1abb4c006b5cecd4944044aead2
Andrew Cagney <cagney@gnu.org>
2020-05-30 09:14:54 -0400

    testing: tweak liveness output

--- testing/pluto/ikev2-liveness-11-silent/final.sh
+++ testing/pluto/ikev2-liveness-11-silent/final.sh
@@ -1,5 +1,5 @@
 # Confirm expired IPsec SA will not trigger a liveness probe
-grep "liveness: no need to send"  /tmp/pluto.log
+grep "liveness: .* was replaced by "  /tmp/pluto.log
 : ==== cut ====
 # stop pluto so if test case is ran manually and left, no legit liveness probes are done
 ipsec stop
