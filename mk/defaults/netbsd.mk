c0b1f18935aec5753e2103783f043085a6b53811
Andrew Cagney <cagney@gnu.org>
2020-05-31 15:10:20 -0400

    initsystems: add rc.d
    Perhaps initsystems/rc.d/Makefile should be used as the model
    of the other directories - it is much simpler.

--- mk/defaults/netbsd.mk
+++ mk/defaults/netbsd.mk
@@ -1,24 +1,32 @@
-USERLAND_CFLAGS += -I/usr/pkg/include
+# sketch out pkgsrc
+LOCALBASE ?= /usr/pkg
+PKG_BASE ?= /usr/pkg
+PKG_DBDIR ?= /var/db/pkg
+PKG_PATH ?= /usr/pkgsrc/packages/All
+
 USERLAND_CFLAGS += -DHAS_SUN_LEN
 USERLAND_CFLAGS += -DNEED_SIN_LEN
 USERLAND_CFLAGS += -DHAVE_NETINET6_IN_H
 USERLAND_CFLAGS += -Ds6_addr16=__u6_addr.__u6_addr16
 USERLAND_CFLAGS += -Ds6_addr32=__u6_addr.__u6_addr32
 
-USE_BSDKAME=true
-USE_LIBCAP_NG=false
+USERLAND_INCLUDES += -I$(PKG_BASE)/include
 
-USERLAND_LDFLAGS +=  -lipsec -L/usr/pkg/lib -Wl,-rpath,/usr/pkg/lib
+USERLAND_LDFLAGS += -lipsec -L$(PKG_BASE)/lib -Wl,-rpath,$(PKG_BASE)/lib
 
-NSS_CFLAGS = -I/usr/pkg/include/nspr -I/usr/pkg/include/nss/nss
-NSS_LDFLAGS = -L/usr/pkg/lib/nss -Wl,-rpath,/usr/pkg/lib/nss -lnss3 -lfreebl3 -lssl3
-NSPR_LDFLAGS = -L/usr/pkg/lib/nspr -Wl,-rpath,/usr/pkg/lib/nspr -lnspr4
+NSS_CFLAGS = -I$(PKG_BASE)/include/nspr -I$(PKG_BASE)/include/nss/nss
+NSS_LDFLAGS = -L$(PKG_BASE)/lib/nss -Wl,-rpath,$(PKG_BASE)/lib/nss -lnss3 -lfreebl3 -lssl3
+NSPR_LDFLAGS = -L$(PKG_BASE)/lib/nspr -Wl,-rpath,$(PKG_BASE)/lib/nspr -lnspr4
 
 CRYPT_LDFLAGS =
 
-INITSYSTEM=
+USE_BSDKAME = true
+USE_LIBCAP_NG = false
 
-FINAL_PAM_D_DIR=/usr/local/etc/pam.d
+INITSYSTEM=rc.d
+
+# not /run/pluto
+FINALRUNDIR=/var/run/pluto
+
 FINALSYSCONFDIR=/usr/local/etc
-FINALRUNDIR=/usr/local/run/pluto
 FINALNSSDIR=/usr/local/etc/ipsec.d
