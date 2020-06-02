f3fa598f12dc0e3a5144e3da3231a6d6d9ea0e24
Paul Wouters <pwouters@redhat.com>
2020-06-02 00:23:52 -0400

    testing: fixup ikev1-03-fuzzer to sanitizer timestamp

--- testing/pluto/ikev1-03-fuzzer/final.sh
+++ testing/pluto/ikev1-03-fuzzer/final.sh
@@ -1,6 +1,6 @@
 # if east was already down, we crashed it
 hostname |grep east > /dev/null && ipsec whack --shutdown
-hostname |grep east > /dev/null && grep "packet from" /tmp/pluto.log
+hostname |grep east > /dev/null && grep "packet from" /tmp/pluto.log | sed "s/^.*packet from//"
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
