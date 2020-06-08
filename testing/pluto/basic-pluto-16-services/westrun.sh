9fcbf38ee7db730b95cb5926911f1d21b4a5c512
Paul Wouters <pwouters@redhat.com>
2020-06-08 16:51:00 -0400

    testing: basic-pluto-16-services dont display ephemeral status output
    We are only verifying return code anyway

--- testing/pluto/basic-pluto-16-services/westrun.sh
+++ testing/pluto/basic-pluto-16-services/westrun.sh
@@ -1,41 +1,41 @@
 systemctl start ipsec.service || echo failed
-systemctl status ipsec.service || echo failed
+systemctl status ipsec.service >/dev/null || echo failed
 systemctl start ipsec.service && echo detected proper failure
-systemctl status ipsec.service || echo failed
+systemctl status ipsec.service >/dev/null || echo failed
 systemctl restart ipsec.service || echo failed
-systemctl status ipsec.service || echo failed
+systemctl status ipsec.service >/dev/null || echo failed
 systemctl stop ipsec.service || echo failed
-systemctl status ipsec.service && echo detected proper failure
+systemctl status ipsec.service >/dev/null && echo detected proper failure
 systemctl stop ipsec.service || echo failed
-systemctl status ipsec.service && echo detected proper failure
+systemctl status ipsec.service >/dev/null && echo detected proper failure
 systemctl start ipsec.service || echo failed
 systemctl force-reload ipsec.service || echo failed
-systemctl status ipsec.service || echo failed	
+systemctl status ipsec.service >/dev/null || echo failed	
 systemctl start ipsec.service || echo failed
 systemctl restart ipsec.service || echo failed
-systemctl status ipsec.service || echo failed	
+systemctl status ipsec.service >/dev/null || echo failed	
 systemctl start ipsec.service || echo failed
 systemctl condrestart ipsec.service || echo failed
-systemctl status ipsec.service || echo failed	
+systemctl status ipsec.service >/dev/null || echo failed	
 service ipsec start || echo failed
-service ipsec status || echo failed
+service ipsec status >/dev/null || echo failed
 service ipsec start || echo failed
-service ipsec status || echo failed
+service ipsec status >/dev/null || echo failed
 service ipsec restart || echo failed
-service ipsec status || echo failed
+service ipsec status >/dev/null || echo failed
 service ipsec stop || echo failed
-service ipsec status && echo detected proper failure
+service ipsec status >/dev/null && echo detected proper failure
 service ipsec stop || echo failed
-service ipsec status && echo detected proper failure
+service ipsec status >/dev/null && echo detected proper failure
 service ipsec start || echo failed
 service ipsec force-reload || echo failed
-service ipsec status || echo failed	
+service ipsec status >/dev/null || echo failed	
 service ipsec start || echo failed
 service ipsec restart || echo failed
-service ipsec status || echo failed	
+service ipsec status >/dev/null || echo failed	
 service ipsec start || echo failed
 service ipsec condrestart || echo failed
-service ipsec status || echo failed	
+service ipsec status >/dev/null || echo failed	
 # test for fix that starts ipsec only after nic/network is online, rhbz#1145245
 grep "After=network-online.target" /lib/systemd/system/ipsec.service
 # test for rhbz#1127313 (IPsec holes for IPv6 neighbour discovery)
