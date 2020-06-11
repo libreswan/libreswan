43feda3b08edfb62ad8a34a7f93e4d5f63723808
Andrew Cagney <cagney@gnu.org>
2020-06-11 12:16:15 -0400

    testing: cleanup ikev1-psk-dual-behind-nat-01
    Drop unnecessary s/EPHEM/, use ping-once.sh

--- testing/pluto/ikev1-psk-dual-behind-nat-01/northinit.sh
+++ testing/pluto/ikev1-psk-dual-behind-nat-01/northinit.sh
@@ -3,8 +3,8 @@ ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ../bin/block-non-ipsec.sh
 ipsec auto --add north-east
-ipsec whack --xauthname 'use1' --xauthpass 'use1pass' --name north-east --initiate
-ping -n -c 4 -I 192.0.2.101 192.0.2.254
-ipsec whack --trafficstatus
 ipsec whack --impair suppress-retransmits
+ipsec whack --xauthname 'use1' --xauthpass 'use1pass' --name north-east --initiate
+../../pluto/bin/ping-once.sh --up -I 192.0.2.101 192.0.2.254
+ipsec whack --trafficstatus
 echo initdone
