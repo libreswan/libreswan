e301c4d2486bf4ddbdf55fd50ef06fd43ef26ebc
Andrew Cagney <cagney@gnu.org>
2020-06-15 20:08:37 -0400

    testing: update ikev2-tcp-01-eof - mention kernel problem

--- testing/pluto/ikev2-tcp-01-eof/westinit.sh
+++ testing/pluto/ikev2-tcp-01-eof/westinit.sh
@@ -1,20 +1,20 @@
 /testing/guestbin/swan-prep
-dmesg -n 6
+# dmesg -n 6
 # nohup tcpdump -i eth1 -s 65535 -X -vv -nn tcp > OUTPUT/west.tcpdump &
-nohup dumpcap -i eth1 -w /tmp/west.pcap > OUTPUT/west.dumpcap &
+# nohup dumpcap -i eth1 -w /tmp/west.pcap > OUTPUT/west.dumpcap &
 # confirm that the network is alive
-../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
+# ../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # make sure that clear text does not get through
-iptables -F
-iptables -X
-iptables -N LOGDROP
-iptables -A LOGDROP -j LOG
-iptables -A LOGDROP -j DROP
+# iptables -F
+# iptables -X
+# iptables -N LOGDROP
+# iptables -A LOGDROP -j LOG
+# iptables -A LOGDROP -j DROP
 # does this block the ping response?
-# iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
-iptables -A OUTPUT -o eth1 -p tcp --dport 4500 -j ACCEPT
+# iptables -A OUTPUT -o eth1 -p tcp --dport 4500 -j ACCEPT
+# iptables -A OUTPUT -o eth1 -s 192.0.2.0/24 -j LOGDROP
 # confirm with a ping
-../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+# ../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
