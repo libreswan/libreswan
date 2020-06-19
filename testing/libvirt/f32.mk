b7aa1cdd94651719270deb196936edb61bbf97af
Andrew Cagney <cagney@gnu.org>
2020-06-19 15:26:18 -0400

    testing: don't update the kernel and xl2tpd
    Upgrading xl2tpd upgrades the kernel, even when not asked.
    
    Better workaround to kernel problem.

--- testing/libvirt/f32.mk
+++ testing/libvirt/f32.mk
@@ -16,16 +16,25 @@ KVM_INSTALL_RPM_LIST = 'rpm -aq > /var/tmp/rpm-qa-fedora-updates.log'
 NSS_VERSION =
 
 # The kernel packages can only be installed.  To stop a new version
-# being installed set this to empty.
+# being installed set this to empty.  XL2TPD sucks in the latest
+# kernel so is included in the list.
 
-KVM_KERNEL_VERSION ?=
-KVM_KERNEL_PACKAGES ?= \
-    kernel$(KVM_KERNEL_VERSION) \
-    kernel-core$(KVM_KERNEL_VERSION) \
-    kernel-modules$(KVM_KERNEL_VERSION) \
-    kernel-devel$(KVM_KERNEL_VERSION) \
-    kernel-headers$(KVM_KERNEL_VERSION) \
-    kernel-modules-extra$(KVM_KERNEL_VERSION)
+# KVM_KERNEL_ARCH ? = x86_64
+# KVM_KERNEL_VERSION ?= 5.8.0-0.rc1.1.fc33.$(KERNEL_ARCH).rpm
+
+#KVM_KERNEL_VERSION ?=
+#KVM_KERNEL_PACKAGES ?= \
+#    kernel$(KVM_KERNEL_VERSION) \
+#    kernel-core$(KVM_KERNEL_VERSION) \
+#    kernel-modules$(KVM_KERNEL_VERSION) \
+#    kernel-devel$(KVM_KERNEL_VERSION) \
+#    kernel-headers$(KVM_KERNEL_VERSION) \
+#    kernel-modules-extra$(KVM_KERNEL_VERSION) \
+#    xl2tpd
+
+#    kernel-debuginfo-$(RPM_KERNEL_VERSION)
+#    kernel-debuginfo-common-$(KERNEL_ARCH)-$(RPM_KERNEL_VERSION)
+
 
 # Strongswan is brokenly dependent on libgcrypt.
 #
@@ -113,7 +122,6 @@ KVM_UPGRADE_PACKAGES ?= \
     vim-enhanced \
     wget \
     wireshark-cli \
-    xl2tpd \
     xmlto \
     $(KVM_STRONGSWAN_PACKAGES) \
     libfaketime \
