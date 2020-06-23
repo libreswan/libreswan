e3ea1eb2b58584f2d9f29af50c078317e62f6b1a
Andrew Cagney <cagney@gnu.org>
2020-06-23 17:01:26 -0400

    testing: use --rekey-{ike,child} to test TCP rekeying (WIP)

--- testing/pluto/ikev2-tcp-10-rekey-child/westrun.sh
+++ testing/pluto/ikev2-tcp-10-rekey-child/westrun.sh
@@ -2,10 +2,10 @@ ipsec auto --up  westnet-eastnet-ikev2
 ../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
 # wait for rekey event
-sleep 20
-sleep 20
-sleep 20
-sleep 20
+sleep 5
+ipsec whack --rekey-ipsec --name 2
 # rekey of IPsec SA means traffic counters should be 0
 ipsec whack --trafficstatus
-echo done
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+
--- testing/pluto/ikev2-tcp-10-rekey-ike/westinit.sh
+++ testing/pluto/ikev2-tcp-10-rekey-ike/westinit.sh
@@ -4,8 +4,8 @@
 # make sure that clear text does not get through
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
-# confirm with a ping
-../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../pluto/bin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+# start ....
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
