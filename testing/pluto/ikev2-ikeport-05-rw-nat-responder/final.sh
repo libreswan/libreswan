8a27328dd01bae8cdf82997219d63a3f212d79d4
Andrew Cagney <cagney@gnu.org>
2020-06-25 23:04:04 -0400

    testing: sprinkle final.sh on new ikev2-ikeport tests

---
+++ testing/pluto/ikev2-ikeport-05-rw-nat-responder/final.sh
@@ -0,0 +1,7 @@
+../../pluto/bin/ipsec-look.sh
+: ==== cut ====
+ipsec auto --status
+: ==== tuc ====
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
