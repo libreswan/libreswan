341a7307ee5220a383320f98de97a0f379dc968c
Paul Wouters <pwouters@redhat.com>
2020-07-09 22:49:58 -0400

    testing: deleted obsolete ikev2-ikeport-01-rw-nat-global

--- testing/pluto/ikev2-ikeport-01-rw-nat-global/roadinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-/testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add road-eastnet-ikev2
-ipsec whack --impair suppress-retransmits
-echo "initdone"
--- testing/pluto/ikev2-ikeport-01-rw-nat-global/roadrun.sh
+++
@@ -1,3 +0,0 @@
-ipsec auto --up road-eastnet-ikev2
-ping -n -c 4 192.0.2.254
-echo done
