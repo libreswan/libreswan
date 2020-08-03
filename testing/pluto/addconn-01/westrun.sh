d2b773eb20d5e846d950993ab1301b3125be84ce
Paul Wouters <pwouters@redhat.com>
2020-08-03 14:36:42 -0400

    testing: addconn-01  avoid path diplay which shows up different for kvm/namespaces

--- testing/pluto/addconn-01/westrun.sh
+++ testing/pluto/addconn-01/westrun.sh
@@ -1,3 +1,3 @@
 # config contains a conn option in config setup, addconn must deal
-cd /tmp; ipsec addconn --config /etc/ipsec.conf --checkconfig ; cd -
+ipsec addconn --config /etc/ipsec.conf --checkconfig
 echo done
