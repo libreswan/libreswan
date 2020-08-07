687ce5605e74b07296ce4f617e2f37e4e3efef52
Andrew Cagney <cagney@gnu.org>
2020-08-07 19:34:00 -0400

    kvm: add -v to more f32-transmogrify.sh commands; drop set +x

--- testing/libvirt/f32-transmogrify.sh
+++ testing/libvirt/f32-transmogrify.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-set -x
+title()
+{
+    printf "\n\n$*\n\n"
+}
+
+title systemd-networkd
 
 cp -v /testing/baseconfigs/all/etc/systemd/network/* /etc/systemd/network
 
 
-# /etc/rc.d/rc.local
-cp /testing/libvirt/rc-local-transmogrify.sh /etc/rc.d/rc.local
-chmod a+x /etc/rc.d/rc.local
+title /etc/rc.d/rc.local
+
+cp -v /testing/libvirt/rc-local-transmogrify.sh /etc/rc.d/rc.local
+chmod -v a+x /etc/rc.d/rc.local
 
 
-# /etc/hosts
-#
+title /etc/hosts
+
 # add easy names so we can jump from vm to vm and map from IP address
 # to hostname
-
 cat <<EOF >> /etc/hosts
 192.0.1.254 west
 192.0.2.254 east
@@ -24,29 +29,17 @@ cat <<EOF >> /etc/hosts
 EOF
 
 
-# set the hostname
+title hostnamer
 
 rm -f /etc/hostname # hostnamectl set-hostname ""
-cat <<EOF > /etc/systemd/system/hostnamer.service
-[Unit]
-  Description=Figure out who we are
-  ConditionFileNotEmpty=|!/etc/hostname
-  After=systemd-networkd-wait-online.service
-  Before=network.target
-[Service]
-  Type=oneshot
-  ExecStart=/testing/libvirt/hostnamer.sh
-[Install]
-  WantedBy=multi-user.target
-EOF
+cp -av /testing/libvirt/hostnamer.service /etc/systemd/system/
 systemctl enable hostnamer.service
 
 
-# default paths
+title add swan to paths
 
 cat <<EOF > /etc/profile.d/swanpath.sh
 # add swan test binaries to path
-
 case ":${PATH:-}:" in
     *:/testing/guestbin:*) ;;
     *) PATH="/testing/guestbin${PATH:+:$PATH}" ;;
@@ -61,19 +54,17 @@ alias git-log-p='git log --pretty=format:"%h %ad%x09%an%x09%s" --date=short'
 export EDITOR=vim
 EOF
 
-ln -s /testing/guestbin/swan-prep /usr/bin/swan-prep
-ln -s /testing/guestbin/swan-build /usr/bin/swan-build
-ln -s /testing/guestbin/swan-install /usr/bin/swan-install
-ln -s /testing/guestbin/swan-update /usr/bin/swan-update
-ln -s /testing/guestbin/swan-run /usr/bin/swan-run
+
+title /usr/bin/swan-...
+
+ln -vs /testing/guestbin/swan-prep /usr/bin/swan-prep
+ln -vs /testing/guestbin/swan-build /usr/bin/swan-build
+ln -vs /testing/guestbin/swan-install /usr/bin/swan-install
+ln -vs /testing/guestbin/swan-update /usr/bin/swan-update
+ln -vs /testing/guestbin/swan-run /usr/bin/swan-run
 
 
-# enable password root logins (f32 disables these per default)
-
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-
-
-# enable entropy
+title enable entropy
 
 cat <<EOF > /etc/modules-load.d/virtio-rng.conf
 # load virtio RNG device to get entropy from the host
@@ -82,8 +73,9 @@ virtio-rng
 EOF
 
 
-# work around for broken systemd/sshd interaction in fedora 20 causes VM hangs
+title ssh shutdown-workaround
 
+# work around for broken systemd/sshd interaction in fedora 20 causes VM hangs
 cat <<EOF > /etc/systemd/system/sshd-shutdown.service
 # work around for broken systemd/sshd interaction in fedora 20 causes VM hangs
 [Unit]
@@ -98,42 +90,45 @@ EOF
 systemctl enable sshd-shutdown.service
 
 
-#ensure we can get coredumps
+title ensure we can get coredumps
 
 echo " * soft core unlimited" >> /etc/security/limits.conf
 echo " DAEMON_COREFILE_LIMIT='unlimited'" >> /etc/sysconfig/pluto
 
 
+title bind
+
 # and bind config - can be run on all hosts (to prevent network DNS
 # packets) as well as on nic
-
 mkdir -p /etc/bind
 cp -av /testing/baseconfigs/all/etc/bind/* /etc/bind/
 
 
-# ssh
+title ssh
 
 mkdir -p /etc/ssh
-chown 755 /etc/ssh
+chown -v 755 /etc/ssh
 mkdir -p /root/.ssh
-chown 700 /root/.ssh
-cp -v /testing/baseconfigs/all/etc/ssh/*key* /etc/ssh/
-cp -v /testing/baseconfigs/all/root/.ssh/* /root/.ssh/
-chmod 600 /etc/ssh/*key* /root/.ssh/*
-restorecon -R /root/.ssh
+chown -v 700 /root/.ssh
+cp -av /testing/baseconfigs/all/etc/ssh/*key* /etc/ssh/
+cp -av /testing/baseconfigs/all/root/.ssh/* /root/.ssh/
+chmod -v 600 /etc/ssh/*key* /root/.ssh/*
+# enable password root logins (f32 disables these per default)
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+restorecon -R /root/.ssh /etc/ssh
 
 
-# get rid of damn cp/mv/rm aliases for root
+title get rid of rm, cp, mv shell aliases
 
 sed -i 's/^alias rm/# alias rm/g' /root/.bashrc
 sed -i 's/^alias cp/# alias cp/g' /root/.bashrc
 sed -i 's/^alias mv/# alias mv/g' /root/.bashrc
 
 
-# these files are needed for systemd-networkd too
+title files mysteriously needed for systemd-networkd too
 
 for fname in /testing/baseconfigs/all/etc/sysconfig/* ; do
     if test -f "${fname}"; then
-	cp -v "${fname}" /etc/sysconfig/
+	cp -av "${fname}" /etc/sysconfig/
     fi
 done
