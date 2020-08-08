3df6d43b6387615a64ef1d532b2f071aef37415a
Andrew Cagney <cagney@gnu.org>
2020-08-07 21:35:56 -0400

    kvm: in hostnamer.sh, handle duplicate entries

--- testing/libvirt/hostnamer.sh
+++ testing/libvirt/hostnamer.sh
@@ -3,7 +3,7 @@
 ip=$(ip address show dev eth0 | awk '$1 == "inet" { print gensub("/[0-9]*", "", 1, $2)}')
 echo ip: ${ip}
 
-hostname=$(awk '$1 == IP { print $2 }' IP=${ip} /etc/hosts)
+hostname=$(awk '$1 == IP { host=$2; } END { print host; }' IP=${ip} /etc/hosts)
 echo hostname: ${hostname}
 
 hostnamectl set-hostname ${hostname}
