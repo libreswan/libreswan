3eaefb64aed7b72bdd7108c4d30738128ee36d0a
Andrew Cagney <cagney@gnu.org>
2020-08-07 23:59:19 -0400

    kvm: leave configuring /etc/ipsec.* to swan-prep, drop it from rc.local's transmogrify
    (the file psec.secrets-via.include is a bit of a mystery)

--- testing/libvirt/rc-local-transmogrify.sh
+++ testing/libvirt/rc-local-transmogrify.sh
@@ -10,29 +10,6 @@ if test -z "${hostname}"; then
     exit 1
 fi
 
-# and some custom ipsec* files, but not directories
-
-# XXX: this at least partially duplicates swan-prep?
-
-if test -d /etc/ipsec.d -a ! -d /etc/ipsec.d.stock ; then
-    mv -v /etc/ipsec.d /etc/ipsec.d.stock
-else
-    rm -rf /etc/ipsec.d
-fi
-cp -rv /testing/baseconfigs/all/etc/ipsec.d /etc/ipsec.d
-find /etc/ipsec.d -name ipsec.conf.common | xargs rm -f
-cp -av /testing/baseconfigs/${hostname}/etc/ipsec.* /etc/
-
-# fixup the nss files that are root-only on a real host, but are world-read
-# in our repository so the qemu user can still read it to copy it
-
-if test -f /etc/ipsec.d/pkcs11.txt; then
-    chmod 600 /etc/ipsec.d/pkcs11.txt
-fi
-chmod 600 /etc/ipsec.d/*.db
-restorecon -R /etc/ipsec.*
-
-
 if test ${hostname} != swanbase; then
     rm -vf /etc/rc.d/rc.local
 fi
