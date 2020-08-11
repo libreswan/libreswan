79ed0cf893a08500d0d39b30a13597877b9c8f5d
Paul Wouters <pwouters@redhat.com>
2020-08-11 18:34:33 -0400

    testing: fixup for new ikev2 initiator rekey stories

--- testing/pluto/ikev2-ike-rekey-03/final.sh
+++ testing/pluto/ikev2-ike-rekey-03/final.sh
@@ -1,7 +1,7 @@
 ipsec whack --trafficstatus
 ipsec status |grep STATE_ | sort
 # there should be only one IKE_INIT exchange created on west
-hostname | grep west > /dev/null && grep "STATE_PARENT_I1: sent v2I1, expected v2R1" /tmp/pluto.log
+hostname | grep west > /dev/null && grep "sent IKE_SA_INIT request" /tmp/pluto.log
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
