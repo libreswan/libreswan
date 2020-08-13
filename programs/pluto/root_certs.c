c4e2b844069c41cbb27ba5cc29a1d96f965df748
Andrew Cagney <cagney@gnu.org>
2020-08-12 21:38:21 -0400

    logging crypto: in root_certs_addref() use lsw_nss_get_authenticated_slot()
    replace PK11_Authenticate() et.al.

--- programs/pluto/root_certs.c
+++ programs/pluto/root_certs.c
@@ -51,16 +51,12 @@ struct root_certs *root_certs_addref(where_t where)
 	ref_add(root_certs, where); /* function result */
 	root_certs->trustcl = CERT_NewCertList();
 
-	PK11SlotInfo *slot = PK11_GetInternalKeySlot();
-	if (slot == NULL) {
-		return root_certs;
-	}
 
-	if (PK11_NeedLogin(slot)) {
-		SECStatus rv = PK11_Authenticate(slot, PR_TRUE,
-				lsw_return_nss_password_file_info());
-		if (rv != SECSuccess)
-			return root_certs;
+	struct logger logger = GLOBAL_LOGGER(null_fd);
+	PK11SlotInfo *slot = lsw_nss_get_authenticated_slot(&logger);
+	if (slot == NULL) {
+		/* already logged */
+		return root_certs; /* empty, but non-null, list */
 	}
 
 	/*
@@ -69,8 +65,9 @@ struct root_certs *root_certs_addref(where_t where)
 	threadtime_t get_time = threadtime_start();
 	CERTCertList *allcerts = PK11_ListCertsInSlot(slot);
 	threadtime_stop(&get_time, SOS_NOBODY, "%s() calling PK11_ListCertsInSlot()", __func__);
-	if (allcerts == NULL)
+	if (allcerts == NULL) {
 		return root_certs;
+	}
 
 	/*
 	 * XXX: would a better call be
