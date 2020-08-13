38a4b08eddba2fc627ef8c57222c7bd70d99a570
Paul Wouters <pwouters@redhat.com>
2020-08-13 00:06:04 -0400

    testing: chunk of updates for recent output/logging changes

--- testing/pluto/newoe-06-prio/roadinit.sh
+++ testing/pluto/newoe-06-prio/roadinit.sh
@@ -1,7 +1,6 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
 echo "192.1.2.0/24" >> /etc/ipsec.d/policies/private-or-clear
-ip -s xfrm monitor > /tmp/xfrm-monitor.out &
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 # give OE policies time to load
--- testing/pluto/newoe-06-prio/roadrun.sh
+++ testing/pluto/newoe-06-prio/roadrun.sh
@@ -3,6 +3,4 @@ ipsec auto --route road-east-ikev2
 # The ping should also get a reply, proving the IPsec SA was
 # preferred over the OE trap policy
 ping -n -c 4 -I 192.1.3.209 192.1.2.23
-killall ip > /dev/null 2> /dev/null
-cp /tmp/xfrm-monitor.out OUTPUT/road.xfrm-monitor.txt
 echo done
