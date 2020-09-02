2c9661209f87f68330df3fdf50cb1683374f1a69
Ravi Teja <hello@rtcms.dev>
2020-09-02 14:40:45 +0530

    testing: Updated NFS mount details of OpenBSD
    - Moved the NFS Mount to swandefault network
    - Added swandefault network to openbsd(east and west) definition files

--- testing/libvirt/BSD/nfs.sh
+++ testing/libvirt/BSD/nfs.sh
@@ -6,5 +6,5 @@ set -e
 if [ -f /lib/systemd/system/nfs-server.service ]; then
 	sudo systemctl start nfs-server > /dev/null
 	sudo exportfs -r
-	sudo exportfs -o rw,no_root_squash 192.1.2.0/24:$dir
+	sudo exportfs -o rw,no_root_squash 192.168.234.0/24:$dir
 fi
