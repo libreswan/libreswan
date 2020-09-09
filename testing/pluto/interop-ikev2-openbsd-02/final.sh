c973533774d27c1c18a4bb7c8b95f9b49305af65
Ravi Teja <hello@rtcms.dev>
2020-09-09 11:04:56 -0400

    testing: added interop-ikev2-openbsd-02 with openbsd as initiator
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

---
+++ testing/pluto/interop-ikev2-openbsd-02/final.sh
@@ -0,0 +1,4 @@
+if [ -f /tmp/iked.log ]; then cp /tmp/iked.log OUTPUT/openbsdw.iked.log ; fi
+../bin/check-for-core.sh
+if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
+: ==== end ====
