82435e5b6a4b9110141b8d9acd48a235cb3805ae
Ravi Teja <hello@rtcms.dev>
2020-09-09 11:04:24 -0400

    testing: clean up interop-ikev2-openbsd-01

--- testing/pluto/interop-ikev2-openbsd-01/final.sh
+++ testing/pluto/interop-ikev2-openbsd-01/final.sh
@@ -1,5 +1,5 @@
-# check output on openbsd end
-test -f /sbin/ipsecctl && ipsecctl -s all | sort 
+if [ -f /tmp/iked.log ]; then cp /tmp/iked.log OUTPUT/openbsde.iked.log ; fi
+test -f /sbin/ipsecctl && ipsecctl -s all | sort
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -r -m avc -ts recent ; fi
 : ==== end ====
--- testing/pluto/interop-ikev2-openbsd-01/openbsdeinit.sh
+++ testing/pluto/interop-ikev2-openbsd-01/openbsdeinit.sh
@@ -2,5 +2,5 @@
 #/testing/guestbin/swan-prep
 cp openbsde.conf /etc/iked.conf
 chmod 600 /etc/iked.conf
-/sbin/iked
+/sbin/iked -dvvv > /tmp/iked.log 2>&1 &
 echo "initdone"
