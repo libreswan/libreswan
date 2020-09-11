libreswan/libreswan

commit 7be41582a340c52af55a6ba8c6d1e14e3cdea9e9

IKEv2: Verify (not ignore) expected TSi/TSr payloads for IPsec rekeys.
The RFC states they MUST be the same for IPsec rekey as they are for the
original exitsing IPsec SA. So for rekeying, the traffic selectors were
just copied from the existing IPsec being rekeyed and ignored the actual
TSi/TSr payloads during IPsec rekey.

It now parses these and returns INVALID_SYNTAX if it fails to parse. This
happens on libreswan 3.30 due to a bug in the IPsec rekey code on that version.

It also verifies the traffic selector that was negotiated for the existing
IPsec SA is still an entry on the received TSi/TSr list. Other entries on
the list are ignored.

programs/pluto/ikev2_parent.c
@@ -4214,6 +4214,9 @@ static void ikev2_child_ike_inR_continue(struct state *st,
 
 /*
  * initiator received a create Child SA Response (RFC 7296 1.3.1, 1.3.2)
+ *
+ * Note: "when rekeying, the new Child SA SHOULD NOT have different Traffic
+ *        Selectors and algorithms than the old one."
  */
 
 static dh_cb ikev2_child_inR_continue;
@@ -4727,6 +4730,18 @@ static stf_status ikev2_child_out_tail(struct ike_sa *ike, struct child_sa *chil
 		ret = ikev2_child_sa_respond(ike, child,
 					     request_md, &sk.pbs,
 					     ISAKMP_v2_CREATE_CHILD_SA);
+	}
+
+	/*
+	 * RFC 7296 https://tools.ietf.org/html/rfc7296#section-2.8
+	 * "when rekeying, the new Child SA SHOULD NOT have different Traffic
+	 *  Selectors and algorithms than the old one."
+	 */
+	if (st->st_state->kind == STATE_V2_REKEY_CHILD_R ||
+	    st->st_state->kind == STATE_V2_REKEY_CHILD_I) {
+		ret = child_rekey_ts_verify(request_md);
+		if (ret != STF_OK)
+			return ret;
 	}
 
 	/* note: pst: parent; md->st: child */

programs/pluto/ikev2_ts.c
@@ -54,6 +54,26 @@ enum fit {
 	END_WIDER_THAN_TS,
 };
 
+
+static bool ts_in_tslist(struct traffic_selectors *haystack,
+			struct traffic_selector *needle)
+{
+	for (unsigned int i = 0; i < haystack->nr; i++) {
+		struct traffic_selector hay = haystack->ts[i];
+
+		if (needle->ts_type == hay.ts_type &&
+			needle->ipprotoid == hay.ipprotoid &&
+			needle->startport == hay.startport &&
+			needle->endport == hay.endport &&
+			sameaddr(&needle->net.start, &hay.net.start) &&
+			sameaddr(&needle->net.end, &hay.net.end))
+                        {
+				return TRUE;
+                        }
+	}
+	return FALSE;
+}
+
 static const char *fit_string(enum fit fit)
 {
 	switch (fit) {
@@ -1150,4 +1170,41 @@ bool v2_process_ts_response(struct child_sa *child,
 			       &c->spd.that.client));
 
 	return true;
+}
+
+/*
+ * RFC 7296 https://tools.ietf.org/html/rfc7296#section-2.8
+ * "when rekeying, the new Child SA SHOULD NOT have different Traffic
+ *  Selectors and algorithms than the old one."
+ *
+ * We already matched the right connection by the SPI of v2N_REKEY_SA
+ */
+stf_status child_rekey_ts_verify(struct msg_digest *md)
+{
+	struct state *st = md->st;
+	passert(st->st_state->kind == STATE_V2_REKEY_CHILD_R || st->st_state->kind == STATE_V2_REKEY_CHILD_I);
+
+	struct traffic_selectors their_tsis = { .nr = 0, };
+	struct traffic_selectors their_tsrs = { .nr = 0, };
+	enum message_role md_role = v2_msg_role(md);
+
+	/* should really return stf_status, not bool */
+	if (!v2_parse_tss(md, &their_tsis, &their_tsrs)) {
+		loglog(RC_LOG_SERIOUS, "Received malformed TSi/TSr payload(s)");
+		return STF_FAIL + v2N_INVALID_SYNTAX;
+	}
+
+	struct traffic_selector ts_this = ikev2_end_to_ts(&st->st_connection->spd.this);
+	struct traffic_selector ts_that = ikev2_end_to_ts(&st->st_connection->spd.that);
+
+	if (!ts_in_tslist(&their_tsis, (md_role == MESSAGE_REQUEST) ? &ts_that : &ts_this)) {
+		loglog(RC_LOG_SERIOUS, "Received TSi payload does not contain existing IPsec SA traffic Selectors");
+		return STF_FAIL + v2N_TS_UNACCEPTABLE;
+	}
+
+	if (!ts_in_tslist(&their_tsrs, (md_role == MESSAGE_REQUEST) ? &ts_this : &ts_that)) {
+		loglog(RC_LOG_SERIOUS, "Received TSr payload(s) does not contain existing IPsec SA Traffic Selectors");
+		return STF_FAIL + v2N_TS_UNACCEPTABLE;
+	}
+	return STF_OK;
 }

programs/pluto/ikev2_ts.h
@@ -49,4 +49,6 @@ stf_status v2_emit_ts_payloads(const struct child_sa *cst,
 			       pb_stream *outpbs,
 			       const struct connection *c);
 
+stf_status child_rekey_ts_verify(struct msg_digest *md);
+
 #endif

commit 8edd8bb910ae91162d1d028e25a64d309df1e484

IKEv2: rekey responder check use exising scoring logic
Fix Windows 10 rekey response. Windows during rekey request a wider TS than
in IKE_AUTH response.

Relax 7be41582a3 check, and respond with same TS as responded in IKE_AUTH.
RFC7296 allow requesting  a superset TS than IKE_AUTH response, still respond
with same TS as in IKE_AUTH response. The predecessor RFC, 5996, possibly
allow changing the TS during rekey.

Fixes: 7be41582a340 "(IKEv2: Verify (not ignore) expected TSi/TSr payloads for IPsec rekeys)"

Signed-off-by: Paul Wouters <pwouters@redhat.com>

programs/pluto/ikev2_parent.c
@@ -4436,6 +4436,13 @@ stf_status ikev2_child_inIoutR(struct ike_sa *ike,
 			/* already logged; already recorded */
 			return STF_FAIL;
 		}
+		if (!child_rekey_responder_ts_verify(child, md)) {
+			record_v2N_response(ike->sa.st_logger, ike, md,
+				v2N_TS_UNACCEPTABLE, NULL/*no data*/,
+					ENCRYPTED_PAYLOAD);
+			return STF_FAIL;
+               }
+
 		pexpect(child->sa.st_ipsec_pred != SOS_NOBODY);
 		break;
 	case STATE_V2_NEW_CHILD_R0:
@@ -4852,20 +4859,6 @@ static stf_status ikev2_child_out_tail(struct ike_sa *ike, struct child_sa *chil
 			jam_v2_stf_status(buf, ret);
 		}
 		return ret; /* abort building the response message */
-	}
-
-	/*
-	 * RFC 7296 https://tools.ietf.org/html/rfc7296#section-2.8
-	 * "when rekeying, the new Child SA SHOULD NOT have different Traffic
-	 *  Selectors and algorithms than the old one."
-	 */
-	if (child->sa.st_state->kind == STATE_V2_REKEY_CHILD_R0) {
-		if (!child_rekey_ts_verify(child, request_md)) {
-			/* logged; but not recorded */
-			record_v2N_response(child->sa.st_logger, ike, request_md, v2N_TS_UNACCEPTABLE,
-					    NULL, ENCRYPTED_PAYLOAD);
-			return STF_FAIL;
-		}
 	}
 
 	/* note: pst: parent; md->st: child */

programs/pluto/ikev2_ts.c
@@ -56,25 +56,6 @@ enum fit {
 };
 
 
-static bool ts_in_tslist(struct traffic_selectors *haystack,
-			struct traffic_selector *needle)
-{
-	for (unsigned int i = 0; i < haystack->nr; i++) {
-		struct traffic_selector hay = haystack->ts[i];
-
-		if (needle->ts_type == hay.ts_type &&
-			needle->ipprotoid == hay.ipprotoid &&
-			needle->startport == hay.startport &&
-			needle->endport == hay.endport &&
-			sameaddr(&needle->net.start, &hay.net.start) &&
-			sameaddr(&needle->net.end, &hay.net.end))
-                        {
-				return TRUE;
-                        }
-	}
-	return FALSE;
-}
-
 static const char *fit_string(enum fit fit)
 {
 	switch (fit) {
@@ -1252,36 +1233,51 @@ bool v2_process_ts_response(struct child_sa *child,
  * "when rekeying, the new Child SA SHOULD NOT have different Traffic
  *  Selectors and algorithms than the old one."
  *
+ * However, when narrowed down, the original TSi/TSr is wider than the
+ * returned narrowed TSi/TSr. Windows 10 is known to use the original
+ * and not the narrowed TSi/TSr.
+ *
+ * RFC 7296 #1.3.3 "The Traffic Selectors for traffic to be sent
+ * on that SA are specified in the TS payloads in the response,
+ * which may be a subset of what the initiator of the Child SA proposed."
+ *
+ * However, the rekey initiator, when it is the original initiator of
+ * the Child SA, may request a super set. And responder should
+ * respond with same set as initially negotiated, ie RFC 7296 #2.8
+ *
+ * See RFC 7296 Section 1.7. for the above change.
+ * Significant Differences between RFC 4306 and RFC 5996
+ *
  * We already matched the right connection by the SPI of v2N_REKEY_SA
  */
-bool child_rekey_ts_verify(struct child_sa *child, struct msg_digest *md)
+bool child_rekey_responder_ts_verify(struct child_sa *child, struct msg_digest *md)
 {
-	if (!pexpect(child->sa.st_state->kind == STATE_V2_REKEY_CHILD_R0 ||
-		     child->sa.st_state->kind == STATE_V2_REKEY_CHILD_I1)) {
+	if (!pexpect(child->sa.st_state->kind == STATE_V2_REKEY_CHILD_R0))
 		return false;
-	}
 
+	const struct connection *c = child->sa.st_connection;
 	struct traffic_selectors their_tsis = { .nr = 0, };
 	struct traffic_selectors their_tsrs = { .nr = 0, };
-	enum message_role md_role = v2_msg_role(md);
 
 	if (!v2_parse_tss(md, &their_tsis, &their_tsrs)) {
 		log_state(RC_LOG_SERIOUS, &child->sa, "received malformed TSi/TSr payload(s)");
 		return false;
 	}
 
-	struct traffic_selector ts_this = ikev2_end_to_ts(&child->sa.st_connection->spd.this);
-	struct traffic_selector ts_that = ikev2_end_to_ts(&child->sa.st_connection->spd.that);
+	const struct ends ends = {
+		.i = &c->spd.that,
+		.r = &c->spd.this,
+	};
 
-	if (!ts_in_tslist(&their_tsis, (md_role == MESSAGE_REQUEST) ? &ts_that : &ts_this)) {
-		log_state(RC_LOG_SERIOUS, &child->sa,
-			  "received TSi payload does not contain existing IPsec SA traffic Selectors");
+	enum fit fitness = END_NARROWER_THAN_TS;
+
+	struct best_score score = score_ends(fitness, c, &ends, &their_tsis,
+			&their_tsrs);
+
+	if (!score.ok) {
+		loglog(RC_LOG_SERIOUS, "Rekey: received Traffic Selectors does not contain existing IPsec SA Traffic Selectors");
 		return false;
 	}
 
-	if (!ts_in_tslist(&their_tsrs, (md_role == MESSAGE_REQUEST) ? &ts_this : &ts_that)) {
-		log_state(RC_LOG_SERIOUS, &child->sa, "received TSr payload(s) does not contain existing IPsec SA Traffic Selectors");
-		return false;
-	}
 	return true;
 }

programs/pluto/ikev2_ts.h
@@ -49,6 +49,6 @@ stf_status v2_emit_ts_payloads(const struct child_sa *cst,
 			       pb_stream *outpbs,
 			       const struct connection *c);
 
-bool child_rekey_ts_verify(struct child_sa *child, struct msg_digest *md);
+bool child_rekey_responder_ts_verify(struct child_sa *child, struct msg_digest *md);
 
 #endif