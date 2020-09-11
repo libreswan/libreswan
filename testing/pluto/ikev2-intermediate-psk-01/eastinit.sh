d609e4aaeabdb59d5df1c608cb45da565f380e4a
Yulia Kuzovkova <ukuzovkova@gmail.com>
2020-09-11 13:08:12 -0400

    testing: Add test cases for Intermediate Exchange
    Signed-off-by: Sahana Prasad <sahana@redhat.com>
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

---
+++ testing/pluto/ikev2-intermediate-psk-01/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
+ipsec auto --status | grep westnet-eastnet-ipv4-psk-ikev2
+echo "initdone"
