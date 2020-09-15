cbce4975ad13ae69b35c95572995fefb529a82bc
Paul Wouters <pwouters@redhat.com>
2020-09-14 22:14:27 -0400

    testing: fixup ikev2-cp-rekey-01 and add ikev2-cp-rekey-02-up-up
    One test case uses --up and another --up for rekey
    One test case uses --up and whack --ipsec-rekey
    
    Behaviour is slightly different. Ideally, the second --up
    command would recognise it is really supposed to be a rekey.

--- testing/pluto/ikev2-cp-rekey-02-up-up/roadrun.sh
+++ testing/pluto/ikev2-cp-rekey-02-up-up/roadrun.sh
@@ -1,8 +1,8 @@
 ipsec auto --up eastnet-any
 ping -n -c 4 -I 100.64.13.2 192.0.2.254
 ipsec whack --trafficstatus
-ipsec whack --rekey-ipsec --name eastnet-any
-# not using: ipsec auto --up eastnet-any
+# not using: ipsec whack --rekey-ipsec --name eastnet-any
+ipsec auto --up eastnet-any
 ping -n -c 4 -I 100.64.13.2 192.0.2.254
 ipsec whack --trafficstatus
 echo done
