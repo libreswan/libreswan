51392b73a385a71c277ff1f665cfc73e5edaba30
Paul Wouters <pwouters@redhat.com>
2020-09-16 17:22:28 -0400

    building: enable USE_NSS_KDF=true
    This requires NSS >= 3.52 or NSS 3.44 with NSS commit 225bb39eade102eef5f3999eae04a7a16da9b330
    applied.

--- mk/config.mk
+++ mk/config.mk
@@ -731,8 +731,8 @@ ifdef USE_NSS_PRF
 $(error ERROR: Deprecated USE_NSS_PRF variable set, use USE_NSS_KDF instead)
 endif
 
-# This is required for FIPS, but required a very recent version of nss
-USE_NSS_KDF ?= false
+# This is required for FIPS, but required a recent nss (see comment above)
+USE_NSS_KDF ?= true
 ifeq ($(USE_NSS_KDF),true)
 USERLAND_CFLAGS += -DUSE_NSS_KDF
 endif
