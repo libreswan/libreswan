b971446b15efcfca129f280dfa2213eee9552dbe
Andrew Cagney <cagney@gnu.org>
2020-09-16 20:14:46 -0400

    testing: expect ikev2-03-basic-rawrsa-ckaid to work first time
    It would be helpful having the log line:
      003 "west-ckaid-rawkey" #1: authenticated using RSA with SHA2_512
    identify the RSA key source.

--- testing/pluto/ikev2-03-basic-rawrsa-ckaid/westrun.sh
+++ testing/pluto/ikev2-03-basic-rawrsa-ckaid/westrun.sh
@@ -1,8 +1,4 @@
 # see description.txt
-# should find east's pubkey using ckaid and leftrsasigkey
-ipsec auto --add east-ckaid-rsasigkey
-# force east/west rsasigkey load using @east/@west
-ipsec auto --add east-rsasigkey
-# now east's pubkey using ckaid is found
-ipsec auto --add east-ckaid
-echo done
+ipsec auto --add west-ckaid-rawkey
+ipsec auto --up west-ckaid-rawkey
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
