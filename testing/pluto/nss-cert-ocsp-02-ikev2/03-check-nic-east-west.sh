b4a15a2f9e55c941a69ee0c1850b7dcb964a59b3
Andrew Cagney <cagney@gnu.org>
2020-09-17 11:12:05 -0400

    testing: exclude debug lines when grepping pluto.log

--- testing/pluto/nss-cert-ocsp-02-ikev2/03-check-nic-east-west.sh
+++ testing/pluto/nss-cert-ocsp-02-ikev2/03-check-nic-east-west.sh
@@ -1,5 +1,5 @@
 # this test should log that west's certificate is revoked
-grep -i "certificate revoked" /tmp/pluto.log
+grep -i -e "^[^|].*certificate revoked" /tmp/pluto.log
 : ==== cut ====
 journalctl /sbin/ocspd --no-pager | tail -n 20
 ipsec auto --status
