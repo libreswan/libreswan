03ab8675c5514fb4c54b89df2a83dd7a8f44d4c1
Antony Antony <antony@phenome.org>
2020-09-17 17:48:04 -0400

    testing: minor fixes to test output
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

--- testing/pluto/ikev1-xfrmi-01/northrun.sh
+++ testing/pluto/ikev1-xfrmi-01/northrun.sh
@@ -1,6 +1,6 @@
 ipsec auto --up north
 # comments bellow are to understand/explore the basics : what is going on
-# ip link add ipsec1 type xfrm xfrmi-id 1 dev eth0
+# ip link add ipsec1 type xfrm if_id 1 dev eth0
 # ip link set ipsec1 up
 # ip route add 192.0.2.0/24 dev ipsec1 src 192.0.3.254
 # tcpdump -s 0 -n -w /tmp/ipsec1.pcap -i ipsec1 & echo $! > /tmp/tcpdump.pid
