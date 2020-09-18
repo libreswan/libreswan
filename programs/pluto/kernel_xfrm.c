libreswan/libreswan

commit 231feaf8faafc9caffffd6669d090db51749f7f8

XFRM: Add ipsec0 support by mapping it onto if_d 16384
This requires ipsec-interface= to take 0 as valid argument,
so we use UINT32_MAX to mean "no" in the parser.

When setting up the XFRM interface, if we encounter if_id 0,
we map it to 16384, but once we make a name we will use "ipsec0"
instead of "ipsec16384".

whack support was also still missing, so whack support was added
with the same mapping.

This is documented in a man page update.

programs/pluto/kernel_xfrm.c
@@ -742,8 +742,8 @@ static bool netlink_raw_eroute(const ip_address *this_host,
 				req.n.nlmsg_len += mark_attr->rta_len;
 			}
 		}
-               if (xfrm_if_id > 0) {
 #ifdef USE_XFRM_INTERFACE
+               if (xfrm_if_id != 0) {
 			struct rtattr *attr = (struct rtattr *)((char *)&req + req.n.nlmsg_len);
 			dbg("%s netlink: XFRMA_IF_ID  %" PRIu32 " req.n.nlmsg_type=%" PRIu32,
 			    __func__, xfrm_if_id, req.n.nlmsg_type);
@@ -758,8 +758,8 @@ static bool netlink_raw_eroute(const ip_address *this_host,
 			attr->rta_len = RTA_LENGTH(sizeof(uint32_t));
 			memcpy(RTA_DATA(attr), &xfrm_if_id, sizeof(uint32_t));
 			req.n.nlmsg_len += attr->rta_len;
-#endif
 	       }
+#endif
 
 	}
 
@@ -1556,8 +1556,8 @@ static bool netlink_add_sa(const struct kernel_sa *sa, bool replace)
 		attr = (struct rtattr *)((char *)attr + attr->rta_len);
 	}
 
-	if (sa->xfrm_if_id > 0) {
 #ifdef USE_XFRM_INTERFACE
+	if (sa->xfrm_if_id != 0) {
 		dbg("%s netlink: XFRMA_IF_ID %" PRIu32 " req.n.nlmsg_type=%" PRIu32,
 		    __func__, sa->xfrm_if_id, req.n.nlmsg_type);
 		attr->rta_type = XFRMA_IF_ID;
@@ -1572,8 +1572,8 @@ static bool netlink_add_sa(const struct kernel_sa *sa, bool replace)
 		memcpy(RTA_DATA(attr), &sa->xfrm_if_id, sizeof(uint32_t));
 		req.n.nlmsg_len += attr->rta_len;
 		attr = (struct rtattr *)((char *)attr + attr->rta_len);
-#endif
 	}
+#endif
 
 	if (sa->nic_offload_dev) {
 		struct xfrm_user_offload xuo = {

lib/libipsecconf/confread.c
@@ -166,7 +166,7 @@ static void ipsecconf_default_values(struct starter_config *cfg)
 	DOPT(KNCF_CLIENTADDRFAMILY, AF_UNSPEC);
 
 	DOPT(KNCF_AUTO, STARTUP_IGNORE);
-	DOPT(KNCF_XFRM_IF_ID, yn_no);
+	DOPT(KNCF_XFRM_IF_ID, UINT32_MAX);
 
 # undef DOPT
 
@@ -190,6 +190,8 @@ static void ipsecconf_default_values(struct starter_config *cfg)
 	d->right.addr = address_any(&ipv4_info);
 	d->right.nexttype = KH_NOTSET;
 	d->right.nexthop = address_any(&ipv4_info);
+
+	d->xfrm_if_id = UINT32_MAX;
 
 	/* default is NOT to look in DNS */
 	d->left.key_from_DNS_on_demand = FALSE;

lib/libipsecconf/keywords.c
@@ -116,11 +116,12 @@ static const struct keyword_enum_values kw_ynf_list = VALUES_INITIALIZER(kw_ynf_
 
 #ifdef USE_XFRM_INTERFACE
 /* Values for no/yes, used by ipsec-interface */
-static const struct keyword_enum_value kw_yn_values[] = {
-	{ "yes",	yn_yes },
-	{ "no",		yn_no },
+static const struct keyword_enum_value kw_yndev_values[] = {
+	{ "yes",	1 /* ipsec1 */ },
+	{ "no",		UINT32_MAX /* disabled */},
+	/* any specified number becomes ipsecXXXX */
 };
-static const struct keyword_enum_values kw_yn_list = VALUES_INITIALIZER(kw_yn_values);
+static const struct keyword_enum_values kw_yndev_list = VALUES_INITIALIZER(kw_yndev_values);
 #endif
 
 /* Values for yes/no/auto, used by encapsulation and nic-offload */
@@ -590,7 +591,7 @@ const struct keyword_def ipsec_conf_keywords[] = {
   { "vti-routing",  kv_conn,  kt_bool,  KNCF_VTI_ROUTING, NULL, NULL, },
   { "vti-shared",  kv_conn,  kt_bool,  KNCF_VTI_SHARED, NULL, NULL, },
 #ifdef USE_XFRM_INTERFACE
-  { "ipsec-interface", kv_conn, kt_loose_enum, KNCF_XFRM_IF_ID, &kw_yn_list, NULL, },
+  { "ipsec-interface", kv_conn, kt_loose_enum, KNCF_XFRM_IF_ID, &kw_yndev_list, NULL, },
 #endif
 
   { "modecfgwins1",  kv_conn,  kt_obsolete,  KNCF_WARNIGNORE, NULL, NULL, },

programs/configs/d.ipsec.conf/ipsec-interface.xml
@@ -30,6 +30,12 @@ the existing init system or via networking tools such as systemd-networkd and Ne
 _updown script handles certain Linux specific interfaces settings required for proper functioning,
 such as forwarding and routing rules for IPsec traffic.
 </para>
+<para>
+The ipsec-interface=0 will create an interface with the same name as the old KLIPS interface, ipsec0.
+This interface name should only be used when required for migration from KLIPS to XFRM interfaces.
+Since XFRM IF_ID and marking cannot use 0, this is mapped to 16384. This means that the
+devices ipsec0 and ipsec16384 cannot both be in use.
+</para>
 </listitem>
 </varlistentry>
 

programs/pluto/connections.c
@@ -1695,10 +1695,11 @@ static bool extract_connection(const struct whack_message *wm,
 		c->vti_routing = wm->vti_routing;
 		c->vti_shared = wm->vti_shared;
 #ifdef USE_XFRM_INTERFACE
-		if (wm->xfrm_if_id != yn_no) {
+		if (wm->xfrm_if_id != UINT32_MAX) {
 			err_t err = xfrm_iface_supported(logger);
 			if (err == NULL) {
-				if (setup_xfrm_interface(c, wm->xfrm_if_id))
+				if (!setup_xfrm_interface(c, wm->xfrm_if_id == 0 ?
+					PLUTO_XFRMI_REMAP_IF_ID_ZERO : wm->xfrm_if_id ))
 					return false;
 			} else {
 				log_message(RC_FATAL, logger,

programs/pluto/ikev1_aggr.c
@@ -834,7 +834,7 @@ static stf_status aggr_inR1_outI2_tail(struct msg_digest *md)
 
 	IKE_SA_established(pexpect_ike_sa(st));
 #ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif
@@ -960,7 +960,7 @@ stf_status aggr_inI2(struct state *st, struct msg_digest *md)
 
 	IKE_SA_established(pexpect_ike_sa(st));
 #ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif

programs/pluto/ikev1_main.c
@@ -1714,7 +1714,7 @@ stf_status main_inI3_outR3(struct state *st, struct msg_digest *md)
 
 	IKE_SA_established(pexpect_ike_sa(st));
 #ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif
@@ -1769,7 +1769,7 @@ stf_status main_inR3(struct state *st, struct msg_digest *md)
 
 	IKE_SA_established(pexpect_ike_sa(st));
 #ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif

programs/pluto/ikev1_quick.c
@@ -1490,7 +1490,7 @@ static stf_status quick_inI1_outR1_continue12_tail(struct msg_digest *md,
 	 */
 #ifdef USE_XFRM_INTERFACE
 	struct connection *c = st->st_connection;
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif
@@ -1720,7 +1720,7 @@ stf_status quick_inR1_outI2_tail(struct msg_digest *md,
 	 * failure won't look like success.
 	 */
 #ifdef USE_XFRM_INTERFACE
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif
@@ -1759,7 +1759,7 @@ stf_status quick_inI2(struct state *st, struct msg_digest *md UNUSED)
 	 */
 #ifdef USE_XFRM_INTERFACE
 	struct connection *c = st->st_connection;
-	if (c->xfrmi != NULL && c->xfrmi->if_id != yn_no)
+	if (c->xfrmi != NULL && c->xfrmi->if_id != 0)
 		if (add_xfrmi(c, st->st_logger))
 			return STF_FATAL;
 #endif

commit f965623b70ed55fbf7401c0413187a329187de4c

building: move the check outside ifdef
Fixes: 231feaf8faafc ("FRM: Add ipsec0 support by mapping it onto if_d 16384")
leave it outside the ifdef otherwise

home/a/libreswan/programs/pluto/kernel_xfrm.c:516:19: error: unused parameter ‘xfrm_if_id’ [-Werror=unused-parameter]
    const uint32_t xfrm_if_id,
                   ^~~~~~~~~~
cc1: all warnings being treated as errors
../../mk/depend.mk:32: recipe for target 'kernel_xfrm.o' failed
-               if (xfrm_if_id > 0) {
 #ifdef USE_XFRM_INTERFACE
+               if (xfrm_if_id != 0) {

programs/pluto/kernel_xfrm.c
@@ -742,8 +742,9 @@ static bool netlink_raw_eroute(const ip_address *this_host,
 				req.n.nlmsg_len += mark_attr->rta_len;
 			}
 		}
+
+		if (xfrm_if_id != 0) {
 #ifdef USE_XFRM_INTERFACE
-               if (xfrm_if_id != 0) {
 			struct rtattr *attr = (struct rtattr *)((char *)&req + req.n.nlmsg_len);
 			dbg("%s netlink: XFRMA_IF_ID  %" PRIu32 " req.n.nlmsg_type=%" PRIu32,
 			    __func__, xfrm_if_id, req.n.nlmsg_type);