52ca60e5fe7b71c53f6caef9051c3b33151fe869
Andrew Cagney <cagney@gnu.org>
2020-09-22 12:38:51 -0400

    crypto: when reloading a cert, use the connection's log prefix

--- programs/pluto/nss_cert_reread.c
+++ programs/pluto/nss_cert_reread.c
@@ -19,14 +19,12 @@ static inline cert_t backup_cert(struct end *dst)
 }
 
 /* Reread a certificate from the NSS database */
-static bool nss_reread_cert(struct fd *whackfd, struct end *dst, const char *connstr)
+static bool nss_reread_cert(struct end *dst, struct logger *logger)
 {
-	struct logger logger[1] = { GLOBAL_LOGGER(whackfd), };
 	const char *nickname = cert_nickname(&dst->cert);
 	if (nickname == NULL) {
 		log_message(RC_BADID, logger,
-			    "connection '%s' certificate cannot reread due to unknown nickname",
-			    connstr);
+			    "certificate cannot reread due to unknown nickname");
 		return false;
 	}
 
@@ -41,31 +39,29 @@ static bool nss_reread_cert(struct fd *whackfd, struct end *dst, const char *con
 	diag_t diag = add_end_cert_and_preload_private_key(cert, dst, logger);
 	if (diag != NULL) {
 		log_diag(RC_BADID, logger, &diag,
-			 "connection '%s' rereading certificate failed for nickname '%s': ",
-			 connstr, nickname);
+			 "rereading certificate failed for nickname '%s': ", nickname);
 		return false;
 	}
 
 	return true;
 }
 
-static void reread_end_cert(struct fd *whackfd, struct end *dst, const char *connstr)
+static void reread_end_cert(struct end *dst, struct logger *logger)
 {
-	cert_t old_cert;
-
 	if (dst->cert.u.nss_cert == NULL) {
-		dbg("No cert exists for %s of %s; nothing to do", dst->leftright, connstr);
+		dbg("no cert exists for %s; nothing to do", dst->leftright);
 	} else {
-		old_cert = backup_cert(dst);
+		cert_t old_cert = backup_cert(dst);
 
-		if (!nss_reread_cert(whackfd, dst, connstr)) {
+		if (!nss_reread_cert(dst, logger)) {
 			dst->cert.ty = old_cert.ty;
 			dst->cert.u.nss_cert = old_cert.u.nss_cert;
 			return;
 		}
 
-		whack_comment(whackfd, "connection '%s' certificate %scert=%s has been reloaded",
-				connstr, dst->leftright, cert_nickname(&dst->cert));
+		log_message(RC_COMMENT, logger,
+			    "certificate %scert=%s has been reloaded",
+			    dst->leftright, cert_nickname(&dst->cert));
 		if (old_cert.u.nss_cert != NULL)
 			CERT_DestroyCertificate(old_cert.u.nss_cert);
 	}
@@ -76,10 +72,11 @@ void reread_cert(struct fd *whackfd, struct connection *c)
 	struct end *dst;
 
 	dbg("rereading certificate(s) for connection '%s'", c->name);
+	struct logger logger[1] = { CONNECTION_LOGGER(c, whackfd), };
 
 	dst = &c->spd.this;
-	reread_end_cert(whackfd, dst, c->name);
+	reread_end_cert(dst, logger);
 
 	dst = &c->spd.that;
-	reread_end_cert(whackfd, dst, c->name);
+	reread_end_cert(dst, logger);
 }
