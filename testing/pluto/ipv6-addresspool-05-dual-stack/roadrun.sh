8575abeb655e4f6d3ebc32e084e2bef727bedb81
Antony Antony <antony@phenome.org>
2020-09-23 15:36:49 +0000

    testing: add ipv6-addresspool-05-dual-stack with A and AAAA

---
+++ testing/pluto/ipv6-addresspool-05-dual-stack/roadrun.sh
@@ -0,0 +1,13 @@
+ipsec auto --up road
+ping6 -c 2 -w 5 -I 2001:db8:0:3:1::0 2001:db8:0:2::254
+ipsec trafficstatus
+../../pluto/bin/ip-addr-show.sh
+ip -6 route
+ip route get to 2001:db8:1:2::23
+#
+# addconn need a non existing --ctlsocket
+# otherwise this add bring the connection down.
+#
+# see the source address selection when the tunnel is established
+ipsec auto --add --verbose --ctlsocket /run/pluto/foo road
+echo done
