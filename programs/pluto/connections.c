d54ea37ea9f7a98c8d0a0c4f809eea53cb9fd8b3
Paul Wouters <pwouters@redhat.com>
2020-09-24 22:32:01 -0400

    pluto: skip testing it conn is better than ourselves in refine_host_connection()

--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -3049,6 +3049,9 @@ struct connection *refine_host_connection(const struct state *st,
 	/* wcip stands for: wildcard Peer IP? */
 	for (bool wcpip = FALSE;; wcpip = TRUE) {
 		for (; d != NULL; d = d->hp_next) {
+			if (c == d)
+				continue; /* not better than ourselves */
+
 			int wildcards;
 			bool matching_peer_id = match_id(peer_id,
 							&d->spd.that.id,
