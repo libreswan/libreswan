2013e18197d1812a20a84fd628d46ad1910bc89d
Andrew Cagney <cagney@gnu.org>
2020-10-13 14:07:36 -0400

    testing: in ikev2-67-esp-null-iperf, don't try to cd east to /tmp

--- testing/pluto/ikev2-67-esp-null-iperf/eastinit.sh
+++ testing/pluto/ikev2-67-esp-null-iperf/eastinit.sh
@@ -8,7 +8,7 @@ ipsec whack --impair suppress-retransmits
 ipsec auto --add west-east
 ipsec auto --status | grep west-east
 taskset 0x1 iperf3 -s -p 5001 & echo "$!"  > OUTPUT/$(hostname)-iperf3-5001.pid
-cd /var/tmp/
+# cd /var/tmp/
 # perf record -g -e cycles --call-graph dwarf & echo "$!"  > $(hostname)-perf.pid
 # taskset 0x1 iperf3 -s -p 5002 &
 # taskset 0x3 iperf3 -s -p 5003 &
