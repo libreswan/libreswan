5466829d292c8c08e9c989bdb046b70b48073550
Antony Antony <antony@phenome.org>
2020-10-14 20:59:50 +0000

    libswan: add white space in log line
    /usr/local/libexec/ipsec/plutoadjusting nssdir to /var/lib/ipsec/nss

--- lib/libswan/lswconf.c
+++ lib/libswan/lswconf.c
@@ -141,7 +141,7 @@ void lsw_conf_confddir(const char *confddir, struct logger *logger)
 	lsw_conf_calculate();
 
 	if (!streq(global_oco.confddir, IPSEC_CONFDDIR))
-		log_message(RC_LOG, logger, "adjusting ipsec.d to %s", global_oco.confddir);
+		log_message(RC_LOG, logger, " adjusting ipsec.d to %s", global_oco.confddir);
 }
 
 void lsw_conf_nssdir(const char *nssdir, struct logger *logger)
@@ -151,7 +151,7 @@ void lsw_conf_nssdir(const char *nssdir, struct logger *logger)
 	lsw_conf_calculate();
 
 	if (!streq(global_oco.nssdir, IPSEC_NSSDIR))
-		log_message(RC_LOG, logger, "adjusting nssdir to %s", global_oco.nssdir);
+		log_message(RC_LOG, logger, " adjusting nssdir to %s", global_oco.nssdir);
 }
 
 void lsw_conf_secretsfile(const char *secretsfile)
