d4832836930e2200ceaf4826af2a61f85167a0a8
Paul Wouters <pwouters@redhat.com>
2020-10-14 12:07:21 -0400

    testing: basic-pluto-17-nssdir  can no longer test for F4 exponent
    because we no longer generate :RSA secrets output

--- testing/pluto/basic-pluto-17-nssdir/westrun.sh
+++ testing/pluto/basic-pluto-17-nssdir/westrun.sh
@@ -8,7 +8,5 @@ ls ${IPSEC_TEST_DIR} | egrep '*.db' # should show db files
 ipsec checknss --nssdir ${IPSEC_TEST_DIR}
 ls ${IPSEC_DIR} | egrep '*.db' # should show no db files
 ls ${IPSEC_TEST_DIR} | egrep '*.db' # should show db files
-ipsec newhostkey --nssdir ${IPSEC_TEST_DIR} --output ${IPSEC_TEST_DIR}/test.secrets > /dev/null 2> /dev/null
-# should show F4 (0x010001) is used for new keys
-grep "PublicExponent: 0x010001" ${IPSEC_TEST_DIR}/test.secrets
+ipsec newhostkey --nssdir ${IPSEC_TEST_DIR} > /dev/null 2> /dev/null
 rm -rf ${IPSEC_TEST_DIR}
