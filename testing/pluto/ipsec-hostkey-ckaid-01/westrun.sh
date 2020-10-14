0413bd5d1a0d06038360f1c150b75fa9458a4b79
Paul Wouters <pwouters@redhat.com>
2020-10-14 12:11:50 -0400

    testing: misc update

--- testing/pluto/ipsec-hostkey-ckaid-01/westrun.sh
+++ testing/pluto/ipsec-hostkey-ckaid-01/westrun.sh
@@ -1,7 +1,5 @@
 /testing/guestbin/swan-prep --nokeys
 rm -f /tmp/newhostkey.txt
-ipsec newhostkey --output /tmp/newhostkey.txt
-grep pubkey= /tmp/newhostkey.txt
+ckaid=$(ipsec newhostkey 2>&1 | grep "showhostkey" | sed "s/^.*ckaid //")
 ipsec showhostkey --list
-ckaid=$(ipsec showhostkey --list | sed -e 's/.*ckaid: //')
 ipsec showhostkey --left --ckaid "$ckaid"
