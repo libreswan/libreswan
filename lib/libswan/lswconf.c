422804a2d6d5a5d3853da15689fbae0d6b781dd5
Tuomo Soini <tis@foobar.fi>
2020-10-14 23:51:59 +0300

    libswan: fix comparing nssdir to default NSSDIR
    Fixes error in b0af03e94d3bc27d904e226daffa7975c988425d

--- lib/libswan/lswconf.c
+++ lib/libswan/lswconf.c
@@ -150,7 +150,7 @@ void lsw_conf_nssdir(const char *nssdir, struct logger *logger)
 	subst(&global_oco.nssdir, nssdir, "override nssdir");
 	lsw_conf_calculate();
 
-	if (!streq(global_oco.confddir, IPSEC_NSSDIR))
+	if (!streq(global_oco.nssdir, IPSEC_NSSDIR))
 		log_message(RC_LOG, logger, "adjusting nssdir to %s", global_oco.nssdir);
 }
 
