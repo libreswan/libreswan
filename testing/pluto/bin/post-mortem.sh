a5118f1fff0a1eca4aed7f631d2eeb10a91ac62c
Andrew Cagney <cagney@gnu.org>
2020-10-15 19:16:13 -0400

    testing: in post-mortem.sh, stop pluto and check for leaks, and check for core files
    This means tests always stop pluto and check for cores/leaks before
    the host is shutdown (look in *.console.verbose.txt).  Tests were
    missing core dumps because pluto was only stopped after the test had
    finished (i.e., during shutdown).
    
    There be leaks (but not too many).
    
    (Should probably also stop strongswan, iked, ...)

--- testing/pluto/bin/post-mortem.sh
+++ testing/pluto/bin/post-mortem.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 # Teardown the host, or more pointedly shutdown any running daemons
 # (pluto, strongswan, iked, ...) and then check for core dumps or
 # other problems.
@@ -8,8 +10,44 @@
 # this script exit with a non-zero status then all the output is
 # exposed.
 
-status=0
+ok=true
 
-echo in teardown
 
-exit ${status}
+# Shutdown pluto; what about strongswan / iked / ...
+
+echo shutting down
+
+if test -r /tmp/pluto.log ; then
+    ipsec stop
+fi
+
+
+# Check for core files and if any are found, copy them to the output
+# directory.
+
+echo checking for core files
+
+if $(dirname $0)/check-for-core.sh ; then
+    echo no core files found
+else
+    ok=false
+fi
+
+
+# check there were no memory leaks
+
+echo checking for leaks
+
+if test -r /tmp/pluto.log ; then
+    # check-01 selftests pluto and that doesn't run leak detective so
+    # the absense of 'leak detective found no leaks' isn't sufficient.
+    if grep 'leak detective found [0-9]* leaks' /tmp/pluto.log ; then
+	ok=false
+	grep -e leak /tmp/pluto.log | grep -v -e '|'
+    fi
+fi
+
+
+# tell kvmrunner
+
+${ok}
