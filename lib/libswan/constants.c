libreswan/libreswan

commit 14e07ddcf2f5f25f1602be68d370f988f8f62956

constants: organize Security Protocol ID name tables inline with IETF
According to IETF notify, delete, and proposal are all independant
(they just happen to use the same numbers).

lib/libswan/constants.c
@@ -543,30 +543,6 @@ enum_names ikev1_protocol_names = {
 	NULL
 };
 
-static const char *const ikev2_protocol_name[] = {
-	"PROTO_v2_RESERVED",
-	"PROTO_v2_IKE",
-	"PROTO_v2_AH",
-	"PROTO_v2_ESP",
-};
-
-enum_names ikev2_protocol_names = {
-	PROTO_v2_RESERVED,
-	PROTO_v2_ESP,
-	ARRAY_REF(ikev2_protocol_name),
-	NULL, /* prefix */
-	NULL
-};
-
-/* subset of protocol names accepted by IKEv2 Delete */
-enum_names ikev2_del_protocol_names = {
-	PROTO_ISAKMP,
-	PROTO_IPSEC_ESP,
-	&ikev2_protocol_name[PROTO_ISAKMP], elemsof(ikev2_protocol_name) - PROTO_ISAKMP,
-	NULL, /* prefix */
-	NULL
-};
-
 /* IPsec ISAKMP transform values */
 static const char *const isakmp_transform_name[] = {
 	"KEY_IKE",
@@ -1905,23 +1881,56 @@ const char *const critical_names[] = {
 /*
  * IKEv2 Security Protocol Identifiers
  */
-static const char *const ikev2_sec_proto_id_name[] = {
-	/* 0 - Reserved */
+
+/* proposal payload allows IKE=1, AH=2, ESP=3 */
+
+static const char *const ikev2_proposal_protocol_id_name[] = {
 	"IKEv2_SEC_PROTO_IKE",
 	"IKEv2_SEC_PROTO_AH",
 	"IKEv2_SEC_PROTO_ESP",
-	"IKEv2_SEC_FC_ESP_HEADER",	/* RFC 4595 */
+	"IKEv2_SEC_FC_ESP_HEADER",		/* RFC 4595 */
 	"IKEv2_SEC_FC_CT_AUTHENTICATION",	/* RFC 4595 */
 	/* 6 - 200 Unassigned */
 	/* 201 - 255 Private use */
 };
 
-enum_names ikev2_sec_proto_id_names = {
+enum_names ikev2_proposal_protocol_id_names = {
 	IKEv2_SEC_PROTO_IKE,
 	IKEv2_SEC_FC_CT_AUTHENTICATION,
-	ARRAY_REF(ikev2_sec_proto_id_name),
-	"IKEv2_SEC_PROTO_", /* prefix */
-	NULL
+	ARRAY_REF(ikev2_proposal_protocol_id_name),
+	.en_prefix = "IKEv2_SEC_PROTO_", /* prefix */
+};
+
+/* delete payload allows IKE=1, AH=2, ESP=3 */
+
+static const char *const ikev2_delete_protocol_id_name[] = {
+	"IKEv2_SEC_PROTO_IKE",
+	"IKEv2_SEC_PROTO_AH",
+	"IKEv2_SEC_PROTO_ESP",
+};
+
+enum_names ikev2_delete_protocol_id_names = {
+	IKEv2_SEC_PROTO_IKE,
+	IKEv2_SEC_PROTO_ESP,
+	ARRAY_REF(ikev2_delete_protocol_id_name),
+	.en_prefix = "IKEv2_SEC_PROTO_", /* prefix */
+};
+
+/* notify payload allows NONE=0, (NO IKE,) AH=2, ESP=3 */
+
+static const char *const ikev2_protocol_id_notify_name[] = {
+#define E(V) [V] = #V
+	E(IKEv2_SEC_PROTO_NONE),
+	E(IKEv2_SEC_PROTO_AH),
+	E(IKEv2_SEC_PROTO_ESP),
+#undef E
+};
+
+enum_names ikev2_notify_protocol_id_names = {
+	IKEv2_SEC_PROTO_NONE,
+	IKEv2_SEC_PROTO_ESP,
+	ARRAY_REF(ikev2_protocol_id_notify_name),
+	.en_prefix = "IKEv2_SEC_PROTO_", /* prefix */
 };
 
 /* Transform-type Encryption */
@@ -2220,6 +2229,7 @@ const char *enum_name(enum_names *ed, unsigned long val)
 	for (p = ed; p != NULL; p = p->en_next_range) {
 		passert(p->en_last - p->en_first + 1 == p->en_checklen);
 		if (p->en_first <= val && val <= p->en_last)
+			/* can be NULL */
 			return p->en_names[val - p->en_first];
 	}
 
@@ -2478,8 +2488,6 @@ static const enum_names *en_checklist[] = {
 	&ikev1_exchange_names,
 	&exchange_names_ikev1orv2,
 	&ikev1_protocol_names,
-	&ikev2_protocol_names,
-	&ikev2_del_protocol_names,
 	&isakmp_transformid_names,
 	&ah_transformid_names,
 	&esp_transformid_names,
@@ -2510,7 +2518,9 @@ static const enum_names *en_checklist[] = {
 	&ikev2_notify_names,
 	&ikev2_ts_type_names,
 	&attr_msg_type_names,
-	&ikev2_sec_proto_id_names,
+	&ikev2_proposal_protocol_id_names,
+	&ikev2_delete_protocol_id_names,
+	&ikev2_notify_protocol_id_names,
 	&ikev2_trans_type_encr_names,
 	&ikev2_trans_type_prf_names,
 	&ikev2_trans_type_integ_names,

include/names_constant.h
@@ -59,8 +59,6 @@ extern enum_names ikev2_exchange_names;
 extern enum_names exchange_names_ikev1orv2;
 extern enum_enum_names exchange_type_names;
 extern enum_names ikev1_protocol_names;
-extern enum_names ikev2_protocol_names;
-extern enum_names ikev2_del_protocol_names;	/* subset of protocol names accepted by IKEv2 Delete */
 extern enum_names isakmp_transformid_names;
 extern enum_names ah_transformid_names;
 extern enum_names esp_transformid_names;
@@ -77,8 +75,10 @@ extern enum_names ikev1_notify_names;
 
 /* IKEv2 */
 extern enum_names ikev2_auth_names;
-extern enum_names ikev2_sec_proto_id_names;
 extern enum_names ikev2_hash_algorithm_names;
+extern enum_names ikev2_proposal_protocol_id_names;	/* 1=IKE SA, 2=AH, 3=ESP */
+extern enum_names ikev2_delete_protocol_id_names;	/* 1=IKE SA, 2=AH, 3=ESP */
+extern enum_names ikev2_notify_protocol_id_names;	/* NONE=0, 2=AH, 3=ESP; NOT IKE! */
 extern enum_names ikev2_trans_type_names;
 extern enum_names ikev2_trans_type_encr_names;
 extern enum_names ikev2_trans_type_prf_names;

programs/pluto/ikev2_parent.c
@@ -3867,7 +3867,7 @@ static bool ikev2_rekey_child_resp(struct ike_sa *ike, struct child_sa *child,
 	 * find old state to rekey
 	 */
 	dbg("CREATE_CHILD_SA IPsec SA rekey Protocol %s",
-	    enum_show(&ikev2_protocol_names, rekey_notify->isan_protoid));
+	    enum_show(&ikev2_notify_protocol_id_names, rekey_notify->isan_protoid));
 
 	if (rekey_notify->isan_spisize != sizeof(ipsec_spi_t)) {
 		log_state(RC_LOG, &child->sa,
@@ -3898,7 +3898,7 @@ static bool ikev2_rekey_child_resp(struct ike_sa *ike, struct child_sa *child,
 	    rekey_notify->isan_protoid != PROTO_IPSEC_AH) {
 		log_state(RC_LOG, &child->sa,
 			  "CREATE_CHILD_SA IPsec SA rekey invalid Protocol ID %s",
-			  enum_show(&ikev2_protocol_names, rekey_notify->isan_protoid));
+			  enum_show(&ikev2_notify_protocol_id_names, rekey_notify->isan_protoid));
 		record_v2N_spi_response(child->sa.st_logger, ike, md,
 					rekey_notify->isan_protoid, &spi,
 					v2N_CHILD_SA_NOT_FOUND,
@@ -3908,7 +3908,7 @@ static bool ikev2_rekey_child_resp(struct ike_sa *ike, struct child_sa *child,
 
 	dbg("CREATE_CHILD_S to rekey IPsec SA(0x%08" PRIx32 ") Protocol %s",
 	    ntohl((uint32_t) spi),
-	    enum_show(&ikev2_protocol_names, rekey_notify->isan_protoid));
+	    enum_show(&ikev2_notify_protocol_id_names, rekey_notify->isan_protoid));
 
 	/*
 	 * From 1.3.3.  Rekeying Child SAs with the CREATE_CHILD_SA
@@ -3924,7 +3924,7 @@ static bool ikev2_rekey_child_resp(struct ike_sa *ike, struct child_sa *child,
 		log_state(RC_LOG, &child->sa,
 			  "CREATE_CHILD_SA no such IPsec SA to rekey SA(0x%08" PRIx32 ") Protocol %s",
 			  ntohl((uint32_t) spi),
-			  enum_show(&ikev2_protocol_names, rekey_notify->isan_protoid));
+			  enum_show(&ikev2_notify_protocol_id_names, rekey_notify->isan_protoid));
 		record_v2N_spi_response(child->sa.st_logger, ike, md,
 					rekey_notify->isan_protoid, &spi,
 					v2N_CHILD_SA_NOT_FOUND,
@@ -5429,7 +5429,7 @@ stf_status process_encrypted_informational_ikev2(struct ike_sa *ike,
 						return STF_INTERNAL_ERROR;	/* cannot happen */
 
 					dbg("delete %s SA(0x%08" PRIx32 ")",
-					    enum_show(&ikev2_protocol_names,
+					    enum_show(&ikev2_delete_protocol_id_names,
 						      v2del->isad_protoid),
 					    ntohl((uint32_t) spi));
 
@@ -5453,11 +5453,12 @@ stf_status process_encrypted_informational_ikev2(struct ike_sa *ike,
 					if (dst == NULL) {
 						libreswan_log(
 						    "received delete request for %s SA(0x%08" PRIx32 ") but corresponding state not found",
-							    enum_show(&ikev2_protocol_names, v2del->isad_protoid),
-								ntohl((uint32_t)spi));
+							    enum_show(&ikev2_delete_protocol_id_names,
+								      v2del->isad_protoid),
+						    ntohl((uint32_t)spi));
 					} else {
 						dbg("our side SPI that needs to be deleted: %s SA(0x%08" PRIx32 ")",
-						    enum_show(&ikev2_protocol_names,
+						    enum_show(&ikev2_delete_protocol_id_names,
 							      v2del->isad_protoid), ntohl((uint32_t)spi));
 
 						/* we just received a delete, don't send another delete */

programs/pluto/ikev2_spdb_struct.c
@@ -302,7 +302,7 @@ static void jam_type_transform(jambuf_t *buf, enum ikev2_trans_type type,
 
 static const char *protoid_name(enum ikev2_sec_proto_id protoid)
 {
-	return enum_short_name(&ikev2_sec_proto_id_names, protoid);
+	return enum_short_name(&ikev2_proposal_protocol_id_names, protoid);
 }
 
 /* <TRANSFORM> { "+" <TRANSFORM> }+ */
@@ -1686,7 +1686,7 @@ static bool append_encrypt_transform(struct ikev2_proposal *proposal,
 				     const struct encrypt_desc *encrypt,
 				     unsigned keylen)
 {
-	const char *protocol = enum_short_name(&ikev2_protocol_names, proposal->protoid);
+	const char *protocol = enum_short_name(&ikev2_proposal_protocol_id_names, proposal->protoid);
 	if (proposal->protoid == 0 || protocol == NULL) {
 		PEXPECT_LOG("%s", "IKEv2 ENCRYPT transform protocol unknown");
 		return FALSE;

programs/pluto/packet.c
@@ -917,7 +917,7 @@ static field_desc ikev2prop_fields[] = {
 	{ ft_zig,  8 / BITS_PER_BYTE, "reserved", NULL },
 	{ ft_len, 16 / BITS_PER_BYTE, "length", NULL },
 	{ ft_nat,  8 / BITS_PER_BYTE, "prop #", NULL },
-	{ ft_enum, 8 / BITS_PER_BYTE, "proto ID", &ikev2_sec_proto_id_names },
+	{ ft_enum, 8 / BITS_PER_BYTE, "proto ID", &ikev2_proposal_protocol_id_names },
 	{ ft_nat,  8 / BITS_PER_BYTE, "spi size", NULL },
 	{ ft_nat,  8 / BITS_PER_BYTE, "# transforms", NULL },
 	{ ft_end,  0, NULL, NULL }
@@ -1289,7 +1289,7 @@ static field_desc ikev2_notify_fields[] = {
 	{ ft_pnpc, 8 / BITS_PER_BYTE, "next payload type", &ikev2_payload_names },
 	{ ft_set, 8 / BITS_PER_BYTE, "flags", critical_names },
 	{ ft_len, 16 / BITS_PER_BYTE, "length", NULL },
-	{ ft_enum, 8 / BITS_PER_BYTE, "Protocol ID", &ikev2_protocol_names },
+	{ ft_enum, 8 / BITS_PER_BYTE, "Protocol ID", &ikev2_notify_protocol_id_names },
 	/* names used are v1 names may be we should use 4306 3.3.1 names */
 	{ ft_nat,  8 / BITS_PER_BYTE, "SPI size", NULL },
 	{ ft_loose_enum, 16 / BITS_PER_BYTE, "Notify Message Type",
@@ -1325,7 +1325,7 @@ static field_desc ikev2_delete_fields[] = {
 	{ ft_pnpc, 8 / BITS_PER_BYTE, "next payload type", &ikev2_payload_names },
 	{ ft_set, 8 / BITS_PER_BYTE, "flags", critical_names },
 	{ ft_len, 16 / BITS_PER_BYTE, "length", NULL },
-	{ ft_enum, 8 / BITS_PER_BYTE, "protocol ID", &ikev2_del_protocol_names },
+	{ ft_enum, 8 / BITS_PER_BYTE, "protocol ID", &ikev2_delete_protocol_id_names },
 	{ ft_nat, 8 / BITS_PER_BYTE, "SPI size", NULL },
 	{ ft_nat, 16 / BITS_PER_BYTE, "number of SPIs", NULL },
 	{ ft_end, 0, NULL, NULL }

testing/check/enum/enumcheck.c
@@ -304,8 +304,6 @@ int main(int argc UNUSED, char *argv[])
 	test_enums("ikev2_exchange_names", &ikev2_exchange_names);
 	test_enums("exchange_names_ikev1orv2", &exchange_names_ikev1orv2);
 	test_enums("ikev1_protocol_names", &ikev1_protocol_names);
-	test_enums("ikev2_protocol_names", &ikev2_protocol_names);
-	test_enums("ikev2_del_protocol_names", &ikev2_del_protocol_names);
 	test_enums("isakmp_transformid_names", &isakmp_transformid_names);
 	test_enums("ah_transformid_names", &ah_transformid_names);
 	test_enums("esp_transformid_names", &esp_transformid_names);
@@ -329,7 +327,9 @@ int main(int argc UNUSED, char *argv[])
 	test_enums("xauth_type_names", &xauth_type_names);
 	test_enum_range("xauth_attr_names", &xauth_attr_names, 0, 256);
 	test_enums("attr_msg_type_names", &attr_msg_type_names);
-	test_enums("ikev2_sec_proto_id_names", &ikev2_sec_proto_id_names);
+	test_enums("ikev2_proposal_protocol_id_names", &ikev2_proposal_protocol_id_names);
+	test_enums("ikev2_delete_protocol_id_names", &ikev2_delete_protocol_id_names);
+	test_enums("ikev2_notify_protocol_id_names", &ikev2_notify_protocol_id_names);
 	test_enums("ikev2_auth_names", &ikev2_auth_names);
 	test_enum_range("ikev2_trans_type_encr_names", &ikev2_trans_type_encr_names, 0, 256);
 	test_enums("ikev2_trans_type_prf_names", &ikev2_trans_type_prf_names);

commit f9fada7234b69d069d00d22163229bfe071ef70e

ikev2: allow Protocol ID IKE in Notify
Cisco send Protocol ID IKE(1) in notifications in IKEv2 IKE_INIT.

Commit 14e07ddcf2f5 would not allow "1" and drop the message.

RFC7296 #section-3.10 is a bit vauge.
"If the SPI field is empty, this field MUST be sent as zero and
 MUST be ignored on receipt."

In this case SPI is empty. May be Cisco should not send "1"?
For now lets allow "1".

Pluto log message

| ***parse IKEv2 Vendor ID Payload:
|    next payload type: ISAKMP_NEXT_v2N (0x29)

| Now let's proceed with payload (ISAKMP_NEXT_v2N)
Protocol ID of IKEv2 Notify Payload has an unknown value: 1 (0x1)
"westnet-eastnet-ipv4-psk-ikev2" #1: malformed payload in packet
| processing: STOP state #0 (in process_md() at demux.c:275)

Wireshark disect.

   Payload: Vendor ID (43) : Cisco Copyright
        Next payload: Notify (41)

    Payload: Notify (41) - NAT_DETECTION_SOURCE_IP
        Next payload: Notify (41)
        0... .... = Critical Bit: Not Critical
        .000 0000 = Reserved: 0x00
        Payload length: 28
        Protocol ID: IKE (1)

->>> above is what pluto refused

        SPI Size: 0
        Notify Message Type: NAT_DETECTION_SOURCE_IP (16388)
        Notification DATA: 158a0910b35701b6831b2f3ff90993cd57b993ff
    Payload: Notify (41) - NAT_DETECTION_DESTINATION_IP

Fixes: 14e07ddcf2f5 ("constants: organize Security Protocol ID name tables inline with IETF")

lib/libswan/constants.c
@@ -1923,6 +1923,7 @@ enum_names ikev2_delete_protocol_id_names = {
 static const char *const ikev2_protocol_id_notify_name[] = {
 #define E(V) [V] = #V
 	E(IKEv2_SEC_PROTO_NONE),
+	E(IKEv2_SEC_PROTO_IKE),
 	E(IKEv2_SEC_PROTO_AH),
 	E(IKEv2_SEC_PROTO_ESP),
 #undef E