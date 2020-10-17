libreswan/libreswan

commit f9fada7234b69d069d00d22163229bfe071ef70e

ikev2: allow Protocol ID IKE in Notify
Cisco send Protocol ID IKE(1) in notifications in IKEv2 IKE_INIT.

Commit 14e07ddcf2f5 would not allow "1" and drop the message.

RFC7296 #section-3.10 is a bit vauge.
"If the SPI field is empty, this field MUST be sent as zero and
 MUST be ignored on receipt."

In this case SPI is empty. May be Cisco should not send "1"?
For now lets allow "1".

Pluto log message

| ***parse IKEv2 Vendor ID Payload:
|    next payload type: ISAKMP_NEXT_v2N (0x29)

| Now let's proceed with payload (ISAKMP_NEXT_v2N)
Protocol ID of IKEv2 Notify Payload has an unknown value: 1 (0x1)
"westnet-eastnet-ipv4-psk-ikev2" #1: malformed payload in packet
| processing: STOP state #0 (in process_md() at demux.c:275)

Wireshark disect.

   Payload: Vendor ID (43) : Cisco Copyright
        Next payload: Notify (41)

    Payload: Notify (41) - NAT_DETECTION_SOURCE_IP
        Next payload: Notify (41)
        0... .... = Critical Bit: Not Critical
        .000 0000 = Reserved: 0x00
        Payload length: 28
        Protocol ID: IKE (1)

->>> above is what pluto refused

        SPI Size: 0
        Notify Message Type: NAT_DETECTION_SOURCE_IP (16388)
        Notification DATA: 158a0910b35701b6831b2f3ff90993cd57b993ff
    Payload: Notify (41) - NAT_DETECTION_DESTINATION_IP

Fixes: 14e07ddcf2f5 ("constants: organize Security Protocol ID name tables inline with IETF")

lib/libswan/constants.c
@@ -1923,6 +1923,7 @@ enum_names ikev2_delete_protocol_id_names = {
 static const char *const ikev2_protocol_id_notify_name[] = {
 #define E(V) [V] = #V
 	E(IKEv2_SEC_PROTO_NONE),
+	E(IKEv2_SEC_PROTO_IKE),
 	E(IKEv2_SEC_PROTO_AH),
 	E(IKEv2_SEC_PROTO_ESP),
 #undef E

commit 83ea2c2f2d5cf702a3a4825d5776fbae2748d3e8

ikev2: fix the comment about protocol ID in notify payload
Fixes: f9fada7234b6 ("ikev2: allow Protocol ID IKE in Notify")

lib/libswan/constants.c
@@ -1918,7 +1918,7 @@ enum_names ikev2_delete_protocol_id_names = {
 	.en_prefix = "IKEv2_SEC_PROTO_", /* prefix */
 };
 
-/* notify payload allows NONE=0, (NO IKE,) AH=2, ESP=3 */
+/* notify payload allows NONE=0, IKE=1 AH=2, ESP=3 */
 
 static const char *const ikev2_protocol_id_notify_name[] = {
 #define E(V) [V] = #V