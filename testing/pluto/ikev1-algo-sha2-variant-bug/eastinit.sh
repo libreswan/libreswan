8b7aa8f8b58e4da46c227a20343ae9e0cf075d27
Paul Wouters <pwouters@redhat.com>
2020-11-30 18:31:51 -0500

    testing: test for reported bug on inbound/outbound being the same key

---
+++ testing/pluto/ikev1-algo-sha2-variant-bug/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-ipv4-psk-ikev1
+ipsec status | grep westnet-eastnet-ipv4-psk-ikev1
+echo "initdone"
---
+++ testing/pluto/ikev1-algo-sha2-variant-bug/westinit.sh
@@ -0,0 +1,7 @@
+/testing/guestbin/swan-prep
+ipsec start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec whack --impair delete-on-retransmit
+ipsec auto --add westnet-eastnet-ipv4-psk-ikev1
+ipsec status |grep westnet-eastnet-ipv4-psk-ikev1
+echo "initdone"
