1809d9a5add208131d2a55c9786aac0cfc0cf2e5
Paul Wouters <pwouters@redhat.com>
2020-12-29 13:40:02 -0500

    testing: remove ephemeral output from ikev1-labeled-ipsec-01

--- testing/pluto/ikev1-labeled-ipsec-01/westrun.sh
+++ testing/pluto/ikev1-labeled-ipsec-01/westrun.sh
@@ -3,6 +3,6 @@ echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
 ipsec auto --up labeled
 ip xfrm state
 ip xfrm pol
-echo "quit" | nc -p 4301 -vvv 192.1.2.23 4300
+echo "quit" | nc -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
 ipsec trafficstatus
 echo done
