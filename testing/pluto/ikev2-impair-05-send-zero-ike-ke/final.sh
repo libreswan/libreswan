ef9499f304d24f6fe6e73710ef41b42c0f86693a
Andrew Cagney <cagney@gnu.org>
2021-01-11 14:49:14 -0500

    testing: update nss-cert-chain-01-ikev2 - expect DH failure
    ... rather than invalid syntax

--- testing/pluto/ikev2-impair-05-send-zero-ike-ke/final.sh
+++ testing/pluto/ikev2-impair-05-send-zero-ike-ke/final.sh
@@ -1,4 +1,4 @@
-grep -e v2N_INVALID_KE_PAYLOAD -e v2N_INVALID_SYNTAX /tmp/pluto.log | grep -v -e '^|'
+grep -e v2N_INVALID_KE_PAYLOAD -e v2N_INVALID_SYNTAX -e SEC_ERROR /tmp/pluto.log | grep -v -e '^|'
 : ==== cut ====
 ipsec auto --status
 : ==== tuc ====
