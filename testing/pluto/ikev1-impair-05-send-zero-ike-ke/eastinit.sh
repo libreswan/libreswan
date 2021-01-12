a1499a1ac58a9f345e09771b7c171135b9aa896f
Andrew Cagney <cagney@gnu.org>
2021-01-12 15:02:37 -0500

    testing: update ikev1-impair-05-send-zero-ike-ke; expect NSS error

--- testing/pluto/ikev1-impair-05-send-zero-ike-ke/eastinit.sh
+++ testing/pluto/ikev1-impair-05-send-zero-ike-ke/eastinit.sh
@@ -2,5 +2,6 @@
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec whack --impair ke-payload:0
+ipsec whack --impair suppress-retransmits
 ipsec auto --add westnet-eastnet-ipv4-psk-slow
 echo "initdone"
