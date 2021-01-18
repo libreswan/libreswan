7cb0a36b87e980f1bdb7e7e8d8ecbbeb29e07baa
Andrew Cagney <cagney@gnu.org>
2021-01-18 11:49:37 -0500

    testing: output tweaks

--- testing/pluto/ikev2-child-rekey-07-deadlock/westrun.sh
+++ testing/pluto/ikev2-child-rekey-07-deadlock/westrun.sh
@@ -9,7 +9,7 @@ ipsec whack --rekey-ipsec --name west-east/3x0 --async
 # CHILD #5->#9
 ipsec whack --rekey-ipsec --name west-east/4x0 --async
 sleep 45
-# state #1(STATE_PARENT_I3) #6, #7, #8  and #9 in STATE_V2_IPSEC_I
+# state #1 in STATE_V2_ESTABLISHED_IKE_SA, and #6, #7, #8 and #9 in STATE_V2_ESTABLISHED_CHILD_SA
 # anything other state is a sign of regression
 ipsec status | grep STATE_
 # this is complex grep line susceptible to changes to log lines.
