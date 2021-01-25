2aece0ebd04e28fa574869d6c934eb1670952bde
Andrew Cagney <cagney@gnu.org>
2021-01-25 15:21:35 -0500

    testing: in xauth-pluto-28-twobehindnat, change -j SNAT to -j MASQUERADE
    latter accepts --to-port 40000-60000 forcing the use of ephemeral ports

--- testing/pluto/xauth-pluto-28-twobehindnat/nicinit.sh
+++ testing/pluto/xauth-pluto-28-twobehindnat/nicinit.sh
@@ -2,7 +2,11 @@
 iptables -t nat -F
 iptables -F
 # NAT
-iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j SNAT --to-source 192.1.2.254
+# eth0: 192.1.2.254 - external
+# eth1: 192.1.3.254 - internal
+# iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j SNAT --to-source 192.1.2.254
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE --proto TCP --source 192.1.3.0/24 --to-port 40000-60000
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE --proto UDP --source 192.1.3.0/24 --to-port 40000-60000
 # make sure that we never acidentially let ESP through.
 iptables -I FORWARD 1 --proto 50 -j DROP
 #iptables -I FORWARD 2 --destination 192.0.2.0/24 -j DROP
