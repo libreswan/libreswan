54aa2763046770e52d7e8ee87ba4967d0d80b7dc
Andrew Cagney <cagney@gnu.org>
2021-01-25 14:06:29 -0500

    testing: merge nss-certs.sh into ikev2-x509-39-OU-comma's eastinit.sh
    And store generated certs under OUTPUT/nss (not the shared testing/x509

--- testing/pluto/ikev2-x509-39-OU-comma/westinit.sh
+++ testing/pluto/ikev2-x509-39-OU-comma/westinit.sh
@@ -1,7 +1,7 @@
 /testing/guestbin/swan-prep
 rm -f /etc/ipsec.d/*.*
 ipsec initnss > /dev/null 2> /dev/null
-pk12util -i /testing/x509/nss/client.p12 -d sql:/etc/ipsec.d -K 'foobar' -W 'foobar'
+pk12util -i OUTPUT/nss/client.p12 -d sql:/etc/ipsec.d -K 'foobar' -W 'foobar'
 certutil -M -d sql:/etc/ipsec.d -n cacert -t 'CT,,'
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
