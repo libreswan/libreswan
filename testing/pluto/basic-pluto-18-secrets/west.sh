fb8884fe9d0c77e311dbff34ec79473133672650
Paul Wouters <pwouters@redhat.com>
2021-01-26 19:33:23 -0500

    testing: fixup basic-pluto-18-secrets so it can also run in namespaces
    Remove tests related to secret file creation as that is no longer required
    as we never generate secret files anymore

--- testing/pluto/basic-pluto-18-secrets/west.sh
+++ testing/pluto/basic-pluto-18-secrets/west.sh
@@ -1,25 +1,21 @@
-swan-prep
-rm -f /etc/ipsec.secrets
-
+rm -rf OUTPUT/nss
+mkdir OUTPUT/nss
+ipsec initnss -d OUTPUT/nss
 # generate first key
-ipsec newhostkey --bits 2192 --seeddev /dev/urandom
-test ! -f /etc/ipsec.secrets || echo oops ipsec.secrets was created
-
+ipsec newhostkey --bits 2192 --seeddev /dev/urandom --nssdir OUTPUT/nss
 # generate second key
-ipsec newhostkey --bits 2192 --seeddev /dev/urandom
-test ! -f /etc/ipsec.secrets || echo oops ipsec.secrets was created
-
+ipsec newhostkey --bits 2192 --seeddev /dev/urandom  --nssdir OUTPUT/nss
 # empty the database
-rm /etc/ipsec.d/*db
-ipsec initnss > /dev/null 2> /dev/null
-
+rm -rf OUTPUT/nss
+mkdir OUTPUT/nss
+ipsec initnss -d OUTPUT/nss > /dev/null 2> /dev/null
 # confirm reject too small keysizes and non-multiples of 16 for RSA
 # min size is 2192 (lost to history why not 2048)
-ipsec newhostkey --bits 512 --seeddev /dev/urandom
-ipsec newhostkey --bits 1024 --seeddev /dev/urandom
-ipsec newhostkey --bits 2048 --seeddev /dev/urandom
-ipsec newhostkey --bits 2051 --seeddev /dev/urandom
-ipsec newhostkey --bits 3192 --seeddev /dev/urandom
+ipsec newhostkey --bits  512 --seeddev /dev/urandom --nssdir OUTPUT/nss
+ipsec newhostkey --bits 1024 --seeddev /dev/urandom --nssdir OUTPUT/nss
+ipsec newhostkey --bits 2048 --seeddev /dev/urandom --nssdir OUTPUT/nss
+ipsec newhostkey --bits 2051 --seeddev /dev/urandom --nssdir OUTPUT/nss
+ipsec newhostkey --bits 3192 --seeddev /dev/urandom --nssdir OUTPUT/nss
 # there should be no keys
-ipsec showhostkey --list
-test ! -f /etc/ipsec.secrets || echo oops ipsec.secrets was created
+ipsec showhostkey --list --nssdir OUTPUT/nss
+rm -rf OUTPUT/nss
