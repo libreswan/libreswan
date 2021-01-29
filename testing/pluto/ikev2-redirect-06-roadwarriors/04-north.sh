da55b1a5064aea04e2c301f29a7777189fdb77c7
Andrew Cagney <cagney@gnu.org>
2021-01-29 07:45:52 -0500

    testing: in ikev2-redirect-06-roadwarriors # sanitize-retransmits

--- testing/pluto/ikev2-redirect-06-roadwarriors/04-north.sh
+++ testing/pluto/ikev2-redirect-06-roadwarriors/04-north.sh
@@ -4,5 +4,5 @@ ipsec start
 ipsec auto --add north-east
 ipsec whack --impair revival
 echo initdone
-ipsec auto --up north-east | sed -e "s/192.0.2.10./192.0.2.10X/"
+ipsec auto --up north-east | sed -e "s/192.0.2.10./192.0.2.10X/" # sanitize-retransmits
 sleep 3
