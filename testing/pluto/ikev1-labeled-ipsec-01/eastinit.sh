28e4fa915f4f61c21c040b0868fa80dbf31ebea4
Andrew Cagney <cagney@gnu.org>
2021-02-03 16:11:23 -0500

    testing: move contrib/labeled-ipsec/ to testing/programs/getpeercon_server/
    build/install; update already failing tests.

--- testing/pluto/ikev1-labeled-ipsec-01/eastinit.sh
+++ testing/pluto/ikev1-labeled-ipsec-01/eastinit.sh
@@ -9,7 +9,5 @@ setsebool nis_enabled=1
 runcon system_u:system_r:ipsec_t:s0 ipsec pluto --config /etc/ipsec.conf
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add labeled
-gcc -Wall -o OUTPUT/getpeercon_server -lselinux /source/contrib/labeled-ipsec/getpeercon_server.c
-chmod 755 OUTPUT/getpeercon_server
-runcon -t netutils_t OUTPUT/getpeercon_server 4300 &
+runcon -t netutils_t ipsec getpeercon_server 4300 &
 echo "initdone"
--- testing/pluto/ikev2-labeled-ipsec-01/eastinit.sh
+++ testing/pluto/ikev2-labeled-ipsec-01/eastinit.sh
@@ -6,7 +6,5 @@ rm -f ipsec-test-module.mod ipsec-test-module.pp
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add labeled
-gcc -Wall -o OUTPUT/getpeercon_server -lselinux /source/contrib/labeled-ipsec/getpeercon_server.c
-chmod 755 OUTPUT/getpeercon_server
-OUTPUT/getpeercon_server 4300 &
+ipsec getpeercon_server 4300 &
 echo "initdone"
