a859f3baf6847883a42e0ebfb095d5a9a1df981b
Andrew Cagney <cagney@gnu.org>
2021-02-11 22:16:49 -0500

    testing: in newoe-01-whack trigger an ICMP ping request
    ... and expect:
    
      002 initiate on demand from 192.1.2.45:8 to 192.1.2.23:0 proto=1 because: whack
      002 can't find expected bare shunt to delete: 192.1.2.45->192.1.2.23 transport_proto='1'
    
    ??? what is the nature of those mismatching acquires?
    
    When initiating the above OE connection, the kernel code first
    inserts the shunt:
        192.1.2.45:8--1-->192.1.2.23:0 %acquire
    for the original packet.  fiddle_bare_shunt() then adds the wider shunt:
        192.1.2.45/32--*-->192.1.2.23/32
    before deleting the %acquire shunt.  Since whack does not add the
    %acquire shunt, it can't delete it.

--- testing/pluto/newoe-01-whack/westrun.sh
+++ testing/pluto/newoe-01-whack/westrun.sh
@@ -1,4 +1,5 @@
-ipsec whack --oppohere 192.1.2.45 --oppothere 192.1.2.23
+# ICMP ping; expect error from trying to delete the kernel's acquire shunt
+ipsec whack --oppohere 192.1.2.45 --oppothere 192.1.2.23 --oppoproto 1 --opposport 8 --oppodport 0
 ping -n -c 4 192.1.2.23
 # should show traffic
 ipsec whack --trafficstatus
