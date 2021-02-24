e70de51c20fd8907c4121944755fd6b3945ab854
Paul Wouters <pwouters@redhat.com>
2021-02-24 14:54:13 -0500

    testing: Show a bit more kernel state in ikev2-tcp-00-yes

--- testing/pluto/ikev2-tcp-00-yes/westrun.sh
+++ testing/pluto/ikev2-tcp-00-yes/westrun.sh
@@ -4,5 +4,6 @@ ping -n -c4 -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
 # should show tcp being used
 ../../pluto/bin/ipsec-look.sh | grep encap
+../../pluto/bin/ipsec-look.sh
 ipsec auto --down ikev2-westnet-eastnet
 echo "done"
