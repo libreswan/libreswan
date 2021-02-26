c2f77890d9d48e2061bd6eca36412f81a9f83e77
Paul Wouters <pwouters@redhat.com>
2021-02-25 21:39:50 -0500

    testing: added ikev2-udp-05-transport-mode

---
+++ testing/pluto/ikev2-udp-05-transport-mode/westrun.sh
@@ -0,0 +1,8 @@
+ipsec auto --up ikev2-west-east
+ping -n -c4 192.1.2.23
+ipsec whack --trafficstatus
+# should show tcp being used
+../../pluto/bin/ipsec-look.sh | grep encap
+../../pluto/bin/ipsec-look.sh
+ipsec auto --down ikev2-west-east
+echo "done"
