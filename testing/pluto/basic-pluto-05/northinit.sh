afce7b916987fc08bbed80e0cccd57787d96f9e4
Andrew Cagney <cagney@gnu.org>
2021-03-21 13:08:59 -0400

    testing: sprinkle ping-once.sh

--- testing/pluto/basic-pluto-05/northinit.sh
+++ testing/pluto/basic-pluto-05/northinit.sh
@@ -1,10 +1,12 @@
 /testing/guestbin/swan-prep
-../../pluto/bin/wait-until-alive -I 192.0.1.254 192.0.2.254
+../../pluto/bin/wait-until-alive -I 192.0.3.254 192.0.2.254
+
 # ensure that clear text does not get through
 iptables -A INPUT -i eth1 -s 192.0.2.254/32 -j DROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
 # confirm clear text does not get through
-ping -c 4 -n -I 192.0.3.254 192.0.2.254
+../../pluto/bin/ping-once.sh --down -I 192.0.3.254 192.0.2.254
+
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add northnet-eastnet-nonat
--- testing/pluto/ikev1-algo-ike-pfs-01/northinit.sh
+++ testing/pluto/ikev1-algo-ike-pfs-01/northinit.sh
@@ -1,4 +1,6 @@
 /testing/guestbin/swan-prep
+../../pluto/bin/wait-until-alive -I 192.0.3.254 192.0.2.254
+
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add northnet-eastnet-nonat
