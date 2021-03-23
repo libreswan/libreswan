c9f9b3c6bf6b3267ef1b42f3782ca84617ca15ff
Paul Wouters <pwouters@redhat.com>
2021-03-22 20:43:51 -0400

    testing: updated ikev2-labeled-ipsec-03-multi-acquires-enforced  but still fails

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/final.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/final.sh
@@ -1,4 +1,5 @@
 ../../pluto/bin/ipsec-look.sh
 ../bin/check-for-core.sh
-semodule -r ipsec-test-module
+semodule -r ipsecspd
+rm -rf tmp ipsecspd.fc ipsecspd.if
 if [ -f /sbin/ausearch ]; then ausearch -ts recent -m AVC | audit2allow ; fi
