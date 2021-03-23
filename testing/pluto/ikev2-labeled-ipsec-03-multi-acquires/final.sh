1d2312d418716ff21a4be0a5affe20017f746fa5
Paul Wouters <pwouters@redhat.com>
2021-03-22 20:44:13 -0400

    testing: update ikev2-labeled-ipsec-03-multi-acquires-enforced
    passes again

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/final.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/final.sh
@@ -1,3 +1,5 @@
 ../../pluto/bin/ipsec-look.sh
+semodule -r ipsecspd
+rm -rf tmp ipsecspd.fc ipsecspd.if
 ../bin/check-for-core.sh
 if [ -f /sbin/ausearch ]; then ausearch -ts recent -m AVC | audit2allow ; fi
