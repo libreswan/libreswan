62406cfa1d8cdb23adb22c0a4c464b84fe95d5a6
Andrew Cagney <cagney@gnu.org>
2021-03-23 15:46:17 -0400

    testing: use 00-nic.sh.et.al. in xauth-pluto-25-mixed-addresspool
    Improve determinism; test still fails :-(

--- testing/pluto/xauth-pluto-25-mixed-addresspool/02-north.sh
+++ testing/pluto/xauth-pluto-25-mixed-addresspool/02-north.sh
@@ -1,9 +1,14 @@
 /testing/guestbin/swan-prep
+../../pluto/bin/wait-until-alive -I 192.0.3.254 192.0.2.254
+
 ipsec start
 /testing/pluto/bin/wait-until-pluto-started
+
 ../bin/block-non-ipsec.sh
+
 ipsec auto --add north-east
 ipsec whack --xauthname 'xnorth' --xauthpass 'use1pass' --name north-east --initiate
+
 # note - initiating during "init" when multiple hosts/namespaces are starting up is unwise - race conditions
 # let east startup and load conns
 sleep 10
