477793c2f2606aeba3aacd24beb0a0158708d175
Andrew Cagney <cagney@gnu.org>
2021-03-24 08:56:44 -0400

    testing: sprinkle ping-once.sh --up

--- testing/pluto/basic-pluto-02/westrun.sh
+++ testing/pluto/basic-pluto-02/westrun.sh
@@ -1,5 +1,5 @@
 ipsec auto --up westnet-all
-ping -n -c 4 -I 192.0.1.254 192.0.2.254
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus
 ip route list
 # testing re-orienting
--- testing/pluto/netkey-vti-03/westrun.sh
+++ testing/pluto/netkey-vti-03/westrun.sh
@@ -3,7 +3,7 @@ ipsec auto --up westnet-eastnet-vti-02
 # our two ranges should orute into the vti device
 ip route list
 ../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-ping -n -c 4 -I 10.0.1.254 10.0.2.254
+../../pluto/bin/ping-once.sh --up -I 10.0.1.254 10.0.2.254
 ipsec whack --trafficstatus
 # show packets went via ipsec0
 ifconfig ipsec0
