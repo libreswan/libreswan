dd535d36e02f2f8141b6aa0f9f1dbf736959c610
Paul Wouters <pwouters@redhat.com>
2021-03-26 14:17:53 -0400

    testing: updated ikev2-systemrole-01

--- testing/pluto/ikev2-systemrole-01/04-east-restart.sh
+++ testing/pluto/ikev2-systemrole-01/04-east-restart.sh
@@ -1,5 +1,4 @@
 # New files should have dropped in, and we are ready to restart
-# NOTE: for now only works on KVM/qemu until we support *.sh in nsrun
 ipsec restart
 /testing/pluto/bin/wait-until-pluto-started
 ipsec status
--- testing/pluto/ikev2-systemrole-01/05-west-restart-test.sh
+++ testing/pluto/ikev2-systemrole-01/05-west-restart-test.sh
@@ -1,11 +1,12 @@
 # New files should have dropped in, and we are ready to restart
-# NOTE: for now only works on KVM/qemu until we support *.sh in nsrun
 ipsec restart
 /testing/pluto/bin/wait-until-pluto-started
 ipsec status
 # this assumes conection loaded with auto=ondemand
 # trigger tunnel - the first trigger ping packet is lost
 ../../pluto/bin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
+sleep 2
+ping -c4 -I 192.1.2.45 192.1.2.23
 # show non-zero IPsec traffic counters
 ipsec whack --trafficstatus
 echo done
