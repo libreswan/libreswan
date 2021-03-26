3fa4aec092551973a66e06cdd3f8eb39c8e7f0c2
Paul Wouters <pwouters@redhat.com>
2021-03-25 22:59:18 -0400

    testing: some more labeled ipsec updates.
    Still some audit hits due to wrong policies, but those look self inflicted.

--- testing/pluto/ikev1-labeled-ipsec-01/westinit.sh
+++ testing/pluto/ikev1-labeled-ipsec-01/westinit.sh
@@ -1,12 +1,10 @@
 /testing/guestbin/swan-prep
-checkmodule -M -m -o ipsec-test-module.mod ipsec-test-module.te
-semodule_package -o ipsec-test-module.pp -m ipsec-test-module.mod
-semodule -i ipsec-test-module.pp > /dev/null 2>/dev/null
-rm -f ipsec-test-module.mod ipsec-test-module.pp
+make -f /usr/share/selinux/devel/Makefile ipsecspd.pp 2> /dev/null
+semodule -i ipsecspd.pp > /dev/null 2>/dev/null
+rm ipsecspd.pp
 setsebool domain_can_mmap_files=1
 setsebool nis_enabled=1
-#ipsec start
-runcon system_u:system_r:ipsec_t:s0 ipsec pluto --config /etc/ipsec.conf
+ipsec start
 /testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add labeled
 echo "initdone"
