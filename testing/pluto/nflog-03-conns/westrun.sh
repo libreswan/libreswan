3557f79469d90475d85c011c382df29e1f3f46b1
Andrew Cagney <cagney@gnu.org>
2021-03-27 19:22:33 -0400

    testing: sprinkle ping-once.sh
    Interesting things here:
    - use multiple ping-once.sh's so that north/road have
      different packet counts
    - use "set +m" to supress shell bg messages; and
      "wait" to sync up a background tcpdump

--- testing/pluto/nflog-03-conns/westrun.sh
+++ testing/pluto/nflog-03-conns/westrun.sh
@@ -4,15 +4,17 @@ iptables -L -n
 ipsec auto --up west-east-nflog
 iptables -L -n
 rm -fr /tmp/nflog-50.pcap
-(tcpdump -c 8 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 >/dev/null 2>/dev/null &)
-ping -n -c 2 -I 192.1.2.45 192.1.2.23
-ping -n -c 2 -I 192.0.1.254 192.0.2.254
-ping -n -c 2 -I 192.1.2.45 192.1.2.23
-ping -n -c 2 -I 192.0.1.254 192.0.2.254
+set +m # silence job control
+tcpdump -c 4 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 >/dev/null 2>&1 &
+../../pluto/bin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../pluto/bin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+wait
+cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
+tcpdump -n -r OUTPUT/nflog-50.pcap 2>/dev/null
 ipsec auto --down westnet-eastnet-nflog
 iptables -L -n
 ipsec auto --down west-east-nflog
 iptables -L -n
-cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
-tcpdump -n -r OUTPUT/nflog-50.pcap 2>/dev/null
 echo done
