c60a54c91020451b5398c4cb81ab32d977c0e17c
Andrew Cagney <cagney@gnu.org>
2021-03-28 11:11:47 -0400

    testing: use same "set +m; tcpdump & wait" trick in all nflog tests
    vis:
    
    	# suppress job monitoring; specify packet count
    	rm -f /tmp/nflog-50.pcap /tmp/tcpdump.log
    	set +m
    	tcpdump -c 4 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 > /tmp/tcpdump.log 2>&1 &
    	../../pluto/bin/wait-for.sh --match 'listening on' -- cat /tmp/tcpdump.log
    
    	../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
    	../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
    
    	# wait for count to reach tcpdump then dump it
    	wait
    	cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
    	tcpdump -n -r OUTPUT/nflog-50.pcap 2>/dev/null
    
    perhaps this should be a script

--- testing/pluto/nflog-02-conn/westrun.sh
+++ testing/pluto/nflog-02-conn/westrun.sh
@@ -1,12 +1,23 @@
 iptables -L -n
 ipsec auto --up westnet-eastnet-ikev2
 iptables -L -n
-rm -fr /tmp/nflog-50.pcap
-rm -fr /tmp/nflog-50.log
-(tcpdump -c 8 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 > /dev/null 2>&1 &)
-ping -n -c 5 -I 192.0.1.254 192.0.2.254
+
+# suppress job monitoring; specify packet count
+rm -f /tmp/nflog-50.pcap /tmp/tcpdump.log
+set +m
+tcpdump -c 4 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 > /tmp/tcpdump.log 2>&1 &
+../../pluto/bin/wait-for.sh --match 'listening on' -- cat /tmp/tcpdump.log
+
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+
 ipsec auto --down westnet-eastnet-ikev2
 iptables -L -n
-cp /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
-tcpdump -n -r /tmp/nflog-50.pcap 2>/dev/null
+
+# wait for count to reach tcpdump then dump it
+wait
+cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
+tcpdump -n -r OUTPUT/nflog-50.pcap 2>/dev/null
+
+
 echo done
--- testing/pluto/nflog-03-conns/westrun.sh
+++ testing/pluto/nflog-03-conns/westrun.sh
@@ -3,18 +3,26 @@ ipsec auto --up westnet-eastnet-nflog
 iptables -L -n
 ipsec auto --up west-east-nflog
 iptables -L -n
-rm -fr /tmp/nflog-50.pcap
-set +m # silence job control
-tcpdump -c 4 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 >/dev/null 2>&1 &
+
+# suppress job monitoring; specify packet count
+rm -f /tmp/nflog-50.pcap /tmp/tcpdump.log
+set +m
+tcpdump -c 4 -s 0 -w /tmp/nflog-50.pcap -i nflog:50 > /tmp/tcpdump.log 2>&1 &
+../../pluto/bin/wait-for.sh --match 'listening on' -- cat /tmp/tcpdump.log
+
 ../../pluto/bin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
 ../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ../../pluto/bin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
 ../../pluto/bin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-wait
-cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
-tcpdump -n -r OUTPUT/nflog-50.pcap 2>/dev/null
+
 ipsec auto --down westnet-eastnet-nflog
 iptables -L -n
 ipsec auto --down west-east-nflog
 iptables -L -n
+
+# wait for count to reach tcpdump then dump it
+wait
+cp  /tmp/nflog-50.pcap OUTPUT/nflog-50.pcap
+tcpdump -n -r OUTPUT/nflog-50.pcap 2>/dev/null
+
 echo done
