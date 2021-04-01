497fc9f9497b17035494ca3c166fe4bb7a277391
Paul Wouters <pwouters@redhat.com>
2021-03-31 21:17:44 -0400

    pluto: port squash:     move clone_slot to connections.c
    from commit f0461b565b8f9

--- programs/pluto/connections.c
+++ programs/pluto/connections.c
@@ -2617,11 +2617,8 @@ struct connection *find_connection_for_clients(struct spd_route **srp,
 		*srp = best_sr;
 
 	// if (clone_cpu_id == UINT32_MAX  && c->sa_clones != 0) {
-	if (c->sa_clones != 0) {
-		best = clone_slot(c, clone_cpu_id);
-		if (best == NULL)  {
-			llog(RC_LOG, c->logge, "AA_2020 %s %d no free clone connection for clonse = %u", __func__, __LINE__, c->sa_clones);
-		}
+	if (best != NULL && best->sa_clones != 0) {
+		best = clone_slot(best, clone_cpu_id);
 	}
 
 	if (DBGP(DBG_BASE)) {
