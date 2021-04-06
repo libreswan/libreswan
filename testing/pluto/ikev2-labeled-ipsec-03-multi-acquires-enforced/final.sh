3e9bfbec187aaec1fa6e742de38f01862b58fdcf
Kavinda Wewegama <kavinda.wewegama@forcepoint.com>
2021-04-06 10:54:56 -0400

    testing: fix SELinux issues in ikev2-labeled-ipsec-03-multi-acquires-enforced
    Signed-off-by: Paul Wouters <pwouters@redhat.com>

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/final.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/final.sh
@@ -1,4 +1,4 @@
 ../../guestbin/ipsec-look.sh
 semodule -r ipsecspd
 rm -rf tmp ipsecspd.fc ipsecspd.if
-if [ -f /sbin/ausearch ]; then ausearch -ts recent -m AVC | audit2allow ; fi
+if [ -f /sbin/ausearch ]; then ausearch -ts recent -m AVC | audit2allow -R ; fi
