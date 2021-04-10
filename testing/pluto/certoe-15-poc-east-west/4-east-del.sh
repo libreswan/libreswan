6e374ea9225fbb6ed70f51f4a1eee536d79783c8
Andrew Cagney <cagney@gnu.org>
2021-04-10 09:06:39 -0400

    testing: don't sleep

--- testing/pluto/certoe-15-poc-east-west/4-east-del.sh
+++ testing/pluto/certoe-15-poc-east-west/4-east-del.sh
@@ -1,8 +1,7 @@
 # confirm tunnel is up
 ipsec whack --trafficstatus
-# east sends a delete by restarting
+# east sends a delete by restarting; # give OE conns time to load
 ipsec restart
-# give OE conns time to load
 ../../guestbin/wait-until-pluto-started
 # should be empty
 ipsec status |grep STATE_
--- testing/pluto/certoe-17-asymmetric-cert-nat-testing-server-restarts/4-east-clean-ipsec-restart.sh
+++ testing/pluto/certoe-17-asymmetric-cert-nat-testing-server-restarts/4-east-clean-ipsec-restart.sh
@@ -1,8 +1,7 @@
 # confirm tunnel is up
 ipsec whack --trafficstatus
-# restart ipsec
+# restart ipsec; give OE conns time to load
 ipsec restart
-# give OE conns time to load
-sleep 5
+../../guestbin/wait-until-pluto-started
 # should be empty
 ipsec status |grep STATE_
