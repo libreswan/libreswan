libreswan/libreswan

commit a3dba042d4e522a986322a9ded83f258eaa385c4

pluto: disable interface-ip= as the feature is not implemented or ready
The keyword now gives an error. A whack option for this seems to have
not been implemented yet.

lib/libipsecconf/keywords.c
@@ -450,7 +450,10 @@ const struct keyword_def ipsec_conf_keywords[] = {
   { "subnets",  kv_conn | kv_leftright,  kt_appendlist,  KSCF_SUBNETS, NULL, NULL, },
   { "sourceip",  kv_conn | kv_leftright,  kt_ipaddr,  KSCF_SOURCEIP, NULL, NULL, },
   { "ikeport",  kv_conn | kv_leftright,  kt_number,  KNCF_IKEPORT, NULL, NULL, },
+#if 0
+  /* requires pluto refcounting IP/mask addresses on ipsec interface
   { "interface-ip", kv_conn | kv_leftright,  kt_subnet, KSCF_INTERFACE_IP, NULL, NULL, },
+#endif
   { "vti",  kv_conn | kv_leftright | kv_processed,  kt_subnet,  KSCF_VTI_IP, NULL, NULL, },
   { "nexthop",  kv_conn | kv_leftright,  kt_ipaddr,  KSCF_NEXTHOP, NULL, NULL, },
   { "updown",  kv_conn | kv_leftright,  kt_filename,  KSCF_UPDOWN, NULL, NULL, },

configs/d.ipsec.conf/ipsec-interface.xml
@@ -43,6 +43,7 @@ devices ipsec0 and ipsec16384 cannot both be in use.
 <term><emphasis remap='B'>interface-ip=</emphasis></term>
 <listitem>
 <para>
+NOTE: This option is currently not implemented pending pluto IP address reference counting.
 The IP address and netmask to configure on a virtual device (eg ipsecXXX). This is often
 used when building Routing based IPsec tunnels using transport mode and GRE, but can also be useful in
 other scenarios. Currently requires <emphasis remap='I'>ipsec-interface=</emphasis>. See also

commit 41c30fc8aecfb627545178b7124cf99d7e179848

building: fix build by adding comment end
Fixes minor issue caused by a3dba042d4e522a986322a9ded83f258eaa385c4

lib/libipsecconf/keywords.c
@@ -451,7 +451,7 @@ const struct keyword_def ipsec_conf_keywords[] = {
   { "sourceip",  kv_conn | kv_leftright,  kt_ipaddr,  KSCF_SOURCEIP, NULL, NULL, },
   { "ikeport",  kv_conn | kv_leftright,  kt_number,  KNCF_IKEPORT, NULL, NULL, },
 #if 0
-  /* requires pluto refcounting IP/mask addresses on ipsec interface
+  /* requires pluto refcounting IP/mask addresses on ipsec interface */
   { "interface-ip", kv_conn | kv_leftright,  kt_subnet, KSCF_INTERFACE_IP, NULL, NULL, },
 #endif
   { "vti",  kv_conn | kv_leftright | kv_processed,  kt_subnet,  KSCF_VTI_IP, NULL, NULL, },