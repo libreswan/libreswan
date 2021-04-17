9267a3fd5da79cabaa242d7aca00c826f395319c
Andrew Cagney <cagney@gnu.org>
2021-04-16 20:53:45 -0400

    testing: make newoe-25-cat-3-4-way more stable
    The main thing is that scripts like westinit.sh which both:
       - started pluto
       - sent pings
    were split into separate -init.sh and -run.sh scripts.

--- testing/pluto/newoe-25-cat-3-4-way/4-road-init.sh
+++ testing/pluto/newoe-25-cat-3-4-way/4-road-init.sh
@@ -7,6 +7,5 @@ ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits
 # give OE policies time to load
-../../guestbin/wait-for.sh --match 'loaded 9,' -- ipsec auto --status
-ipsec auto --status
+../../guestbin/wait-for.sh --match 'loaded 10,' -- ipsec auto --status
 echo "initdone"
