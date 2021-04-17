fbd61d8e3ccbc1ecb4841365a19e6eb1c502b611
Antony Antony <antony@phenome.org>
2021-04-17 18:35:59 +0000

    testing: ikev2-55-ipseckey-06 use swan prep
    this should work by now. swan-prep dns tests also run in namesapce

--- testing/pluto/ikev2-55-ipseckey-06/nicinit.sh
+++ testing/pluto/ikev2-55-ipseckey-06/nicinit.sh
@@ -1,4 +1,8 @@
 setenforce Permissive
-../../guestbin/start-dns.sh
+/testing/guestbin/swan-prep --dnssec
+dig +short  @127.0.0.1  road.testing.libreswan.org  IPSECKEY | sort
+: ==== cut ====
+dig +short @192.1.2.254 chaos version.server txt
+: ==== tuc ====
 echo done
 : ==== end ====
