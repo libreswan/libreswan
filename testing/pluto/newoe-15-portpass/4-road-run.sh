beedb41c7efa152351a6e905f41aa77095a01246
Andrew Cagney <cagney@gnu.org>
2021-04-21 16:26:17 -0400

    testing: tweaks

--- testing/pluto/newoe-15-portpass/4-road-run.sh
+++ testing/pluto/newoe-15-portpass/4-road-run.sh
@@ -1,9 +1,8 @@
-ping -n -q -c 2 -I 192.1.3.209 192.1.2.23
-# wait on OE retransmits and rekeying
-sleep 5
+# trigger OE; should establish
+../../guestbin/ping-once.sh --forget -I 192.1.3.209 192.1.2.23
+../../guestbin/wait-for.sh --match private-or-clear -- ipsec whack --trafficstatus
 # ping should succeed through tunnel
 ../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
-sleep 5
 ipsec whack --trafficstatus
 # echo should go through passthrough, not increase traffic counter of tunnel
 echo "PLAINTEXT" | nc -s 192.1.3.209 192.1.2.23 22
