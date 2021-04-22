10238e535d069ce0fd2fb9cf4025f76126244662
Andrew Cagney <cagney@gnu.org>
2021-04-22 14:27:43 -0400

    testing: avoid 25% packet loss
    certoe-17-ike-replace-initiator is worth a note
    
    It changes:
    
     # sleep 20 ...
     # ipsec status | grep "STATE_"
     000 #24: "private#192.1.2.0/24"[1] ...192.1.2.23:500 STATE_V2_ESTABLISHED_CHILD_SA (IPsec SA established); EVENT_SA_REKEY in XXs; newest IPSEC; eroute owner; isakmp#26; idle;
     000 #26: "private#192.1.2.0/24"[1] ...192.1.2.23:500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); EVENT_SA_REKEY in XXs; newest ISAKMP; idle;
    
    to:
    
     # ../../guestbin/wait-for.sh --match '#26:.*established IKE SA' -- ipsec status
     000 #26: "private#192.1.2.0/24"[1] ...192.1.2.23:500 STATE_V2_ESTABLISHED_IKE_SA (established IKE SA); EVENT_SA_REKEY in XXs; newest ISAKMP; idle;
    
    so that things have some built in flex

--- testing/pluto/basic-pluto-13-netkey-ondemand/2-west-init.sh
+++ testing/pluto/basic-pluto-13-netkey-ondemand/2-west-init.sh
@@ -3,6 +3,5 @@
 ../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
 ipsec start
 ../../guestbin/wait-until-pluto-started
-sleep 4 # wait for addconn thread
 ipsec auto --status | grep westnet-eastnet-route
 echo "initdone"
