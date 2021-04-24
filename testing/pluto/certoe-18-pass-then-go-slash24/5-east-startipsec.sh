e59731c20f457fd027566964af603dcbe11de7d3
Andrew Cagney <cagney@gnu.org>
2021-04-23 22:33:38 -0400

    testing: cleanup certoe-18-pass-then-go-slash{24,32}{,-keyingtries1}

--- testing/pluto/certoe-18-pass-then-go-slash24/5-east-startipsec.sh
+++ testing/pluto/certoe-18-pass-then-go-slash24/5-east-startipsec.sh
@@ -2,8 +2,10 @@ ipsec start
 ../../guestbin/wait-until-pluto-started
 # give OE policies time to load
 ../../guestbin/wait-for.sh --match 'loaded 11,' -- ipsec auto --status
-# trigger OE
-ping -n -q -c 4 -I 192.1.2.23 192.1.3.209
-sleep 1
-# traffic minus first packet should have flown through tunnel
+# trigger OE; traffic minus first packet should have flown through tunnel
+../../guestbin/ping-once.sh --forget -I 192.1.2.23 192.1.3.209
+../../guestbin/wait-for.sh --match private-or-clear -- ipsec trafficstatus
+../../guestbin/ping-once.sh --up -I 192.1.2.23 192.1.3.209
+../../guestbin/ping-once.sh --up -I 192.1.2.23 192.1.3.209
+../../guestbin/ping-once.sh --up -I 192.1.2.23 192.1.3.209
 ipsec trafficstatus
