b89622ad2e52aa564c815c5a8778f6d82d3acd71
Andrew Cagney <cagney@gnu.org>
2021-05-07 14:33:57 -0400

    testing: sort output from dig

--- testing/pluto/ikev2-55-ipseckey-01/roadinit.sh
+++ testing/pluto/ikev2-55-ipseckey-01/roadinit.sh
@@ -1,7 +1,7 @@
 /testing/guestbin/swan-prep
 ipsec start
 ../../guestbin/wait-until-pluto-started
-dig +short  @192.1.3.254 road.testing.libreswan.org  IPSECKEY
+dig +short  @192.1.3.254 road.testing.libreswan.org  IPSECKEY | sort
 ipsec auto --add road-east-ikev2
 ipsec whack --impair suppress-retransmits
 echo "initdone"
