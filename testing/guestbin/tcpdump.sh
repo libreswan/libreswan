9ae28b222eb9adfd29f479f670232e43a929e229
Andrew Cagney <cagney@gnu.org>
2021-05-13 16:54:04 -0400

    testing: use tweaked tcpdump.sh in ikev2-xfrmi-09-systemd-networkd
    Add --kill which skips dump.  Print progress.
    Update tcpdump.sed.

--- testing/guestbin/tcpdump.sh
+++ testing/guestbin/tcpdump.sh
@@ -9,7 +9,6 @@ fi
 interface="eth1"
 sync_wait_stop=5
 sync_wait_start=1
-sync_wai=${sync_wait_start}
 this_host=$(hostname)
 host=${this_host}
 
@@ -20,10 +19,15 @@ Usage:
 
     $0 --start -i <ethX> [--host <hostname>]
     $0 --stop [--host <hostname>]
+    $0 --kill [--host <hostname>]
 
-Start tcpdump with host test specific name and pid /tmp/
+Start tcpdump saving output to /tmp/
 
-Stop and read tcpdump output to the console
+Stop tcpdump and dump captured output to the console
+
+Kill tcpdump
+
+Specify --host to only run on <hostname>.
 
 EOF
 }
@@ -33,7 +37,7 @@ if test $# -lt 1; then
 	exit 1
 fi
 
-OPTIONS=$(getopt -o h,i: --long verbose,help,testname:,host:,start,stop -- "$@")
+OPTIONS=$(getopt -o h,i: --long help,host:,start,stop,kill -- "$@")
 if (( $? != 0 )); then
     err 4 "Error calling getopt"
 fi
@@ -63,6 +67,10 @@ while true; do
 			action="stop"
 			shift
 			;;
+		--kill )
+			action="kill"
+			shift
+			;;
 		* )
 			shift
 			break
@@ -74,41 +82,59 @@ function set_file_names()
 {
 	tmp_dir=/tmp
 	testname=$(basename ${PWD})
-	out_file="${host}.${testname}.pcap"
-	out_path="${tmp_dir}/${out_file}"
-	pid_path="${tmp_dir}/${host}.${testname}.pid"
+	out_path="${tmp_dir}/${host}.${testname}.tcpdump.pcap"
+	log_path="${tmp_dir}/${host}.${testname}.tcpdump.log"
+	pid_path="${tmp_dir}/${host}.${testname}.tcpdump.pid"
 }
 
-function start_tcpudmp()
+function start_tcpdump()
 {
-	set_file_names
 	# call stop if there are any previous runawy tcpdump
-	sync_wait=${sync_wait_start}
-	stop_tcpudmp
-	rm -fr ${out_path}
-	rm -fr ${pid_path}
-	tcpdump -s 0 -i ${interface} -w ${out_path} 2>/dev/null > /dev/null & echo $! > ${pid_path}
+	stop_tcpdump
+	rm -f ${out_path}
+	rm -f ${log_path}
+	rm -f ${pid_path}
+	tcpdump -s 0 -i ${interface} -w ${out_path} > ${log_path} 2>&1 &
+	echo $! > ${pid_path}
+	sleep ${sync_wait_start}
+	echo tcpdump started
 }
 
-function stop_tcpudmp()
+function stop_tcpdump()
 {
-	set_file_names
-	pid=$(cat ${pid_path} 2>/dev/null) || true
-	kill -TERM -p ${pid} 2>/dev/null > /dev/null || true
-	# for tcpudump output to write and sync
-	sleep ${sync_wait}; sync;
-	rm -fr ${pid_path}
+    if test -r ${pid_path} ; then
+	pid=$(cat ${pid_path})
+	if kill -TERM ${pid} ; then
+	    # wait for tcpudump output to write and sync
+	    sleep 1
+	    while kill -0 ${pid} > /dev/null 2>&1 ; do
+		sleep 1
+	    done
+	    cp ${out_path} OUTPUT/
+	    cp ${log_path} OUTPUT/
+	    rm -f ${pid_path}
+	    echo tcpdump stopped
+	fi
+    else
+	echo tcpdump is not running
+    fi
 }
 
 if [ "${host}" != "${this_host}" ]; then
 	exit 0
 fi
 
-if [ "${action}" = "start" ]; then
-	start_tcpudmp
-elif [ "${action}" = "stop" ]; then
-	sync_wait=${sync_wait_stop}
-	stop_tcpudmp
-	cp ${out_path} OUTPUT/
-	tcpdump -n -r OUTPUT/${out_file} not arp and not ip6 and not stp
-fi
+set_file_names
+
+case "${action}" in
+    start)
+	start_tcpdump
+	;;
+    stop)
+	stop_tcpdump
+	tcpdump -n -r ${out_path} not arp and not icmp6 and not stp
+	;;
+    kill)
+	stop_tcpdump
+	;;
+esac
--- testing/pluto/ikev2-xfrmi-09-systemd-networkd/northrun.sh
+++ testing/pluto/ikev2-xfrmi-09-systemd-networkd/northrun.sh
@@ -1,15 +1,15 @@
 ipsec auto --up north
 sleep  2
-tcpdump -w /tmp/north-ikev2-xfrmi-09-systemd-networkd-ipsec2.pcap -s 0 -n -i ipsec2 & echo $! > /tmp/north-ikev2-xfrmi-09-systemd-networkd-tcpdump.pid
-ping -n -q -w 4 -c 4 192.0.2.254
+../../guestbin/tcpdump.sh --start -i ipsec2
+../../guestbin/ping-once.sh --up  192.0.2.254
+../../guestbin/ping-once.sh --up  192.0.2.254
+../../guestbin/ping-once.sh --up  192.0.2.254
+../../guestbin/ping-once.sh --up  192.0.2.254
 ip -s link show ipsec2
 ip rule show
 ip route
 ip route show table 50
-kill -9 $(cat /tmp/north-ikev2-xfrmi-09-systemd-networkd-tcpdump.pid)
-sync
-sleep 2
-cp /tmp/north-ikev2-xfrmi-09-systemd-networkd-ipsec2.pcap OUTPUT/
+../../guestbin/tcpdump.sh --kill
 # rm the test specific systemd-networkd file for next test
 rm /etc/systemd/network/ipsec2.netdev
 echo done
