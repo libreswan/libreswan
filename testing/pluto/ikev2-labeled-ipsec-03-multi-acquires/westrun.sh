8b45634721c61807333fa5d33ba391c634be7785
Paul Wouters <paul.wouters@aiven.io>
2021-06-09 16:16:11 -0400

    testing: WIP: ikev2-labeled-ipsec-03-multi-acquires

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/westrun.sh
@@ -4,10 +4,14 @@ ipsec auto --up labeled
 ip xfrm state
 ip xfrm pol
 echo "quit" | runcon -t netutils_t nc -w 50 -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
-ipsec trafficstatus
-ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
-# let another on-demand label establish
 sleep 3
-# we are expecting three tunnels now (main one with 0 byte counters)
 ipsec trafficstatus
+# there should be no shunts
+ipsec shuntstatus
+# let another on-demand label establish
+ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
+sleep 3
+ipsec trafficstatus
+# there should be no shunts
+ipsec shuntstatus
 echo done
