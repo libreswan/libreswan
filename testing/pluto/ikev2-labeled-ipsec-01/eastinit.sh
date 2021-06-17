635fe84b7573ba8e85ded9ae6f928e4e0eb9f728
Andrew Cagney <cagney@gnu.org>
2021-06-17 10:51:21 -0400

    testing: only build se module on east; update output

--- testing/pluto/ikev2-labeled-ipsec-01/eastinit.sh
+++ testing/pluto/ikev2-labeled-ipsec-01/eastinit.sh
@@ -1,10 +1,10 @@
 /testing/guestbin/swan-prep
 echo 3 > /proc/sys/net/core/xfrm_acq_expires
-# generate in output directory
-rm -f OUTPUT/ipsecspd.pp OUTPUT/ipsecspd.te
-ln -s ../ipsecspd.te OUTPUT
-make -C OUTPUT/ -f /usr/share/selinux/devel/Makefile ipsecspd.pp > /dev/null 2>/dev/null
-semodule -i OUTPUT/ipsecspd.pp > /dev/null 2>/dev/null
+# generate in OUTPUT directory
+( cd OUTPUT && rm -f ipsecspd.pp ipsecspd.te )
+( cd OUTPUT && ln -s ../ipsecspd.te )
+( cd OUTPUT && make -f /usr/share/selinux/devel/Makefile ipsecspd.pp )
+( cd OUTPUT && semodule -i ipsecspd.pp )
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add labeled
--- testing/pluto/ikev2-labeled-ipsec-01/westinit.sh
+++ testing/pluto/ikev2-labeled-ipsec-01/westinit.sh
@@ -1,10 +1,7 @@
 /testing/guestbin/swan-prep
 echo 3 > /proc/sys/net/core/xfrm_acq_expires
-# generate in output directory
-rm -f OUTPUT/ipsecspd.pp OUTPUT/ipsecspd.te
-ln -s ../ipsecspd.te OUTPUT
-make -C OUTPUT/ -f /usr/share/selinux/devel/Makefile ipsecspd.pp >/dev/null 2>/dev/null
-semodule -i OUTPUT/ipsecspd.pp > /dev/null 2>/dev/null
+# generated in OUTPUT by east
+( cd OUTPUT && semodule -i ipsecspd.pp )
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add labeled
