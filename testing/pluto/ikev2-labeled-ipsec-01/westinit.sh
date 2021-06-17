d408d5eca4b2eb12ef6b86b5c63a18b42337775b
Paul Wouters <paul.wouters@aiven.io>
2021-06-16 22:10:49 -0400

    testing: ikev2-labeled-ipsec-01 tweaks
    console output not yet updated because it still needs a fix for
    the SPD policies.

--- testing/pluto/ikev2-labeled-ipsec-01/westinit.sh
+++ testing/pluto/ikev2-labeled-ipsec-01/westinit.sh
@@ -1,8 +1,9 @@
 /testing/guestbin/swan-prep
+echo 3 > /proc/sys/net/core/xfrm_acq_expires
 # generate in output directory
 rm -f OUTPUT/ipsecspd.pp OUTPUT/ipsecspd.te
 ln -s ../ipsecspd.te OUTPUT
-make -C OUTPUT/ -f /usr/share/selinux/devel/Makefile ipsecspd.pp
+make -C OUTPUT/ -f /usr/share/selinux/devel/Makefile ipsecspd.pp >/dev/null 2>/dev/null
 semodule -i OUTPUT/ipsecspd.pp > /dev/null 2>/dev/null
 ipsec start
 ../../guestbin/wait-until-pluto-started
--- testing/pluto/ikev2-labeled-ipsec-01/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-01/westrun.sh
@@ -3,12 +3,15 @@ echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
 ipsec auto --route labeled
 ip xfrm state
 ip xfrm pol
-echo "quit" | runcon -t netutils_t nc  -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
-# there should be 1 tunnel
+echo "quit" | runcon -t netutils_t timeout 15 nc  -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
+# there should be 2 tunnels - both inactive in one direction
 ipsec trafficstatus
 # there should be no bare shunts
 ipsec shuntstatus
-# one IPsec SA and a %trap policy from the template conn
+# let larval state expire
+sleep 4
+# There should be FOUR IPsec SA states (two sets), all with same reqid
 ip xfrm state
+# There should be one set of tunnel policies using the configured ipsec_spd_t label, plus an outgoing %trap policy
 ip xfrm pol
 echo done
