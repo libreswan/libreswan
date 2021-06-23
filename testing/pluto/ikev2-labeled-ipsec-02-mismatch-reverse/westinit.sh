8c11ba5965a8ec4b2a27665e56d0d7a17131d642
Andrew Cagney <cagney@gnu.org>
2021-06-22 20:49:29 -0400

    testing: drop ikev2-labeled-ipsec-02-mismatch, covered by ikev2-labeled-ipsec-04-no-label-on-*

--- testing/pluto/ikev2-labeled-ipsec-02-mismatch-reverse/westinit.sh
+++
@@ -1,8 +0,0 @@
-/testing/guestbin/swan-prep
-# install selinux; generated in OUTPUT by east
-semodule -i OUTPUT/ipsecspd.pp
-# start pluto
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add labeled
-echo "initdone"
