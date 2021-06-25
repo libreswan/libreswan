cda5ee12410fef20ba9c3470710929c8c9551d85
Paul Wouters <paul.wouters@aiven.io>
2021-06-24 21:51:36 -0400

    testing: tweak ikev2-labeled-ipsec-03-multi-acquires

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/eastinit.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/eastinit.sh
@@ -6,5 +6,5 @@ ipsec start
 ../../guestbin/wait-until-pluto-started
 echo 1 > /proc/sys/net/core/xfrm_acq_expires
 ipsec auto --add labeled
-runcon -t netutils_t ipsec getpeercon_server 4300 &
+ipsec getpeercon_server 4300 &
 echo "initdone"
