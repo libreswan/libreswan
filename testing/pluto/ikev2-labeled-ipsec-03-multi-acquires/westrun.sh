ce87714b149c5d3949401545d77370483f61964a
Paul Wouters <paul.wouters@aiven.io>
2021-06-24 22:08:10 -0400

    testing: kev2-labeled-ipsec-03-multi-acquires  more tweaks
    show the states, run the peercon as unconfined similar to
    the -01 test case. Update ipsecspd.te.

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires/westrun.sh
@@ -1,7 +1,7 @@
 # for port re-use in tests with protoport selectors
 echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
 ipsec auto --up labeled
-# expect policy but not state
+# expect policy but no states
 ../../guestbin/ipsec-look.sh
 # trigger an acquire
 echo "quit" | runcon -t netutils_t nc -w 50 -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
@@ -14,4 +14,5 @@ ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
 ../../guestbin/wait-for.sh --match 'labeled..2.' ipsec trafficstatus
 # there should be no shunts
 ipsec shuntstatus
+ipsec showstates
 echo done
