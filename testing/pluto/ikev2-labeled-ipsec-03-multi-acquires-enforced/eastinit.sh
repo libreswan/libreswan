75415812abadc64ed59fe0d2adc5f2fbc0f3018f
Paul Wouters <paul.wouters@aiven.io>
2021-07-04 21:22:16 -0400

    testing: WIP ikev2-labeled-ipsec-03-multi-acquires-enforced
    sync up to the non-enforced variant.

--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/eastinit.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/eastinit.sh
@@ -1,11 +1,12 @@
 /testing/guestbin/swan-prep
 # build install se module
 ../../guestbin/semodule.sh ipsecspd.te
+# cheat that might not work? start before enabling selinux
+ipsec getpeercon_server 4300 &
 setenforce 1
 # get pluto going
 ipsec start
 ../../guestbin/wait-until-pluto-started
 echo 1 > /proc/sys/net/core/xfrm_acq_expires
 ipsec auto --add labeled
-runcon -t netutils_t ipsec getpeercon_server 4300 &
 echo "initdone"
--- testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-03-multi-acquires-enforced/westrun.sh
@@ -1,13 +1,18 @@
 # for port re-use in tests with protoport selectors
 echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
 ipsec auto --up labeled
-ip xfrm state
-ip xfrm pol
+# expect policy but no states
+../../guestbin/ipsec-look.sh
+# trigger an acquire
 echo "quit" | runcon -t netutils_t nc -w 50 -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
-ipsec trafficstatus
-ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
+# wait for it to establish and flow
+../../guestbin/wait-for.sh --match 'labeled..1.' ipsec trafficstatus
+# there should be no shunts
+ipsec shuntstatus
 # let another on-demand label establish
-sleep 3
-# we are expecting three tunnels now (main one with 0 byte counters)
-ipsec trafficstatus
+ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
+../../guestbin/wait-for.sh --match 'labeled..2.' ipsec trafficstatus
+# there should be no shunts
+ipsec shuntstatus
+ipsec showstates
 echo done
