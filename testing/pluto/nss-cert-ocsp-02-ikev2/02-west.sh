e70035441f6b37cde08245594016149ba1fc77be
Andrew Cagney <cagney@gnu.org>
2021-07-17 09:35:37 -0400

    testing: ikev2 tweaks

--- testing/pluto/nss-cert-ocsp-02-ikev2/02-west.sh
+++ testing/pluto/nss-cert-ocsp-02-ikev2/02-west.sh
@@ -3,6 +3,7 @@ certutil -d sql:/etc/ipsec.d -D -n east
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec whack --impair suppress-retransmits
+ipsec whack --impair revival
 ipsec auto --add nss-cert-ocsp
 ipsec auto --status |grep nss-cert-ocsp
 echo "initdone"
