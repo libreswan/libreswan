10cdef71f1fd80ac60ce041a6bc509eb0ca567a1
Andrew Cagney <cagney@gnu.org>
2021-07-17 10:33:23 -0400

    testing: expect cert name, and expect auth fail

--- testing/pluto/fips-13-ikev2-x509-key2032/westrun.sh
+++ testing/pluto/fips-13-ikev2-x509-key2032/westrun.sh
@@ -1,4 +1,4 @@
-ipsec whack --impair delete-on-retransmit
+ipsec whack --impair suppress-retransmits
 # should fail - our FIPS code requires 3072 minimum key
 ipsec auto --up westnet-eastnet-ikev2
 echo done
