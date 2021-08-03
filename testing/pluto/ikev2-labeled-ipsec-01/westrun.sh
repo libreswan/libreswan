a155d02a146c376fb2df71364a2eaf13df92e2ef
Andrew Cagney <cagney@gnu.org>
2021-08-03 16:40:40 -0400

    testing: update ikev2-labeled-ipsec-01
    - use ipsecspd-full-perm semodule (from Kavinda Wewegama)
    - establish SA that captures everything; not just TCP
    - use ping-once.sh --runcon ...ping_t... --{forget,up} to trigger SA
    - expect only one SA and with the ping_t context

--- testing/pluto/ikev2-labeled-ipsec-01/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-01/westrun.sh
@@ -3,8 +3,10 @@ echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
 # route; should be two policies
 ipsec auto --route labeled
 ../../guestbin/ipsec-look.sh
-# trigger traffic
-echo "quit" | runcon -t netutils_t timeout 15 nc  -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
+# trigger traffic using the predefined ping_t context
+../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match 192.1.2.23 -- ipsec trafficstatus
+../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --up     -I 192.1.2.45 192.1.2.23
 # there should be 2 tunnels - both inactive in one direction
 ipsec trafficstatus
 # there should be no bare shunts
