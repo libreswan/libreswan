5887be978dfdb284ff28da32a8fefcc66ca135fb
Andrew Cagney <cagney@gnu.org>
2021-08-05 09:51:25 -0400

    testing: sync up iikev2-labeled-ipsec-01-ike-{ondemand,childless}
    - consistent names
    - also use ping in childless test

--- testing/pluto/ikev2-labeled-ipsec-01-ike-childless/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-01-ike-childless/westrun.sh
@@ -1,13 +1,13 @@
 # for port re-use in tests with protoport selectors
 echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
-# route; should be two policies
-ipsec auto --route labeled
+# should install two labels
+ipsec auto --up labeled
 ../../guestbin/ipsec-look.sh
 # trigger traffic using the predefined ping_t context
 ../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --forget -I 192.1.2.45 192.1.2.23
 ../../guestbin/wait-for.sh --match 192.1.2.23 -- ipsec trafficstatus
 ../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --up     -I 192.1.2.45 192.1.2.23
-# there should be 2 tunnels - both inactive in one direction
+# there should be 1 tunnel in each direction
 ipsec trafficstatus
 # there should be no bare shunts
 ipsec shuntstatus
---
+++ testing/pluto/ikev2-labeled-ipsec-01-ike-ondemand/westrun.sh
@@ -0,0 +1,16 @@
+# for port re-use in tests with protoport selectors
+echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
+# route; should be two policies
+ipsec auto --route labeled
+../../guestbin/ipsec-look.sh
+# trigger traffic using the predefined ping_t context
+../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match 192.1.2.23 -- ipsec trafficstatus
+../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --up     -I 192.1.2.45 192.1.2.23
+# there should be 1 tunnel in each direction
+ipsec trafficstatus
+# there should be no bare shunts
+ipsec shuntstatus
+# let larval state expire
+../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ip xfrm state
+echo done
