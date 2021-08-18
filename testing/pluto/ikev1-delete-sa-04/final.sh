66aa842071445a61bcf0127504dcccaf5bdcdc26
Paul Wouters <paul.wouters@aiven.io>
2021-08-18 14:42:19 -0400

    testing: minor updates

--- testing/pluto/ikev1-delete-sa-04/final.sh
+++ testing/pluto/ikev1-delete-sa-04/final.sh
@@ -4,4 +4,4 @@ sleep 30
 # There should be no IKE SA and no IPsec SA
 ipsec whack --trafficstatus
 # east howvever, should be attempting to connect to west because it has auto=start
-ipsec status |grep EVENT_RETRANSMIT | sed "s/EVENT_RETRANSMIT in .*$/EVENT_RETRANSMIT in .../"
+ipsec status |grep RETRANSMIT | sed "s/RETRANSMIT in .*$/RETRANSMIT in .../"
