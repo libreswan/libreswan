ca32d0a66281f55b40d2860531776560c6f4934a
Andrew Cagney <cagney@gnu.org>
2021-09-09 09:04:48 -0400

    testing: output tweaks

--- testing/pluto/ikev2-42-rw-replace-responder/roadrun.sh
+++ testing/pluto/ikev2-42-rw-replace-responder/roadrun.sh
@@ -6,6 +6,6 @@ ipsec status
 sleep 60
 sleep 60
 ../../guestbin/ping-once.sh --up -I 192.0.2.100 192.1.2.23
-grep -E  'EVENT_.*_EXPIRE|EVENT_.*_REPLACE' OUTPUT/road.pluto.log | grep -e '#[0-9]' | grep -v pe@
+grep -E 'EVENT_.*_EXPIRE|EVENT_.*_REPLACE' OUTPUT/road.pluto.log | grep -e '#[0-9]' | sed -e 's/@[a-z9-9]*/@0x.../'
 ipsec status
 echo done
