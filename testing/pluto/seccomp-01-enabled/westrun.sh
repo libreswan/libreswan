23e84eb7c3423c606a1368304bd780045546db8e
Andrew Cagney <cagney@gnu.org>
2021-09-09 20:21:57 -0400

    testing: in seccomp-01-enabled use: ipsec whack --seccomp-crashtest & disown ; sleep 2
    instead of spredding command across 3 lines resulting in intermingled
    output

--- testing/pluto/seccomp-01-enabled/westrun.sh
+++ testing/pluto/seccomp-01-enabled/westrun.sh
@@ -3,9 +3,7 @@ ipsec auto --down nss-cert
 # pluto should still be running
 pidof pluto > /dev/null || echo not running?
 # whack socket will hang because pluto is expected to die
-ipsec whack --seccomp-crashtest --asynchronous &
-disown
-sleep 1
+ipsec whack --seccomp-crashtest & disown ; sleep 2
 # pluto should not be running anymore
 pidof pluto
 # one entry of SECCOMP activating should show up in the log
