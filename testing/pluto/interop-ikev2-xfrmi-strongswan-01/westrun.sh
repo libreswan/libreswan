4b417979e1bb642fd9182180172c7f8d6ced78ae
Andrew Cagney <cagney@gnu.org>
2021-09-10 10:06:02 -0400

    testing: use ping-once.sh in interop-ikev2-xfrmi-strongswan-01
    <<ping -w 4 -c 4>> makes for a very close shave

--- testing/pluto/interop-ikev2-xfrmi-strongswan-01/westrun.sh
+++ testing/pluto/interop-ikev2-xfrmi-strongswan-01/westrun.sh
@@ -1,7 +1,13 @@
 swanctl --initiate --child westnet-eastnet
-ping -n -q -w 4 -c 4 -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 ip xfrm policy
 ip xfrm state
 ip link set up dev ipsec2
 ip route add 192.0.2.0/24 dev ipsec2
-ping -n -q -w 4 -c 4 -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
