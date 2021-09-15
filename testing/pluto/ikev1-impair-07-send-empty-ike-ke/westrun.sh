fec6eedd2cee64c43fe28e00029960f724d463b8
Andrew Cagney <cagney@gnu.org>
2021-09-15 16:22:13 -0400

    testing: output tweaks (and add more --impair revivals)

--- testing/pluto/ikev1-impair-07-send-empty-ike-ke/westrun.sh
+++ testing/pluto/ikev1-impair-07-send-empty-ike-ke/westrun.sh
@@ -2,13 +2,16 @@
 # responder should return invalid KE
 ipsec whack --impair ke-payload:empty
 ipsec whack --impair delete-on-retransmit
+ipsec whack --impair revival
 ipsec auto --up westnet-eastnet-ipv4-psk
+ipsec auto --delete westnet-eastnet-ipv4-psk
 ipsec whack --impair none
 
 # initiator sends valid KE
 # responder sends back empty KE, should be rejected
 # (responder also has re-transmits disabled)
 ipsec whack --impair suppress-retransmits
+ipsec whack --impair revival
 ipsec auto --add westnet-eastnet-ipv4-psk
 ipsec auto --up westnet-eastnet-ipv4-psk
-
+ipsec auto --delete westnet-eastnet-ipv4-psk
