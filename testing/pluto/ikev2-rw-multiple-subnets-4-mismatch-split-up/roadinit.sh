4e5b8bf5a0ec85e9c13b94ba99ccdc08bc74ff9a
Andrew Cagney <cagney@gnu.org>
2021-09-15 20:25:12 -0400

    testing: timing tweaks

--- testing/pluto/ikev2-rw-multiple-subnets-4-mismatch-split-up/roadinit.sh
+++ testing/pluto/ikev2-rw-multiple-subnets-4-mismatch-split-up/roadinit.sh
@@ -1,6 +1,5 @@
 /testing/guestbin/swan-prep --x509
 ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec whack --impair revival
 ipsec auto --add road
 echo "initdone"
--- testing/pluto/ikev2-rw-multiple-subnets-4-mismatch-split-up/roadrun.sh
+++ testing/pluto/ikev2-rw-multiple-subnets-4-mismatch-split-up/roadrun.sh
@@ -1,12 +1,27 @@
 # matched by peer
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
 ipsec auto --up road/0x1
+ipsec whack --impair none
+
 # not matched by peer, should fail
+ipsec whack --impair revival
 ipsec auto --up road/0x2
+ipsec whack --impair none
+
 # matched by peer
+ipsec whack --impair revival
+ipsec whack --impair suppress-retransmits
 ipsec auto --up road/0x3
+ipsec whack --impair none
+
 # not matched by peer, should fail
+ipsec whack --impair revival
 ipsec auto --up road/0x4
+ipsec whack --impair none
+
 # There should be 2 tunnels up, and 2 broken tunnels
 ipsec trafficstatus
 ipsec showstates
+
 echo done
