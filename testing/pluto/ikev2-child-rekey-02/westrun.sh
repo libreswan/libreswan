17eacccc951fa32f74a5de48a3584c355b09a586
Andrew Cagney <cagney@gnu.org>
2021-09-20 14:37:21 -0400

    testing: fix race in ikev2-child-rekey-02
    Replace sleep with:
    
      # rekey to Child #5 #6 #7
      ../../guestbin/wait-for.sh --match '#5: established Child SA' -- cat /tmp/pluto.log
      ../../guestbin/wait-for.sh --match '#6: established Child SA' -- cat /tmp/pluto.log
      ../../guestbin/wait-for.sh --match '#7: established Child SA' -- cat /tmp/pluto.log
    
    (at this point the new Child SA has established but the old Child SA lurks)
    
      ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
      ../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
      ../../guestbin/ping-once.sh --up -I 192.0.101.254 192.0.201.254
      # wait for old, so that they are not in traffic status
    
    (i.e, give the old Child SA time to die)
    
      ../../guestbin/wait-for.sh --match '#2: ESP traffic information:' -- cat /tmp/pluto.log
      ../../guestbin/wait-for.sh --match '#3: ESP traffic information:' -- cat /tmp/pluto.log
      ../../guestbin/wait-for.sh --match '#4: ESP traffic information:' -- cat /tmp/pluto.log
      # expect Child #5 #6 #7
      ipsec whack --trafficstatus

--- testing/pluto/ikev2-child-rekey-02/westrun.sh
+++ testing/pluto/ikev2-child-rekey-02/westrun.sh
@@ -1,21 +1,41 @@
+# IKE #1 Child #3
 ipsec auto --up westnet-eastnet-ikev2a
-ping -n -q -w 4 -c 4 -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+# Child #3
 ipsec auto --up westnet-eastnet-ikev2b
-ping -n -q -w 4 -c 4 -I 192.0.100.254 192.0.200.254
+../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
+# Child #4
 ipsec auto --up westnet-eastnet-ikev2c
-ping -n -q -w 4 -c 4 -I 192.0.101.254 192.0.201.254
+../../guestbin/ping-once.sh --up -I 192.0.101.254 192.0.201.254
+# expect Child #2 #3 #4
 ipsec whack --trafficstatus
-ipsec status |grep STATE_
-echo "sleep 23"
-sleep 23
-ping -n -q -w 4 -c 4 -I 192.0.1.254 192.0.2.254
-ping -n -q -w 4 -c 4 -I 192.0.100.254 192.0.200.254
-ping -n -q -w 4 -c 4 -I 192.0.101.254 192.0.201.254
+
+# rekey to Child #5 #6 #7
+../../guestbin/wait-for.sh --match '#5: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#6: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#7: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
+../../guestbin/ping-once.sh --up -I 192.0.101.254 192.0.201.254
+# wait for old, so that they are not in traffic status
+../../guestbin/wait-for.sh --match '#2: ESP traffic information:' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#3: ESP traffic information:' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#4: ESP traffic information:' -- cat /tmp/pluto.log
+# expect Child #5 #6 #7
 ipsec whack --trafficstatus
-ipsec status |grep STATE_
-echo "sleep 25"
-sleep 25
-ping -n -q -w 4 -c 4 -I 192.0.1.254 192.0.2.254
-ping -n -q -w 4 -c 4 -I 192.0.100.254 192.0.200.254
-ping -n -q -w 4 -c 4 -I 192.0.101.254 192.0.201.254
+
+# rekey to Child #8 #9 #10
+../../guestbin/wait-for.sh --match '#8: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#9: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#10: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
+../../guestbin/ping-once.sh --up -I 192.0.101.254 192.0.201.254
+# wait for old, so that they are not in traffic status
+../../guestbin/wait-for.sh --match '#5: ESP traffic information:' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#6: ESP traffic information:' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#7: ESP traffic information:' -- cat /tmp/pluto.log
+# expect Child #8 #9 #10
+ipsec whack --trafficstatus
+
 echo done
