02d0cdf7062d45ad83bc836122dfa570dd571cb4
Andrew Cagney <cagney@gnu.org>
2021-09-22 17:28:26 -0400

    testing: sprinkle --trafficstatus
    confirm pings worked

--- testing/pluto/ikev1-xfrmi-04-aggr/northrun.sh
+++ testing/pluto/ikev1-xfrmi-04-aggr/northrun.sh
@@ -1,4 +1,5 @@
 ipsec auto --up northnet-eastnet
 ../../guestbin/ping-once.sh --up -I 192.0.3.254 192.0.22.254
 ../../guestbin/ping-once.sh --up -I 192.0.3.254 192.0.22.254
+ipsec whack --trafficstatus
 echo done
