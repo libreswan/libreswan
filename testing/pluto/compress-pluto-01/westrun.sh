9fe40b017ae2612a866406cccbbcd5d892fa6a1f
Andrew Cagney <cagney@gnu.org>
2021-09-23 18:50:45 -0400

    testing: update output

--- testing/pluto/compress-pluto-01/westrun.sh
+++ testing/pluto/compress-pluto-01/westrun.sh
@@ -1,8 +1,14 @@
 ipsec auto --up westnet-eastnet-compress
-# this ping wont be compressed
-ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
+# these small pings won't be compressed
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 # test compression via large pings that can be compressed on IPCOMP SA
-ping -n -q -c 4 -s 8184  -p ff -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -s 8184 -p ff -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -s 8184 -p ff -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -s 8184 -p ff -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -s 8184 -p ff -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
 ../../guestbin/ipsec-look.sh
 ipsec auto --down westnet-eastnet-compress
