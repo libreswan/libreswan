cc378ce9f46584e26816034150a058d67e451684
Andrew Cagney <cagney@gnu.org>
2021-09-24 17:31:30 -0400

    testing: use responder's log line to spot a rekey

--- testing/pluto/ikev2-42-rw-replace-responder/roadrun.sh
+++ testing/pluto/ikev2-42-rw-replace-responder/roadrun.sh
@@ -2,10 +2,14 @@
 ../../guestbin/ping-once.sh --up 192.1.2.23
 ipsec auto --up road-east-x509-ipv4
 ../../guestbin/ping-once.sh --up -I 192.0.2.100 192.1.2.23
-ipsec status
-sleep 60
-sleep 60
+ipsec whack --trafficstatus
+# east's rekey is 50s, margin 10s
+sleep 40
+# wait for both rekey, ...
+../../guestbin/wait-for.sh --match '#3: rekeyed IKE SA #1' -- cat /tmp/pluto.log
+# and delete, ...
+../../guestbin/wait-for.sh --match '#1: deleting' -- cat /tmp/pluto.log
+# before trying ping (else ping races response)
 ../../guestbin/ping-once.sh --up -I 192.0.2.100 192.1.2.23
-grep -E 'EVENT_.*_EXPIRE|EVENT_.*_REPLACE' OUTPUT/road.pluto.log | grep -e '#[0-9]' | sed -e 's/@0x[0-9a-f]*/@0xXXX/'
 ipsec status
 echo done
