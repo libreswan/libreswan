fc60879224fafc2a4f59ecf51d51213c30cf9175
Andrew Cagney <cagney@gnu.org>
2021-09-26 14:47:04 -0400

    testing: split compress-pluto-01 into compress-pluto-01{,-cleanup}
    latter is WIP (bug 514) and demonstrates compression states not
    being cleaned up

--- testing/pluto/compress-pluto-01-cleanup/westrun.sh
+++ testing/pluto/compress-pluto-01-cleanup/westrun.sh
@@ -10,4 +10,6 @@ ipsec auto --up westnet-eastnet-compress
 ../../guestbin/ping-once.sh --up -s 8184 -p ff -I 192.0.1.254 192.0.2.254
 ../../guestbin/ping-once.sh --up -s 8184 -p ff -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus | sed -e 's/Bytes=6[0-9][0-9],/Bytes=6nn,/g'
+../../guestbin/ipsec-look.sh
+ipsec auto --down westnet-eastnet-compress | sed -e 's/=6[0-9][0-9]B /=6nnB /g'
 echo done
