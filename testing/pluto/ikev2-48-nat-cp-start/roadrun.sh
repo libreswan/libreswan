b9eaa35f4915a2ed69983cc77fbce5eb919d9ab1
Andrew Cagney <cagney@gnu.org>
2021-09-30 21:55:45 -0400

    testing: fix ping in ikev2-48-nat-cp-start
    unfortunatly it doesn't fix the test

--- testing/pluto/ikev2-48-nat-cp-start/roadrun.sh
+++ testing/pluto/ikev2-48-nat-cp-start/roadrun.sh
@@ -1,10 +1,10 @@
 # wait for autostart to complete
 ../../guestbin/wait-for.sh --match 192.0.2.1 -- ipsec whack --trafficstatus
 
-# ipsec will configure 192.0.2.1 on eth0
+# ipsec will configure 192.0.2.1->192.1.2.23 on eth0
 ip -4 route
 ip addr show  dev eth0
-../../guestbin/ping-once.sh --up 192.0.2.1 192.1.2.23
+../../guestbin/ping-once.sh --up 192.1.2.23
 
 ipsec auto --down westnet-eastnet-ipv4-psk-ikev2
 #check if the address, 192.0.2.1, is removed
