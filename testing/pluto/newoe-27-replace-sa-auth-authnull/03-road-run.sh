5d792aee1dba100dc5017a4f2922a0cf892d5238
Andrew Cagney <cagney@gnu.org>
2021-10-12 21:04:45 -0400

    testing: in newoe-27-replace-sa-auth-authnull, wait for AUTHENTICATION_FAILED

--- testing/pluto/newoe-27-replace-sa-auth-authnull/03-road-run.sh
+++ testing/pluto/newoe-27-replace-sa-auth-authnull/03-road-run.sh
@@ -1,20 +1,21 @@
 # setup authenticated static conn
+# should established tunnel and no bare shunts
+# ping should succeed through tunnel
 ipsec auto --up authenticated
-# should show established tunnel and no bare shunts
+../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
 ../../guestbin/ipsec-look.sh
-# ping should succeed through tunnel
-../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
-ipsec whack --trafficstatus
+
+# now delete the authenticated sa
 ipsec whack --impair send-no-delete
 ipsec auto --delete authenticated
-sleep 5
+
 # the ping triggers an OE authnull attempt. It should fail because
 # east should not replace an authenticated conn with an authnull conn
 ../../guestbin/ping-once.sh --down -I 192.1.3.209 192.1.2.23
+../../guestbin/wait-for.sh --match '#3: IKE SA authentication request rejected by peer' -- cat /tmp/pluto.log
 # There should NOT be an IPsec SA, and a partial OE attempt going?
-sleep 5
 ipsec status |grep STATE_
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
