6e83f0fd6f0d0188077da3adc381618c79ce20ea
Paul Wouters <paul.wouters@aiven.io>
2021-10-14 18:27:46 -0400

    testing: remove whackrecord-01
    option to record whack messages was removed long ago

--- testing/pluto/whackrecord-01/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec whack --whackrecord /var/tmp/east.record
-ipsec auto --add westnet-eastnet
-ipsec whack --debug none --debug control --debug controlmore --debug crypt
--- testing/pluto/whackrecord-01/final.sh
+++
@@ -1,2 +0,0 @@
-ipsec setup stop
-ls /var/tmp/east.record
