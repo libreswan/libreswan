88bca68ec9233011c67c592f73977d613909d2c6
Paul Wouters <paul.wouters@aiven.io>
2021-10-14 18:45:43 -0400

    testing: first chunk IKEv1 updates due to changed replay-window default

--- testing/pluto/ikev1-impair-06-send-no-ike-ke/westrun.sh
+++ testing/pluto/ikev1-impair-06-send-no-ike-ke/westrun.sh
@@ -1,14 +1,11 @@
 # don't come back
 ipsec whack --impair revival
-
 # expect quick fail
 ipsec whack --impair ke-payload:omit
 ipsec whack --impair delete-on-retransmit
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
 ipsec whack --impair none
-
 # expect slower fail
 ipsec whack --impair suppress-retransmits
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
-
 echo done
