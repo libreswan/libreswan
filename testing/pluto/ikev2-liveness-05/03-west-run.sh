43028ab40c61e7e062d31eda8256273ecacedcde
Andrew Cagney <cagney@gnu.org>
2021-10-22 09:44:45 -0400

    testing: update, ikev2-liveness-05 mark as WIP, see github:531
    also change ikev2-liveness-07 back to dpdaction=restart

--- testing/pluto/ikev2-liveness-05/03-west-run.sh
+++ testing/pluto/ikev2-liveness-05/03-west-run.sh
@@ -1,27 +1,31 @@
-#!/bin/sh
+# establish the tunnel
 ipsec auto --up westnet-eastnet-ikev2
-ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
-# Tunnel should be up
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
+
 # Let R_U_THERE packets flow
 sleep 15
+
 # Setting up block via iptables
 iptables -I INPUT -s 192.1.2.23/32 -d 0/0 -j DROP
 iptables -I OUTPUT -d 192.1.2.23/32 -s 0/0 -j DROP
-sleep 30
+
 # DPD should have triggered now
+../../guestbin/wait-for.sh --match '#1: liveness action' -- cat /tmp/pluto.log
+
 # Tunnel should be down with %trap or %hold preventing packet leaks
 # But shuntstatus only shows bare shunts, not connection shunts :(
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
-# packets should be caught in firewall and no icmp replies should happen
-ping -n -q -w 2 -c 3 -I 192.0.1.254 192.0.2.254
+
+# packets should be caught in firewall and no icmp replies should
+# happen
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+
 # Remove the Blockage
 iptables -D INPUT -s 192.1.2.23/32 -d 0/0 -j DROP
 iptables -D OUTPUT -d 192.1.2.23/32 -s 0/0 -j DROP
-# trigger traffic
-ping -n -q -W 1 -w 1 -c 1 -I 192.0.1.254 192.0.2.254
-sleep 2
-ping -n -q -W 1 -w 4 -c 4 -I 192.0.1.254 192.0.2.254
+
 # Tunnel should be back up now even without triggering traffic
-echo done
+../../guestbin/wait-for.sh --match '#[3-9]: established Child SA' -- cat /tmp/pluto.log
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
