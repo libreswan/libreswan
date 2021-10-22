2fcca20b152362740beecc485691616a31f83081
Andrew Cagney <cagney@gnu.org>
2021-10-22 09:15:18 -0400

    testing: update ikev2-liveness-06
    - retransmit-timeout=...
    - wait for 'liveness action'

--- testing/pluto/ikev2-liveness-06/03-road-run.sh
+++ testing/pluto/ikev2-liveness-06/03-road-run.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
-ping -n -q -c 2 192.1.2.23
+# in the clear; then establish
+../../guestbin/ping-once.sh --up 192.1.2.23
 ipsec auto --up road-east-x509-ipv4
-ping -n -q -c 4 -I 192.0.2.100 192.1.2.23
+../../guestbin/ping-once.sh --up -I 192.0.2.100 192.1.2.23
 ipsec whack --trafficstatus
+
 # Let R_U_THERE packets flow
 sleep 15
+
 # Setting up block via iptables
 iptables -I INPUT -s 192.1.2.23/32 -d 0/0 -j DROP
 iptables -I OUTPUT -d 192.1.2.23/32 -s 0/0 -j DROP
-sleep 30
-# DPD should have triggered now
+
+# Wait for liveness to trigger
+../../guestbin/wait-for.sh --match '#1: liveness action' -- cat /tmp/pluto.log
+
 # Tunnel should be cleared, no trap/hold
 # shuntstatus does not show connection shunt unfortunately
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
-echo done
