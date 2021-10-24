e9cb04855fd8c7f0a29b2b5efd56a04ece8327b0
Andrew Cagney <cagney@gnu.org>
2021-10-23 21:37:51 -0400

    testing: in ikev1-algo-ike-dh-ecp-01, pass --alive to libreswan-up-down.sh

--- testing/pluto/ikev1-algo-ike-dh-ecp-01/westrun.sh
+++ testing/pluto/ikev1-algo-ike-dh-ecp-01/westrun.sh
@@ -1,5 +1,3 @@
-../../guestbin/libreswan-up-down.sh ikev1-ike=aes128-sha1-dh19 -I 192.0.1.254 192.0.2.254
-../../guestbin/libreswan-up-down.sh ikev1-ike=aes128-sha1-dh20 -I 192.0.1.254 192.0.2.254
-../../guestbin/libreswan-up-down.sh ikev1-ike=aes128-sha1-dh21 -I 192.0.1.254 192.0.2.254
-
-echo done
+../../guestbin/libreswan-up-down.sh ikev1-ike=aes128-sha1-dh19 --alive -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ikev1-ike=aes128-sha1-dh20 --alive -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ikev1-ike=aes128-sha1-dh21 --alive -I 192.0.1.254 192.0.2.254
