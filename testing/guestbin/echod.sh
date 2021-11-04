4fbad38a0fd8ed0aa4e2e02c889638203bb11044
Andrew Cagney <cagney@gnu.org>
2021-11-04 09:52:38 -0400

    testing: allow echod.sh 333

--- testing/guestbin/echod.sh
+++ testing/guestbin/echod.sh
@@ -3,4 +3,17 @@
 # see https://github.com/nmap/nmap/issues/962 for why
 # ncat doesn't cut it.
 
-socat -v tcp-l:7,fork exec:'/bin/cat' > OUTPUT/echod.log 2>&1 &
+port=7
+
+while test $# -gt 0 ; do
+    case "$1" in
+	*:* | *.* ) echo addresses are not implemented ; exit 1 ;;
+	* )
+	    port=$1
+	    echo listening on port ${port}
+	    ;;
+    esac
+    shift
+done
+
+socat -v tcp-l:${port},fork exec:'/bin/cat' > OUTPUT/echod.log 2>&1 &
