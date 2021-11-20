fa0adc6a78ea3e4f60f28db1cb55ebf68cde44e8
Andrew Cagney <cagney@gnu.org>
2021-11-20 12:13:50 -0500

    testing: expect ikev2-tcp-06-fail-ike-sa-init-invalid-ke to pass
    See github #549

--- testing/pluto/ikev2-tcp-06-fail-ike-sa-init-invalid-ke/03-west-run.sh
+++ testing/pluto/ikev2-tcp-06-fail-ike-sa-init-invalid-ke/03-west-run.sh
@@ -1,4 +1,4 @@
-# unfortunately does not yet indicate it is using TCP
+# look for ESPinTCP
 ipsec auto --up ikev2-westnet-eastnet
 ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
