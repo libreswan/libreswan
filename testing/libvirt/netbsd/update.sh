27657e64a57089c70af327189c6c3a1bc2eca473
Andrew Cagney <cagney@gnu.org>
2021-11-27 19:51:07 -0500

    kvm: during <<make kvm-netbsd-update>> install dependencies for NetBSD build

--- testing/libvirt/netbsd/update.sh
+++ testing/libvirt/netbsd/update.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+set -xe
 
+: git crashes, do not know why
+pkg_add git || true
+pkg_add gmake
+pkg_add nss
+pkg_add unbound
+pkg_add bison
+pkg_add flex
+pkg_add ldns
+pkg_add xmlto
+pkg_add pkg-config
+pkg_add mozilla-rootcerts
+mozilla-rootcerts install
