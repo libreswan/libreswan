a8a4a7002ba4d39c5bc22d345d91b30cf410d07a
Andrew Cagney <cagney@gnu.org>
2021-12-08 15:20:47 -0500

    testing: sprinkle impair-revival

--- testing/pluto/ikev2-04-basic-x509-no-ca-mismatch-02/westrun.sh
+++ testing/pluto/ikev2-04-basic-x509-no-ca-mismatch-02/westrun.sh
@@ -1,4 +1,4 @@
-# we are expecting to fail
-ipsec whack --impair suppress-retransmits
+ipsec whack --impair suppress-retransmits # expect failure response
+ipsec whack --impair revival # don't come back
 ipsec auto --up westnet-eastnet-ikev2
 echo done
--- testing/pluto/ikev2-asymmetric-17-auth-mismatch-reverse/westrun.sh
+++ testing/pluto/ikev2-asymmetric-17-auth-mismatch-reverse/westrun.sh
@@ -1,4 +1,4 @@
-# expected to fail
-ipsec whack --impair suppress-retransmits
+ipsec whack --impair suppress-retransmits # expect failure response
+ipsec whack --impair revival
 ipsec auto --up westnet-eastnet-ikev2
 echo done
