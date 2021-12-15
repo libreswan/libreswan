9c2947dd5eda9e2fd613f143eaa5695728f8f259
Andrew Cagney <cagney@gnu.org>
2021-12-15 13:23:26 -0500

    testing: in newoe-15-portpass, tcpdump eth1

--- testing/pluto/newoe-15-portpass/00-nic-init.sh
+++ testing/pluto/newoe-15-portpass/00-nic-init.sh
@@ -1,3 +1,3 @@
 #/usr/sbin/named
 iptables -F
-( tcpdump -l -A & echo $? > /tmp/nic.tcpdump.pid ) > /tmp/nic.tcpdump.log 2>&1
+( tcpdump -i eth1 -l -A & echo $? > /tmp/nic.tcpdump.pid ) > /tmp/nic.tcpdump.log 2>&1
