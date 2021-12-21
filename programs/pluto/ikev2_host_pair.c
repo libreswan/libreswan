f08f936e9d1c6bc763bda121b27f32fc601ecff5
Andrew Cagney <cagney@gnu.org>
2021-12-21 14:50:41 -0500

    ikev2: overhaul ikev2_find_host_connection()
    used by IKE_SA_INIT responder
    - replace find_host_pair() with FOR_EACH_HOST_PAIR_CONNECTION()
    - drop IDi check, not applicable to IKE_SA_INIT
    - drop policy excludes, always LEMPTY
    - explicitly skip ID_NULL+INSTANCE
    follow up #584 and discussion with DHR

--- programs/pluto/ikev2_host_pair.c
+++ programs/pluto/ikev2_host_pair.c
@@ -30,330 +30,261 @@
 #include "iface.h"
 #include "unpack.h"
 
-static struct connection *find_next_v2_host_connection(struct connection *c,
-						       lset_t req_policy, lset_t policy_exact_mask,
-						       const struct id *peer_id)
+static bool match_policy(struct connection *c, lset_t req_policy)
 {
-	const enum ike_version ike_version = IKEv2;
-	policy_buf pb;
-	dbg("find_next_host_connection policy=%s",
-	    str_policy(req_policy, &pb));
+	if (c->config->ike_version != IKEv2) {
+		connection_buf cb;
+		dbg("  skipping "PRI_CONNECTION", not IKEv2",
+		    pri_connection(c, &cb));
+		return false;
+	}
 
-	for (; c != NULL; c = c->hp_next) {
-		policy_buf fb;
-		dbg("found policy = %s (%s)",
-		    str_connection_policies(c, &fb),
-		    c->name);
+	if (c->kind == CK_INSTANCE && c->spd.that.id.kind == ID_NULL) {
+		connection_buf cb;
+		dbg("  skipping "PRI_CONNECTION", ID_NULL instance",
+		    pri_connection(c, &cb));
+		return false;
+	}
 
-		if (NEVER_NEGOTIATE(c->policy)) {
-			/* are we a block or clear connection? */
-			enum shunt_policy shunt = c->config->prospective_shunt;
-			if (shunt != SHUNT_TRAP) {
-				/*
-				 * We need to match block/clear so we can send back
-				 * NO_PROPOSAL_CHOSEN, otherwise not match so we
-				 * can hit packetdefault to do real IKE.
-				 * clear and block do not have POLICY_OPPORTUNISTIC,
-				 * but clear-or-private and private-or-clear do, but
-				 * they don't do IKE themselves but allow packetdefault
-				 * to be hit and do the work.
-				 * if not policy_oppo -> we hit clear/block so this is right c
-				 */
-				if ((c->policy & POLICY_OPPORTUNISTIC))
-					continue;
-
-				/* shunt match - stop the search for another conn if we are groupinstance*/
-				if (c->policy & POLICY_GROUPINSTANCE)
-					break;
+	if (NEVER_NEGOTIATE(c->policy)) {
+		/* are we a block or clear connection? */
+		enum shunt_policy shunt = c->config->prospective_shunt;
+		if (shunt != SHUNT_TRAP) {
+			/*
+			 * We need to match block/clear so we can send back
+			 * NO_PROPOSAL_CHOSEN, otherwise not match so we
+			 * can hit packetdefault to do real IKE.
+			 * clear and block do not have POLICY_OPPORTUNISTIC,
+			 * but clear-or-private and private-or-clear do, but
+			 * they don't do IKE themselves but allow packetdefault
+			 * to be hit and do the work.
+			 * if not policy_oppo -> we hit clear/block so this is right c
+			 */
+			if ((c->policy & POLICY_OPPORTUNISTIC)) {
+				connection_buf cb;
+				dbg("  skipping "PRI_CONNECTION", never negotiate + oe",
+				    pri_connection(c, &cb));
+				return false;
 			}
-			continue;
+
+			/* shunt match - stop the search for another conn if we are groupinstance*/
+			if (c->policy & POLICY_GROUPINSTANCE) {
+				return true;
+			}
 		}
-
-		/*
-		 * Success may require exact match of:
-		 * (1) XAUTH (POLICY_XAUTH)
-		 * (2) kind of IKEV1 (POLICY_AGGRESSIVE)
-		 * (3) IKE_VERSION
-		 * So if any bits are on in the exclusive OR, we fail.
-		 * Each of our callers knows what is known so specifies
-		 * the policy_exact_mask.
-		 */
-		if (c->config->ike_version != ike_version)
-			continue;
-		if ((req_policy ^ c->policy) & policy_exact_mask)
-			continue;
-
-		if (peer_id != NULL && !same_id(peer_id, &c->spd.that.id) &&
-		    (c->spd.that.id.kind != ID_FROMCERT && !id_is_any(&c->spd.that.id))) {
-				continue; /* incompatible ID */
-		}
-
-		/*
-		 * Success if all specified policy bits are in candidate's policy.
-		 * It works even when the exact-match bits are included.
-		 */
-		if ((req_policy & ~c->policy) == LEMPTY)
-			break;
+		connection_buf cb;
+		dbg("  skipping "PRI_CONNECTION", never negotiate",
+		    pri_connection(c, &cb));
+		return false;
 	}
 
-	if (DBGP(DBG_BASE)) {
-		if (c == NULL) {
-			DBG_log("find_next_host_connection returns <empty>");
-		} else {
-			connection_buf ci;
-			DBG_log("find_next_host_connection returns "PRI_CONNECTION"",
-				pri_connection(c, &ci));
-		}
-	}
-
-	return c;
-}
-
-/*
- * find_host_connection: find the first satisfactory connection
- *	with this pair of hosts.
- *
- * find_next_host_connection: find the next satisfactory connection
- *	Starts where find_host_connection left off.
- *	NOTE: it will return its argument; if you want to
- *	advance, use c->hp_next.
- *
- * We start with the list that find_host_pair_connections would yield
- * but we narrow the selection.
- *
- * We only yield a connection that can negotiate.
- *
- * The caller can specify policy requirements as
- * req_policy and policy_exact_mask.
- *
- * All policy bits found in req_policy must be in the
- * policy of the connection.
- *
- * For all bits in policy_exact mask, the req_policy
- * and connection's policy must be equal.  Likely candidates:
- * - XAUTH (POLICY_XAUTH)
- * - kind of IKEV1 (POLICY_AGGRESSIVE)
- * These should only be used if the caller actually knows
- * the exact value and has included it in req_policy.
- */
-
-static struct connection *find_v2_host_connection(const ip_address local_address,
-						  const ip_address remote_address,
-						  lset_t req_policy, lset_t policy_exact_mask,
-						  const struct id *peer_id)
-{
-	const enum ike_version ike_version = IKEv2;
-	address_buf lb;
-	address_buf rb;
-	policy_buf pb;
-	dbg("find_host_connection %s local=%s remote=%s policy=%s but ignoring ports",
-	    enum_name(&ike_version_names, ike_version),
-	    str_address(&local_address, &lb),
-	    str_address(&remote_address, &rb),
-	    str_policy(req_policy, &pb));
-
-	struct host_pair *hp = find_host_pair(local_address, remote_address);
-	if (hp == NULL) {
-		return NULL;
-	}
-
-	/* XXX: don't be fooled by "next", the search includes hp->connections */
-	struct connection *c = find_next_v2_host_connection(hp->connections,
-							    req_policy, policy_exact_mask, peer_id);
 	/*
-	 * This could be a shared IKE SA connection, in which case
-	 * we prefer to find the connection that has the IKE SA
-	 *
-	 * XXX: need to advance candidate before calling
-	 * find_next_host_connection() as otherwise it returns the
-	 * same connection, ARGH!
+	 * Skip when any REQ_POLICY bit is unset (negate makes
+	 * them set).
 	 */
-	for (struct connection *candidate = c;
-	     candidate != NULL;
-	     candidate = find_next_v2_host_connection(candidate->hp_next,
-						      req_policy, policy_exact_mask, peer_id)) {
-		if (candidate->newest_ike_sa != SOS_NOBODY)
-			return candidate;
+	if ((req_policy & ~c->policy) != LEMPTY) {
+		connection_buf cb;
+		dbg("  skipping "PRI_CONNECTION", missing policy",
+		    pri_connection(c, &cb));
+		return false;
 	}
 
-	return c;
+	return true;
 }
 
 static struct connection *ikev2_find_host_connection(struct msg_digest *md,
-						     lset_t policy, bool *send_reject_response)
+						     lset_t req_policy,
+						     bool *send_reject_response)
 {
 	const ip_endpoint *local_endpoint = &md->iface->local_endpoint;
 	const ip_endpoint *remote_endpoint = &md->sender;
-	struct id peer_id;
-	struct id *ppeer_id = NULL;
-
-	struct payload_digest *const pl_id_peer = md->chain[ISAKMP_NEXT_v2IDi];
-	if (pl_id_peer == NULL) {
-		dbg("IKEv2 no peer ID received");
-	} else {
-		enum ike_id_type hik = pl_id_peer->payload.v2id.isai_type; /* Peers Id Kind */
-		diag_t d = unpack_peer_id(hik, &peer_id, &pl_id_peer->pbs);
-		if (d != NULL) {
-			dbg("IKEv2 mode peer ID extraction failed - ignored peer ID for connection lookup");
-		} else {
-			ppeer_id = &peer_id;
-		}
-	}
 
 	/* just the adddress */
 	ip_address local_address = endpoint_address(*local_endpoint);
 	ip_address remote_address = endpoint_address(*remote_endpoint);
 
-	struct connection *c = find_v2_host_connection(local_address,
-						       remote_address,
-						       policy, LEMPTY, ppeer_id);
-	if (c == NULL) {
+	address_buf lb;
+	address_buf rb;
+	policy_buf pb;
+	dbg("%s() %s->%s policy=%s", __func__,
+	    str_address(&remote_address, &rb),
+	    str_address(&local_address, &lb),
+	    str_policy(req_policy, &pb));
+
+	struct connection *c = NULL;
+	FOR_EACH_HOST_PAIR_CONNECTION(local_address, remote_address, d) {
+		if (!match_policy(d, req_policy)) {
+			continue;
+		}
+
 		/*
-		 * See if a wildcarded connection can be found.  We
-		 * cannot pick the right connection, so we're making a
-		 * guess.  All Road Warrior connections are fair game:
-		 * we pick the first we come across (if any).  If we
-		 * don't find any, we pick the first opportunistic
-		 * with the smallest subnet that includes the peer.
-		 * There is, of course, no necessary relationship
-		 * between an Initiator's address and that of its
-		 * client, but Food Groups kind of assumes one.
+		 * This could be a shared IKE SA connection, in which
+		 * case we prefer to find the connection that has the
+		 * IKE SA
 		 */
-		for (struct connection *d = find_v2_host_connection(local_address,
-								    unset_address,
-								    policy, LEMPTY, ppeer_id);
-		     d != NULL; d = find_next_v2_host_connection(d->hp_next, policy, LEMPTY, ppeer_id)) {
-			if (d->kind == CK_GROUP) {
-				dbg("  skipping as GROUP");
-				continue;
-			}
-
-			/*
-			 * Road Warrior: we have an instant winner.
-			 */
-			if (d->kind == CK_TEMPLATE && !(d->policy & POLICY_OPPORTUNISTIC)) {
-				c = d;
-				dbg("  accepting non-opportunistic");
-				break;
-			}
-
-			/*
-			 * Opportunistic or Shunt:
-			 *
-			 * Keep searching selecting the narrowest
-			 * match, based on addresses, each time.
-			 *
-			 * Don't consider the protocol/port as, at
-			 * this point (just received an IKE_SA_INIT
-			 * request), they are not known (and won't be
-			 * known until the next exchange - IKE_AUTH).
-			 *
-			 * The end result, which depends on the order
-			 * that the connections are loaded, is
-			 * probably going to be wrong (for instance
-			 * when connections include protocol / port).
-			 */
-
-			if (!address_in_selector_range(remote_address, d->spd.that.client)) {
-				address_buf ab;
-				selector_buf sb;
-				dbg("  skipping as %s is-not in range:%s",
-				    str_address(&remote_address, &ab),
-				    str_selector(&d->spd.that.client, &sb));
-				continue;
-			}
-
-			if (c != NULL &&
-			    selector_range_in_selector_range(c->spd.that.client,
-							     d->spd.that.client)) {
-				selector_buf s1, s2;
-				dbg("  skipping as best range of %s is narrower than %s",
-				    str_selector(&c->spd.that.client, &s1),
-				    str_selector(&d->spd.that.client, &s2));
-				continue;
-			}
-
-			selector_buf s1, s2;
-			dbg("  saving oppo %s for later, previous %s",
-			    str_selector(&d->spd.that.client, &s1),
-			    c == NULL ? "n/a" : str_selector(&c->spd.that.client, &s2));
+		if (d->newest_ike_sa != SOS_NOBODY) {
+			/* instant winner */
 			c = d;
-			/* keep looking */
+			break;
 		}
-
 		if (c == NULL) {
-			endpoint_buf b;
-			policy_buf pb;
-			ldbg(md->md_logger,
-			     "%s message received on %s but no connection has been authorized with policy %s",
-			     enum_name(&ikev2_exchange_names, md->hdr.isa_xchg),
-			     str_endpoint(local_endpoint, &b),
-			     str_policy(policy, &pb));
-			*send_reject_response = true;
-			return NULL;
+			c = d;
 		}
+	}
 
-		if (c->kind != CK_TEMPLATE) {
-			endpoint_buf b;
-			connection_buf cib;
-			ldbg(md->md_logger,
-			     "%s message received on %s for "PRI_CONNECTION" with kind=%s dropped",
-			     enum_name(&ikev2_exchange_names, md->hdr.isa_xchg),
-			     str_endpoint(local_endpoint, &b),
-			     pri_connection(c, &cib),
-			     enum_name(&connection_kind_names, c->kind));
-			/*
-			 * This is used when in IKE_INIT request is
-			 * received but hits an OE clear
-			 * foodgroup. There is no point sending the
-			 * message as it is unauthenticated and cannot
-			 * be trusted by the initiator. And the
-			 * responder is revealing itself to the
-			 * initiator while it is configured to never
-			 * talk to that particular initiator. With
-			 * this, the system does not need to enforce
-			 * this policy using a firewall.
-			 *
-			 * Note that this technically violates the
-			 * IKEv2 specification that states we MUST
-			 * answer (with NO_PROPOSAL_CHOSEN).
-			 */
-			*send_reject_response = false;
-			return NULL;
-		}
-
-		/* only allow opportunistic for IKEv2 connections */
-		if (LIN(POLICY_OPPORTUNISTIC, c->policy) &&
-		    c->config->ike_version == IKEv2) {
-			connection_buf cb;
-			ldbg(md->md_logger, "oppo_instantiate called by %s with "PRI_CONNECTION,
-			     __func__, pri_connection(c, &cb));
-			c = oppo_instantiate(c, &c->spd.that.id,
-					     &local_address, &remote_address);
-		} else {
-			/* regular roadwarrior */
-			ldbg(md->md_logger, "rw_instantiate");
-			c = rw_instantiate(c, &remote_address, NULL, NULL);
-		}
-	} else {
+	if (c != NULL) {
 		/*
 		 * We found a non-wildcard connection.
-		 * Double check whether it needs instantiation anyway (eg. vnet=)
+		 *
+		 * Double check whether it needs instantiation anyway
+		 * (eg. vnet=)
 		 */
 		/* vnet=/vhost= should have set CK_TEMPLATE on connection loading */
 		passert(c->spd.this.virt == NULL);
-
 		if (c->kind == CK_TEMPLATE && c->spd.that.virt != NULL) {
 			ldbg(md->md_logger,
 			     "local endpoint has virt (vnet/vhost) set without wildcards - needs instantiation");
-			c = rw_instantiate(c, &remote_address, NULL, NULL);
-		} else if ((c->kind == CK_TEMPLATE) &&
-				(c->policy & POLICY_IKEV2_ALLOW_NARROWING)) {
+			return rw_instantiate(c, &remote_address, NULL, NULL);
+		}
+
+		if ((c->kind == CK_TEMPLATE) &&
+		    (c->policy & POLICY_IKEV2_ALLOW_NARROWING)) {
 			ldbg(md->md_logger,
 			     "local endpoint has narrowing=yes - needs instantiation");
-			c = rw_instantiate(c, &remote_address, NULL, NULL);
+			return rw_instantiate(c, &remote_address, NULL, NULL);
 		}
+
+		return c;
 	}
+
+	/*
+	 * See if a wildcarded connection can be found.  We cannot
+	 * pick the right connection, so we're making a guess.  All
+	 * Road Warrior connections are fair game: we pick the first
+	 * we come across (if any).  If we don't find any, we pick the
+	 * first opportunistic with the smallest subnet that includes
+	 * the peer.  There is, of course, no necessary relationship
+	 * between an Initiator's address and that of its client, but
+	 * Food Groups kind of assumes one.
+	 */
+	FOR_EACH_HOST_PAIR_CONNECTION(local_address, unset_address, d) {
+		if (!match_policy(d, req_policy)) {
+			continue;
+		}
+
+		if (d->kind == CK_GROUP) {
+			dbg("  skipping as GROUP");
+			continue;
+		}
+
+		/*
+		 * Road Warrior: we have an instant winner.
+		 */
+		if (d->kind == CK_TEMPLATE && !(d->policy & POLICY_OPPORTUNISTIC)) {
+			c = d;
+			dbg("  accepting non-opportunistic");
+			break;
+		}
+
+		/*
+		 * Opportunistic or Shunt:
+		 *
+		 * Keep searching selecting the narrowest
+		 * match, based on addresses, each time.
+		 *
+		 * Don't consider the protocol/port as, at
+		 * this point (just received an IKE_SA_INIT
+		 * request), they are not known (and won't be
+		 * known until the next exchange - IKE_AUTH).
+		 *
+		 * The end result, which depends on the order
+		 * that the connections are loaded, is
+		 * probably going to be wrong (for instance
+		 * when connections include protocol / port).
+		 */
+
+		if (!address_in_selector_range(remote_address, d->spd.that.client)) {
+			address_buf ab;
+			selector_buf sb;
+			dbg("  skipping as %s is-not in range:%s",
+			    str_address(&remote_address, &ab),
+			    str_selector(&d->spd.that.client, &sb));
+			continue;
+		}
+
+		if (c != NULL &&
+		    selector_range_in_selector_range(c->spd.that.client,
+						     d->spd.that.client)) {
+			selector_buf s1, s2;
+			dbg("  skipping as best range of %s is narrower than %s",
+			    str_selector(&c->spd.that.client, &s1),
+			    str_selector(&d->spd.that.client, &s2));
+			continue;
+		}
+
+		selector_buf s1, s2;
+		dbg("  saving oppo %s for later, previous %s",
+		    str_selector(&d->spd.that.client, &s1),
+		    c == NULL ? "n/a" : str_selector(&c->spd.that.client, &s2));
+		c = d;
+		/* keep looking */
+	}
+
+	if (c == NULL) {
+		endpoint_buf b;
+		policy_buf pb;
+		ldbg(md->md_logger,
+		     "%s message received on %s but no connection has been authorized with policy %s",
+		     enum_name(&ikev2_exchange_names, md->hdr.isa_xchg),
+		     str_endpoint(local_endpoint, &b),
+		     str_policy(req_policy, &pb));
+		*send_reject_response = true;
+		return NULL;
+	}
+
+	if (c->kind != CK_TEMPLATE) {
+		endpoint_buf b;
+		connection_buf cib;
+		ldbg(md->md_logger,
+		     "%s message received on %s for "PRI_CONNECTION" with kind=%s dropped",
+		     enum_name(&ikev2_exchange_names, md->hdr.isa_xchg),
+		     str_endpoint(local_endpoint, &b),
+		     pri_connection(c, &cib),
+		     enum_name(&connection_kind_names, c->kind));
+		/*
+		 * This is used when in IKE_INIT request is
+		 * received but hits an OE clear
+		 * foodgroup. There is no point sending the
+		 * message as it is unauthenticated and cannot
+		 * be trusted by the initiator. And the
+		 * responder is revealing itself to the
+		 * initiator while it is configured to never
+		 * talk to that particular initiator. With
+		 * this, the system does not need to enforce
+		 * this policy using a firewall.
+		 *
+		 * Note that this technically violates the
+		 * IKEv2 specification that states we MUST
+		 * answer (with NO_PROPOSAL_CHOSEN).
+		 */
+		*send_reject_response = false;
+		return NULL;
+	}
+
+	/* only allow opportunistic for IKEv2 connections */
+	if (LIN(POLICY_OPPORTUNISTIC, c->policy) &&
+	    c->config->ike_version == IKEv2) {
+		connection_buf cb;
+		ldbg(md->md_logger, "oppo_instantiate called by %s with "PRI_CONNECTION,
+		     __func__, pri_connection(c, &cb));
+		c = oppo_instantiate(c, &c->spd.that.id,
+				     &local_address, &remote_address);
+	} else {
+		/* regular roadwarrior */
+		ldbg(md->md_logger, "rw_instantiate");
+		c = rw_instantiate(c, &remote_address, NULL, NULL);
+	}
+
 	return c;
 }
 
