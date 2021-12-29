2b7d08fc2ebc497e19ed8310925f26c056a587fe
Andrew Cagney <cagney@gnu.org>
2021-12-29 11:20:16 -0500

    testing: in interop-netbsd-01-pluto-initiator-ikev2 don't force ESN=no

--- testing/pluto/interop-netbsd-01-pluto-initiator-ikev2/02-netbsdw-init.sh
+++ testing/pluto/interop-netbsd-01-pluto-initiator-ikev2/02-netbsdw-init.sh
@@ -1,4 +1,7 @@
 # note swan-prep does not yet support BSD
+rm -rf /usr/local/etc/ipsec.*
+mkdir -p /usr/local/etc/ipsec.d/
 cp ipsec.* /usr/local/etc/
 ipsec start
+ipsec auto --add eastnet-westnet-ikev2
 echo "initdone"
