dbbc8e0701b771890735fd37205bf1cb4399798b
Andrew Cagney <cagney@gnu.org>
2022-01-08 12:07:13 -0500

    testing: in seccomp-02-tolerant use ausearch, not service hack

--- testing/pluto/seccomp-02-tolerant/westrun.sh
+++ testing/pluto/seccomp-02-tolerant/westrun.sh
@@ -3,5 +3,5 @@ ipsec auto --down nss-cert
 # pluto should survive this and report back
 ipsec whack --seccomp-crashtest
 # no log entries should appear
-grep SECCOMP /var/log/audit/audit.log
+ausearch -r -m seccomp -ts boot
 echo done
