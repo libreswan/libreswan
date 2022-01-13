a6ad6f26bcae9fb92a25ce76ec363db9635cc142
Andrew Cagney <cagney@gnu.org>
2022-01-13 09:26:39 -0500

    building: delete pfkey_help.h, unused

--- include/pfkey_help.h
+++
@@ -1,9 +0,0 @@
-/* two pfkey I/O routines
- * definitions: lib/libswan/pfkey_sock.c, lib/libswan/pfkey_error.c
- */
-
-/* opens a pfkey socket, or dumps to stderr the reason why it failed */
-extern int pfkey_open_sock_with_error(void);
-
-extern void pfkey_write_error(int writeerror, int err);
-
