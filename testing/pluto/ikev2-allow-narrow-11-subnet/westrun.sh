175da39cb1d1e41507440af5d615a9728c804513
Andrew Cagney <cagney@gnu.org>
2022-02-09 14:14:17 -0500

    testing: add narrow subnet and subnet-protoport tests
    shuffle others

---
+++ testing/pluto/ikev2-allow-narrow-11-subnet/westrun.sh
@@ -0,0 +1,3 @@
+ipsec auto --up westnet-eastnet-ikev2
+ipsec whack --trafficstatus
+echo done
---
+++ testing/pluto/ikev2-allow-narrow-12-subnet-protoport/westinit.sh
@@ -0,0 +1,9 @@
+/testing/guestbin/swan-prep
+# confirm that the network is alive
+../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-ikev2
+ipsec auto --status | grep westnet-eastnet-ikev2
+ipsec whack --impair suppress-retransmits
+echo "initdone"
