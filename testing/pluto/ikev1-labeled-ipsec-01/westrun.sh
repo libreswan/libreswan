dc10154784c8dcdc6dd0bebb1512d086b4cf58d1
Andrew Cagney <cagney@gnu.org>
2022-02-14 17:40:18 -0500

    testing: update ikev1-labeled-ipsec-01
    Use a contexted ping.
    
    It screws up some of the security checks, but the connection
    works.

--- testing/pluto/ikev1-labeled-ipsec-01/westrun.sh
+++ testing/pluto/ikev1-labeled-ipsec-01/westrun.sh
@@ -1,8 +1,15 @@
 # for port re-use in tests with protoport selectors
 echo 1 >/proc/sys/net/ipv4/tcp_tw_reuse
 ipsec auto --up labeled
+# there should be 1 (bogus) tunnel in each direction
 ip xfrm state
 ip xfrm policy
-echo "quit" | runcon -t netutils_t nc -w 50 -p 4301 -vvv 192.1.2.23 4300 2>&1 | sed "s/received in .*$/received .../"
+ipsec trafficstatus
+# trigger acquire using the predefined ping_t context
+../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match '#3' -- ipsec trafficstatus
+sleep 5 # allow for IKEv1 protocol sillyness
+../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --up     -I 192.1.2.45 192.1.2.23
+# there should be 2 tunnel in each direction
 ipsec trafficstatus
 echo done
