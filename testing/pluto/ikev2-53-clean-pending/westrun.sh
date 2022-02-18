2487c2088233678000637a27bc202655a36cee91
Andrew Cagney <cagney@gnu.org>
2022-02-18 10:00:54 -0500

    testing: in ikev2-53-clean-pending, don't play with dpd
    instead expect the retrying IKE SA to carry over the pending Child SAs

--- testing/pluto/ikev2-53-clean-pending/westrun.sh
+++ testing/pluto/ikev2-53-clean-pending/westrun.sh
@@ -1,11 +1,4 @@
 ipsec auto --status | grep westnet-eastnet-ipv4-psk-ikev2
-#sleep for more than PENDING_PHASE2_INTERVAL > 121s
-sleep 30
-sleep 30
-sleep 30
-sleep 30
-sleep 1
+# wait for the IKE SA to die
+../../guestbin/wait-for.sh --timeout 40 --match '#2: pending CHILD SA' -- ipsec auto --status
 ipsec auto --status | grep westnet-eastnet-ipv4-psk-ikev2
-# this should find a match
-grep "took too long" /tmp/pluto.log
-echo done
