7ca139ccb118c77aebeeb3367e353b7e3e910557
Andrew Cagney <cagney@gnu.org>
2022-02-18 09:34:35 -0500

    testing: drop dpdtimeout= from (most) IKEv2 tests
    and enable addconn-09-dpd-vs-liveness
    
    Two tests are left broken:
      ikev2-53-clean-pending - has bigger problems
      ikev2-liveness-09 - uses sleep

--- testing/pluto/addconn-09-dpd-vs-liveness/west.sh
+++ testing/pluto/addconn-09-dpd-vs-liveness/west.sh
@@ -2,11 +2,12 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 
-ipsec auto --add ikev1-dpdtimeout=10s		# requires dpddelay
-ipsec auto --add ikev1-dpddelay=10s		# requires dpdtimeout
-ipsec auto --add ikev1-dpdaction=clear		# requires dpddelay+dpdtimeout
+# don't use tabs, guest things it is tab completion
+ipsec auto --add ikev1-dpdtimeout=10s            # requires dpddelay
+ipsec auto --add ikev1-dpddelay=10s              # requires dpdtimeout
+ipsec auto --add ikev1-dpdaction=clear           # requires dpddelay+dpdtimeout
 ipsec auto --add ikev1-dpdaction=clear-dpdtimeout=10s # requires dpddelay
 ipsec auto --add ikev1-dpddelay=10s-dpdtimeout=0s # requires dpdtimeout!=0
 
-ipsec auto --add ikev2-dpdtimeout=10s		# ignore dpdtimeout
-ipsec auto --add ikev2-dpdaction=clear		# requires dpddelay
+ipsec auto --add ikev2-dpdtimeout=10s            # ignore dpdtimeout
+ipsec auto --add ikev2-dpdaction=clear           # requires dpddelay
