ea01c80eaf00c1ff63b23fc69f075afc652e663c
Andrew Cagney <cagney@gnu.org>
2022-02-18 17:16:57 -0500

    bsd: enable kernel_pfkeyv2 by default

--- mk/defaults/freebsd.mk
+++ mk/defaults/freebsd.mk
@@ -14,6 +14,8 @@ NSS_LDFLAGS = -L$(PKG_BASE)/lib/nss -Wl,-rpath,$(PKG_BASE)/lib/nss -lnss3 -lfree
 NSPR_LDFLAGS = -L$(PKG_BASE)/lib/nspr -Wl,-rpath,$(PKG_BASE)/lib/nspr -lnspr4
 
 USE_BSDKAME = true
+USE_PFKEYV2 = true
+
 USE_LIBCAP_NG = false
 USE_UNBOUND_EVENT_H_COPY = true
 USE_PTHREAD_SETSCHEDPRIO = false
--- mk/defaults/netbsd.mk
+++ mk/defaults/netbsd.mk
@@ -18,6 +18,7 @@ NSPR_LDFLAGS = -L$(PKG_BASE)/lib/nspr -Wl,-rpath,$(PKG_BASE)/lib/nspr -lnspr4
 CRYPT_LDFLAGS =
 
 USE_BSDKAME = true
+USE_PFKEYV2 = true
 USE_LIBCAP_NG = false
 
 INITSYSTEM=rc.d
