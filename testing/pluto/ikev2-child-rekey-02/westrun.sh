7f8b484f3873936212c9b3634b244f66dc55fc2d
Andrew Cagney <cagney@gnu.org>
2022-02-25 15:56:36 -0500

    testing: wait-for.sh "rekeyed Child SA"

--- testing/pluto/ikev2-child-rekey-02/westrun.sh
+++ testing/pluto/ikev2-child-rekey-02/westrun.sh
@@ -11,9 +11,9 @@ ipsec auto --up westnet-eastnet-ikev2c
 ipsec whack --trafficstatus
 
 # rekey to Child #5 #6 #7
-../../guestbin/wait-for.sh --match '#5: initiator established Child SA' -- cat /tmp/pluto.log
-../../guestbin/wait-for.sh --match '#6: initiator established Child SA' -- cat /tmp/pluto.log
-../../guestbin/wait-for.sh --match '#7: initiator established Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#5: initiator rekeyed Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#6: initiator rekeyed Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#7: initiator rekeyed Child SA' -- cat /tmp/pluto.log
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
 ../../guestbin/ping-once.sh --up -I 192.0.101.254 192.0.201.254
@@ -25,9 +25,9 @@ ipsec whack --trafficstatus
 ipsec whack --trafficstatus
 
 # rekey to Child #8 #9 #10
-../../guestbin/wait-for.sh --match '#8: initiator established Child SA' -- cat /tmp/pluto.log
-../../guestbin/wait-for.sh --match '#9: initiator established Child SA' -- cat /tmp/pluto.log
-../../guestbin/wait-for.sh --match '#10: initiator established Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#8: initiator rekeyed Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#9: initiator rekeyed Child SA' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#10: initiator rekeyed Child SA' -- cat /tmp/pluto.log
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
 ../../guestbin/ping-once.sh --up -I 192.0.101.254 192.0.201.254
