65d022569c893b865470fcd000282f6ca1d057c2
Andrew Cagney <cagney@gnu.org>
2022-03-09 17:35:59 -0500

    testing: in ikev2-ike-rekey-04 use ipsec showstates
    See #466

--- testing/pluto/ikev2-ike-rekey-04/final.sh
+++ testing/pluto/ikev2-ike-rekey-04/final.sh
@@ -1,4 +1,4 @@
-ipsec status |grep STATE_
+ipsec showstates
 # there should be only one IKE_INIT exchange
 grep "PARENT_I1 with STF_OK" /tmp/pluto.log
 grep "PARENT_R1 with status STF_OK" /tmp/pluto.log
--- testing/pluto/ikev2-ike-rekey-04/westrun.sh
+++ testing/pluto/ikev2-ike-rekey-04/westrun.sh
@@ -1,18 +1,15 @@
 ipsec auto --up westnet-eastnet-ikev2a
 ipsec auto --up westnet-eastnet-ikev2b
 ipsec auto --up westnet-eastnet-ikev2c
-# expect: ike #1 IPsec #2 #3 #4
-ipsec status |grep STATE_
+# confirm
+ipsec showstates # expect: IKE #1 Child #2 #3 #4
+# wait for Child SAs to rekey
 sleep 30
-# ipsec should be rekeyed
-# expect: ike #1 IPsec #5 #6 #7
-ipsec status |grep STATE_
+ipsec showstates # expect: IKE #1 Child #5 #6 #7
+# wait for IKE SA to rekey
 sleep 20
-# ike sa should be rekeyed
-# expect: ike #8 IPsec #9 #10 #11
-ipsec status |grep STATE_
+ipsec showstates # expect: IKE #8 Child #9 #10 #11
+# wait for Child SAs to rekey again
 sleep 30
-# second rekey of IPsec SAs
-# expect: ike #8 IPsec #12 #13 #14
-ipsec status |grep STATE_
+ipsec showstates # expect: IKE #8 Child #12 #13 #14
 echo done
