c8f3b12939dbc4f390aa60ed7a4bba6a216dd42a
Andrew Cagney <cagney@gnu.org>
2022-03-21 16:58:27 -0400

    testing: update liveness, slightly more agressive

--- testing/pluto/ikev2-liveness-03-both-active-clear/03-west-up.sh
+++ testing/pluto/ikev2-liveness-03-both-active-clear/03-west-up.sh
@@ -1,4 +1,5 @@
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-# look for a probe
-../../guestbin/wait-for.sh --match 'received message ... 2 ' -- sed -n -e 's/ [a-z]* 2 (.*/ ... 2 (...)/p' /tmp/pluto.log
+# look for two probes
+../../guestbin/wait-for.sh --match 'responder received message ... 2 ' -- sed -n -e 's/ [a-z]* 2 (.*/ ... 2 (...)/p' /tmp/pluto.log
+../../guestbin/wait-for.sh --match 'initiator received message ... 2 ' -- sed -n -e 's/ [a-z]* 2 (.*/ ... 2 (...)/p' /tmp/pluto.log
