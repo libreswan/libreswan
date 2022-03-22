10f12fd7692cb479e77e55a4b9e43335e2212b66
Andrew Cagney <cagney@gnu.org>
2022-03-22 08:25:09 -0400

    testing: fix typo, update interop-ikev2-strongswan-38-digsig-impair
    Because pluto is ignoring strongswan's SIGNATURE_HASH_ALGORITHMS
    notify, it authenticates itself using RSA, not digital signatures.

--- testing/pluto/interop-ikev2-strongswan-38-digsig-impair/eastinit.sh
+++ testing/pluto/interop-ikev2-strongswan-38-digsig-impair/eastinit.sh
@@ -2,5 +2,5 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-ipsec whack --impair ignore-v2n-certificate-hash-algorithms
+ipsec whack --impair ignore-v2n-signature-hash-algorithms
 echo "initdone"
