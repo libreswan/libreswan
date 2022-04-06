aa160a4ddbabe2ccde0244246109f7915e5e6eab
Andrew Cagney <cagney@gnu.org>
2022-04-06 11:33:55 -0400

    testing: in ikev1-02-fuzzer target PSK PSK+RSASIG RSASIG
    as well as random auth

--- testing/pluto/ikev1-02-fuzzer/ike-scan.sh
+++ testing/pluto/ikev1-02-fuzzer/ike-scan.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-COUNT=0
-while ((COUNT += 1)) ; [ ${COUNT} -lt 10 ] ; do
+# https://datatracker.ietf.org/doc/html/rfc2409#page-33
 
-    R1=$(( ( RANDOM % 32 )  + 1 ))
-    R2=$(( ( RANDOM % 32 )  + 1 ))
-    R3=$(( ( RANDOM % 32 )  + 1 ))
-    R4=$(( ( RANDOM % 32 )  + 1 ))
-    echo "COUNT=$COUNT R1=${R1} R2=${R2} R3=${R3} R4=${R4}"
+for AUTH in 3=1 3=3 3=1,3=3 3=$(( ( RANDOM % 32 )  + 1 ))
+do
+    COUNT=0
+    while ((COUNT += 1)) ; [ ${COUNT} -lt 5 ] ; do
 
-    #EXTRA="--aggressive"
-    EXTRA=""
+	ENCR=1=$(( ( RANDOM % 32 )  + 1 ))
+	HASH=2=$(( ( RANDOM % 32 )  + 1 ))
+	GROUP=4=$(( ( RANDOM % 32 )  + 1 ))
+	echo "COUNT=$COUNT ${AUTH} ${ENCR} ${HASH} ${GROUP}"
 
-    ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(1=$R1,2=$R2,3=$R3,4=$R4)" 192.1.2.23
-    ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(2=$R2,3=$R3,4=$R4)" 192.1.2.23
-    ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(1=$R1,3=$R3,4=$R4)" 192.1.2.23
-    ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(1=$R1,2=$R2,4=$R4)" 192.1.2.23
-    ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(1=$R1,2=$R2,3=$R3)" 192.1.2.23
+	#EXTRA="--aggressive"
+	EXTRA=""
+
+	ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(${ENCR},${HASH},${AUTH},${GROUP})" 192.1.2.23
+	ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(${HASH},${AUTH},${GROUP})" 192.1.2.23
+	ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(${ENCR},${AUTH},${GROUP})" 192.1.2.23
+	ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(${ENCR},${HASH},${GROUP})" 192.1.2.23
+	ike-scan $EXTRA --nodns -v --retry 1 --showbackoff --backoff=1 --interval=10  --trans="(${ENCR},${HASH},${AUTH})" 192.1.2.23
+    done
 done
