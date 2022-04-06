1ea0e9919e1a53aece4df441403e979f9e6aa825
Andrew Cagney <cagney@gnu.org>
2022-04-06 10:51:57 -0400

    testing: in ikev1-02-fuzzer add both rsasig and secret connections
    Increase odds that fuzzer will be accepted.
    (targeted fuzz script still needs work)

--- testing/pluto/ikev1-02-fuzzer/eastinit.sh
+++ testing/pluto/ikev1-02-fuzzer/eastinit.sh
@@ -2,5 +2,6 @@
 # Start only one, so it is easier to spot a crash
 ipsec pluto --config /etc/ipsec.conf 
 ../../guestbin/wait-until-pluto-started
-ipsec auto --add test
+ipsec auto --add rsasig
+ipsec auto --add secret
 echo "initdone"
