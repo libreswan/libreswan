51fe2580be4b08555473acb4d9d7155858a59761
Andrew Cagney <cagney@gnu.org>
2022-04-08 13:10:43 -0400

    testing: update ikev2-oe-09-transport-ipv6; mark it good
    ref: #215

--- testing/pluto/ikev2-oe-09-transport-ipv6/01-east-init.sh
+++ testing/pluto/ikev2-oe-09-transport-ipv6/01-east-init.sh
@@ -1,5 +1,9 @@
 /testing/guestbin/swan-prep --46
 cp policies/* /etc/ipsec.d/policies/
+echo "2001:db8:1:3::0/64" >> /etc/ipsec.d/policies/private-or-clear
+echo "2001:db8:1:3::254/128" >> /etc/ipsec.d/policies/clear
+echo "2001:db8:1:2::254/128" >> /etc/ipsec.d/policies/clear
+echo "fe80::/10" >> /etc/ipsec.d/policies/clear
 ipsec start
 ../../guestbin/wait-until-pluto-started
 # give OE policies time to load
