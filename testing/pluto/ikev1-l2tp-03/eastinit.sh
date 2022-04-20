418162d62eb81147ec9cf7b99429ff6ee65b304e
Andrew Cagney <cagney@gnu.org>
2022-04-20 17:30:21 -0400

    testing: get ikev1-l2tp-03 to print "Virtual IP overlaps with connection"

--- testing/pluto/ikev1-l2tp-03/eastinit.sh
+++ testing/pluto/ikev1-l2tp-03/eastinit.sh
@@ -1,7 +1,10 @@
-/testing/guestbin/swan-prep
+/testing/guestbin/swan-prep --x509
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec auto --add any-east-l2tp
+ipsec auto --add north-on-east-l2tp
+ipsec auto --add road-l2tp # distraction
+
 # ensure that clear text does not get through
 iptables -A INPUT  -i eth1 -d 192.1.2.23 -m policy --dir in --pol none -p udp --dport 1701 -j REJECT
 iptables -A INPUT -m policy --dir in --pol ipsec -j ACCEPT
--- testing/pluto/ikev1-l2tp-03/northinit.sh
+++ testing/pluto/ikev1-l2tp-03/northinit.sh
@@ -1,4 +1,5 @@
-/testing/guestbin/swan-prep
+/testing/guestbin/swan-prep --x509
+
 iptables -F INPUT
 iptables -F OUTPUT
 # ensure that clear text does not get through
@@ -8,9 +9,11 @@ iptables -A OUTPUT -o eth1 -d 192.1.2.23 -m policy --dir out --pol none -p udp -
 iptables -A OUTPUT -m policy --dir out --pol ipsec -j ACCEPT
 iptables -A INPUT -i eth1 -s 192.1.2.23 -m policy --dir in --pol none -p udp --sport 1701 -j REJECT
 iptables -A INPUT -m policy --dir in --pol ipsec -j ACCEPT
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec auto --add north-east-l2tp
+ipsec auto --add north-on-north-l2tp
+
 (cd /tmp && xl2tpd -D 2>/tmp/xl2tpd.log ) &
-ipsec auto --route north-east-l2tp
+ipsec auto --route north-on-north-l2tp
 echo done
