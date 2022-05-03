3f5c0a1890575b766c9671b20bd4f1393eaaa690
Andrew Cagney <cagney@gnu.org>
2022-05-03 14:37:28 -0400

    testing: make certoe-17-asymmetric-cert-nat-testing-client-crash more robust

--- testing/pluto/certoe-17-asymmetric-cert-nat-testing-client-crash/03-road-ping.sh
+++ testing/pluto/certoe-17-asymmetric-cert-nat-testing-client-crash/03-road-ping.sh
@@ -2,7 +2,8 @@
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
 # trigger ping, this will be lost
-../../guestbin/ping-once.sh --down -I 192.1.3.209 192.1.2.23
+../../guestbin/ping-once.sh --fire-and-forget -I 192.1.3.209 192.1.2.23
+../../guestbin/wait-for.sh --match '#2' -- ipsec whack --trafficstatus
 # ping should succeed through tunnel
 ../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
 ipsec whack --trafficstatus
