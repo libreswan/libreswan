6255371fe15592e1d6dff32818d01fc86dd39f1b
Andrew Cagney <cagney@gnu.org>
2022-05-17 19:49:53 -0400

    testing: eliminate <<sleep 2>> in ikev2-systemrole-01-host-to-host

--- testing/pluto/ikev2-systemrole-01-host-to-host/05-west-restart-test.sh
+++ testing/pluto/ikev2-systemrole-01-host-to-host/05-west-restart-test.sh
@@ -4,9 +4,9 @@ ipsec restart
 ipsec status
 # this assumes connection loaded with auto=ondemand
 # trigger tunnel - the first trigger ping packet is lost
+../../guestbin/ping-once.sh --down -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match 192.1.2.45-to-192.1.2.23 -- ipsec whack --trafficstatus
 ../../guestbin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
-sleep 2
-ping -n -q -c 4 -I 192.1.2.45 192.1.2.23
 # show non-zero IPsec traffic counters
 ipsec whack --trafficstatus
 echo done
