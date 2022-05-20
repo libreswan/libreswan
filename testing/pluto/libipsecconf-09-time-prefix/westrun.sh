b33136d103a15cdcf744f00cea07b926593d045f
Antony Antony <antony@phenome.org>
2022-05-20 10:51:29 +0000

    testing: timeparsing wip tests
    add hex number in config. This connection should fail to load?
    I wonder if pervious parser code allowed hex numbers?
    Now the connection is loads with the default value instead of throwing error.

--- testing/pluto/libipsecconf-09-time-prefix/westrun.sh
+++ testing/pluto/libipsecconf-09-time-prefix/westrun.sh
@@ -2,7 +2,7 @@ ipsec auto --add time-good
 # rekey_margin: should be less than rekey interval
 ipsec status | grep margin
 # output is unpredictable, include ephemeral values
-ipsec addconn --verbose time-wip
-ipsec status | grep margin
-ipsec addconn --verbose time-wip
-ipsec status | grep margin
+ipsec addconn time-wip
+ipsec status | grep time-wip | grep margin
+ipsec addconn time-wip-hex
+ipsec status | grep time-wip-hex | grep margin
