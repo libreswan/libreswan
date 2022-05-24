49f930dcd5501325ba2d44e34401a3347b7a6d38
Andrew Cagney <cagney@gnu.org>
2022-05-24 15:20:15 -0400

    kvm: on NetBSD install a custom kernel when available

--- testing/libvirt/netbsd/transmogrify.sh
+++ testing/libvirt/netbsd/transmogrify.sh
@@ -6,6 +6,7 @@ GATEWAY=@@GATEWAY@@
 POOLDIR=@@POOLDIR@@
 SOURCEDIR=@@SOURCEDIR@@
 TESTINGDIR=@@TESTINGDIR@@
+PREFIX=@@PREFIX@@
 
 echo GATEWAY=${GATEWAY}
 echo POOLDIR=${POOLDIR}
@@ -15,14 +16,22 @@ echo TESTINGDIR=${TESTINGDIR}
 
 # update /etc/fstab with current /source and /testing
 
-mkdir -p /pool /source /testing
+mkdir -p /source /testing
 sed -i -e '/source/d' -e '/testing/d' /etc/fstab
 cat <<EOF | tee -a /etc/fstab
 ${GATEWAY}:${SOURCEDIR}   /source         nfs     rw
 ${GATEWAY}:${TESTINGDIR}  /testing        nfs     rw
 EOF
+echo
+cat /etc/fstab
+echo
+
+k=/pool/${PREFIX}netbsd-kernel
+if test -r $k ; then
+    cp -v $k /netbsd
+fi
 
 mount /testing
-cp /testing/libvirt/netbsd/rc.local /etc/
+cp -v /testing/libvirt/netbsd/rc.local /etc/
 
 exit 0
