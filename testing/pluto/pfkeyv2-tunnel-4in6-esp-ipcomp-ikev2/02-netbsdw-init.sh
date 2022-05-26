078a9c5f391c87f65ebb96cf3d84e8b1c37d3978
Andrew Cagney <cagney@gnu.org>
2022-05-25 22:04:18 -0400

    testing: interop NetBSD->FreeBSD in pfkeyv2 4to6 and 6to4 tests
    basic ESP/AH more-or-less pass
    IPcomp doesn't

--- testing/pluto/pfkeyv2-tunnel-4in6-esp-ipcomp-ikev2/02-netbsdw-init.sh
+++ testing/pluto/pfkeyv2-tunnel-4in6-esp-ipcomp-ikev2/02-netbsdw-init.sh
@@ -2,5 +2,6 @@
 ../../guestbin/netbsd-prep.sh
 
 ipsec start
+../../guestbin/wait-until-pluto-started
 ipsec auto --add eastnet-westnet-ikev2
 echo "initdone"
--- testing/pluto/pfkeyv2-tunnel-6in4-esp-ikev2/01-freebsde-init.sh
+++ testing/pluto/pfkeyv2-tunnel-6in4-esp-ikev2/01-freebsde-init.sh
@@ -1,4 +1,6 @@
-/testing/guestbin/swan-prep --46
+# note swan-prep does not yet support BSD
+../../guestbin/netbsd-prep.sh
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add eastnet-westnet-ikev2
