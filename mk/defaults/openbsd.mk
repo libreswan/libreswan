e8ba15b26b8adeea025a39db2f1e9b9b2340f136
Andrew Cagney <cagney@gnu.org>
2022-05-29 15:54:44 -0400

    openbsd: add openbsd.mk stub (it doesn't enable a kernel backend)

---
+++ mk/defaults/openbsd.mk
@@ -0,0 +1,40 @@
+BSD_VARIANT = openbsd
+# sketch out pkgsrc
+PKG_BASE ?= /usr/local
+
+# See: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=264288
+# See: https://github.com/libreswan/libreswan/issues/735
+#CC=gcc
+
+WARNING_CFLAGS=
+USERLAND_CFLAGS += -DHAS_SUN_LEN
+USERLAND_CFLAGS += -DNEED_SIN_LEN
+
+USERLAND_INCLUDES += -I$(PKG_BASE)/include
+
+USERLAND_LDFLAGS += -L$(PKG_BASE)/lib -Wl,-rpath,$(PKG_BASE)/lib
+
+# NSS includes more than needed in ldflags
+NSS_LDFLAGS = -L$(PKG_BASE)/lib/nss -Wl,-rpath,$(PKG_BASE)/lib/nss -lnss3 -lfreebl3 -lssl3
+NSPR_LDFLAGS = -L$(PKG_BASE)/lib/nspr -Wl,-rpath,$(PKG_BASE)/lib/nspr -lnspr4
+
+#USE_BSDKAME = true
+#USE_PFKEYV2 = true
+CRYPT_LDFLAGS =
+RT_LDFLAGS = 
+
+USE_LIBCAP_NG = false
+USE_UNBOUND_EVENT_H_COPY = true
+USE_PTHREAD_SETSCHEDPRIO = false
+USE_AUTHPAM = false
+USE_DNSSEC = true
+
+INITSYSTEM=rc.d
+
+# not /run/pluto
+FINALRUNDIR=/var/run/pluto
+
+# PREFIX = /usr/local from mk/config.mk
+FINALSYSCONFDIR=$(PREFIX)/etc
+FINALNSSDIR=$(PREFIX)/etc/ipsec.d
+FINALEXAMPECONFDIR=$(PREFIX)/share/examples/libreswan
