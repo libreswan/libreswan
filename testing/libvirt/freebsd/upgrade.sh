57835965716291ec0e60189144ecb1f2c8dec047
Andrew Cagney <cagney@gnu.org>
2022-06-09 09:48:11 -0400

    kvm: enable FreeBSD's package download cache

--- testing/libvirt/freebsd/upgrade.sh
+++ testing/libvirt/freebsd/upgrade.sh
@@ -2,7 +2,21 @@
 
 set -xe
 
+# download /usr/local/etc/pkg.conf
+
 pkg bootstrap -y
+
+# Enable the cache
+
+mkdir -p /pool/pkg.freebsd
+ed <<EOF /usr/local/etc/pkg.conf
+/PKG_CACHEDIR/ s;.*PKG_CACHEDIR.*;PKG_CACHEDIR = "/pool/pkg.freebsd";
+w
+q
+EOF
+
+# install dependencies
+
 pkg install -y gmake
 pkg install -y git
 pkg install -y pkgconf # aka pkg-config; grrr
