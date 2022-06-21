fba89035ddc05607f4dac20f20b49c6e75d9be42
Andrew Cagney <cagney@gnu.org>
2022-06-21 15:20:06 -0400

    building: drop unused #define DEBUG_NO_STATIC static

--- include/libreswan.h
+++ include/libreswan.h
@@ -36,8 +36,6 @@
 # endif
 #endif
 
-#define DEBUG_NO_STATIC static
-
 #if !defined(ESPINUDP_WITH_NON_IKE)
 #define ESPINUDP_WITH_NON_IKE   1       /* draft-ietf-ipsec-nat-t-ike-00/01 */
 #define ESPINUDP_WITH_NON_ESP   2       /* draft-ietf-ipsec-nat-t-ike-02    */
