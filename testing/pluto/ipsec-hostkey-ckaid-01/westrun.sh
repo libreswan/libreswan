f319d26e591286c9823831496f3c028d481e26e4
Andrew Cagney <cagney@gnu.org>
2022-07-03 10:40:10 -0400

    testing: check showhostkey --ipseckey

--- testing/pluto/ipsec-hostkey-ckaid-01/westrun.sh
+++ testing/pluto/ipsec-hostkey-ckaid-01/westrun.sh
@@ -2,4 +2,6 @@
 rm -f /tmp/newhostkey.txt
 ckaid=$(ipsec newhostkey 2>&1 | grep "showhostkey" | sed "s/^.*ckaid //")
 ipsec showhostkey --list
-ipsec showhostkey --left --ckaid "$ckaid"
+ipsec showhostkey --dump
+ipsec showhostkey --left --ckaid "${ckaid}"
+ipsec showhostkey --ipseckey --ckaid "${ckaid}"
