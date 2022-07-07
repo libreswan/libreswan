libreswan/libreswan

commit b79030c16674e449dc3187460c5d18aba90b35f2

lib: parser add binary prefix support
needed for ipsec-max-bytes and ipsec-max-packets

 - Binary prefixes Ki,Mi,Gi,Ti,Pi,Ei ISO/IEC standard packets
 - Bytes prefixes B,KiB,MiB,GiB,TiB,PiB,EiB ISO/IEC standard

https://en.wikipedia.org/wiki/Binary_prefix

include/binary-iec-60027-2.h
@@ -1,0 +1,24 @@
+/* binary IEC 60027-2 prefix and scale, for libreswan
+ *
+ * Copyright (C) 2022  Antony Antony
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <https://www.gnu.org/licenses/lgpl-2.1.txt>.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ * License for more details.
+ *
+ */
+
+#ifndef TTOBINARY_H
+#define TTOBINARY_H    /* seen it, no need to see it again */
+
+#include "diag.h"
+
+diag_t ttobinary(const char *t, unsigned long *b, bool prefix_B);
+
+#endif

include/binaryscale-iec-60027-2.h
@@ -1,0 +1,38 @@
+/* scale binary IEC 60027-2, for libreswan
+ *
+ * Copyright (C) 2022  Antony Antony
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <https://www.gnu.org/licenses/lgpl-2.1.txt>.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ * License for more details.
+ *
+ */
+
+#ifndef BINARYSCALE_H
+#define BINARYSCALE_H    /* seen it, no need to see it again */
+
+#include <stdint.h>	/* for uintmax_t */
+
+#include "shunk.h"
+
+struct binaryscale {
+	const char *suffix;
+	uintmax_t b;
+};
+
+extern const struct binaryscale bin_default;
+extern const struct binaryscale bin_bytedefult;
+
+#define PRI_BINARYSCALE "1%s(%ju%s)"
+#define pri_binaryscale(B) (B).suffix, (B).b, (B).prefix
+
+const struct binaryscale *ttobinaryscale(shunk_t s);
+const struct binaryscale *ttobinarybytesscale(shunk_t s);
+
+#endif

include/constants.h
@@ -40,6 +40,15 @@ enum {
 	secs_per_day = 24 * secs_per_hour
 };
 
+enum binary {
+	binary_per_kilo = 1024UL,
+	binary_per_mega = 1024UL * binary_per_kilo,
+	binary_per_giga = 1024UL * binary_per_mega,
+	binary_per_tera = 1024UL * binary_per_giga,
+	binary_per_peta = 1024UL * binary_per_tera,
+	binary_per_exa  = 1024UL * binary_per_peta, /* 2^64 s 16 Exa */
+};
+
 /*
  * This file was split into internal constants (Libreswan/pluto related),
  * and external constants (defined by IETF, etc.)

lib/libipsecconf/parser.y
@@ -26,10 +26,12 @@
 #include <limits.h>
 #include <unistd.h>
 #include <errno.h>
+#include <inttypes.h>
 #define YYDEBUG 1
 
 #include "deltatime.h"
 #include "timescale.h"
+#include "binary-iec-60027-2.h"
 
 #include "ipsecconf/keywords.h"
 #include "ipsecconf/parser.h"	/* includes parser.tab.h" */
@@ -72,6 +74,8 @@ static struct starter_comments_list *parser_comments;
 %token <k>      TIMEWORD
 %token <k>      BOOLWORD
 %token <k>      PERCENTWORD
+%token <k>      BINARYWORD
+%token <k>      BYTEWORD
 %token <k>      COMMENT
 %%
 
@@ -184,6 +188,8 @@ statement_kw:
 		case kt_number:
 		case kt_time:
 		case kt_percent:
+		case kt_binary:
+		case kt_byte:
 			yyerror("keyword value is a keyword, but type not a string");
 			assert(kw.keydef->type != kt_bool);
 			break;
@@ -248,6 +254,8 @@ statement_kw:
 		case kt_number:
 		case kt_time:
 		case kt_percent:
+		case kt_binary:
+		case kt_byte:
 			yyerror("valid keyword, but value is not a number");
 			assert(kw.keydef->type != kt_bool);
 			break;
@@ -324,6 +332,42 @@ statement_kw:
 		new_parser_kw(&$1, NULL, $<num>3);
 	}
 	| KEYWORD EQUAL { /* this is meaningless, we ignore it */ }
+	| BINARYWORD EQUAL INTEGER {
+		struct keyword *kw = &$1;
+		unsigned long b = $3;
+		new_parser_kw(kw, NULL, b);
+	}
+	| BINARYWORD EQUAL STRING {
+		struct keyword *kw = &$1;
+		const char *const str = $3;
+		unsigned long b;
+
+		diag_t diag = ttobinary(str, &b, 0 /* no B prefix */);
+		if (diag != NULL) {
+			yyerror(str_diag(diag));
+			pfree_diag(&diag);
+		} else {
+			new_parser_kw(kw, NULL, b);
+		}
+	}
+	| BYTEWORD EQUAL INTEGER {
+		struct keyword *kw = &$1;
+		unsigned long b = $3;
+		new_parser_kw(kw, NULL, b);
+	}
+	| BYTEWORD EQUAL STRING {
+		struct keyword *kw = &$1;
+		const char *const str = $3;
+		unsigned long b;
+
+		diag_t diag = ttobinary(str, &b, 1 /* with B prefix */);
+		if (diag != NULL) {
+			yyerror(str_diag(diag));
+			pfree_diag(&diag);
+		} else {
+			new_parser_kw(kw, NULL, b);
+		}
+	}
 	;
 %%
 

include/ipsecconf/keywords.h
@@ -368,6 +368,8 @@ enum keyword_type {
 	kt_number,              /* an integer */
 	kt_time,                /* a number representing time */
 	kt_percent,             /* a number representing percentage */
+	kt_byte,                /* a number representing Binary bytes with prefixs. KiB.. IEC 60027-2/ISO 8000 */
+	kt_binary,              /* a number representing Binary prefixes Ki. IEC 60027-2/ISO 8000  */
 	kt_range,               /* ip address range 1.2.3.4-1.2.3.10 */
 	kt_ipaddr,              /* an IP address */
 	kt_subnet,              /* an IP address subnet */

lib/libipsecconf/confread.c
@@ -354,6 +354,8 @@ static bool load_setup(struct starter_config *cfg,
 		case kt_number:
 		case kt_time:
 		case kt_percent:
+		case kt_binary:
+		case kt_byte:
 			/* all treated as a number for now */
 			assert(f < elemsof(cfg->setup.options));
 			cfg->setup.options[f] = kw->number;
@@ -911,6 +913,8 @@ static bool translate_field(struct starter_conn *conn,
 	case kt_number:
 	case kt_time:
 	case kt_percent:
+	case kt_binary:
+	case kt_byte:
 		/* all treated as a number for now */
 		assert(opt_floor <= field && field < opt_roof);
 

lib/libipsecconf/confwrite.c
@@ -161,6 +161,8 @@ static void confwrite_int(FILE *out,
 			break;
 
 		case kt_time: /* special number, but do work later XXX */
+		case kt_binary:
+		case kt_byte:
 		case kt_number:
 			if (options_set[k->field])
 				fprintf(out, "\t%s%s=%d\n", side, k->keyname,
@@ -228,6 +230,8 @@ static void confwrite_str(FILE *out,
 			break;
 
 		case kt_time:
+		case kt_binary:
+		case kt_byte:
 			/* special number, not a string */
 			break;
 

lib/libipsecconf/keywords.c
@@ -653,6 +653,12 @@ int parser_find_keyword(const char *s, YYSTYPE *lval)
 	case kt_time:
 		keywordtype = TIMEWORD;
 		break;
+	case kt_binary:
+		keywordtype = BINARYWORD;
+		break;
+	case kt_byte:
+		keywordtype = BYTEWORD;
+		break;
 	case kt_comment:
 		keywordtype = COMMENT;
 		break;

commit fee9e621e21ecc54003561142cbc6dbaf6cda997

pluto: 32bit fixes for binary keyword
Fixes: b79030c16674 ("lib: parser add binary prefix support")
Fixes: 1045691b8da5 ("pluto: add support for xfrm sa expire messages")

include/binary-iec-60027-2.h
@@ -19,6 +19,6 @@
 
 #include "diag.h"
 
-diag_t ttobinary(const char *t, unsigned long *b, bool prefix_B);
+diag_t ttobinary(const char *t,  uint64_t *b, bool prefix_B);
 
 #endif

include/binaryscale-iec-60027-2.h
@@ -23,7 +23,7 @@
 
 struct binaryscale {
 	const char *suffix;
-	uintmax_t b;
+	uint64_t b;
 };
 
 extern const struct binaryscale bin_default;

include/constants.h
@@ -41,12 +41,12 @@ enum {
 };
 
 enum binary {
-	binary_per_kilo = 1024UL,
-	binary_per_mega = 1024UL * binary_per_kilo,
-	binary_per_giga = 1024UL * binary_per_mega,
-	binary_per_tera = 1024UL * binary_per_giga,
-	binary_per_peta = 1024UL * binary_per_tera,
-	binary_per_exa  = 1024UL * binary_per_peta, /* 2^64 s 16 Exa */
+	binary_per_kilo = UINT64_C(1024),
+	binary_per_mega = UINT64_C(1024) * binary_per_kilo,
+	binary_per_giga = UINT64_C(1024) * binary_per_mega,
+	binary_per_tera = UINT64_C(1024) * binary_per_giga,
+	binary_per_peta = UINT64_C(1024) * binary_per_tera,
+	binary_per_exa  = UINT64_C(1024) * binary_per_peta, /* 2^64 s 16 Exa */
 };
 
 /*

lib/libipsecconf/parser.y
@@ -340,7 +340,7 @@ statement_kw:
 	| BINARYWORD EQUAL STRING {
 		struct keyword *kw = &$1;
 		const char *const str = $3;
-		unsigned long b;
+		uint64_t b;
 
 		diag_t diag = ttobinary(str, &b, 0 /* no B prefix */);
 		if (diag != NULL) {
@@ -352,13 +352,13 @@ statement_kw:
 	}
 	| BYTEWORD EQUAL INTEGER {
 		struct keyword *kw = &$1;
-		unsigned long b = $3;
+		uint64_t b = $3;
 		new_parser_kw(kw, NULL, b);
 	}
 	| BYTEWORD EQUAL STRING {
 		struct keyword *kw = &$1;
 		const char *const str = $3;
-		unsigned long b;
+		uint64_t b;
 
 		diag_t diag = ttobinary(str, &b, 1 /* with B prefix */);
 		if (diag != NULL) {

programs/pluto/kernel.c
@@ -1956,7 +1956,7 @@ static bool setup_half_ipsec_sa(struct state *st, bool inbound)
 						   c->sa_ipsec_max_packets,
 						   margin_packets,
 						   c->sa_rekey_fuzz);
-		dbg("%s %d #%lu ipsec-max-bytes %lu/%lu ipsec-max-packets %lu/%lu margin bytes %lu margin mackets %lu IPSEC_SA_MAX_SOFT_LIMIT_PERCENTAGE %u", __func__, __LINE__,  st->st_serialno,  sa_ipsec_soft_bytes, c->sa_ipsec_max_bytes, sa_ipsec_soft_packets, c->sa_ipsec_max_packets, margin_bytes, margin_packets, IPSEC_SA_MAX_SOFT_LIMIT_PERCENTAGE);
+		dbg("%s %d #%lu ipsec-max-bytes %"PRIu64"/%"PRIu64" ipsec-max-packets %"PRIu64"/%"PRIu64" margin bytes %"PRIu64" margin mackets %"PRIu64" IPSEC_SA_MAX_SOFT_LIMIT_PERCENTAGE %u", __func__, __LINE__,  st->st_serialno,  sa_ipsec_soft_bytes, c->sa_ipsec_max_bytes, sa_ipsec_soft_packets, c->sa_ipsec_max_packets, margin_bytes, margin_packets, IPSEC_SA_MAX_SOFT_LIMIT_PERCENTAGE);
 	}
 	const struct kernel_sa said_boilerplate = {
 		.src.address = &kernel_policy.src.host,