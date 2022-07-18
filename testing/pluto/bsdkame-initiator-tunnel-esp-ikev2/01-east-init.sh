ded5b71f25bcb656a1bb868357310d653fcf4351
Andrew Cagney <cagney@gnu.org>
2022-07-18 16:23:52 -0400

    testing: drop bsdkame-initiator-tunnel-esp-ikev2
    covered by PF KEY v2 tests

--- testing/pluto/bsdkame-initiator-tunnel-esp-ikev2/01-east-init.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add eastnet-westnet-ikev2
-ipsec whack --impair suppress-retransmits
-echo "initdone"
--- testing/pluto/bsdkame-initiator-tunnel-esp-ikev2/03-netbsdw-run.sh
+++
@@ -1,5 +0,0 @@
-../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
-ipsec auto --up eastnet-westnet-ikev2
-../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-setkey -D
-setkey -DP
