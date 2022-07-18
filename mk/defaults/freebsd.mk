7038322df56fa15c21064089ad8d5986c68fa01f
Andrew Cagney <cagney@gnu.org>
2022-07-18 11:43:12 -0400

    building: cite upstream LLVM bug

--- mk/defaults/freebsd.mk
+++ mk/defaults/freebsd.mk
@@ -2,6 +2,7 @@ BSD_VARIANT=freebsd
 # sketch out pkgsrc
 PKG_BASE ?= /usr/local
 
+# See: https://github.com/llvm/llvm-project/issues/55963
 # See: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=264288
 # See: https://github.com/libreswan/libreswan/issues/735
 CC=gcc
