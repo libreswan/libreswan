862d4a1ef43fd6dfea4c19f8a4e1cb1ebc3b1d56
Andrew Cagney <cagney@gnu.org>
2022-07-28 12:44:10 -0400

    testing: update openbsd tests - now with bash

--- testing/pluto/interop-ikev2-openbsd-01-iked-responder/01-openbsde-init.sh
+++ testing/pluto/interop-ikev2-openbsd-01-iked-responder/01-openbsde-init.sh
@@ -1,8 +1,4 @@
-set +o emacs
-set +o vi
-set +o gmacs
 # note swan-prep does not yet support iked
-#/testing/guestbin/swan-prep
 cp openbsde.conf /etc/iked.conf
 chmod 600 /etc/iked.conf
 /sbin/iked -dvvv > /tmp/iked.log 2>&1 &
--- testing/pluto/interop-ikev2-openbsd-02-iked-initiator/02-openbsdw-init.sh
+++ testing/pluto/interop-ikev2-openbsd-02-iked-initiator/02-openbsdw-init.sh
@@ -1,7 +1,4 @@
-set +o emacs
-set +o vi
-set +o gmacs
-# note swan-prep does not yet support iked 
+# note swan-prep does not yet support iked
 cp openbsdw.conf /etc/iked.conf
 chmod 600 /etc/iked.conf
 /sbin/iked -dvvv > /tmp/iked.log 2>&1 &
