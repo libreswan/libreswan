libreswan/libreswan

commit 94bdee3301da2b28c72e6d12ca6f787abcd6f87d

crypto: pass enum ipseckey_algorithm_type into unpack_dnssec_pubkey()
Let unpack_dnssec_pubkey() handle mapping to pubkey_type.

lib/libswan/secrets.c
@@ -1147,7 +1147,7 @@ static struct pubkey *alloc_pubkey(const struct id *id, /* ASKK */
 
 err_t unpack_dnssec_pubkey(const struct id *id, /* ASKK */
 			   enum dns_auth_level dns_auth_level,
-			   const struct pubkey_type *type,
+			   enum ipseckey_algorithm_type algorithm_type,
 			   realtime_t install_time, realtime_t until_time,
 			   uint32_t ttl,
 			   const chunk_t dnssec_pubkey,
@@ -1160,21 +1160,32 @@ err_t unpack_dnssec_pubkey(const struct id *id, /* ASKK */
 	ckaid_t ckaid;
 	size_t size;
 
-	if (type == NULL) {
-		diag_t d = pubkey_der_to_pubkey_content(HUNK_AS_SHUNK(dnssec_pubkey),
-							&scratch_pkc,
+	const struct pubkey_type *type = NULL;
+	if (algorithm_type == IPSECKEY_ALGORITHM_X_PUBKEY) {
+		diag_t d = pubkey_der_to_pubkey_content(HUNK_AS_SHUNK(dnssec_pubkey), &scratch_pkc,
 							&keyid, &ckaid, &size, &type);
 		if (d != NULL) {
 			pfree_diag(&d);
 			return "invalid pubkey";
 		}
-	} else {
-		err_t err = type->ipseckey_rdata_to_pubkey_content(dnssec_pubkey,
-								   &scratch_pkc,
-								   &keyid, &ckaid, &size);
-		if (err != NULL) {
-			return err;
-		}
+	}
+
+	switch (algorithm_type) {
+	case IPSECKEY_ALGORITHM_RSA:
+		type = &pubkey_type_rsa;
+		break;
+	case IPSECKEY_ALGORITHM_ECDSA:
+		type = &pubkey_type_ecdsa;
+		break;
+	default:
+		return "invalid IPSECKEY Algorithm Type";
+	}
+
+	err_t e = type->ipseckey_rdata_to_pubkey_content(dnssec_pubkey,
+							  &scratch_pkc,
+							  &keyid, &ckaid, &size);
+	if (e != NULL) {
+		return e;
 	}
 
 	struct pubkey *pubkey = alloc_pubkey(id, dns_auth_level, type,

include/secrets.h
@@ -238,14 +238,16 @@ extern struct pubkey_list *pubkeys;	/* keys from ipsec.conf */
 
 extern struct pubkey_list *free_public_keyentry(struct pubkey_list *p);
 extern void free_public_keys(struct pubkey_list **keys);
+
 err_t unpack_dnssec_pubkey(const struct id *id, /* ASKK */
 			   enum dns_auth_level dns_auth_level,
-			   const struct pubkey_type *type,
+			   enum ipseckey_algorithm_type algorithm_type,
 			   realtime_t install_time, realtime_t until_time,
 			   uint32_t ttl,
 			   chunk_t dnssec_pubkey,
 			   struct pubkey **pubkey,
 			   struct pubkey_list **head);
+
 void replace_public_key(struct pubkey_list **pubkey_db,
 			struct pubkey **pk);
 void delete_public_keys(struct pubkey_list **head,

programs/pluto/ikev2_ipseckey.c
@@ -109,7 +109,7 @@ static void add_dns_pubkeys_to_pluto(struct p_dns_req *dnsr, struct dns_pubkey *
 
 		chunk_t keyval = chunk2(dns_pubkey->ptr, dns_pubkey->len);
 		err_t ugh = unpack_dnssec_pubkey(keyid, /*dns_auth_level*/al,
-						 &pubkey_type_rsa,
+						 IPSECKEY_ALGORITHM_RSA,
 						 install_time, realtimesum(install_time, deltatime(ttl_used)),
 						 ttl, keyval, NULL/*don't-return-pubkey*/, &pluto_pubkeys);
 		if (ugh != NULL) {

programs/pluto/ikev2_ipseckey_dnsr.c
@@ -35,9 +35,6 @@
 #include "ikev2_ipseckey.h" /* for dns_status */
 #include "ikev2_ipseckey_dnsr.h"
 
-/* Do not include secrets.h as it will cause conflicts via NSS/OPENSSL headers */
-extern const struct pubkey_type pubkey_type_rsa;
-
 struct p_dns_req *pluto_dns_list = NULL; /* DNS queries linked list */
 
 void free_ipseckey_dns(struct p_dns_req *d)
@@ -195,7 +192,7 @@ static bool extract_dns_pubkey(struct p_dns_req *dnsr, ldns_rdf *rdf, uint32_t t
 		size_t len = strlen(pubkey);
 		struct dns_pubkey *dns_pubkey = alloc_bytes(sizeof(struct dns_pubkey) + len,
 							    "temp pubkey bin store");
-		dns_pubkey->type = &pubkey_type_rsa;
+		dns_pubkey->algorithm_type = IPSECKEY_ALGORITHM_RSA;
 		dns_pubkey->ttl = ttl;
 
 		char err_buf[TTODATAV_BUF];

programs/pluto/ikev2_ipseckey_dnsr.h
@@ -9,7 +9,7 @@ typedef void dnsr_cb_fn(struct p_dns_req *);
 
 struct dns_pubkey {
 	/* ID? */
-	const struct pubkey_type *type;
+	enum ipseckey_algorithm_type algorithm_type;
 	struct dns_pubkey *next;
 	uint32_t ttl;
 	/* chunk_t like */

programs/pluto/rcv_whack.c
@@ -332,7 +332,7 @@ static void key_add_request(const struct whack_message *msg, struct logger *logg
 
 		/* add the public key */
 		struct pubkey *pubkey = NULL; /* must-delref */
-		err_t ugh = unpack_dnssec_pubkey(&keyid, PUBKEY_LOCAL, type,
+		err_t ugh = unpack_dnssec_pubkey(&keyid, PUBKEY_LOCAL, msg->pubkey_alg,
 						 /*install_time*/realnow(),
 						 /*until_time*/realtime_epoch,
 						 /*ttl*/0,

commit ae78807c3bea20412f54469ec1910efb7d114d9a

crypto: fix decoding pubkey=
broken by 94bdee3301da2b28c72e6d12ca6f787abcd6f87d
crypto: pass enum ipseckey_algorithm_type into unpack_dnssec_pubkey()

lib/libswan/secrets.c
@@ -1160,35 +1160,38 @@ err_t unpack_dnssec_pubkey(const struct id *id, /* ASKK */
 	ckaid_t ckaid;
 	size_t size;
 
-	const struct pubkey_type *type = NULL;
+	const struct pubkey_type *pubkey_type = NULL; /* TBD */
+
 	if (algorithm_type == IPSECKEY_ALGORITHM_X_PUBKEY) {
 		diag_t d = pubkey_der_to_pubkey_content(dnssec_pubkey, &scratch_pkc,
-							&keyid, &ckaid, &size, &type);
+							&keyid, &ckaid, &size, &pubkey_type);
 		if (d != NULL) {
 			pfree_diag(&d);
 			return "invalid pubkey";
 		}
+		passert(pubkey_type != NULL);
+	} else {
+
+		switch (algorithm_type) {
+		case IPSECKEY_ALGORITHM_RSA:
+			pubkey_type = &pubkey_type_rsa;
+			break;
+		case IPSECKEY_ALGORITHM_ECDSA:
+			pubkey_type = &pubkey_type_ecdsa;
+			break;
+		default:
+			return "invalid IPSECKEY Algorithm Type";
+		}
+
+		err_t e = pubkey_type->ipseckey_rdata_to_pubkey_content(dnssec_pubkey,
+									&scratch_pkc,
+									&keyid, &ckaid, &size);
+		if (e != NULL) {
+			return e;
+		}
 	}
 
-	switch (algorithm_type) {
-	case IPSECKEY_ALGORITHM_RSA:
-		type = &pubkey_type_rsa;
-		break;
-	case IPSECKEY_ALGORITHM_ECDSA:
-		type = &pubkey_type_ecdsa;
-		break;
-	default:
-		return "invalid IPSECKEY Algorithm Type";
-	}
-
-	err_t e = type->ipseckey_rdata_to_pubkey_content(dnssec_pubkey,
-							 &scratch_pkc,
-							 &keyid, &ckaid, &size);
-	if (e != NULL) {
-		return e;
-	}
-
-	struct pubkey *pubkey = alloc_pubkey(id, dns_auth_level, type,
+	struct pubkey *pubkey = alloc_pubkey(id, dns_auth_level, pubkey_type,
 					     install_time, until_time, ttl,
 					     &scratch_pkc, &keyid, &ckaid,
 					     null_shunk,	/* raw keys have no issuer */