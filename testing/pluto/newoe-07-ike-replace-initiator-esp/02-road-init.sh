8eb02e16ee77800c04ea1c178de45fdb31ffc7a6
Andrew Cagney <cagney@gnu.org>
2022-08-13 20:57:30 -0400

    testing: robustify newoe-07-ike-replace-initiator-esp

--- testing/pluto/newoe-07-ike-replace-initiator-esp/02-road-init.sh
+++ testing/pluto/newoe-07-ike-replace-initiator-esp/02-road-init.sh
@@ -1,9 +1,11 @@
 /testing/guestbin/swan-prep
 cp policies/* /etc/ipsec.d/policies/
 ipsec start
+
 # ensure for tests acquires expire before our failureshunt=2m
 echo 30 > /proc/sys/net/core/xfrm_acq_expires
 ../../guestbin/wait-until-pluto-started
+
 # give OE policies time to load
 ../../guestbin/wait-for.sh --match 'loaded 11,' -- ipsec auto --status
 ip -s xfrm monitor > /tmp/xfrm-monitor.out &
---
+++ testing/pluto/newoe-07-ike-replace-initiator-esp/03-road-run.sh
@@ -0,0 +1,31 @@
+# trigger OE
+
+../../guestbin/ping-once.sh --forget -I 192.1.3.209 192.1.2.23
+../../guestbin/wait-for.sh --match private-or-clear -- ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
+ipsec whack --trafficstatus
+
+# Kill some time while waiting for a rekey.  With a lifetime of 60s,
+# margin of 10s, and fuzz of 100% it will happen somewhere between
+# 40s-50s so 60s is safe.
+#
+# To stop the re-key declaring the connection idle, send a ping.
+# Without this the re-key will see that there's been no traffic since
+# the above <<ipsec whack --trafficstatus>> which is well beyond
+# REKEY_MARGIN.
+#
+# (remember everytime <<ipsec whack --trafficstatus>> runs and sees
+# that traffic has followed it sets last-traffic time to "now").
+
+sleep 30
+../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
+sleep 30
+
+../../guestbin/wait-for.sh --match '#3' -- ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
+
+# Should show state #3 to indicate it has rekeyed
+ipsec whack --trafficstatus
+ipsec whack --shuntstatus
+killall ip > /dev/null 2> /dev/null
+echo done
