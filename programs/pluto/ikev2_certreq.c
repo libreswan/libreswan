42bbe43d990d9a8cdb3543a15a346df54fecdc4e
Andrew Cagney <cagney@gnu.org>
2022-08-19 16:57:18 -0400

    ikev2: emit one CERTREQ payload with all the hashes

--- programs/pluto/ikev2_certreq.c
+++ programs/pluto/ikev2_certreq.c
@@ -122,74 +122,61 @@ void process_v2CERTREQ_payload(struct ike_sa *ike, struct msg_digest *md)
 	}
 }
 
-#if 0
 /*
- * returns the concatenated SHA-1 hashes of each public key in the chain
+ * Compute and emit the hash for the given CA
  */
-static chunk_t ikev2_hash_ca_keys(x509cert_t *ca_chain)
-{
-	unsigned char combined_hash[SHA1_DIGEST_SIZE * 8 /*max path len*/];
-	x509cert_t *ca;
-	chunk_t result = EMPTY_CHUNK;
-	size_t sz = 0;
-
-	zero(&combined_hash);
-
-	for (ca = ca_chain; ca != NULL; ca = ca->next) {
-		unsigned char sighash[SHA1_DIGEST_SIZE];
-		SHA1_CTX ctx_sha1;
-
-		SHA1Init(&ctx_sha1);
-		SHA1Update(&ctx_sha1, ca->signature.ptr, ca->signature.len);
-		SHA1Final(sighash, &ctx_sha1);
-
-		if (DBGP(DBG_CRYPT)) {
-			DBG_dump("SHA-1 of CA signature",
-				 sighash, SHA1_DIGEST_SIZE);
-		}
-
-		memcpy(combined_hash + sz, sighash, SHA1_DIGEST_SIZE);
-		sz += SHA1_DIGEST_SIZE;
-	}
-	passert(sz <= sizeof(combined_hash));
-	result = clone_bytes_as_chunk(combined_hash, sz, "combined CERTREQ hash");
-	if (DBGP(DBG_CRYPT)) {
-		DBG_dump_hunk("Combined CERTREQ hashes", result);
-	}
-	return result;
-}
-#endif
-
-/*
- * Instead of ikev2_hash_ca_keys use this for now. A single key
- * hash.
- */
-static chunk_t ikev2_hash_nss_cert_key(CERTCertificate *cert,
-				       struct logger *logger)
+static bool emit_v2CERTREQ_ca_hash(struct pbs_out *cr_pbs,
+				   CERTCertificate *cert)
 {
 	unsigned char sighash[SHA1_DIGEST_SIZE];
 	zero(&sighash);
 
 	/*
-	 * TODO: This should use SHA1 even if USE_SHA1 is disabled for
-	 * IKE/IPsec.
+	 * XXX: RFC requires that SHA-1 is used, regardless of the
+	 * system's crypto policy.  Since crypt_hash*() uses the
+	 * low-leve PK11*() interface it should be ok.
 	 */
 	struct crypt_hash *ctx = crypt_hash_init("SHA-1 of Certificate Public Key",
-						 &ike_alg_hash_sha1, logger);
+						 &ike_alg_hash_sha1,
+						 cr_pbs->outs_logger);
 	crypt_hash_digest_bytes(ctx, "pubkey",
 				cert->derPublicKey.data,
 				cert->derPublicKey.len);
 	crypt_hash_final_bytes(&ctx, sighash, sizeof(sighash));
-	chunk_t result = clone_bytes_as_chunk(sighash, SHA1_DIGEST_SIZE, "pkey hash");
+	pexpect(ctx == NULL);
 
-	return result;
+
+	diag_t d = pbs_out_raw(cr_pbs, sighash, sizeof(sighash), "Certification Authority hash");
+	if (d != NULL) {
+		llog_diag(RC_LOG_SERIOUS, cr_pbs->outs_logger, &d, "%s", "");
+		return false;
+	}
+
+	return true;
 }
 
-static bool build_and_emit_v2CERTREQ(enum ike_cert_type type,
-				     chunk_t ca, struct pbs_out *outs)
+static bool emit_v2CERTREQ_header(struct pbs_out *outs, enum ike_cert_type type,
+				  struct pbs_out *cr_pbs)
+{
+	struct ikev2_certreq cr_hd = {
+		.isacertreq_critical =  ISAKMP_PAYLOAD_NONCRITICAL,
+		.isacertreq_enc = type,
+	};
+
+	/* build CR header */
+	if (!out_struct(&cr_hd, &ikev2_certificate_req_desc, outs, cr_pbs)) {
+		return false;
+	}
+
+	return true;
+}
+
+static bool emit_v2CERTREQ_ca(struct pbs_out *cr_pbs, chunk_t ca)
 {
 	/*
-	 * CERT_GetDefaultCertDB() simply returns the contents of a
+	 * This call is fast and cheap.
+	 *
+	 * CERT_GetDefaultCertDB() just returns the contents of a
 	 * static variable set by NSS_Initialize().  It doesn't check
 	 * the value and doesn't set PR error.  Short of calling
 	 * CERT_SetDefaultCertDB(NULL), the value can never be NULL.
@@ -197,77 +184,65 @@ static bool build_and_emit_v2CERTREQ(enum ike_cert_type type,
 	CERTCertDBHandle *handle = CERT_GetDefaultCertDB();
 	passert(handle != NULL);
 
-	pb_stream cr_pbs;
-	struct ikev2_certreq cr_hd = {
-		.isacertreq_critical =  ISAKMP_PAYLOAD_NONCRITICAL,
-		.isacertreq_enc = type,
-	};
-
-	/* build CR header */
-	if (!out_struct(&cr_hd, &ikev2_certificate_req_desc, outs, &cr_pbs))
-		return false;
 	/*
-	 * The Certificate Encoding field has the same values as those defined
-	 * in Section 3.6.  The Certification Authority field contains an
-	 * indicator of trusted authorities for this certificate type.  The
-	 * Certification Authority value is a concatenated list of SHA-1 hashes
-	 * of the public keys of trusted Certification Authorities (CAs).  Each
-	 * is encoded as the SHA-1 hash of the Subject Public Key Info element
-	 * (see section 4.1.2.7 of [PKIX]) from each Trust Anchor certificate.
-	 * The 20-octet hashes are concatenated and included with no other
-	 * formatting.
-	 *
-	 * How are multiple trusted CAs chosen?
+	 * The Certificate Encoding field has the same values as those
+	 * defined in Section 3.6.  The Certification Authority field
+	 * contains an indicator of trusted authorities for this
+	 * certificate type.  The Certification Authority value is a
+	 * concatenated list of SHA-1 hashes of the public keys of
+	 * trusted Certification Authorities (CAs).  Each is encoded
+	 * as the SHA-1 hash of the Subject Public Key Info element
+	 * (see section 4.1.2.7 of [PKIX]) from each Trust Anchor
+	 * certificate.  The 20-octet hashes are concatenated and
+	 * included with no other formatting.
 	 */
 
-	if (ca.ptr != NULL) {
-		SECItem caname = same_shunk_as_dercert_secitem(ASN1(ca));
+	SECItem caname = same_shunk_as_dercert_secitem(ASN1(ca));
+	CERTCertificate *cacert = CERT_FindCertByName(handle, &caname); /* must unref */
 
-		CERTCertificate *cacert =
-			CERT_FindCertByName(handle, &caname);
-
-		if (cacert != NULL && CERT_IsCACert(cacert, NULL)) {
-			dbg("located CA cert %s for CERTREQ", cacert->subjectName);
-			/*
-			 * build CR body containing the concatenated SHA-1 hashes of the
-			 * CA's public key. This function currently only uses a single CA
-			 * and should support more in the future
-			 * */
-			chunk_t cr_full_hash = ikev2_hash_nss_cert_key(cacert,
-								       outs->outs_logger);
-
-			if (!out_hunk(cr_full_hash, &cr_pbs, "CA cert public key hash")) {
-				free_chunk_content(&cr_full_hash);
-				return false;
-			}
-			free_chunk_content(&cr_full_hash);
-		} else {
-			LSWDBGP(DBG_BASE, buf) {
-				jam(buf, "NSS: locating CA cert \'");
-				jam_dn(buf, ASN1(ca), jam_sanitized_bytes);
-				jam(buf, "\' for CERTREQ using CERT_FindCertByName() failed: ");
-				jam_nss_error_code(buf, PR_GetError());
-			}
+	if (cacert == NULL) {
+		LSWDBGP(DBG_BASE, buf) {
+			jam(buf, "NSS: locating CA cert \'");
+			jam_dn(buf, ASN1(ca), jam_sanitized_bytes);
+			jam(buf, "\' for CERTREQ using CERT_FindCertByName() failed: ");
+			jam_nss_error_code(buf, PR_GetError());
 		}
+		return true;
 	}
-	/*
-	 * can it be empty?
-	 * this function's returns need fixing
-	 * */
-	close_output_pbs(&cr_pbs);
+
+	if (!CERT_IsCACert(cacert, NULL)) {
+		dbg("located CA cert %s for CERTREQ is not a CA", cacert->subjectName);
+		CERT_DestroyCertificate(cacert);
+		return true;
+	}
+
+	if (!emit_v2CERTREQ_ca_hash(cr_pbs, cacert)) {
+		CERT_DestroyCertificate(cacert);
+		return false;
+	}
+
 	return true;
 }
 
-stf_status emit_v2CERTREQ(struct ike_sa *ike, struct msg_digest *md,
+stf_status emit_v2CERTREQ(const struct ike_sa *ike,
+			  struct msg_digest *md,
 			  struct pbs_out *outpbs)
 {
-	if (ike->sa.st_connection->kind == CK_PERMANENT) {
-		dbg("connection->kind is CK_PERMANENT so send CERTREQ");
+	struct pbs_out cr_pbs;
+	if (!emit_v2CERTREQ_header(outpbs, CERT_X509_SIGNATURE, &cr_pbs)) {
+		/* already logged */
+		return STF_INTERNAL_ERROR; /*fatal*/
+	}
 
-		if (!build_and_emit_v2CERTREQ(CERT_X509_SIGNATURE,
-					      ike->sa.st_connection->remote->config->host.ca,
-					      outpbs))
-			return STF_INTERNAL_ERROR;
+	if (ike->sa.st_connection->kind == CK_PERMANENT) {
+		dbg("connection->kind is CK_PERMANENT so send (possibly empty) CERTREQ");
+		chunk_t ca = ike->sa.st_connection->remote->config->host.ca;
+		if (ca.len > 0) {
+			if (!emit_v2CERTREQ_ca(&cr_pbs, ca)) {
+				/* already logged */
+				return STF_INTERNAL_ERROR; /*fatal*/
+			}
+		}
 	} else {
 		dbg("connection->kind is not CK_PERMANENT (instance), so collect CAs");
 
@@ -277,21 +252,18 @@ stf_status emit_v2CERTREQ(struct ike_sa *ike, struct msg_digest *md,
 			dbg("connection is RW, lookup CA candidates");
 
 			for (generalName_t *ca = gn; ca != NULL; ca = ca->next) {
-				if (!build_and_emit_v2CERTREQ(CERT_X509_SIGNATURE,
-							      ca->name, outpbs)) {
+				if (!emit_v2CERTREQ_ca(&cr_pbs, ca->name)) {
 					free_generalNames(gn, false);
-					return STF_INTERNAL_ERROR;
+					return STF_INTERNAL_ERROR; /*fatal*/
 				}
 			}
 			free_generalNames(gn, false);
 		} else {
 			dbg("not a roadwarrior instance, sending empty CA in CERTREQ");
-			if (!build_and_emit_v2CERTREQ(CERT_X509_SIGNATURE,
-						       EMPTY_CHUNK, outpbs)) {
-				return STF_INTERNAL_ERROR;
-			}
 		}
 	}
+
+	close_output_pbs(&cr_pbs);
 	return STF_OK;
 }
 
