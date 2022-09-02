d9388feb0a257ee199d5c772384d5750c9bbb9d5
Andrew Cagney <cagney@gnu.org>
2022-09-02 17:11:26 -0400

    testing: try to make ikev2-systemrole-02-net-to-net more stable

--- testing/pluto/ikev2-systemrole-02-net-to-net/03-nic-push-systemroles.sh
+++ testing/pluto/ikev2-systemrole-02-net-to-net/03-nic-push-systemroles.sh
@@ -1,3 +1,4 @@
 # nic is used as the System Role provisioning host
 /testing/guestbin/nic-vpn-role-tmp
-ansible-playbook --skip-tags packages -i ansible-inventory.yml test-east-west.yml 2>&1 | tee OUTPUT/ansible.out | grep -v DEPRECATED | tail -3 | sort
+ansible-playbook --skip-tags packages -i ansible-inventory.yml test-east-west.yml > OUTPUT/ansible.out 2>&1
+grep -e '^ok:.*>' OUTPUT/ansible.out
