02b7c919346a3d639e17f207a9a33acb69b245bd
Andrew Cagney <cagney@gnu.org>
2022-09-06 10:29:48 -0400

    testing: sprinkle suppress-retransmits over ikev2-43-init-retransmit
    Jacob Two-Two not Jacob Three-Two

--- testing/pluto/ikev2-43-init-retransmit/03-west-up.sh
+++ testing/pluto/ikev2-43-init-retransmit/03-west-up.sh
@@ -1,5 +1,9 @@
+# say everything twice; but not using a retransmit
+ipsec whack --impair suppress-retransmits
 ipsec whack --impair jacob-two-two
+
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
+
 echo done
--- testing/pluto/ikev2-43-init-retransmit/04-east-got-two-two.sh
+++ testing/pluto/ikev2-43-init-retransmit/04-east-got-two-two.sh
@@ -1,2 +1,1 @@
-../../guestbin/ipsec-look.sh
 hostname | grep east > /dev/null && grep ikev2-responder-retransmit /tmp/pluto.log
