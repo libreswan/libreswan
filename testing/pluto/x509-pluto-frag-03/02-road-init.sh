4b0f1c7bcdf8ef28f654cc75657cd3bd6bac5124
Andrew Cagney <cagney@gnu.org>
2022-09-14 10:14:48 -0400

    testing: sprinkle impair revival

--- testing/pluto/x509-pluto-frag-03/02-road-init.sh
+++ testing/pluto/x509-pluto-frag-03/02-road-init.sh
@@ -2,5 +2,6 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 iptables -I INPUT -p udp -m length --length 0x5dc:0xffff -j DROP
+ipsec whack --impair revival
 ipsec auto --add x509
 echo done
