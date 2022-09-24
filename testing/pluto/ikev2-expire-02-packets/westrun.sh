91476f36ceecd126ff2a07eeeacbab020ad800c0
Andrew Cagney <cagney@gnu.org>
2022-09-24 12:13:45 -0400

    testing: in ikev2-expire-02-packets get soft-limit from debug log
    should be from showstate, later

--- testing/pluto/ikev2-expire-02-packets/westrun.sh
+++ testing/pluto/ikev2-expire-02-packets/westrun.sh
@@ -1,15 +1,22 @@
 ipsec auto --up west
-# pings will not trigger rekey
-ping -n -q -c 16 -I 192.0.1.254 192.0.2.254
-# expect #2 IPsec original Child SA
+
+# find out the actual number of packets
+actual=$(sed -n -e 's/.* ipsec-max-packets.* actual-limit=\([0-9]\).*/\1/ p' /tmp/pluto.log | head -1)
+echo $actual
+
+# $actual-1 pings will not trigger rekey; expect #2 to remain
+ping -n -q -c $((actual - 1)) -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus
-# next pings will go over and initiate a rekey
-ping -n -q -c 8 -I 192.0.1.254 192.0.2.254
+
+# next ping will go over and initiate a rekey; and then further pings
+# go to #3
+ping -n -q -c ${actual} -I 192.0.1.254 192.0.2.254
 sleep 5
-# expect only #3 IPsec first rekeyed Child SA
 ipsec trafficstatus
-ping -n -q -c 10 -I 192.0.1.254 192.0.2.254
+
+# and again
+ping -n -q -c ${actual} -I 192.0.1.254 192.0.2.254
 sleep 5
-# expect only #4 IPsec second rekeyed Child SA
 ipsec trafficstatus
+
 echo done
