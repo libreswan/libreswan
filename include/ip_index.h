ea81cad5507a31a461e00dd824e3221d471f7fc6
Andrew Cagney <cagney@gnu.org>
2022-10-19 14:40:29 -0400

    ip: really add ip_index.h
    Fix: ip: add enum ip_index { 1, 2, }

---
+++ include/ip_index.h
@@ -0,0 +1,28 @@
+/* ip address type index, for libreswan
+ *
+ * Copyright (C) 2022 Andrew Cagney
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <https://www.gnu.org/licenses/lgpl-2.1.txt>.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ * License for more details.
+ *
+ */
+
+#ifndef IP_INDEX_H
+#define IP_INDEX_H
+
+enum ip_index {
+	IPv4_INDEX = 1,
+	IPv6_INDEX = 2,
+};
+
+#define IP_INDEX_FLOOR 1
+#define IP_INDEX_ROOF 3
+
+#endif
