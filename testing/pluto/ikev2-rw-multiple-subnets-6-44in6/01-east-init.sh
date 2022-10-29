3c90599ed8117181b0f56a5e14c9cb7329bbf017
Andrew Cagney <cagney@gnu.org>
2022-10-29 11:55:04 -0400

    testing: more subnets combinations

--- testing/pluto/ikev2-rw-multiple-subnets-6-44in6/01-east-init.sh
+++ testing/pluto/ikev2-rw-multiple-subnets-6-44in6/01-east-init.sh
@@ -1,5 +1,6 @@
 /testing/guestbin/swan-prep --46 --x509
 ip addr add 192.0.20.254/24 dev eth0
+ip addr add 2001:db8:0:20::256/64 dev eth0
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add east
--- testing/pluto/ikev2-rw-multiple-subnets-6-44in6/03-road-up.sh
+++ testing/pluto/ikev2-rw-multiple-subnets-6-44in6/03-road-up.sh
@@ -1,4 +1,1 @@
 ipsec auto --up road
-../../guestbin/ping-once.sh --up 192.0.2.254
-../../guestbin/ping-once.sh --up 192.0.20.254
-echo done
