24f9fa6d66749d6387949415b9e4033ad60e35d8
Andrew Cagney <cagney@gnu.org>
2022-11-08 11:28:42 -0500

    testing: add ikev2-subnets-{44,46,66}in{4,6}-rw-{alias,ike}
    Most are WIP but all really should work.
    -alias uses subnets=...
    -ike uses subnet=...
    Replace kev2-rw-multiple-subnets-6-*.

---
+++ testing/pluto/ikev2-subnets-66in4-rw-alias/01-east-init.sh
@@ -0,0 +1,10 @@
+/testing/guestbin/swan-prep --46 --x509
+
+ip addr add 192.0.20.254/24 dev eth0
+ip addr add 2001:db8:0:20::256/64 dev eth0
+ip addr show eth0 | grep global | sort
+
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec auto --add east
+echo "initdone"
---
+++ testing/pluto/ikev2-subnets-66in4-rw-ike/final.sh
@@ -0,0 +1,4 @@
+# non-zero counts confirm encrypted traffic flowing
+ipsec trafficstatus
+# sane values confirm things like src correct
+../../guestbin/ipsec-look.sh
