066f54ff449e959b71d8945f3afe8ab2edc50ba1
Andrew Cagney <cagney@gnu.org>
2022-11-29 13:38:00 -0500

    openbsd: build using egcc (gcc), not llvm
    Assume https://github.com/llvm/llvm-project/issues/55963

--- mk/defaults/openbsd.mk
+++ mk/defaults/openbsd.mk
@@ -5,7 +5,8 @@ PKG_BASE ?= /usr/local
 # See: https://github.com/llvm/llvm-project/issues/55963
 # See: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=264288
 # See: https://github.com/libreswan/libreswan/issues/735
-#CC=gcc
+# OpenBSD seems to call GCC egcc
+CC=egcc
 
 WARNING_CFLAGS += -Wno-unused
 USERLAND_CFLAGS += -DUSE_SOCKADDR_LEN
