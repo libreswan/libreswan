ffb2b19f3073a2b86ca7dcaec43231d099ff34bf
Andrew Cagney <cagney@gnu.org>
2022-12-16 10:06:00 -0500

    testing: update IPv4 vs IPv6 tests

--- testing/pluto/libipsecconf-13-addresspool/west.sh
+++ testing/pluto/libipsecconf-13-addresspool/west.sh
@@ -2,7 +2,27 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 
-ipsec auto --add 4Xin4
-ipsec auto --add 4Xin6
-ipsec auto --add 6Xin4
-ipsec auto --add 6Xin6
+ipsec auto --add comma-4
+ipsec auto --add comma-6
+ipsec auto --add comma-4-comma
+ipsec auto --add comma-6-comma
+ipsec auto --add 4-comma
+ipsec auto --add 6-comma
+
+ipsec auto --add 4toXin4
+ipsec auto --add 4toXin6
+ipsec auto --add 6toXin4
+ipsec auto --add 6toXin6
+
+ipsec auto --add 4to4in4
+ipsec auto --add 4to6in6
+ipsec auto --add 6to4in4
+ipsec auto --add 6to6in6
+
+ipsec auto --add 46to4in4
+ipsec auto --add 64to4in4
+ipsec auto --add 46to6in6
+ipsec auto --add 64to6in6
+
+ipsec auto --add 46to64in4
+ipsec auto --add 64to46in6
