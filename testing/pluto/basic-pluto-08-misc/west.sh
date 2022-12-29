beeb833008455c3524f11f2790810c730a306a26
Andrew Cagney <cagney@gnu.org>
2022-12-29 18:10:59 -0500

    testing: use chown bin:bin
    not bin.bin, f37 grr

--- testing/pluto/basic-pluto-08-misc/west.sh
+++ testing/pluto/basic-pluto-08-misc/west.sh
@@ -12,7 +12,7 @@ rm -rf /var/cache/otheruser
 mkdir -p /var/cache/otheruser/var/run/pluto /var/cache/otheruser/etc
 cp /etc/ipsec.d/*.* /var/cache/otheruser/etc/
 mv /etc/ipsec.conf /var/cache/otheruser/etc/
-chown -R bin.bin /var/cache/otheruser/var/run/pluto /var/cache/otheruser/etc
+chown -R bin:bin /var/cache/otheruser/var/run/pluto /var/cache/otheruser/etc
 chmod -R 755 /var/cache/otheruser
 ipsec pluto --rundir /var/cache/otheruser/var/run/pluto --secretsfile /var/cache/otheruser/etc/ipsec.secrets
 # give pluto time to start and create its socket and pid files
