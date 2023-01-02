7188c22515946c64d794887019f09be5fb7d984e
Andrew Cagney <cagney@gnu.org>
2023-01-01 21:00:25 -0500

    testing: more ikev2-redirect-06-roadwarriors robustification

--- testing/pluto/ikev2-redirect-06-roadwarriors/99-east-west-road-north.sh
+++ testing/pluto/ikev2-redirect-06-roadwarriors/99-east-west-road-north.sh
@@ -1,4 +1,4 @@
-ipsec whack --trafficstatus | sed -e 's/#[0-9]/#N/' -e 's/192.0.2.10[1-2]/192.0.2.10x/' -e 's/192.1.3.[0-9]*/192.1.3.x/' | sort
+ipsec whack --trafficstatus | sed -e 's/#[0-9]/#N/' -e 's/\[[0-9]\]/[N]/' -e 's/192.0.2.10[0-9]/192.0.2.10x/' | sort
 : ==== cut ====
 ipsec auto --status
 ip xfrm state
