84fc696a120e2512eb9ab40c9cd80ca45c90427d
Paul Wouters <paul.wouters@aiven.io>
2023-01-08 16:03:58 -0500

    testing: fixup ikev2-rw-multiple-subnets-2
    I see no aliens on kvm or namespace runs

--- testing/pluto/ikev2-rw-multiple-subnets-2/03-road-run.sh
+++ testing/pluto/ikev2-rw-multiple-subnets-2/03-road-run.sh
@@ -2,4 +2,6 @@
 ../../guestbin/wait-for.sh --match road/0x2 -- ipsec whack --trafficstatus
 ../../guestbin/ping-once.sh --up 192.0.2.254
 ../../guestbin/ping-once.sh --up 192.0.20.254
+sleep 1
+ipsec trafficstatus
 echo done
