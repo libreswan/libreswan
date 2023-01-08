09d37eb4275483a43f37206433d4ab6c79f8aca0
Paul Wouters <paul.wouters@aiven.io>
2023-01-08 16:47:08 -0500

    testing: added ikev2-selectors-44in4-rw-{alias|ike}-mismatch-02
    Note that ikev2-selectors-44in4-rw-ike-mismatch-02 works properly.

---
+++ testing/pluto/ikev2-selectors-44in4-rw-alias-mismatch-02/06-east-look.sh
@@ -0,0 +1,2 @@
+# This should show a %hold shunt for 192.0.30.0/24
+../../guestbin/ipsec-look.sh
---
+++ testing/pluto/ikev2-selectors-44in4-rw-ike-mismatch-02/04-road-look.sh
@@ -0,0 +1,3 @@
+# There should be a %hold shunt for 192.0.30.0/24 which MUST NOT
+# appear as TS on the tunnel.
+../../guestbin/ipsec-look.sh
