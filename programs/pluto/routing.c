a20998e631cfd71df01d69dcf699744680baac52
Andrew Cagney <cagney@gnu.org>
2023-01-21 14:56:41 -0500

    routing: simplify connection_timeout()

--- programs/pluto/routing.c
+++ programs/pluto/routing.c
@@ -38,76 +38,57 @@ enum connection_action connection_timeout(struct connection *c,
 		return CONNECTION_RETRY;
 
 	case RT_UNROUTED_NEGOTIATION:
+		if (try_limit > 0 && tries_so_far >= try_limit) {
+			ldbg(logger, "maximum number of establish retries reached - abandoning");
+			if (c->policy & POLICY_OPPORTUNISTIC) {
+				/*
+				 * A failed OE initiator, make shunt
+				 * bare.
+				 *
+				 * Checking .kind above seems pretty
+				 * dodgy.  Suspect it is trying to
+				 * capture the initial IKE exchange
+				 * when the child hasn't yet been
+				 * created, except that when kind is
+				 * STATE_V2_PARENT_I2 the larval Child
+				 * SA has been created?!?
+				 */
+				orphan_holdpass(c, c->spd, logger);
+				/*
+				 * Change routing so we don't get
+				 * cleared out when state/connection
+				 * dies.
+				 */
+				set_child_routing(c, RT_UNROUTED);
+				dbg("kernel: orphan_holdpas() done - returning success");
+			}
+			return CONNECTION_FAIL;
+		}
+		return CONNECTION_RETRY;
+
 	case RT_ROUTED_NEGOTIATION:
 		if (try_limit > 0 && tries_so_far >= try_limit) {
 			ldbg(logger, "maximum number of establish retries reached - abandoning");
 			if (c->policy & POLICY_OPPORTUNISTIC) {
-
-				struct spd_route *sr = c->spd; /*XXX:only-one!?!*/
-				enum shunt_policy failure_shunt = c->config->failure_shunt;
-				enum shunt_policy nego_shunt = c->config->negotiation_shunt;
-
-				dbg("OE: delete_state orphaning hold with failureshunt %s (negotiation shunt would have been %s)",
-				    enum_name_short(&shunt_policy_names, failure_shunt),
-				    enum_name_short(&shunt_policy_names, nego_shunt));
-
-				enum routing ro = c->child.routing;        /* routing, old */
-				enum shunt_policy negotiation_shunt = c->config->negotiation_shunt;
-
-				if (negotiation_shunt != failure_shunt ) {
-					dbg("kernel: failureshunt != negotiationshunt, needs replacing");
-				} else {
-					dbg("kernel: failureshunt == negotiationshunt, no replace needed");
-				}
-
-				dbg("kernel: orphan_holdpass() called for %s with transport_proto '%d' and sport %d and dport %d",
-				    c->name, sr->local->client.ipproto, sr->local->client.hport, sr->remote->client.hport);
-
-				passert(LHAS(LELEM(CK_PERMANENT) |
-					     LELEM(CK_INSTANCE) |
-					     LELEM(CK_GOING_AWAY), c->kind));
-
-				enum routing rn = c->child.routing;        /* routing, new */
-				switch (ro) {
-				case RT_UNROUTED_NEGOTIATION:
-					rn = RT_UNROUTED;
-					dbg("kernel: orphan_holdpass unrouted: hold -> pass");
-					break;
-				case RT_UNROUTED:
-					rn = RT_UNROUTED_NEGOTIATION;
-					dbg("kernel: orphan_holdpass unrouted: pass -> hold");
-					break;
-				case RT_ROUTED_NEGOTIATION:
-					rn = RT_ROUTED_PROSPECTIVE;
-					dbg("kernel: orphan_holdpass routed: hold -> trap (?)");
-					break;
-				default:
-					dbg("kernel: no routing change needed for ro=%s - negotiation shunt matched failure shunt?",
-					    enum_name(&routing_story, ro));
-					break;
-				}
-
-				ldbg(logger,
-				     "kernel: orphaning holdpass for connection '%s', routing %s -> %s",
-				    c->name,
-				    enum_name(&routing_story, ro),
-				    enum_name(&routing_story, rn));
-
 				/*
-				 * A failed OE initiator, make shunt bare.
+				 * A failed OE initiator, make shunt
+				 * bare.
 				 *
-				 * Checking .kind above seems pretty dodgy.  Suspect
-				 * it is trying to capture the initial IKE exchange
-				 * when the child hasn't yet been created, except that
-				 * when kind is STATE_V2_PARENT_I2 the larval Child SA
-				 * has been created?!?
+				 * Checking .kind above seems pretty
+				 * dodgy.  Suspect it is trying to
+				 * capture the initial IKE exchange
+				 * when the child hasn't yet been
+				 * created, except that when kind is
+				 * STATE_V2_PARENT_I2 the larval Child
+				 * SA has been created?!?
 				 */
 				orphan_holdpass(c, c->spd, logger);
-				/* change routing so we don't get
+				/*
+				 * Change routing so we don't get
 				 * cleared out when state/connection
-				 * dies */
-				set_child_routing(c, rn);
-				dbg("kernel: orphan_holdpas() done - returning success");
+				 * dies.
+				 */
+				set_child_routing(c, RT_ROUTED_PROSPECTIVE);
 			}
 			return CONNECTION_FAIL;
 		}
