b4b6416064cad65169840a939c54854861557a0b
Andrew Cagney <cagney@gnu.org>
2023-02-08 12:06:41 -0500

    testing: groundhogday

--- testing/pluto/nss-cert-11-cert-expired-initiator-ikev2/01-east-init.sh
+++ testing/pluto/nss-cert-11-cert-expired-initiator-ikev2/01-east-init.sh
@@ -1,6 +1,12 @@
 /testing/guestbin/swan-prep --nokeys
 
-# Generate a sane CA and a sane peer certificate, back-dated by a few
+NOW=$(date +%s)
+THEN=$((${NOW} - 45 * 24 * 60 * 60))
+
+GROUNDHOGDAY=$(date -d @${THEN} +%Y-%m-%d)
+sed -i -e "s/groundhogday=.*/groundhogday=${GROUNDHOGDAY}/" /etc/ipsec.conf
+
+# Generate a sane CA and a sane peer certificate, back-dated by two
 # months.  Export these then wipe so that peer has to send the cert
 # back.
 
@@ -10,16 +16,16 @@ pk12util -W secret -o OUTPUT/new-west.p12   -n new-west -d /etc/ipsec.d
 certutil -L -n new-west -d /etc/ipsec.d -a > OUTPUT/new-west.crt
 certutil -F -n new-west -d /etc/ipsec.d
 
-# Now generate an expired certificate; these two overlap but a month
-# ago.
+# Now generate a CA that expired 1 month ago.  Use that to sign a
+# certificate (for west) that is valid.
 
-certutil -m 1 -S -k rsa -x -w -13 -n old-ca  -s "CN=old-ca"  -v 12 -t "CT,C,C" -d /etc/ipsec.d -z ipsec.conf
+certutil -m 1 -S -k rsa -x -w -13 -v 12 -n old-ca  -s "CN=old-ca"  -v 12 -t "CT,C,C" -d /etc/ipsec.d -z ipsec.conf
 certutil -m 2 -S -k rsa -c old-ca -w -11 -n old-west -s "CN=old-west" -v 12 -t "u,u,u"  -d /etc/ipsec.d -z ipsec.conf
 pk12util -W secret -o OUTPUT/old-west.p12   -n old-west -d /etc/ipsec.d
 certutil -L -n old-west -d /etc/ipsec.d -a > OUTPUT/old-west.crt
 certutil -F -n old-west -d /etc/ipsec.d
 
-# use vfychain
+# Use vfychain to confirm the above settings
 #
 # -p -p engages the new PKIX interface that pluto is using.
 #
@@ -27,7 +33,8 @@ certutil -F -n old-west -d /etc/ipsec.d
 #
 # -b YYMMDDHHMMZ (yea, CC is magic)
 
+VFYDATE=$(date -d @${THEN} +%y%m%d000000Z)
 /usr/lib64/nss/unsupported-tools/vfychain -v -u 12 -p -p -d /etc/ipsec.d -a OUTPUT/new-west.crt
-/usr/lib64/nss/unsupported-tools/vfychain -v -u 12 -p -p -b 2212300000Z -d /etc/ipsec.d -a OUTPUT/new-west.crt
+/usr/lib64/nss/unsupported-tools/vfychain -v -u 12 -p -p -b ${VFYDATE} -d /etc/ipsec.d -a OUTPUT/new-west.crt
 /usr/lib64/nss/unsupported-tools/vfychain -v -u 12 -p -p -d /etc/ipsec.d -a OUTPUT/old-west.crt
-/usr/lib64/nss/unsupported-tools/vfychain -v -u 12 -p -p -b 2212300000Z -d /etc/ipsec.d -a OUTPUT/old-west.crt
+/usr/lib64/nss/unsupported-tools/vfychain -v -u 12 -p -p -b ${VFYDATE} -d /etc/ipsec.d -a OUTPUT/old-west.crt
--- testing/pluto/nss-cert-11-cert-expired-initiator-ikev2/02-west-init.sh
+++ testing/pluto/nss-cert-11-cert-expired-initiator-ikev2/02-west-init.sh
@@ -1,9 +1,11 @@
 /testing/guestbin/swan-prep --nokeys
 
-# Generate a sane certificate with a sane date
+# Generate west's CA and then use that to generate a signed
+# cert+private-key that east can present when authenticating.  All
+# dates for these certs are sane.
 
-certutil -m 1 -S -k rsa -x         -w -2 -n west-ca -s "CN=west-ca"  -v 12 -t "CT,C,C" -d /etc/ipsec.d -z ipsec.conf
-certutil -m 2 -S -k rsa -c west-ca -w -2 -n east    -s "CN=east" -v 12 -t "u,u,u"  -d /etc/ipsec.d -z ipsec.conf
+certutil -m 1 -S -k rsa -x         -n west-ca -s "CN=west-ca"  -v 12 -t "CT,C,C" -d /etc/ipsec.d -z ipsec.conf
+certutil -m 2 -S -k rsa -c west-ca -n east    -s "CN=east" -v 12 -t "u,u,u"  -d /etc/ipsec.d -z ipsec.conf
 pk12util -W secret -o OUTPUT/east.p12 -n east -d /etc/ipsec.d
 certutil -L -n east -d /etc/ipsec.d -a > OUTPUT/east.crt
 certutil -F -n east -d /etc/ipsec.d
