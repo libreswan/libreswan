1d3ab17e0e8b7c37d29e7469228b8f9b9142687b
Andrew Cagney <cagney@gnu.org>
2023-02-16 14:33:45 -0500

    testing: in certoe-12-nat-server, s/sleep/wait-for-s.h/
    And drop:
    -4: ip_vti0@NONE: <NOARP> mtu XXXX qdisc noop state DOWN group default
    -    link/ipip 0.0.0.0 brd 0.0.0.0
    from expected out (hangover from vti test?).

--- testing/pluto/certoe-12-nat-server/roadrun.sh
+++ testing/pluto/certoe-12-nat-server/roadrun.sh
@@ -1,9 +1,9 @@
 # one packet, which gets eaten by XFRM, so east does not initiate
 ../../guestbin/ping-once.sh --down -I 192.1.3.209 192.1.3.130
 # wait on OE IKE negotiation
+../../guestbin/wait-for.sh --match private-or-clear -- ipsec trafficstatus
 # ping should succeed through tunnel
 ../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.3.130
-sleep 1
 # should show established tunnel and no bare shunts
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
