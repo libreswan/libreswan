8b193cdaa40b50eaa727e3aefb0bae914e34cec7
Vukašin Karadžić <vukasin.karadzic@gmail.com>
2023-02-28 20:00:09 +0100

    ikev2: modify .story of STATE_V2_PARENT_R1 state

--- programs/pluto/ikev2_states.c
+++ programs/pluto/ikev2_states.c
@@ -85,7 +85,7 @@ struct finite_state v2_states[] = {
 
 	S(STATE_V2_PARENT_I1, "sent IKE_SA_INIT request", CAT_HALF_OPEN_IKE_SA),
 	S(STATE_V2_PARENT_R0, "processing IKE_SA_INIT request", CAT_HALF_OPEN_IKE_SA),
-	S(STATE_V2_PARENT_R1, "sent IKE_SA_INIT reply", CAT_HALF_OPEN_IKE_SA, .v2.secured = true),
+	S(STATE_V2_PARENT_R1, "sent IKE_SA_INIT (or IKE_INTERMEDIATE) response", CAT_HALF_OPEN_IKE_SA, .v2.secured = true),
 	S(STATE_V2_PARENT_R_EAP, "sent EAP request", CAT_OPEN_IKE_SA, .v2.secured = true),
 
 	/*
