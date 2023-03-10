c7d6d6a4c4f68454239bb9ed3fc1442fdad1ac74
Andrew Cagney <cagney@gnu.org>
2023-03-09 20:07:26 -0500

    testing: in newoe-18-poc-clear tighten grep pattern

--- testing/pluto/newoe-18-poc-clear/4-road-run.sh
+++ testing/pluto/newoe-18-poc-clear/4-road-run.sh
@@ -1,6 +1,6 @@
 # trigger OE; check initiated
 ../../guestbin/ping-once.sh --forget -I 192.1.3.209 192.1.2.23
-../../guestbin/wait-for.sh --match "initiate on-demand" -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match "^initiate on-demand" -- cat /tmp/pluto.log
 ip xfrm state
 # wait on OE retransmits and rekeying, and larval state to timeout
 ../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ip xfrm state
@@ -11,7 +11,7 @@ ipsec whack --shuntstatus
 # ping should succeed in the clear
 ../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
 # should show for our failed attempt at OE
-grep "initiate on-demand" /tmp/pluto.log
+grep "^initiate on-demand" /tmp/pluto.log
 # save ip log
 killall ip > /dev/null 2> /dev/null
 cp /tmp/xfrm-monitor.out OUTPUT/road.xfrm-monitor.txt
