9cd534eb25aafbc5e48a7bd9cf3ebcf802e5a58d
Andrew Cagney <cagney@gnu.org>
2023-04-13 16:53:12 -0400

    testing: update ikev2-ondemand-02-permanent-* to check removal of policy/state
    see #1056

--- testing/pluto/ikev2-ondemand-02-permanent-auto-transport/03-west-up.sh
+++ testing/pluto/ikev2-ondemand-02-permanent-auto-transport/03-west-up.sh
@@ -1,4 +1,11 @@
+# trap installed
+../../guestbin/ipsec-kernel-policy.sh
+
 ../../guestbin/ping-once.sh --forget -I 192.1.2.45 192.1.2.23
 ../../guestbin/wait-for.sh --match west -- ipsec whack --trafficstatus
 ../../guestbin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
 ipsec whack --trafficstatus
+
+# real policy installed
+../../guestbin/ipsec-kernel-policy.sh
+../../guestbin/ipsec-kernel-state.sh
---
+++ testing/pluto/ikev2-ondemand-02-permanent-auto-transport/04-west-down.sh
@@ -0,0 +1,6 @@
+ipsec auto --down west
+
+# only trap
+../../guestbin/ipsec-kernel-policy.sh
+# everything cleared out
+../../guestbin/ipsec-kernel-state.sh
