17fb28868a227a52ab1f7b535d56493306d3143d
Andrew Cagney <cagney@gnu.org>
2023-04-15 07:57:34 -0400

    testing: update ikev2-revive-through-nat-02-cleanfail

--- testing/pluto/ikev2-revive-through-nat-02-cleanfail/06-east-revival.sh
+++ testing/pluto/ikev2-revive-through-nat-02-cleanfail/06-east-revival.sh
@@ -1,7 +1,8 @@
 # road is down, east with autostart=keep should try to revive; while
 # this is happening kernel-policy should still be in place
-../../guestbin/wait-for.sh --match 'deleting IKE SA but connection is supposed to remain up' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#1: connection is supposed to remain up' -- cat /tmp/pluto.log
 ../../guestbin/ipsec-kernel-policy.sh
+
 # but road is really down, so that fails; and everything is deleted
-../../guestbin/wait-for.sh --match '5 second timeout exceeded after 4 retransmits' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#3: STATE_V2_PARENT_I1: 5 second timeout exceeded after 4 retransmits' -- cat /tmp/pluto.log
 ../../guestbin/ipsec-kernel-policy.sh
