6f7563db3f38905a18a3d51dea0798ce55c9bea2
Andrew Cagney <cagney@gnu.org>
2023-05-26 13:01:48 -0400

    testing: in ikev2-impair-06-send-no-ike-ke keep impairing revival

--- testing/pluto/ikev2-impair-06-send-no-ike-ke/westrun.sh
+++ testing/pluto/ikev2-impair-06-send-no-ike-ke/westrun.sh
@@ -1,10 +1,13 @@
 # expect quick fail
+ipsec whack --impair none
+ipsec whack --impair revival
 ipsec whack --impair ke-payload:omit
 ipsec whack --impair timeout-on-retransmit
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
-ipsec whack --impair none
 
 # expect slower fail
+ipsec whack --impair none
+ipsec whack --impair revival
 ipsec whack --impair suppress-retransmits
 ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
 
