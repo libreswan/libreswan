96ba3747cfcdd2db90e86dddf0509e74628ea738
Andrew Cagney <cagney@gnu.org>
2023-06-21 09:18:26 -0400

    testing: update certoe-14-poc-del-slash32 certoe-15-poc-east-west
    pexpect removed from code
    close #528

--- testing/pluto/certoe-14-poc-del-slash32/5-road-redo.sh
+++ testing/pluto/certoe-14-poc-del-slash32/5-road-redo.sh
@@ -2,14 +2,15 @@
 ipsec whack --trafficstatus
 ../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ip xfrm state
 # try triggering again, ondemand policy should re-trigger OE
-ip xfrm state
-ip xfrm policy
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
 ../../guestbin/ping-once.sh --forget -I 192.1.3.209 192.1.2.23
 ../../guestbin/wait-for.sh --match private-or-clear -- ipsec whack --trafficstatus
 # should show established tunnel and no bare shunts
 ../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ip xfrm state
-ip xfrm state
-ip xfrm policy
+../../guestbin/ping-once.sh --up -I 192.1.3.209 192.1.2.23
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
 ipsec whack --trafficstatus
 ipsec whack --shuntstatus
 # ping should succeed through tunnel
--- testing/pluto/certoe-15-poc-east-west/5-west-redo.sh
+++ testing/pluto/certoe-15-poc-east-west/5-west-redo.sh
@@ -3,14 +3,15 @@ ipsec whack --trafficstatus
 # let the old acquire expire so it won't interfere with our new trigger
 ../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ip xfrm state
 # try triggering again, ondemand policy should re-trigger OE
-ip xfrm state
-ip xfrm policy
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
 ../../guestbin/ping-once.sh --forget -I 192.1.2.45 192.1.2.23
 ../../guestbin/wait-for.sh --match private -- ipsec whack --trafficstatus
 # wait on OE to re-establish IPsec SA
 ../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ip xfrm state
-ip xfrm state
-ip xfrm policy
+../../guestbin/ping-once.sh --forget -I 192.1.2.45 192.1.2.23
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
 ipsec whack --shuntstatus
 # ping should succeed through tunnel
 ../../guestbin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
