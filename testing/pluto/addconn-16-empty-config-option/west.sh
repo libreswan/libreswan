aec44b035aa5a2a991d85f2415efc200d382b2d8
Andrew Cagney <cagney@gnu.org>
2023-06-21 11:17:49 -0400

    testing: update addconn-16-empty-config-option, good

--- testing/pluto/addconn-16-empty-config-option/west.sh
+++ testing/pluto/addconn-16-empty-config-option/west.sh
@@ -1,11 +1,11 @@
 /testing/guestbin/swan-prep
 ipsec start
 ../../guestbin/wait-until-pluto-started
-# this should have failed to --add 
+# this should add, subnet= silently ignored
 ipsec auto --add first
+# now append a second conn; should also add
 echo 'conn second' >> /etc/ipsec.conf
 echo '        left=2.3.4.5' >> /etc/ipsec.conf
 echo '        right=5.6.7.8' >> /etc/ipsec.conf
-# now it fails properly
 ipsec auto --add first
 echo "initdone"
