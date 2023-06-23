56a4e3c212a56b6813e04e8bfa372015face642c
Andrew Cagney <cagney@gnu.org>
2023-06-23 16:49:38 -0400

    testing: add impair-20-ikev2-rekey-sa-protoid, corrupt protoid in REKEY_SA

---
+++ testing/pluto/impair-20-ikev2-rekey-sa-protoid/westrun.sh
@@ -0,0 +1,36 @@
+ipsec whack --impair revival
+
+# bring up west and confirm
+ipsec auto --up west
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+ipsec whack --rekey-ipsec --name west --async
+../../guestbin/wait-for.sh --match '#3: initiator rekeyed Child SA #2' -- cat /tmp/pluto.log
+ipsec auto --down west
+
+# protoid=none
+ipsec auto --up west
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+ipsec whack --impair v2n_rekey_sa_protoid:0 --impair emitting
+ipsec whack --rekey-ipsec --name west --async
+../../guestbin/wait-for.sh --match '#6: CREATE_CHILD_SA failed' -- cat /tmp/pluto.log
+ipsec auto --down west
+
+# protoid=IKE
+ipsec auto --up west
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+ipsec whack --impair v2n_rekey_sa_protoid:1 --impair emitting
+ipsec whack --rekey-ipsec --name west --async
+../../guestbin/wait-for.sh --match '#9: CREATE_CHILD_SA failed' -- cat /tmp/pluto.log
+ipsec auto --down west
+
+# protoid=unknown
+ipsec auto --up west
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+ipsec whack --impair v2n_rekey_sa_protoid:4 --impair emitting
+ipsec whack --rekey-ipsec --name west --async
+../../guestbin/wait-for.sh --match '#12: CREATE_CHILD_SA failed' -- cat /tmp/pluto.log
+ipsec auto --down west
