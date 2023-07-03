8cddb82b028e5b6580fa048eed04f3f595ccf87b
Andrew Cagney <cagney@gnu.org>
2023-07-02 21:45:11 -0400

    ipsec delete: drop recursive call

--- programs/pluto/whack_delete.c
+++ programs/pluto/whack_delete.c
@@ -25,44 +25,42 @@
  * Terminate and then delete connections with the specified name.
  */
 
-static void terminate_connection(struct connection **c, struct logger *logger)
+static void terminate_connection(struct connection **c, struct logger *logger, where_t where)
 {
-	connection_attach(*c, logger);
-
-	llog(RC_LOG, (*c)->logger, "terminating SAs using this connection");
-	del_policy(*c, POLICY_UP);
-	remove_connection_from_pending(*c);
-
-	/*
-	 * For CK_INSTANCE, this could also delete the *C connection.
-	 */
-	delete_states_by_connection(c);
-
-	connection_detach(*c, logger);
-}
-
-static void terminate_connections(struct connection **c, struct logger *logger, where_t where)
-{
+	if (never_negotiate(*c)) {
+		ldbg((*c)->logger, "skipping as never-negotiate");
+		PEXPECT(logger, (is_permanent(*c) || is_template(*c)));
+		return;
+	}
 	switch ((*c)->local->kind) {
 	case CK_PERMANENT:
 	case CK_INSTANCE:
 	case CK_LABELED_PARENT:
+		/*
+		 * For CK_INSTANCE, this could also delete the *C
+		 * connection.
+		 */
+		connection_attach(*c, logger);
+		llog(RC_LOG, (*c)->logger, "terminating SAs using this connection");
+		del_policy(*c, POLICY_UP);
+		remove_connection_from_pending(*c);
+		delete_states_by_connection(c);
+		connection_detach(*c, logger);
+		return;
 	case CK_LABELED_CHILD:
-		terminate_connection(c, logger); /* could delete C! */
+		/* let labeled parent terminate the child */
+		PEXPECT(logger, (*c)->config->ike_version == IKEv2);
+		return;
+	case CK_GROUP:
+		/* nothing to do */
 		return;
 	case CK_TEMPLATE:
-	case CK_GROUP:
 	case CK_LABELED_TEMPLATE:
-	{
-		struct connection_filter cq = {
-			.clonedfrom = *c,
-			.where = HERE,
-		};
-		while (next_connection_old2new(&cq)) {
-			terminate_connections(&cq.c, logger, where);
-		}
+		/* need to unroute */
+		connection_attach(*c, logger);
+		connection_unroute(*c, where);
+		connection_detach(*c, logger);
 		return;
-	}
 	case CK_INVALID:
 		break;
 	}
@@ -72,7 +70,7 @@ static void terminate_connections(struct connection **c, struct logger *logger, 
 static bool whack_delete_connection(struct show *s, struct connection **c,
 				   const struct whack_message *m UNUSED)
 {
-	terminate_connections(c, show_logger(s), HERE);
+	terminate_connection(c, show_logger(s), HERE);
 	if (*c != NULL) {
 		connection_attach(*c, show_logger(s));
 		delete_connection(c);
@@ -88,9 +86,13 @@ void whack_delete(const struct whack_message *m, struct show *s)
 		return;
 	}
 
+	/*
+	 * This is new-to-old which means that instances are processed
+	 * before templates.
+	 */
 	whack_each_connection(m, s, whack_delete_connection,
 			      (struct each) {
 				      .log_unknown_name = false,
-				      .skip_instances = true,
+				      .skip_instances = false, /* handled by whack_delete_connection() */
 			      });
 }
