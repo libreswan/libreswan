e33b036cf30a730437ed64f5ce3d2b2b8f05031d
Antony Antony <antony@phenome.org>
2023-07-19 18:01:31 +0200

    testing: fixes due changes tcpdump.sh

--- testing/pluto/ikev2-xfrmi-09-systemd-networkd/northrun.sh
+++ testing/pluto/ikev2-xfrmi-09-systemd-networkd/northrun.sh
@@ -9,7 +9,7 @@ ip -s link show ipsec2
 ip rule show
 ip route
 ip route show table 50
-../../guestbin/tcpdump.sh --kill
+../../guestbin/tcpdump.sh --stop -i ipsec1
 # rm the test specific systemd-networkd file for next test
 rm /etc/systemd/network/ipsec2.netdev
 echo done
