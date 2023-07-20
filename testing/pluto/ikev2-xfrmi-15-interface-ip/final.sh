184375767cf73384df0553a50b5dae453ef3c295
Antony Antony <antony@phenome.org>
2023-07-20 16:44:41 +0000

    testing: ikev2-xfrmi-15-interface-ip ad ipsec-look
    - use ipsec to to see if_id in xfrm policy and state
    - remove unsed nicinit.sh

--- testing/pluto/ikev2-xfrmi-15-interface-ip/final.sh
+++ testing/pluto/ikev2-xfrmi-15-interface-ip/final.sh
@@ -2,7 +2,7 @@ ipsec whack --trafficstatus
 ip -s link show ipsec1
 ip rule show
 ip route show table 50
-ip route
+../../guestbin/ipsec-look.sh
 : ==== cut ====
 # this is global will be noisy and change based on host.
 ip addr show
