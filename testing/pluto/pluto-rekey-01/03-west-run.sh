005b4572fd619d5e9b5f2553cc6e2a28bed95645
Andrew Cagney <cagney@gnu.org>
2023-08-03 17:09:01 -0400

    testing: expect IKEv1 to use ISAKMP

--- testing/pluto/pluto-rekey-01/03-west-run.sh
+++ testing/pluto/pluto-rekey-01/03-west-run.sh
@@ -3,8 +3,8 @@ ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
 
 # wait for an IKE rekey which happens at the 1m mark
 sleep 45
-../../guestbin/wait-for.sh --match '#3: initiating IKEv1 Main Mode connection to replace #1' -- cat /tmp/pluto.log
-../../guestbin/wait-for.sh --match '#3.*IKE SA established' -- ipsec whack --showstates
+../../guestbin/wait-for.sh --match '#3: initiating .* to replace #1' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#3: .* SA established' -- ipsec whack --showstates
 
 # because both ends are fighting over who is establishing the ISAKMP
 # SA, showstates is always changing.
