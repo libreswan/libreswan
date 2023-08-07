e9ba3b9f50921b6d465564eac841025b46029412
Andrew Cagney <cagney@gnu.org>
2023-08-07 12:56:25 -0400

    testing: in ikev2-tcp-02-timeout allow a few ERETRYs

--- testing/pluto/ikev2-tcp-02-timeout/final.sh
+++ testing/pluto/ikev2-tcp-02-timeout/final.sh
@@ -1,2 +1,5 @@
-ipsec look
-grep '^connection from' /tmp/pluto.log
+# should be gone
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
+grep '^connection from' /tmp/pluto.log | grep -v EAGAIN
+eagain=$(grep EAGAIN /tmp/pluto.log | wc -l) ; test ${eagain} -gt 30 && echo "${eagain} is too much EAGAIN?"
