e0bd02d81ac705b06c8cef3eba353e5e7e4520dd
Andrew Cagney <cagney@gnu.org>
2023-08-08 17:28:35 -0400

    ipsec down ikev2: use whack_connection_delete_states()
    fixes transition to UNROUTED instead of ROUTED_ONDEMAND

--- programs/pluto/whack_down.c
+++ programs/pluto/whack_down.c
@@ -93,10 +93,8 @@ static void down_connection(struct connection **c, struct logger *logger)
 			 * state :-/
 			 */
 			dbg("connection not shared - terminating IKE and IPsec SA");
-			delete_v2_states_by_connection(*c);
+			whack_connection_delete_states(*c, HERE);
 			if (is_instance(*c)) {
-				remove_connection_from_pending(*c);
-				connection_unroute(*c, HERE);
 				delete_connection(c);
 			}
 		}
