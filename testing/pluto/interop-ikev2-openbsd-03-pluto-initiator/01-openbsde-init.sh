0ece247d8b43c33876646ab6be8c9aefc742b498
Andrew Cagney <cagney@gnu.org>
2023-08-11 16:15:12 -0400

    testing: give iked a second to start

--- testing/pluto/interop-ikev2-openbsd-03-pluto-initiator/01-openbsde-init.sh
+++ testing/pluto/interop-ikev2-openbsd-03-pluto-initiator/01-openbsde-init.sh
@@ -3,5 +3,5 @@ cp openbsde.conf /etc/iked.conf
 chmod 600 /etc/iked.conf
 rm -f /tmp/iked.log
 ln -s $PWD/OUTPUT/openbsde.iked.log /tmp/iked.log
-/sbin/iked -dvvv > /tmp/iked.log 2>&1 &
+/sbin/iked -dvvv > /tmp/iked.log 2>&1 & sleep 1
 echo "initdone"
