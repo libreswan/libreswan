19c1e6a73dd0253134390c0242edcc5990e746c7
Andrew Cagney <cagney@gnu.org>
2023-08-11 21:59:26 -0400

    testing: switch github-1188-lost-policy to authby=secret
    ref #1188; still not failing

--- testing/pluto/github-1188-lost-policy/05-road-status.sh
+++ testing/pluto/github-1188-lost-policy/05-road-status.sh
@@ -5,3 +5,7 @@ ipsec showstates
 # now check policy/state
 ../../guestbin/ipsec-kernel-policy.sh
 
+# wait for #1 to die
+../../guestbin/wait-for.sh --no-match '#1:' -- ipsec showstates
+../../guestbin/ipsec-kernel-policy.sh
+
