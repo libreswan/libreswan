2678be15356c7ee21ef22cb1de0d195aec6c9e24
Andrew Cagney <cagney@gnu.org>
2023-08-12 13:50:58 -0400

    pfkeyv2: for esp=aes_gcm set auth=0
    it seems pfkeyv2 has 251 for ah=none and 0 for AEAD's no-auth
    fix #1220

--- programs/pluto/kernel_pfkeyv2.c
+++ programs/pluto/kernel_pfkeyv2.c
@@ -25,6 +25,7 @@
 #include "ip_encap.h"
 #include "chunk.h"
 #include "hunk.h"
+#include "ike_alg_integ.h"	/* for ike_alg_integ_none; */
 
 #include "kernel.h"
 #include "kernel_alg.h"
@@ -260,8 +261,10 @@ static struct sadb_sa *put_sadb_sa(struct outbuf *msg,
 				   const struct encrypt_desc *encrypt,
 				   const struct ipcomp_desc *ipcomp)
 {
-	unsigned aalg = (integ != NULL ? integ->integ_sadb_aalg_id :
+	unsigned aalg = (integ == &ike_alg_integ_none && encrypt_desc_is_aead(encrypt) ? SADB_AALG_NONE :
+			 integ != NULL ? integ->integ_sadb_aalg_id :
 			 0);
+
 	unsigned ealg = (encrypt != NULL ? encrypt->encrypt_sadb_ealg_id :
 			 /* XXX: NetBSD treats IPCOMP like ENCRYPT */
 			 ipcomp != NULL ? ipcomp->ipcomp_sadb_calg_id : 0);
