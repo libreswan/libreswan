8ec2b1c47f65a7e33bf2ead94cbd13be89ca2ad0
Andrew Cagney <cagney@gnu.org>
2023-08-20 20:18:01 -0400

    testing: robustify impair-20-ikev2-rekey-sa-protoid
    after the Child SA has rekeyed, wait for replaced Child to exit

--- testing/pluto/impair-20-ikev2-rekey-sa-protoid/westrun.sh
+++ testing/pluto/impair-20-ikev2-rekey-sa-protoid/westrun.sh
@@ -1,11 +1,12 @@
 ipsec whack --impair revival
 
-# bring up west and confirm
+# bring up west and then immediately re-key
 ipsec auto --up west
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
 ipsec whack --rekey-ipsec --name west --async
 ../../guestbin/wait-for.sh --match '#3: initiator rekeyed Child SA #2' -- cat /tmp/pluto.log
+../../guestbin/wait-for.sh --match '#2: ESP traffic information:' -- cat /tmp/pluto.log
 ipsec auto --down west
 
 # protoid=none
