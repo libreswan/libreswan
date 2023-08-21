bc58c47c37261a0f8d33f62358f322e410497fb1
Andrew Cagney <cagney@gnu.org>
2023-08-20 21:26:39 -0400

    testing: robustify ikev2-xfrmi-09-systemd-networkd a little
    sleep 20 so that tcpdump has time to capture all 4 pings
    (better solution needed)

--- testing/pluto/ikev2-xfrmi-09-systemd-networkd/northrun.sh
+++ testing/pluto/ikev2-xfrmi-09-systemd-networkd/northrun.sh
@@ -1,5 +1,5 @@
 ipsec auto --up north
-sleep  2
+sleep  2 # why?
 ../../guestbin/tcpdump.sh --start -i ipsec2
 ../../guestbin/ping-once.sh --up  192.0.2.254
 ../../guestbin/ping-once.sh --up  192.0.2.254
@@ -9,6 +9,7 @@ ip -s link show ipsec2
 ip rule show
 ip route
 ip route show table 50
+sleep 20 # wait for tcpdump to collect all events
 ../../guestbin/tcpdump.sh --stop -i ipsec2
 # rm the test specific systemd-networkd file for next test
 rm /etc/systemd/network/ipsec2.netdev
