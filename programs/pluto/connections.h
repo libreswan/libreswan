cd207d98e0b0bb97ef2d7b5c71546115abff0748
Andrew Cagney <cagney@gnu.org>
2023-08-25 16:22:07 -0400

    connections: make st_connection_delref() a no-op

--- programs/pluto/connections.h
+++ programs/pluto/connections.h
@@ -735,7 +735,7 @@ void delete_connection_where(struct connection **cp, where_t where);
  * Compensate for the state's .st_connection not being refcnt'd.  Once
  * that happens this becomes a no-op.
  */
-#define st_connection_delref(CP) connection_delref_where(CP, (*CP)->logger, HERE)
+#define st_connection_delref(CP) /* can't trust CP */
 
 struct connection *connection_addref_where(struct connection *c, const struct logger *owner, where_t where);
 void connection_delref_where(struct connection **cp, const struct logger *owner, where_t where);
