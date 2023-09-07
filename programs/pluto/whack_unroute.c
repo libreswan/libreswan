672d2925ee849f5334a521122faea761a31fdc8a
Andrew Cagney <cagney@gnu.org>
2023-09-07 13:33:12 -0400

    ipsec unroute: use whack_connection()
    explicitly exclude instances (presumably because the template
    owns the route)

--- programs/pluto/whack_unroute.c
+++ programs/pluto/whack_unroute.c
@@ -25,19 +25,48 @@
 static bool whack_unroute_connection(struct show *s, struct connection *c,
 				     const struct whack_message *m UNUSED)
 {
-	connection_attach(c, show_logger(s));
-	/*
-	 * Let code know of intent.
-	 *
-	 * Functions such as connection_unroute() don't fiddle policy
-	 * bits as they are called as part of unroute/route sequences.
-	 */
-	del_policy(c, POLICY_ROUTE);
-	connection_unroute(c, HERE);
-	connection_detach(c, show_logger(s));
-	return true; /* ok; keep going */
-}
+	struct logger *logger = show_logger(s);
+	switch (c->local->kind) {
 
+	case CK_TEMPLATE:
+	case CK_LABELED_TEMPLATE:
+	case CK_PERMANENT:
+		/*
+		 * Let code know of intent.
+		 *
+		 * Functions such as connection_unroute() don't fiddle
+		 * policy bits as they are called as part of
+		 * unroute/route sequences.
+		 */
+		connection_attach(c, show_logger(s));
+		del_policy(c, POLICY_ROUTE);
+		connection_unroute(c, HERE);
+		connection_detach(c, show_logger(s));
+		return true; /* ok; keep going */
+
+	case CK_LABELED_PARENT:
+	case CK_LABELED_CHILD:
+	case CK_INSTANCE:
+		/*
+		 * Skip instances, why?
+		 *
+		 * This assumes that an instance was routed by its
+		 * template?  And hence only templates and permanents
+		 * need unrouting?
+		 */
+		connection_attach(c, logger);
+		llog(RC_LOG, c->logger, "cannot initiate");
+		connection_detach(c, logger);
+		return false;
+
+	case CK_GROUP:
+		return whack_connection_instances(m, s, c, whack_unroute_connection);
+
+	case CK_INVALID:
+		break;
+	}
+	bad_enum(show_logger(s), &connection_kind_names, c->local->kind);
+}
 
 void whack_unroute(const struct whack_message *m, struct show *s)
 {
@@ -48,9 +77,8 @@ void whack_unroute(const struct whack_message *m, struct show *s)
 		return;
 	}
 
-	whack_each_connection(m, s, whack_unroute_connection,
-			      (struct each) {
-				      .log_unknown_name = true,
-				      .skip_instances = true,
-			      });
+	whack_connection(m, s, whack_unroute_connection,
+			 (struct each) {
+				 .log_unknown_name = true,
+			 });
 }
