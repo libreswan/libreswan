3fb5ff3ec490704ab8a708ae0ae9596f4c09e371
Andrew Cagney <cagney@gnu.org>
2023-09-11 22:17:12 -0400

    testing: add wip ikev2-invalid-ke-08-rekey-child
    see #1259

---
+++ testing/pluto/ikev2-invalid-ke-08-rekey-child/westrun.sh
@@ -0,0 +1,15 @@
+# bring up west and confirm
+ipsec auto --up west
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+# expect IKE #1 CHILD #2
+ipsec whack --trafficstatus
+# why?
+echo "sleep 9"
+sleep 9
+# rekey CHILD SA
+ipsec whack --rekey-ipsec --name west
+sleep 2
+# expect IKE #1 CHILD #3
+ipsec showstates
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
