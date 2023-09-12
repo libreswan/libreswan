2429009335b39763c196fd23b129bca42edf6f16
Andrew Cagney <cagney@gnu.org>
2023-09-11 23:00:45 -0400

    testing: update ikev2-invalid-ke-07-second-child
    expect log to include suggested KE

--- testing/pluto/ikev2-invalid-ke-07-second-child/03-west-up.sh
+++ testing/pluto/ikev2-invalid-ke-07-second-child/03-west-up.sh
@@ -1,6 +1,2 @@
 ipsec up west-cuckold
-ipsec up west-cuckoo
-
-../../guestbin/ping-once.sh --up 192.0.2.254
-../../guestbin/ping-once.sh --up 192.0.20.254
-ipsec trafficstatus
+ipsec up west-cuckoo # fail with invalid KE
