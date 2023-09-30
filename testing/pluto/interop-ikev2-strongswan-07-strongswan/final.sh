1c62425f47beee022f9b00166894dad16fa4d0c0
Andrew Cagney <cagney@gnu.org>
2023-09-29 22:09:57 -0400

    testing: sprinkle state/policy over an SH if statement

--- testing/pluto/interop-ikev2-strongswan-07-strongswan/final.sh
+++ testing/pluto/interop-ikev2-strongswan-07-strongswan/final.sh
@@ -1,4 +1,4 @@
 ip xfrm policy
 ip xfrm state
-if [ -f /var/run/pluto/pluto.pid ]; then ../../guestbin/ipsec-look.sh ; fi
+if [ -f /var/run/pluto/pluto.pid ]; then ../../guestbin/ipsec-kernel-state.sh ; ../../guestbin/ipsec-kernel-policy.sh ; fi
 if [ -f /var/run/charon.pid -o -f /var/run/strongswan/charon.pid ]; then strongswan statusall ; fi
--- testing/pluto/interop-ikev2-strongswan-36-initiator-sha1-sha2-rsa_pss-no/final.sh
+++ testing/pluto/interop-ikev2-strongswan-36-initiator-sha1-sha2-rsa_pss-no/final.sh
@@ -1,4 +1,4 @@
-if [ -f /var/run/pluto/pluto.pid ]; then ../../guestbin/ipsec-look.sh ; fi
+if [ -f /var/run/pluto/pluto.pid ]; then ../../guestbin/ipsec-kernel-state.sh ; ../../guestbin/ipsec-kernel-policy.sh ; fi
 # expect state #2, state #1 responded with INVALID_KE
 if [ -f /var/run/pluto/pluto.pid ]; then grep " authenticated peer " /tmp/pluto.log ; fi
 if [ -f /var/run/charon.pid -o -f /var/run/strongswan/charon.pid ]; then strongswan status ; fi
