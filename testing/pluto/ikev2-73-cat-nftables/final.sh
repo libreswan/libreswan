d88c6c6c59d978183f6b83765a9a89b4e04238a9
Andrew Cagney <cagney@gnu.org>
2023-10-04 12:48:44 -0400

    testing: update nftables

--- testing/pluto/ikev2-73-cat-nftables/final.sh
+++ testing/pluto/ikev2-73-cat-nftables/final.sh
@@ -1,2 +1,3 @@
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
+nft list ruleset
--- testing/pluto/nflog-01-global-nftables/final.sh
+++ testing/pluto/nflog-01-global-nftables/final.sh
@@ -1,5 +1,6 @@
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
+nft list ruleset
 ipsec stop
 # show no nflog left behind
 # it may leave an empty table ip filter
