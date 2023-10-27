a01798240860c1561d3fc6c8488ebd91374fa54a
Andrew Cagney <cagney@gnu.org>
2023-10-26 23:55:40 -0400

    testing: in impair-12-ikev2-transform-none rekey connections
    see #1244

--- testing/pluto/impair-12-ikev2-transform-none/westrun.sh
+++ testing/pluto/impair-12-ikev2-transform-none/westrun.sh
@@ -1,21 +1,21 @@
 # normal case, not sent
 ipsec whack --impair v2-proposal-integ:no
-../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm --up --rekey -I 192.0.1.254 192.0.2.254
 # force its addition
 ipsec whack --impair v2-proposal-integ:allow-none
-../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm --up --rekey -I 192.0.1.254 192.0.2.254
 # force its exclusion
 ipsec whack --impair v2-proposal-integ:drop-none
-../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm --up --rekey -I 192.0.1.254 192.0.2.254
 ipsec whack --impair v2-proposal-integ:no
 
 # normal case, not sent
 ipsec whack --impair v2-proposal-dh:no
-../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm --up --rekey -I 192.0.1.254 192.0.2.254
 # force its addition
 ipsec whack --impair v2-proposal-dh:allow-none
-../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm --up --rekey -I 192.0.1.254 192.0.2.254
 # force its exclusion
 ipsec whack --impair v2-proposal-dh:drop-none
-../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm  -I 192.0.1.254 192.0.2.254
+../../guestbin/libreswan-up-down.sh ike,esp=aes_gcm --up --rekey -I 192.0.1.254 192.0.2.254
 ipsec whack --impair v2-proposal-integ:no
