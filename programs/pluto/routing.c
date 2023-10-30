bab91527ddeaa9dd46d221b78534af0b74557c3e
Andrew Cagney <cagney@gnu.org>
2023-10-29 20:26:15 -0400

    routing: clone teardown_routed_tunnel() to teardown_unrouted_tunnel()

--- programs/pluto/routing.c
+++ programs/pluto/routing.c
@@ -760,6 +760,84 @@ static void teardown_routed_tunnel(enum routing_event event,
 	set_routing(event, c, RT_UNROUTED, NULL);
 }
 
+static void teardown_unrouted_tunnel(enum routing_event event,
+				     struct connection *c,
+				     struct child_sa **child,
+				     where_t where)
+{
+	PASSERT((*child)->sa.st_logger, c == (*child)->sa.st_connection);
+
+	if (c->newest_routing_sa > (*child)->sa.st_serialno) {
+		/* no longer child's */
+		ldbg_routing((*child)->sa.st_logger,
+			     "keeping connection kernel policy; routing SA "PRI_SO" is newer",
+			     pri_so(c->newest_routing_sa));
+		return;
+	}
+
+	if (c->newest_ipsec_sa > (*child)->sa.st_serialno) {
+		/* covered by above; no!? */
+		ldbg_routing((*child)->sa.st_logger,
+			     "keeping connection kernel policy; IPsec SA "PRI_SO" is newer",
+			     pri_so(c->newest_ipsec_sa));
+		return;
+	}
+
+	if (scheduled_child_revival(*child, "received Delete/Notify")) {
+		routed_tunnel_to_routed_ondemand(event, (*child), where);
+		return;
+	}
+
+	/*
+	 * Should this go back to on-demand?
+	 */
+	if (is_permanent(c) && c->policy.route) {
+		/* it's being stripped of the state, hence SOS_NOBODY */
+		routed_tunnel_to_routed_ondemand(event, (*child), where);
+		return;
+	}
+
+	/*
+	 * Is there a failure shunt?
+	 */
+	if (is_permanent(c) && c->config->failure_shunt != SHUNT_NONE) {
+		routed_tunnel_to_routed_failure(event, (*child), where);
+		return;
+	}
+
+	/*
+	 * Never delete permanent connections.
+	 */
+	if (is_permanent(c)) {
+		ldbg_routing((*child)->sa.st_logger,
+			     "keeping connection; it is permanent");
+		do_updown_spds(UPDOWN_DOWN, c, &c->child.spds, &(*child)->sa,
+			       (*child)->sa.st_logger);
+		delete_spd_kernel_policies(&c->child.spds,
+					   EXPECT_KERNEL_POLICY_OK,
+					   (*child)->sa.st_logger,
+					   where, "delete");
+		/*
+		 * update routing; route_owner() will see this and not
+		 * think this route is the owner?
+		 */
+		set_routing(event, c, RT_UNROUTED, NULL);
+		do_updown_unroute(c, *child);
+		return;
+	}
+
+	PASSERT((*child)->sa.st_logger, is_instance(c));
+
+	do_updown_spds(UPDOWN_DOWN, c, &c->child.spds,
+		       &(*child)->sa, (*child)->sa.st_logger);
+
+	delete_spd_kernel_policies(&c->child.spds,
+				   EXPECT_KERNEL_POLICY_OK,
+				   (*child)->sa.st_logger,
+				   where, "delete");
+	set_routing(event, c, RT_UNROUTED, NULL);
+}
+
 static void teardown_unrouted_inbound(enum routing_event event,
 				      struct connection *c,
 				      struct child_sa **child,
@@ -1495,7 +1573,7 @@ static bool dispatch_1(enum routing_event event,
 	case X(TIMEOUT_CHILD, UNROUTED_TUNNEL, INSTANCE):
 	case X(TIMEOUT_CHILD, UNROUTED_TUNNEL, PERMANENT):
 		ldbg_routing(logger, "OOPS: UNROUTED_TUNNEL isn't routed!");
-		teardown_routed_tunnel(event, c, e->child, e->where);
+		teardown_unrouted_tunnel(event, c, e->child, e->where);
 		return true;
 
 	case X(DELETE_CHILD, UNROUTED_INBOUND, INSTANCE):
