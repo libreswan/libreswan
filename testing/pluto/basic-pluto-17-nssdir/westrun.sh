f65e935b8c8527d54dc4d767d25a0079018a029c
Andrew Cagney <cagney@gnu.org>
2023-11-02 13:59:47 -0400

    testing: update pluto/basic-pluto-17-nssdir
    eliminate: grep '*.db'; use find; see #1367
    don't throw away output from <<ipsec newhostkey>>

--- testing/pluto/basic-pluto-17-nssdir/westrun.sh
+++ testing/pluto/basic-pluto-17-nssdir/westrun.sh
@@ -3,10 +3,10 @@ IPSEC_TEST_DIR=/tmp/TESTipsec.d
 mkdir ${IPSEC_TEST_DIR}
 rm -f ${IPSEC_DIR}/*.db
 ipsec initnss --nssdir ${IPSEC_TEST_DIR}
-ls ${IPSEC_DIR} | grep -E '*.db' # should show no db files
-ls ${IPSEC_TEST_DIR} | grep -E '*.db' # should show db files
+find ${IPSEC_DIR}      -name '*.db' # should show no db files
+find ${IPSEC_TEST_DIR} -name '*.db' # should show db files
 ipsec checknss --nssdir ${IPSEC_TEST_DIR}
-ls ${IPSEC_DIR} | grep -E '*.db' # should show no db files
-ls ${IPSEC_TEST_DIR} | grep -E '*.db' # should show db files
-ipsec newhostkey --nssdir ${IPSEC_TEST_DIR} > /dev/null 2> /dev/null
+find ${IPSEC_DIR}      -name '*.db' # should show no db files
+find ${IPSEC_TEST_DIR} -name '*.db' # should show db files
+ipsec newhostkey --nssdir ${IPSEC_TEST_DIR}
 rm -rf ${IPSEC_TEST_DIR}
