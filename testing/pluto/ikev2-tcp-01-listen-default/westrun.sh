f067fc095631680559be6f99d431e0938d9cb462
Andrew Cagney <cagney@gnu.org>
2023-11-06 09:55:03 -0500

    testing: add wip ikev2-tcp-01-listen-* tests
    results are interesting

---
+++ testing/pluto/ikev2-tcp-01-listen-default/westrun.sh
@@ -0,0 +1,7 @@
+# unfortunately does not yet indicate it is using TCP
+ipsec up west
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec whack --trafficstatus
+# should show tcp being used
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
