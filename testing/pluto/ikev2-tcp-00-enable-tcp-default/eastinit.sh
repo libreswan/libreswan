76534c99880abf77915d70760786a74029970856
Andrew Cagney <cagney@gnu.org>
2023-11-06 09:52:01 -0500

    testing: add ikev2-tcp-01-listen-default - good

---
+++ testing/pluto/ikev2-tcp-00-enable-tcp-default/eastinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec add east
+# block IKE and ESP over UDP
+iptables -I INPUT -p udp --dport 500 -j DROP
+iptables -I INPUT -p udp --dport 4500 -j DROP
+echo "initdone"
