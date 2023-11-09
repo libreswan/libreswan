4d2b3ddcdb20c440603b9b153f6cee9ba2106b44
Andrew Cagney <cagney@gnu.org>
2023-11-08 19:39:08 -0500

    testing: robustify ikev1-cryptoload-01
    use wait-for.sh; estimate nr sleeps was way off

--- testing/pluto/ikev1-cryptoload-01/westrun.sh
+++ testing/pluto/ikev1-cryptoload-01/westrun.sh
@@ -1,13 +1,9 @@
-export EF_DISABLE_BANNER=1
-ipsec pluto --impair helper_thread_delay:1 --config /etc/ipsec.conf
-# expecting 2*16 + 5 = 37 tunnels to come up
-sleep 10
-# waiting
-sleep 10
-# waiting
-sleep 10
-# waiting
-sleep 10
-# waiting
+# everything as auto=start
+EF_DISABLE_BANNER=1 ipsec pluto --impair helper_thread_delay:1 --config /etc/ipsec.conf
+
+# Expecting 2*16 + 5 = 37 tunnels to come up.  Each requires two 2 DH
+# offloads (plus possible extra offloads for things like NONCE).
+
 ipsec status | grep Total
-echo done
+
+../../guestbin/wait-for.sh --timeout 180 --match 'loaded 37, active 37' -- ipsec status
