8384c01bdd6cd3c8d4c8c899f7c94b9f0fc8aba5
Andrew Cagney <cagney@gnu.org>
2023-11-12 08:29:32 -0500

    testing: update ipsec showstates output

--- testing/pluto/ikev2-53-clean-pending/westrun.sh
+++ testing/pluto/ikev2-53-clean-pending/westrun.sh
@@ -1,4 +1,4 @@
 ipsec auto --status | grep westnet-eastnet-ipv4-psk-ikev2
 # wait for the IKE SA to die
-../../guestbin/wait-for.sh --timeout 40 --match '#2: pending CHILD SA' -- ipsec auto --status
+../../guestbin/wait-for.sh --timeout 40 --match '#2: pending Child SA' -- ipsec auto --status
 ipsec auto --status | grep westnet-eastnet-ipv4-psk-ikev2
