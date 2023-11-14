5d25985ecc9d6b7977f41a7d840ecfbaff852a63
Andrew Cagney <cagney@gnu.org>
2023-11-14 18:30:34 -0500

    testing: try robustifying ikev2-child-ipsec-replace-ike

--- testing/pluto/ikev2-child-ipsec-replace-ike/westrun.sh
+++ testing/pluto/ikev2-child-ipsec-replace-ike/westrun.sh
@@ -1,11 +1,22 @@
 ipsec auto --up westnet-eastnet-ikev2a
-ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+
 ipsec auto --up westnet-eastnet-ikev2b
-ping -n -q -c 4 -I 192.0.100.254 192.0.200.254
+../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
+
 ipsec auto --up westnet-eastnet-ikev2c
 ipsec whack --trafficstatus
-sleep 40
-ping -n -q -c 4 -I 192.0.1.254 192.0.2.254
-ping -n -q -c 4 -I 192.0.100.254 192.0.200.254
-ipsec whack --trafficstatus
+
+# wait for the replace
+../../guestbin/wait-for.sh --match '#5: initiating IKEv2 connection to replace established IKE SA #1' -- cat /tmp/pluto.log
+
+../../guestbin/wait-for.sh --match '#6: initiator established Child SA using #5' -- cat /tmp/pluto.log | sed -e 's/[a-c]"/."/'
+../../guestbin/wait-for.sh --match '#7: initiator established Child SA using #5' -- cat /tmp/pluto.log | sed -e 's/[a-c]"/."/'
+../../guestbin/wait-for.sh --match '#8: initiator established Child SA using #5' -- cat /tmp/pluto.log | sed -e 's/[a-c]"/."/'
+
+../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
+
+ipsec whack --trafficstatus |  sed -e 's/[a-c]"/."/'
+
 echo done
