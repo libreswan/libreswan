825e46f7eab99f4c5a37e8e0e7ed45547c80e4ca
Andrew Cagney <cagney@gnu.org>
2023-11-17 18:00:19 -0500

    ikev2: initiating IKEv2 connection to ... using TCP
    or ... UDP; fix #1398

--- programs/pluto/ikev2_ike_sa_init.c
+++ programs/pluto/ikev2_ike_sa_init.c
@@ -613,8 +613,13 @@ struct ike_sa *initiate_v2_IKE_SA_INIT_request(struct connection *c,
 			  what, predecessor->st_serialno);
 		move_pending(ike_sa(predecessor, HERE), ike);
 	} else {
+		address_buf ab;
+		const struct ip_protocol *protocol = endpoint_protocol(ike->sa.st_remote_endpoint);
+		ip_address remote_addr = endpoint_address(ike->sa.st_remote_endpoint);
 		llog_sa(log_stream | (RC_NEW_V2_STATE + STATE_V2_PARENT_I1), ike,
-			  "initiating IKEv2 connection");
+			"initiating IKEv2 connection to %s using %s",
+			str_address(&remote_addr, &ab),
+			protocol->name);
 	}
 
 	/*
