74ea29f5ab6dd40013a338efc136eb9488ceba24
Andrew Cagney <cagney@gnu.org>
2023-11-21 10:42:20 -0500

    testing: add real connalias= tests
    ref #1415
    ref #1412

---
+++ testing/pluto/connalias-02-twoconn-ikev1/02-west-init.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep --46 --nokey
+ipsec start
+../../guestbin/wait-until-pluto-started
+
+ipsec add west-cuckold
+ipsec add west-cuckoo
+
+echo "initdone"
---
+++ testing/pluto/connalias-02-twoconn-ikev2/03-west-up.sh
@@ -0,0 +1,9 @@
+ipsec up west
+ipsec down west
+
+ipsec up cuckold
+ipsec up cuckoo
+ipsec down west
+
+ipsec delete cuckold
+ipsec delete cuckoo
