688b4d12c4062bcfc5379cc22c230d7bed200c4d
Andrew Cagney <cagney@gnu.org>
2023-11-29 11:44:22 -0500

    testing: sprinkle ping-once.sh

--- testing/pluto/basic-pluto-19-seedbits/westrun.sh
+++ testing/pluto/basic-pluto-19-seedbits/westrun.sh
@@ -13,7 +13,7 @@ ipsec start
 ../../guestbin/wait-until-pluto-started
 sleep 10
 # this ping should fail due to the type=block connection
-ping -n -q -c 1 192.1.2.23
+../../guestbin/ping-once.sh --error 192.1.2.23
 ipsec stop
 grep -E "bits random|bytes from|seeded" /tmp/pluto.log
 test -f /usr/local/libexec/ipsec/pluto && PLUTOBIN="/usr/local/libexec/ipsec/pluto"
--- testing/pluto/ikev2-liveness-11-silent/westrun.sh
+++ testing/pluto/ikev2-liveness-11-silent/westrun.sh
@@ -10,41 +10,41 @@ echo "Tunnel should be up"
 ipsec whack --trafficstatus
 # Wait more then 15 seconds while ensuring there is traffic
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 # force a rekey
 ipsec auto --up west-east
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 sleep 1
-ping -n -q -c 1 192.1.2.23 >/dev/null
+../../guestbin/ping-once.sh --up 192.1.2.23 >/dev/null
 echo done
