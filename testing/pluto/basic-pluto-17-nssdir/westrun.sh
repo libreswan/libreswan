f3be94c30c888d0e122318e8a6d7771075a9418e
Andrew Cagney <cagney@gnu.org>
2023-11-30 22:44:38 -0500

    testing: in basic-pluto-17-nssdir sort find output

--- testing/pluto/basic-pluto-17-nssdir/westrun.sh
+++ testing/pluto/basic-pluto-17-nssdir/westrun.sh
@@ -3,10 +3,10 @@ IPSEC_TEST_DIR=/tmp/TESTipsec.d
 mkdir ${IPSEC_TEST_DIR}
 rm -f ${IPSEC_DIR}/*.db
 ipsec initnss --nssdir ${IPSEC_TEST_DIR}
-find ${IPSEC_DIR}      -name '*.db' # should show no db files
-find ${IPSEC_TEST_DIR} -name '*.db' # should show db files
+find ${IPSEC_DIR}      -name '*.db' | sort  # should show no db files
+find ${IPSEC_TEST_DIR} -name '*.db' | sort  # should show db files
 ipsec checknss --nssdir ${IPSEC_TEST_DIR}
-find ${IPSEC_DIR}      -name '*.db' # should show no db files
-find ${IPSEC_TEST_DIR} -name '*.db' # should show db files
+find ${IPSEC_DIR}      -name '*.db' | sort  # should show no db files
+find ${IPSEC_TEST_DIR} -name '*.db' | sort  # should show db files
 ipsec newhostkey --nssdir ${IPSEC_TEST_DIR}
 rm -rf ${IPSEC_TEST_DIR}
