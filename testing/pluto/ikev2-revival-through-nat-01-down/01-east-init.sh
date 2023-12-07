8558b75fd6c5469379a1884c2368ec843ba896f8
Andrew Cagney <cagney@gnu.org>
2023-12-07 17:09:43 -0500

    testing: robustify ikev2-revival-through-nat-01-down
    use --impair trigger-revival to step through easts revival

--- testing/pluto/ikev2-revival-through-nat-01-down/01-east-init.sh
+++ testing/pluto/ikev2-revival-through-nat-01-down/01-east-init.sh
@@ -2,4 +2,10 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 # loaded via ipsec.conf - no ipsec auto --keep yet
+
+# Late in the game there will be a revival attempt; make it pause so
+# it can be run manually.
+
+ipsec whack --impair revival
+
 echo "initdone"
--- testing/pluto/ikev2-revival-through-nat-01-down/06-east-revival.sh
+++ testing/pluto/ikev2-revival-through-nat-01-down/06-east-revival.sh
@@ -2,6 +2,11 @@
 # this is happening kernel-policy should still be in place
 ../../guestbin/wait-for.sh --match 'supposed to remain up' -- cat /tmp/pluto.log
 ../../guestbin/ipsec-kernel-policy.sh
+
+# Now trigger the revival.  Since ROAD is down it will fail.  And
+# being KEEP further revivals are abandoned.
+ipsec whack --impair trigger-revival:2
+
 # since the NAT port is still open road should allow recovery
 ../../guestbin/wait-for.sh --match '^".*#4: initiator established Child SA using #3' -- cat /tmp/pluto.log
 ../../guestbin/ipsec-kernel-policy.sh
