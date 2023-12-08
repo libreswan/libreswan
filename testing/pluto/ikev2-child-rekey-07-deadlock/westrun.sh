0e908e612fae0dd554cf74d8a66aea1ee0ba6122
Andrew Cagney <cagney@gnu.org>
2023-12-07 21:56:44 -0500

    testing: robustify ikev2-child-rekey-07-deadlock
    drop:
     this is complex grep line susceptible to changes to log lines.
     until we find better one keep this.
     May be once the bug is fixed comment it out?

--- testing/pluto/ikev2-child-rekey-07-deadlock/westrun.sh
+++ testing/pluto/ikev2-child-rekey-07-deadlock/westrun.sh
@@ -1,19 +1,25 @@
 # IKE: #1 CHILD: #2, #3, #4, and #5
 ipsec auto --up west-east
+
 # CHILD #2->#6
 ipsec whack --rekey-ipsec --name west-east/1x0 --async
+
 # CHILD #3->#7
 ipsec whack --rekey-ipsec --name west-east/2x0 --async
+
 # CHILD #4->#8
 ipsec whack --rekey-ipsec --name west-east/3x0 --async
+
 # CHILD #5->#9
 ipsec whack --rekey-ipsec --name west-east/4x0 --async
-sleep 45
-# state #1 in STATE_V2_ESTABLISHED_IKE_SA, and #6, #7, #8 and #9 in STATE_V2_ESTABLISHED_CHILD_SA
-# anything other state is a sign of regression
-ipsec showstates
-# this is complex grep line susceptible to changes to log lines.
-# until we find better one keep this.
-# May be once the bug is fixed comment it out?
-grep -E  'Message ID:|emit IKEv2 Delete Payload|exchange type:|\*\*emit ISAKMP Message|\*\*parse ISAKMP Message' /tmp/pluto.log | sed -e 's/ (.*/ (..)/'
-echo done
+
+# state #1 in STATE_V2_ESTABLISHED_IKE_SA, and #6, #7, #8 and #9 in
+# STATE_V2_ESTABLISHED_CHILD_SA anything other state is a sign of
+# regression
+../../guestbin/wait-for.sh --match '#6:' -- ipsec trafficstatus
+../../guestbin/wait-for.sh --match '#7:' -- ipsec trafficstatus
+../../guestbin/wait-for.sh --match '#8:' -- ipsec trafficstatus
+../../guestbin/wait-for.sh --match '#9:' -- ipsec trafficstatus
+
+../../guestbin/wait-for.sh --no-match '#5:' -- ipsec trafficstatus
+
