ec4f90717c788033c25b9285322ed9765b6d3778
Paul Wouters <paul.wouters@aiven.io>
2023-12-29 21:10:07 -0500

    testing: fix interop-ikev2-strongswan-14-delete-sa-shared
    This never worked before because we used "strongswan down conn"
    instead of "strongswan down conn{1}". The first deletes the IKE SA,
    the latter deletes the Child SA.
    
    The console output of strongswan confirms libreswan's delete response.

--- testing/pluto/interop-ikev2-strongswan-14-delete-sa-shared/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-14-delete-sa-shared/westrun.sh
@@ -1,8 +1,8 @@
 strongswan up westnet-eastnet-ikev2
 strongswan up westnet-eastnet2-ikev2
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-strongswan down westnet-eastnet-ikev2
+strongswan down "westnet-eastnet-ikev2{1}"
 sleep 1
-strongswan down westnet-eastnet2-ikev2
+strongswan down "westnet-eastnet2-ikev2{2}"
 sleep 3
 echo done
