a8c9ecb14031191c328163a12acf4d4d6307a439
Paul Wouters <paul.wouters@aiven.io>
2023-12-29 20:01:04 -0500

    testing: delete freeswan era multinet-03
    depends on too many timers, dates to uml/freeswan days

--- testing/pluto/multinet-03/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-subnets
-ipsec status | grep westnet-eastnet
-echo "initdone"
--- testing/pluto/multinet-03/westrun.sh
+++
@@ -1,8 +0,0 @@
-ipsec auto --up westnet-eastnet-subnets
-ipsec whack --trafficstatus | sort
-# rekey
-sleep 5
-ipsec auto --up westnet-eastnet-subnets
-ipsec whack --trafficstatus | sort
-ipsec showstates | sort
-echo done
