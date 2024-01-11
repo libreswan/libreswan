5db57ccd49f1c7e7e91148cf7b4520e2950df6cf
Andrew Cagney <cagney@gnu.org>
2024-01-10 21:01:29 -0500

    testing: update addconn-37-nic-offload, add to TESTLIST

--- testing/pluto/addconn-37-nic-offload/west.sh
+++ testing/pluto/addconn-37-nic-offload/west.sh
@@ -10,17 +10,16 @@ ipsec add addconn--nic-offload=auto
 ipsec add addconn--nic-offload=packet
 ipsec add addconn--nic-offload=crypto
 
-ipsec whack --name whack                                           --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload        --nic-offload         --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload=no     --nic-offload=no      --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload=yes    --nic-offload=yes     --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload=auto   --nic-offload=auto    --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload=packet --nic-offload=packet  --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload=crypto --nic-offload=crypto  --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack                                           --transport --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--nic-offload-x      --nic-offload x       --transport --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--nic-offload=no     --nic-offload=no      --transport --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--nic-offload=yes    --nic-offload=yes     --transport --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--nic-offload=auto   --nic-offload=auto    --transport --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--nic-offload=packet --nic-offload=packet  --transport --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--nic-offload=crypto --nic-offload=crypto  --transport --host 1.2.3.4 --to --host 5.6.7.8
 
 ipsec whack --name whack--passthrough                                            --pass --host 1.2.3.4 --to --host 5.6.7.8
 
-ipsec whack --name whack--passthrough--nic-offload         --nic-offload         --pass --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--passthrough--nic-offload=no      --nic-offload=no      --pass --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--passthrough--nic-offload=yes     --nic-offload=yes     --pass --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--passthrough--nic-offload=auto    --nic-offload=auto    --pass --host 1.2.3.4 --to --host 5.6.7.8
