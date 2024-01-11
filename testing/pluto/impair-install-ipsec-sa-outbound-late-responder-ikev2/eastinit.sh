f9f602398ecd0e99c662f3edfa7246ba1993cd75
Andrew Cagney <cagney@gnu.org>
2024-01-11 13:05:39 -0500

    testing: update impair-install-ipsec-sa-* tests
    ref #1510

--- testing/pluto/impair-install-ipsec-sa-outbound-late-responder-ikev2/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-ipv4-psk-ikev2
-ipsec whack --impair install_ipsec_sa_outbound_late
-echo "initdone"
