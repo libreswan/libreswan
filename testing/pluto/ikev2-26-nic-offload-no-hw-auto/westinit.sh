1d09af8cde61f12db1826b427d76360c9faf9812
Paul Wouters <paul.wouters@aiven.io>
2024-01-16 10:13:45 -0500

    testing: remove ikev2-26-nic-offload-no-hw-auto
    the option is no longer supported.

--- testing/pluto/ikev2-26-nic-offload-no-hw-auto/westinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add west-east-transport
-ipsec whack --impair suppress_retransmits
-echo "initdone"
--- testing/pluto/ikev2-26-nic-offload-no-hw-auto/westrun.sh
+++
@@ -1,5 +0,0 @@
-ipsec auto --up west-east-transport
-# should be no crypto hardware options in state
-ip xfrm state
-ip xfrm policy
-echo done
