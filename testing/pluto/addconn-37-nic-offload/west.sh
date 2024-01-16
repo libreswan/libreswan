fc3013aaf90a54ef1f1321c89be30091bcb187c3
Paul Wouters <paul.wouters@aiven.io>
2024-01-16 10:15:25 -0500

    testing: update addconn-37-nic-offload

--- testing/pluto/addconn-37-nic-offload/west.sh
+++ testing/pluto/addconn-37-nic-offload/west.sh
@@ -6,7 +6,6 @@ ipsec add addconn
 
 ipsec add addconn--nic-offload=no
 ipsec add addconn--nic-offload=yes
-ipsec add addconn--nic-offload=auto
 ipsec add addconn--nic-offload=packet
 ipsec add addconn--nic-offload=crypto
 
@@ -14,7 +13,6 @@ ipsec whack --name whack                                           --transport -
 ipsec whack --name whack--nic-offload-x      --nic-offload x       --transport --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--nic-offload=no     --nic-offload=no      --transport --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--nic-offload=yes    --nic-offload=yes     --transport --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--nic-offload=auto   --nic-offload=auto    --transport --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--nic-offload=packet --nic-offload=packet  --transport --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--nic-offload=crypto --nic-offload=crypto  --transport --host 1.2.3.4 --to --host 5.6.7.8
 
@@ -22,7 +20,6 @@ ipsec whack --name whack--passthrough                                           
 
 ipsec whack --name whack--passthrough--nic-offload=no      --nic-offload=no      --pass --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--passthrough--nic-offload=yes     --nic-offload=yes     --pass --host 1.2.3.4 --to --host 5.6.7.8
-ipsec whack --name whack--passthrough--nic-offload=auto    --nic-offload=auto    --pass --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--passthrough--nic-offload=packet  --nic-offload=packet  --pass --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--passthrough--nic-offload=crypto  --nic-offload=crypto  --pass --host 1.2.3.4 --to --host 5.6.7.8
 
