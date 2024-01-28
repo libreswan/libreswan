63ca6bc22b67d9dc5e4827c201b3f5b3fc5a114b
Andrew Cagney <cagney@gnu.org>
2024-01-28 16:54:30 -0500

    testing: add --timeout to wait-for-pluto.sh

--- testing/guestbin/wait-for-pluto.sh
+++ testing/guestbin/wait-for-pluto.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-../../guestbin/wait-for.sh --match "$1" -- cat /tmp/pluto.log
+if test $# -eq 0 ; then
+    cat <<EOF
+Usage:
+  $0 [ --timeout <seconds> ] <match>
+EOF
+fi
+
+timeout=
+while test $# -gt 0 ; do
+    case $1 in
+	--timeout )
+	    timeout="--timeout $2"
+	    shift
+	    shift
+	    ;;
+	* )
+	    pattern="$1"
+	    shift
+	    ;;
+    esac
+done
+
+../../guestbin/wait-for.sh ${timeout} --match "${pattern}" -- cat /tmp/pluto.log
