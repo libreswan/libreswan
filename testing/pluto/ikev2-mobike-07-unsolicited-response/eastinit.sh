99b3af3aa96140217fd03517c1d7514e18ddd789
Andrew Cagney <cagney@gnu.org>
2024-02-07 07:30:29 -0500

    testing: add ikev2-mobike-07-unsolicited-response

---
+++ testing/pluto/ikev2-mobike-07-unsolicited-response/eastinit.sh
@@ -0,0 +1,6 @@
+/testing/guestbin/swan-prep
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec add westnet-eastnet-ipv4-psk-ikev2
+ipsec whack --impair add_v2_notification:MOBIKE_SUPPORTED
+echo "initdone"
