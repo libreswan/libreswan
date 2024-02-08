203d39e24fc192897529d39974d6605cbcd2e86a
Andrew Cagney <cagney@gnu.org>
2024-02-08 14:35:27 -0500

    testing: update interop-ikev2-strongswan-38-digsig-impair
    use --impair ignore_v2_notification:SIGNATURE_HASH_ALGORITHMS

--- testing/pluto/interop-ikev2-strongswan-38-digsig-impair/eastinit.sh
+++ testing/pluto/interop-ikev2-strongswan-38-digsig-impair/eastinit.sh
@@ -2,5 +2,5 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-ipsec whack --impair ignore_v2n_signature_hash_algorithms
+ipsec whack --impair ignore_v2_notification:SIGNATURE_HASH_ALGORITHMS
 echo "initdone"
