49f9f8c483ff8297e581e23b02202d6aee9ab790
Andrew Cagney <cagney@gnu.org>
2024-02-09 15:12:45 -0500

    testing: expect leftupdown= and leftsubnet= to DTRT
    For the first, a disabled updown script, and the second a pluto
    error.

--- testing/pluto/addconn-16-empty-config-option/west.sh
+++ testing/pluto/addconn-16-empty-config-option/west.sh
@@ -1,9 +1,9 @@
 /testing/guestbin/swan-prep
 ipsec start
 ../../guestbin/wait-until-pluto-started
-# this should add, subnet= silently ignored
+# this should fail, subnet= rejected by pluto
 ipsec auto --add first
-# now append a second conn; should also add
+# now append a second conn; still rejected by pluto
 echo 'conn second' >> /etc/ipsec.conf
 echo '        left=2.3.4.5' >> /etc/ipsec.conf
 echo '        right=5.6.7.8' >> /etc/ipsec.conf
