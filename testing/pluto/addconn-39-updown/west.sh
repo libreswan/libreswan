e3a69ff42e41a4fc6272c50017564425ea31f130
Andrew Cagney <cagney@gnu.org>
2024-02-09 11:09:17 -0500

    testing: add addconn-39-updown good
    The test demonstrates existing behaviour, which is pretty strange:
    - unoriented interfaces only show left, and as my_
    - never-negotiate interfaces get an updown script
    - leftupdown= is ignored?!?
    See:
      "passthrough": route-client output: need at least a destination address #1607
      how can an unoriented interface have my_updown #1611

---
+++ testing/pluto/addconn-39-updown/west.sh
@@ -0,0 +1,25 @@
+/testing/guestbin/swan-prep
+ipsec start
+../../guestbin/wait-until-pluto-started
+
+ipsec add addconn
+
+ipsec add addconn--leftupdown=
+ipsec add addconn--leftupdown=quotes
+
+ipsec add addconn--leftupdown=%disabled
+ipsec add addconn--leftupdown=my-updown
+ipsec add addconn--leftupdown=left
+
+ipsec add addconn--rightupdown=right
+
+ipsec add addconn--type=passthrough
+ipsec add addconn--type=passthrough--leftupdown=left
+
+ipsec whack --name whack                                      --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--updown=from  --updown=from          --host 1.2.3.4 --to --host 5.6.7.8
+
+ipsec whack --name whack--pass               --pass                --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--pass--updown=from  --pass --updown=from  --host 1.2.3.4 --to --host 5.6.7.8
+
+ipsec connectionstatus | sed -n -e 's/\(^[^:]*:\).* \([a-z_]*updown=[^;]*\);.*/\1 \2/p' | sort
