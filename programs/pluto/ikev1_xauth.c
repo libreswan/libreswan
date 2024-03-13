ffd05a73342197c5a91e23c5f78a893568cb9cb0
Andrew Cagney <cagney@gnu.org>
2024-03-12 23:19:26 -0400

    ikev1: in xauth_client_resp() don't double close message

--- programs/pluto/ikev1_xauth.c
+++ programs/pluto/ikev1_xauth.c
@@ -2186,8 +2186,7 @@ static stf_status xauth_client_resp(struct state *st,
 
 	fixup_xauth_hash(st, &hash_fixup, rbody->cur);
 
-	if (!ikev1_close_message(rbody, st) ||
-	    !ikev1_close_and_encrypt_message(rbody, st))
+	if (!ikev1_close_and_encrypt_message(rbody, st))
 		return STF_INTERNAL_ERROR;
 
 	return STF_OK;
