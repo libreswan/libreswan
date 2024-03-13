b5e87ba3b26c98e0435b6179e23b156c6a8e090a
Andrew Cagney <cagney@gnu.org>
2024-03-13 13:34:21 -0400

    ikev1: in modecfg_send_request() drop message close

--- programs/pluto/ikev1_xauth.c
+++ programs/pluto/ikev1_xauth.c
@@ -744,12 +744,6 @@ stf_status modecfg_send_request(struct state *st)
 	}
 
 	fixup_xauth_hash(st, &hash_fixup, rbody.cur);
-
-	if (!ikev1_close_message(&rbody, st))
-		return STF_INTERNAL_ERROR;
-
-	close_output_pbs(&reply);
-
 	init_phase2_iv(st, &st->st_v1_msgid.phase15);
 
 	if (!ikev1_close_and_encrypt_message(&rbody, st))
