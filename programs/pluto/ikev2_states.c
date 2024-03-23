8c1b366da869afbcb418ead2aea306793b687e11
Andrew Cagney <cagney@gnu.org>
2024-03-23 10:31:37 -0400

    ikev2: fix typo, STATE_V2_PARENT_R_EAP is "sent EAP response"

--- programs/pluto/ikev2_states.c
+++ programs/pluto/ikev2_states.c
@@ -86,7 +86,7 @@ struct finite_state v2_states[] = {
 	S(STATE_V2_PARENT_I1, "sent IKE_SA_INIT request", CAT_HALF_OPEN_IKE_SA),
 	S(STATE_V2_PARENT_R0, "processing IKE_SA_INIT request", CAT_HALF_OPEN_IKE_SA),
 	S(STATE_V2_PARENT_R1, "sent IKE_SA_INIT (or IKE_INTERMEDIATE) response", CAT_HALF_OPEN_IKE_SA, .v2.secured = true),
-	S(STATE_V2_PARENT_R_EAP, "sent EAP request", CAT_OPEN_IKE_SA, .v2.secured = true),
+	S(STATE_V2_PARENT_R_EAP, "sent EAP response", CAT_OPEN_IKE_SA, .v2.secured = true),
 
 	/*
 	 * All IKEv1 MAIN modes except the first (half-open) and last
