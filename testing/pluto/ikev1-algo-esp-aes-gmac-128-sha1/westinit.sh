04a1e94bce718e3c18ade532560ae36759553c8b
Andrew Cagney <cagney@gnu.org>
2024-03-27 10:42:49 -0400

    testing: add ikev1-algo-esp-aes-gmac-128-sha1

---
+++ testing/pluto/ikev1-algo-esp-aes-gmac-128-sha1/westinit.sh
@@ -0,0 +1,14 @@
+/testing/guestbin/swan-prep
+# confirm that the network is alive
+../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
+# ensure that clear text does not get through
+iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
+iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+# confirm clear text does not get through
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec whack --impair suppress-retransmits
+ipsec whack --impair proposal-parser
+ipsec auto --add westnet-eastnet-null
+echo "initdone"
