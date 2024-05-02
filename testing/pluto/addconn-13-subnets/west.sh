b1634f968cbafbae3dd02df46e6cf88d8de2f4af
Andrew Cagney <cagney@gnu.org>
2024-05-01 22:14:41 -0400

    testing: update addconn-13-subnets
    test subnet= and subnets=

--- testing/pluto/addconn-13-subnets/west.sh
+++ testing/pluto/addconn-13-subnets/west.sh
@@ -2,15 +2,23 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 
-ipsec auto --add ss1-ss1
-ipsec auto --add ss1-ss2
-ipsec auto --add ss2-ss1
-ipsec auto --add ss2-ss2
+ipsec auto --add s0-s1
+ipsec auto --add s1-s1
+ipsec auto --add s1-s2
+ipsec auto --add s2-s1
+ipsec auto --add s2-s2
 
+ipsec auto --add s0-ss1
 ipsec auto --add s1-ss1
 ipsec auto --add s1-ss2
 ipsec auto --add s2-ss1
 ipsec auto --add s2-ss2
+
+ipsec auto --add ss0-ss1
+ipsec auto --add ss1-ss1
+ipsec auto --add ss1-ss2
+ipsec auto --add ss2-ss1
+ipsec auto --add ss2-ss2
 
 ipsec auto --add s1ss1-s1
 ipsec auto --add s1ss2-s1
