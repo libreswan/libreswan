f665a576724166fe894a0548fc54e1feb3403acb
Wolfgang Nothdurft <wolfgang@linogate.de>
2024-05-06 13:53:57 +0200

    testing: add test for multiple tunnels with xfrmi, mark-out and metric set
    This test verifies that routes in the routing table 50 are not deleted by other states when using multiple tunnels with xfrmi, mark-out and metric to the same peer.
    
    That can happen if you have a subnet and a gateway tunnel to the same peer without using different metric settings.
    
    north #
    ipsec briefconnectionstatus
    192.1.3.33 <==> 192.1.2.23      from 192.1.3.33 to 192.1.2.23 (84B/84B) north-east-gw, reqid=16388
    192.1.3.33 <==> 192.0.21.0/24   from 192.1.3.33 to 192.1.2.23 (84B/84B) north-east-sn, reqid=16392
    # Total IPsec connections: loaded 2, active 2
    
    north #
    ip r
    default via 192.1.3.254 dev eth1
    192.0.3.0/24 dev eth0 proto kernel scope link src 192.0.3.254
    192.0.21.0/24 dev ipsec1 scope link metric 2050
    192.1.2.23 dev ipsec1 scope link metric 2049
    192.1.3.0/24 dev eth1 proto kernel scope link src 192.1.3.33
    
    north #
    ip r list table 50
    192.1.2.23 via 192.1.3.254 dev eth1 metric 2049
    192.1.2.23 via 192.1.3.254 dev eth1 metric 2050
    
    north #
    ip ru
    0:      from all lookup local
    100:    from all to 192.1.2.23 fwmark 0x1 lookup 50
    100:    from all to 192.0.21.0/24 fwmark 0x1 lookup 50
    32766:  from all lookup main
    32767:  from all lookup default

---
+++ testing/pluto/ikev2-xfrmi-13-multiple-tunnels-metric/northinit.sh
@@ -0,0 +1,7 @@
+ip addr add 192.1.3.33/24 dev eth1 2>/dev/null
+/testing/guestbin/swan-prep
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec auto --add north-east-gw
+ipsec auto --add north-east-sn
+echo "initdone"
