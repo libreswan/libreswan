ab5f8c70a4366b1f51301d4f179885c850519e71
Andrew Cagney <cagney@gnu.org>
2024-07-29 16:16:09 -0400

    testing: add ikev2-xfrmi-15-interface-ip-order
    tries <<ip link>> and <<ipsec add>> in various orders
    
    ref #1771 after ip link add ipsec1; ipsec add ... connection becomes unloadable

---
+++ testing/pluto/ikev2-xfrmi-15-interface-ip-order/02-west-init.sh
@@ -0,0 +1,4 @@
+/testing/guestbin/swan-prep --46
+ipsec start
+../../guestbin/wait-until-pluto-started
+echo "initdone"
---
+++ testing/pluto/ikev2-xfrmi-15-interface-ip-order/03-west-run.sh
@@ -0,0 +1,38 @@
+#
+# valid device; <<ip link>> is before <<ipsec add>>
+#
+
+ip link add dev ipsec1 type xfrm dev eth1 if_id 0x1
+ip addr add 192.0.1.251/24 dev ipsec1
+ip addr add 2001:db8:0:1::251/64 dev ipsec1
+
+# won't load!?!
+ipsec add westnet4-eastnet4
+ipsec add westnet6-eastnet6
+
+# should still be there
+ip --color=never link show ipsec1
+ip link del dev ipsec1
+ip --color=never link show ipsec1
+
+#
+# valid device; <<ipsec add>> is before <<ip link>>
+#
+
+ipsec add westnet4-eastnet4
+ipsec add westnet6-eastnet6
+
+ip link add dev ipsec1 type xfrm dev eth1 if_id 0x1
+ip addr add 192.0.1.251/24 dev ipsec1
+ip addr add 2001:db8:0:1::251/64 dev ipsec1
+
+ipsec up westnet4-eastnet4
+ipsec up westnet6-eastnet6
+ipsec delete westnet4-eastnet4
+ipsec delete westnet6-eastnet6
+
+ip --color=never link show ipsec1
+ip link del dev ipsec1
+ip --color=never link show ipsec1
+
+echo done
