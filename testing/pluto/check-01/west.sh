7311d4d746dc071a542496c86462209a32f68595
Andrew Cagney <cagney@gnu.org>
2024-07-30 16:59:01 -0400

    testing: update check-01 - run under valgrind
    fix #1768 run test utilities under valgrind

--- testing/pluto/check-01/west.sh
+++ testing/pluto/check-01/west.sh
@@ -10,19 +10,19 @@
 # $ ./OBJ.linux.x86_64/testing/enumcheck/enumcheck > testing/enumcheck/OUTPUT.enumcheck.txt
 # $ git diff
 
-ipsec _enumcheck > OUTPUT/enumcheck.out || echo "Enum check barfed"
+valgrind --quiet $(ipsec -n _enumcheck) > OUTPUT/enumcheck.out || echo "Enum check barfed"
 cmp ../../programs/_enumcheck/OUTPUT.enumcheck.txt OUTPUT/enumcheck.out || echo "Does the file OUTPUT.enumcheck.txt need updating? See description.txt"
 
 # other checks
 
-ipsec _jambufcheck > /dev/null || echo failed
-ipsec _timecheck > /dev/null || echo failed
-ipsec _hunkcheck > /dev/null || echo failed
-ipsec _dncheck > /dev/null || echo failed
-ipsec _keyidcheck > /dev/null || echo failed
-ipsec _asn1check > /dev/null || echo failed
-ipsec _vendoridcheck > /dev/null || echo failed
-ipsec _ttodatacheck -r
+valgrind --quiet $(ipsec -n _jambufcheck) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _timecheck) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _hunkcheck) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _dncheck) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _keyidcheck) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _asn1check) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _vendoridcheck) > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _ttodatacheck -r)
 
 # Need to disable DNS tests; localhost is ok
-ipsec _ipcheck --dns=hosts-file > /dev/null || echo failed
+valgrind --quiet $(ipsec -n _ipcheck --dns=hosts-file) > /dev/null || echo failed
