4d5cde8227c6a639c7d15d67fb7be702ff63f086
Andrew Cagney <cagney@gnu.org>
2024-08-08 17:44:32 -0400

    FIPS: use  once^count++ for IKEv2 CTR wire_iv
    like AEAD (except for AEAD this is hidden by NSS)
    
    ref #1743 IKEv2 in CTR mode uses PK11_Encrypt(), should it use PK11_CipherOp()

--- lib/libswan/ike_alg_encrypt_nss_ctr_ops.c
+++ lib/libswan/ike_alg_encrypt_nss_ctr_ops.c
@@ -35,6 +35,34 @@
 #include "ike_alg_encrypt_ops.h"
 #include "crypt_cipher.h"
 #include "rnd.h"
+#include "hunk.h"
+
+struct cipher_op_context {
+	uint64_t v2_wire_iv;
+	uint64_t v2_wire_count;
+};
+
+static struct cipher_op_context *cipher_op_context_create_ctr_nss(const struct encrypt_desc *cipher UNUSED,
+								  enum cipher_op op UNUSED,
+								  enum cipher_iv_source iv_source,
+								  PK11SymKey *symkey UNUSED,
+								  shunk_t salt UNUSED,
+								  struct logger *logger)
+{
+	struct cipher_op_context *context = alloc_thing(struct cipher_op_context, __func__);
+	switch (iv_source) {
+	case FILL_WIRE_IV:
+		context->v2_wire_iv = get_rnd_uintmax();
+		context->v2_wire_count = 0;
+		ldbgf(DBG_CRYPT, logger, "%s() initial wire_iv %"PRIx64" count %"PRIu64,
+		      __func__, context->v2_wire_iv, context->v2_wire_count);
+		break;
+	case USE_WIRE_IV:
+	case USE_IKEv1_IV:
+		break;
+	}
+	return context;
+}
 
 static void cipher_op_ctr_nss(const struct encrypt_desc *cipher,
 			      struct cipher_op_context *context,
@@ -47,8 +75,10 @@ static void cipher_op_ctr_nss(const struct encrypt_desc *cipher,
 			      struct crypt_mac *ikev1_iv/*possbly-NULL*/,
 			      struct logger *logger)
 {
-	ldbgf(DBG_CRYPT, logger, "%s() enter %s %p",
-	      __func__, cipher->common.fqn, context);
+	ldbgf(DBG_CRYPT, logger, "%s() enter %s %p %s wire_iv %"PRIx64" count %"PRIu64,
+	      __func__, cipher->common.fqn, context,
+	      str_cipher_iv_source(iv_source),
+	      context->v2_wire_iv, context->v2_wire_count);
 
 	passert(sym_key);
 	if (sym_key == NULL) {
@@ -57,6 +87,7 @@ static void cipher_op_ctr_nss(const struct encrypt_desc *cipher,
 
 	CK_AES_CTR_PARAMS ctr_params = {
 		.ulCounterBits = sizeof(uint32_t) * 8, /* Per RFC 3686 */
+		.cb = {0}, /* be explicit */
 	};
 
 	switch (iv_source) {
@@ -70,7 +101,12 @@ static void cipher_op_ctr_nss(const struct encrypt_desc *cipher,
 	case FILL_WIRE_IV:
 		PASSERT(logger, ikev1_iv == NULL);
 		PASSERT(logger, salt.len + wire_iv.len + ctr_params.ulCounterBits / 8 == sizeof(ctr_params.cb));
-		fill_rnd_chunk(wire_iv);
+		PASSERT(logger, wire_iv.len > 0);
+		PASSERT(logger, context->v2_wire_iv != 0);
+		PASSERT(logger, sizeof(context->v2_wire_iv) >= wire_iv.len);
+		/* like AEAD, use RND ^ COUNT++ */
+		hton_chunk(context->v2_wire_iv ^ context->v2_wire_count, wire_iv);
+		context->v2_wire_count++;
 		memcpy(ctr_params.cb, salt.ptr, salt.len);
 		memcpy(ctr_params.cb + salt.len, wire_iv.ptr, wire_iv.len);
 		ctr_params.cb[sizeof(ctr_params.cb) - 1] = 1;
@@ -82,6 +118,10 @@ static void cipher_op_ctr_nss(const struct encrypt_desc *cipher,
 		break;
 	}
 
+	LDBGP_JAMBUF(DBG_CRYPT, logger, buf) {
+		jam(buf, "ctr_param: count %lu iv ", ctr_params.ulCounterBits);
+		jam_hex_bytes(buf, ctr_params.cb, sizeof(ctr_params.cb));
+	}
 
 	SECItem param;
 	param.type = siBuffer;
@@ -146,6 +186,13 @@ static void cipher_op_ctr_nss(const struct encrypt_desc *cipher,
 	ldbgf(DBG_CRYPT, logger, "do_aes_ctr: exit");
 }
 
+static void cipher_op_context_destroy_ctr_nss(struct cipher_op_context **context,
+					      struct logger *logger)
+{
+	ldbgf(DBG_CRYPT, logger, "%s()", __func__);
+	pfreeany(*context);
+}
+
 static void cipher_check_ctr_nss(const struct encrypt_desc *cipher,
 				 struct logger *logger)
 {
@@ -156,5 +203,7 @@ static void cipher_check_ctr_nss(const struct encrypt_desc *cipher,
 const struct encrypt_ops ike_alg_encrypt_nss_ctr_ops = {
 	.backend = "NSS(CTR)",
 	.cipher_check = cipher_check_ctr_nss,
+	.cipher_op_context_create = cipher_op_context_create_ctr_nss,
 	.cipher_op_normal = cipher_op_ctr_nss,
+	.cipher_op_context_destroy = cipher_op_context_destroy_ctr_nss,
 };
