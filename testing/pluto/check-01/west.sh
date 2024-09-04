c5c7ae078b948b19e18039b962b8dfd5a3de2764
Andrew Cagney <cagney@gnu.org>
2024-09-04 15:39:34 -0400

    testing: move enumcheck to check-02-enumcheck
    Drop diff of OUTPUT.enumcheck.txt.
    
    Instead let enumcheck write to stdout where test framework
    will compare it to west.console.check
    
    Hopefully this is more intuitive.

--- testing/pluto/check-01/west.sh
+++ testing/pluto/check-01/west.sh
@@ -1,20 +1,5 @@
 /testing/guestbin/swan-prep
 
-# Check the enum name tables
-#
-# Use CMP not DIFF.  When there's a difference, the output from diff
-# mixed in with the output from comparing console.txt files looks too
-# much like console.txt needs updating when it doesn't.
-#
-# To update OUTPUT.enumcheck.txt run something like:
-# $ ./OBJ.linux.x86_64/testing/enumcheck/enumcheck > testing/enumcheck/OUTPUT.enumcheck.txt
-# $ git diff
-
-valgrind --quiet $(ipsec -n _enumcheck) > OUTPUT/enumcheck.out || echo "Enum check barfed"
-cmp ../../programs/_enumcheck/OUTPUT.enumcheck.txt OUTPUT/enumcheck.out || echo "Does the file OUTPUT.enumcheck.txt need updating? See description.txt"
-
-# other checks
-
 valgrind --quiet $(ipsec -n _jambufcheck) > /dev/null || echo failed
 valgrind --quiet $(ipsec -n _timecheck) > /dev/null || echo failed
 valgrind --quiet $(ipsec -n _hunkcheck) > /dev/null || echo failed
