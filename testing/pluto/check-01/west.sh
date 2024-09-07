0e1090bbc9cde31da27d01745733dd165cada63f
Andrew Cagney <cagney@gnu.org>
2024-09-07 17:28:20 -0400

    testing: separate out vendoridcheck
    like for enumcheck dump to stdout

--- testing/pluto/check-01/west.sh
+++ testing/pluto/check-01/west.sh
@@ -6,7 +6,6 @@ valgrind --quiet $(ipsec -n _hunkcheck) > /dev/null || echo failed
 valgrind --quiet $(ipsec -n _dncheck) > /dev/null || echo failed
 valgrind --quiet $(ipsec -n _keyidcheck) > /dev/null || echo failed
 valgrind --quiet $(ipsec -n _asn1check) > /dev/null || echo failed
-valgrind --quiet $(ipsec -n _vendoridcheck) > /dev/null || echo failed
 valgrind --quiet $(ipsec -n _ttodatacheck -r)
 
 # Need to disable DNS tests; localhost is ok
---
+++ testing/pluto/check-03-vendoridcheck/west.sh
@@ -0,0 +1,3 @@
+/testing/guestbin/swan-prep
+
+valgrind --quiet $(ipsec -n _vendoridcheck)
