0a16706b01a76305dff319b905c8c7623be1b82e
Andrew Cagney <cagney@gnu.org>
2024-09-09 10:35:44 -0400

    testing: update ikev2-mobike-08-nat-expire-liveness-east
    Document and demonstrate how the configuration is broken:
    
      When ROAD (behind the NAT) sends traffic, per the RFC,
      EAST concludes that the SA is still established and skips
      liveness probes.
    
    ref #1728 Periodic Failure with IPSec Sessions Dropping Out - Require Restart

--- testing/pluto/ikev2-mobike-08-nat-expire-liveness-east/06-east-liveness.sh
+++
@@ -1,3 +0,0 @@
-../../guestbin/wait-for-pluto.sh --match '10 second timeout exceeded'
-../../guestbin/wait-for-pluto.sh --match 'ESP traffic information'
-../../guestbin/wait-for-pluto.sh --match 'deleting connection instance with peer'
