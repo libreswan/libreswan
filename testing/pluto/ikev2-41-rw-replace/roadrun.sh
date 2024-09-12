bef79bba6002d8b233f0344d74b47a53129217ae
Andrew Cagney <cagney@gnu.org>
2024-09-12 16:11:33 -0400

    testing: update ikev2-41-rw-replace again
    remove source line references in output

--- testing/pluto/ikev2-41-rw-replace/roadrun.sh
+++ testing/pluto/ikev2-41-rw-replace/roadrun.sh
@@ -6,7 +6,7 @@ ipsec status
 ../../guestbin/wait-for.sh --timeout 120 --match '^".*#3: initiator rekeyed IKE SA #1' -- cat /tmp/pluto.log
 ../../guestbin/wait-for-pluto.sh '^".*#1: deleting IKE SA'
 ../../guestbin/ping-once.sh --up -I 192.0.2.100 192.1.2.23
-grep -E  'EVENT_v2_EXPIRE|EVENT_v2_REPLACE' OUTPUT/road.pluto.log | grep '#' | sed -e 's/ timeout in [1-9][^ ]* / timeout in N.N /' -e 's/@0x[0-9a-f]*/@0xXXX/'
+grep -E  'EVENT_v2_EXPIRE|EVENT_v2_REPLACE' OUTPUT/road.pluto.log | grep '#' | sed -e 's/ timeout in [1-9][^ ]* / timeout in N.N /' -e 's/@0x[0-9a-f]*/@0xXXX/' -e 's/ (.*//'
 : "re-authenticateded. The state number should 3 and 2"
 ipsec status
 echo done
