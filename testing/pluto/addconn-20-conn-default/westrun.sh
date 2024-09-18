7b5b1ab8276689254ec2f6f44867fb0fbf3ba92a
Tuomo Soini <tis@foobar.fi>
2024-09-18 18:25:51 +0300

    testing: fix comments to have correct algorithm

--- testing/pluto/addconn-20-conn-default/westrun.sh
+++ testing/pluto/addconn-20-conn-default/westrun.sh
@@ -1,6 +1,6 @@
 ipsec auto --add first
 ipsec auto --add second
-# conn second should inherit the conn %default values with 3des-md5
+# conn second should inherit the conn %default values with 3des-sha1
 ipsec status |grep "algorithms:"
 # connection should fail to load - don't accept %fromcert without cert
 ipsec auto --add cert-complain
