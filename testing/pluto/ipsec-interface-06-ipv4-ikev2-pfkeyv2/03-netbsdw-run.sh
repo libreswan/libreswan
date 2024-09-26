ba47af37f838252a29f751710108a750c8619180
Andrew Cagney <cagney@gnu.org>
2024-09-26 13:28:34 -0400

    testing: update ipsec-interface-06-ipv4-ikev2-pfkeyv2

--- testing/pluto/ipsec-interface-06-ipv4-ikev2-pfkeyv2/03-netbsdw-run.sh
+++ testing/pluto/ipsec-interface-06-ipv4-ikev2-pfkeyv2/03-netbsdw-run.sh
@@ -2,20 +2,30 @@ ifconfig ipsec1
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
 
+ifconfig ipsec1 create
+ifconfig ipsec1 tunnel 192.1.2.45 192.1.2.23
+ifconfig ipsec1 inet 192.0.1.251/24 192.0.1.251
+ifconfig ipsec1 up
+ifconfig ipsec1
+../../guestbin/ipsec-kernel-state.sh
+../../guestbin/ipsec-kernel-policy.sh
+ifconfig ipsec1 destroy
+
 ipsec up west
 
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
 
-../../guestbin/tcpdump.sh --start -i ipsec1 &
+../../guestbin/tcpdump.sh --start -i ipsec1
 
 ifconfig ipsec1
 ../../guestbin/ping-once.sh --up -I 192.0.1.251 192.0.2.254
+../../guestbin/ping-once.sh --up -I ipsec1 192.0.2.254
 
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
 
 ../../guestbin/tcpdump.sh --stop -i ipsec1
 
-ipsec down west
-ipsec delete west
+# ipsec down west
+# ipsec delete west
