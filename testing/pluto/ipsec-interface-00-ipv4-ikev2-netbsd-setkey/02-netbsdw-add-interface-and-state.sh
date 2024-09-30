bf7f618347703d88d18fb0d821e7d333a7e101d0
Andrew Cagney <cagney@gnu.org>
2024-09-29 23:10:32 -0400

    testing: update ipsec-interface-00-ipv4-ikev2-netbsd-setkey
    magic up the msgid needed to install the SADB

---
+++ testing/pluto/ipsec-interface-00-ipv4-ikev2-netbsd-setkey/02-netbsdw-add-interface-and-state.sh
@@ -0,0 +1,16 @@
+../../guestbin/prep.sh
+
+ifconfig ipsec1 create
+ifconfig ipsec1 -link2
+ifconfig ipsec1 inet tunnel 192.1.2.45 192.1.2.23
+ifconfig ipsec1 inet 192.0.1.251/24 192.0.2.254
+
+ifconfig ipsec1
+../../guestbin/ipsec-kernel-policy.sh
+
+echo 'add 192.1.2.23 192.1.2.45 esp 2345 -m transport -u 16385 -E rijndael-cbc "23--SecureKey-45";' | setkey -c
+echo 'add 192.1.2.45 192.1.2.23 esp 4523 -m transport -u 16386 -E rijndael-cbc "45--SecureKey-23";' | setkey -c
+
+../../guestbin/ipsec-kernel-state.sh
+
+echo added
