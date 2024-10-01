f2e73171be05bf591d24dc48f179ed422cc5a002
Andrew Cagney <cagney@gnu.org>
2024-10-01 13:15:50 -0400

    testing: update ipsec-interface-00-ipv4-ikev2-openbsd-iked
    it now works

--- testing/pluto/ipsec-interface-00-ipv4-ikev2-openbsd-iked/04-openbsdw-ping.sh
+++ testing/pluto/ipsec-interface-00-ipv4-ikev2-openbsd-iked/04-openbsdw-ping.sh
@@ -1,5 +1,4 @@
-# Peer's sec(4) doesn't send back a response?!?
-../../guestbin/ping-once.sh --down -I 192.0.45.1 192.0.23.1
+../../guestbin/ping-once.sh --up -I 192.0.45.1 192.0.23.1
 
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
