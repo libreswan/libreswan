8bf15e55f0be857a87cb93e4b5bea7aa91c49de2
Andrew Cagney <cagney@gnu.org>
2024-10-07 13:01:13 -0400

    crypto: support CHACHA on BSD when available
    (based on SADB_X_AALG_CHACHA20POLY1305 appearing in the header)

--- lib/libswan/ike_alg_encrypt_chacha20_poly1305.c
+++ lib/libswan/ike_alg_encrypt_chacha20_poly1305.c
@@ -32,6 +32,9 @@ const struct encrypt_desc ike_alg_encrypt_chacha20_poly1305 = {
 			[IKEv1_OAKLEY_ID] = -1,
 			[IKEv1_IPSEC_ID] = -1,
 			[IKEv2_ALG_ID] = IKEv2_ENCR_CHACHA20_POLY1305,
+#ifdef SADB_X_AALG_CHACHA20POLY1305
+			[SADB_ALG_ID] = SADB_X_AALG_CHACHA20POLY1305,
+#endif
 		},
 	},
 	.keylen_omitted = true,
