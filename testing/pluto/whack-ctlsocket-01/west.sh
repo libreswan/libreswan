74af21a246d5469db005f9cf6627521181252ea4
Andrew Cagney <cagney@gnu.org>
2024-10-11 13:14:36 -0400

    testing: add whack-ctlsocket-01
    test whack error: duplicated flag "ctlsocket" #1840

---
+++ testing/pluto/whack-ctlsocket-01/west.sh
@@ -0,0 +1,14 @@
+ipsec start
+../../guestbin/wait-until-pluto-started
+
+ipsec -n --briefstatus
+ipsec    --briefstatus
+
+# now for some fun
+ln -s /var/run/pluto/pluto.ctl /tmp/tmp.ctl
+
+ipsec -n --ctlsocket /tmp/tmp.ctl whack --briefstatus
+ipsec    --ctlsocket /tmp/tmp.ctl whack --briefstatus
+
+ipsec -n whack --ctlsocket /tmp/tmp.ctl --briefstatus
+ipsec    whack --ctlsocket /tmp/tmp.ctl --briefstatus
