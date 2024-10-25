b94706b0c55b0f4207ac07cdc3684cf12c6464f0
Andrew Cagney <cagney@gnu.org>
2024-10-25 14:01:40 -0400

    testing web: delete lsw_date2iso_long(), unused

--- testing/web/lsw-date2iso.ts
+++ testing/web/lsw-date2iso.ts
@@ -10,16 +10,3 @@ function lsw_date2iso(d) {
 	return ">>> " + d + " <<<"
     }
 }
-
-// include seconds
-function lsw_date2iso_long(d) {
-    // Force to UTC?
-    if (d && d.toISOString && !isNaN(d)) {
-	d = d.toISOString()
-	d = d.match("([^T]*)T([0-9]*:[0-9]*:[0-9]*)")
-	return d[1] + " " + d[2]
-    } else {
-	console.log("not a date", d, new Error().stack)
-	return ">>> " + d + " <<<"
-    }
-}
