48004e7a2224eccc7608a7d8874b3c0a6d168eb6
Andrew Cagney <cagney@gnu.org>
2024-10-29 13:42:36 -0400

    testing: delete ikev2-52-rw-replace-ping-f
    trying to use a file in OUTPUT/ that doesn't exist

--- testing/pluto/ikev2-52-rw-replace-ping-f/eastinit.sh
+++
@@ -1,5 +0,0 @@
-/testing/guestbin/swan-prep --x509
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add rw-east-pool-x509-ipv4
-echo "initdone"
--- testing/pluto/ikev2-52-rw-replace-ping-f/roadinit.sh
+++
@@ -1,5 +0,0 @@
-/testing/guestbin/swan-prep --x509
-ipsec start
-../../guestbin/wait-until-pluto-started
-ipsec auto --add road-east-x509-ipv4
-echo "initdone"
