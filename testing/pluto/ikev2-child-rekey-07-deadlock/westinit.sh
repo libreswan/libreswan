257686d2de44b1a000d8017fdd2ad47081d06d6a
Andrew Cagney <cagney@gnu.org>
2024-11-04 19:50:03 -0500

    testing: update ikev2-child-rekey-07-deadlock
    use
    - nhelpers=0
    - impair helper_thread_delay:0
    should make it more deterministic

--- testing/pluto/ikev2-child-rekey-07-deadlock/westinit.sh
+++ testing/pluto/ikev2-child-rekey-07-deadlock/westinit.sh
@@ -3,6 +3,7 @@ ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec whack --impair revival
 ipsec whack --impair suppress_retransmits
+ipsec whack --impair helper_thread_delay:0
 ipsec auto --add west-east
 sleep 4
 echo "initdone"
