3a97eca4e9d9497205efee4b996bd43dea71b62a
Andrew Cagney <cagney@gnu.org>
2024-11-11 18:04:56 -0500

    testing web: put first child at start of commit.children[]
    fix #1906 commits.js is creating .children[] reversed

--- testing/web/Commits.js
+++ testing/web/Commits.js
@@ -1,10 +1,10 @@
 class Commits {
-    constructor(commits) {
+    constructor(commits_json) {
 
 	// turn raw json into proper objects, retain (new-to-old)
 	// order
 	let i = 0
-	this.commits = commits.map((json) => {
+ 	this.commits = commits_json.map((json) => {
 	    return new Commit(json, i++)
 	})
 
@@ -15,9 +15,9 @@ class Commits {
 	}
 
 	// fill in parents
-	for (let json of commits) {
-	    let commit = this.hash_to_commit[json.hash]
-	    for (let parent_hash of json.parents) {
+	for (let commit_json of commits_json) {
+	    let commit = this.hash_to_commit[commit_json.hash]
+	    for (let parent_hash of commit_json.parents) {
 		let parent = this.hash_to_commit[parent_hash]
 		if (parent) {
 		    commit.parents.push(parent)
@@ -28,11 +28,10 @@ class Commits {
 	}
 
 	// fill in children
-	for (let commit of this.commits) {
-	    for (let parent of commit.parents) {
-		parent.children.push(commit)
-	    }
-	}
+	//
+	// walk first parent before walking second parent so that
+	// first child back link is to first parent
+	this._fill_children(this.commits[0])
 
 	/* save oldest and and newest based on commit date */
 	this.oldest = this.commits.reduce((oldest, commit) => {
@@ -44,4 +43,27 @@ class Commits {
 
 	console.log("commits", i, this)
     }
+
+    _fill_children(child) {
+	// assume child has at least one parent
+	let branches = []
+	branches.push([child, 0])
+	while (branches.length > 0) {
+	    let [child, level] = branches.pop()
+	    do {
+		if (child.parents.length > level+1) {
+		    branches.push([child, level+1])
+		}
+		let parent = child.parents[level]
+		if (parent.children.includes(child)) {
+		    break
+		}
+		parent.children.push(child)
+		child = parent
+		level = 0
+	    } while (child.parents.length > 0)
+	}
+	return
+    }
+
 }
