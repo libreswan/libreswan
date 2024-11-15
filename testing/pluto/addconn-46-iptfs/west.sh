48ee615e3118e89af0e0bf2eac127b4517cd3021
Andrew Cagney <cagney@gnu.org>
2024-11-15 08:36:21 -0500

    testing: add addconn-46-iptfs
    Note that because the kernel lacks IPTFS and there's no way
    to force behaviour this test coverage is deficient.
    
    see #1881 whack --iptfs=no

---
+++ testing/pluto/addconn-46-iptfs/west.sh
@@ -0,0 +1,23 @@
+/testing/guestbin/swan-prep
+ipsec start
+../../guestbin/wait-until-pluto-started
+
+ipsec add addconn
+ipsec add addconn--iptfs=no
+ipsec add addconn--iptfs=yes
+
+ipsec add addconn--type=passthrough
+ipsec add addconn--type=passthrough--iptfs=no
+ipsec add addconn--type=passthrough--iptfs=yes
+
+ipsec whack --name whack                     --encrypt --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--iptfs      --iptfs     --encrypt --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--iptfs=no   --iptfs=no  --encrypt --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--iptfs=yes  --iptfs=yes --encrypt --host 1.2.3.4 --to --host 5.6.7.8
+
+ipsec whack --name whack--passthrough                     --pass --auth-never --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--passthrough--iptfs      --iptfs     --pass --auth-never --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--passthrough--iptfs=no   --iptfs=no  --pass --auth-never --host 1.2.3.4 --to --host 5.6.7.8
+ipsec whack --name whack--passthrough--iptfs=yes  --iptfs=yes --pass --auth-never --host 1.2.3.4 --to --host 5.6.7.8
+
+ipsec connectionstatus | sed -n -e 's/\(.* policy:\) .*PFS.*/\1 PFS/p' | sort
