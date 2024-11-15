3215d156d6ec7fd6b8153de19c99f6fce2f080df
Andrew Cagney <cagney@gnu.org>
2024-11-14 21:32:06 -0500

    testing web: don't confuse <<#2: ...>> for a comment
    close #1912 terminal.js things #2 ... is a comment

--- testing/web/terminal.js
+++ testing/web/terminal.js
@@ -9,22 +9,37 @@ function terminal(terminal_txt, terminal_id) {
 	.enter()
 	.append("span")
 
+    // ENUM
+    let OUTPUT = 1
+    let COMMENT = 2
+    let COMMAND = 3
+
+    let match = (line, type) => {
+	if (line.match(/^[a-z]+#/)) {
+	    return type == COMMAND
+	}
+	if (line.match(/^#( |$)/)) {
+	    return type == COMMENT
+	}
+	return type == OUTPUT
+    }
+
     // prompt# command
-    td.filter((line) => line.match(/^[a-z]+#/))
+    td.filter((line) => match(line, COMMAND))
 	.selectAll("span")
-	.data((line) => line.split("#"))
+	.data((line) => line.match(/^([^#]*)#(.*)$/).slice(1,3))
 	.enter()
 	.append("span")
 	.attr("class", (d, i) => (i == 0 ? "prompt" : "command"))
 	.text((d, i) => (i == 0 ? d+"#" : d+"\n"))
 
     // output
-    td.filter((line) => !line.match(/^[a-z]*#/))
+    td.filter((line) => match(line, OUTPUT))
 	.attr("class", "output")
 	.text((line) => line+"\n")
 
-    // comments
-    td.filter((line) => line.match(/^#/))
+    // comments; not '#2 ...'
+    td.filter((line) => match(line, COMMENT))
 	.attr("class", "comment")
 	.text((line) => line+"\n")
 
