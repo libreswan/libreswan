bda0917c0fa345e767b105d9adb97b6e3fb4a74b
Andrew Cagney <cagney@gnu.org>
2024-12-11 13:41:48 -0500

    testing: make ikev2-resume-04-instance very distracted

--- testing/pluto/ikev2-resume-04-instance/final.sh
+++ testing/pluto/ikev2-resume-04-instance/final.sh
@@ -1,2 +1,7 @@
 ../../guestbin/ipsec-kernel-state.sh
 ../../guestbin/ipsec-kernel-policy.sh
+
+# on east should see IKE_AUTH switch from distraction to any-east and
+# then the IKE_SESSION_RESUME switch from resume-distraction to
+# any-east.
+grep -e ': switched to ' /tmp/pluto.log
