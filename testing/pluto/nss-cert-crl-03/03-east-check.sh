16a05d2828d67a1b52d531dda39e86c8aebbea65
Andrew Cagney <cagney@gnu.org>
2025-01-16 08:39:51 -0500

    testing: in nss-cert-crl-03 don't expect lurking sha1 cert

--- testing/pluto/nss-cert-crl-03/03-east-check.sh
+++ testing/pluto/nss-cert-crl-03/03-east-check.sh
@@ -6,7 +6,6 @@ ipsec crlutil -L | grep mainca
 # The first fetch, which happens happens after 5s, may or may-not be
 # too quick for the exchange.
 ../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=nic.testing.libreswan.org'
-../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=hashsha1.testing.libreswan.org'
 ../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=north.testing.libreswan.org'
 ../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=road.testing.libreswan.org'
 ../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=east.testing.libreswan.org'
