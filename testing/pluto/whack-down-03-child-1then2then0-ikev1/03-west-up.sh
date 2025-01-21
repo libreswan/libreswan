063a1cfbfab26e19ee8f298cea854b4c2e310193
Andrew Cagney <cagney@gnu.org>
2025-01-21 15:37:59 -0500

    testing: update <<ipsec down>> tests
    - add tests where the IKE SA's connection has no children
      yet the IKE SA does have children (just not its own)
    
      see: #2010 a connection with only an IKE SA, is deleted when it has other children
      close: #2009 ipsec down deleting connection's ISAKMP SA ... (i.e., no Child) not tested
    
    - work around down IKE
    
      see: #2003 an IKE SA for a down connection should delete when it has no children

---
+++ testing/pluto/whack-down-03-child-1then2then0-ikev1/03-west-up.sh
@@ -0,0 +1,7 @@
+ipsec up west-cuckold
+ipsec up west-cuckoo-1
+ipsec up west-cuckoo-2
+
+../../guestbin/ping-once.sh --up 192.0.2.254
+../../guestbin/ping-once.sh --up 192.0.20.254
+ipsec trafficstatus
