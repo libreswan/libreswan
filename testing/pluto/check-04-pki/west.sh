6d98a5e3a240bdcef1e1324fc71896252555a675
Andrew Cagney <cagney@gnu.org>
2025-01-30 13:11:54 -0500

    testing x509: in check-04-pki, import/dump NSS generated certs
    and add NIC, which is a bit exotic with its OCSP EKU

--- testing/pluto/check-04-pki/west.sh
+++ testing/pluto/check-04-pki/west.sh
@@ -1,20 +1,19 @@
 /testing/guestbin/swan-prep --nokeys
-ipsec certutil -A -i /testing/x509/cacerts/mainca.crt -n mainca -t "CT,,"
-ipsec pk12util -W foobar -i /testing/x509/pkcs12/mainca/west.p12
+/testing/x509/import.sh real/mainca/west.all.p12
+/testing/x509/import.sh real/mainca/nic.all.p12
 ipsec certutil -L
 ipsec certutil -L -n mainca | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
 ipsec certutil -L -n west | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
+ipsec certutil -L -n nic | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
 
 /testing/guestbin/swan-prep --nokeys
-ipsec certutil -A -i /testing/x509/cacerts/otherca.crt -n otherca -t "CT,,"
-ipsec pk12util -W foobar -i /testing/x509/pkcs12/otherca/otherwest.p12
+/testing/x509/import.sh real/otherca/otherwest.all.p12
 ipsec certutil -L
 ipsec certutil -L -n otherca | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
 ipsec certutil -L -n otherwest | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
 
 /testing/guestbin/swan-prep --nokeys
-ipsec certutil -A -i /testing/x509/cacerts/badca.crt -n badca -t "CT,,"
-ipsec pk12util -W foobar -i /testing/x509/pkcs12/badca/badwest.p12
+/testing/x509/import.sh real/badca/badwest.all.p12
 ipsec certutil -L
 ipsec certutil -L -n badca | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
 ipsec certutil -L -n badwest | sed -e '/^ *[^a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]:/ d'
