2b674487d431cb0e17e72c94b44c622ffec7ae85
Andrew Cagney <cagney@gnu.org>
2025-01-31 13:58:12 -0500

    testing: update ikev2-mobike-08-nat-expire-liveness-east
    it's relying on a debug log message

--- testing/pluto/ikev2-mobike-08-nat-expire-liveness-east/08-east-liveness.sh
+++ testing/pluto/ikev2-mobike-08-nat-expire-liveness-east/08-east-liveness.sh
@@ -1,6 +1,6 @@
 ../../guestbin/ipsec-kernel-state.sh
 cat /proc/net/xfrm_stat
-# the ping stops liveness
-../../guestbin/wait-for-pluto.sh --match 'recent IPsec traffic' | sed -e 's/ [^ ]* seconds/ NNN seconds/g'
+# the ping stops liveness; this is a debug message!
+../../guestbin/wait-for.sh --match 'recent IPsec traffic' -- cat /tmp/pluto.log | sed -e 's/ [^ ]* seconds/ NNN seconds/g'
 # but no further pings let liveness continue
 ../../guestbin/wait-for-pluto.sh --match '10 second timeout exceeded'
