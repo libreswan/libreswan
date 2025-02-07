5ce120985969cd53caac7fd368c9eaf00b0afec1
Paul Wouters <paul.wouters@aiven.io>
2025-02-06 21:45:58 -0500

    testing: fixup newoe-27-replace-sa-authnull-authnull
    Do not use killall, it doesn't work well using namespaces
    Same for "ipsec restart".

--- testing/pluto/newoe-27-replace-sa-authnull-authnull/roadrun.sh
+++ testing/pluto/newoe-27-replace-sa-authnull-authnull/roadrun.sh
@@ -7,7 +7,8 @@ ipsec whack --shuntstatus
 ipsec whack --trafficstatus
 # prevent delete notify
 ipsec whack --impair send_no_delete
-ipsec restart
+ipsec whack --shutdown
+ipsec start
 ../../guestbin/wait-until-pluto-started
 # give OE policies time to load
 ../../guestbin/wait-for.sh --match 'loaded 10,' -- ipsec auto --status
@@ -19,12 +20,13 @@ ipsec restart
 
 # Now imitate a second independent client to east to show this 2nd
 # client doesn't kill 1st client
-killall -9 pluto
+ipsec whack --impair send_no_delete
+ipsec whack --shutdown
 ../../guestbin/ip.sh address del 192.1.3.209/24 dev eth0
 ../../guestbin/ip.sh address add 192.1.3.210/24 dev eth0
 ../../guestbin/ip.sh route add 0.0.0.0/0 via 192.1.3.254
 # wait on OE to load; give it a different byte count
-ipsec restart
+ipsec start
 ../../guestbin/wait-until-pluto-started
 ../../guestbin/ping-once.sh --forget -I 192.1.3.210 192.1.2.23
 ../../guestbin/wait-for.sh --match private-or-clear -- ipsec whack --trafficstatus
