0a2b425fc1a5f8d7e294ef3f9228ad50e0cdf6c3
Andrew Cagney <cagney@gnu.org>
2025-02-10 17:05:39 -0500

    testing x509: drop --userland strongswan --x509
    replace with explicit cp commands to install the root cert
    and the end's cert+key
    
    However, in several cases, also install the peer's set.

--- testing/pluto/interop-ikev1-strongswan-x509-aggr-12-san-dn-match/westinit.sh
+++ testing/pluto/interop-ikev1-strongswan-x509-aggr-12-san-dn-match/westinit.sh
@@ -1,6 +1,8 @@
-/testing/guestbin/swan-prep --userland strongswan --x509
-# strongswan expects the certs in /etc/strongswan/certs for some reason
-mkdir -p /etc/strongswan/certs
-cp -a /etc/strongswan/ipsec.d/certs/* /etc/strongswan/certs/
+/testing/guestbin/swan-prep --userland strongswan
+
+cp /testing/x509/real/mainca/root.cert /etc/strongswan/ipsec.d/cacerts/mainca.crt
+cp /testing/x509/keys/`hostname`.key /etc/strongswan/ipsec.d/private/
+cp /testing/x509/certs/`hostname`.crt /etc/strongswan/ipsec.d/certs/
+
 ../../guestbin/strongswan-start.sh
 echo "initdone"
--- testing/pluto/interop-ikev2-strongswan-42-initiator-digsig-rsa-pss-384/westinit.sh
+++ testing/pluto/interop-ikev2-strongswan-42-initiator-digsig-rsa-pss-384/westinit.sh
@@ -1,4 +1,11 @@
-/testing/guestbin/swan-prep --userland strongswan --x509
+/testing/guestbin/swan-prep --userland strongswan
+
+cp /testing/x509/real/mainca/root.cert /etc/strongswan/ipsec.d/cacerts/mainca.crt
+cp /testing/x509/keys/`hostname`.key /etc/strongswan/ipsec.d/private/
+cp /testing/x509/certs/`hostname`.crt /etc/strongswan/ipsec.d/certs/
+# why?
+cp /testing/x509/certs/east.crt /etc/strongswan/ipsec.d/certs/
+
 # confirm that the network is alive
 ../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # ensure that clear text does not get through
