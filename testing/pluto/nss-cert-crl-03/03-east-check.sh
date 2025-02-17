a629279ff73e2c7b99ac637a633c691e18d7ce75
Andrew Cagney <cagney@gnu.org>
2025-02-17 15:04:35 -0500

    testing CRLs: use crl-is-{out-of,up-to}-date.crl

--- testing/pluto/nss-cert-crl-03/03-east-check.sh
+++ testing/pluto/nss-cert-crl-03/03-east-check.sh
@@ -1,13 +1,11 @@
 # Check that mainca is present
 ipsec crlutil -L | grep mainca
+
 # DANGER: race.
 #
 # Wait for the CRL fetch helper to be triggered and fetch the CRLs.
 # The first fetch, which happens happens after 5s, may or may-not be
 # too quick for the exchange.
-../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=nic.testing.libreswan.org'
-../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=north.testing.libreswan.org'
-../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=road.testing.libreswan.org'
 ../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=east.testing.libreswan.org'
 ../../guestbin/wait-for-pluto.sh 'imported CRL.*CN=Libreswan test CA for mainca'
 # check the queue is drained
