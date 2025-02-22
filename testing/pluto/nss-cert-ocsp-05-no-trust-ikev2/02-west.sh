66b1dae44c5dd1ad7aef97d3e28f79d89c70a2c7
Andrew Cagney <cagney@gnu.org>
2025-02-21 20:19:13 -0500

    testing ocscp: use NSS generated CRL

--- testing/pluto/nss-cert-ocsp-05-no-trust-ikev2/02-west.sh
+++ testing/pluto/nss-cert-ocsp-05-no-trust-ikev2/02-west.sh
@@ -1,5 +1,7 @@
-/testing/guestbin/swan-prep --x509
-ipsec certutil -D -n east
+/testing/guestbin/swan-prep --nokeys
+
+/testing/x509/import.sh real/mainca/`hostname`.all.p12
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add nss-cert-ocsp
--- testing/pluto/nss-cert-ocsp-05-no-trust-strict-ikev1/01-east.sh
+++ testing/pluto/nss-cert-ocsp-05-no-trust-strict-ikev1/01-east.sh
@@ -1,5 +1,8 @@
-/testing/guestbin/swan-prep --x509
-ipsec certutil -D -n west
+/testing/guestbin/swan-prep --nokeys
+
+/testing/x509/import.sh real/mainca/`hostname`.all.p12
+/testing/x509/import.sh real/mainca/nic.end.cert
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec whack --impair timeout_on_retransmit
