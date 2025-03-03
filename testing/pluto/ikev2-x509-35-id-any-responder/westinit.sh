fe2321bea00402a8692033c4b14aa9afef60a7eb
Andrew Cagney <cagney@gnu.org>
2025-03-03 11:48:55 -0500

    testing x4509: use NSS's {east,west}-nosan

--- testing/pluto/ikev2-x509-35-id-any-responder/westinit.sh
+++ testing/pluto/ikev2-x509-35-id-any-responder/westinit.sh
@@ -1,5 +1,5 @@
-/testing/guestbin/swan-prep --x509 --x509name west-nosan
-ipsec certutil -D -n east
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/west-nosan.all.p12
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add ikev2-westnet-eastnet-x509-cr
