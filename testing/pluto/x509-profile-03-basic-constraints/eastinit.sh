3e52428f80d6eb0a4d37e6cc0d51c481b7c7a9c2
Andrew Cagney <cagney@gnu.org>
2025-03-08 21:25:56 -0500

    testing x509: add x509-profile-03-basic-constraints
    This tries to test:
    
    5.1.3.9.  BasicConstraints
    
       The PKIX certificate profile mandates that CA certificates contain
       this extension and that it be marked critical.  IKE implementations
       SHOULD reject CA certificates that do not contain this extension.
       For backwards compatibility, implementations may accept such
       certificates if explicitly configured to do so, but the default for
       this setting MUST be to reject such certificates.
    
    Which I find vague, so is interpreted as:
    
    - Basic Constraints don't matter on end certs, so anything like:
    
        west-bc-missing
        west-bc-n
        west-bc-n-critical
        west-bc-y-critical
    
      works
    
    - Intermediate and Root Certs need bc=ca=y
    
      (but since NSS supports basic constraints, critical doesn't matter)
    
      A cert chain is tested where the intermediate has no BC:
    
        west-bc-missing-chain-int
        west-bc-missing-chain-end
    
      and it should fail.

---
+++ testing/pluto/x509-profile-03-basic-constraints/eastinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep --nokeys
+
+/testing/x509/import.sh real/mainca/`hostname`.all.p12
+
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec add east
+echo "initdone"
