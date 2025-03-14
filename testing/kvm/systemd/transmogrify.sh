eb852464f5322c7c6359fb8fe64ea4cc7cae0d30
Antony Antony <antony@phenome.org>
2025-03-14 23:57:36 +0100

    testing: kvm remove hostnamer
    use kernel cmdline systemd.hostname instead of this service

--- testing/kvm/systemd/transmogrify.sh
+++ testing/kvm/systemd/transmogrify.sh
@@ -27,16 +27,3 @@ DHCP=yes
 EOF
 
 
-:
-: hostnamer
-:
-
-# hostnamer runs whenever /etc/hostname is empty; it detects east,
-# west, et.al., but for build domains lets the above kick in
-# rm -f /etc/hostname # hostnamectl set-hostname ""
-
-cp -v /bench/testing/kvm/systemd/hostnamer.service /etc/systemd/system
-cp -v /bench/testing/kvm/systemd/hostnamer.sh /usr/local/sbin/hostnamer.sh
-chmod a+x /usr/local/sbin/hostnamer.sh
-test -x /usr/sbin/restorecon && restorecon -R /etc/systemd/system
-systemctl enable hostnamer.service
