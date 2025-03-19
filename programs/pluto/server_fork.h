7b5e568720ebc7c60fc0c2599122504fad4a11b8
Andrew Cagney <cagney@gnu.org>
2025-03-19 14:42:14 -0400

    server: clarify param to server_fork_exec()
    ... in x509_crl, only pass LOG_OUTPUT when debugging enabled
    (fix stray debug logging pointed out by Tuomo)

--- programs/pluto/server_fork.h
+++ programs/pluto/server_fork.h
@@ -27,15 +27,16 @@ enum stream;
 /*
  * Create a child process using fork()
  *
- * Typically used to perform a thread unfriendly operation, such as
- * calling PAM.
+ * server_fork() is 
+ *
+ * server_fork_exec(), with a more traditional subprocess, is used as
+ * an alternative to popen() et.al.
  *
  * On callback:
  *
  * ST either points at the state matching SERIALNO, or NULL (SERIALNO
  * is either SOS_NOBODY or the state doesn't exist).  A CB expecting a
- * state back MUST check ST before processing.  Caller sets CUR_STATE
- * so don't play with that.
+ * state back MUST check ST before processing.
  *
  * STATUS is the child processes exit code as returned by things like
  * waitpid().
@@ -43,11 +44,17 @@ enum stream;
 
 typedef stf_status server_fork_cb(struct state *st,
 				  struct msg_digest *md,
-				  int status, shunk_t output,
+				  int wstatus, shunk_t output,
 				  void *context,
 				  struct logger *logger);
-typedef int server_fork_op(void *context, struct logger *logger);
 
+/*
+ * Call OP() within a child process.
+ *
+ * Used to perform a thread unfriendly operation, such as calling PAM.
+ */
+
+typedef int server_fork_op(void *context, struct logger *logger);
 extern int server_fork(const char *name,
 		       so_serial_t serialno,
 		       struct msg_digest *md,
@@ -56,9 +63,22 @@ extern int server_fork(const char *name,
 		       void *callback_context,
 		       struct logger *logger);
 
+/*
+ * Run a program as a child process in the background.
+ *
+ * INPUT is written to the child process after it has been created.
+ *
+ * LOG_OUTPUT, when non-zero, is where to send subprocess output.
+ *
+ * On exit CALLBACK(wstatus, output, context, ...) is called.  WSTATUS
+ * was returned by waitpid(); OUTPUT contains all the accumulated
+ * output from the process.
+ */
+
 void server_fork_exec(const char *path,
 		      char *argv[], char *envp[],
-		      shunk_t input, enum stream output,
+		      shunk_t input,
+		      enum stream log_output,
 		      server_fork_cb *callback,
 		      void *callback_context,
 		      struct logger *logger);
@@ -68,4 +88,4 @@ void init_server_fork(struct logger *logger);
 void check_server_fork(struct logger *logger);
 void show_process_status(struct show *s);
 
-#endif /* _SERVER_H */
+#endif
