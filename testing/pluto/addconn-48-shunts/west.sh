ae60928ea11f2d660d0da1d6127e1c9c09fcdc2a
Andrew Cagney <cagney@gnu.org>
2025-03-28 21:39:21 -0400

    testing: in addconn-48-shunts tickle type=%reject's warning

--- testing/pluto/addconn-48-shunts/west.sh
+++ testing/pluto/addconn-48-shunts/west.sh
@@ -14,5 +14,11 @@ ipsec add failureshunt=drop
 ipsec add failureshunt=hold
 ipsec add failureshunt=reject
 
-ipsec connectionstatus | sed -n -e 's/\(.* policy:\) .*\(NEGO[_A-Z]*\).*/\1 \2/p' | sort -u
+ipsec add type=drop
+ipsec add type=reject
+ipsec add type=passthrough
+
+ipsec connectionstatus | sed -n -e 's/\(.* policy:\) .*\(DROP\).*/\1 \2/p' | sort -u
+ipsec connectionstatus | sed -n -e 's/\(.* policy:\) .*\(PASS\).*/\1 \2/p' | sort -u
+ipsec connectionstatus | sed -n -e 's/\(.* policy:\) .*\(NEGO_[_A-Z]*\).*/\1 \2/p' | sort -u
 ipsec connectionstatus | sed -n -e 's/\(.* policy:\) .*\(failure[_A-Z]*\).*/\1 \2/p' | sort -u
