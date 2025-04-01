0db13b75b58b97b4fdb8d42a43076ce3efc30fbb
Andrew Cagney <cagney@gnu.org>
2025-04-01 10:11:01 -0400

    testing: sprinkle NN-east-established.sh over xfrm-vti-01
    should stop the flaky xfrm mismatch count

--- testing/pluto/xfrm-vti-01/05-west-ping.sh
+++ testing/pluto/xfrm-vti-01/05-west-ping.sh
@@ -1,11 +1,14 @@
-ipsec auto --up westnet-eastnet-vti
-# since we have vti-routing=no, no marking, so unencrypted packets are dropped
+# since we have vti-routing=no, no marking, so unencrypted packets are
+# dropped; this triggers an XFRM mismatch(?)
 ../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
+
+# now packets into vti0 device will get marked, and encrypted and
+# counted
 ../../guestbin/ip.sh route add 192.0.2.0/24 dev vti0
-# now packets into vti0 device will get marked, and encrypted and counted
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec whack --trafficstatus
+
 # ensure no error on delete
 ipsec auto --delete westnet-eastnet-vti
 echo done
