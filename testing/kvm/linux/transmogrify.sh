1378c077d8e6240237247a43040ef6b4370adf6c
Andrew Cagney <cagney@gnu.org>
2025-04-09 18:55:32 -0400

    testing kvm: workaround libvirt/761 stupidity
    libvirt flip-flops initrd's ownership between ROOT and current-user
    which means that QEMU, running as current-user, can find
    it inaccessible

--- testing/kvm/linux/transmogrify.sh
+++ testing/kvm/linux/transmogrify.sh
@@ -197,13 +197,19 @@ done
 
 TITLE save the latest kernels
 
-# Saved kernel is called linux.transmogrify.* so that cleaning up
-# transmogrify, using make uninstall, cleans up the files
+# The Saved kernel is called linux.* so that cleaning up transmogrify,
+# using `make uninstall`, cleans up the files.
 kernel=$(ls /boot/vmlinuz-* | sort -V | tail -1)
 cp -vf ${kernel} /pool/${PREFIX}linux.vmlinuz
 ramfs=$(ls /boot/initramfs-*.img | sort -V | tail -1)
 cp -vf ${ramfs} /pool/${PREFIX}linux.initramfs
 
+# Ensure that the files are globally readable.  Work-around for
+# libvirt/761 where the file ownership is flip-flops between ROOT and
+# WHOAMI - a u=r,go= file when owned by ROOT isn't accessible by QEMU
+# when running as WHOAMI.
+chmod go+r  /pool/${PREFIX}linux.vmlinuz
+chmod go+r /pool/${PREFIX}linux.initramfs
 
 TITLE finally ... SElinux fixup with errors in /tmp/chcon.log
 
