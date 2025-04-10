4063320b3d50460123552ae6960fcd26eda0900f
Andrew Cagney <cagney@gnu.org>
2025-04-10 14:35:38 -0400

    x509: wrap SSL profile in #ifdef USE_NSS_SSL_SECURITY_PROFILE
    not #if, fix:
        x509: wrap SSL profiles in #if USE_NSS_SSL_SECURITY_PROFILE

--- programs/pluto/nss_cert_verify.c
+++ programs/pluto/nss_cert_verify.c
@@ -221,7 +221,7 @@ static bool verify_end_cert(struct logger *logger,
 
 	static const struct usage_desc usages[] = {
 		{ certificateUsageIPsec, "IPsec" },
-#if USE_NSS_SSL_SECURITY_PROFILE
+#ifdef USE_NSS_SSL_SECURITY_PROFILE
 		{ certificateUsageSSLClient, "TLS Client" },
 		{ certificateUsageSSLServer, "TLS Server" }
 #endif
