1786f6a77cd91666d9c2222b9c7ae61bfab53f54
Andrew Cagney <cagney@gnu.org>
2025-04-25 12:33:44 -0400

    testing: use `ipsec addconn --name ...` in x509-profile-03-basic-constraints-ca
    Replace the run() command with the ./run.sh script based
    loosely on ipsec-start-stop.sh. Use:
       ipsec addconn --name NAME rightid=...
    to add the connections (deleting much of ipsec.conf)
    
    Also add begin#/end# markers so sanitizers can find
    ipsec commands being run by the script.
    
    Delete ipsec-start-stop.sh as no longer used.

---
+++ testing/pluto/x509-profile-03-basic-constraints-ca/run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+RUN() {
+    echo "begin #"
+    echo " $@"
+    "$@"
+    echo "end #"
+}
+
+cert=$1
+name=$(basename $1)
+
+RUN /testing/x509/import.sh ${cert}.p12
+RUN ipsec certutil -L -n ${name}
+
+RUN ipsec start
+
+/testing/guestbin/wait-until-pluto-started
+ipsec whack --impair suppress_retransmits
+ipsec whack --impair revival
+RUN ipsec addconn \
+    --name ${name} \
+    'rightid=C=CA, ST=Ontario, L=Toronto, O=Libreswan, OU=Test Department, CN=east.testing.libreswan.org, E=user-east@testing.libreswan.org' \
+    'right=192.1.2.23' \
+    'left=%defaultroute' \
+    'leftid=%fromcert' \
+    'leftcert='${name}
+
+RUN ipsec up ${name} || true
+
+RUN ipsec stop
