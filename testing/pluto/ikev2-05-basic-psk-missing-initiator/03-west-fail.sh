bca0403fbd6a975106f6caa5a3eaa3068091ab57
Andrew Cagney <cagney@gnu.org>
2025-04-25 21:31:49 -0400

    testing: update ikev2-05-basic-psk-missing-{initator,responder}
    fix ping-once.sh expectation, it should fail

--- testing/pluto/ikev2-05-basic-psk-missing-initiator/03-west-fail.sh
+++ testing/pluto/ikev2-05-basic-psk-missing-initiator/03-west-fail.sh
@@ -1,4 +1,6 @@
-ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
-../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-ipsec whack --trafficstatus
+# expected to fail - initiator's password is missing
+ipsec up westnet-eastnet-ipv4-psk-ikev2
+# expect block/trap (even when reviving)
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
 echo done
--- testing/pluto/ikev2-05-basic-psk-missing-responder/03-west-fail.sh
+++ testing/pluto/ikev2-05-basic-psk-missing-responder/03-west-fail.sh
@@ -1,4 +1,6 @@
-ipsec auto --up westnet-eastnet-ipv4-psk-ikev2
-../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-ipsec whack --trafficstatus
+# expect a fail - peer's password is missing
+ipsec up westnet-eastnet-ipv4-psk-ikev2
+# expect block/acquire stopping traffic
+../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
 echo done
