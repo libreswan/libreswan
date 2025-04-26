94702c6cb677f29a9ad100d0d9939100e588b2f7
Andrew Cagney <cagney@gnu.org>
2025-04-26 00:15:19 -0400

    testing: enable nft in nflog-01-global and nflog-01-global

--- testing/pluto/nflog-01-global/westinit.sh
+++ testing/pluto/nflog-01-global/westinit.sh
@@ -2,7 +2,7 @@
 # confirm that the network is alive
 ../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # ensure that clear text does not get through
-# ../../guestbin/nftable-westneteastnet-ipsec-only.nft
+../../guestbin/nftable-westneteastnet-ipsec-only.nft
 # confirm clear text does not get through
 ../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
 ipsec start
