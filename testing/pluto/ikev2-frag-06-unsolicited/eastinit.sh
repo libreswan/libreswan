426304aa2e37b66f4efb9c25fedc52fa8fbfd044
Andrew Cagney <cagney@gnu.org>
2025-04-26 09:51:28 -0400

    testing: fix certs in ikev2-frag-06-unsolicited

--- testing/pluto/ikev2-frag-06-unsolicited/eastinit.sh
+++ testing/pluto/ikev2-frag-06-unsolicited/eastinit.sh
@@ -1,6 +1,9 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/east.p12
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec add westnet-eastnet-ikev2
+ipsec add east
+
 ipsec whack --impair add_v2_notification:IKEV2_FRAGMENTATION_SUPPORTED
 echo "initdone"
--- testing/pluto/ikev2-frag-06-unsolicited/westinit.sh
+++ testing/pluto/ikev2-frag-06-unsolicited/westinit.sh
@@ -1,4 +1,6 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/west.p12
+
 # confirm that the network is alive
 ../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # ensure that clear text does not get through
@@ -6,7 +8,9 @@ iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
 # confirm clear text does not get through
 ../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-ikev2
+ipsec add west
+
 echo "initdone"
