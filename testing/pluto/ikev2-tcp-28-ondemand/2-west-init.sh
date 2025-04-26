dcdab2b89adaebc115afd73b056e577f1ae08c36
Andrew Cagney <cagney@gnu.org>
2025-04-26 11:00:46 -0400

    testing: fix ikev2-tcp-28-ondemand
    add missing fire-and-forget packet to trigger exchange

--- testing/pluto/ikev2-tcp-28-ondemand/2-west-init.sh
+++ testing/pluto/ikev2-tcp-28-ondemand/2-west-init.sh
@@ -1,7 +1,4 @@
 /testing/guestbin/swan-prep --nokeys
-# confirm that the network is alive
-../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
 ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec auto --status
 echo "initdone"
--- testing/pluto/ikev2-tcp-28-ondemand/3-west-up.sh
+++ testing/pluto/ikev2-tcp-28-ondemand/3-west-up.sh
@@ -1,5 +1,8 @@
+# trigger TCP
+../../guestbin/ping-once.sh --fire-and-forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for-pluto.sh --match '#2: initiator established Child SA'
+
 ../../guestbin/ping-once.sh --up -I 192.1.2.45 192.1.2.23
-ipsec whack --trafficstatus
+ipsec trafficstatus
 ipsec _kernel state
 ipsec _kernel policy
-
