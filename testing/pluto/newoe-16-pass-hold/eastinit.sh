a4565ca356b9a82a28c152ae87419e3af8a915ad
Andrew Cagney <cagney@gnu.org>
2025-04-26 21:24:50 -0400

    testing: drop newoe-16-pass-hold
    it's covered by newoe-30-shunt-slash*

--- testing/pluto/newoe-16-pass-hold/eastinit.sh
+++
@@ -1,9 +0,0 @@
-/testing/guestbin/swan-prep --nokeys
-cp policies/* /etc/ipsec.d/policies/
-echo "192.1.3.0/24"  >> /etc/ipsec.d/policies/clear-or-private
-ipsec start
-../../guestbin/wait-until-pluto-started
-# give OE policies time to load
-../../guestbin/wait-for.sh --match 'loaded 10,' -- ipsec auto --status
-ipsec whack --impair send_no_ikev2_auth
-echo "initdone"
--- testing/pluto/newoe-16-pass-hold/final.sh
+++
@@ -1,2 +0,0 @@
-# should not show any hits
-grep "^[^|].* established Child SA" /tmp/pluto.log
