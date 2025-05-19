9ca4b2d9a79ecdb05a9aed1df1588b459dcaeec8
Andrew Cagney <cagney@gnu.org>
2025-05-19 09:49:31 -0400

    testing: remove clientaddrfamily=

--- testing/pluto/addconn-12-family/west.sh
+++ testing/pluto/addconn-12-family/west.sh
@@ -22,13 +22,4 @@ ipsec auto --add 6in4-good
 ipsec auto --add 6in4-bad
 ipsec auto --add 6in4-protoport
 
-ipsec auto --add 44family4
-ipsec auto --add 44family6
-ipsec auto --add 46family4
-ipsec auto --add 46family6
-ipsec auto --add 64family4
-ipsec auto --add 64family6
-ipsec auto --add 66family4
-ipsec auto --add 66family6
-
 ipsec status | grep '==='
