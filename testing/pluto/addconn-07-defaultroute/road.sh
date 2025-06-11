99038007df35041db733854e49545e7f776091d2
Andrew Cagney <cagney@gnu.org>
2025-06-11 10:37:24 -0400

    testing: add nexthop=%direct et.al. to addconn-07-defaultroute

--- testing/pluto/addconn-07-defaultroute/road.sh
+++ testing/pluto/addconn-07-defaultroute/road.sh
@@ -1,4 +1,6 @@
 /testing/guestbin/swan-prep --46
+ipsec start
+../../guestbin/wait-until-pluto-started
 
 ../../guestbin/ip.sh -4 route
 ../../guestbin/ip.sh -6 route
@@ -17,18 +19,36 @@
 
 # simple case, peer known
 
-ipsec addconn --verbose ipv4-src 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
-ipsec addconn --verbose ipv6-src 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
+ipsec add defaultroute-ipv4
+ipsec connectionstatus defaultroute-ipv4 | grep ' host: '
+ipsec add defaultroute-ipv6
+ipsec connectionstatus defaultroute-ipv6 | grep ' host: '
 
-# now with no forced host
+# simple case but with clash
 
-ipsec addconn --verbose ipv4-default 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
-ipsec addconn --verbose ipv6-default 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
+ipsec add defaultroute4-ipv6 # fail
+ipsec add defaultroute6-ipv4 # fail
+
+# defaultroute-direct-ip
+
+ipsec add defaultroute-direct-ipv4
+ipsec connectionstatus defaultroute-direct-ipv4 | grep ' host: '
+ipsec add defaultroute-direct-ipv6
+ipsec connectionstatus defaultroute-direct-ipv6 | grep ' host: '
+
+# simple case, forced family
+
+ipsec add hostaddrfamily-ipv4-defaultroute-any
+ipsec connectionstatus hostaddrfamily-ipv4-defaultroute-any | grep ' host: '
+ipsec add hostaddrfamily-ipv6-defaultroute-any
+ipsec connectionstatus hostaddrfamily-ipv6-defaultroute-any | grep ' host: '
 
 # newoe case, peer is group (see newoe-20-ipv6)
 
-ipsec addconn --verbose ipv4-src-group 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
-ipsec addconn --verbose ipv6-src-group 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
+ipsec add hostaddrfamily-ipv4-defaultroute-group
+ipsec connectionstatus hostaddrfamily-ipv4-defaultroute-group | grep ' host: '
+ipsec add hostaddrfamily-ipv6-defaultroute-group
+ipsec connectionstatus hostaddrfamily-ipv6-defaultroute-group | grep ' host: '
 
 # re-adding while route is up (see ikev1-hostpair-02)
 
@@ -39,8 +59,10 @@ ip -6 address add dev eth0  2001:db8:0:2::1 nodad
 ../../guestbin/ip.sh -4 route
 ../../guestbin/ip.sh -6 route
 
-ipsec addconn --verbose ipv4-src-group 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
-ipsec addconn --verbose ipv6-src-group 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
+ipsec add hostaddrfamily-ipv4-defaultroute-group
+ipsec connectionstatus hostaddrfamily-ipv4-defaultroute-group | grep ' host: '
+ipsec add hostaddrfamily-ipv6-defaultroute-group
+ipsec connectionstatus hostaddrfamily-ipv6-defaultroute-group | grep ' host: '
 
 ../../guestbin/ip.sh -4 route   del dev eth0       192.1.2.23 via       192.1.3.254 src       192.0.2.1
 ../../guestbin/ip.sh -6 route   del dev eth0 2001:db8:1:2::23 via 2001:db8:1:3::254 src 2001:db8:0:2::1
@@ -55,4 +77,5 @@ ip -6 address del dev eth0  2001:db8:0:2::1
 ../../guestbin/ip.sh -6 route add default dev eth0 via fe80::1000:ff:fe32:64ba
 ../../guestbin/ip.sh -6 route
 
-ipsec addconn --verbose ipv6-src 2>&1 | grep -e '^resolving ' -e '^  [^ ]'
+ipsec add defaultroute-ipv6
+ipsec connectionstatus defaultroute-ipv6 | grep ' host: '
