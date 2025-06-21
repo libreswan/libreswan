d1f418d629b9c61a2545bac20c7f54fa10bf8da4
Andrew Cagney <cagney@gnu.org>
2025-06-21 16:14:11 -0400

    ipsec connectionstatus: drop redundant cisco-unity: field

--- programs/pluto/whack_connectionstatus.c
+++ programs/pluto/whack_connectionstatus.c
@@ -780,7 +780,6 @@ static void show_connection_status(struct show *s, const struct connection *c)
 		jam_string(buf, c->name);
 		jam_string(buf, ":  ");
 		jam(buf, " initial-contact:%s;", bool_str(c->config->send_initial_contact));
-		jam(buf, " cisco-unity:%s;", bool_str(c->config->host.cisco.unity));
 		jam(buf, " fake-strongswan:%s;", bool_str(c->config->send_vid_fake_strongswan));
 		jam(buf, " send-vendorid:%s;", bool_str(c->config->send_vendorid));
 		jam(buf, " send-no-esp-tfc:%s;", bool_str(c->config->child.send.esp_tfc_padding_not_supported));
