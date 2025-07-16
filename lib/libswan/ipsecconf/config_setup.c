d38489b4efd4e73529ed1f16202812141ba7cba6
Andrew Cagney <cagney@gnu.org>
2025-07-16 08:54:22 -0400

    libipsecconf: downgrade 'obsolete keyword ignored' to a true warning
    not a warning: at level ERROR!

--- lib/libswan/ipsecconf/config_setup.c
+++ lib/libswan/ipsecconf/config_setup.c
@@ -361,8 +361,8 @@ bool parse_ipsec_conf_config_setup(const struct ipsec_conf *cfgp,
 
 		case kt_obsolete:
 		{
-			llog(ERROR_STREAM, logger,
-			     PRI_KEYVAL_SAL": warning: obsolete keyword ignored: %s=%s",
+			llog(WARNING_STREAM, logger,
+			     PRI_KEYVAL_SAL": obsolete keyword ignored: %s=%s",
 			     pri_keyval_sal(kv), kv->key->keyname, kv->val);
 			continue;
 		}
