f1f8361a7ac7b581def30f84447d11862c1b4879
Andrew Cagney <cagney@gnu.org>
2025-08-26 09:19:10 -0400

    testing: update expected alg names

--- testing/pluto/algparse-01/west.sh
+++ testing/pluto/algparse-01/west.sh
@@ -21,4 +21,4 @@ grep ^FIPS /tmp/pluto.log
 
 # Check pluto algorithm list.
 
-sed -n -e '/^|/d' -e ':algs / algorithms:/ { :alg ; p ; n ; /^  / b alg ; b algs }' /tmp/pluto.log
+sed -n -e '/^|/d' -e '/Encryption Algorithm.*:/,/^testing/ p' /tmp/pluto.log
