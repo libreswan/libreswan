fb6d0e53ae79e2592ee36684d738caf8cf476481
Andrew Cagney <cagney@gnu.org>
2025-09-09 16:06:57 -0400

    testing: in PPK IKE_INTERMEDIATE tests, expect the PPK's ID

--- testing/pluto/ikev2-ppk-intermediate-02-addke-modp8192-ml-kem-768/final.sh
+++ testing/pluto/ikev2-ppk-intermediate-02-addke-modp8192-ml-kem-768/final.sh
@@ -1,7 +1,2 @@
 # confirm PPK was used
-grep "PPK used in IKE_INTERMEDIATE" /tmp/pluto.log
-# confirm west sent 2 different PPK_IDENTITY_KEY notifies
-hostname | grep west > /dev/null && grep "PPK_ID: 50 50 4b 49  44 41" /tmp/pluto.log
-hostname | grep west > /dev/null && grep "PPK_ID: 50 50 4b 49  44 42" /tmp/pluto.log
-
-ipsec whack --shutdown
+grep -e '^[^].*PPK.*used in' /tmp/pluto.log
