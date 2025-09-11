9f3a796206bcd7f7dcab45ee9d25e0e9a5f1fc15
Andrew Cagney <cagney@gnu.org>
2025-09-11 15:23:04 -0400

    testing: in ikev2-redirect-06-roadwarriors use "any-east"[1] and $3

--- testing/pluto/ikev2-redirect-06-roadwarriors/20-east-redirect.sh
+++ testing/pluto/ikev2-redirect-06-roadwarriors/20-east-redirect.sh
@@ -1,6 +1,8 @@
 # both clients should be connected now
 ipsec whack --trafficstatus
 # send REDIRECT in informational to all tunnels from connection east-any (north and road)
+ipsec whack --name '"east-any"[1]' --redirect-to 192.1.2.45
+ipsec whack --name '$3' --redirect-to 192.1.2.45
 ipsec whack --name east-any --redirect-to 192.1.2.45
 # give them time to be redirected
 ../../guestbin/wait-for.sh --no-match east-any -- ipsec trafficstatus
