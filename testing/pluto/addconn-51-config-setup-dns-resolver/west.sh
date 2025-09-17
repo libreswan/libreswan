190a95d2348f6ce1ca35ffab45e7a62839b55530
Andrew Cagney <cagney@gnu.org>
2025-09-17 16:03:53 -0400

    testing: update addconn-51-config-setup-dns-resolver

--- testing/pluto/addconn-51-config-setup-dns-resolver/west.sh
+++ testing/pluto/addconn-51-config-setup-dns-resolver/west.sh
@@ -2,7 +2,7 @@
 cp ../../guestbin/updown.sh /tmp
 chmod a+x /tmp/updown.sh
 
-show_dns_resolver() { set -x ; ipsec status | sed -n -e 's/.* \(dns-resolver=[^, ]*\).*/\1/p' ; grep PLUTO_DNS_RESOLVER /tmp/updown.env ; }
+show_dns_resolver() { printf "ipsec status: " ; ipsec status | sed -n -e 's/.* \(dns-resolver=[^, ]*\).*/\1/p' ; printf "updown: " ; grep PLUTO_DNS_RESOLVER /tmp/updown.env ; }
 
 cp west.conf /etc/ipsec.conf
 ipsec start
