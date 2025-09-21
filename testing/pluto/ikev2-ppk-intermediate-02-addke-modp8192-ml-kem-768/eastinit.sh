d4c91e00a8c881c80a64467e6bc6d9a9ab0543b3
Andrew Cagney <cagney@gnu.org>
2025-09-21 10:52:58 -0400

    testing: add ikev2-ppk-intermediate-02-addke
    trying combinations of addke=alg/none; confirm PPK attaches
    to last IKE_INTERMEDIATE, or adds one if it's NONE.
    
    replace ikev2-ppk-intermediate-02-addke-modp8192-ml-kem-768

--- testing/pluto/ikev2-ppk-intermediate-02-addke-modp8192-ml-kem-768/eastinit.sh
+++
@@ -1,6 +0,0 @@
-/testing/guestbin/swan-prep --nokeys
-ipsec pluto --config /etc/ipsec.conf --leak-detective
-../../guestbin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-ipv4-psk-ppk
-ipsec auto --status | grep westnet-eastnet-ipv4-psk-ppk
-echo "initdone"
