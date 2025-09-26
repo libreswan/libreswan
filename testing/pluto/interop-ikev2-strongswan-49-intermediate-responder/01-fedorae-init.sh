4dc9b321af9a52ae4012e9924e6e906c47711445
Andrew Cagney <cagney@gnu.org>
2025-09-26 12:09:53 -0400

    testing: re-org strongswan intermediate interop tests
    - and add more interops
    
    close #2452 replace interop ml-kem tests with ...-intermediate-{initiator,responder}

--- testing/pluto/interop-ikev2-strongswan-49-intermediate-responder/01-fedorae-init.sh
+++ testing/pluto/interop-ikev2-strongswan-49-intermediate-responder/01-fedorae-init.sh
@@ -1,4 +1,3 @@
 /testing/guestbin/swan-prep --userland strongswan
 ../../guestbin/strongswan-start.sh
 echo "initdone"
-update-crypto-policies --set DEFAULT:TEST-PQ
---
+++ testing/pluto/interop-ikev2-strongswan-49-intermediate-responder/03-fedoraw-run.sh
@@ -0,0 +1,2 @@
+../../guestbin/pluto-up-down.sh ike=aes-sha1-modp8192-ml_kem_768 -- -I 192.0.1.254 192.0.2.254
+../../guestbin/pluto-up-down.sh ike=aes-sha1-modp8192-ml_kem_768 fragmentation=no -- -I 192.0.1.254 192.0.2.254
