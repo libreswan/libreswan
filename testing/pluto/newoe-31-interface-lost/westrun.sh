38a72a42ae8d6a5cbb06b0f9fb8b1397050019e8
Andrew Cagney <cagney@gnu.org>
2025-10-04 15:09:20 -0400

    testing: add newoe-31-interface-lost
    see crash in #2413

---
+++ testing/pluto/newoe-31-interface-lost/westrun.sh
@@ -0,0 +1,14 @@
+# bring up OE
+ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --fire-and-forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match 192.1.2.23 -- ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --up   -I 192.1.2.45 192.1.2.23
+ipsec whack --trafficstatus
+
+# take it down
+ipsec down '"private-or-clear#192.1.2.0/24"'
+
+# now loose the interface 192.1.2.0/24; this should cause that OE
+# connection to be deleted.
+ifconfig eth1 down
+ipsec listen
