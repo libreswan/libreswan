21fa2512755ff0eb145f8bd92b067e73ce67f46a
Andrew Cagney <cagney@gnu.org>
2025-10-08 09:06:16 -0400

    testing: update ikev2-x509-eddsa-01
    - run strongswan-gen.sh
    
    - use import.sh to bring in keys
    
    - add to TESTLIST as skip

--- testing/pluto/ikev2-x509-eddsa-01/eastinit.sh
+++ testing/pluto/ikev2-x509-eddsa-01/eastinit.sh
@@ -1,11 +1,11 @@
-/testing/guestbin/swan-prep 
-/usr/bin/pk12util -i /testing/x509/strongswan/strongEastEd25519.p12 -d sql:/etc/ipsec.d -w /testing/x509/nss-pw
-# Tuomo: why doesn't ipsec checknss --settrust work here?
-certutil -M -d sql:/etc/ipsec.d -n "strongSwan CA - strongSwan" -t CT,,
-#ipsec start
-ipsec _stackmanager start
-ipsec pluto --config /etc/ipsec.conf --leak-detective
+/testing/guestbin/swan-prep --userland strongswan
+/testing/x509/strongswan-gen.sh
+
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh strongswan/strong-ED/strongEast.p12
+
+ipsec start
 ../../guestbin/wait-until-pluto-started
-ipsec auto --add westnet-eastnet-ikev2
+ipsec add westnet-eastnet-ikev2
 ipsec whack --impair suppress-retransmits
 echo "initdone"
