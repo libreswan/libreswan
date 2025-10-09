4a37b850f2d6d455dbe16784edc49120afd34f4a
Andrew Cagney <cagney@gnu.org>
2025-10-09 16:16:33 -0400

    testing: in newoe-31-interface-lost try both with OE up and down

--- testing/pluto/newoe-31-interface-lost/westrun.sh
+++ testing/pluto/newoe-31-interface-lost/westrun.sh
@@ -1,3 +1,13 @@
+# lost interface with no connection
+
+# now loose the interface 192.1.2.0/24
+ifconfig eth1 down
+ipsec listen
+
+# restore eth1
+ifconfig eth1 192.1.2.45 up
+ipsec listen
+
 # bring up OE
 ipsec whack --trafficstatus
 ../../guestbin/ping-once.sh --fire-and-forget -I 192.1.2.45 192.1.2.23
@@ -5,14 +15,21 @@ ipsec whack --trafficstatus
 ../../guestbin/ping-once.sh --up   -I 192.1.2.45 192.1.2.23
 ipsec whack --trafficstatus
 
-# take it down
-ipsec down '"private-or-clear#192.1.2.0/24"'
+# lost interface with OE connection
 
-# now loose the interface 192.1.2.0/24; this should cause that OE
-# connection to be deleted.
+# now loose the interface 192.1.2.0/24
 ifconfig eth1 down
 ipsec listen
 
 # restore eth1
 ifconfig eth1 192.1.2.45 up
 ipsec listen
+
+# bring up OE
+ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --fire-and-forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match 192.1.2.23 -- ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --fire-and-forget -I 192.1.2.45 192.1.2.23
+../../guestbin/wait-for.sh --match 192.1.2.23 -- ipsec whack --trafficstatus
+ipsec whack --trafficstatus
+../../guestbin/ping-once.sh --up   -I 192.1.2.45 192.1.2.23
