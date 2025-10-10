83e73c45c8cbf2f33fc28518b54dcdfa959dd31d
Andrew Cagney <cagney@gnu.org>
2025-10-10 10:44:20 -0400

    testing: simplify newoe-31-interface-lost
    only add private-or-clear

--- testing/pluto/newoe-31-interface-lost/westinit.sh
+++ testing/pluto/newoe-31-interface-lost/westinit.sh
@@ -2,9 +2,8 @@
 
 cp policies/* /etc/ipsec.d/policies/
 echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
-cp ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
 
 ipsec start
 ../../guestbin/wait-until-pluto-started
 # give OE policies time to load
-../../guestbin/wait-for.sh --match 'loaded 9' -- ipsec auto --status
+../../guestbin/wait-for.sh --match 'loaded 2, routed 1, active 0' -- ipsec auto --status
