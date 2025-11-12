b7c7b312f81edb3e9e547aad9b693b9230f3cf56
Andrew Cagney <cagney@gnu.org>
2025-11-12 16:37:59 -0500

    defaultroute: in BSD's get_route_1() dump values as they are found

--- lib/libswan/defaultroute_bsd.c
+++ lib/libswan/defaultroute_bsd.c
@@ -76,10 +76,34 @@ void resolve_default_route(struct resolve_end *host,
 	vfatal(PLUTO_EXIT_FAIL, 0, "addcon: without XFRM, cannot resolve_defaultroute()");
 }
 
-static enum route_status get_route_1(int s, ip_address dst,
+static bool parse_address(const struct ip_info *afi,
+			  ip_address *out,
+			  const char *what,
+			  const struct sockaddr *sa,
+			  struct verbose verbose)
+{
+	if (sa->sa_family != afi->af) {
+		verbose("%s: ignored, %d is wrong family", what, sa->sa_family);
+		return true;
+	}
+
+	err_t e = sockaddr_to_address_port(sa, sa->sa_len,
+					   out, NULL);
+	if (e != NULL) {
+		verror(0, "%s: sockaddr to address failed: %s", what, e);
+		return false;
+	}
+
+	address_buf ob;
+	verbose("%s: %s", what, str_address(out, &ob));
+	return true;
+}
+
+static enum route_status get_route_1(int s,
+				     ip_address dest,
 				     const struct ip_info *afi,
 				     struct ip_route *route,
-				     struct logger *logger)
+				     struct verbose verbose)
 {
 	struct {
 		struct rt_msghdr hdr;
@@ -93,106 +117,156 @@ static enum route_status get_route_1(int s, ip_address dst,
 		},
 	};
 
-	uint8_t *ap = msg.buf;
+	/*
+	 * Build the request.
+	 */
+	{
+		uint8_t *ap = msg.buf;
 
-	/* append dst */
-	msg.hdr.rtm_addrs |= RTA_DST;
-	ip_sockaddr sa = sockaddr_from_address(dst);
-	memcpy(ap, &sa.sa, sa.len);
-	RT_ADVANCE(ap, (&sa.sa.sa));
+		/* append dst */
+		msg.hdr.rtm_addrs |= RTA_DST;
+		ip_sockaddr sa = sockaddr_from_address(dest);
+		memcpy(ap, &sa.sa, sa.len);
+		RT_ADVANCE(ap, (&sa.sa.sa));
 
-	/* append IFP */
-	msg.hdr.rtm_addrs |= RTA_IFP;
-	struct sockaddr *ifp = (void*)ap;
-	ifp->sa_family = AF_LINK,
-	ifp->sa_len = sizeof(struct sockaddr_dl),
-	RT_ADVANCE(ap, ifp);
+		/* append IFP */
+		msg.hdr.rtm_addrs |= RTA_IFP;
+		struct sockaddr *ifp = (void*)ap;
+		ifp->sa_family = AF_LINK;
+		ifp->sa_len = sizeof(struct sockaddr_dl);
+		RT_ADVANCE(ap, ifp);
 
-	/* final length */
-	msg.hdr.rtm_msglen = ap - (uint8_t*)&msg;
+		/* final length */
+		msg.hdr.rtm_msglen = ap - (uint8_t*)&msg;
 
-	/* send */
-	int w = write(s, &msg, msg.hdr.rtm_msglen);
-	if (w < 0) {
-		llog_errno(ERROR_STREAM, logger, errno, "write failed: ");
-		return ROUTE_FATAL;
+		/* send */
+		int w = write(s, &msg, msg.hdr.rtm_msglen);
+		if (w < 0) {
+			verror(errno, "write failed: ");
+			return ROUTE_FATAL;
+		}
+
 	}
 
 	/* recv */
 	int r = read(s, &msg, sizeof(msg));
 	if (r < 0) {
-		llog_errno(ERROR_STREAM, logger, errno, "write failed: ");
+		verror(errno, "write failed: ");
 		return ROUTE_FATAL;
 	}
 
 	/* verify */
 	if (sizeof(msg.hdr.rtm_msglen) > (unsigned)r) {
-		llog(ERROR_STREAM, logger, "response of %d bytes way too small", r);
+		verror(0, "response of %d bytes way too small", r);
 		return ROUTE_FATAL;
 	}
+
 	if (msg.hdr.rtm_msglen > r) {
-		llog(ERROR_STREAM, logger, "response of %d bytes was truncated", r);
+		verror(0, "response of %d bytes was truncated", r);
 		return ROUTE_FATAL;
 	}
+
 	if (msg.hdr.rtm_version != RTM_VERSION) {
-		llog(ERROR_STREAM, logger, "response version %d wrong",
-		     msg.hdr.rtm_version);
+		verror(0, "response version %d wrong", msg.hdr.rtm_version);
 		return ROUTE_FATAL;
 	}
+
 	if (msg.hdr.rtm_errno != 0) {
-		llog(ERROR_STREAM, logger, "response failed: %s", strerror(errno));
+		verror(msg.hdr.rtm_errno, "response failed: ");
 		return ROUTE_FATAL;
 	}
+
 	if (msg.hdr.rtm_type != RTM_GET) {
-		llog(ERROR_STREAM, logger, "response type %d wrong",
-		     msg.hdr.rtm_type);
+		verror(0, "response type %d wrong", msg.hdr.rtm_type);
 		return ROUTE_FATAL;
 	}
 
 	/* go through bits lsb->msb; match payload */
-	ap = msg.buf;
+	ip_address gateway = unset_address;
+	ip_address ifa = unset_address;
+	ip_address netmask = unset_address;
+	ip_address genmask = unset_address;
+	ip_address dst = unset_address;
+	unsigned interface_index = 0;
+	char interface_name[IFNAMSIZ] = {0};
+
+	/*
+	 * Iterate through the bitmask .rtm_addrs.
+	 */
+	uint8_t *ap = msg.buf;
 	unsigned a = msg.hdr.rtm_addrs;
 	for (unsigned b = 0; a != 0; b++) {
 		unsigned m = 1 << b;
 		if (a & m) {
-			name_buf eb;
-			ldbg(logger, "found %s", str_sparse_long(&rta_names, m, &eb));
 			a &= ~m;
-			struct sockaddr *sa = (void*)ap;
+			const struct sockaddr *sa = (void*)ap;
 			RT_ADVANCE(ap, sa);
-			if (sa->sa_family != afi->af) {
-				continue;
-			}
-			err_t e = NULL;
 			switch (m) {
+
 			case RTA_DST:		/* destination sockaddr present */
+				if (!parse_address(afi, &dst, "RTA_DST", sa, verbose)) {
+					return ROUTE_FATAL;
+				}
 				break;
 			case RTA_GATEWAY:	/* gateway sockaddr present */
-				e = sockaddr_to_address_port(sa, sa->sa_len,
-							     &route->gateway, NULL);
-				break;
-			case RTA_NETMASK:	/* netmask sockaddr present */
-			case RTA_GENMASK:	/* cloning mask sockaddr present */
-			case RTA_IFP:		/* interface name sockaddr present */
+				if (!parse_address(afi, &gateway, "RTA_GATEWAY", sa, verbose)) {
+					return ROUTE_FATAL;
+				}
 				break;
 			case RTA_IFA:		/* interface addr sockaddr present */
-				e = sockaddr_to_address_port(sa, sa->sa_len,
-							     &route->source, NULL);
+				if (!parse_address(afi, &ifa, "RTA_IFA", sa, verbose)) {
+					return ROUTE_FATAL;
+				}
 				break;
+			case RTA_NETMASK:	/* netmask sockaddr present */
+				if (!parse_address(afi, &netmask, "RTA_NETMASK", sa, verbose)) {
+					return ROUTE_FATAL;
+				}
+				break;
+			case RTA_GENMASK:	/* cloning mask sockaddr present */
+				if (!parse_address(afi, &genmask, "RTA_GENMASK", sa, verbose)) {
+					return ROUTE_FATAL;
+				}
+				break;
+
+			case RTA_IFP:		/* interface name sockaddr present */
+			{
+				if (sa->sa_family != AF_LINK) {
+					verbose("RTA_IFP: ignored, need AF_LINK");
+					break;
+				}
+				const struct sockaddr_dl *sdl = (struct sockaddr_dl *)sa;
+				if (sdl->sdl_nlen == 0) {
+					verbose("RTA_IFP: ignored, .sdl_nlen == 0");
+					break;
+				}
+				interface_index = sdl->sdl_index;
+				if_indextoname(interface_index, interface_name);
+				verbose("RTA_IFP: %s (%d)", interface_name, interface_index);
+				break;
+			}
+
 			case RTA_AUTHOR:	/* sockaddr for author of redirect */
 			case RTA_BRD:		/* for NEWADDR, broadcast or p-p dest addr */
 #ifdef RTA_TAG
 			case RTA_TAG:		/* route tag */
 #endif
+			{
+				name_buf eb;
+				verbose("%s: ignored", str_sparse_long(&rta_names, m, &eb));
 				break;
 			}
-			if (e != NULL) {
-				llog(ERROR_STREAM, logger,
-				     "invalid %s", e);
-				return ROUTE_FATAL;
+			default:
+			{
+				verbose("unrecognized %d", m);
+				break;
+			}
 			}
 		}
 	}
+
+	route->gateway = gateway;
+	route->source = ifa;
 
 	/* unpack */
 	return ROUTE_SUCCESS;
@@ -200,17 +274,18 @@ static enum route_status get_route_1(int s, ip_address dst,
 
 enum route_status get_route(ip_address dst, struct ip_route *route, struct logger *logger)
 {
+	struct verbose verbose = VERBOSE(DEBUG_STREAM, logger, NULL);
+
 	zero(route);
 	const struct ip_info *afi = address_info(dst);
 	int s = cloexec_socket(PF_ROUTE, SOCK_RAW, afi->af);
 	if (s < 0) {
-		llog_errno(ERROR_STREAM, logger, errno,
-			   "cloexec_socket(PF_ROUTE, SOCK_RAW, %s) failed: ",
-			   afi->ip_name);
+		verror(errno, "cloexec_socket(PF_ROUTE, SOCK_RAW, %s) failed: ",
+		       afi->ip_name);
 		return ROUTE_FATAL;
 	}
 
-	enum route_status status = get_route_1(s, dst, afi, route, logger);
+	enum route_status status = get_route_1(s, dst, afi, route, verbose);
 	close(s);
 	return status;
 }
