5b412092fee6efe02eb8060e698022ab65d4f6ee
Andrew Cagney <cagney@gnu.org>
2025-11-28 11:14:45 -0500

    testing: shuffle last few IMPAIR: logs

--- testing/pluto/crossing-streams-04-ikev2-initiator-ike-rekey-responder-child-up/06-east-oops.sh
+++ testing/pluto/crossing-streams-04-ikev2-initiator-ike-rekey-responder-child-up/06-east-oops.sh
@@ -7,7 +7,7 @@ ipsec whack --impair helper_thread_delay:5
 date
 ipsec whack --impair drip_inbound:1
 date
-../../guestbin/wait-for-pluto.sh '#1: IMPAIR: job 3 helper 0 #1 process_v2_CREATE_CHILD_SA_rekey_ike_request '
+../../guestbin/wait-for-pluto.sh 'IMPAIR: .* process_v2_CREATE_CHILD_SA_rekey_ike_request'
 ipsec whack --impair helper_thread_delay:no
 
 # With the request in limbo, initiate a new child sa exchange.  Doing
