f1574cf552b04899529244f640d4d86e752e0731
Andrew Cagney <cagney@gnu.org>
2026-01-13 17:32:30 -0500

    testing: run l2tpd tests on alpine
    The key takeaway is that ALPINE, and presumably the *BSD
    domains are all now sufficiently functional to run complex
    tests.
    
    (the debian domain's suffered bitrot)

--- testing/guestbin/l2tpd.sh
+++ testing/guestbin/l2tpd.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
+copy()
+{
+    local src=$1
+    local dst=$2
+    rm -f ${dst}
+    for f in $(hostname).${src}  ${src} ; do
+	test ! -r ${f} && continue
+	if test -r ${dst} ; then
+	    echo "duplicate ${dst}: $@" 1>&2
+	    exit 1
+	fi
+	mkdir -p $(dirname ${dst})
+	cp -v ${f} ${dst}
+	chmod u=r,go= ${dst}
+    done
+}
+
+mkdir -p /var/run/xl2tpd
+
+copy xl2tpd.conf /etc/xl2tpd/xl2tpd.conf
+copy chap-secrets /etc/ppp/chap-secrets
+copy ppp-options.xl2tpd /etc/ppp/options.xl2tpd
+
+mknod /dev/ppp c 108 0
+
 cd /tmp
 xl2tpd -D 2>/tmp/xl2tpd.log &
 
 # give it a chance to start
 sleep 1
-
--- testing/pluto/ikev1-l2tp-01/03-alpinen-run.sh
+++ testing/pluto/ikev1-l2tp-01/03-alpinen-run.sh
@@ -1,13 +1,17 @@
-ipsec auto --up north-east-l2tp
+ipsec up north-east-l2tp
+
 echo "c server" > /var/run/xl2tpd/l2tp-control
-sleep 5
+sleep 10
+
 # should be non-zero counters if l2tp worked
 # workaround for diff err msg between fedora versions resulting in diff byte count
 ipsec whack --trafficstatus | grep -v "inBytes=0" | sed "s/type=ESP.*$/[...]/"
+
 : ==== cut ====
 cat /tmp/xl2tpd.log
 : ==== tuc ====
+
 # testing passthrough of non-l2tp/ipsec traffic
 echo quit | socat - TCP:192.0.2.254:7
-../../guestbin/ip.sh address show dev ppp0 | sed "s/ qdisc.*$//"
+../../guestbin/ip.sh address show dev ppp0 | sed -e 's/ qdisc.*$//' -e '/inet6/,$ d'
 echo done
