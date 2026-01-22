23a1d289c5b4716bc6dfa8b3bee247c9dae6909d
Andrew Cagney <cagney@gnu.org>
2026-01-22 09:07:27 -0500

    testing kvm: robustify systemd/transmogrify-networkd.sh a little
    - fix test for restoreconn
    - unconditionally wipe hostname
    - unconditionally wipe old systemd/network/ files

--- testing/kvm/systemd/transmogrify-networkd.sh
+++ testing/kvm/systemd/transmogrify-networkd.sh
@@ -13,10 +13,13 @@ systemctl enable systemd-networkd-wait-online.service
 # systemd.hostname=... or zzz.eth0.network which defaults to the local
 # platform.
 
-rm /etc/hostname
+rm -f /etc/hostname
+rm -f /etc/systemd/network/*
 cp -v /bench/testing/kvm/systemd/network/*.network /etc/systemd/network/
 sed -i -e "s/@@PLATFORM@@/${PLATFORM}/" /etc/systemd/network/*.network
 
 # test specific configs; bound using ethernet addresses
 
-test -x /usr/sbin/restorecon && restorecon -R /etc/systemd/network
+if test -x /usr/sbin/restorecon ; then
+    restorecon -R /etc/systemd/network
+fi
