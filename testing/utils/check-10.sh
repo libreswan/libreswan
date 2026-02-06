a73966858200402b6c4eeb1ac291f254c8aa1861
Andrew Cagney <cagney@gnu.org>
2026-02-06 18:35:33 -0500

    testing: use ifconfig everywhere in check-10-*

--- testing/utils/check-10.sh
+++ testing/utils/check-10.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 basedir=testing/pluto/$(basename $0 .sh)
-hosts="east west rise set nic north"
+hosts="east west rise set north" # nic
 # same platforms as KVM
 platforms=$(ls -d1 testing/kvm/platform/[a-z]*/ | cut -d/ -f4)
 
@@ -41,6 +41,8 @@ set_sunnet=${sunnet}.15
 set_westnet=${westnet}.15
 
 in=
+
+# connectivity HOST name-of-dest ...
 
 connectivity()
 {
@@ -87,16 +89,16 @@ test pluto between:
 - RISE and SET
 EOF
 
-    for host in ${hosts} ; do
+    for host in ${hosts} nic ; do
 	touch ${dir}/${host}.console.txt
     done
 
     case ${platform} in
-	netbsd )  eth=vioif ; ifconfig=ifconfig ;;
-	openbsd ) eth=vio   ; ifconfig=ifconfig ;;
-	freebsd ) eth=vtnet ; ifconfig=ifconfig ;;
-	alpine )  eth=eth   ; ifconfig=ifconfig ;;
-	* )       eth=eth   ; ifconfig="ip link set" ;;
+	netbsd )  eth=vioif ;;
+	openbsd ) eth=vio ;;
+	freebsd ) eth=vtnet ;;
+	alpine )  eth=eth ;;
+	* )       eth=eth ;;
     esac
 
     # start again
@@ -111,19 +113,11 @@ EOF
 
     # bring up all the networks
 
-    for host in east west ; do
+    for host in east west rise set north ; do
 	echo "" >> ${in}
 	cat <<EOF >> ${in}
-${host}# ../../guestbin/ip.sh addr show eth0
-${host}# ../../guestbin/ip.sh addr show eth1
-EOF
-    done
-
-    for host in rise set ; do
-	echo "" >> ${in}
-	cat <<EOF >> ${in}
-${host}# ${ifconfig} ${eth}0
-${host}# ${ifconfig} ${eth}1
+${host}# ifconfig ${eth}0
+${host}# ifconfig ${eth}1
 EOF
     done
 
