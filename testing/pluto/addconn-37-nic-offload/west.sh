59553e4b19cb7a7024b2fd27756e6303d0f3f647
Andrew Cagney <cagney@gnu.org>
2026-02-15 11:52:24 -0500

    testing: expand addconn-37-nic-offload
    try compress=, encapsulation=, type=, phase2=

--- testing/pluto/addconn-37-nic-offload/west.sh
+++ testing/pluto/addconn-37-nic-offload/west.sh
@@ -2,15 +2,26 @@
 ipsec start
 ../../guestbin/wait-until-pluto-started
 
-ipsec add addconn
+ipsec addconn --name nic-offload=        #nic-offload=
+ipsec addconn --name nic-offload=no      nic-offload=no
+ipsec addconn --name nic-offload=yes     nic-offload=yes
+ipsec addconn --name nic-offload=packet  nic-offload=packet
+ipsec addconn --name nic-offload=crypto  nic-offload=crypto
 
-ipsec add addconn--nic-offload=no
-ipsec add addconn--nic-offload=yes
-ipsec add addconn--nic-offload=packet
-ipsec add addconn--nic-offload=crypto
+ipsec addconn --name type=transport-nic-offload=packet  type=transport nic-offload=packet
+ipsec addconn --name type=tunnel-nic-offload=packet     type=tunnel    nic-offload=packet
 
-# should fail to load
-ipsec add addconn-encapsulation=yes
+ipsec addconn --name phase2=esp-type=transport-nic-offload=packet  phase2=esp type=transport nic-offload=packet
+ipsec addconn --name phase2=ah-type=transport-nic-offload=packet   phase2=ah  type=transport nic-offload=packet
+
+ipsec addconn --name compress=yes-type=transport-nic-offload=packet  compress=yes type=transport nic-offload=packet
+ipsec addconn --name compress=no-type=transport-nic-offload=packet   compress=no  type=transport nic-offload=packet
+
+ipsec addconn --name encapsulation=yes-type=transport-nic-offload=packet   encapsulation=yes  type=transport nic-offload=packet
+ipsec addconn --name encapsulation=no-type=transport-nic-offload=packet    encapsulation=no   type=transport nic-offload=packet
+ipsec addconn --name encapsulation=auto-type=transport-nic-offload=packet  encapsulation=auto type=transport nic-offload=packet
+
+#
 
 ipsec whack --name whack                                           --transport --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--nic-offload-x      --nic-offload x       --transport --host 1.2.3.4 --to --host 5.6.7.8
