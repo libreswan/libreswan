c016b79378c7c31bc61bff0fcc6504bd18a58add
Andrew Cagney <cagney@gnu.org>
2026-02-16 16:00:44 -0500

    testing: update alparse-*, use --opt, not -opt

--- testing/pluto/algparse-01/west.sh
+++ testing/pluto/algparse-01/west.sh
@@ -2,16 +2,16 @@
 
 # Run the parser tests.
 
-ipsec algparse -tp -v1
-ipsec algparse -tp -v1 -pfs
-ipsec algparse -tp -v2
-ipsec algparse -tp -v2 -pfs
+ipsec algparse --tp --v1
+ipsec algparse --tp --v1 --pfs
+ipsec algparse --tp --v2
+ipsec algparse --tp --v2 --pfs
 
-ipsec algparse -tp -v2 -pfs -addke
+ipsec algparse --tp --v2 --pfs --addke
 
 # Run the algorithm tests; there should be no fails.
 
-ipsec algparse -ta > /dev/null
+ipsec algparse --ta > /dev/null
 
 # Check pluto is starting in the correct mode.
 
