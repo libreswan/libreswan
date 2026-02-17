ceca522efcd7447eabc84e2920861eab52cd1914
Andrew Cagney <cagney@gnu.org>
2026-02-17 12:41:56 -0500

    testing: in addconn-39-updown, check PATH=

--- testing/pluto/addconn-39-updown/west.sh
+++ testing/pluto/addconn-39-updown/west.sh
@@ -19,10 +19,6 @@ ipsec add addconn--rightupdown=right
 ipsec add addconn--type=passthrough
 ipsec add addconn--type=passthrough--leftupdown=left
 
-ipsec add --auto=route addconn--updown-flags=exec
-grep -e PLUTO_VERB= -e PLUTO_CONNECTION= /tmp/updown.env
-ipsec delete addconn--updown-flags=exec
-
 ipsec whack --name whack                                      --host 1.2.3.4 --to --host 5.6.7.8
 ipsec whack --name whack--updown=from  --updown=from          --host 1.2.3.4 --to --host 5.6.7.8
 
@@ -30,3 +26,14 @@ ipsec whack --name whack--pass               --pass                --host 1.2.3.
 ipsec whack --name whack--pass--updown=from  --pass --updown=from  --host 1.2.3.4 --to --host 5.6.7.8
 
 ipsec connectionstatus | sed -n -e '/: .*updown=/ s/  */ /gp' | sort
+
+ipsec stop
+
+PATH=$PATH:/silly/path ipsec pluto --config /etc/ipsec.conf --logfile /tmp/pluto.log
+../../guestbin/wait-until-pluto-started
+
+ipsec add --auto=route addconn--updown-flags=exec
+grep -e PATH= -e PLUTO_VERB= -e PLUTO_CONNECTION= /tmp/updown.env
+ipsec delete addconn--updown-flags=exec
+
+ipsec stop
