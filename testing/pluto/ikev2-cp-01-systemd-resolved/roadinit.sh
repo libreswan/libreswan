c942b08fe3bd57344b43a2b9c523dd9ca18a00e0
Andrew Cagney <cagney@gnu.org>
2026-02-19 14:42:12 -0500

    testing: add ikev2-cp-01-systemd-resolved good

---
+++ testing/pluto/ikev2-cp-01-systemd-resolved/roadinit.sh
@@ -0,0 +1,9 @@
+/testing/guestbin/swan-prep --nokeys
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec auto --add eastnet-any
+# confirm we have stock resolv.conf
+cat /etc/resolv.conf
+echo initdone
+
+systemctl start systemd-resolved
