e2a98fed7f4461ed839bc4e0031461a7b89980f2
Andrew Cagney <cagney@gnu.org>
2026-02-28 15:55:35 -0500

    testing: in ikev2-mobike-08-nat-expire-liveness-east suppress unstable tcpdump output

--- testing/pluto/ikev2-mobike-08-nat-expire-liveness-east/04-nic-remap.sh
+++ testing/pluto/ikev2-mobike-08-nat-expire-liveness-east/04-nic-remap.sh
@@ -1,3 +1,3 @@
-../../guestbin/tcpdump.sh --stop -i eth1
+../../guestbin/tcpdump.sh --stop -i eth1 > /dev/null
 ../../guestbin/nic-nat.sh 192.1.3.0/24 192.1.2.254 50000 1
 ../../guestbin/tcpdump.sh --start -i eth1
