e5bead08d30236664546a63cb80d35521fd60f56
Andrew Cagney <cagney@gnu.org>
2026-02-28 11:40:41 -0500

    testing: refresh ikev2-child-ipsec-replace-ike
    It's checking multiple connections come back up after a replace
    (reauth).
    
    Drop the third connection, it's a distraction.
    
    Document why the test is unstable
    see #2644 add a custom now queue
    
    Document how the revival reverses the connections
    see #2643 tear down Child SAs in OLD2NEW order in send_n_log_delete_ike_family_now()

--- testing/pluto/ikev2-child-ipsec-replace-ike/westinit.sh
+++ testing/pluto/ikev2-child-ipsec-replace-ike/westinit.sh
@@ -1,14 +1,13 @@
 /testing/guestbin/swan-prep --nokeys
 ../../guestbin/ip.sh address add 192.0.100.254/24 dev eth0:1
 ../../guestbin/ip.sh route add 192.0.200.0/24 via 192.1.2.23  dev eth1
-# ensure that clear text does not get through
-iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
-iptables -A INPUT -i eth1 -s 192.0.200.0/24 -j DROP
-iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
+
 ipsec whack --impair suppress_retransmits
-ipsec auto --add westnet-eastnet-ikev2a
-ipsec auto --add westnet-eastnet-ikev2b
-ipsec auto --add westnet-eastnet-ikev2c
+
+ipsec add westnet-eastnet-ikev2a
+ipsec add westnet-eastnet-ikev2b
+
 echo "initdone"
--- testing/pluto/ikev2-child-ipsec-replace-ike/westrun.sh
+++ testing/pluto/ikev2-child-ipsec-replace-ike/westrun.sh
@@ -1,22 +1,20 @@
-ipsec auto --up westnet-eastnet-ikev2a
+# IKE SA #1(A), Child SA #2(A)
+ipsec up westnet-eastnet-ikev2a
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 
-ipsec auto --up westnet-eastnet-ikev2b
+# Child SA #3(B)
+ipsec up westnet-eastnet-ikev2b
 ../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
-
-ipsec auto --up westnet-eastnet-ikev2c
-ipsec whack --trafficstatus
 
 # wait for the replace
-../../guestbin/wait-for-pluto.sh '#5: initiating IKEv2 connection'
+../../guestbin/wait-for-pluto.sh '#4: initiating IKEv2 connection'
 
-../../guestbin/wait-for-pluto.sh '#6: initiator established Child SA using #5' | sed -e 's/[a-c]"/."/'
-../../guestbin/wait-for-pluto.sh '#7: initiator established Child SA using #5' | sed -e 's/[a-c]"/."/'
-../../guestbin/wait-for-pluto.sh '#8: initiator established Child SA using #5' | sed -e 's/[a-c]"/."/'
+../../guestbin/wait-for-pluto.sh '#5: initiator established Child SA using'
+../../guestbin/wait-for-pluto.sh '#6: initiator established Child SA using'
 
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ../../guestbin/ping-once.sh --up -I 192.0.100.254 192.0.200.254
 
-ipsec whack --trafficstatus |  sed -e 's/[a-c]"/."/'
+ipsec whack --trafficstatus
 
 echo done
