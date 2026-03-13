6a216545d90abe5bfb9b8fe68b9159df2674f861
Andrew Cagney <cagney@gnu.org>
2026-03-12 22:17:42 -0400

    testing: sprinkle # sanitize-retransmits
    and newspeak commands

---
+++ testing/pluto/connalias-02-twoconn-ikev1/westrun.sh
@@ -0,0 +1,9 @@
+ipsec up west # sanitize-retransmits
+ipsec down west
+
+ipsec up cuckold # sanitize-retransmits
+ipsec up cuckoo # sanitize-retransmits
+ipsec down west
+
+ipsec delete cuckold
+ipsec delete cuckoo
