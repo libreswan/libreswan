e789bc6e29886f7595bb9e1401f2538126c3ecf5
Andrew Cagney <cagney@gnu.org>
2026-03-23 17:37:17 -0400

    testing: replace basic-pluto-19-seedbits with whack-pluto-03-seedbits
    just the seedy bits of basic-pluto-19-seedbits; block is tested
    elsewhere; seccheck is #2706.
    
    close #2705 basic-pluto-19-seedbits out of control

---
+++ testing/pluto/whack-pluto-03-seedbits/west.sh
@@ -0,0 +1,24 @@
+/testing/guestbin/swan-prep --nokeys
+
+sed "s/seedbits=.*$/seedbits=520/" ipsec.conf > /etc/ipsec.conf
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec stop
+grep -E "^seeded" /tmp/pluto.log
+
+sed "s/seedbits=.*$/seedbits=1024/" ipsec.conf > /etc/ipsec.conf
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec stop
+grep -E "^seeded" /tmp/pluto.log
+
+sed "s/seedbits=.*$/seedbits=2048/" ipsec.conf > /etc/ipsec.conf
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec stop
+grep -E "^seeded" /tmp/pluto.log
+
+ipsec pluto --config /etc/ipsec.conf --leak-detective --seedbits=1024
+../../guestbin/wait-until-pluto-started
+ipsec whack --shutdown
+grep -E "^seeded" /tmp/pluto.log
