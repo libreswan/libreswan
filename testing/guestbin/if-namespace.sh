5f9f7f550fe1c032a5280452923865d09941e602
Fatma <fatmahashem2004@gmail.com>
2026-04-10 13:36:50 +0200

    testing: fix nsenter path detection for namespace testing
    grep was matching /bin/nsenter but nsrun passes /usr/sbin/nsenter.
    Change to match 'nsenter' regardless of path.
    
    Fixes: #2662 where does nsenter live
    Signed-off-by: Andrew Cagney <cagney@gnu.org>

--- testing/guestbin/if-namespace.sh
+++ testing/guestbin/if-namespace.sh
@@ -2,7 +2,7 @@
 
 # running under a namespace?
 # BETTER? expr "$SUDO_COMMAND" : ".*/nsenter " > /dev/null
-if echo $SUDO_COMMAND | grep "/bin/nsenter " > /dev/null 2>&1 ; then
+if echo $SUDO_COMMAND | grep "nsenter " > /dev/null 2>&1 ; then
     if "$@" > OUTPUT/if-namespace.$$.log 2>&1 ; then
 	echo === cut ===
 	cat OUTPUT/if-namespace.$$.log
