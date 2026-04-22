44d5207f9a67470c90ed904569d6d6953016ab3e
Andrew Cagney <cagney@gnu.org>
2026-04-22 12:29:24 -0400

    testing kvm: don't try to disable NetworkManager on debian
    it isn't there

--- testing/kvm/systemd/transmogrify-networkd.sh
+++ testing/kvm/systemd/transmogrify-networkd.sh
@@ -3,11 +3,8 @@
 set -xe ; exec < /dev/null
 
 :
-: systemd-networkd
+: Switch from NetworkManager to systemd-networkd+systemd-resolved
 :
-
-systemctl enable systemd-networkd.service
-systemctl enable systemd-networkd-wait-online.service
 
 # Drop hostname, instead rely either on the kernel cmdline and
 # systemd.hostname=... or zzz.eth0.network which defaults to the local
@@ -18,7 +15,13 @@ rm -f /etc/systemd/network/*
 cp -v /bench/testing/kvm/systemd/network/*.network /etc/systemd/network/
 sed -i -e "s/@@DOMAIN_PLATFORM@@/${PLATFORM}/" /etc/systemd/network/*.network
 
-# test specific configs; bound using ethernet addresses
+# Debian, for instance, doesn't have NetworkManager
+if systemctl status NetworkManager > /dev/null ; then
+    systemctl disable NetworkManager
+fi
+
+systemctl enable systemd-networkd.service
+systemctl enable systemd-networkd-wait-online.service
 
 if test -x /usr/sbin/restorecon ; then
     restorecon -R /etc/systemd/network
