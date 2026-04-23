6d6494b346b2c54bade6e02a7951d168331f9cfe
Andrew Cagney <cagney@gnu.org>
2026-04-23 17:06:23 -0400

    testing: switch addresspool-6in6-strongswan-initiator-ikev2 to swanctl.conf

--- testing/pluto/addresspool-6in6-strongswan-initiator-ikev2/westinit.sh
+++ testing/pluto/addresspool-6in6-strongswan-initiator-ikev2/westinit.sh
@@ -4,4 +4,5 @@ ip6tables -A INPUT -i eth1 -s 2001:db8:0:2::254 -p ipv6-icmp -j DROP
 ip6tables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
 ../../guestbin/ping-once.sh --down 2001:db8:0:2::254
 ../../guestbin/strongswan-start.sh
+swanctl --load-conns
 echo "initdone"
