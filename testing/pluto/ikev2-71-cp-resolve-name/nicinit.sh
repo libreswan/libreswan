7a14cfa78c8c9dbec9ec7d069c8cddf43422c000
Andrew Cagney <cagney@gnu.org>
2026-04-25 16:36:38 -0400

    testing: delete dnssec.sh, use start-dns.sh on nic
    a continuation of:
      testing: replace swan-prep --dnssec with dnssec.sh
      see #2678 drop swan-prep --dnssec
    and now there is one dns script!

--- testing/pluto/ikev2-71-cp-resolve-name/nicinit.sh
+++ testing/pluto/ikev2-71-cp-resolve-name/nicinit.sh
@@ -1,11 +1,6 @@
 iptables -t nat -F
 iptables -F
 setenforce Permissive
-/testing/guestbin/dnssec.sh
-dig +short  @127.0.0.1 road.testing.libreswan.org
-dig +short  @127.0.0.1 east.testing.libreswan.org
-: ==== cut ====
-dig +short @192.1.2.254 chaos version.server txt
-: ==== tuc ====
+/testing/guestbin/start-dns.sh
 echo "initdone"
 : ==== end ====
