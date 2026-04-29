b625272258fdedd9dfcb866d75fda6940506446e
Andrew Cagney <cagney@gnu.org>
2026-04-29 16:44:47 -0400

    testing x509: on ROAD, don't import then delete EAST (+ROAD) certs
    (It still deletes ROAD)
    
    Like OCSP tests, only import what ROAD needs - cert-chain+private key.

--- testing/pluto/certoe-06-nat-packet-cop/roadinit.sh
+++ testing/pluto/certoe-06-nat-packet-cop/roadinit.sh
@@ -1,6 +1,6 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/road.p12
 ipsec certutil -D -n road
-ipsec certutil -D -n east
 cp road-ikev2-oe.conf /etc/ipsec.d/ikev2-oe.conf
 cp policies/* /etc/ipsec.d/policies/
 echo "192.1.2.0/24"  >> /etc/ipsec.d/policies/private-or-clear
