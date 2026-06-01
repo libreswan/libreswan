1e7ba00217f71bafbccd6eb3f0693990a4f3ba22
Andrew Cagney <cagney@gnu.org>
2026-05-31 22:13:55 -0400

    testing ttodatacheck: rework ttodata() calls
    Don't rely on:
        check(n, ttodata(&n))
    passing in an up-to-date N

--- testing/programs/ttodatacheck/ttodatacheck.c
+++ testing/programs/ttodatacheck/ttodatacheck.c
@@ -22,9 +22,12 @@
 #include "constants.h"
 #include "ttodata.h"
 
+#define LN __LINE__
+
 struct artab;
-static void check(struct artab *r, char *buf, size_t n, err_t oops,
-		  int *status);
+static void check_ttodata(unsigned line, const struct artab *r,
+			  shunk_t ascii, int base,
+			  int *status);
 static void regress(void);
 static void hexout(const char *s, size_t len, FILE *f);
 
@@ -120,123 +123,134 @@ static void hexout(const char *s,
 		fprintf(f, "%02x", (unsigned char)s[i]);
 }
 
-struct artab {
+const struct artab {
+	unsigned line;
 	int base;
 	char *ascii;	/* NULL for end */
 	char *data;	/* NULL for error expected */
 } atodatatab[] = {
-	{ 0, "", NULL, },
-	{ 0, "0", NULL, },
-	{ 0, "0x", NULL, },
-	{ 0, "0xa", NULL, },
-	{ 0, "0xab", "\xab", },
-	{ 0, "0xabc", NULL, },
-	{ 0, "0xabcd", "\xab\xcd", },
-	{ 0, "0x0123456789", "\x01\x23\x45\x67\x89", },
-	{ 0, "0x01x", NULL, },
-	{ 0, "0xabcdef", "\xab\xcd\xef", },
-	{ 0, "0xABCDEF", "\xab\xcd\xef", },
-	{ 0, "0XaBc0eEd81f", "\xab\xc0\xee\xd8\x1f", },
-	{ 0, "0XaBc0_eEd8", "\xab\xc0\xee\xd8", },
-	{ 0, "0XaBc0_", NULL, },
-	{ 0, "0X_aBc0", NULL, },
-	{ 0, "0Xa_Bc0", NULL, },
-	{ 16, "aBc0eEd8", "\xab\xc0\xee\xd8", },
-	{ 0, "0s", NULL, },
-	{ 0, "0sA", NULL, },
-	{ 0, "0sBA", NULL, },
-	{ 0, "0sCBA", NULL, },
-	{ 0, "0sDCBA", "\x0c\x20\x40", },
-	{ 0, "0SDCBA", "\x0c\x20\x40", },
-	{ 0, "0sDA==", "\x0c", },
-	{ 0, "0sDC==", NULL, },
-	{ 0, "0sDCA=", "\x0c\x20", },
-	{ 0, "0sDCB=", NULL, },
-	{ 0, "0sDCAZ", "\x0c\x20\x19", },
-	{ 0, "0sDCAa", "\x0c\x20\x1a", },
-	{ 0, "0sDCAz", "\x0c\x20\x33", },
-	{ 0, "0sDCA0", "\x0c\x20\x34", },
-	{ 0, "0sDCA9", "\x0c\x20\x3d", },
-	{ 0, "0sDCA+", "\x0c\x20\x3e", },
-	{ 0, "0sDCA/", "\x0c\x20\x3f", },
-	{ 0, "0sAbraCadabra+", "\x01\xba\xda\x09\xa7\x5a\x6e\xb6\xbe", },
-	{ 0, "0t", NULL, },
-	{ 0, "0tabc_xyz", "abc_xyz", },
-	{ 256, "abc_xyz", "abc_xyz", },
-	{ 0, NULL, NULL, },
+	{ LN, 0, "", NULL, },
+	{ LN, 0, "0", NULL, },
+	{ LN, 0, "0x", NULL, },
+	{ LN, 0, "0xa", NULL, },
+	{ LN, 0, "0xab", "\xab", },
+	{ LN, 0, "0xabc", NULL, },
+	{ LN, 0, "0xabcd", "\xab\xcd", },
+	{ LN, 0, "0x0123456789", "\x01\x23\x45\x67\x89", },
+	{ LN, 0, "0x01x", NULL, },
+	{ LN, 0, "0xabcdef", "\xab\xcd\xef", },
+	{ LN, 0, "0xABCDEF", "\xab\xcd\xef", },
+	{ LN, 0, "0XaBc0eEd81f", "\xab\xc0\xee\xd8\x1f", },
+	{ LN, 0, "0XaBc0_eEd8", "\xab\xc0\xee\xd8", },
+	{ LN, 0, "0XaBc0_", NULL, },
+	{ LN, 0, "0X_aBc0", NULL, },
+	{ LN, 0, "0Xa_Bc0", NULL, },
+	{ LN, 16, "aBc0eEd8", "\xab\xc0\xee\xd8", },
+	{ LN, 0, "0s", NULL, },
+	{ LN, 0, "0sA", NULL, },
+	{ LN, 0, "0sBA", NULL, },
+	{ LN, 0, "0sCBA", NULL, },
+	{ LN, 0, "0sDCBA", "\x0c\x20\x40", },
+	{ LN, 0, "0SDCBA", "\x0c\x20\x40", },
+	{ LN, 0, "0sDA==", "\x0c", },
+	{ LN, 0, "0sDC==", NULL, },
+	{ LN, 0, "0sDCA=", "\x0c\x20", },
+	{ LN, 0, "0sDCB=", NULL, },
+	{ LN, 0, "0sDCAZ", "\x0c\x20\x19", },
+	{ LN, 0, "0sDCAa", "\x0c\x20\x1a", },
+	{ LN, 0, "0sDCAz", "\x0c\x20\x33", },
+	{ LN, 0, "0sDCA0", "\x0c\x20\x34", },
+	{ LN, 0, "0sDCA9", "\x0c\x20\x3d", },
+	{ LN, 0, "0sDCA+", "\x0c\x20\x3e", },
+	{ LN, 0, "0sDCA/", "\x0c\x20\x3f", },
+	{ LN, 0, "0sAbraCadabra+", "\x01\xba\xda\x09\xa7\x5a\x6e\xb6\xbe", },
+	{ LN, 0, "0t", NULL, },
+	{ LN, 0, "0tabc_xyz", "abc_xyz", },
+	{ LN, 256, "abc_xyz", "abc_xyz", },
+	{ LN, 0, NULL, NULL, },
 };
 
-struct drtab {
+const struct drtab {
+	unsigned line;
 	char *data;	/* input; NULL for end */
 	char format;
 	int buflen;	/* -1 means big buffer */
 	int outlen;	/* -1 means strlen(ascii)+1 */
 	char *ascii;	/* NULL for error expected */
 } datatoatab[] = {
-	{ "", 'x', -1, -1, NULL, },
-	{ "", 'X', -1, -1, NULL, },
-	{ "", 'n', -1, -1, NULL, },
-	{ "0", 'x', -1, -1, "0x30", },
-	{ "0", 'x', 0, 5, "---", },
-	{ "0", 'x', 1, 5, "", },
-	{ "0", 'x', 2, 5, "0", },
-	{ "0", 'x', 3, 5, "0x", },
-	{ "0", 'x', 4, 5, "0x3", },
-	{ "0", 'x', 5, 5, "0x30", },
-	{ "0", 'x', 6, 5, "0x30", },
-	{ "\xab\xcd", 'x', -1, -1, "0xabcd", },
-	{ "\x01\x23\x45\x67\x89", 'x', -1, -1, "0x0123456789",
+	{ LN, "", 'x', -1, -1, NULL, },
+	{ LN, "", 'X', -1, -1, NULL, },
+	{ LN, "", 'n', -1, -1, NULL, },
+	{ LN, "0", 'x', -1, -1, "0x30", },
+	{ LN, "0", 'x', 0, 5, "---", },
+	{ LN, "0", 'x', 1, 5, "", },
+	{ LN, "0", 'x', 2, 5, "0", },
+	{ LN, "0", 'x', 3, 5, "0x", },
+	{ LN, "0", 'x', 4, 5, "0x3", },
+	{ LN, "0", 'x', 5, 5, "0x30", },
+	{ LN, "0", 'x', 6, 5, "0x30", },
+	{ LN, "\xab\xcd", 'x', -1, -1, "0xabcd", },
+	{ LN, "\x01\x23\x45\x67\x89", 'x', -1, -1, "0x0123456789",
 	},
-	{ "\xab\xcd\xef", 'x', -1, -1, "0xabcdef", },
-	{ "\xab\xc0\xee\xd8\x1f", 'x', -1, -1, "0xabc0eed81f",
+	{ LN, "\xab\xcd\xef", 'x', -1, -1, "0xabcdef", },
+	{ LN, "\xab\xc0\xee\xd8\x1f", 'x', -1, -1, "0xabc0eed81f",
 	},
-	{ "\x01\x02", 'h', -1, -1, "0x0102", },
-	{ "\x01\x02\x03\x04\x05\x06", 'h', -1, -1, "0x01020304_0506", },
-	{ "\xab\xc0\xee\xd8\x1f", 16, -1, -1, "abc0eed81f",
+	{ LN, "\x01\x02", 'h', -1, -1, "0x0102", },
+	{ LN, "\x01\x02\x03\x04\x05\x06", 'h', -1, -1, "0x01020304_0506", },
+	{ LN, "\xab\xc0\xee\xd8\x1f", 16, -1, -1, "abc0eed81f",
 	},
-	{ "\x0c\x20\x40", 's', -1, -1, "0sDCBA", },
-	{ "\x0c\x20\x40", 's', 0, 7, "---", },
-	{ "\x0c\x20\x40", 's', 1, 7, "", },
-	{ "\x0c\x20\x40", 's', 2, 7, "0", },
-	{ "\x0c\x20\x40", 's', 3, 7, "0s", },
-	{ "\x0c\x20\x40", 's', 4, 7, "0sD", },
-	{ "\x0c\x20\x40", 's', 5, 7, "0sDC", },
-	{ "\x0c\x20\x40", 's', 6, 7, "0sDCB", },
-	{ "\x0c\x20\x40", 's', 7, 7, "0sDCBA", },
-	{ "\x0c\x20\x40", 's', 8, 7, "0sDCBA", },
-	{ "\x0c", 's', -1, -1, "0sDA==", },
-	{ "\x0c\x20", 's', -1, -1, "0sDCA=", },
-	{ "\x0c\x20\x19", 's', -1, -1, "0sDCAZ", },
-	{ "\x0c\x20\x1a", 's', -1, -1, "0sDCAa", },
-	{ "\x0c\x20\x33", 's', -1, -1, "0sDCAz", },
-	{ "\x0c\x20\x34", 's', -1, -1, "0sDCA0", },
-	{ "\x0c\x20\x3d", 's', -1, -1, "0sDCA9", },
-	{ "\x0c\x20\x3e", 's', -1, -1, "0sDCA+", },
-	{ "\x0c\x20\x3f", 's', -1, -1, "0sDCA/", },
-	{ "\x01\xba\xda\x09\xa7\x5a\x6e\xb6\xbe", 's', -1, -1, "0sAbraCadabra+", },
-	{ "\x01\xba\xda\x09\xa7\x5a\x6e\xb6\xbe", 64, -1, -1, "AbraCadabra+", },
-	{ NULL, 'x', -1, -1, NULL, },
+	{ LN, "\x0c\x20\x40", 's', -1, -1, "0sDCBA", },
+	{ LN, "\x0c\x20\x40", 's', 0, 7, "---", },
+	{ LN, "\x0c\x20\x40", 's', 1, 7, "", },
+	{ LN, "\x0c\x20\x40", 's', 2, 7, "0", },
+	{ LN, "\x0c\x20\x40", 's', 3, 7, "0s", },
+	{ LN, "\x0c\x20\x40", 's', 4, 7, "0sD", },
+	{ LN, "\x0c\x20\x40", 's', 5, 7, "0sDC", },
+	{ LN, "\x0c\x20\x40", 's', 6, 7, "0sDCB", },
+	{ LN, "\x0c\x20\x40", 's', 7, 7, "0sDCBA", },
+	{ LN, "\x0c\x20\x40", 's', 8, 7, "0sDCBA", },
+	{ LN, "\x0c", 's', -1, -1, "0sDA==", },
+	{ LN, "\x0c\x20", 's', -1, -1, "0sDCA=", },
+	{ LN, "\x0c\x20\x19", 's', -1, -1, "0sDCAZ", },
+	{ LN, "\x0c\x20\x1a", 's', -1, -1, "0sDCAa", },
+	{ LN, "\x0c\x20\x33", 's', -1, -1, "0sDCAz", },
+	{ LN, "\x0c\x20\x34", 's', -1, -1, "0sDCA0", },
+	{ LN, "\x0c\x20\x3d", 's', -1, -1, "0sDCA9", },
+	{ LN, "\x0c\x20\x3e", 's', -1, -1, "0sDCA+", },
+	{ LN, "\x0c\x20\x3f", 's', -1, -1, "0sDCA/", },
+	{ LN, "\x01\xba\xda\x09\xa7\x5a\x6e\xb6\xbe", 's', -1, -1, "0sAbraCadabra+", },
+	{ LN, "\x01\xba\xda\x09\xa7\x5a\x6e\xb6\xbe", 64, -1, -1, "AbraCadabra+", },
+	{ LN, NULL, 'x', -1, -1, NULL, },
 };
 
 /*
  * regress - regression-test ttodata() and datatot()
  */
-static void check(struct artab *r, char *buf, size_t n, err_t oops, int *status)
+static void check_ttodata(unsigned line, const struct artab *r,
+			  shunk_t ascii, int base,
+			  int *status)
 {
+	char buf[100];
+	size_t n;
+	err_t oops = ttodata(ascii, base, buf, sizeof(buf), &n);
+
 	if (oops != NULL && r->data == NULL) {
 		/* error expected */
 	} else if (oops != NULL) {
+		printf("+%u +%u %s: ", r->line, line, __FILE__);
 		printf("`%s' gave error `%s', expecting %zu `", r->ascii,
-			oops, strlen(r->data));
+		       oops, strlen(r->data));
 		hexout(r->data, strlen(r->data), stdout);
 		printf("'\n");
 		*status = 1;
 	} else if (r->data == NULL) {
+		printf("+%u +%u %s: ", r->line, line, __FILE__);
 		printf("`%s' gave %zu `", r->ascii, n);
 		hexout(buf, n, stdout);
 		printf("', expecting error\n");
 		*status = 1;
 	} else if (n != strlen(r->data)) {
+		printf("+%u +%u %s: ", r->line, line, __FILE__);
 		printf("length wrong in `%s': got %zu `", r->ascii, n);
 		hexout(buf, n, stdout);
 		printf("', expecting %zu `", strlen(r->data));
@@ -244,6 +258,7 @@ static void check(struct artab *r, char *buf, size_t n, err_t oops, int *status)
 		printf("'\n");
 		*status = 1;
 	} else if (!memeq(buf, r->data, n)) {
+		printf("+%u +%u %s: ", r->line, line, __FILE__);
 		printf("`%s' gave %zu `", r->ascii, n);
 		hexout(buf, n, stdout);
 		printf("', expecting %zu `", strlen(r->data));
@@ -256,16 +271,12 @@ static void check(struct artab *r, char *buf, size_t n, err_t oops, int *status)
 
 static void regress(void)
 {
-	struct artab *r;
-	struct drtab *dr;
-	char buf[100];
-	size_t n;
 	int status = 0;
 
-	for (r = atodatatab; r->ascii != NULL; r++) {
+	for (const struct artab *r = atodatatab; r->ascii != NULL; r++) {
 		int base = r->base;
-		int xbase = 0;
 
+		int xbase = 0;
 		if (base == 0 && r->ascii[0] == '0') {
 			switch (r->ascii[1]) {
 			case 'x':
@@ -283,30 +294,29 @@ static void regress(void)
 			}
 		}
 
-		check(r, buf, n,
-		      ttodata(shunk1(r->ascii), base,
-			      buf, sizeof(buf), &n),
-		      &status);
-		if (base == 64 || xbase == 64)
-			check(r, buf, n,
-			      ttodata(shunk1(r->ascii), base,
-				      buf, sizeof(buf), &n),
-			      &status);
+		check_ttodata(LN, r, shunk1(r->ascii),
+			      base, &status);
+		if (base == 64 || xbase == 64) {
+			check_ttodata(LN, r, shunk1(r->ascii),
+				      base, &status);
+		}
 
-		if (xbase != 0) {
-			check(r, buf, n,
-			      ttodata(shunk1(r->ascii + 2), xbase,
-				      buf, sizeof(buf), &n),
-			      &status);
-			if (base == 64 || xbase == 64)
-				check(r, buf, n,
-				      ttodata(shunk1(r->ascii + 2), xbase,
-					      buf, sizeof(buf), &n),
-				      &status);
+		if (xbase == 0) {
+			continue;
+		}
+
+		check_ttodata(LN, r, shunk1(r->ascii + 2),
+			      xbase, &status);
+		if (base == 64 || xbase == 64) {
+			check_ttodata(LN, r, shunk1(r->ascii + 2),
+				      xbase, &status);
 		}
 	}
 
-	for (dr = datatoatab; dr->data != NULL; dr++) {
+	char buf[100];
+	size_t n;
+
+	for (const struct drtab *dr = datatoatab; dr->data != NULL; dr++) {
 		strcpy(buf, "---");
 		n = datatot(dr->data, strlen(dr->data), dr->format, buf,
 			    (dr->buflen == -1) ? sizeof(buf) : (size_t)dr->buflen);
@@ -316,24 +326,28 @@ static void regress(void)
 		if (n == 0 && dr->ascii == NULL) {
 			/* error expected */
 		} else if (n == 0) {
+			printf("+%u %s: ", dr->line, __FILE__);
 			printf("`");
 			hexout(dr->data, strlen(dr->data), stdout);
 			printf("' %c gave error, expecting %zu `%s'\n",
 			       dr->format, should, dr->ascii);
 			status = 1;
 		} else if (dr->ascii == NULL) {
+			printf("+%u %s: ", dr->line, __FILE__);
 			printf("`");
 			hexout(dr->data, strlen(dr->data), stdout);
 			printf("' %c gave %zu `%.*s', expecting error\n",
 			       dr->format, n, (int)n, buf);
 			status = 1;
 		} else if (n != should) {
+			printf("+%u %s: ", dr->line, __FILE__);
 			printf("length wrong in `");
 			hexout(dr->data, strlen(dr->data), stdout);
 			printf("': got %zu `%s'", n, buf);
 			printf(", expecting %zu `%s'\n", should, dr->ascii);
 			status = 1;
 		} else if (!streq(buf, dr->ascii)) {
+			printf("+%u %s: ", dr->line, __FILE__);
 			printf("`");
 			hexout(dr->data, strlen(dr->data), stdout);
 			printf("' gave %zu `%s'", n, buf);
