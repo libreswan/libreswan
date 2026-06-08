d716f81f829f62f5f940d4814b25820af9a87511
Andrew Cagney <cagney@gnu.org>
2026-06-08 16:20:39 -0400

    testing: in ikev2-ppk-intermediate-02-addke use addke1= et.al.
    and only specify intermedate=yes when needed
    (when there's no negotiated addke)

--- testing/pluto/ikev2-ppk-intermediate-02-addke/westrun.sh
+++ testing/pluto/ikev2-ppk-intermediate-02-addke/westrun.sh
@@ -1,5 +1,6 @@
-../../guestbin/pluto-up-down.sh 'ike=aes;dh20'                     -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
-../../guestbin/pluto-up-down.sh 'ike=aes;dh20-none'                -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
-../../guestbin/pluto-up-down.sh 'ike=aes;dh20-none-ml_kem_768'     -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
-../../guestbin/pluto-up-down.sh 'ike=aes;dh20-modp8192-none'       -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
-../../guestbin/pluto-up-down.sh 'ike=aes;dh20-modp8192-ml_kem_768' -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
+../../guestbin/pluto-up-down.sh 'ike=aes;dh20'                                   -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
+../../guestbin/pluto-up-down.sh 'ike=aes;dh20' intermediate=yes                  -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
+../../guestbin/pluto-up-down.sh 'ike=aes;dh20;addke1=none'                       -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
+../../guestbin/pluto-up-down.sh 'ike=aes;dh20;addke1=none;addke2=ml_kem_768'     -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
+../../guestbin/pluto-up-down.sh 'ike=aes;dh20;addke1=modp8192;addke2=none'       -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
+../../guestbin/pluto-up-down.sh 'ike=aes;dh20;addke1=modp8192;addke2=ml_kem_768' -- -I 192.0.1.254 192.0.2.254 # sanitize-retransmits
