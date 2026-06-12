2a52eb993c5211d3e2dee0cbebe429da666fe1ec
Andrew Cagney <cagney@gnu.org>
2026-06-12 19:23:34 -0400

    testing: update ikev2-labeled-ipsec-04-no-label-on-responder-ike-ondemand
    match all log lines containing TS_UNACCEPTABLE

--- testing/pluto/ikev2-labeled-ipsec-04-no-label-on-responder-ike-ondemand/westrun.sh
+++ testing/pluto/ikev2-labeled-ipsec-04-no-label-on-responder-ike-ondemand/westrun.sh
@@ -1,15 +1,23 @@
 ipsec whack --impair suppress_retransmits
+
 # IKE will be triggered by acquire; expect two labels
 ipsec route labeled
 ipsec _kernel state
 ipsec _kernel policy
+
 # trigger acquire using the predefined ping_t context; won't work
+
 ../../guestbin/ping-once.sh --runcon "system_u:system_r:ping_t:s0:c1.c256" --forget -I 192.1.2.45 192.1.2.23
-../../guestbin/wait-for-pluto.sh 'TS_UNACCEPTABLE'
+
+../../guestbin/wait-for-pluto.sh 'processing IKE_AUTH response'
+../../guestbin/wait-for-pluto.sh 'IKE_AUTH response rejected Child SA'
+../../guestbin/wait-for-pluto.sh 'peer rejected Child SA'
+
 # there should be 0 tunnels - child rejected
 ipsec trafficstatus
 # there should be no bare shunts
 ipsec shuntstatus
+
 # let larval state expire
 ../../guestbin/wait-for.sh --no-match ' spi 0x00000000 ' -- ipsec _kernel state
 echo done
