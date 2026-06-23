e1588e06a30dac7727ad4843082a027cadab805c
Andrew Cagney <cagney@gnu.org>
2026-06-23 10:04:21 -0400

    testing: fix zombie pluto in seccomp-03-updown-ikev2
    ... need to restart systemd!?!
    
    close #492 seccomp-03-updown hangs as pluto turned into a zombie

--- testing/pluto/seccomp-03-updown-ikev2/roadrun.sh
+++ testing/pluto/seccomp-03-updown-ikev2/roadrun.sh
@@ -1,33 +1,45 @@
 ipsec up westnet-eastnet-ipv4-psk-ikev2
 ipsec trafficstatus
-sleep 5
 
 # confirm we updated unbound - should not be empty once DNS CP is
 # added
+
 unbound-control list_forwards
 ipsec down westnet-eastnet-ipv4-psk-ikev2
 
 # try some things to trigger new syscalls
+
 ipsec status > /dev/null
 ipsec whack --listen > /dev/null
-ipsec whack --globalstatus > /dev/null
-ipsec whack --shuntstatus > /dev/null
+ipsec globalstatus > /dev/null
+ipsec shuntstatus > /dev/null
 ipsec secrets > /dev/null
 ipsec delete westnet-eastnet-ipv4-psk-ikev2
 ipsec stop
 
 # ensure seccomp did not kill pluto - should not show any hits
+
 ausearch -ts recent -i -m SECCOMP
 
 # Test seccomp actually works - that it records pluto crashing ....
+
 ipsec start
 ../../guestbin/wait-until-pluto-started
 
 # Since whack hangs - it does not know that pluto died - run it in the
 # background.  Give it a few seconds to do its job.
-ipsec whack --seccomp-crashtest & sleep 2
+
+ipsec whack --seccomp-crashtest & pid=$! ; sleep 2
+kill ${pid} ; sleep 1
 
 # should show 1 entry of pluto crashing now
+
 ausearch -ts recent -i -m SECCOMP | sed -e "s/ ip=[^ ]* / ip=XXX /" -e "s/ pid=[^ ]* / pid=XXX /" -e "s/msg=audit(.*) /msg=audit(XXX) /"
+
+# cleanup the zombie child; systemctl asks the console for its size!
+# And remove ctl files
+
+nohup systemctl daemon-reexec > OUTPUT/$(hostname).systemd-restart.log
+rm /var/run/pluto/*
 
 echo done
