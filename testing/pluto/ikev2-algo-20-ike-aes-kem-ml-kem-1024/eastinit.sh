25e616cb20c49b3af76f9174d033e9ac2937ef43
Ondrej Moris <omoris@redhat.com>
2026-06-29 13:46:44 +0200

    testing: extend test covereage for ML-KEM
    The following tests added:
    
    ikev2-intermediate-07-fallback-no-intermediate
    - Related to Issue #2834
    - Test ubutuatuib fallback to classical when responder lacks
      ML-KEM support
    
    ikev2-intermediate-08-addke-ml-kem-1024
    - Test multiple key exchanges in IKEv2 (RFC 9370), with DH31
      and ML-KEM-1024
    
    ikev2-intermediate-09-max-size-hybrid
    - Test realistic maximum-size hybrid configuration
    
    ikev2-algo-20-ike-aes-kem-ml-kem-1024
    - Test pure ML-KEM-1024
    
    Signed-off-by: Ondrej Moris <omoris@redhat.com>

---
+++ testing/pluto/ikev2-algo-20-ike-aes-kem-ml-kem-1024/eastinit.sh
@@ -0,0 +1,4 @@
+../../guestbin/swan-prep --46
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec add east
---
+++ testing/pluto/ikev2-intermediate-09-max-size-hybrid/westrun.sh
@@ -0,0 +1,4 @@
+ipsec up west
+# verify fragmentation occurred with large keys
+../../guestbin/wait-for-pluto.sh 'reassembled from'
+echo done
