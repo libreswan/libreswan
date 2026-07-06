78e592b0d132f0d6c5798bb58d596d135ca2cb23
Andrew Cagney <cagney@gnu.org>
2026-07-06 17:12:35 -0400

    connection: change type of struct host_end .port from uint16_t to ip_port
    update references:
    
    - use PRI_HPORT() / pri_hport() when printing
    - use endpoint_port() and not endpoint_hport() when extracting
    
    Also update comments around .port, .addr, and .first_addr.

--- programs/pluto/ikev2_mobike.c
+++ programs/pluto/ikev2_mobike.c
@@ -154,27 +154,33 @@ static bool update_mobike_endpoints(struct ike_sa *ike, const struct msg_digest 
 
 	switch (md_role) {
 	case MESSAGE_RESPONSE:
+	{
 		/* MOBIKE initiator processing response */
 		c->local->host.addr = endpoint_address(child->sa.st_v2_mobike.local_endpoint);
-		ldbg(logger, "%s() %s.host_port: %u->%u", __func__,
+		ip_port new_port = endpoint_port(child->sa.st_v2_mobike.local_endpoint);
+		ldbg(logger, "%s() %s.host_port: "PRI_HPORT"->"PRI_HPORT, __func__,
 		     c->local->config->leftright,
-		     c->local->host.port,
-		     endpoint_hport(child->sa.st_v2_mobike.local_endpoint, HERE));
-		c->local->host.port =
-			endpoint_hport(child->sa.st_v2_mobike.local_endpoint, HERE);
+		     pri_hport(c->local->host.port),
+		     pri_hport(new_port));
+		c->local->host.port = new_port;
 		c->local->host.nexthop = child->sa.st_v2_mobike.host_nexthop;
 		break;
+	}
 	case MESSAGE_REQUEST:
+	{
 		/* MOBIKE responder processing request */
 		c->remote->host.addr = endpoint_address(md->sender);
-		ldbg(logger, "%s() %s.host_port: %u->%u",
+		ip_port new_port = endpoint_port(md->sender);
+		ldbg(logger, "%s() %s.host_port: "PRI_HPORT"->"PRI_HPORT,
 		     __func__, c->remote->config->leftright,
-		     c->remote->host.port, endpoint_hport(md->sender, HERE));
-		c->remote->host.port = endpoint_hport(md->sender, HERE);
+		     pri_hport(c->remote->host.port),
+		     pri_hport(new_port));
+		c->remote->host.port = new_port;
 
 		/* for the consistency, correct output in ipsec status */
 		child->sa.st_remote_endpoint = ike->sa.st_remote_endpoint = md->sender;
 		break;
+	}
 	default:
 		bad_case(md_role);
 	}
--- programs/pluto/ipsec_doi.c
+++ programs/pluto/ipsec_doi.c
@@ -132,8 +132,8 @@ void jam_child_sa_details(struct jambuf *buf, struct state *st)
 
 		if (nat) {
 			ldbg(st->logger,
-			     "NAT-T: NAT Traversal detected - their IKE port is '%d'",
-			     c->remote->host.port);
+			     "NAT-T: NAT Traversal detected - their IKE port is '"PRI_HPORT"'",
+			     pri_hport(c->remote->host.port));
 		}
 
 		jam(buf, "ESP%s%s%s%s=>0x%08" PRIx32 " <0x%08" PRIx32 "",
