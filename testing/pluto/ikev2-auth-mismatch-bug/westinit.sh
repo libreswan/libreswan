6751e651927dc91ed6702f58002bba63dc1d12a7
Vukašin Karadžić <vukasin.karadzic@gmail.com>
2026-07-20 20:33:09 +0200

    testing: rename, refactor and simplify pluto/ikev2-x509-ecdsa-04-also-rsa/ test

--- testing/pluto/ikev2-auth-mismatch-bug/westinit.sh
+++ testing/pluto/ikev2-auth-mismatch-bug/westinit.sh
@@ -1,4 +1,5 @@
-/testing/guestbin/swan-prep
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainec/`hostname`.p12
 # confirm that the network is alive
 ../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
 # ensure that clear text does not get through
@@ -6,13 +7,8 @@ iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
 # confirm clear text does not get through
 ../../guestbin/ping-once.sh --down -I 192.0.1.254 192.0.2.254
-/testing/x509/import.sh real/mainec/`hostname`.p12
-# Tuomo: why doesn't ipsec checknss --settrust work here?
-ipsec certutil -M -n "strongSwan CA - strongSwan" -t CT,,
-#ipsec start
-ipsec pluto --config /etc/ipsec.conf
+ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-ikev2
-ipsec status
 ipsec whack --impair suppress_retransmits
 echo "initdone"
