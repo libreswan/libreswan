bb7980b83626035cf4cb6de31f4145f56e1fa513
Daiki Ueno <dueno@redhat.com>
2026-07-22 18:12:28 +0900

    ikev2: add test for responder omitting ADDITIONAL_KEY_EXCHANGE
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

---
+++ testing/pluto/ikev2-intermediate-12-rekey-ike-omit-addke-responder/02-west-init.sh
@@ -0,0 +1,14 @@
+/testing/guestbin/swan-prep --46 --nokey
+
+../../guestbin/ip.sh address show eth0 | grep global | sort
+
+ipsec start
+../../guestbin/wait-until-pluto-started
+
+ipsec whack --impair revival
+ipsec whack --impair suppress_retransmits
+
+ipsec add west-cuckold
+ipsec add west-cuckoo
+
+echo "initdone"
