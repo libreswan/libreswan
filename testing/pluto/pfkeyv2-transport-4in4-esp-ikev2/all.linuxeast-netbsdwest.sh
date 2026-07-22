e16bc8a23164e9093205d2d45997f8e52dba371e
Andrew Cagney <cagney@gnu.org>
2026-07-22 13:09:23 -0400

    testing: add all.platform-...sh to tests with all.console.txt
    ... so that ./kvm will use the *.sh file for commands

---
+++ testing/pluto/pfkeyv2-transport-4in4-esp-ikev2/all.linuxeast-netbsdwest.sh
@@ -0,0 +1,18 @@
+east# /testing/guestbin/swan-prep --46
+east# ipsec start
+east# ../../guestbin/wait-until-pluto-started
+east# ipsec add eastnet-westnet-ikev2
+east# ipsec whack --impair suppress_retransmits
+east# echo "initdone"
+west# ../../guestbin/prep.sh
+west# ipsec start
+west# ../../guestbin/wait-until-pluto-started
+west# ipsec add eastnet-westnet-ikev2
+west# echo "initdone"
+west# ipsec up eastnet-westnet-ikev2
+west# ipsec _kernel policy
+west# ../../guestbin/ping-once.sh --up 192.1.2.23
+west# ipsec _kernel state
+west# ../../guestbin/ping-once.sh --medium --up 192.1.2.23
+west# ipsec _kernel state
+west# dmesg | grep ipsec
