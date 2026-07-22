2b6d4b1f25f13ad1b2dc5751d8740a95ab9d99f2
Daiki Ueno <dueno@redhat.com>
2026-07-22 18:12:28 +0900

    ikev2: add test for responder omitting ADDITIONAL_KEY_EXCHANGE
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

---
+++ testing/pluto/ikev2-intermediate-12-rekey-ike-omit-addke-responder/03-west-up.sh
@@ -0,0 +1,6 @@
+ipsec up west-cuckold
+ipsec up west-cuckoo
+
+ipsec whack --rekey-ike --name west-cuckold --asynchronous
+
+../../guestbin/wait-for-pluto.sh '#4: missing ADDITIONAL_KEY_EXCHANGE notification'
