0db33bc0d284960c4ab429b42057a56036bfdc7e
Andrew Cagney <cagney@gnu.org>
2026-07-25 14:02:52 -0400

    testing: for --46 EAST-ROAD tests, don't load NIC, NORTH, WEST certs
    replace --x509 with explicit import.sh

--- testing/pluto/addresspool-6in6-defaultroute-dual-dns-ikev2/eastinit.sh
+++ testing/pluto/addresspool-6in6-defaultroute-dual-dns-ikev2/eastinit.sh
@@ -1,4 +1,6 @@
-/testing/guestbin/swan-prep --46 --x509
+/testing/guestbin/swan-prep --46 --nokeys
+/testing/x509/import.sh real/mainca/east.p12
+/testing/x509/import.sh real/mainca/road.end.cert
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec add east
