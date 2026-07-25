5ee46d05ff9a06233eba76d3b2b717173e789429
Andrew Cagney <cagney@gnu.org>
2026-07-25 09:09:37 -0400

    testing: for EAST-ROAD-NORTH tests, don't load NIC, WEST certs
    Replace --x509 with an explict import.sh

--- testing/pluto/addresspool-4in4-in-use-ikev2-ikev1/01-east-init.sh
+++ testing/pluto/addresspool-4in4-in-use-ikev2-ikev1/01-east-init.sh
@@ -1,4 +1,7 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/east.p12
+/testing/x509/import.sh real/mainca/road.end.cert
+/testing/x509/import.sh real/mainca/north.end.cert
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec add any-east # alias
--- testing/pluto/ikev2-liveness-08/eastinit.sh
+++ testing/pluto/ikev2-liveness-08/eastinit.sh
@@ -1,4 +1,7 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/east.p12
+/testing/x509/import.sh real/mainca/road.end.cert
+/testing/x509/import.sh real/mainca/north.end.cert
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add rw-east-pool-x509-ipv4
