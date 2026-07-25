e8c3b08f41b49ef788e32e21d1fa015a1564aea2
Andrew Cagney <cagney@gnu.org>
2026-07-25 13:36:49 -0400

    testing: for EAST-ROAD-WEST, don't load NIC, NORTH certs
    replace -x509 with explicit import.sh

--- testing/pluto/ikev2-48-nat-cp-two-servers-same/roadinit.sh
+++ testing/pluto/ikev2-48-nat-cp-two-servers-same/roadinit.sh
@@ -1,4 +1,7 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/road.p12
+/testing/x509/import.sh real/mainca/east.end.cert
+/testing/x509/import.sh real/mainca/west.end.cert
 iptables -A INPUT -i eth1 -s 192.0.1.0/24 -j DROP
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 iptables -I INPUT -m policy --dir in --pol ipsec -j ACCEPT
--- testing/pluto/ikev2-48-nat-cp-two-servers/westinit.sh
+++ testing/pluto/ikev2-48-nat-cp-two-servers/westinit.sh
@@ -1,4 +1,7 @@
-/testing/guestbin/swan-prep --x509
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/west.p12
+/testing/x509/import.sh real/mainca/east.end.cert
+/testing/x509/import.sh real/mainca/road.end.cert
 ipsec start
 ../../guestbin/wait-until-pluto-started
 ipsec auto --add rw-westnet-pool-x509-ipv4
