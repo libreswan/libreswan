1245de311468bf0a5dc8a37f9897ab5fd1dba0a6
Andrew Cagney <cagney@gnu.org>
2026-07-28 12:00:52 -0400

    documentation: add link entities for ipsec.conf.5 parameters
    for instance
      &leftauth;
    should generate a link to the description
    
    see #2945 patches for {pub,rsasig,ecdsa,eddsa}key documentation

--- configs/ipsec.conf.5.entities.sh
+++ configs/ipsec.conf.5.entities.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
+entity()
+{
+    echo "<!ENTITY ${1} '<link linkend=\"${2}\"><option>${3}</option></link>'>"
+}
+
 for x in "$@" ; do
     d=$(basename $(dirname $x))
     s=$(basename $x .xml)
-    echo '<!ENTITY '"$d.$s"' SYSTEM "'"$PWD/d.ipsec.conf/$d/$s.xml"'">'
+
+    # entity to include the relevant conn parameter description
+    echo "<!ENTITY ${d}.${s} SYSTEM '$PWD/d.ipsec.conf/$d/$s.xml'>"
+
+    # hack for left= and right=
+    if test "${s}" == host ; then
+	entity left ${d}.${s} left
+	entity right ${d}.${s} right
+	continue
+    fi
+
+    # entity to refer to the conn parameter definition
+    if grep -e "<option>left${s}=" "${x}" > /dev/null ; then
+	entity left${s} ${d}.${s} left${s}
+    fi
+    if grep -e "<option>right${s}=" "${x}" > /dev/null ; then
+	entity right${s} ${d}.${s} right${s}
+    fi
+    if grep -e "<option>${s}=" "${x}" > /dev/null ; then
+	entity ${s} ${d}.${s} ${s}
+    fi
 done
