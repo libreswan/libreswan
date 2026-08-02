056beb6c26eb2348cd1aebb0838e7a3ea647fc39
Andrew Cagney <cagney@gnu.org>
2026-08-02 11:38:06 -0400

    Revert "testing: run strongswan via /usr/libexec/strongswan/charon"
    pushed wrong version
    
    This reverts commit 563e8e64a0dd4ed8aab089eda959034f01fd78e5.

--- testing/guestbin/strongswan-start.sh
+++ testing/guestbin/strongswan-start.sh
@@ -36,7 +36,7 @@ EOF
 
 start_charon()
 {
-    /usr/local/libexec/strongswan/charon > OUTPUT/$(hostname).charon.log 2>&1 &
+    /usr/bin/charon-systemd > OUTPUT/$(hostname).charon.log 2>&1 &
     pid=$!
     echo ${pid} > ${pidfile}
 
