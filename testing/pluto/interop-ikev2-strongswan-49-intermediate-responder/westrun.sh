ebcd3d407261019a2e5fa8c2c503b2191297ba8f
Andrew Cagney <cagney@gnu.org>
2026-08-03 18:41:36 -0400

    testing: add still more rekeys to intermediate interop test
    note that the logs for strongswan->libreswan are misleading;
    it's not working

--- testing/pluto/interop-ikev2-strongswan-49-intermediate-responder/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-49-intermediate-responder/westrun.sh
@@ -1,15 +1,27 @@
-ipsec up intermediate-fragmentation-yes
-/testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-ipsec trafficstatus
-ipsec whack --rekey-ike --name intermediate-fragmentation-yes
-/testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-ipsec trafficstatus
-ipsec down intermediate-fragmentation-yes
-
 ipsec up intermediate-fragmentation-no
 /testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus
+
 ipsec whack --rekey-ike --name intermediate-fragmentation-no
 /testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
 ipsec trafficstatus
+
+ipsec whack --rekey-ike --name intermediate-fragmentation-no
+/testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
+
 ipsec down intermediate-fragmentation-no
+
+ipsec up intermediate-fragmentation-yes
+/testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
+
+ipsec whack --rekey-ike --name intermediate-fragmentation-yes
+/testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
+
+ipsec whack --rekey-ike --name intermediate-fragmentation-yes
+/testing/guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+ipsec trafficstatus
+
+ipsec down intermediate-fragmentation-yes
