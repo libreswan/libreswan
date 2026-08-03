453596bef590a3a1865af054156e4054bc341251
Andrew Cagney <cagney@gnu.org>
2026-08-03 18:03:13 -0400

    testing: add another rekey to ikev2-intermediate-10-rekey-ike
    ... which has the effect of confirming the two ends
    agree on the updated key

--- testing/pluto/ikev2-intermediate-10-rekey-ike/03-west-up.sh
+++ testing/pluto/ikev2-intermediate-10-rekey-ike/03-west-up.sh
@@ -2,8 +2,10 @@ ipsec up west-cuckold
 ipsec up west-cuckoo
 
 ipsec whack --rekey-ike --name west-cuckold
-
 ../../guestbin/wait-for-pluto.sh '#4: initiator rekeyed IKE SA #1'
+
+ipsec whack --rekey-ike --name west-cuckold
+../../guestbin/wait-for-pluto.sh '#5: initiator rekeyed IKE SA #4'
 
 ../../guestbin/ping-once.sh --up 192.0.2.254
 ../../guestbin/ping-once.sh --up 192.0.20.254
