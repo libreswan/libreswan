1e639748f921bf2fec3bd8b500d0b66124683bb7
Andrew Cagney <cagney@gnu.org>
2026-08-02 22:33:25 -0400

    testing: strongswan ipsec.conf ike rekey/reauth -> swanctl.conf
    Note that the auth test forces make_before_break=no

--- testing/pluto/interop-ikev2-strongswan-28-reauth/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-28-reauth/westrun.sh
@@ -1,7 +1,6 @@
 strongswan up westnet-eastnet-ikev2
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-echo "sleep 90 sec to ike to rekey "
-sleep 50
-sleep 40
+echo "sleep 20+10 so IKE re-authenticates"
+sleep 30
 echo done
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
--- testing/pluto/interop-ikev2-strongswan-29-rekey/westrun.sh
+++ testing/pluto/interop-ikev2-strongswan-29-rekey/westrun.sh
@@ -1,14 +1,6 @@
 # bring up the tunnel
 strongswan up westnet-eastnet-ikev2
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-sleep 5
-strongswan status
-../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-echo "sleep 30 sec to ike to rekey "
-sleep 30
-../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-sleep 5
-strongswan status
 echo "sleep 30 sec to ike to rekey "
 sleep 30
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
