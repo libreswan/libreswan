2df3749688d96e8f25906206b2413ea707e3ce78
Ondrej Moris <omoris@redhat.com>
2026-08-12 15:50:23 +0200

    fixup! fixup! ikev2: implementation of RFC 9611 - per-CPU Child SA

--- programs/pluto/routing.c
+++ programs/pluto/routing.c
@@ -550,8 +550,10 @@ static void set_established_inbound(struct connection *c,
 				    const struct routing_annex *e)
 {
 	struct child_sa *child = (*e->child);
-	c->routing_sa = child->sa.st_serialno;
-	c->negotiating_child_sa = child->sa.st_serialno;
+	if (child->sa.st_v2_resource_info.cpu_id == CPU_ID_NONE) {
+		c->routing_sa = child->sa.st_serialno;
+		c->negotiating_child_sa = child->sa.st_serialno;
+	}
 	c->routing.state = new_routing;
 }
 
@@ -599,11 +601,11 @@ static void set_established_outbound(struct connection *c,
 		}
 	}
 	c->routing.state = routing;
-	c->routing_sa = child->sa.st_serialno;
-	c->negotiating_child_sa = child->sa.st_serialno;
 
-	/* We want to keep trask of Initial Child SA (RFC 9611) only */
+	/* Only Initial Child SAs own the connection (RFC 9611) */
 	if (child->sa.st_v2_resource_info.cpu_id == CPU_ID_NONE) {
+		c->routing_sa = child->sa.st_serialno;
+		c->negotiating_child_sa = child->sa.st_serialno;
 		c->established_child_sa = child->sa.st_serialno;
 	}
 }
