528298d4301bbf1fd37fa4462dfcbedd6210ef55
Andrew Cagney <cagney@gnu.org>
2026-08-16 14:13:55 -0400

    testing: update check-10-topolgy output

--- testing/pluto/check-10-topology-fedora/all.nic-fedoraeast-fedoranorth-fedorapole-fedorarise-fedoraroad-fedoraset-fedorawest.sh
+++ testing/pluto/check-10-topology-fedora/all.nic-fedoraeast-fedoranorth-fedorapole-fedorarise-fedoraroad-fedoraset-fedorawest.sh
@@ -9,6 +9,8 @@ nic# ifconfig eth2 | grep -e 'inet ' -e 'inet6 .*2001:'
 north# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
 north# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
 
+pole# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
+
 rise# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
 rise# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
 
@@ -61,3 +63,9 @@ road# ../../guestbin/ping-once.sh --up 192.1.3.254 # nic_nicnet4
 road# ../../guestbin/ping-once.sh --up 192.1.2.254 # nic_internet4
 road# ../../guestbin/ping-once.sh --up 192.1.2.23 # east_internet4
 road# ../../guestbin/ping-once.sh --up 192.1.2.45 # west_internet4
+# pole to north_northnet4 north_nicnet4 nic_nicnet4 east_internet4 west_internet4
+pole# ../../guestbin/ping-once.sh --up 198.18.66.254 # north_northnet4
+pole# ../../guestbin/ping-once.sh --up 192.1.3.33 # north_nicnet4
+pole# ../../guestbin/ping-once.sh --up 192.1.3.254 # nic_nicnet4
+pole# ../../guestbin/ping-once.sh --up 192.1.2.23 # east_internet4
+pole# ../../guestbin/ping-once.sh --up 192.1.2.45 # west_internet4
--- testing/pluto/check-10-topology-netbsd/all.nic-netbsdeast-netbsdnorth-netbsdpole-netbsdrise-netbsdroad-netbsdset-netbsdwest.sh
+++ testing/pluto/check-10-topology-netbsd/all.nic-netbsdeast-netbsdnorth-netbsdpole-netbsdrise-netbsdroad-netbsdset-netbsdwest.sh
@@ -1,24 +1,26 @@
 nic# : make certain NIC is running
 
-east# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
-east# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
+east# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
+east# ifconfig vioif1 | grep -e 'inet ' -e 'inet6 .*2001:'
 
 nic# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
 nic# ifconfig eth2 | grep -e 'inet ' -e 'inet6 .*2001:'
 
-north# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
-north# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
+north# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
+north# ifconfig vioif1 | grep -e 'inet ' -e 'inet6 .*2001:'
 
-rise# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
-rise# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
+pole# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
 
-road# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
+rise# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
+rise# ifconfig vioif1 | grep -e 'inet ' -e 'inet6 .*2001:'
 
-set# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
-set# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
+road# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
 
-west# ifconfig eth0 | grep -e 'inet ' -e 'inet6 .*2001:'
-west# ifconfig eth1 | grep -e 'inet ' -e 'inet6 .*2001:'
+set# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
+set# ifconfig vioif1 | grep -e 'inet ' -e 'inet6 .*2001:'
+
+west# ifconfig vioif0 | grep -e 'inet ' -e 'inet6 .*2001:'
+west# ifconfig vioif1 | grep -e 'inet ' -e 'inet6 .*2001:'
 # east to west_internet4 nic_internet4 nic_nicnet4 north_nicnet4
 east# ../../guestbin/ping-once.sh --up 192.1.2.45 # west_internet4
 east# ../../guestbin/ping-once.sh --up 192.1.2.254 # nic_internet4
@@ -61,3 +63,9 @@ road# ../../guestbin/ping-once.sh --up 192.1.3.254 # nic_nicnet4
 road# ../../guestbin/ping-once.sh --up 192.1.2.254 # nic_internet4
 road# ../../guestbin/ping-once.sh --up 192.1.2.23 # east_internet4
 road# ../../guestbin/ping-once.sh --up 192.1.2.45 # west_internet4
+# pole to north_northnet4 north_nicnet4 nic_nicnet4 east_internet4 west_internet4
+pole# ../../guestbin/ping-once.sh --up 198.18.66.254 # north_northnet4
+pole# ../../guestbin/ping-once.sh --up 192.1.3.33 # north_nicnet4
+pole# ../../guestbin/ping-once.sh --up 192.1.3.254 # nic_nicnet4
+pole# ../../guestbin/ping-once.sh --up 192.1.2.23 # east_internet4
+pole# ../../guestbin/ping-once.sh --up 192.1.2.45 # west_internet4
