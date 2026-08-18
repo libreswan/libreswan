3b09b36fffdf7e2ca80ca077809c3901ee9ea2be
Andrew Cagney <cagney@gnu.org>
2026-08-18 15:39:37 -0400

    testing: update ikev2-x509-eddsa-01 to get ED keys from OUTPUT/strongswan
    note: this is still a skiptest as it requires a custom NSS

--- testing/pluto/ikev2-x509-eddsa-01/eastinit.sh
+++ testing/pluto/ikev2-x509-eddsa-01/eastinit.sh
@@ -1,9 +1,10 @@
+# use strongswan config to generate keys in OUTPUT/strongswan
 /testing/guestbin/swan-prep --userland strongswan
-/testing/x509/strongswan-gen.sh
-#/testing/x509/openssl-gen.sh
+/testing/x509/bin/strongswan.sh strong-ED --type ed25519
 
+# now config for pluto; importing key
 /testing/guestbin/swan-prep --nokeys
-/testing/x509/import.sh strongswan/strong-ED/strongEast.p12
+/testing/x509/import.sh $PWD/OUTPUT/strongswan/strong-ED/strongEast.p12
 
 ipsec start
 ../../guestbin/wait-until-pluto-started
