75a4a8ce19ada5e8c7e17b9f6dc7c265e2cb1db6
Andrew Cagney <cagney@gnu.org>
2026-08-20 22:40:26 -0400

    testing: add SKIPTEST ikev2-x509-eddsa-02-sendcerts
    demonstrates EdDSA certs not validating
    
    requires upstream NSS patch, hence the skip

---
+++ testing/pluto/ikev2-x509-eddsa-02-sendcerts/all.linuxeast-linuxwest.sh
@@ -0,0 +1,37 @@
+# use strongswan config to generate keys in OUTPUT/strongswan
+
+east# /testing/guestbin/swan-prep --userland strongswan
+east# /testing/x509/bin/strongswan.sh strong-ED --type ed25519
+
+# now config for pluto; importing key
+
+east# /testing/guestbin/swan-prep --nokeys
+west# /testing/guestbin/swan-prep --nokeys
+
+east# /testing/x509/import.sh $PWD/OUTPUT/strongswan/strong-ED/strongEast.p12
+west# /testing/x509/import.sh $PWD/OUTPUT/strongswan/strong-ED/strongWest.p12
+
+east# ipsec start
+west# ipsec start
+
+east# ../../guestbin/wait-until-pluto-started
+west# ../../guestbin/wait-until-pluto-started
+
+east# ipsec add eddsa-east
+west# ipsec add eddsa-west
+
+east# ipsec whack --impair suppress-retransmits
+west# ipsec whack --impair suppress-retransmits
+
+# initdone
+
+# west westrun.sh
+
+west# ipsec up eddsa-west
+west# ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
+west# ipsec trafficstatus
+west# ipsec listpubkeys
+
+# test delete/free
+
+west# ipsec delete westnet-eastnet-ikev2
