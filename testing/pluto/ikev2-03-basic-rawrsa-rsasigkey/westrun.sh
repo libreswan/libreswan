74f104d3e8b122b98ce4b2330a8daf5c638cd24b
Andrew Cagney <cagney@gnu.org>
2026-08-26 21:28:53 -0400

    testing: update ikev2-03-basic-rawrsa-rsasigkey
    - new speak
    - sanitize retransmits

--- testing/pluto/ikev2-03-basic-rawrsa-rsasigkey/westrun.sh
+++ testing/pluto/ikev2-03-basic-rawrsa-rsasigkey/westrun.sh
@@ -1,4 +1,4 @@
 # see description.txt
-ipsec auto --add west-rsasigkey-east-rsasigkey
-ipsec auto --up west-rsasigkey-east-rsasigkey
+ipsec add west-rsasigkey-east-rsasigkey
+ipsec up west-rsasigkey-east-rsasigkey # sanitize-retransmits
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
