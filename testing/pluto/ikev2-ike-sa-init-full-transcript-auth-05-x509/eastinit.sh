958356e9875ef277fb9d20b5d73156cd46a9d453
Vinnu124 <vinayaksandur04@gmail.com>
2026-09-05 14:25:12 +0530

    testing: add ike-sa-init-full-transcript-auth x509 and tamper tests

---
+++ testing/pluto/ikev2-ike-sa-init-full-transcript-auth-05-x509/eastinit.sh
@@ -0,0 +1,8 @@
+/testing/guestbin/swan-prep --nokeys
+/testing/x509/import.sh real/mainca/east.p12
+/testing/x509/import.sh real/mainca/west.end.cert
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec add east
+ipsec whack --impair suppress_retransmits
+echo "initdone"
