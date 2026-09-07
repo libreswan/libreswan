3d9ccc821ba5bd84aaa9708f670094e58a1f51df
Andrew Cagney <cagney@gnu.org>
2026-09-06 21:02:09 -0400

    testing: add more cases to addconn-22-authby

--- testing/pluto/addconn-22-authby/westrun.sh
+++ testing/pluto/addconn-22-authby/westrun.sh
@@ -1,7 +1,8 @@
 # these should load properly
 
 add() ( name=$1 ; shift ; set -x ; ipsec addconn --name ${name} "$@" ; )
-authby() ( name=$1 ; shift ; add ${name} authby=${name} "$@" ; )
+authby() ( name=$1 ; shift ; add authby=${name} authby=${name} "$@" ; )
+leftauth() ( name=$1 ; shift ; add leftauth=${name} leftauth=${name} "$@" ; )
 
 add defaults
 
@@ -11,8 +12,9 @@ authby secret
 authby never # fail
 authby never type=drop
 
-authby eaponly # fail
-authby eaponly leftautheap=tls
+authby eaponly # fails
+authby eaponly leftautheap=tls rightautheap=tls # should probably fail
+leftauth eaponly leftautheap=tls
 
 authby eddsa
 
@@ -21,6 +23,7 @@ authby ecdsa-sha2
 authby ecdsa-sha2_256
 authby ecdsa-sha2_384
 authby ecdsa-sha2_512
+authby ecdsa-sha2_256,ecdsa-sha2_384,ecdsa-sha2_512 # merges
 
 authby rsa
 authby rsasig
@@ -29,6 +32,8 @@ authby rsa-sha2
 authby rsa-sha2_256
 authby rsa-sha2_384
 authby rsa-sha2_512
+authby rsa-sha1,rsa-sha2
+authby rsa-sha2_256,rsa-sha2_384,rsa-sha2_512
 
 # these pass but should fail
 
