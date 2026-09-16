3b7819a8659097ff69592db2596d8cb1ba0a3d41
Andrew Cagney <cagney@gnu.org>
2026-09-16 19:37:13 -0400

    ikev2: on responder use submit_local_v2AUTH_signature_generator()
    ... and drop submit_v2AUTH_generate_responder_signature() et.al.
    Caller responsible for emitting AUTHENTICATION_FAILED notify

--- programs/pluto/ikev2_ike_auth.c
+++ programs/pluto/ikev2_ike_auth.c
@@ -984,7 +984,16 @@ static stf_status process_v2_IKE_AUTH_request_tail(struct state *ike_st,
 	 */
 	v2_IKE_AUTH_responder_id_payload(ike);
 
-	return submit_v2AUTH_generate_responder_signature(ike, md, process_v2_IKE_AUTH_request_auth_signature_continue);
+	if (!submit_local_v2AUTH_signature_generator(ike, md,
+						     process_v2_IKE_AUTH_request_auth_signature_continue)) {
+		record_v2N_response(ike->sa.logger, ike, md,
+				    v2N_AUTHENTICATION_FAILED,
+				    empty_shunk/*no data*/,
+				    ENCRYPTED_PAYLOAD);
+		return STF_FATAL;
+	}
+
+	return STF_SUSPEND;
 }
 
 bool v2_ike_sa_auth_responder_establish(struct ike_sa *ike, bool *send_redirection)
