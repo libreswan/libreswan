9770a024e609e99927aa473791b5b208112d89b0
Andrew Cagney <cagney@gnu.org>
2026-09-17 17:44:11 -0400

    testing: update tests matching helper task numbers

--- testing/pluto/crossing-streams-21-second-child-crypto-ikev2/06-west-cross.sh
+++ testing/pluto/crossing-streams-21-second-child-crypto-ikev2/06-west-cross.sh
@@ -2,7 +2,7 @@
 # needs to finish DH so expect it it be submitted to the crypto
 # helper.
 
-../../guestbin/drip-inbound.sh 1 'IMPAIR: "cuckoo" #3: task 4, dh for process_v2_CREATE_CHILD_SA_child_response'
+../../guestbin/drip-inbound.sh 1 'IMPAIR: "cuckoo" #3: task 5'
 
 # Now process the peer's CREATE_CHILD_SA request.  This two needs a
 # full crypto calculation so expect it to be sent to the crypto
@@ -10,7 +10,7 @@
 #
 # XXX: it is tied to #1/#1 but should be #1/#4
 
-../../guestbin/drip-inbound.sh 2 'IMPAIR: "cuckoo" #4: task 5'
+../../guestbin/drip-inbound.sh 2 'IMPAIR: "cuckoo" #4: task 6'
 
 # finally wait for things to establish
 ../../guestbin/wait-for-pluto.sh '#4: responder established Child SA using #1'
