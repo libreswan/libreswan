d20d1ed46b5058114bfd539ca1ae2d0c3027d062
Andrew Cagney <andrew.cagney@gmail.com>
2026-09-22 18:39:04 -0400

    testing: update linux-audit-01-ok-ikev1
    - newspeak
    - sanitize-retransmits

--- testing/pluto/linux-audit-01-ok-ikev1/westrun.sh
+++ testing/pluto/linux-audit-01-ok-ikev1/westrun.sh
@@ -1,5 +1,5 @@
-ipsec up ikev1
-ipsec delete ikev1
-ipsec up ikev1-aggr
-ipsec delete ikev1-aggr
+ipsec up ikev1 # sanitize-retransmits
+ipsec delete ikev1 # sanitize-retransmits
+ipsec up ikev1-aggr # sanitize-retransmits
+ipsec delete ikev1-aggr # sanitize-retransmits
 echo done
