1e79793cf3485d252fdbbcd1c7dd32cd1e282c1c
Vinnu124 <vinayaksandur04@gmail.com>
2026-09-24 22:25:49 +0530

    testing: add tests for the global ike-sa-init-full-transcript-auth

---
+++ testing/pluto/ikev2-ike-sa-init-full-transcript-auth-07-global-override/final.sh
@@ -0,0 +1,4 @@
+ipsec showstates | grep full-transcript-auth
+grep 'v2AUTH transcript' /tmp/pluto.log | sort -u
+ipsec _kernel state
+ipsec _kernel policy
---
+++ testing/pluto/ikev2-ike-sa-init-full-transcript-auth-07-global-override/westinit.sh
@@ -0,0 +1,7 @@
+/testing/guestbin/swan-prep --nokeys
+../../guestbin/wait-until-alive -I 192.0.1.254 192.0.2.254
+ipsec start
+../../guestbin/wait-until-pluto-started
+ipsec add westnet-eastnet-ipv4-psk-ikev2
+ipsec whack --impair suppress_retransmits
+echo "initdone"
