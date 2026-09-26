8970c707c3211931adcc4407d13b1e5fcfdd2aec
Andrew Cagney <andrew.cagney@gmail.com>
2026-09-25 22:38:01 -0400

    testing: newspeak+sanitize-retransmits ikev1-impair-01-replay-duplicates

--- testing/pluto/ikev1-impair-01-replay-duplicates/westrun.sh
+++ testing/pluto/ikev1-impair-01-replay-duplicates/westrun.sh
@@ -1,5 +1,5 @@
-ipsec up westnet-eastnet
+ipsec up westnet-eastnet # sanitize-retransmits
 ../../guestbin/ping-once.sh --up -I 192.0.1.254 192.0.2.254
-ipsec whack --trafficstatus
-ipsec down  westnet-eastnet
+ipsec trafficstatus
+ipsec down westnet-eastnet
 echo done
